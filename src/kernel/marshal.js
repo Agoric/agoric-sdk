d848762a3266cf2757f0cc653dc3f4f2a71b22e4
Brian Warner <warner@lothar.com>
2019-03-12 00:00:11 -0700

    add marshalling code from PlaygroundVat
    * modified to use slots instead of webkeys
    * Presences are not yet proxies for method calls

---
+++ src/kernel/marshal.js
@@ -0,0 +1,339 @@
+import harden from '@agoric/harden';
+import Nat from '@agoric/nat';
+
+// Special property name that indicates an encoding that needs special
+// decoding.
+const QCLASS = '@qclass';
+
+// objects can only be passed in one of two/three forms:
+// 1: pass-by-presence: all properties (own and inherited) are methods,
+//    the object itself is of type object, not function
+// 2: pass-by-copy: all string-named own properties are data, not methods
+//    the object must inherit from Object.prototype or null
+// 3: the empty object is pass-by-presence, for identity comparison
+
+// todo: maybe rename pass-by-presence to pass-as-presence, or pass-by-proxy
+// or remote reference
+
+// all objects must be frozen
+
+// anything else will throw an error if you try to serialize it
+
+// with these restrictions, our remote call/copy protocols expose all useful
+// behavior of these objects: pass-by-presence objects have no other data (so
+// there's nothing else to copy), and pass-by-copy objects have no other
+// behavior (so there's nothing else to invoke)
+
+function canPassByCopy(val) {
+  if (!Object.isFrozen(val)) {
+    return false;
+  }
+  if (typeof val !== 'object') {
+    return false;
+  }
+  const names = Object.getOwnPropertyNames(val);
+  const hasFunction = names.some(name => typeof val[name] === 'function');
+  if (hasFunction) return false;
+  const p = Object.getPrototypeOf(val);
+  if (p !== null && p !== Object.prototype && p !== Array.prototype) {
+    // todo: arrays should also be Array.isArray(val)
+    return false;
+  }
+  if (names.length === 0) {
+    // empty objects are pass-by-presence, not pass-by-copy
+    return false;
+  }
+  return true;
+}
+
+function mustPassByPresence(val) {
+  // throws exception if cannot
+  if (!Object.isFrozen(val)) {
+    throw new Error(`cannot serialize non-frozen objects like ${val}`);
+  }
+  if (typeof val !== 'object') {
+    throw new Error(`cannot serialize non-objects like ${val}`);
+  }
+
+  const names = Object.getOwnPropertyNames(val);
+  names.forEach(name => {
+    if (name === 'e') {
+      // hack to allow Vows to pass-by-presence
+      return;
+    }
+    if (typeof val[name] !== 'function') {
+      throw new Error(
+        `cannot serialize objects with non-methods like the .${name} in ${val}`,
+      );
+      // return false;
+    }
+  });
+
+  const p = Object.getPrototypeOf(val);
+  if (p !== null && p !== Object.prototype) {
+    mustPassByPresence(p);
+  }
+  // ok!
+}
+
+function makePresence(slotID) {
+  return harden({
+    test_getSlotID() {
+      return slotID;
+    },
+  });
+}
+
+export default function makeMarshal() {
+  const valToSlotID = new WeakMap();
+  const slotIDToVal = new Map();
+  let nextExportID = 1;
+
+  function allocateExportID() {
+    const exportID = nextExportID;
+    nextExportID += 1;
+    return exportID;
+  }
+
+  function serializeSlot(val, slots, slotMap) {
+    mustPassByPresence(val);
+    if (!valToSlotID.has(val)) {
+      console.log('must be a new export', JSON.stringify(val));
+      // must be a new export
+      const exportID = allocateExportID();
+      valToSlotID.set(val, exportID);
+      slotIDToVal.set(exportID, val);
+    }
+    const slotID = valToSlotID.get(val);
+    console.log(`serializeSlot slotID=${slotID}`);
+    if (!slotMap.has(val)) {
+      const slotIndex = slots.length;
+      slots.push(slotID);
+      slotMap.set(val, slotIndex);
+    }
+    const slotIndex = slotMap.get(val);
+    return harden({ [QCLASS]: 'slot', index: slotIndex });
+  }
+
+  function makeReplacer(slots, slotMap) {
+    const ibidMap = new Map();
+    let ibidCount = 0;
+
+    return function replacer(_, val) {
+      // First we handle all primitives. Some can be represented directly as
+      // JSON, and some must be encoded as [QCLASS] composites.
+      switch (typeof val) {
+        case 'object': {
+          if (val === null) {
+            return null;
+          }
+          if (!Object.isFrozen(val)) {
+            console.log(
+              'asked to serialize',
+              val,
+              typeof val,
+              Object.isFrozen(val),
+            );
+            throw new Error(
+              `non-frozen objects like ${val} are disabled for now`,
+            );
+          }
+          break;
+        }
+        case 'function': {
+          throw new Error(`bare functions like ${val} are disabled for now`);
+        }
+        case 'undefined': {
+          return harden({ [QCLASS]: 'undefined' });
+        }
+        case 'string':
+        case 'boolean': {
+          return val;
+        }
+        case 'number': {
+          if (Number.isNaN(val)) {
+            return harden({ [QCLASS]: 'NaN' });
+          }
+          if (Object.is(val, -0)) {
+            return harden({ [QCLASS]: '-0' });
+          }
+          if (val === Infinity) {
+            return harden({ [QCLASS]: 'Infinity' });
+          }
+          if (val === -Infinity) {
+            return harden({ [QCLASS]: '-Infinity' });
+          }
+          return val;
+        }
+        case 'symbol': {
+          const optKey = Symbol.keyFor(val);
+          if (optKey === undefined) {
+            // TODO: Symmetric unguessable identity
+            throw new TypeError('Cannot serialize unregistered symbol');
+          }
+          return harden({
+            [QCLASS]: 'symbol',
+            key: optKey,
+          });
+        }
+        case 'bigint': {
+          return harden({
+            [QCLASS]: 'bigint',
+            digits: String(val),
+          });
+        }
+        default: {
+          // TODO non-std exotic objects are allowed other typeofs.
+          // Perhaps a warning and break would be better.
+          throw new TypeError(`unrecognized typeof ${typeof val}`);
+        }
+      }
+
+      // Now that we've handled all the primitives, it is time to deal with
+      // objects. The only things which can pass this point are frozen and
+      // non-null.
+
+      if (QCLASS in val) {
+        // TODO Hilbert hotel
+        throw new Error(`property "${QCLASS}" reserved`);
+      }
+
+      // if we've seen this object before, serialize a backref
+
+      if (ibidMap.has(val)) {
+        // Backreference to prior occurrence
+        return harden({
+          [QCLASS]: 'ibid',
+          index: ibidMap.get(val),
+        });
+      }
+      ibidMap.set(val, ibidCount);
+      ibidCount += 1;
+
+      // We can serialize some things as plain pass-by-copy: arrays, and
+      // objects with one or more data properties but no method properties.
+
+      // todo: handle this properly, by constructing a @qclass: error
+      if (val instanceof Error) {
+        console.log('cannot yet serialize Errors correctly', val);
+        console.log('stack was:', val);
+        throw new Error('cannot yet serialize Errors correctly');
+      }
+
+      if (canPassByCopy(val)) {
+        // console.log(`canPassByCopy: ${val}`);
+        // Purposely in-band for readability, but creates need for
+        // Hilbert hotel.
+        return val;
+      }
+
+      // beyond this point it must either be a proxy (created by an inbound
+      // call) or a pass-by-presence object
+      console.log(`serializeSlot: ${val}`);
+      return serializeSlot(val, slots, slotMap);
+    };
+  }
+
+  // val might be a primitive, a pass by (shallow) copy object, a
+  // remote reference, or other.  We treat all other as a local object
+  // to be exported as a local webkey.
+  function serialize(val) {
+    const slots = [];
+    const slotMap = new Map(); // maps val (proxy or presence) to index of slots[]
+    return {
+      argsString: JSON.stringify(val, makeReplacer(slots, slotMap)),
+      slots,
+    };
+  }
+
+  function unserializeSlot(data, slots) {
+    const slotID = slots[Nat(data.index)];
+    // todo assert slotID is a non-zero integer
+    if (slotID < 0) {
+      // this is an import
+      if (!slotIDToVal.has(slotID)) {
+        // this is a new import
+        console.log(`assigning new import ${slotID}`);
+        const val = makePresence(slotID);
+        valToSlotID.set(val, slotID);
+        slotIDToVal.set(slotID, val);
+      }
+    }
+    // else this is one of our previous exports. In either case we get it
+    // from the map
+    return slotIDToVal.get(slotID);
+  }
+
+  function makeReviver(slots) {
+    const ibids = [];
+
+    return function reviver(_, data) {
+      if (Object(data) !== data) {
+        // primitives pass through
+        return data;
+      }
+      if (QCLASS in data) {
+        const qclass = `${data[QCLASS]}`;
+        switch (qclass) {
+          // Encoding of primitives not handled by JSON
+          case 'undefined': {
+            return undefined;
+          }
+          case '-0': {
+            return -0;
+          }
+          case 'NaN': {
+            return NaN;
+          }
+          case 'Infinity': {
+            return Infinity;
+          }
+          case '-Infinity': {
+            return -Infinity;
+          }
+          case 'symbol': {
+            return Symbol.for(data.key);
+          }
+          case 'bigint': {
+            /* eslint-disable-next-line no-undef */
+            return BigInt(data.digits);
+          }
+
+          case 'ibid': {
+            const index = Nat(data.index);
+            if (index >= ibids.length) {
+              throw new RangeError(`ibid out of range: ${index}`);
+            }
+            return ibids[index];
+          }
+
+          case 'slot': {
+            data = unserializeSlot(data, slots);
+            // overwrite data and break to ibid registration.
+            break;
+          }
+          default: {
+            // TODO reverse Hilbert hotel
+            throw new TypeError(`unrecognized ${QCLASS} ${qclass}`);
+          }
+        }
+      } else {
+        // The unserialized copy also becomes pass-by-copy, but we don't need
+        // to mark it specially
+        // todo: what if the unserializer is given "{}"?
+      }
+      // The ibids case returned early to avoid this.
+      ibids.push(data);
+      return harden(data);
+    };
+  }
+
+  function unserialize(str, slots) {
+    return JSON.parse(str, makeReviver(slots));
+  }
+
+  return harden({
+    serialize,
+    unserialize,
+  });
+}
---
+++ test/test-marshal.js
@@ -0,0 +1,161 @@
+/* globals BigInt */
+
+import { test } from 'tape-promise/tape';
+import harden from '@agoric/harden';
+import makeMarshal from '../src/kernel/marshal';
+
+test('serialize static data', t => {
+  const m = makeMarshal();
+  const ser = val => m.serialize(val);
+  t.throws(
+    () => ser([1, 2]),
+    /non-frozen objects like .* are disabled for now/,
+  );
+  t.deepEqual(ser(harden([1, 2])), { argsString: '[1,2]', slots: [] });
+  t.deepEqual(ser(harden({ foo: 1 })), { argsString: '{"foo":1}', slots: [] });
+  t.deepEqual(ser(true), { argsString: 'true', slots: [] });
+  t.deepEqual(ser(1), { argsString: '1', slots: [] });
+  t.deepEqual(ser('abc'), { argsString: '"abc"', slots: [] });
+  t.deepEqual(ser(undefined), {
+    argsString: '{"@qclass":"undefined"}',
+    slots: [],
+  });
+  t.deepEqual(ser(-0), { argsString: '{"@qclass":"-0"}', slots: [] });
+  t.deepEqual(ser(NaN), { argsString: '{"@qclass":"NaN"}', slots: [] });
+  t.deepEqual(ser(Infinity), {
+    argsString: '{"@qclass":"Infinity"}',
+    slots: [],
+  });
+  t.deepEqual(ser(-Infinity), {
+    argsString: '{"@qclass":"-Infinity"}',
+    slots: [],
+  });
+  t.deepEqual(ser(Symbol.for('sym1')), {
+    argsString: '{"@qclass":"symbol","key":"sym1"}',
+    slots: [],
+  });
+  let bn;
+  try {
+    bn = BigInt(4);
+  } catch (e) {
+    if (!(e instanceof ReferenceError)) {
+      throw e;
+    }
+  }
+  if (bn) {
+    t.deepEqual(ser(bn), {
+      argsString: '{"@qclass":"bigint","digits":"4"}',
+      slots: [],
+    });
+  }
+  t.end();
+});
+
+test('unserialize static data', t => {
+  const m = makeMarshal();
+  const uns = val => m.unserialize(val, []);
+  t.equal(uns('1'), 1);
+  t.equal(uns('"abc"'), 'abc');
+  t.equal(uns('false'), false);
+
+  // JS primitives that aren't natively representable by JSON
+  t.deepEqual(uns('{"@qclass":"undefined"}'), undefined);
+  t.ok(Object.is(uns('{"@qclass":"-0"}'), -0));
+  t.notOk(Object.is(uns('{"@qclass":"-0"}'), 0));
+  t.ok(Object.is(uns('{"@qclass":"NaN"}'), NaN));
+  t.deepEqual(uns('{"@qclass":"Infinity"}'), Infinity);
+  t.deepEqual(uns('{"@qclass":"-Infinity"}'), -Infinity);
+  t.deepEqual(uns('{"@qclass":"undefined"}'), undefined);
+  t.deepEqual(uns('{"@qclass":"symbol", "key":"sym1"}'), Symbol.for('sym1'));
+  let bn;
+  try {
+    bn = BigInt(4);
+  } catch (e) {
+    if (!(e instanceof ReferenceError)) {
+      throw e;
+    }
+  }
+  if (bn) {
+    t.deepEqual(uns('{"@qclass":"bigint","digits":"1234"}'), BigInt(1234));
+  }
+
+  t.deepEqual(uns('[1,2]'), [1, 2]);
+  t.deepEqual(uns('{"a":1,"b":2}'), { a: 1, b: 2 });
+  t.deepEqual(uns('{"a":1,"b":{"c": 3}}'), { a: 1, b: { c: 3 } });
+
+  // should be frozen
+  const arr = uns('[1,2]');
+  t.ok(Object.isFrozen(arr));
+  const a = uns('{"b":{"c":{"d": []}}}');
+  t.ok(Object.isFrozen(a));
+  t.ok(Object.isFrozen(a.b));
+  t.ok(Object.isFrozen(a.b.c));
+  t.ok(Object.isFrozen(a.b.c.d));
+
+  t.end();
+});
+
+test('serialize exports', t => {
+  const m = makeMarshal();
+  const ser = val => m.serialize(val);
+  const o1 = harden({});
+  const o2 = harden({
+    meth1() {
+      return 4;
+    },
+  });
+  t.deepEqual(ser(o1), {
+    argsString: '{"@qclass":"slot","index":0}',
+    slots: [1],
+  });
+  // m now remembers that o1 is exported as 1
+  t.deepEqual(ser(harden([o1, o1])), {
+    argsString: '[{"@qclass":"slot","index":0},{"@qclass":"ibid","index":1}]',
+    slots: [1],
+  });
+  t.deepEqual(ser(harden([o2, o1])), {
+    argsString: '[{"@qclass":"slot","index":0},{"@qclass":"slot","index":1}]',
+    slots: [2, 1],
+  });
+
+  t.end();
+});
+
+test('deserialize imports', t => {
+  const m = makeMarshal();
+  const a = m.unserialize('{"@qclass":"slot","index":0}', [-1]);
+  // a should be a proxy/presence. For now these are obvious.
+  t.equal(a.test_getSlotID(), -1);
+  t.ok(Object.isFrozen(a));
+
+  // m now remembers the proxy
+  const b = m.unserialize('{"@qclass":"slot","index":0}', [-1]);
+  t.is(a, b);
+
+  // the slotid is what matters, not the index
+  const c = m.unserialize('{"@qclass":"slot","index":2}', ['x', 'x', -1]);
+  t.is(a, c);
+
+  t.end();
+});
+
+test('deserialize exports', t => {
+  const m = makeMarshal();
+  const o1 = harden({});
+  m.serialize(o1); // allocates slot=1
+  const a = m.unserialize('{"@qclass":"slot","index":0}', [1]);
+  t.is(a, o1);
+
+  t.end();
+});
+
+test('serialize imports', t => {
+  const m = makeMarshal();
+  const a = m.unserialize('{"@qclass":"slot","index":0}', [-1]);
+  t.deepEqual(m.serialize(a), {
+    argsString: '{"@qclass":"slot","index":0}',
+    slots: [-1],
+  });
+
+  t.end();
+});
