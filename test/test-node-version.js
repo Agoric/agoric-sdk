90f9e677b5b070a59a27d3c8dd00cb2e46addf66
Brian Warner <warner@lothar.com>
2019-03-14 09:49:35 -0700

    demand Node 11
    We need the IO queue to be higher priority than the Promise queue, so we can
    detect when all resolved promises have settled down.

--- test/test-node-version.js
+++ test/test-node-version.js
@@ -2,6 +2,9 @@ import semver from 'semver';
 import { test } from 'tape-promise/tape';
 
 test('Node version', t => {
-  t.true(semver.satisfies(process.version, '>=10.15.1'));
+  t.true(
+    semver.satisfies(process.version, '>=11.11.0'),
+    'we need the IO queue to be higher priority than the Promise queue',
+  );
   t.end();
 });
