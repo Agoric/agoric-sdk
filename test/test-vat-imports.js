ba8db3fa5f16585a633e566dc730d073f84529d7
Brian Warner <warner@lothar.com>
2019-03-27 12:11:22 -0700

    add require(@agoric/evaluate) and tests

--- test/test-vat-imports.js
+++ test/test-vat-imports.js
@@ -34,3 +34,20 @@ test('vat can require harden without SES', async t => {
   await requireHarden(t, false);
   t.end();
 });
+
+async function requireEvaluate(t, withSES) {
+  const config = { bootstrapIndexJS: require.resolve('./vat-imports-1.js') };
+  const c = await buildVatController(config, withSES, ['evaluate']);
+  await c.step();
+  t.deepEqual(c.dump().log, ['evaluate-1', '3', '4', '5']);
+}
+
+test('vat can require evaluate with SES', async t => {
+  await requireEvaluate(t, true);
+  t.end();
+});
+
+test('vat can require evaluate without SES', async t => {
+  await requireEvaluate(t, false);
+  t.end();
+});
