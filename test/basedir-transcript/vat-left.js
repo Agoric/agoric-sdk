8d09f09a26826bc79106f27e8b90594acfc38f87
Brian Warner <warner@lothar.com>
2019-04-03 13:11:32 -0700

    fix saveState(), start on loadState()

--- test/basedir-transcript/vat-left.js
+++ test/basedir-transcript/vat-left.js
@@ -9,39 +9,6 @@ function build(E, log) {
         .then(a => log(`left.then ${a}`));
       return 3;
     },
-    call2(arg) {
-      log(`left.call2 ${arg}`);
-      return harden({
-        call3(x) {
-          log(`left.call3 ${x}`);
-          return 3;
-        },
-      });
-    },
-
-    returnArg(arg) {
-      log(`left.returnArg`);
-      return arg;
-    },
-
-    returnMyObject() {
-      return harden({
-        foo(x) {
-          log(`left.myobject.call ${x}`);
-        },
-      });
-    },
-
-    takePromise(p1) {
-      log(`left.takePromise`);
-      return harden(
-        p1.then(t1 => {
-          log(`left.takePromise.then`);
-          E(t1).foo(1);
-          return 4;
-        }),
-      );
-    },
   };
   return harden(obj0);
 }
