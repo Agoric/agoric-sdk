20b4e600ddd987d1a341f98817117be90ee2b4e0
Brian Warner <warner@lothar.com>
2019-04-03 21:03:53 -0700

    demo/left-right: cosmetic changes

--- demo/left-right/bootstrap.js
+++ demo/left-right/bootstrap.js
@@ -1,19 +1,20 @@
 import harden from '@agoric/harden';
 
-console.log(`loading bootstrap`);
+console.log(`loading bootstrap.js`);
 
 export default function setup(syscall, state, helpers) {
   function log(what) {
     helpers.log(what);
     console.log(what);
   }
-  log(`bootstrap called`);
+  log(`setup called`);
   return helpers.makeLiveSlots(
     syscall,
     state,
     E =>
       harden({
         bootstrap(argv, vats) {
+          console.log('bootstrap() called');
           E(vats.left)
             .callRight(1, vats.right)
             .then(r => log(`b.resolved ${r}`), err => log(`b.rejected ${err}`));
