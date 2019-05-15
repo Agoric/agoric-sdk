9a94e77de06594100deb5d573cf1f92f415cd5a8
Brian Warner <warner@lothar.com>
2019-05-15 14:45:34 -0700

    devices/channel: add notes on how to use from different sides

--- src/devices/channel.js
+++ src/devices/channel.js
@@ -1,5 +1,32 @@
 import harden from '@agoric/harden';
 
+/*
+
+This "Channel Device" enables "inbound" communication from the host
+environment into a set of vats, and "outbound" communication from those vats
+into the host environment.
+
+The device calls these different sides "machines", each of which has a name.
+Every message is sent from one machine (`fromMachineName`) and to a different
+machine (`toMachineName`). The message body itself is always a string.
+
+To send a message from the host environment, call `deliverInbound(from, to,
+data)`. This `deliverInbound` is part of the object returned by
+`buildChannel()`.
+
+To send a message from a vat (given access to the device node, held in a
+variable named `devNode`), use `D(devNode).sendOutbound(from, to, data)`.
+
+To receive messages in the host environment, call
+`registerOutboundCallback(to, f)` (which is part of the object returned by
+`buildChannel`). The callback function will be invoked like `f(from, data)`.
+
+To receive messages from a vat, use `D(devNode).registerInboundHandler(to,
+handler)`. The handler object will be invoked like `handler.inbound(from,
+data)`.
+
+*/
+
 export default function buildChannel() {
   const bridge = harden(new Map());
   const srcPath = require.resolve('./channel-src');
