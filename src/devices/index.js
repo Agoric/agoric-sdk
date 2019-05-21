b13a3b4aff863919a09a7cbf7cc1c3d7fec93268
Michael FIG <mfig@agoric.com>
2019-05-21 09:49:43 -0700

    Reenable bin/vat
    Add missing src/devices/index.js with named exports from the devices'
    top-halves.
    
    fixes #51

---
+++ src/devices/index.js
@@ -0,0 +1,5 @@
+// Export all the default exports of the driver top-halves.
+export { default as buildChannel } from './channel';
+export { default as buildInbound } from './inbound';
+export { default as buildOutbox } from './outbox';
+export { default as buildSharedStringTable } from './sharedTable';
