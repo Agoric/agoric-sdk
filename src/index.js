307a672990ff6a55fffaa6e8a3bfc4faf845886d
Brian Warner <warner@lothar.com>
2019-05-22 14:23:26 -0700

    export buildMailbox and friends as top-level exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { loadBasedir, buildVatController } from './controller';
+import { buildMailboxStateMap, buildMailbox } from './devices/mailbox';
 
-export { loadBasedir, buildVatController };
+export { loadBasedir, buildVatController, buildMailboxStateMap, buildMailbox };
