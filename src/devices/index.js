de206103d53bb9ca92b8ce249a959c5f45f5934f
Brian Warner <warner@lothar.com>
2019-05-22 17:22:17 -0700

    delete now-unused 'channel' device

--- src/devices/index.js
+++ src/devices/index.js
@@ -1,5 +1,4 @@
 // Export all the default exports of the driver top-halves.
-export { default as buildChannel } from './channel';
 export { default as buildInbound } from './inbound';
 export { buildMailboxStateMap, buildMailbox } from './mailbox';
 export { default as buildSharedStringTable } from './sharedTable';
