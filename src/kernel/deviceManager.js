c4a30582906d774a35369c8c3298752e059937c5
Kate Sills <katelynsills@gmail.com>
2019-05-23 17:12:24 -0700

    comment out dispatch.getState

--- src/kernel/deviceManager.js
+++ src/kernel/deviceManager.js
@@ -132,13 +132,13 @@ export default function makeDeviceManager(
 
   function loadState(savedState) {
     deviceKeeper.loadManagerState(savedState.managerState);
-    dispatch.setState(savedState.deviceState);
+    // dispatch.setState(savedState.deviceState);
   }
 
   function getCurrentState() {
     return harden({
       managerState: deviceKeeper.getManagerState(),
-      deviceState: dispatch.getState(),
+      // deviceState: dispatch.getState(),
     });
   }
 
