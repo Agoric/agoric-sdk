02e6df8e660b256c8152a81000a556b6906767ad
Brian Warner <warner@lothar.com>
2019-07-31 13:30:20 -0700

    require Node.js v12
    There's a new ordering issue that requires Node.js >= v12.
    
    closes #99

--- test/test-node-version.js
+++ test/test-node-version.js
@@ -4,8 +4,8 @@ import { test } from 'tape-promise/tape';
 
 test('Node version', t => {
   t.true(
-    semver.satisfies(process.version, '>=11.0'),
-    'we need Node 11 where the IO queue is higher priority than the Promise queue',
+    semver.satisfies(process.version, '>=12.0'),
+    'we need Node 11 where the IO queue is higher priority than the Promise queue, and Node 12 for a different ordering issue (see #99 for details)',
   );
   t.end();
 });
