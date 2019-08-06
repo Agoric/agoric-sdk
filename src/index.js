4b5aaf8cdbf711132880e53da466bfbb1dde1db2
Brian Warner <warner@lothar.com>
2019-08-06 14:55:10 -0700

    hush eslint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import {
-  loadBasedir,
-  buildVatController,
-} from './controller';
+import { loadBasedir, buildVatController } from './controller';
 import { buildMailboxStateMap, buildMailbox } from './devices/mailbox';
 
-export {
-  loadBasedir,
-  buildVatController,
-  buildMailboxStateMap,
-  buildMailbox,
-};
+export { loadBasedir, buildVatController, buildMailboxStateMap, buildMailbox };
 
 export function getVatTPSourcePath() {
   return require.resolve('./vat-tp/vattp');
