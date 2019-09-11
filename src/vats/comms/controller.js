284d8e4fb0f41c1ad43ed3ac7f4d42a8f72a74e2
Brian Warner <warner@lothar.com>
2019-09-11 14:21:07 -0700

    switch to 'CapData': update comms
    This probably also fixes some lurking bugs w.r.t. messages that arrive in the
    comms layer for promises that it knows to have been resolved earlier. We
    don't yet have tests for this case, partially because I don't know how to
    provoke it from the higher-level ("ocap layer" / on-top-of-liveslots) code.

--- src/vats/comms/controller.js
+++ src/vats/comms/controller.js
@@ -1,9 +1,13 @@
 import Nat from '@agoric/nat';
+import harden from '@agoric/harden';
 import { addRemote } from './remote';
 import { addEgress, addIngress } from './clist';
 import { insist } from '../../insist';
 
-const UNDEFINED = JSON.stringify({ '@qclass': 'undefined' });
+const UNDEFINED = harden({
+  body: JSON.stringify({ '@qclass': 'undefined' }),
+  slots: [],
+});
 
 // deliverToController() is used for local vats which want to talk to us as a
 // vat, rather than as a conduit to talk to remote vats. The bootstrap
@@ -17,21 +21,29 @@ const UNDEFINED = JSON.stringify({ '@qclass': 'undefined' });
 export function deliverToController(
   state,
   method,
-  data,
-  slots,
+  controllerArgs,
   result,
   syscall,
 ) {
-  function doAddRemote(args) {
+  // We use a degenerate form of deserialization, just enough to handle the
+  // handful of methods implemented by the commsController. 'args.body' can
+  // normally have arbitrary {'@qclass': whatever} objects, but we only
+  // handle {'@qclass':'slot', index} objects, which point into the
+  // 'args.slots' array.
+
+  function doAddRemote() {
     // comms!addRemote(name, tx, setRx)
     //  we then do setRx!setReceiver(rx)
+    const args = JSON.parse(controllerArgs.body);
+    const { slots } = controllerArgs;
+
     const name = args[0];
     insist(name === `${name}`, `bad addRemote name ${name}`);
     if (args[1]['@qclass'] !== 'slot' || args[1].index !== 0) {
-      throw new Error(`unexpected args for addRemote(): ${data}`);
+      throw new Error(`unexpected args for addRemote(): ${controllerArgs}`);
     }
     if (args[2]['@qclass'] !== 'slot' || args[2].index !== 1) {
-      throw new Error(`unexpected args for addRemote(): ${data}`);
+      throw new Error(`unexpected args for addRemote(): ${controllerArgs}`);
     }
     const transmitterID = slots[args[1].index];
     const setReceiverID = slots[args[2].index];
@@ -39,30 +51,38 @@ export function deliverToController(
     const { receiverID } = addRemote(state, name, transmitterID);
 
     const rxArg = { '@qclass': 'slot', index: 0 };
-    const setReceiverBody = JSON.stringify({ args: [rxArg] });
-    syscall.send(setReceiverID, 'setReceiver', setReceiverBody, [receiverID]);
+    const setReceiverArgs = harden({
+      body: JSON.stringify([rxArg]),
+      slots: [receiverID],
+    });
+    syscall.send(setReceiverID, 'setReceiver', setReceiverArgs);
     // todo: consider, this leaves one message (setReceiver) on the queue,
     // rather than giving the caller of comms!addRemote() something to
     // synchronize upon. I don't think it hurts, but might affect debugging.
-    syscall.fulfillToData(result, UNDEFINED, []);
+    syscall.fulfillToData(result, UNDEFINED);
   }
 
-  function doAddEgress(args) {
+  function doAddEgress() {
     // comms!addEgress(name, index, obj)
+    const args = JSON.parse(controllerArgs.body);
+    const { slots } = controllerArgs;
+
     const remoteName = args[0];
     insist(state.names.has(remoteName), `unknown remote name ${remoteName}`);
     const remoteID = state.names.get(remoteName);
     const remoteRefID = Nat(args[1]);
     if (args[2]['@qclass'] !== 'slot' || args[2].index !== 0) {
-      throw new Error(`unexpected args for addEgress(): ${data}`);
+      throw new Error(`unexpected args for addEgress(): ${controllerArgs}`);
     }
     const localRef = slots[args[2].index];
     addEgress(state, remoteID, remoteRefID, localRef);
-    syscall.fulfillToData(result, UNDEFINED, []);
+    syscall.fulfillToData(result, UNDEFINED);
   }
 
-  function doAddIngress(args) {
+  function doAddIngress() {
     // obj = comms!addIngress(name, index)
+    const args = JSON.parse(controllerArgs.body);
+
     const remoteName = args[0];
     insist(state.names.has(remoteName), `unknown remote name ${remoteName}`);
     const remoteID = state.names.get(remoteName);
@@ -71,20 +91,13 @@ export function deliverToController(
     syscall.fulfillToPresence(result, localRef);
   }
 
-  // This is a degenerate form of deserialization, just enough to handle the
-  // handful of methods implemented by the commsController. 'argsbytes' can
-  // normally have arbitrary {'@qclass': whatever} objects, but we only
-  // handle {'@qclass':'slot', index} objects, which point into the 'slots'
-  // array.
-  const { args } = JSON.parse(data);
-
   switch (method) {
     case 'addRemote':
-      return doAddRemote(args);
+      return doAddRemote();
     case 'addEgress':
-      return doAddEgress(args);
+      return doAddEgress();
     case 'addIngress':
-      return doAddIngress(args);
+      return doAddIngress();
     default:
       throw new Error(`method ${method} is not available`);
   }
