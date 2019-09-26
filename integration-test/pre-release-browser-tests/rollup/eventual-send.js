2e4bddb4ba9c91daf9cd58761fb89a3a2233ff73
Michael FIG <mfig@agoric.com>
2019-09-25 21:21:32 -0600

    lint fix

--- integration-test/pre-release-browser-tests/rollup/eventual-send.js
+++ integration-test/pre-release-browser-tests/rollup/eventual-send.js
@@ -1,4 +1,5 @@
 /* eslint-disable-next-line import/no-unresolved */
 import { maybeExtendPromise } from '@agoric/eventual-send';
 
+// eslint-disable-next-line import/prefer-default-export
 export { maybeExtendPromise };
