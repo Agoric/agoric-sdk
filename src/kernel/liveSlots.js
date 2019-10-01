9a29faf4058965246fd4e7d5a0fa91d94b7fb89b
Brian Warner <warner@lothar.com>
2019-09-30 19:14:17 -0700

    liveSlots: import E() from eventual-send
    .. instead of defining it locally. We have to drop a couple of safety
    features, like detecting/complaining about E(E(x)) and E(devicenode).
    
    TODO: we rely upon the imported E to harden the promise returned by
    E(x).foo() like our EPromiseHandler used to, to handle issue #95, since we
    cannot reach this promise to handle externally. The current eventual-send
    branch does not harden it, because it might be used outside of SES where
    harden() isn't necessarily available.

--- src/kernel/liveSlots.js
+++ src/kernel/liveSlots.js
@@ -1,5 +1,6 @@
 import harden from '@agoric/harden';
 import Nat from '@agoric/nat';
+import { E } from '@agoric/eventual-send';
 import { QCLASS, mustPassByPresence, makeMarshal } from '@agoric/marshal';
 import { insist } from '../insist';
 import { insistVatType, makeVatSlot, parseVatSlot } from '../parseVatSlots';
@@ -207,82 +208,6 @@ function build(syscall, _state, makeRoot, forVatID) {
 
     return done.p;
   }
-
-  /**
-   * A Proxy handler for E(x).
-   *
-   * @param {Promise} ep Promise with eventual send API
-   * @returns {ProxyHandler} the Proxy handler
-   */
-  function EPromiseHandler(ep) {
-    return {
-      get(_target, p, _receiver) {
-        if (`${p}` !== p) {
-          return undefined;
-        }
-        // Harden this Promise because it's our only opportunity to ensure
-        // p1=E(x).foo() is hardened. The Handled Promise API does not (yet)
-        // allow the handler to synchronously influence the promise returned
-        // by the handled methods, so we must freeze it from the outside. See
-        // #95 for details.
-        return (...args) => harden(ep.post(p, args));
-      },
-      deleteProperty(_target, p) {
-        return harden(ep.delete(p));
-      },
-      set(_target, p, value, _receiver) {
-        return harden(ep.put(p, value));
-      },
-      apply(_target, _thisArg, argArray = []) {
-        return harden(ep.post(undefined, argArray));
-      },
-      has(_target, _p) {
-        // We just pretend everything exists.
-        return true;
-      },
-    };
-  }
-
-  function E(x) {
-    // p = E(x).name(args)
-    //
-    // E(x) returns a proxy on which you can call arbitrary methods. Each of
-    // these method calls returns a promise. The method will be invoked on
-    // whatever 'x' designates (or resolves to) in a future turn, not this
-    // one. 'x' might be/resolve-to:
-    //
-    // * a local object: do x[name](args) in a future turn
-    // * a normal Promise: wait for x to resolve, then x[name](args)
-    // * a Presence: send message to remote Vat to do x[name](args)
-    // * a Promise that we returned earlier: send message to whichever Vat
-    //   gets to decide what the Promise resolves to
-
-    if (outstandingProxies.has(x)) {
-      throw Error('E(E(x)) is invalid, you probably want E(E(x).foo()).bar()');
-    }
-
-    const slot = valToSlot.get(x);
-
-    if (slot && parseVatSlot(slot).type === 'device') {
-      throw new Error(`E() does not accept device nodes`);
-    }
-
-    lsdebug(` treating as promise`);
-    const targetP = Promise.resolve(x);
-
-    // targetP might resolve to a Presence
-    const handler = EPromiseHandler(targetP);
-
-    const p = harden(new Proxy({}, handler));
-    outstandingProxies.add(p);
-    return p;
-  }
-  // Like Promise.resolve, except that if applied to a presence, it
-  // would be better for it to return the remote promise for this
-  // specimen, rather than a fresh local promise fulfilled by this
-  // specimen.
-  // TODO: for now, just alias Promise.resolve.
-  E.resolve = specimen => Promise.resolve(specimen);
 
   function DeviceHandler(slot) {
     return {
