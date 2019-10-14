1f6aecdbabb07bfa952a75098aa1d80090e6dbab
Mark S. Miller <erights@users.noreply.github.com>
2019-10-14 10:56:03 -0700

    loosen eslint rules a bit (#12)

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,16 @@ module.exports = {
     'arrow-parens': 'off',
     strict: 'off',
     'no-console': 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-unused-vars': ['error', {
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_'
+    }],
     'no-return-assign': 'off',
     'no-param-reassign': 'off',
     'no-restricted-syntax': ['off', 'ForOfStatement'],
     'no-unused-expressions': 'off',
     'no-loop-func': 'off',
+    'no-inner-declarations': 'off',
     'import/prefer-default-export': 'off', // contrary to Agoric standard
   },
 };
