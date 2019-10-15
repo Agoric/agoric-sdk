59f11cbf72850dc8f0447e1c082978298e914a3c
Kate Sills <kate@agoric.com>
2019-10-15 16:44:25 -0700

    refactor public swap (#157)

--- core/zoe/contracts/publicSwap.js
+++ core/zoe/contracts/publicSwap.js
@@ -1,91 +1,9 @@
 import harden from '@agoric/harden';
 
-import makePromise from '../../../util/makePromise';
-
 const makeContract = harden(zoe => {
-  const isValidOffer = (
-    extentOps,
-    offerIds,
-    offerIdsToOfferDescs,
-    offerMadeDesc,
-  ) => {
-    const makeHasOkRules = validRules => offer =>
-      validRules.every((rule, i) => rule === offer[i].rule, true);
-
-    const ruleEqual = (leftRule, rightRule) => leftRule.rule === rightRule.rule;
-
-    const extentEqual = (extentOp, leftRule, rightRule) =>
-      extentOp.equals(leftRule.assetDesc.extent, rightRule.assetDesc.extent);
-
-    const assayEqual = (leftRule, rightRule) =>
-      leftRule.assetDesc.label.assay === rightRule.assetDesc.label.assay;
-
-    const makeSecondOffer = firstOffer =>
-      harden([
-        {
-          rule: firstOffer[1].rule,
-          assetDesc: firstOffer[0].assetDesc,
-        },
-        {
-          rule: firstOffer[0].rule,
-          assetDesc: firstOffer[1].assetDesc,
-        },
-      ]);
-
-    // Check that two offers are equal in both their rules and their assetDescs
-    const offerEqual = (leftOffer, rightOffer) => {
-      const isLengthEqual = leftOffer.length === rightOffer.length;
-      if (!isLengthEqual) {
-        return false;
-      }
-      return leftOffer.every(
-        (leftRule, i) =>
-          ruleEqual(leftRule, rightOffer[i]) &&
-          assayEqual(leftRule, rightOffer[i]) &&
-          extentEqual(extentOps[i], leftRule, rightOffer[i]),
-        true,
-      );
-    };
-
-    const isFirstOffer = offerIds.length === 0;
-    const isSecondOffer = offerIds.length === 1;
-    const hasOkRulesOfferFirst = makeHasOkRules([
-      'offerExactly',
-      'wantExactly',
-    ]);
-    const hasOkRulesWantFirst = makeHasOkRules(['wantExactly', 'offerExactly']);
-    const isValidFirstOfferDesc = newOfferDesc =>
-      hasOkRulesOfferFirst(newOfferDesc) || hasOkRulesWantFirst(newOfferDesc);
-    const isValidSecondOfferDesc = (firstOffer, newOfferDesc) =>
-      offerEqual(makeSecondOffer(firstOffer), newOfferDesc);
-    return (
-      (isFirstOffer && isValidFirstOfferDesc(offerMadeDesc)) ||
-      (isSecondOffer &&
-        isValidSecondOfferDesc(
-          offerIdsToOfferDescs.get(offerIds[0]),
-          offerMadeDesc,
-        ))
-    );
-  };
-
-  const canReallocate = (offerIds, _offerIdsToOfferDescs) =>
-    offerIds.length === 2;
-  const reallocate = (
-    _extentOps,
-    offerIds,
-    _offerIdsToOfferDescs,
-    getExtentsFor,
-  ) =>
-    harden({
-      reallocOfferIds: offerIds,
-      reallocExtents: harden([
-        ...getExtentsFor(harden([offerIds[1]])),
-        ...getExtentsFor(harden([offerIds[0]])),
-      ]),
-    });
-
-  const offerIdsToOfferDescs = new WeakMap();
-  const offerIds = [];
+  let firstOfferId;
+  let firstOfferDesc;
+  let matchingOfferId;
 
   return harden({
     makeOffer: async escrowReceipt => {
@@ -93,42 +11,68 @@ const makeContract = harden(zoe => {
         escrowReceipt,
       );
 
-      const status = makePromise();
+      const isMatchingOfferDesc = (leftOffer, rightOffer) => {
+        // "matching" means that assetDescs are the same, but that the
+        // rules have switched places in the array
+        return (
+          leftOffer[0].assetDesc.extent === rightOffer[0].assetDesc.extent &&
+          leftOffer[1].assetDesc.extent === rightOffer[1].assetDesc.extent &&
+          leftOffer[0].assetDesc.label.assay ===
+            rightOffer[0].assetDesc.label.assay &&
+          leftOffer[1].assetDesc.label.assay ===
+            rightOffer[1].assetDesc.label.assay &&
+          leftOffer[0].rule === rightOffer[1].rule &&
+          leftOffer[1].rule === rightOffer[0].rule
+        );
+      };
+
+      const isFirstOffer = firstOfferId === undefined;
+
+      const isValidFirstOfferDesc = newOfferDesc =>
+        ['offerExactly', 'wantExactly'].every(
+          (rule, i) => rule === newOfferDesc[i].rule,
+          true,
+        );
+
+      const isValidOffer =
+        (isFirstOffer && isValidFirstOfferDesc(offerMadeDesc)) ||
+        (!isFirstOffer && isMatchingOfferDesc(firstOfferDesc, offerMadeDesc));
 
       // Eject if the offer is invalid
-      if (
-        !isValidOffer(
-          zoe.getExtentOps(),
-          offerIds,
-          offerIdsToOfferDescs,
-          offerMadeDesc,
-        )
-      ) {
+      if (!isValidOffer) {
         zoe.complete(harden([id]));
-        status.rej('The offer was invalid. Please check your refund.');
-        return status.p;
+        return Promise.reject(
+          new Error(`The offer was invalid. Please check your refund.`),
+        );
       }
 
-      // Save the offer.
-      offerIdsToOfferDescs.set(id, offerMadeDesc);
-      offerIds.push(id);
+      // Save the valid offer
+      if (firstOfferId === undefined) {
+        firstOfferId = id;
+        firstOfferDesc = offerMadeDesc;
+      } else {
+        matchingOfferId = id;
+      }
 
       // Check if we can reallocate and reallocate.
-      if (canReallocate(offerIds, offerIdsToOfferDescs)) {
-        const { reallocOfferIds, reallocExtents } = reallocate(
-          zoe.getExtentOps(),
+      if (matchingOfferId !== undefined) {
+        const offerIds = harden([firstOfferId, matchingOfferId]);
+        const [firstOfferExtents, matchingOfferExtents] = zoe.getExtentsFor(
           offerIds,
-          offerIdsToOfferDescs,
-          zoe.getExtentsFor,
         );
-        zoe.reallocate(reallocOfferIds, reallocExtents);
+        // reallocate by switching the extents of the firstOffer and matchingOffer
+        zoe.reallocate(
+          offerIds,
+          harden([matchingOfferExtents, firstOfferExtents]),
+        );
         zoe.complete(offerIds);
-      }
 
-      status.res(
-        'The offer has been accepted. Once the contract has been completed, please check your winnings',
-      );
-      return status.p;
+        // clear state and start over
+        firstOfferId = undefined;
+        firstOfferDesc = undefined;
+        matchingOfferId = undefined;
+      }
+      return `The offer has been accepted. Once the contract has been completed, please check your winnings`;
     },
   });
 });
