fb3630a8759a15c94965475682ecfbe5f3b7fe1b
Michael FIG <michael+github@fig.org>
2019-10-19 10:44:27 -0600

    fix(captp): inband CTP_ABORT to reject all pending promises

--- test/disco.js
+++ test/disco.js
@@ -3,9 +3,15 @@ import { harden, makeCapTP } from '../lib/captp';
 
 test('try disconnecting captp', async t => {
   try {
-    const { dispatch, getBootstrap, disconnected } = makeCapTP('us', obj => {}, () => harden({}));
-    const pr = t.rejects(getBootstrap(), Error, 'rejected after disconnect');
-    disconnected();
+    const objs = [];
+    const { dispatch, getBootstrap } = makeCapTP('us', obj => objs.push(obj), () => harden({}));
+    t.deepEqual(objs, [], 'expected no messages');
+    const bs = getBootstrap();
+    t.deepEqual(objs, [{ type: 'CTP_BOOTSTRAP', questionID: 1 }], 'expected bootstrap messages');
+    const pr = t.rejects(bs, Error, 'rejected after disconnect');
+    const abort = { type: 'CTP_ABORT', exception: Error('disconnect') };
+    dispatch(abort);
+    t.deepEqual(objs, [{ type: 'CTP_BOOTSTRAP', questionID: 1 }, abort], 'expected disconnect messages');
     await pr;
   } catch (e) {
     t.isNot(e, e, 'unexpected exception');
