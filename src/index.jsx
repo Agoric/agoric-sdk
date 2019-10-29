ea670e28735b239c87b2de911fae5ca14e723b5f
JF Paradis <jfparadis@users.noreply.github.com>
2019-10-29 09:58:14 -0700

    Theme using Agoric red.

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,30 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import ApplicationContextProvider from './contexts/Application';
 import App from './pages/App';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#AB2328',
+    },
+    secondary: {
+      main: '#508AA8',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
 render(
   <ApplicationContextProvider>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </ApplicationContextProvider>,
   document.getElementById('root'),
 );
