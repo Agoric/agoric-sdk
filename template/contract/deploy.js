5a983da5a661b6eae6a68164492da7c73281446d
Brian Warner <warner@lothar.com>
2019-11-02 12:52:32 -0700

    deploy simpleSwap, not automaticRefund

--- template/contract/deploy.js
+++ template/contract/deploy.js
@@ -4,8 +4,8 @@ import fs from 'fs';
 const DAPP_NAME = "@DIR@";
 
 export default async function deployContract(homeP, { bundleSource, pathResolve }) {
-  // Create a source bundle for the Zoe automaticRefeund contract.
-  const { source, moduleFormat } = await bundleSource(`./automaticRefund.js`);
+  // Create a source bundle for the Zoe simpleSwap contract.
+  const { source, moduleFormat } = await bundleSource(`./simpleSwap.js`);
 
   const contractHandle = homeP~.zoe~.install(source, moduleFormat);
   const contractID = await homeP~.registrar~.register(DAPP_NAME, contractHandle);
