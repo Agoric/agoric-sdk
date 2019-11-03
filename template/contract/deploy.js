18a8fc64b807cfb48455451758ad227f370b7913
Brian Warner <warner@lothar.com>
2019-11-02 18:00:44 -0700

    Add "myFirstDapp" contract (#1)

--- template/contract/deploy.js
+++ template/contract/deploy.js
@@ -1,17 +1,85 @@
 // Agoric Dapp contract deployment script
 import fs from 'fs';
 
+import harden from '@agoric/harden';
+
 const DAPP_NAME = "@DIR@";
 
 export default async function deployContract(homeP, { bundleSource, pathResolve }) {
-  // Create a source bundle for the Zoe automaticRefeund contract.
-  const { source, moduleFormat } = await bundleSource(`./automaticRefund.js`);
+  // Create a source bundle for the "myFirstDapp" smart contract.
+  const { source, moduleFormat } = await bundleSource(`./myFirstDapp.js`);
 
-  const contractHandle = homeP~.zoe~.install(source, moduleFormat);
-  const contractID = await homeP~.registrar~.register(DAPP_NAME, contractHandle);
+  const installationHandle = await homeP~.zoe~.install(source, moduleFormat);
+  const contractId = await homeP~.registrar~.register(DAPP_NAME, installationHandle);
   const contractsJson = JSON.stringify({
-    [DAPP_NAME]: contractID,
+    [DAPP_NAME]: contractId,
   });
+
+  const CONTRACT_NAME = 'myFirstDapp';
+
+  console.log('- myFirstDapp installed', CONTRACT_NAME, '=>',  installationHandle);
+  
+  // 1. Assays
+  const assays = await Promise.all([
+    homeP~.moolaMint~.getAssay(),
+    homeP~.simoleanMint~.getAssay(),
+  ]);
+
+  // 2. Contract instance.
+  try {
+    await homeP~.zoe~.makeInstance(installationHandle, { assays });
+  } catch(e) {}
+  const { instance, instanceHandle, terms } = await homeP~.zoe~.makeInstance(installationHandle, { assays });
+
+  // 3. Offer rules
+  const units = await Promise.all([
+    terms~.assays~.[0]~.makeUnits(10000),
+    terms~.assays~.[1]~.makeUnits(10000),
+    terms~.assays~.[2]~.makeUnits(0),
+  ]);
+
+  const offerRules = harden({
+    payoutRules: [
+      {
+        kind: 'offerExactly',
+        units: units[0],
+      },
+      {
+        kind: 'offerExactly',
+        units: units[1],
+      },
+      {
+        kind: 'wantAtLeast',
+        units: units[2],
+      },
+    ],
+    exitRule: {
+      kind: 'onDemand',
+    },
+  });
+
+  // 4. Payments (from mint, not from purse)
+
+  const faucets = await Promise.all([
+    homeP~.moolaMint~.mint(units[0]),
+    homeP~.simoleanMint~.mint(units[1]),
+  ]);
+
+  const payments = await Promise.all([
+    faucets[0]~.withdrawAll(),
+    faucets[1]~.withdrawAll(),
+  ]);
+
+  // 5. Liquidities.
+  const { escrowReceipt } = await homeP~.zoe~.escrow(offerRules, payments);
+  const liquidityOk = await instance~.addLiquidity(escrowReceipt);
+  console.log(liquidityOk);
+
+  if (liquidityOk) {
+    // Only store if the contract instance has liquidities.    
+    const instanceId = await homeP~.registrar~.register(CONTRACT_NAME, instanceHandle);
+    console.log('- Autoswap instance', CONTRACT_NAME, '=>', instanceId);
+  }
 
   // Save the contractID somewhere where the UI can find it.
   const cjfile = pathResolve(`../api/contracts.json`);
