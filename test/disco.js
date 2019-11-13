b9ac6b578731013fc5d8debf45b4904a48dabb98
Michael FIG <michael+github@fig.org>
2019-11-13 15:47:15 -0600

    feat(abort): provide a separate function to abort the connection

--- test/disco.js
+++ test/disco.js
@@ -4,14 +4,14 @@ import { harden, makeCapTP } from '../lib/captp';
 test('try disconnecting captp', async t => {
   try {
     const objs = [];
-    const { dispatch, getBootstrap } = makeCapTP('us', obj => objs.push(obj), () => harden({}));
+    const { getBootstrap, abort } = makeCapTP('us', obj => objs.push(obj), () => harden({}));
     t.deepEqual(objs, [], 'expected no messages');
     const bs = getBootstrap();
     t.deepEqual(objs, [{ type: 'CTP_BOOTSTRAP', questionID: 1 }], 'expected bootstrap messages');
     const pr = t.rejects(bs, Error, 'rejected after disconnect');
-    const abort = { type: 'CTP_ABORT', exception: Error('disconnect') };
-    dispatch(abort);
-    t.deepEqual(objs, [{ type: 'CTP_BOOTSTRAP', questionID: 1 }, abort], 'expected disconnect messages');
+    const abortMsg = { type: 'CTP_ABORT', exception: Error('disconnect') };
+    abort(abortMsg.exception);
+    t.deepEqual(objs, [{ type: 'CTP_BOOTSTRAP', questionID: 1 }, abortMsg], 'expected disconnect messages');
     await pr;
   } catch (e) {
     t.isNot(e, e, 'unexpected exception');
