16447e8b7851625d017055cfe70608772a34dd9b
Kate Sills <kate@agoric.com>
2020-01-14 11:43:04 -0800

    move tests from test-makeMint.js to test-mint.js and add test for an alternative mintKeeper

--- packages/ERTP/test/unitTests/core/test-mint.js
+++ packages/ERTP/test/unitTests/core/test-mint.js
@@ -1,6 +1,9 @@
 import { test } from 'tape-promise/tape';
+import harden from '@agoric/harden';
 
 import { makeMint } from '../../../core/mint';
+import { makeCoreMintKeeper } from '../../../core/config/coreMintKeeper';
+import { makePrivateName } from '../../../util/PrivateName';
 
 test('split bad units', t => {
   try {
@@ -249,4 +252,216 @@ test('claimAll goodUnits', async t => {
   } finally {
     t.end();
   }
+});
+
+test('makeMint with no config', t => {
+  t.plan(1);
+
+  const mint = makeMint('test');
+  const purse = mint.mint(mint.getAssay().makeUnits(1000));
+
+  t.equal(purse.getBalance().extent, 1000);
+});
+
+// This tests calling makeMint with a partial config
+// Right now, partial configs throw
+// In the future (https://github.com/Agoric/agoric-sdk/issues/324), partial configs
+// will be completed by makeMint
+test('makeMint with partial config', t => {
+  t.plan(1);
+
+  function makeMintTrait(_coreMint) {
+    return {
+      get37: () => 37,
+    };
+  }
+
+  t.throws(() => {
+    makeMint('test', { makeMintTrait });
+  });
+});
+
+function completeConfig(partialConfig) {
+  return {
+    makeAssayTrait: _makeMintContext => _self => {
+      return {};
+    },
+    makePaymentTrait: _makeMintContext => _self => {
+      return {};
+    },
+    makePurseTrait: _makeMintContext => _self => {
+      return {};
+    },
+    makeMintTrait: _makeMintContext => _self => {
+      return {};
+    },
+    makeMintKeeper: makeCoreMintKeeper,
+    extentOpsName: 'natExtentOps',
+    extentOpsArgs: [],
+    ...partialConfig,
+  };
+}
+
+test('makeMint with specific makePaymentTrait config', t => {
+  t.plan(2);
+
+  function makePaymentTrait(_makeMintContext) {
+    return _corePayment => ({
+      get37: () => 37,
+    });
+  }
+
+  const mint = makeMint('test', completeConfig({ makePaymentTrait }));
+  const purse = mint.mint(12);
+  const payment = purse.withdrawAll();
+
+  t.equals(payment.getBalance().extent, 12);
+  t.equals(payment.get37(), 37);
+});
+
+test('makeMint with specific makePurseTrait config', t => {
+  t.plan(2);
+
+  function makePurseTrait(_makeMintContext) {
+    return _corePurse => ({
+      get37: () => 37,
+    });
+  }
+
+  const mint = makeMint('test', completeConfig({ makePurseTrait }));
+  const purse = mint.mint(13);
+
+  t.equals(purse.getBalance().extent, 13);
+  t.equals(purse.get37(), 37);
+});
+
+test('makeMint with specific makeMintTrait config', t => {
+  t.plan(2);
+
+  function makeMintTrait(_makeMintContext) {
+    return _coreMint => ({
+      get37: () => 37,
+    });
+  }
+
+  const mint = makeMint('test bloublou', completeConfig({ makeMintTrait }));
+
+  t.equals(mint.getAssay().getLabel().allegedName, 'test bloublou');
+  t.equals(mint.get37(), 37);
+});
+
+test('makeMint with specific makeAssayTrait config', t => {
+  t.plan(2);
+
+  function makeAssayTrait(_makeMintContext) {
+    return _coreAssay => ({
+      get37: () => 37,
+    });
+  }
+
+  const mint = makeMint('test bloublou', completeConfig({ makeAssayTrait }));
+  const assay = mint.getAssay();
+
+  t.equals(assay.getLabel().allegedName, 'test bloublou');
+  t.equals(assay.get37(), 37);
+});
+
+test('makeMint with alternative mintKeeper', t => {
+  // This mint will stop making tokens once a cap is reached (aka,
+  // when currentSupply === supplyCap)
+  const makeCappedSupplyMintKeeper = unitOps => {
+    let currentSupply = unitOps.empty();
+    const supplyCap = unitOps.make(3);
+
+    function makeAssetKeeper() {
+      // asset to units
+      const units = makePrivateName();
+      return harden({
+        updateUnits(asset, newUnits) {
+          const oldBalance = units.get(asset);
+          let newSupply;
+
+          // we are decreasing supply
+          if (unitOps.includes(oldBalance, newUnits)) {
+            const difference = unitOps.without(oldBalance, newUnits);
+            newSupply = unitOps.without(currentSupply, difference);
+            // we are increasing supply
+          } else if (unitOps.includes(newUnits, oldBalance)) {
+            const difference = unitOps.without(newUnits, oldBalance);
+            newSupply = unitOps.with(currentSupply, difference);
+          }
+          if (unitOps.includes(newSupply, supplyCap)) {
+            throw new Error(
+              'We have reached the supply cap and cannot create new assets',
+            );
+          }
+          currentSupply = newSupply;
+          units.set(asset, newUnits);
+        },
+        recordNew(asset, initialUnits) {
+          const newSupply = unitOps.with(currentSupply, initialUnits);
+          // Have we passed the supply cap?
+          if (unitOps.includes(newSupply, supplyCap)) {
+            throw new Error(
+              'We have reached the supply cap and cannot create new assets',
+            );
+          }
+          currentSupply = newSupply;
+          units.init(asset, initialUnits);
+        },
+        getUnits(asset) {
+          return units.get(asset);
+        },
+        has(asset) {
+          return units.has(asset);
+        },
+        remove(asset) {
+          const balance = units.get(asset);
+          currentSupply = unitOps.without(currentSupply, balance);
+          units.delete(asset);
+        },
+      });
+    }
+
+    const purseKeeper = makeAssetKeeper();
+    const paymentKeeper = makeAssetKeeper();
+
+    const mintKeeper = harden({
+      purseKeeper,
+      paymentKeeper,
+      isPurse(asset) {
+        return purseKeeper.has(asset);
+      },
+      isPayment(asset) {
+        return paymentKeeper.has(asset);
+      },
+    });
+    return mintKeeper;
+  };
+
+  const mint = makeMint(
+    'test capped supply',
+    completeConfig({ makeMintKeeper: makeCappedSupplyMintKeeper }),
+  );
+  const assay = mint.getAssay();
+
+  t.equals(assay.getLabel().allegedName, 'test capped supply');
+  const purse1 = mint.mint(1);
+  t.equals(purse1.getBalance().extent, 1);
+  const purse2 = mint.mint(1);
+  t.equals(purse2.getBalance().extent, 1);
+  t.throws(
+    () => mint.mint(1),
+    /We have reached the supply cap and cannot create new assets/,
+  );
+
+  // TODO: we do not yet conserve currency over each call, so the
+  // following will fail. We need to rewrite the operations such that
+  // if a payment is created from a purse, the two updated amounts are
+  // done in one call.
+  // const payment1 = purse1.withdrawAll();
+  // assay.burnAll(payment1);
+  // const purse3 = mint.mint(1);
+  // t.equals(purse3.getBalance().extent, 1);
+  t.end();
 });
