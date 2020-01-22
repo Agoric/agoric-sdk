c22c962a6965b2f85e3029112f16755e226bc1ad
Michael FIG <mfig@agoric.com>
2020-01-22 15:20:05 -0600

    fix(template): use yarn workspaces to install

--- packages/agoric-cli/lib/install.js
+++ packages/agoric-cli/lib/install.js
@@ -13,34 +13,10 @@ export default async function installMain(progname, rawArgs, priv) {
     cp.on('error', () => resolve(-1));
   });
 
-  const spawnOutput = (cmd, args = [], opts = {}) => new Promise((resolve, reject) => {
-    let buf = '';
-    const cp = spawn(cmd, args, { ...opts, stdio: ['inherit', 'pipe', 'inherit' ] });
-    cp.stdout.on('data', chunk => buf += String(chunk));
-    cp.on('exit', () => resolve(buf));
-    cp.on('error', () => resolve(buf));
-  });
-
-  // Install Yarn.
-  const yarn = `${__dirname}/../node_modules/.bin/yarn`;
-  const pmRet = await pspawn(yarn, ['--version']);
-  const pm = pmRet === 0 ? yarn : 'npm';
-
-  // Check for golang version 1.12+.
-  const goVsn = await spawnOutput('go', ['version']);
-  const match = goVsn.match(/^go version go1\.(\d+)\./);
-  const goCmd = match && Number(match[1]) >= 12 &&
-    pspawn('npm', ['install'], { cwd: '.agservers', stdio: 'inherit' });
-
-  const iRets = await Promise.all([
-    // pspawn(pm, ['install'], { cwd: 'ui', stdio: 'inherit' }),
-    pspawn(pm, ['install'], { cwd: 'api', stdio: 'inherit' }),
-    pspawn(pm, ['install'], { cwd: 'contract', stdio: 'inherit' }),
-    goCmd,
-  ]);
-
-  if (!goCmd) {
-    console.log(chalk.bold.yellow(`To run Agoric locally you will need to install Go 1.12+ and rerun '${progname} install'`));
-  }
+  // Install via Yarn.
+  if (await pspawn('yarn', ['install'], { stdio: 'inherit'})) {
+    error('Cannot install');
+    return 1;
+  };
   console.log(chalk.bold.green('Done installing'));
 }
