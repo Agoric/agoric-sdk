9eee270beeeef415bad3a988c7ee890523f9d7e8
Michael FIG <mfig@agoric.com>
2020-02-24 15:31:38 -0800

    fix(ag-lcd): tolerate cosmos/cosmos-sdk#5592

--- packages/deployment/bigdipper/ag-lcd.sh
+++ packages/deployment/bigdipper/ag-lcd.sh
@@ -6,5 +6,8 @@ ncf=`curl -Ss https://testnet.agoric.com/network-config`
 l=`echo "$ncf" | jq '.rpcAddrs | length'`
 eval rp=`echo "$ncf" | jq ".rpcAddrs[$(( RANDOM % l ))]"`
 eval cn=`echo "$ncf" | jq '.chainName'`
-exec ag-cosmos-helper rest-server --node="tcp://$rp" --chain-id="$cn"
+# FIXME: Once https://github.com/cosmos/cosmos-sdk/issues/5592 is resolved,
+# we can use:
+#exec ag-cosmos-helper rest-server --node="tcp://$rp" --chain-id="$cn"
+exec ag-cosmos-helper rest-server --node="tcp://$rp" --trust-node
 exit $?
