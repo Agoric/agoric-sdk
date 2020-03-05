8ad86c174e84e9ef4327135afa38b66fb339e0f7
Michael FIG <mfig@agoric.com>
2020-03-04 18:27:09 -0600

    build: install new agoric-wallet-build

---
+++ packages/agoric-cli/agoric-wallet-build/precache-manifest.f8c384bc7269326d82d2a3e81dc36019.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f8f789856f9c5e3dc707563b24aff1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c8ade19d8d9338df3d21",
+    "url": "./static/js/2.f41d39ba.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "./static/js/2.f41d39ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7551803308688e683b2",
+    "url": "./static/js/main.10025eb6.chunk.js"
+  },
+  {
+    "revision": "ce519bdb244b322bcd66",
+    "url": "./static/js/runtime-main.6d19b3b8.js"
+  }
+]);
