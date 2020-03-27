94150029b2b8a3a5b618e5d933105968f690b9c4
David Bruant <davidbruant@protonmail.com>
2020-03-28 00:49:59 +0100

    opera tickets auditorium part before Alice

--- packages/zoe/src/contracts/operaConcertTicket.js
+++ packages/zoe/src/contracts/operaConcertTicket.js
@@ -1,22 +1,26 @@
 import { makeZoeHelpers, defaultAcceptanceMsg } from './helpers/zoeHelpers';
 
+import produceIssuer from '@agoric/ertp';
+
 /*
-    Roles in the arrangement:
-    - Contract creator: describes the contract with:
-        - number of seats, show, date/time of start
-        - expected (ERTP) amount per ticket (we assume all tickets cost the same)
-    - Smart Contract: 
-        - mints the tickets
-        - provides the seats
-    - Auditorium (unique contract seat, usually taken by the contract creator): the person hosting 
-    the Opera show, selling the tickets and getting the payment back
-    - Ticket buyers (contract seat created on demand): 
-        - can see the available opera show seats
-        - can consult the terms
-        - can redeem the zoe invite with the proper payment to get the ticket back
+  Roles in the arrangement:
+  - Contract creator: describes the contract with:
+    - number of seats, show, date/time of start
+    - expected (ERTP) amount per ticket (we assume all tickets cost the same)
+  - Smart Contract: 
+    - mints the tickets
+    - provides the seats
+  - Auditorium (unique contract seat, usually taken by the contract creator): the person hosting 
+  the Opera show, selling the tickets and getting the payment back
+  - Ticket buyers (contract seat created on demand): 
+    - can see the available opera show seats
+    - can consult the terms
+    - can redeem the zoe invite with the proper payment to get the ticket back
 */
 
-export const makeContract = harden(zoe => {
+export const makeContract = harden(async zoe => {
+
+  const zoeService = zoe.getZoeService();
 
   const {
     rejectOffer,
@@ -27,7 +31,31 @@ export const makeContract = harden(zoe => {
     assertKeywords,
   } = makeZoeHelpers(zoe);
 
+  // Create the internal ticket mint
+  const { issuer, mint, amountMath } = produceIssuer('Opera tickets', 'set');
+  await zoe.addNewIssuer(issuer, 'Auditorium');
+  
+  const { terms: { show, start, count, expectedAmountPerTicket } } = zoe.getInstanceRecord();
+  
+  const ticketDescriptionObjects = Array(count)
+    .fill()
+    .map((_, i) => harden({ show, start, number: i + 1 }));
 
+  const availableTicketAmountsByTicketNumber = new Map(
+    ticketDescriptionObjects.map(description => [description.number, amountMath.make(harden([description]))])
+  )
+
+  // Create the offers in Zoe for the tickets
+  const ticketPaymentInviteHandles = await Promise.all([...availableTicketAmountsByTicketNumber.values()].map(amount => {
+    const {invite, inviteHandle} = zoe.makeInvite()
+    zoeService.redeem(
+      invite,
+      harden({want: {Buyer: expectedAmountPerTicket}, give: {Auditorium: amount}}),
+      harden({Auditorium: mint.mintPayment(amount)}) // mint and pass to Zoe right away
+    )
+    return inviteHandle
+  }))
+  
   const auditoriumSeat = harden({
     getSalesMoney(){
       throw `TODO`
@@ -35,11 +63,41 @@ export const makeContract = harden(zoe => {
   })
   const auditoriumInvite = zoe.makeInvite(auditoriumSeat);
 
-
   const makeBuyerInvite = () => {
     const seat = harden({
-      addOrder: () => {
-        throw `TODO`
+      performExchange: async () => {
+        const offer = zoe.getOffer(await inviteHandle)
+
+        console.log('performExchange offer', offer)
+
+        /*
+        // simple exchange code
+        const buyAssetForPrice = harden({
+          give: [PRICE],
+          want: [ASSET],
+        });
+        const sellAssetForPrice = harden({
+          give: [ASSET],
+          want: [PRICE],
+        });
+        if (checkIfProposal(inviteHandle, sellAssetForPrice)) {
+          // Save the valid offer and try to match
+          sellInviteHandles.push(inviteHandle);
+          buyInviteHandles = [...zoe.getOfferStatuses(buyInviteHandles).active];
+          return swapIfCanTrade(buyInviteHandles, inviteHandle);
+          
+        } else if (checkIfProposal(inviteHandle, buyAssetForPrice)) {
+          // Save the valid offer and try to match
+          buyInviteHandles.push(inviteHandle);
+          sellInviteHandles = [
+            ...zoe.getOfferStatuses(sellInviteHandles).active,
+          ];
+          return swapIfCanTrade(sellInviteHandles, inviteHandle);
+        } else {
+          // Eject because the offer must be invalid
+          return rejectOffer(inviteHandle);
+        }
+        */
       },
     });
     const { invite, inviteHandle } = zoe.makeInvite(seat);
@@ -48,6 +106,14 @@ export const makeContract = harden(zoe => {
 
   return harden({
     invite: auditoriumInvite,
-    publicAPI: { makeBuyerInvite },
+    publicAPI: { 
+      makeBuyerInvite,
+      getTicketIssuer(){
+        throw `TODO`
+      },
+      getAvailableTickets(){
+        return new Map(availableTicketAmountsByTicketNumber);
+      }
+    },
   });
 });
--- packages/zoe/test/unitTests/contracts/test-operaConcertTicket.js
+++ packages/zoe/test/unitTests/contracts/test-operaConcertTicket.js
@@ -23,7 +23,7 @@ Bob tries to buy ticket 1 and fails. He buys ticket #2 and #3
 Christine asks the contract for an invite and the contract answers that there are no tickets left to buy
 
 The Opera is told about the show being sold out. It gets all the moolas from the sale`, async t => {
-
+    
   // Setup initial conditions
   const { mints, issuers: defaultIssuers, moola } = setup();
   const [moolaIssuer] = defaultIssuers;
@@ -32,11 +32,11 @@ The Opera is told about the show being sold out. It gets all the moolas from the
   const zoe = makeZoe({ require });
   const inviteIssuer = zoe.getInviteIssuer();
 
-  // == Initial Opera de Bordeaux part == 
+  // === Initial Opera de Bordeaux part === 
   const { source, moduleFormat } = await bundleSource(operaConcertTicketRoot);
 
   const installationHandle = zoe.install(source, moduleFormat);
-  const auditoriumInvite = await zoe.makeInstance(installationHandle, {Buyer: moolaIssuer}, {
+  const { invite: auditoriumInvite } = await zoe.makeInstance(installationHandle, harden({Buyer: moolaIssuer}), {
     show: "Steven Universe, the Opera",
     start: "Web, March 25th 2020 at 8pm",
     count: 3,
@@ -46,9 +46,9 @@ The Opera is told about the show being sold out. It gets all the moolas from the
   console.log('auditoriumInvite', auditoriumInvite)
 
   const {
-    extent: [{ instanceHandle }],
+    extent: [{ instanceHandle: auditoriumInstanceHandle }],
   } = await inviteIssuer.getAmountOf(auditoriumInvite);
-  const { publicAPI } = zoe.getInstance(instanceHandle);
+  const { publicAPI } = zoe.getInstance(auditoriumInstanceHandle);
 
   // The auditorium redeems its invite. It contains a function to get all the moolas accumulated by the contract
   // as part of the ticket sales
@@ -56,22 +56,83 @@ The Opera is told about the show being sold out. It gets all the moolas from the
     seat: {getSalesMoney}
   } = await zoe.redeem(auditoriumInvite, harden({}), undefined);
 
-  t.equal(typeof getSalesMoney, 'function', 'getSalesMoney should be a function')
+  t.equal(typeof getSalesMoney, 'function', 'getSalesMoney should be a function');
+  
+  // The Opera makes the publicAPI function publicly available
+  publicAPI;
 
-  // The Opera makes the makeInvite function publicly available
-  const {makeBuyerInvite} = publicAPI;
+  t.equal(typeof publicAPI.makeBuyerInvite, 'function', 'makeBuyerInvite should be a function')
+  t.equal(typeof publicAPI.getTicketIssuer, 'function', 'getTicketIssuer should be a function')
+  t.equal(typeof publicAPI.getAvailableTickets, 'function', 'getAvailableTickets should be a function')
 
-  t.equal(typeof makeBuyerInvite, 'function', 'makeBuyerInvite should be a function')
 
 
-  // == Alice part ==
+  // === Alice part ===
   // Alice starts with 100 moolas
+  const alicePurse = moolaIssuer.makeEmptyPurse()
+  alicePurse.deposit(moolaMint.mintPayment(moola(100)));
 
-  // she gets an invite
+  // Alice makes an invite
+  const aliceInvite = inviteIssuer.claim(publicAPI.makeBuyerInvite().invite)
+  const {
+    extent: [{ instanceHandle: instanceHandleOfAlice }],
+  } = await inviteIssuer.getAmountOf(aliceInvite);
+
+  const {terms: termsOfAlice} = await zoe.getInstance(instanceHandleOfAlice)
+
+  // Alice checks terms
+  t.equal(termsOfAlice.show, "Steven Universe, the Opera")
+  t.equal(termsOfAlice.start, "Web, March 25th 2020 at 8pm")
+  t.equal(termsOfAlice.expectedAmountPerTicket.brand, moola(22).brand)
+  t.equal(termsOfAlice.expectedAmountPerTicket.extent, moola(22).extent)
+
+  const availableTickets = await publicAPI.getAvailableTickets()
 
   // and sees the currently available tickets
+  t.equal(availableTickets.size, 3, 'Alice should see 3 available tickets')
+  t.ok([...availableTickets.keys()].includes(1), `availableTickets contains ticket number 1`)
+  t.ok([...availableTickets.keys()].includes(2), `availableTickets contains ticket number 2`)
+  t.ok([...availableTickets.keys()].includes(3), `availableTickets contains ticket number 3`)
+
+  const aliceProposal = harden({
+    give: {Buyer: moola(22)},
+    want: {Auditorium: [...availableTickets.values()].find(t => t.extent[0].number === 1)},
+  });
+  const alicePaymentForTicket = await alicePurse.withdraw(moola(22));
+
+  const {seat: {performExchange}, payout: payoutP} = await zoe.redeem(aliceInvite, aliceProposal, {Buyer: alicePaymentForTicket})
+/*
+  try{
+    performExchange(); // this function call may be useless? See https://github.com/Agoric/agoric-sdk/issues/783
+
+    const payout = await payoutP
+    console.log('payout', payout);
+
+    const [ticketPaymentFromZoe] = await Promise.all(await payoutP);
+    const ticketPayment = await publicAPI.getTicketIssuer().claim(await ticketPaymentFromZoe)
+
+    t.equal(ticketPayment.extent.show, "Steven Universe, the Opera", 'Alice should have receieved the ticket for the correct show')
+    t.equal(ticketPayment.extent.number, 1, 'Alice should have receieved the ticket for the correct number')
+
+
+
+    // === Bob part ===
+    // Bob starts with 100 moolas
+    const bobPurse = moolaIssuer.makeEmptyPurse()
+    bobPurse.deposit(moolaMint.mintPayment(moola(100)));
+
+    // Bob makes an invite
+    const bobInvite = inviteIssuer.claim(publicAPI.makeBuyerInvite().invite)
+    const {
+      extent: [{ instanceHandle, terms }],
+    } = await inviteIssuer.getAmountOf(bobInvite);
 
   
+  }
+  catch(e){
+    console.error('caught err', e)
+    t.fail('should not throw')
+  }*/
 
-  t.end()
+  
 });
