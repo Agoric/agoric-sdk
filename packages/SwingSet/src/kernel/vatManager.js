5b1f40cd12ccc7df58cdf4a8e41c6d1b6a0481a8
Chip Morningstar <chip@fudco.com>
2020-03-27 14:14:32 -0700

    fix: Clear kernel c-list entries for promises when they are resolved

--- packages/SwingSet/src/kernel/vatManager.js
+++ packages/SwingSet/src/kernel/vatManager.js
@@ -345,6 +345,7 @@ export default function makeVatManager(
         ['notifyFulfillToPresence', vpid, slot],
         `vat[${vatID}].promise[${vpid}] fulfillToPresence failed`,
       );
+      vatKeeper.deleteCListEntry(kpid, vpid);
     } else if (kp.state === 'redirected') {
       throw new Error('not implemented yet');
     } else if (kp.state === 'fulfilledToData') {
@@ -357,6 +358,7 @@ export default function makeVatManager(
         ['notifyFulfillToData', vpid, vatData],
         `vat[${vatID}].promise[${vpid}] fulfillToData failed`,
       );
+      vatKeeper.deleteCListEntry(kpid, vpid);
     } else if (kp.state === 'rejected') {
       const vpid = mapKernelSlotToVatSlot(kpid);
       const vatData = harden({
@@ -367,6 +369,7 @@ export default function makeVatManager(
         ['notifyReject', vpid, vatData],
         `vat[${vatID}].promise[${vpid}] reject failed`,
       );
+      vatKeeper.deleteCListEntry(kpid, vpid);
     } else {
       throw new Error(`unknown kernelPromise state '${kp.state}'`);
     }
