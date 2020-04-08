b91b5b1ef72860e3d0e9e984e71cad15da0d2668
Brian Warner <warner@lothar.com>
2020-04-08 11:51:06 -0700

    chore: add kernel-side clist vpid tests
    refs #859

--- packages/SwingSet/test/test-vpid-kernel.js
+++ packages/SwingSet/test/test-vpid-kernel.js
@@ -86,7 +86,7 @@ function buildRawVat(name, kernel) {
 // 6: CT G  NOT
 // 7: CS T  NOT
 
-function resolveP(syscallA, vpid, mode, target2) {
+function doResolveSyscall(syscallA, vpid, mode, target2) {
   switch (mode) {
     case 'presence':
       syscallA.fulfillToPresence(vpid, target2);
@@ -161,7 +161,7 @@ function inCList(kernel, vatID, kpid, vpid) {
 async function doTest123(t, which, mode) {
   const kernel = buildKernel(makeEndowments());
   // vatA is our primary actor
-  const { getSyscall: getSyscallA } = buildRawVat('vatA', kernel);
+  const { log: logA, getSyscall: getSyscallA } = buildRawVat('vatA', kernel);
   // we use vatB when necessary to send messages to vatA
   const { log: logB, getSyscall: getSyscallB } = buildRawVat('vatB', kernel);
   await kernel.start(undefined); // no bootstrapVatName, so no bootstrap call
@@ -177,46 +177,123 @@ async function doTest123(t, which, mode) {
   const vatA = kernel.vatNameToID('vatA');
   const vatB = kernel.vatNameToID('vatB');
 
-  // A will need a reference to B, to send anything
+  // A will need a reference to B, to send anything, and vice versa
   const rootBvatB = 'o+0';
   const rootBkernel = kernel.addExport(vatB, rootBvatB);
   const rootBvatA = kernel.addImport(vatA, rootBkernel);
-  const p1vatA = 'p+1';
+  const rootAvatA = 'o+0';
+  const rootAkernel = kernel.addExport(vatA, rootAvatA);
+  const rootAvatB = kernel.addImport(vatB, rootAkernel);
+  const exportedP1VatA = 'p+1';
+  const exportedP1VatB = 'p+2';
   const expectedP1kernel = 'kp40';
-  const expectedP1vatB = 'p-60';
+  const importedP1VatB = 'p-60';
+  const importedP1VatA = 'p-60';
   const slot0arg = { '@qclass': 'slot', index: 0 };
+  let p1kernel;
+  let p1VatA;
+  let p1VatB;
 
-  // A: bob~.one(p1); resolve_p1(mode)
-  syscallA.send(rootBvatA, 'one', capargs([slot0arg], [p1vatA]));
-  const p1kernel = clistVatToKernel(kernel, vatA, p1vatA);
-  t.equal(p1kernel, expectedP1kernel);
+  if (which === 1) {
+    // 1: Alice creates a new promise, sends it to Bob, and resolves it
+    // A: bob~.one(p1); resolve_p1(mode) // to bob, 4, or reject
+    p1VatA = exportedP1VatA;
+    p1VatB = importedP1VatB;
+    syscallA.send(rootBvatA, 'one', capargs([slot0arg], [exportedP1VatA]));
+    p1kernel = clistVatToKernel(kernel, vatA, exportedP1VatA);
+    t.equal(p1kernel, expectedP1kernel);
+    await kernel.run();
+
+    t.deepEqual(logB.shift(), {
+      type: 'deliver',
+      targetSlot: rootBvatB,
+      method: 'one',
+      args: capargs([slot0arg], [importedP1VatB]),
+      resultSlot: null,
+    });
+    t.deepEqual(logB, []);
+
+    syscallB.subscribe(importedP1VatB);
+    await kernel.run();
+    t.deepEqual(logB, []);
+    t.equal(inCList(kernel, vatB, p1kernel, importedP1VatB), true);
+  } else if (which === 2) {
+    // 2: Bob sends a message to Alice, Alice resolves the result promise
+    // B: alice~.one()
+    // A: function one() { return resolution; }
+    p1VatB = exportedP1VatB;
+    p1VatA = importedP1VatA;
+    console.log(`exportedP1VatB ${exportedP1VatB} in vatB ${vatB}`);
+    syscallB.send(rootAvatB, 'one', capargs([], []), exportedP1VatB);
+    syscallB.subscribe(exportedP1VatB);
+    p1kernel = clistVatToKernel(kernel, vatB, p1VatB);
+    await kernel.run();
+    // expect logA to have deliver(one)
+    console.log(`importedP1VatA ${importedP1VatA}`);
+    t.deepEqual(logA.shift(), {
+      type: 'deliver',
+      targetSlot: rootAvatA,
+      method: 'one',
+      args: capargs([], []),
+      resultSlot: importedP1VatA,
+    });
+    t.deepEqual(logA, []);
+    t.deepEqual(logB, []);
+  } else if (which === 3) {
+    // 3: Bob sends a message to Alice with the promise as an argument, then
+    //    sends a second message with the same promise as the result. Then
+    //    Alice resolves the result.
+    // B: alice~.one(p1); alice~.two(result=p1)  // (more than liveslots)
+    // A: function two { return resolution }
+    p1VatB = exportedP1VatB;
+    p1VatA = importedP1VatA;
+    syscallB.send(rootAvatB, 'one', capargs([slot0arg], [exportedP1VatB]));
+    p1kernel = clistVatToKernel(kernel, vatB, p1VatB);
+    await kernel.run();
+    // expect logA to have deliver(one)
+    t.deepEqual(logA.shift(), {
+      type: 'deliver',
+      targetSlot: rootAvatA,
+      method: 'one',
+      args: capargs([slot0arg], [importedP1VatA]),
+      resultSlot: null,
+    });
+    t.deepEqual(logA, []);
+
+    syscallB.subscribe(exportedP1VatB);
+    await kernel.run();
+    t.deepEqual(logA, []);
+    t.deepEqual(logB, []);
+
+    syscallB.send(rootAvatB, 'two', capargs([], []), exportedP1VatB);
+    await kernel.run();
+    // expect logA to have deliver(two)
+    t.deepEqual(logA.shift(), {
+      type: 'deliver',
+      targetSlot: rootAvatA,
+      method: 'two',
+      args: capargs([], []),
+      resultSlot: importedP1VatA,
+    });
+    t.deepEqual(logA, []);
+  }
+
+  doResolveSyscall(syscallA, p1VatA, mode, rootBvatA);
   await kernel.run();
 
-  t.deepEqual(logB.shift(), {
-    type: 'deliver',
-    targetSlot: rootBvatB,
-    method: 'one',
-    args: capargs([slot0arg], [expectedP1vatB]),
-    resultSlot: null,
-  });
-  t.deepEqual(logB, []);
-
-  syscallB.subscribe(expectedP1vatB);
-  await kernel.run();
-  t.deepEqual(logB, []);
-  t.equal(inCList(kernel, vatB, p1kernel, expectedP1vatB), true);
-
-  resolveP(syscallA, p1vatA, mode, rootBvatA);
-  await kernel.run();
-  t.deepEqual(logB.shift(), resolutionOf(expectedP1vatB, mode, rootBvatB));
+  t.deepEqual(logB.shift(), resolutionOf(p1VatB, mode, rootBvatB));
   t.deepEqual(logB, []);
 
   // TODO: once kernel->vat resolution notification retires clist entries,
   // switch to the other set of assertions
-  t.equal(inCList(kernel, vatB, p1kernel, expectedP1vatB), true);
-  // t.equal(inCList(kernel, vatB, p1kernel, expectedP1vatB), false);
+  t.equal(inCList(kernel, vatA, p1kernel, p1VatA), true);
+  t.equal(inCList(kernel, vatB, p1kernel, p1VatB), true);
+
+  // t.equal(inCList(kernel, vatA, p1kernel, p1VatA), false);
+  // t.equal(clistKernelToVat(kernel, vatA, p1kernel), undefined);
+  // t.equal(clistVatToKernel(kernel, vatA, p1VatA), undefined);
   // t.equal(clistKernelToVat(kernel, vatB, p1kernel), undefined);
-  // t.equal(clistVatToKernel(kernel, vatB, expectedP1vatB), undefined);
+  // t.equal(clistVatToKernel(kernel, vatB, p1VatB), undefined);
 
   t.end();
 }
@@ -231,4 +308,28 @@ test('kernel vpid handling case1 data', async t => {
 
 test('kernel vpid handling case1 reject', async t => {
   await doTest123(t, 1, 'reject');
+});
+
+test('kernel vpid handling case2 presence', async t => {
+  await doTest123(t, 2, 'presence');
+});
+
+test('kernel vpid handling case2 data', async t => {
+  await doTest123(t, 2, 'data');
+});
+
+test('kernel vpid handling case2 reject', async t => {
+  await doTest123(t, 2, 'reject');
+});
+
+test('kernel vpid handling case3 presence', async t => {
+  await doTest123(t, 3, 'presence');
+});
+
+test('kernel vpid handling case3 data', async t => {
+  await doTest123(t, 3, 'data');
+});
+
+test('kernel vpid handling case3 reject', async t => {
+  await doTest123(t, 3, 'reject');
 });
