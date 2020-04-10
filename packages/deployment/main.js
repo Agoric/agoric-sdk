504a8ce6a004d08c9436ed88a39e3c63ecb5202b
Michael FIG <mfig@agoric.com>
2020-04-10 14:03:56 -0600

    fix: reinstate carrying forward public keys

--- packages/deployment/main.js
+++ packages/deployment/main.js
@@ -497,6 +497,21 @@ show-config      display the client connection parameters
         );
       });
 
+      // Install any pubkeys from a former instantiation.
+      await guardFile(`${CONTROLLER_DIR}/pubkeys.stamp`, () =>
+        needReMain([
+          'ssh',
+          'ag-pserver',
+          'sudo',
+          '-u',
+          'ag-pserver',
+          '/usr/src/app/ve3/bin/ag-pserver',
+          'add-pubkeys',
+          '-c',
+          'http://localhost:8000/private/repl',
+        ]),
+      );
+
       let pserverFlags = '';
       const installFlags = [];
       const pub = `${networkName}.crt`;
