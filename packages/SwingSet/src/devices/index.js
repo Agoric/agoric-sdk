f096e3b5afa78175f897736c2cf7f68d86dceb12
Brian Warner <warner@lothar.com>
2020-04-13 22:48:05 -0700

    fix: swingset: remove unused 'inbound' device
    Technically this is a breaking change, but I'm pretty sure nobody was
    importing the module I removed.

--- packages/SwingSet/src/devices/index.js
+++ packages/SwingSet/src/devices/index.js
@@ -1,4 +1,3 @@
 // Export all the default exports of the driver top-halves.
-export { default as buildInbound } from './inbound';
 export { buildMailboxStateMap, buildMailbox } from './mailbox';
 export { buildTimer } from './timer';
