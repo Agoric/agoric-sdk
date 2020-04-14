4b07cddf5db86b3ee886b0aeb1a4932e664bdc39
Brian Warner <warner@lothar.com>
2020-04-13 22:54:35 -0700

    feat: swingset: add 'bridge' device
    This devices provides bidirectional pathways between a function in the host
    and a handler in the swingset: asynchronous messages inbound from host into
    vat, synchronous call+response outbound from vat into host.
    
    refs #860

--- packages/SwingSet/src/index.js
+++ packages/SwingSet/src/index.js
@@ -1,7 +1,8 @@
 export { loadBasedir, buildVatController } from './controller';
+
 export { buildMailboxStateMap, buildMailbox } from './devices/mailbox';
 export { buildTimer } from './devices/timer';
-
+export { buildBridge } from './devices/bridge';
 export { default as buildCommand } from './devices/command';
 
 export function getVatTPSourcePath() {
