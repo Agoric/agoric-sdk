0737e4dd9f71df326f996e6d572b58679d5da5f6
Kris Kowal <kris@cixar.com>
2020-04-15 15:31:19 -0700

    feat(swingset): Draft protocol for Go to JS bridge

--- packages/cosmic-swingset/x/swingset/handler.go
+++ packages/cosmic-swingset/x/swingset/handler.go
@@ -1,7 +1,6 @@
 package swingset
 
 import (
-	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -11,20 +10,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-
-	channelexported "github.com/cosmos/cosmos-sdk/x/ibc/04-channel/exported"
-	channeltypes "github.com/cosmos/cosmos-sdk/x/ibc/04-channel/types"
 )
-
-type ibcPacketAction struct {
-	Type           string       `json:"type"`
-	Data64         string       `json:"data64"`
-	Tuple          ChannelTuple `json:"tuple"`
-	IBCHandlerPort int          `json:"ibcHandlerPort"`
-	StoragePort    int          `json:"storagePort"`
-	BlockHeight    int64        `json:"blockHeight"`
-	BlockTime      int64        `json:"blockTime"`
-}
 
 type deliverInboundAction struct {
 	Type        string          `json:"type"`
@@ -43,14 +29,6 @@ func NewHandler(keeper Keeper) sdk.Handler {
 		defer SetControllerKeeper(keeper)()
 
 		switch msg := msg.(type) {
-		// IBC channel support.
-		case channeltypes.MsgPacket:
-			return handleIBCPacket(ctx, keeper, "IBC_PACKET", msg.Packet)
-			// these too
-
-		case channeltypes.MsgTimeout:
-			return handleIBCPacket(ctx, keeper, "IBC_TIMEOUT", msg.Packet)
-
 		// Legacy deliver inbound.
 		// TODO: Sometime merge with IBC?
 		case MsgDeliverInbound:
@@ -69,46 +47,6 @@ func mailboxPeer(key string) (string, error) {
 		return "", errors.New("Can only access 'mailbox.PEER'")
 	}
 	return path[1], nil
-}
-
-func handleIBCPacket(ctx sdk.Context, keeper Keeper, actionType string, packet channelexported.PacketI) (*sdk.Result, error) {
-	tuple := ChannelTuple{
-		Source: ChannelEndpoint{
-			Channel: packet.GetSourceChannel(),
-			Port:    packet.GetSourcePort(),
-		},
-		Destination: ChannelEndpoint{
-			Channel: packet.GetDestChannel(),
-			Port:    packet.GetDestPort(),
-		},
-	}
-
-	data := packet.GetData()
-	data64 := make([]byte, base64.StdEncoding.EncodedLen(len(data)))
-	base64.StdEncoding.Encode(data64, data)
-
-	action := &ibcPacketAction{
-		Type:           actionType,
-		Data64:         string(data64),
-		Tuple:          tuple,
-		StoragePort:    GetPort("controller"),
-		IBCHandlerPort: GetPort("dibc"),
-		BlockHeight:    ctx.BlockHeight(),
-		BlockTime:      ctx.BlockTime().Unix(),
-	}
-
-	// fmt.Fprintf(os.Stderr, "Context is %+v\n", ctx)
-	b, err := json.Marshal(action)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
-	}
-
-	_, err = keeper.CallToController(string(b))
-	// fmt.Fprintln(os.Stderr, "Returned from SwingSet", out, err)
-	if err != nil {
-		return nil, err
-	}
-	return &sdk.Result{}, nil
 }
 
 func handleMsgDeliverInbound(ctx sdk.Context, keeper Keeper, msg MsgDeliverInbound) (*sdk.Result, error) {
