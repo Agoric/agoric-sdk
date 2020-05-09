8da69e08a88dcb6884c6c8cc7a37e36bc2972bbc
Mark S. Miller <erights@gmail.com>
2020-05-08 18:56:40 -0700

    fix: done and comment

--- packages/notifier/src/notifier.js
+++ packages/notifier/src/notifier.js
@@ -2,6 +2,26 @@ import { producePromise } from '@agoric/produce-promise';
 import { E } from '@agoric/eventual-send';
 import harden from '@agoric/harden';
 
+/**
+ * Adaptor from a notifierP in an async iterable.
+ * The notifierP can be any object that has an eventually invokable
+ * `getUpdateSince` method that behaves according to the notifier
+ * spec. Typically, this will actually be a reference to a
+ * possibly-remote notifier. But it is also used internally below
+ * so that a notifier itself is an async iterable.
+ *
+ * An async iterable is an object with a `[Symbol.asyncIterator]()` method
+ * that returns an async iterator. The async iterator we return here has only
+ * a `next()` method, without the optional `return` and `throw` methods. The
+ * omitted methods, if present, would be used by the for/await/of loop to
+ * inform the iterator of early termination. But this adaptor would not do
+ * anything useful in reaction to this notification.
+ *
+ * An async iterator's `next()` method returns a promise for an iteration
+ * result. An iteration result is a record with `value` and `done` properties,
+ * where the `value` property's value might be a promise for the next
+ * iteration variable.
+ */
 export function makeAsyncIterable(notifierP) {
   return harden({
     [Symbol.asyncIterator]: () => {
@@ -10,11 +30,33 @@ export function makeAsyncIterable(notifierP) {
       return harden({
         next: () => {
           if (!myIterationResultP) {
+            // In this adaptor, once `next()` is called and returns an
+            // unresolved promise, `myIterationResultP`, and until
+            // `myIterationResultP` is fulfilled with an
+            // iteration result, further `next()` calls will return the same
+            // `myIterationResultP` promise again without asking the notifier
+            // for more updates. If there's already an unanswered ask in the
+            // air, all further asks should just use the result of that one.
+            //
+            // This reuse behavior is only needed for code that uses the async
+            // iterator protocol explicitly. When this async iterator is
+            // consumed by a for/await/of loop, `next()` will only be called
+            // after the promise for the previous iteration result has
+            // fulfilled. If it fulfills with `done: true`, the for/await/of
+            // loop will never call `next()` again.
             myIterationResultP = E(notifierP)
               .getUpdateSince(myHandle)
               .then(({ value, updateHandle, done }) => {
                 myHandle = updateHandle;
-                myIterationResultP = undefined;
+                if (!done) {
+                  // Once the outstanding question has been answered, stop
+                  // using that answer, so any further `next()` questions
+                  // cause a new `getUpdateSince` request.
+                  //
+                  // But only if more answers are expected. Once the notifier
+                  // is `done`, that was the last answer do reuse it forever.
+                  myIterationResultP = undefined;
+                }
                 return harden({ value, done });
               });
           }
@@ -87,7 +129,7 @@ export const produceNotifier = () => {
   // is tightly held
   const notifier = harden({
     getUpdateSince,
-    ...makeAsyncIterable({ getUpdateSince }),
+    ...makeAsyncIterable(harden({ getUpdateSince })),
   });
   const updater = harden({ updateState, resolve });
   return harden({ notifier, updater });
