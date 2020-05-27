0cbc017af2b562a5e5c8016e5beda7a7895d09c4
Brian Warner <warner@lothar.com>
2020-05-26 19:18:20 -0700

    install-ses: move to SES-0.8.0

--- packages/install-ses/install-ses.js
+++ packages/install-ses/install-ses.js
@@ -1,8 +1,12 @@
-import { lockdown } from 'ses';
+/* global lockdown */
+// 'lockdown' appears on the global as a side-effect of importing 'ses'
+import 'ses';
 
 lockdown();
 // We are now in the "Start Compartment". Our global has all the same
 // powerful things it had before, but the primordials have changed to make
 // them safe to use in the arguments of API calls we make into more limited
-// compartments. 'Compartment' and 'harden' are now present in our global
-// scope.
+// compartments
+
+// 'Compartment' and 'harden' (and `StaticModuleRecord`) are now present in
+// our global scope.
