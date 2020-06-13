43426b2b65441b136fcebd507bf22982c54b408d
Brian Warner <warner@lothar.com>
2020-06-12 19:25:30 -0700

    delete ses-adapter

--- packages/ses-adapter/test/test-ses.js
+++
@@ -1,8 +0,0 @@
-import './install-ses.js';
-import { testHarden } from './do-harden.js';
-import { testCompartment } from './do-compartment.js';
-import { testHandledPromise } from './do-handled-promise.js';
-
-testHarden();
-testCompartment();
-testHandledPromise();
