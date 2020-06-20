98dadbb605a895676d3392b290fb7b5c523545e0
Brian Warner <warner@lothar.com>
2020-06-20 14:34:16 -0700

    transform-eventual-send: remove SES1 tests, enhance new tests
    The new tests specifically exercise the string-to-string transformation
    functionality, whereas the ones I deleted were also testing that
    SES1 (configured with the transformation) could evaluate tildot-bearing code
    correctly. I *think* that was out-of-scope for what this module should be
    doing, and the SES1 stuff went away anyways.

--- packages/transform-eventual-send/test/test-transformer.js
+++ packages/transform-eventual-send/test/test-transformer.js
@@ -11,5 +11,21 @@ test('transformer', t => {
     output,
     `let p = HandledPromise.applyMethod(bob, "foo", [arg1, arg2]);`,
   );
+  t.equal(
+    transformer(output),
+    `let p = HandledPromise.applyMethod(bob, "foo", [arg1, arg2]);`,
+  );
+  t.equal(transformer('123;456'), '123;456;');
+  t.equal(transformer('123;'), '123;');
+  t.equal(transformer('123'), '123;');
+  t.equal(transformer(`"abc"~.length`), 'HandledPromise.get("abc", "length");',
+         '.get() works');
+  t.equal(transformer(`({foo(nick) { return "hello " + nick; }})~.foo('person')`),
+          `HandledPromise.applyMethod({ foo(nick) {return "hello " + nick;} }, "foo", ['person']);`,
+         '.applyMethod() works');
+  t.equal(transformer(`((punct) => "world" + punct)~.('!')`), `HandledPromise.applyFunction(punct => "world" + punct, ['!']);`,
+         '.applyFunction() works');
+  t.equal(transformer(`["a", "b", "c"]~.[2]`), `HandledPromise.get(["a", "b", "c"], 2);`, 'computed .get works');
+  t.equal(transformer(`({foo(greeting) { return greeting + ' world';}})~.foo~.('hello')`), `HandledPromise.applyFunction(HandledPromise.get({ foo(greeting) {return greeting + ' world';} }, "foo"), ['hello']);`, 'double eventual send works');
   t.end();
 });
