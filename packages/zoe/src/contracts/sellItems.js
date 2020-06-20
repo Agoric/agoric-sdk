7fb25292475ae0cda7d970d347b43f29a53c57a4
Chris Hibbert <hibbert@agoric.com>
2020-06-19 17:29:27 -0700

    docs: add documentation to all Zoe contracts
    I was going to touch every contract in order to change the way that
    zcf is declared for typescript, so I fixed all the documentation as well.
    switch makeContract declaration and export to a simpler format

--- packages/zoe/src/contracts/sellItems.js
+++ packages/zoe/src/contracts/sellItems.js
@@ -14,111 +14,120 @@ import { makeZoeHelpers, defaultAcceptanceMsg } from '../contractSupport';
  *
  * The `pricePerItem` is to be set in the terms. It is expected that all items
  * are sold for the same uniform price.
+ *
+ * The initial offer should be { give: { Items: items } }, accompanied by
+ * terms as described above.
+ * Buyers use offers that match { want: { Items: items } give: { Money: m } }.
+ * The items provided should match particular items that the seller still has
+ * available to sell, and the money should be pricePerItem times the number of
+ * items requested.
+ *
+ * @typedef {import('../zoe').ContractFacet} ContractFacet
+ * @param {ContractFacet} zcf
  */
+const makeContract = zcf => {
+  const allKeywords = ['Items', 'Money'];
+  const {
+    assertKeywords,
+    rejectOffer,
+    checkHook,
+    assertNatMathHelpers,
+  } = makeZoeHelpers(zcf);
+  assertKeywords(harden(allKeywords));
 
-// zcf is the Zoe Contract Facet, i.e. the contract-facing API of Zoe
-export const makeContract = harden(
-  /** @param {ContractFacet} zcf */ zcf => {
-    const allKeywords = ['Items', 'Money'];
-    const {
-      assertKeywords,
-      rejectOffer,
-      checkHook,
-      assertNatMathHelpers,
-    } = makeZoeHelpers(zcf);
-    assertKeywords(harden(allKeywords));
+  const { pricePerItem } = zcf.getInstanceRecord().terms;
+  assertNatMathHelpers(pricePerItem.brand);
+  let sellerOfferHandle;
 
-    const { pricePerItem } = zcf.getInstanceRecord().terms;
-    assertNatMathHelpers(pricePerItem.brand);
-    let sellerOfferHandle;
+  const sellerOfferHook = offerHandle => {
+    sellerOfferHandle = offerHandle;
+    return defaultAcceptanceMsg;
+  };
 
-    const sellerOfferHook = offerHandle => {
-      sellerOfferHandle = offerHandle;
-      return defaultAcceptanceMsg;
-    };
-
-    const buyerOfferHook = buyerOfferHandle => {
-      const { brandKeywordRecord } = zcf.getInstanceRecord();
-      const [sellerAllocation, buyerAllocation] = zcf.getCurrentAllocations(
-        [sellerOfferHandle, buyerOfferHandle],
-        [brandKeywordRecord, brandKeywordRecord],
-      );
-      const currentItemsForSale = sellerAllocation.Items;
-      const providedMoney = buyerAllocation.Money;
-
-      const { proposal } = zcf.getOffer(buyerOfferHandle);
-      const wantedItems = proposal.want.Items;
-      const numItemsWanted = wantedItems.extent.length;
-      const totalCostExtent = pricePerItem.extent * numItemsWanted;
-      const moneyAmountMaths = zcf.getAmountMath(pricePerItem.brand);
-      const itemsAmountMath = zcf.getAmountMath(wantedItems.brand);
-
-      const totalCost = moneyAmountMaths.make(totalCostExtent);
-
-      // Check that the wanted items are still for sale.
-      if (!itemsAmountMath.isGTE(currentItemsForSale, wantedItems)) {
-        return rejectOffer(
-          buyerOfferHandle,
-          `Some of the wanted items were not available for sale`,
-        );
-      }
-
-      // Check that the money provided to pay for the items is greater than the totalCost.
-      if (!moneyAmountMaths.isGTE(providedMoney, totalCost)) {
-        return rejectOffer(
-          buyerOfferHandle,
-          `More money (${totalCost}) is required to buy these items`,
-        );
-      }
-
-      // Reallocate and complete the buyer offer.
-      const newSellerAllocation = {
-        Money: moneyAmountMaths.add(sellerAllocation.Money, providedMoney),
-        Items: itemsAmountMath.subtract(sellerAllocation.Items, wantedItems),
-      };
-
-      const newBuyerAllocation = {
-        Money: moneyAmountMaths.getEmpty(),
-        Items: itemsAmountMath.add(buyerAllocation.Items, wantedItems),
-      };
-
-      zcf.reallocate(
-        [sellerOfferHandle, buyerOfferHandle],
-        [newSellerAllocation, newBuyerAllocation],
-      );
-      zcf.complete([buyerOfferHandle]);
-      return defaultAcceptanceMsg;
-    };
-
-    const buyerExpected = harden({
-      want: { Items: null },
-      give: { Money: null },
-    });
-
-    zcf.initPublicAPI(
-      harden({
-        makeBuyerInvite: () => {
-          const itemsAmount = zcf.getCurrentAllocation(sellerOfferHandle).Items;
-          const itemsAmountMath = zcf.getAmountMath(itemsAmount.brand);
-          assert(
-            sellerOfferHandle && !itemsAmountMath.isEmpty(itemsAmount),
-            details`no items are for sale`,
-          );
-          return zcf.makeInvitation(
-            checkHook(buyerOfferHook, buyerExpected),
-            'buyer',
-          );
-        },
-        getAvailableItems: () => {
-          if (!sellerOfferHandle) {
-            throw new Error(`no items have been escrowed`);
-          }
-          return zcf.getCurrentAllocation(sellerOfferHandle).Items;
-        },
-        getItemsIssuer: () => zcf.getInstanceRecord().issuerKeywordRecord.Items,
-      }),
+  const buyerOfferHook = buyerOfferHandle => {
+    const { brandKeywordRecord } = zcf.getInstanceRecord();
+    const [sellerAllocation, buyerAllocation] = zcf.getCurrentAllocations(
+      [sellerOfferHandle, buyerOfferHandle],
+      [brandKeywordRecord, brandKeywordRecord],
     );
+    const currentItemsForSale = sellerAllocation.Items;
+    const providedMoney = buyerAllocation.Money;
 
-    return zcf.makeInvitation(sellerOfferHook, 'seller');
-  },
-);
+    const { proposal } = zcf.getOffer(buyerOfferHandle);
+    const wantedItems = proposal.want.Items;
+    const numItemsWanted = wantedItems.extent.length;
+    const totalCostExtent = pricePerItem.extent * numItemsWanted;
+    const moneyAmountMaths = zcf.getAmountMath(pricePerItem.brand);
+    const itemsAmountMath = zcf.getAmountMath(wantedItems.brand);
+
+    const totalCost = moneyAmountMaths.make(totalCostExtent);
+
+    // Check that the wanted items are still for sale.
+    if (!itemsAmountMath.isGTE(currentItemsForSale, wantedItems)) {
+      return rejectOffer(
+        buyerOfferHandle,
+        `Some of the wanted items were not available for sale`,
+      );
+    }
+
+    // Check that the money provided to pay for the items is greater than the totalCost.
+    if (!moneyAmountMaths.isGTE(providedMoney, totalCost)) {
+      return rejectOffer(
+        buyerOfferHandle,
+        `More money (${totalCost}) is required to buy these items`,
+      );
+    }
+
+    // Reallocate and complete the buyer offer.
+    const newSellerAllocation = {
+      Money: moneyAmountMaths.add(sellerAllocation.Money, providedMoney),
+      Items: itemsAmountMath.subtract(sellerAllocation.Items, wantedItems),
+    };
+
+    const newBuyerAllocation = {
+      Money: moneyAmountMaths.getEmpty(),
+      Items: itemsAmountMath.add(buyerAllocation.Items, wantedItems),
+    };
+
+    zcf.reallocate(
+      [sellerOfferHandle, buyerOfferHandle],
+      [newSellerAllocation, newBuyerAllocation],
+    );
+    zcf.complete([buyerOfferHandle]);
+    return defaultAcceptanceMsg;
+  };
+
+  const buyerExpected = harden({
+    want: { Items: null },
+    give: { Money: null },
+  });
+
+  zcf.initPublicAPI(
+    harden({
+      makeBuyerInvite: () => {
+        const itemsAmount = zcf.getCurrentAllocation(sellerOfferHandle).Items;
+        const itemsAmountMath = zcf.getAmountMath(itemsAmount.brand);
+        assert(
+          sellerOfferHandle && !itemsAmountMath.isEmpty(itemsAmount),
+          details`no items are for sale`,
+        );
+        return zcf.makeInvitation(
+          checkHook(buyerOfferHook, buyerExpected),
+          'buyer',
+        );
+      },
+      getAvailableItems: () => {
+        if (!sellerOfferHandle) {
+          throw new Error(`no items have been escrowed`);
+        }
+        return zcf.getCurrentAllocation(sellerOfferHandle).Items;
+      },
+      getItemsIssuer: () => zcf.getInstanceRecord().issuerKeywordRecord.Items,
+    }),
+  );
+
+  return zcf.makeInvitation(sellerOfferHook, 'seller');
+};
+
+harden(makeContract);
+export { makeContract };
