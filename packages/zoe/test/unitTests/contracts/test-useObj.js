fd7f97a516c90feb5593d6b6df4e85825fd34a5b
Kate Sills <kate@agoric.com>
2020-07-07 21:03:17 -0700

    Add example of a contract that gives and revokes use objects based on escrowed assets (#1272)
    * feat: add use object contract example: color pixels

---
+++ packages/zoe/test/unitTests/contracts/test-useObj.js
@@ -0,0 +1,75 @@
+/* global harden */
+
+import '@agoric/install-ses';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { test } from 'tape-promise/tape';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import bundleSource from '@agoric/bundle-source';
+
+import { makeZoe } from '../../../src/zoe';
+import { setup } from '../setupBasicMints';
+
+const contractRoot = `${__dirname}/useObjExample`;
+
+test('zoe - useObj', async t => {
+  t.plan(3);
+  const { moolaIssuer, moolaMint, moola } = setup();
+  const zoe = makeZoe();
+
+  // pack the contract
+  const bundle = await bundleSource(contractRoot);
+  // install the contract
+  const installationHandle = await zoe.install(bundle);
+
+  // Setup Alice
+  const aliceMoolaPayment = moolaMint.mintPayment(moola(3));
+
+  // Alice creates an instance
+  const issuerKeywordRecord = harden({
+    Pixels: moolaIssuer,
+  });
+  const { invite: aliceInvite } = await zoe.makeInstance(
+    installationHandle,
+    issuerKeywordRecord,
+  );
+
+  // Alice escrows with zoe
+  const aliceProposal = harden({
+    give: { Pixels: moola(3) },
+  });
+  const alicePayments = { Pixels: aliceMoolaPayment };
+
+  // Alice makes an offer
+  const {
+    payout: alicePayoutP,
+    outcome: useObjP,
+    completeObj,
+  } = await zoe.offer(aliceInvite, aliceProposal, alicePayments);
+
+  const useObj = await useObjP;
+
+  t.equals(
+    useObj.colorPixels('purple'),
+    `successfully colored 3 pixels purple`,
+    `use of use object works`,
+  );
+
+  completeObj.complete();
+
+  const alicePayout = await alicePayoutP;
+
+  const aliceMoolaPayoutPayment = await alicePayout.Pixels;
+
+  t.deepEquals(
+    await moolaIssuer.getAmountOf(aliceMoolaPayoutPayment),
+    moola(3),
+    `alice gets everything she escrowed back`,
+  );
+
+  console.log('EXPECTED ERROR ->>>');
+  t.throws(
+    () => useObj.colorPixels('purple'),
+    /the escrowing offer is no longer active/,
+    `use of use object fails once offer is withdrawn or amounts are reallocated`,
+  );
+});
---
+++ packages/zoe/test/unitTests/contracts/useObjExample.js
@@ -0,0 +1,75 @@
+/* global harden */
+// @ts-check
+
+// Eventually will be importable from '@agoric/zoe-contract-support'
+import { assert, details } from '@agoric/assert';
+import { makeZoeHelpers } from '../../../src/contractSupport';
+
+/**
+ * Give a use object when a payment is escrowed
+ *
+ * @typedef {import('../../../src/zoe').ContractFacet} ContractFacet
+ * @typedef {import('@agoric/ERTP').Amount} Amount
+ * @param {ContractFacet} zcf
+ */
+const makeContract = zcf => {
+  const { assertKeywords, checkHook } = makeZoeHelpers(zcf);
+  assertKeywords(harden(['Pixels']));
+  const { brandKeywordRecord } = zcf.getInstanceRecord();
+  const amountMath = zcf.getAmountMath(brandKeywordRecord.Pixels);
+
+  const makeUseObjHook = offerHandle => {
+    const useObj = harden({
+      /**
+       * (Pretend to) color some pixels.
+       * @param {string} color
+       * @param {Amount} amountToColor
+       */
+      colorPixels: (color, amountToColor = undefined) => {
+        // Throw if the offer is no longer active, i.e. the user has
+        // completed their offer and the assets are no longer escrowed.
+        assert(
+          zcf.isOfferActive(offerHandle),
+          `the escrowing offer is no longer active`,
+        );
+        const { Pixels: escrowedAmount } = zcf.getCurrentAllocation(
+          offerHandle,
+          brandKeywordRecord,
+        );
+        // If no amountToColor is provided, color all the pixels
+        // escrowed for this offer.
+        if (amountToColor === undefined) {
+          amountToColor = escrowedAmount;
+        }
+        // Ensure that the amount of pixels that we want to color is
+        // covered by what is actually escrowed.
+        assert(
+          amountMath.isGTE(escrowedAmount, amountToColor),
+          details`The pixels to color were not all escrowed. Currently escrowed: ${escrowedAmount}, amount to color: ${amountToColor}`,
+        );
+
+        // Pretend to color
+        return `successfully colored ${amountToColor.extent} pixels ${color}`;
+      },
+    });
+    return useObj;
+  };
+
+  const expected = harden({
+    give: { Pixels: null },
+  });
+
+  const makeUseObjInvite = () =>
+    zcf.makeInvitation(checkHook(makeUseObjHook, expected), 'use object');
+
+  zcf.initPublicAPI({
+    // The only publicAPI method is to make an invite.
+    makeInvite: makeUseObjInvite,
+  });
+
+  // Return an invite.
+  return makeUseObjInvite();
+};
+
+harden(makeContract);
+export { makeContract };
