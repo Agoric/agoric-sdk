5aa546977f92307387de76bfc080b2c9b43b1542
Kate Sills <kate@agoric.com>
2020-07-07 18:01:03 -0700

    feat: add use object contract example: color pixels

---
+++ packages/zoe/src/contracts/useObj.js
@@ -0,0 +1,58 @@
+/* global harden */
+// @ts-check
+
+// Eventually will be importable from '@agoric/zoe-contract-support'
+import { assert, details } from '@agoric/assert';
+import { makeZoeHelpers } from '../contractSupport';
+
+/**
+ * Give a use object when a payment is escrowed
+ *
+ * @typedef {import('../zoe').ContractFacet} ContractFacet
+ * @param {ContractFacet} zcf
+ */
+const makeContract = zcf => {
+  const { assertKeywords, checkHook } = makeZoeHelpers(zcf);
+  assertKeywords(harden(['Pixels']));
+  const { brandKeywordRecord } = zcf.getInstanceRecord();
+  const amountMath = zcf.getAmountMath(brandKeywordRecord.Pixels);
+
+  const makeUseObjHook = offerHandle => {
+    return harden({
+      colorPixels: (color, amountToColor) => {
+        assert(
+          zcf.isOfferActive(offerHandle),
+          `the escrowing offer is no longer active`,
+        );
+        const { Pixels: escrowedAmount } = zcf.getCurrentAllocation(
+          offerHandle,
+          brandKeywordRecord,
+        );
+        if (amountToColor === undefined) {
+          amountToColor = escrowedAmount;
+        }
+        assert(
+          amountMath.isGTE(escrowedAmount, amountToColor),
+          details`The pixels to color were not all escrowed. Currently escrowed: ${escrowedAmount}, amount to color: ${amountToColor}`,
+        );
+        return `successfully colored ${amountToColor.extent} pixels ${color}`;
+      },
+    });
+  };
+
+  const expected = harden({
+    give: { Pixels: null },
+  });
+
+  const makeUseObjInvite = () =>
+    zcf.makeInvitation(checkHook(makeUseObjHook, expected), 'use object');
+
+  zcf.initPublicAPI({
+    makeInvite: makeUseObjInvite,
+  });
+
+  return makeUseObjInvite();
+};
+
+harden(makeContract);
+export { makeContract };
