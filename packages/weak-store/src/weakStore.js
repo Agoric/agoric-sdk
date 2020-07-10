9e3284c5b4bc5f389fd2411bd4ea4953210e8fa6
Michael FIG <mfig@agoric.com>
2020-07-10 16:26:08 -0600

    docs: harmonise comments around store and weak-store

--- packages/weak-store/src/weakStore.js
+++ packages/weak-store/src/weakStore.js
@@ -5,19 +5,14 @@
 import { assert, details, q } from '@agoric/assert';
 
 /**
- * WeakMap that distinguishes between adding a new key (init) and updating or
- * referencing a key (get, set, delete).
- *
- * `init` is only allowed if the key does not already exist. `Get`,
- * `set` and `delete` are only allowed if the key does already exist.
- *
  * @template K,V
- * @typedef {Object} WeakStore
- * @property {(key: K) => boolean} has
- * @property {(key: K, value: V) => void} init
- * @property {(key: K) => V} get
- * @property {(key: K, value: V) => void} set
- * @property {(key: K) => void} delete
+ * @typedef {Object} WeakStore - A safety wrapper around a WeakMap
+ * @property {(key: K) => boolean} has - Check if a key exists
+ * @property {(key: K, value: V) => void} init - Initialize the key only if it doesn't already exist
+ * @property {(key: K) => V} get - Return a value for the key. Throws
+ * if not found.
+ * @property {(key: K, value: V) => void} set - Set the key. Throws if not found.
+ * @property {(key: K) => void} delete - Remove the key. Throws if not found.
  */
 
 /**
@@ -27,7 +22,7 @@ import { assert, details, q } from '@agoric/assert';
  * `init` is only allowed if the key does not already exist. `Get`,
  * `set` and `delete` are only allowed if the key does already exist.
  * @template K,V
- * @param  {string} keyName - the column name for the key
+ * @param {string} [keyName='key'] - the column name for the key
  * @returns {WeakStore<K,V>}
  */
 function makeStore(keyName = 'key') {
