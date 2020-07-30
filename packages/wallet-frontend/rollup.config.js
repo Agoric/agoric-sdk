86dd402080558cebbab66f32fcca9aa42c39d49d
Michael FIG <mfig@agoric.com>
2020-07-30 13:27:25 -0600

    fix: rollup smui properly

--- packages/wallet-frontend/rollup.config.js
+++ packages/wallet-frontend/rollup.config.js
@@ -52,7 +52,7 @@ export default {
 		// consult the documentation for details:
 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
-			browser: true,
+			mainFields: ['module', 'browser', 'main'],
 			dedupe: ['svelte']
 		}),
 		commonjs(),
