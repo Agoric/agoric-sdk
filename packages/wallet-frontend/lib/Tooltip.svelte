499478db9fe663e982dcf275c13920e7c0f1e811
Dean Tribble <tribble@agoric.com>
2020-07-31 23:31:05 -0700

    feat: formatting improvements
    - display non-fungible tokens with a number
    - added tooltips for non-fungible token amount values
    - added debug buttons to amounts
    - separated Purse from Purses
    - reformatted a few files
    - deleted unnecessary styles
    - generally renamed "main" to "section" since there shoudl just be one "main" per page

---
+++ packages/wallet-frontend/lib/Tooltip.svelte
@@ -0,0 +1,53 @@
+<script>
+  // Only show tooltip behavior if not disabled
+  export let disable = false;
+</script>
+
+<style>
+  /* Tooltip container */
+  .tooltip {
+    position: relative;
+    display: inline-block;
+    border-bottom: 1px dotted black; /* If you want dots under the hoverable text */
+  }
+
+  /* Tooltip text */
+  .tooltip div {
+    visibility: hidden;
+    width: 120px;
+    background-color: black;
+    color: #fff;
+    text-align: center;
+    padding: 5px 0;
+    border-radius: 6px;
+
+    position: absolute;
+    z-index: 1;
+  }
+
+  /* Show the tooltip text when you mouse over the tooltip container */
+  .tooltip:hover div {
+    visibility: visible;
+  }
+</style>
+
+{#if disable}
+  <section>
+    <slot name="target" />
+  </section>
+{:else}
+  <section class="tooltip">
+    <slot name="target" />
+    <div>
+      <slot />
+    </div>
+  </section>
+{/if}
+
+<!-- 
+  <Tooltip>
+    <div slot="target">
+      <Button>Hover me</Button>
+    </div>
+    How are you doing?
+  </Tooltip> -->
---
+++ packages/wallet-frontend/src/Purse.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Amount from "./Amount.svelte";
+  import Petname from "./Petname.svelte";
+  import Debug from "../lib/Debug.svelte";
+  
+  export let purse;
+</script>
+
+<style>
+</style>
+
+<section>
+  <Petname name={purse.pursePetname} /> <Debug title="Amount Detail" target={purse.currentAmount} />
+  <Amount amount={purse.currentAmount} />
+</section>
