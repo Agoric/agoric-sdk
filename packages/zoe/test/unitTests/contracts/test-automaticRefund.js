e49249a59bab1ac06a8ed5f6e422b2f2e0dbd56e
Kate Sills <kate@agoric.com>
2020-07-31 18:44:07 -0700

    refactor: basic AutomaticRefund test

--- packages/zoe/test/unitTests/contracts/test-automaticRefund.js
+++ packages/zoe/test/unitTests/contracts/test-automaticRefund.js
@@ -17,19 +17,18 @@ test('zoe - simplest automaticRefund', async t => {
   t.plan(1);
   try {
     // Setup zoe and mints
-    const { moolaR, moola } = setup();
-    const zoe = makeZoe(fakeVatAdmin);
+    const { moolaR, moola, zoe } = setup();
     // Pack the contract.
     const bundle = await bundleSource(automaticRefundRoot);
-    const installationHandle = await zoe.install(bundle);
+    const installation = await zoe.install(bundle);
 
     // Setup Alice
     const aliceMoolaPayment = moolaR.mint.mintPayment(moola(3));
 
     // 1: Alice creates an automatic refund instance
     const issuerKeywordRecord = harden({ Contribution: moolaR.issuer });
-    const { invite } = await zoe.makeInstance(
-      installationHandle,
+    const { creatorInvitation } = await zoe.makeInstance(
+      installation,
       issuerKeywordRecord,
     );
 
@@ -39,19 +38,19 @@ test('zoe - simplest automaticRefund', async t => {
     });
     const alicePayments = { Contribution: aliceMoolaPayment };
 
-    const { payout: payoutP } = await zoe.offer(
-      invite,
+    const seat = await zoe.offer(
+      creatorInvitation,
       aliceProposal,
       alicePayments,
     );
 
-    const alicePayout = await payoutP;
-    const aliceMoolaPayout = await alicePayout.Contribution;
+    const aliceMoolaPayout = await seat.getPayout('Contribution');
 
     // Alice got back what she put in
     t.deepEquals(
       await moolaR.issuer.getAmountOf(aliceMoolaPayout),
       aliceProposal.give.Contribution,
+      `Alice's payout matches what she put in`,
     );
   } catch (e) {
     t.assert(false, e);
@@ -63,8 +62,7 @@ test('zoe - automaticRefund same issuer', async t => {
   t.plan(1);
   try {
     // Setup zoe and mints
-    const { moolaR, moola } = setup();
-    const zoe = makeZoe(fakeVatAdmin);
+    const { moolaR, moola, zoe } = setup();
     // Pack the contract.
     const bundle = await bundleSource(automaticRefundRoot);
     const installationHandle = await zoe.install(bundle);
