30ebf720edf1a4fcb366c3605a1b7ddeca94e0bb
Dean Tribble <tribble@agoric.com>
2020-08-03 10:44:28 -0700

    Spike to handoff AppBar

---
+++ packages/wallet-frontend/src/menu.js
@@ -0,0 +1,8 @@
+export const navMenu = [
+  { to: "/dark-mode", text: "Dark mode" }
+];
+
+export const topMenu = [
+  { to: "/breakpoints", text: "Breakpoints" },
+  { to: "/dark-mode", text: "Dark mode" }
+];
