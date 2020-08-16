3ca90a9035cba14fc6450f4af963f5445077a2fa
Michael FIG <mfig@agoric.com>
2020-08-15 18:20:32 -0600

    test(swingset-runner): update demo/exchangeBenchmark for new Zoe

--- packages/swingset-runner/demo/exchangeBenchmark/helpers.js
+++ packages/swingset-runner/demo/exchangeBenchmark/helpers.js
@@ -3,6 +3,8 @@
 import { E } from '@agoric/eventual-send';
 import { makeLocalAmountMath } from '@agoric/ertp';
 
+import '@agoric/zoe/exported';
+
 export async function showPurseBalance(purseP, name, log) {
   try {
     const amount = await E(purseP).getCurrentAmount();
@@ -12,6 +14,11 @@ export async function showPurseBalance(purseP, name, log) {
   }
 }
 
+/**
+ * @param {ZoeService} zoe
+ * @param {Issuer[]} issuers
+ * @param {Payment[]} payments
+ */
 export async function setupPurses(zoe, issuers, payments) {
   const purses = issuers.map(issuer => E(issuer).makeEmptyPurse());
   const [moolaIssuer, simoleanIssuer] = issuers;
