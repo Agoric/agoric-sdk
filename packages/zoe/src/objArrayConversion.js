27226f50d162a88dc707d0f79682e318b03f220e
Kate Sills <kate@agoric.com>
2020-08-16 17:27:49 -0700

    chore: remove more unused functions

--- packages/zoe/src/objArrayConversion.js
+++ packages/zoe/src/objArrayConversion.js
@@ -35,51 +35,6 @@ export const assertSubset = (whole, part) => {
 };
 
 /**
- * Return a new object with only the keys in subsetKeys.
- * `obj` must have values for all the `subsetKeys`.
- * @template T
- * @template {(keyof T)[]} U
- * @param {T} obj
- * @param {U} subsetKeys
- * @returns {Pick<T,U[number]>}
- */
-export const filterObj = (obj, subsetKeys) => {
-  /** @type {Partial<Pick<T,U[number]>>} */
-  const newObj = {};
-  subsetKeys.forEach(key => {
-    assert(
-      obj[key] !== undefined,
-      details`obj[key] must be defined for keyword ${q(key)}`,
-    );
-    newObj[key] = obj[key];
-  });
-
-  const picked = /** @type {Pick<T,U[number]>} */ (newObj);
-  return picked;
-};
-
-/**
- * Return a new object with only the keys in `amountMathKeywordRecord`, but fill
- * in empty amounts for any key that is undefined in the original allocation
- * @param {Allocation} allocation
- * @param {AmountMathKeywordRecord} amountMathKeywordRecord
- * @returns {Allocation}
- * */
-export const filterFillAmounts = (allocation, amountMathKeywordRecord) => {
-  /** @type {Allocation} */
-  const filledAllocation = {};
-  const subsetKeywords = Object.getOwnPropertyNames(amountMathKeywordRecord);
-  subsetKeywords.forEach(keyword => {
-    if (allocation[keyword] === undefined) {
-      filledAllocation[keyword] = amountMathKeywordRecord[keyword].getEmpty();
-    } else {
-      filledAllocation[keyword] = allocation[keyword];
-    }
-  });
-  return filledAllocation;
-};
-
-/**
  * @template T, U
  * @template {keyof T} K
  * @param {Record<K, T>} original
