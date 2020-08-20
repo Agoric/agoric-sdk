cb5cc0e7340a529288ee33098428309c0d4561e2
Mark S. Miller <erights@gmail.com>
2020-08-19 19:06:40 -0700

    fix: typo

--- packages/zoe/src/types.js
+++ packages/zoe/src/types.js
@@ -377,8 +377,8 @@
 /**
  * @typedef {Object} ZCFSeat
  * @property {() => void} exit
- * @property {(reason?: any) => never} kickOut alled with the reason this seat
- * is being kicked out, where reason is normally an instanceof Error.
+ * @property {(reason?: any) => never} kickOut called with the reason this
+ * seat is being kicked out, where reason is normally an instanceof Error.
  * @property {() => Notifier<Allocation>} getNotifier
  * @property {() => boolean} hasExited
  * @property {() => ProposalRecord} getProposal
