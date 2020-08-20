3d9dd219a3443916dd0e7ac49b50b8bc1cd631e7
Kate Sills <kate@agoric.com>
2020-08-19 17:15:24 -0700

    chore: whoops, move catch to the correct location

--- packages/zoe/src/contractFacet/exit.js
+++ packages/zoe/src/contractFacet/exit.js
@@ -26,19 +26,21 @@ export const makeExitObj = (proposal, zoeSeatAdmin, zcfSeatAdmin) => {
 
   if (exitKind === 'afterDeadline') {
     // Automatically exit the seat after deadline.
-    E(proposal.exit.afterDeadline.timer).setWakeup(
-      proposal.exit.afterDeadline.deadline,
-      harden({
-        wake: exitFn,
-      }).catch(err => {
+    E(proposal.exit.afterDeadline.timer)
+      .setWakeup(
+        proposal.exit.afterDeadline.deadline,
+        harden({
+          wake: exitFn,
+        }),
+      )
+      .catch(err => {
         console.error(
           `The seat could not be made with the provided timer ${proposal.exit.afterDeadline.timer} and deadline ${proposal.exit.afterDeadline.deadline}`,
         );
         console.error(err);
         exitFn();
         throw err;
-      }),
-    );
+      });
   } else if (exitKind === 'onDemand') {
     // Allow the user to exit their seat on demand. Note: we must wrap
     // it in an object to send it back to Zoe because our marshalling layer
