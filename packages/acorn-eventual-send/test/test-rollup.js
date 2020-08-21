660161c3cc0dfe612dbd732cc07eabc04caf5146
Brian Warner <warner@lothar.com>
2020-08-20 23:23:56 -0700

    chore: update 'acorn-eventual-send' package to use AVA for testing

--- packages/acorn-eventual-send/test/test-rollup.js
+++ packages/acorn-eventual-send/test/test-rollup.js
@@ -1,20 +1,14 @@
-import { test } from 'tape-promise/tape';
+import test from 'ava';
 import { rollup } from 'rollup/dist/rollup.es';
 import * as acorn from 'acorn';
 import eventualSend from '..';
 
 test('SwingSet bug', async t => {
-  try {
-    const bundle = await rollup({
-      input: require.resolve('../encouragementBotCommsWavyDot/bootstrap.js'),
-      treeshake: false,
-      external: [],
-      acornInjectPlugins: [eventualSend(acorn)],
-    });
-    t.ok(bundle);
-  } catch (e) {
-    t.isNot(e, e, 'unexpected exception');
-  } finally {
-    t.end();
-  }
+  const bundle = await rollup({
+    input: require.resolve('../encouragementBotCommsWavyDot/bootstrap.js'),
+    treeshake: false,
+    external: [],
+    acornInjectPlugins: [eventualSend(acorn)],
+  });
+  t.truthy(bundle);
 });
