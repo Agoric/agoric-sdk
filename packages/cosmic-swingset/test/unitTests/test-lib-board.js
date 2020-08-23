1a54efddc9a342e73d411c310e55e235cc263828
Brian Warner <warner@lothar.com>
2020-08-23 16:51:12 -0700

    chore: update 'cosmic-swingset' package to use AVA
    refs #1568

--- packages/cosmic-swingset/test/unitTests/test-lib-board.js
+++ packages/cosmic-swingset/test/unitTests/test-lib-board.js
@@ -2,33 +2,27 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 
 import '@agoric/install-ses';
-import { test } from 'tape-promise/tape';
+import test from 'ava';
 
 import { makeBoard } from '../../lib/ag-solo/vats/lib-board';
 
 test('makeBoard', async t => {
-  try {
-    const board = makeBoard();
+  const board = makeBoard();
 
-    const obj1 = harden({});
-    const obj2 = harden({});
+  const obj1 = harden({});
+  const obj2 = harden({});
 
-    t.deepEquals(board.ids(), [], `board is empty to start`);
+  t.deepEqual(board.ids(), [], `board is empty to start`);
 
-    const idObj1 = board.getId(obj1);
-    t.deepEquals(board.ids(), [idObj1], `board has one id`);
+  const idObj1 = board.getId(obj1);
+  t.deepEqual(board.ids(), [idObj1], `board has one id`);
 
-    const idObj2 = board.getId(obj2);
-    t.deepEquals(board.ids().length, 2, `board has two ids`);
+  const idObj2 = board.getId(obj2);
+  t.deepEqual(board.ids().length, 2, `board has two ids`);
 
-    t.deepEquals(board.getValue(idObj1), obj1, `id matches value obj1`);
-    t.deepEquals(board.getValue(idObj2), obj2, `id matches value obj2`);
+  t.deepEqual(board.getValue(idObj1), obj1, `id matches value obj1`);
+  t.deepEqual(board.getValue(idObj2), obj2, `id matches value obj2`);
 
-    t.deepEquals(board.getId(obj1), idObj1, `value matches id obj1`);
-    t.deepEquals(board.getId(obj2), idObj2, `value matches id obj2`);
-  } catch (e) {
-    t.isNot(e, e, 'unexpected exception');
-  } finally {
-    t.end();
-  }
+  t.deepEqual(board.getId(obj1), idObj1, `value matches id obj1`);
+  t.deepEqual(board.getId(obj2), idObj2, `value matches id obj2`);
 });
