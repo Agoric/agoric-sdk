edfb210f370bd6ae8f0a899047c8c3945c6dfbfb
Dan Connolly <dckc@madmode.com>
2020-08-26 22:52:52 -0500

    chore(endo-load): ts-check up to contents vs. FileModule (WIP)
    src/endo-load.js:78:13 - error TS2339: Property 'contents' does not exist on type 'Compartment'.
    
    78     const { contents, modules } = compartmap.compartments[loc];

--- packages/xs-vat-worker/src/endo-load.js
+++ packages/xs-vat-worker/src/endo-load.js
@@ -1,39 +1,67 @@
 /* global Compartment */
+// @ts-check
 
+/** @param { unknown[] } _args  */
 function debug(..._args) {
   // console.log(...args);
 }
 
+/**
+ *
+ * @param { endo.CompartmentMap } compartmap
+ * @param { unknown } HandledPromise
+ */
 export function loadMain(compartmap, HandledPromise) {
-  // ISSUE: doesn't seem to work: const { entries, fromEntries, keys } = Object;
+  // ISSUE: doesn't seem to work:
+  const { entries, fromEntries, keys } = Object;
+  // const entries = o => Object.entries(o);
+  // const fromEntries = pvs => Object.fromEntries(pvs);
+  // const keys = o => Object.keys(o);
   // debug('entries, ...', { entries: typeof entries, fromEntries: typeof fromEntries, keys: typeof keys });
-  const entries = o => Object.entries(o);
-  const fromEntries = pvs => Object.fromEntries(pvs);
-  const keys = o => Object.keys(o);
 
-  function SESCompartment(endowments, map, options) {
-    debug('SESCompartment', { endowments, map, options });
-    const sesGlobals = {
-      harden,
-      console,
-      HandledPromise,
-      Compartment: SESCompartment,
-    };
-    return new Compartment({ ...sesGlobals, ...endowments }, map, options);
+  /**
+   *
+   * @param {{ [globalPropertyName: string]: any }} endowments
+   * @param {{[key: string]: FullSpecifier | ModuleNamespace}} map
+   * @param { CompartmentConstructorOptions } options
+   */
+  class SESCompartment extends Compartment {
+    constructor(endowments = {}, map = {}, options = {}) {
+      debug('SESCompartment', { endowments, map, options });
+      const sesGlobals = {
+        harden,
+        console,
+        HandledPromise,
+        Compartment: SESCompartment,
+      };
+      super({ ...sesGlobals, ...endowments }, map, options);
+    }
   }
 
+  /**
+   * @template T
+   * @param { (k: string) => T } f
+   * @return {(k: string) => T}
+   */
   const memoize = f => {
+    /** @type { {[k: string]: T} } */
     const cache = {};
     return k => cache[k] || (cache[k] = f(k));
   };
+  /** @type {(spec: string) => string} */
   const unjs = spec => spec.replace(/\.js$/, '');
+  /** @type {(spec: string) => string} */
   const unrel = spec => spec.replace(/^\.\//, '/');
+  /** @type {(base: string, ref: string) => string} */
   const join = (base, ref) => `${base}${unjs(ref.slice(2))}`;
+  /** @type {(loc: string) => Compartment} */
   const pkgCompartment = memoize(loc => {
+    /** @type {(ref: string) => [string, string]} */
     const intraPkg = ref => {
       debug('intraPkg', { loc, ref });
       return [unjs(unrel(ref).slice(1)), join(loc, ref)];
     };
+    /** @type {(entry: [string, { compartment: string, module: string }]) => [string, ModuleNamespace]} */
     function interPkg([specifier, { compartment, module }]) {
       const pc = pkgCompartment(compartment);
       const fullSpecifier = unjs(module.slice(2));
@@ -48,6 +76,7 @@ export function loadMain(compartmap, HandledPromise) {
       return [specifier, pc.importNow(fullSpecifier)];
     }
     const { contents, modules } = compartmap.compartments[loc];
+    /** @type { {[key: string]: FullSpecifier | ModuleNamespace} } */
     const cmap = fromEntries([
       ...contents.map(intraPkg),
       ...entries(modules).map(interPkg),
