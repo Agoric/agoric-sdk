56e0cb363af5d6130e74ee6b1b5335c200f4042e
Michael FIG <mfig@agoric.com>
2020-08-31 21:29:31 -0600

    fix: don't rely on validator DNS names when finding the network

--- packages/deployment/bigdipper/ag-lcd.sh
+++ packages/deployment/bigdipper/ag-lcd.sh
@@ -3,9 +3,30 @@
 set -e
 PATH=$PATH:/usr/local/bin
 ncf=`curl -Ss https://testnet.agoric.com/network-config`
-l=`echo "$ncf" | jq '.rpcAddrs | length'`
-eval rp=`echo "$ncf" | jq ".rpcAddrs[$(( RANDOM % l ))]"`
-eval cn=`echo "$ncf" | jq '.chainName'`
+cn=`echo "$ncf" | jq -r '.chainName'`
+
+origRpcAddrs=( $(echo $ncf | jq -r '.rpcAddrs | join(" ")' ) )
+
+rpcAddrs=(${origRpcAddrs[@]})
+rp=
+while [[ ${#rpcAddrs[@]} -gt 0 ]]; do
+  r=$(( $RANDOM % ${#rpcAddrs[@]} ))
+  selected=${rpcAddrs[$r]}
+  rpcAddrs=( ${rpcAddrs[@]/$selected} )
+
+  if curl -s http://$selected/status > /dev/null; then
+    # Found an active node.
+    rp=$selected
+    break
+  fi
+done
+
+if test -z "$rp"; then
+  echo "Cannot find an active node; last tried $selected"
+  sleep 20
+  exit 1
+fi
+
 # FIXME: Once https://github.com/cosmos/cosmos-sdk/issues/5592 is resolved,
 # we can use:
 #exec ag-cosmos-helper rest-server --node="tcp://$rp" --chain-id="$cn"
