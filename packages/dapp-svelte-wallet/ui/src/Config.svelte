2ad36eb100b5d35c745367699a84c21ea036e98d
Kate Sills <kate@agoric.com>
2020-10-14 14:46:12 -0700

    refactor: remove theme toggle (#1868)

--- packages/dapp-svelte-wallet/ui/src/Config.svelte
+++ packages/dapp-svelte-wallet/ui/src/Config.svelte
@@ -1,5 +1,4 @@
 <script>
-  import { ThemeToggle } from 'svelte-themer';
 
   import Icon from "smelte/src/components/Icon";
   import Card from "smelte/src/components/Card";
@@ -12,11 +11,3 @@
     margin: 1em 0 0 2em;
   }
 </style>
-
-<div class="fullwidth px-2 py-2">
-  <Card.Title title="Config" />
-
-  <div class="actions">
-    <ThemeToggle />
-  </div>
-</div>
