a7ae5a610510b831a5478e80a4e39f395e7d2a3b
Zarutian <zarutian@gmail.com>
2020-10-30 03:56:45 +0000

    chore: .
    up next lint wrangling

--- packages/eventual-send/test/test-hp.js
+++ packages/eventual-send/test/test-hp.js
@@ -308,5 +308,8 @@ test.skip('resolveWithPresence test nr 4', async t => {
     pr = { ...pr, resolve, reject, resolveWithPresence };
   });
   await Promise.resolve();
-  pr.resolveWithPresence(presenceEventualHandler, {});
+  pr.resolveWithPresence(
+    presenceEventualHandler,
+    { proxy: { handler: presenceImmediateHandler, target: {} },
+  );
 });
