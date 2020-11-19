c3cca70a6e049dee7ecd34100d3198e646b8f29e
Kate Sills <kate@agoric.com>
2020-11-18 19:55:45 -0800

    chore: revert #2025, but keep the additional tests. (#2029)

--- packages/zoe/test/unitTests/contracts/test-throwInOfferHandler.js
+++ packages/zoe/test/unitTests/contracts/test-throwInOfferHandler.js
@@ -31,7 +31,14 @@ test('throw in offerHandler', async t => {
 
   const throwsInOfferHandlerSeat = E(zoe).offer(throwInOfferHandlerInvitation);
 
-  await t.throwsAsync(() => E(throwsInOfferHandlerSeat).getOfferResult(), {
+  const throwsInOfferHandlerResult = E(
+    throwsInOfferHandlerSeat,
+  ).getOfferResult();
+
+  // Uncomment below to see what the user would see
+  // await throwsInOfferHandlerResult;
+
+  await t.throwsAsync(() => throwsInOfferHandlerResult, {
     message: 'error thrown in offerHandler in contract',
   });
 
@@ -39,7 +46,35 @@ test('throw in offerHandler', async t => {
     throwInDepositToSeatInvitation,
   );
 
-  await t.throwsAsync(() => E(throwsInDepositToSeatSeat).getOfferResult(), {
+  const throwsInDepositToSeatResult = E(
+    throwsInDepositToSeatSeat,
+  ).getOfferResult();
+
+  // Uncomment below to see what the user would see
+  // await throwsInDepositToSeatResult;
+
+  // TODO: make this logging more informative, including information
+  // about the error originating in the offerHandler in depositToSeat
+
+  // Currently the entirety of the log is:
+
+  // "Rejected promise returned by test. Reason:
+  // Error {
+  //   message: '"brand" not found: (an object)',
+  // }
+  // › makeDetailedError (/Users/katesills/code/agoric-sdk/node_modules/ses/dist/ses.cjs:3437:17)
+  // › fail (/Users/katesills/code/agoric-sdk/node_modules/ses/dist/ses.cjs:3582:19)
+  // › baseAssert (/Users/katesills/code/agoric-sdk/node_modules/ses/dist/ses.cjs:3600:13)
+  // › assertKeyExists (/Users/katesills/code/agoric-sdk/packages/store/src/weak-store.js:19:5)
+  // › Object.get [as getByBrand] (/Users/katesills/code/agoric-sdk/packages/store/src/weak-store.js:27:7)
+  // › getAmountMath (src/zoeService/zoe.js:44:46)
+  // › src/cleanProposal.js:65:5
+  // › Array.map (<anonymous>)
+  // › coerceAmountKeywordRecord (src/cleanProposal.js:64:34)
+  // › cleanProposal (src/cleanProposal.js:116:10)
+  // › src/zoeService/zoe.js:408:28"
+
+  await t.throwsAsync(() => throwsInDepositToSeatResult, {
     message: `"brand" not found: (an object)`,
   });
 });
