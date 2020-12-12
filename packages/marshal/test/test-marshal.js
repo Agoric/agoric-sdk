874c95e1834351882712ba7a2c9d789b6e31417a
Mark S. Miller <erights@gmail.com>
2020-12-11 18:32:57 -0800

    fix: port #2037 and #2044 onto master

--- packages/marshal/test/test-marshal.js
+++ packages/marshal/test/test-marshal.js
@@ -70,7 +70,7 @@ test('serialize static data', t => {
     emptyem = harden(e);
   }
   t.deepEqual(ser(emptyem), {
-    body: '{"@qclass":"error","name":"Error","message":""}',
+    body: '{"@qclass":"error","name":"Error","message":"","errorId":"error#1"}',
     slots: [],
   });
 
@@ -81,7 +81,8 @@ test('serialize static data', t => {
     em = harden(e);
   }
   t.deepEqual(ser(em), {
-    body: '{"@qclass":"error","name":"ReferenceError","message":"msg"}',
+    body:
+      '{"@qclass":"error","name":"ReferenceError","message":"msg","errorId":"error#2"}',
     slots: [],
   });
 
