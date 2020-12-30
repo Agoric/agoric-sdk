c13efcfcd7107eac8fa5e64f5e4b4bde045fb184
Dan Connolly <dckc@madmode.com>
2020-12-30 13:19:46 -0600

    fix: flesh out TextEncoder / TextDecoder stubs
    ... using ArrayBuffer.fromString and String.fromArrayBuffer

--- packages/xs-vat-worker/src/text-shim.js
+++ packages/xs-vat-worker/src/text-shim.js
@@ -1,17 +1,66 @@
-export function TextEncoder() {
-  return harden({
-    encode(_) {
-      throw new Error('not impl');
-    },
-  });
+/**
+ * Shim TextEncoder using XS ArrayBuffer.fromString
+ * and likewise TextDecoder using String.fromArrayBuffer.
+ * Note lockdown() will remove those XS extensions,
+ * so run this shim first.
+ *
+ * Note issue #2118: UTF-8 in XS: compatibility, security impact?
+ * https://github.com/Agoric/agoric-sdk/issues/2118
+ */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable max-classes-per-file */
+
+/**
+ * minimal TextDecoder
+ * No support for encodeInto.
+ *
+ * ref https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder
+ */
+class TextEncoder {
+  get encoding() {
+    return 'utf-8';
+  }
+
+  encode(s) {
+    return new Uint8Array(ArrayBuffer.fromString(s));
+  }
 }
 
-export function TextDecoder() {
-  return harden({
-    decode(_) {
-      throw new Error('not impl');
-    },
-  });
+const UTF8Names = ['unicode-1-1-utf-8', 'utf-8', 'utf8'];
+
+/**
+ * minimal utf-8 TextDecoder
+ * no support for fatal, stream, etc.
+ *
+ * ref https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder
+ */
+class TextDecoder {
+  /**
+   * @param {string=} utfLabel optional name for UTF-8
+   * @param {*} options fatal is not supported
+   */
+  constructor(utfLabel, options) {
+    if (utfLabel && !UTF8Names.includes(utfLabel)) {
+      throw new TypeError(utfLabel);
+    }
+    if (options && options.fatal) {
+      throw new TypeError('fatal not supported');
+    }
+  }
+
+  /**
+   * @param {Uint8Array} bytes
+   * @param {*} options stream is not supported
+   */
+  decode(bytes, options) {
+    if (options && options.stream) {
+      throw new TypeError('stream is unsupported');
+    }
+    if (!(bytes instanceof Uint8Array)) {
+      throw new TypeError('arg must be Uint8Array');
+    }
+    return String.fromArrayBuffer(bytes.buffer);
+  }
 }
 
 globalThis.TextEncoder = TextEncoder;
