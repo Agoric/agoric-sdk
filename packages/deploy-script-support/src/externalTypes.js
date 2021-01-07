e9edc3924374f57a4aa469ca61e0dfa20d0c3599
Kate Sills <kate@agoric.com>
2021-01-07 15:56:27 -0800

    chore: add type definition that had originally been imported from dapp-svelte-wallet

--- packages/deploy-script-support/src/externalTypes.js
+++ packages/deploy-script-support/src/externalTypes.js
@@ -105,3 +105,10 @@
  * @param {string} expectedOfferResult
  * @returns {Promise<void>}
  */
+
+/**
+ * @typedef {string | string[]} Petname A petname can either be a plain string
+ * or a path for which the first element is a petname for the origin, and the
+ * rest of the elements are a snapshot of the names that were first given by that
+ * origin.  We are migrating away from using plain strings, for consistency.
+ */
