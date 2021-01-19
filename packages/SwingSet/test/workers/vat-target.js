320c8fde53dff9029a9429fdeb808ed2a1dd61b6
Brian Warner <warner@lothar.com>
2021-01-18 21:47:24 -0800

    fix: improve test-worker.js to assert promise results are correct
    Also hush some console messages, and test the local worker as well (for
    parallelism).
    
    closes #1778

--- packages/SwingSet/test/workers/vat-target.js
+++ packages/SwingSet/test/workers/vat-target.js
@@ -21,12 +21,13 @@ export function buildRootObject(vatPowers) {
   const precC = makePromiseKit();
   let callbackObj;
 
-  // zero: dispatch.deliver(target, method="one", result=pA, args=[callbackObj, pD, pE])
-  //       syscall.subscribe(pD)
-  //       syscall.subscribe(pE)
-  //       syscall.send(callbackObj, method="callback", result=rp2, args=[11, 12]);
-  //       syscall.subscribe(rp2)
-  //       syscall.fulfillToData(pA, [pB, pC]);
+  // crank 1:
+  //   dispatch.deliver(target, method="zero", result=pA, args=[callbackObj, pD, pE])
+  //   syscall.subscribe(pD)
+  //   syscall.subscribe(pE)
+  //   syscall.send(callbackObj, method="callback", result=rp2, args=[11, 12]);
+  //   syscall.subscribe(rp2)
+  //   syscall.fulfillToData(pA, [pB, pC]);
   function zero(obj, pD, pE) {
     callbackObj = obj;
     const pF = E(callbackObj).callback(11, 12); // syscall.send
@@ -35,20 +36,21 @@ export function buildRootObject(vatPowers) {
     return [precB.promise, precC.promise, pF]; // syscall.fulfillToData
   }
 
-  // one: dispatch.deliver(target, method="two", result=rp3, args=[])
-  //      syscall.fulfillToPresence(pB, callbackObj)
-  //      syscall.reject(pC, Error('oops'))
-  //      syscall.fulfillToData(rp3, 1)
+  // crank 2:
+  //   dispatch.deliver(target, method="one", result=rp3, args=[])
+  //   syscall.fulfillToPresence(pB, callbackObj)
+  //   syscall.reject(pC, Error('oops'))
+  //   syscall.fulfillToData(rp3, 'rp3 good')
   function one() {
     precB.resolve(callbackObj); // syscall.fulfillToPresence
     precC.reject(Error('oops')); // syscall.reject
-    return 1;
+    return 'rp3 good';
   }
 
-  // two: dispatch.notify(pD, false, callbackObj)
-  // three: dispatch.notify(pE, true, Error('four'))
+  // crank 3: dispatch.notify(pD, false, callbackObj)
+  // crank 4: dispatch.notify(pE, true, Error('four'))
 
-  // four: dispatch.notify(pF, false, ['data', callbackObj])
+  // crank 5: dispatch.notify(pF, false, ['data', callbackObj])
 
   const target = harden({
     zero,
