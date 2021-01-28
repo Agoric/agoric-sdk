0c06e3264ca63213b9b3d8a177e0d23812d0930a
Michael FIG <mfig@agoric.com>
2021-01-28 09:52:04 -0600

    ci: use go-1.15 as required by tendermint

--- golang/cosmos/app/version_check.go
+++ golang/cosmos/app/version_check.go
@@ -1,6 +1,6 @@
-//+build !go1.14
+//+build !go1.15
 
 package gaia
 
-cosmic-swingset requires requires Golang version 1.14.
+cosmic-swingset requires requires Golang version 1.15.
 See https://golang.org/dl/ for upgrade information.
