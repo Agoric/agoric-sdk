003c34caa9832c8171675dfbb466127ca14b360e
Michael FIG <mfig@agoric.com>
2021-02-15 16:49:56 -0600

    feat: wire metrics into fake-chain too

--- packages/cosmic-swingset/lib/kernel-stats.js
+++ packages/cosmic-swingset/lib/kernel-stats.js
@@ -1,3 +1,6 @@
+import { MeterProvider } from '@opentelemetry/metrics';
+import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
+
 // All the kernel metrics we are prepared for.
 const KERNEL_STATS_SUM_METRICS = [
   {
@@ -153,4 +156,39 @@ export function exportKernelStats({
     });
     batchObserverResult.observe(labels, observations);
   });
+}
+
+/**
+ * Interpret the meter provider environment variables.
+ *
+ * @param {{ log: Console['log'] }} console
+ * @param {Record<string, string>} env
+ */
+export function getMeterProvider(console, env) {
+  if (
+    !env.OTEL_EXPORTER_PROMETHEUS_HOST &&
+    !env.OTEL_EXPORTER_PROMETHEUS_PORT
+  ) {
+    return undefined;
+  }
+  const host =
+    env.OTEL_EXPORTER_PROMETHEUS_HOST ||
+    PrometheusExporter.DEFAULT_OPTIONS.host ||
+    '0.0.0.0';
+  const port =
+    Number(env.OTEL_EXPORTER_PROMETHEUS_PORT) ||
+    PrometheusExporter.DEFAULT_OPTIONS.port;
+  const exporter = new PrometheusExporter(
+    {
+      startServer: true,
+      host,
+      port,
+    },
+    () => {
+      console.log(
+        `Prometheus scrape endpoint: http://${host}:${port}${PrometheusExporter.DEFAULT_OPTIONS.endpoint}`,
+      );
+    },
+  );
+  return new MeterProvider({ exporter, interval: 1000 });
 }
