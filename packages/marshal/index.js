dfa4d8fb716c2e71e2d110fc8a8448ea3f45d522
Mark S. Miller <erights@gmail.com>
2021-02-16 02:11:02 -0800

    fix: Suppress dangerous *undiagnosed* error

--- packages/marshal/index.js
+++ packages/marshal/index.js
@@ -11,4 +11,11 @@ export {
   Far,
 } from './src/marshal';
 
-export { stringify, parse } from './src/marshal-stringify';
+// Commenting out the following reexport prevents the error seen at
+// https://github.com/Agoric/agoric-sdk/pull/2437/checks?check_run_id=1909543649
+// This is crazy because reexporting something that no one imports should
+// not be able to cause an error. If anyone was importing it, then
+// commenting out the export should cause an error, which it does not.
+// TODO Investigate. This anomaly may be a big deal.
+//
+// export { stringify, parse } from './src/marshal-stringify';
