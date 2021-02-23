854ca63f70f7b76bbfc238cdee6decd26767dda1
Mark S. Miller <erights@gmail.com>
2021-02-22 18:44:36 -0800

    fix: make reexports explicit

--- packages/notifier/src/index.js
+++ packages/notifier/src/index.js
@@ -1,3 +1,16 @@
-export * from './notifier';
-export * from './subscriber';
-export * from './asyncIterableAdaptor';
+export {
+  makeNotifier,
+  makeNotifierKit,
+  makeNotifierFromAsyncIterable,
+} from './notifier';
+export { makeSubscription, makeSubscriptionKit } from './subscriber';
+export {
+  observeNotifier,
+  observeIterator,
+  observeIteration,
+  // deprecated. Consider not reexporting
+  updateFromIterable,
+  updateFromNotifier,
+  // Consider deprecating or not reexporting
+  makeAsyncIterableFromNotifier,
+} from './asyncIterableAdaptor';
