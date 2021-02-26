2bd4a86b5d6d3de7ef39251262da8859db2459df
Dan Connolly <connolly@agoric.com>
2021-02-25 18:56:46 -0600

    refactor(avaXS): clarify runTestScript; simplify main()
      - hoist asset paths to module constants
      - hoist, document externals as module constant
      - untangle withXSnap; use spawnXSnap instead
      - move bundleSource of test script from main to runTestScript,
        since runTestScript needs bundleSource power anyway
      - add @returns for runTest
      - fold extractFlag into main()
        - don't mutate global process.argv
        - only look for --debug as 1st arg

--- packages/xsnap/src/avaXS.js
+++ packages/xsnap/src/avaXS.js
@@ -2,56 +2,98 @@
 
 Usage:
 
-agoric-sdk/packages/ERTP$ node -r esm ../xsnap/src/avaXS.js test/unitTests/test-*.js
+  node -r esm avaXS.js [--debug] test-*.js
 
 */
+
 // @ts-check
 
 /* eslint-disable no-await-in-loop */
 import '@agoric/install-ses';
 import { xsnap } from './xsnap';
 
+// scripts for use in xsnap subprocesses
+const SESboot = `../dist/bootstrap.umd.js`;
+const avaAssert = `./avaAssertXS.js`;
+const avaHandler = `./avaHandler.js`;
+
 const importMetaUrl = `file://${__filename}`;
 /** @type { (ref: string, readFile: typeof import('fs').promises.readFile ) => Promise<string> } */
 const asset = (ref, readFile) =>
   readFile(new URL(ref, importMetaUrl).pathname, 'utf8');
 
+/**
+ * When we bundle test scripts, we leave these externals
+ * as `require(...)` style graph exits and (in avaHandler.js)
+ * supply them via a `require` endowment
+ * on the Compartment used to run the script.
+ */
+const externals = [
+  'ava',
+  '@agoric/bundle-source',
+  '@agoric/install-ses',
+  '@agoric/install-metering-and-ses',
+];
+
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 
 /**
- * @param {string} input
- * @param {string} src
- * @param {{
- *   withXSnap: (o: Object, fn: ((w: XSnap) => Promise<void>)) => Promise<void>,
- *   bundleSource: (...args: [string, ...unknown[]]) => Promise<Bundle>,
- *   resolve: typeof import('path').resolve,
- *   dirname: typeof import('path').dirname,
- *   debug: boolean,
- * }} io
+ * Run one test script in an xsnap subprocess.
  *
- * @typedef {{
- *   id?: number, status: 'ok' | 'not ok' | 'SKIP', message?: string
- * } | { plan: number} | { note: string, label?: string }} TapMessage
- * @typedef {ReturnType<typeof import('./xsnap').xsnap>} XSnap
- * @typedef {{ moduleFormat: string }} Bundle
+ * The subprocess reports back once for each test assertion.
+ *
+ * @typedef {{ id?: number, status: Status, message?: string }
+ *         | { plan: number}
+ *         | { note: string, label?: string }
+ * } TapMessage
+ *
+ * It also calls back if a test calls `bundleSource`.
+ *
+ * @typedef {{ moduleFormat: string, source: string }} Bundle
+ *
+ * And finally it reports back a summary of assertion results.
  *
  * @typedef {{
  *   pass: number,
  *   fail: number,
  *   total: number,
  * }} Summary
+ *
+ * @param { string } filename
+ * @param { string[] } preamble scripts to run in XS start compartment
+ * @param { boolean } verbose
+ * @param {{
+ *   spawnXSnap: (opts: object) => XSnap,
+ *   bundleSource: (...args: [string, ...unknown[]]) => Promise<Bundle>,
+ *   resolve: typeof import('path').resolve,
+ *   dirname: typeof import('path').dirname,
+ * }} io
+ * @returns {Promise<{
+ *   qty: number,
+ *   byStatus: Record<Status, number>
+ * }>} quantity of tests run and breakdown by status
+ *
+ * @typedef { 'ok' | 'not ok' | 'SKIP' } Status
+ * @typedef {ReturnType<typeof import('./xsnap').xsnap>} XSnap
  */
-async function runTest(
-  input,
-  src,
-  { withXSnap, bundleSource, resolve, dirname, debug },
+async function runTestScript(
+  filename,
+  preamble,
+  verbose,
+  { spawnXSnap, bundleSource, resolve, dirname },
 ) {
-  let label = '';
+  const testBundle = await bundleSource(filename, 'getExport', { externals });
+
   let qty = 0;
   const byStatus = { ok: 0, 'not ok': 0, SKIP: 0 };
+  let label = '';
 
-  /** @type { (msg: ArrayBuffer) => Promise<ArrayBuffer> } */
+  /**
+   * Handle callback "command" from xsnap subprocess.
+   *
+   *  @type { (msg: ArrayBuffer) => Promise<ArrayBuffer> }
+   */
   async function handleCommand(message) {
     /**
      * See also send() in avaHandler.js
@@ -69,43 +111,43 @@ async function runTest(
 
     if ('label' in msg) {
       label = msg.label || label;
-      if (debug) console.log(`${input}: ${msg.label} ${msg.note}`);
+      if (verbose) {
+        console.log(`${filename}: ${msg.label} ${msg.note}`);
+      }
     }
     if ('status' in msg) {
       byStatus[msg.status] += 1;
       qty += 1;
       if (msg.status === 'not ok') {
-        console.warn({ ...msg, input, label });
+        console.warn({ ...msg, filename, label });
       }
     }
     return encoder.encode('null');
   }
 
-  const testPath = resolve(input);
-  const literal = JSON.stringify;
-
   // ISSUE: only works in one file / dir
-  // TODO: migrate to import.meta.url
+  const literal = JSON.stringify;
+  const testPath = resolve(filename);
   const pathGlobalsKludge = `
     globalThis.__filename = ${literal(testPath)};
     globalThis.__dirname = ${literal(dirname(testPath))};
    `;
-  await withXSnap({ name: input, handleCommand }, async worker => {
+
+  const worker = spawnXSnap({ handleCommand });
+  try {
+    for (const script of preamble) {
+      await worker.evaluate(script);
+    }
+
     await worker.evaluate(pathGlobalsKludge);
-    await worker.issueStringCommand(src);
-  });
+
+    // Send the test script to avaHandler.
+    await worker.issueStringCommand(testBundle.source);
+  } finally {
+    await worker.terminate();
+  }
 
   return { qty, byStatus };
-}
-
-/** @type {(argv: string[], name: string) => boolean } */
-function extractFlag(argv, name) {
-  const ix = argv.indexOf(name);
-  if (ix >= 0) {
-    argv.splice(ix, 1);
-    return true;
-  }
-  return false;
 }
 
 /**
@@ -123,60 +165,42 @@ async function main(
   argv,
   { bundleSource, spawn, osType, readFile, resolve, dirname },
 ) {
-  async function testSource(input) {
-    const bundle = await bundleSource(input, 'getExport', {
-      externals: [
-        'ava',
-        '@agoric/bundle-source',
-        '@agoric/install-ses',
-        '@agoric/install-metering-and-ses',
-      ],
+  const args = argv.slice(2);
+  const debug = args[0] === '--debug';
+  const files = debug ? args.slice(1) : args;
+
+  const spawnXSnap = opts =>
+    xsnap({
+      ...opts,
+      debug,
+      spawn,
+      os: osType(),
+      meteringLimit: 0,
+      stdout: 'inherit',
+      stderr: 'inherit',
     });
-    return bundle.source;
-  }
-
-  const sesShim = await asset(`../dist/bootstrap.umd.js`, readFile);
-  const tinyAva = await asset(`./avaAssertXS.js`, readFile);
-  const avaHandler = await asset(`./avaHandler.js`, readFile);
-
-  const debug = extractFlag(argv, '--debug');
 
   // we only use import() in type annotations
   const hideImport = (/** @type { string } */ src) =>
     src.replace(/import\(/g, '');
 
-  async function withXSnap(opts, thunk) {
-    const worker = xsnap({
-      meteringLimit: 0,
-      spawn,
-      os: osType(),
-      stdout: 'inherit',
-      stderr: 'inherit',
-      debug,
-      ...opts,
-    });
-    try {
-      await worker.evaluate(sesShim);
-      await worker.evaluate(hideImport(tinyAva));
-      await worker.evaluate(hideImport(avaHandler));
-      return await thunk(worker);
-    } finally {
-      worker.terminate();
-    }
-  }
+  const preamble = [
+    await asset(SESboot, readFile),
+    hideImport(await asset(avaAssert, readFile)),
+    hideImport(await asset(avaHandler, readFile)),
+  ];
 
   let totalTests = 0;
   const stats = { ok: 0, 'not ok': 0, SKIP: 0 };
-  for (const input of argv.slice(2)) {
-    console.log('# test script:', input);
-    const src = await testSource(input);
 
-    const { qty, byStatus } = await runTest(input, src, {
-      withXSnap,
+  for (const filename of files) {
+    console.log('# test script:', filename);
+
+    const { qty, byStatus } = await runTestScript(filename, preamble, debug, {
+      spawnXSnap,
       bundleSource,
       resolve,
       dirname,
-      debug,
     });
 
     totalTests += qty;
@@ -191,7 +215,7 @@ async function main(
 
 /* eslint-disable global-require */
 if (require.main === module) {
-  main(process.argv, {
+  main([...process.argv], {
     bundleSource: require('@agoric/bundle-source').default,
     spawn: require('child_process').spawn,
     osType: require('os').type,
