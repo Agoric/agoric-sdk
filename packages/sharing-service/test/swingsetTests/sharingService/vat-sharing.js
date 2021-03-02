e88abfe68ebaee26c89b7fcddb32785fa133e987
Brian Warner <warner@lothar.com>
2021-03-02 09:17:53 -0800

    fix(sharing-service): add Far to all pass-by-reference objects
    refs #2018

--- packages/sharing-service/test/swingsetTests/sharingService/vat-sharing.js
+++ packages/sharing-service/test/swingsetTests/sharingService/vat-sharing.js
@@ -1,7 +1,8 @@
 // Copyright (C) 2019 Agoric, under Apache License 2.0
 
+import { Far } from '@agoric/marshal';
 import { makeSharingService } from '../../../src/sharing';
 
 export function buildRootObject(_vatPowers) {
-  return harden({ makeSharingService });
+  return Far('root', { makeSharingService });
 }
