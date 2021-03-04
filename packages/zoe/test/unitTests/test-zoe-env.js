6efc37d4b6c17d13c43894df16816b342b7bb3f7
Dan Connolly <connolly@agoric.com>
2021-03-04 14:23:50 -0600

    feat(zoe): tools/prepare-test-env for makeKind etc.

---
+++ packages/zoe/test/unitTests/test-zoe-env.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import '@agoric/zoe/tools/prepare-test-env';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import test from 'ava';
+
+test('harden from SES is in the zoe contract environment', t => {
+  harden();
+  t.pass();
+});
+
+test('(mock) makeKind from SwingSet is in the zoe contract environment', t => {
+  makeKind();
+  t.pass();
+});
+
+test('(mock) makeWeakStore from SwingSet is in the zoe contract environment', t => {
+  makeWeakStore();
+  t.pass();
+});
