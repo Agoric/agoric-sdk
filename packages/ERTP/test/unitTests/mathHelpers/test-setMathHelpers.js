3a18a50070dbdaf838d01683b6d7d6256f1f53dc
Kate Sills <kate@agoric.com>
2021-03-03 17:34:20 -0800

    chore: lint-fix

--- packages/ERTP/test/unitTests/mathHelpers/test-setMathHelpers.js
+++ packages/ERTP/test/unitTests/mathHelpers/test-setMathHelpers.js
@@ -115,7 +115,7 @@ const runSetMathHelpersTests = (t, [a, b, c], a2 = undefined) => {
     `m.isEmpty([]) is true`,
   );
   t.throws(
-    () => m.isEmpty({ brand: mockBrand, value:  Data({}) }),
+    () => m.isEmpty({ brand: mockBrand, value: Data({}) }),
     { message: `value (an object) must be a Nat or an array` },
     `m.isEmpty({}) throws`,
   );
