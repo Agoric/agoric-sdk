b158da654e37bdc2a83e6e3e51358de2dd95f064
Kate Sills <kate@agoric.com>
2021-03-16 17:03:02 -0700

    chore: fix fake price authority usage in cosmic-swingset

--- packages/cosmic-swingset/lib/ag-solo/vats/vat-priceAuthority.js
+++ packages/cosmic-swingset/lib/ag-solo/vats/vat-priceAuthority.js
@@ -1,18 +1,12 @@
 import { Far } from '@agoric/marshal';
 import { makePriceAuthorityRegistry } from '@agoric/zoe/tools/priceAuthorityRegistry';
 import { makeFakePriceAuthority } from '@agoric/zoe/tools/fakePriceAuthority';
-import { makeLocalAmountMath } from '@agoric/ertp';
 
 export function buildRootObject(_vatPowers) {
   return Far('root', {
     makePriceAuthority: makePriceAuthorityRegistry,
     async makeFakePriceAuthority(options) {
-      const { issuerIn, issuerOut } = options;
-      const [mathIn, mathOut] = await Promise.all([
-        makeLocalAmountMath(issuerIn),
-        makeLocalAmountMath(issuerOut),
-      ]);
-      return makeFakePriceAuthority({ ...options, mathIn, mathOut });
+      return makeFakePriceAuthority({ ...options });
     },
   });
 }
