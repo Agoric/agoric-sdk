d934f14f3c1900319f8b2f37d635812c0b37b841
Kate Sills <kate@agoric.com>
2021-03-24 21:30:46 -0700

    chore: lint-fix and yarn install

--- packages/ui-components/src/index.js
+++ packages/ui-components/src/index.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line import/no-unresolved
 export { default as makeNatAmountInput } from './components/NatAmountInput';
+// eslint-disable-next-line import/no-unresolved
 export { default as makeNatPurseSelector } from './components/NatPurseSelector';
 export * from './display';
