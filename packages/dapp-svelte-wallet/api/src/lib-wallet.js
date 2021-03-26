85cb9e8f623f7cfd5e4513a841b1910980dec51d
Kate Sills <kate@agoric.com>
2021-03-25 22:36:38 -0700

    chore: fix linting

--- packages/dapp-svelte-wallet/api/src/lib-wallet.js
+++ packages/dapp-svelte-wallet/api/src/lib-wallet.js
@@ -244,6 +244,7 @@ export function makeWallet({
      * @type {PursesJSONState}
      */
     const jstate = {
+      brand,
       brandBoardId,
       ...(depositBoardId && { depositBoardId }),
       brandPetname,
