4be223efba4ddecdb1e54ed7b51c038c380fe64e
Kate Sills <kate@agoric.com>
2021-04-27 16:43:58 -0700

    refactor!: remove makePercent (#2987)

--- packages/zoe/src/contracts/callSpread/percent.js
+++ packages/zoe/src/contracts/callSpread/percent.js
@@ -6,10 +6,12 @@ import { makeRatio } from '../../contractSupport';
 
 const BASIS_POINTS = 10000n;
 
+/** @type {Make100Percent} */
 export function make100Percent(brand) {
   return makeRatio(BASIS_POINTS, brand, BASIS_POINTS);
 }
 
+/** @type {Make0Percent} */
 export function make0Percent(brand) {
   return makeRatio(0n, brand);
 }
