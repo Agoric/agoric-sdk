138399195e2552cd08b449f352fd405adf4357ed
Dan Connolly <connolly@agoric.com>
2021-05-01 17:44:19 -0500

    refactor(xsnap): meter detail test

--- packages/xsnap/test/test-xsnap.js
+++ packages/xsnap/test/test-xsnap.js
@@ -510,17 +510,26 @@ test('meter details', async t => {
   const opts = options();
   const vat = xsnap(opts);
   t.teardown(() => vat.terminate());
-  const result = await vat.evaluate(`({ size: 2, color: 'green' })`);
+  const result = await vat.evaluate(`
+  let m = new Map();
+  for (ix = 0; ix < 20000; ix++) {
+    m.set(ix, 'garbage');
+    // m.delete(m);
+  }
+  `);
   const {
     meterUsage: { meterType, ...meters },
   } = result;
+  t.log(meters);
   t.is(meterType, 'xs-meter-3');
+  const { entries, fromEntries } = Object;
   t.deepEqual(
-    ['compute', 'allocate', 'allocateChunksCalls', 'allocateSlotsCalls'],
-    Object.keys(meters),
-  );
-  t.deepEqual(
-    ['number', 'number', 'number', 'number'],
-    Object.values(meters).map(v => typeof v),
+    {
+      compute: 'number',
+      allocate: 'number',
+      allocateChunksCalls: 'number',
+      allocateSlotsCalls: 'number',
+    },
+    fromEntries(entries(meters).map(([p, v]) => [p, typeof v])),
   );
 });
