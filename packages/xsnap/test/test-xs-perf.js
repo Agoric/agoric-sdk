bc2029a71a5ea67a997e16543e1354707a57baf7
Dan Connolly <connolly@agoric.com>
2021-05-03 13:45:01 -0500

    test(xsnap): benchmark harden()

--- packages/xsnap/test/test-xs-perf.js
+++ packages/xsnap/test/test-xs-perf.js
@@ -1,7 +1,18 @@
+/* global print, now */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import test from 'ava';
+import * as fs from 'fs';
+import * as path from 'path';
 import { xsnap } from '../src/xsnap';
 import { options } from './test-xsnap';
+
+const importModuleUrl = `file://${__filename}`;
+
+const asset = async (...segments) =>
+  fs.promises.readFile(
+    path.join(importModuleUrl.replace('file:/', ''), '..', ...segments),
+    'utf-8',
+  );
 
 test('meter details', async t => {
   const opts = options();
@@ -53,4 +64,63 @@ test('high resolution timer', async t => {
   const [x] = opts.messages.map(JSON.parse);
   t.log({ usec: x, date: new Date(x * 1000.0) });
   t.is('number', typeof x);
+});
+
+test('harden performance', async t => {
+  const bootScript = await asset('..', 'dist', 'bundle-ses-boot.umd.js');
+  const opts = { ...options(), meteringLimit: 0 };
+  const name = 'SES lockdown worker';
+  const vat = xsnap({ ...opts, name });
+  // t.teardown(() => vat.terminate());
+  await vat.evaluate(bootScript);
+  t.deepEqual([], opts.messages);
+
+  await vat.evaluate(`
+    const encoder = new TextEncoder();
+    globalThis.send = msg => issueCommand(encoder.encode(JSON.stringify(msg)).buffer);
+  `);
+
+  const script = `
+  const events = [];
+  let t = now();
+  function event(...args) {
+    const te = now();
+    events.push([te, te - t, ...args]);
+    t = te;
+  }
+  function bench() {
+    // 12 iterations, 65,536 objects, ~never finishes
+    for (let iter = 0; iter < 11; iter += 1) {
+      const size = 1024 * (1 << Math.round(iter / 2));
+      print('iter', iter, size);
+      event('prep iter', iter, size);
+      const keys = new Array(size);
+      for (let ix = 0; ix < size; ix += 1) {
+        keys[ix] = {};
+      }
+      event('start loop over keys', iter, size);
+      for (const _key of keys) {
+        // how long does looping take?
+      }
+      event('start hardening keys', iter, size);
+      for (const key of keys) {
+        harden(key);
+      }
+      event('done hardening', iter, size);
+    }
+  }
+  print('bench...');
+  bench(events);
+  print('send...', events.length);
+  send(events);
+  print('done');
+  `;
+  await vat.evaluate(script);
+  vat.close();
+  const [events] = opts.messages.map(JSON.parse);
+  t.is(11 * 4, events.length);
+  // roughly CSV
+  for (const event of events) {
+    t.log(event.map(v => `${v}`).join(','));
+  }
 });
