44ef9e4b1750716ff1e4314f66f3681807f250aa
Chris Hibbert <hibbert@agoric.com>
2021-05-06 14:58:53 -0700

    fix: simplify scheduling in distributeFees
    closes #3044

--- packages/vats/src/distributeFees.js
+++ packages/vats/src/distributeFees.js
@@ -25,15 +25,13 @@ export function makeTreasuryFeeCollector(zoe, treasuryCreatorFacet) {
 // from accounts to purses. Each time the epochTimer signals the end of an
 // Epoch, it will ask the bank's notifier for a fresh list of accounts and ask
 // the treasury for the fees that have been collected to date. It will then
-// divide the funds evenly amount the accounts, and send payments in batches of
-// depositsPerUpdate every updateInterval. When the payment doesn't divide
-// evenly among the accounts, it holds the remainder till the next epoch.
+// divide the funds evenly among the accounts, and send payments.
+// When the payment doesn't divide evenly among the accounts, it holds the
+// remainder till the next epoch.
 
 /** @type {BuildFeeDistributor} */
-export function buildDistributor(treasury, bank, epochTimer, timer, params) {
+export function buildDistributor(treasury, bank, epochTimer, params) {
   const {
-    depositsPerUpdate,
-    updateInterval,
     // By default, we assume the epochTimer fires once per epoch.
     epochInterval = 1n,
     runIssuer,
@@ -42,44 +40,6 @@ export function buildDistributor(treasury, bank, epochTimer, timer, params) {
   const accountsNotifier = E(bank).getAccountsNotifier();
   let leftOverPayment;
   let leftOverValue = 0n;
-  const queuedAccounts = [];
-  const queuedPayments = [];
-  let lastWallTimeUpdate;
-  const timerNotifier = E(timer).makeNotifier(0n, updateInterval);
-
-  /**
-   * @param {(pmt: Payment[]) => void} disposeRejectedPayments
-   */
-  async function scheduleDeposits(disposeRejectedPayments) {
-    if (!queuedPayments.length) {
-      return;
-    }
-
-    ({ updateCount: lastWallTimeUpdate } = await E(
-      timerNotifier,
-    ).getUpdateSince(lastWallTimeUpdate));
-
-    // queuedPayments may have changed since the `await`.
-    if (!queuedPayments.length) {
-      return;
-    }
-
-    const accounts = queuedAccounts.splice(0, depositsPerUpdate);
-    const payments = queuedPayments.splice(0, depositsPerUpdate);
-    E(bank)
-      .depositMultiple(runBrand, accounts, payments)
-      .then(settledResults => {
-        const rejectedPayments = payments.filter(
-          (_pmt, i) =>
-            settledResults[i] && settledResults[i].status === 'rejected',
-        );
-
-        // Redeposit the payments.
-        disposeRejectedPayments(rejectedPayments);
-      })
-      .catch(e => console.error(`distributeFees cannot depositMultiple`, e));
-    scheduleDeposits(disposeRejectedPayments);
-  }
 
   async function schedulePayments() {
     let payment = E(treasury).collectFees();
@@ -105,12 +65,17 @@ export function buildDistributor(treasury, bank, epochTimer, timer, params) {
     const manyPayments = await E(runIssuer).splitMany(payment, amounts);
     // manyPayments is hardened, so we can't use pop()
     leftOverPayment = manyPayments[manyPayments.length - 1];
-    queuedPayments.push(...manyPayments.slice(0, manyPayments.length - 1));
-    queuedAccounts.push(...accounts);
+    const actualPayments = manyPayments.slice(0, manyPayments.length - 1);
 
-    scheduleDeposits(_pmts => {
-      // TODO: Somehow reclaim the rejected payments.
-    });
+    // eslint-disable-next-line no-unused-vars
+    const settledResultsP = Promise.allSettled(
+      // The scheduler will decide how fast to process the deposits.
+      accounts.map((acct, i) =>
+        E(bank).deposit(runBrand, acct, actualPayments[i]),
+      ),
+    );
+
+    // TODO: examine settledResultsP for failures, and reclaim those payments.
   }
 
   const timeObserver = {
