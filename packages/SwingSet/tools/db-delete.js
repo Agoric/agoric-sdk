d14fa1e85eb4e5be2c8eec4ac0af7f0cffbdc3c7
Chip Morningstar <chip@fudco.com>
2021-05-07 01:39:53 -0700

    feat: tools for fiddling with kernel DB

---
+++ packages/SwingSet/tools/db-delete.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env -S node -r esm
+
+import '@agoric/install-ses';
+import process from 'process';
+import { openSwingStore } from '@agoric/swing-store-lmdb';
+
+const log = console.log;
+
+function usage() {
+  log(`
+Command line:
+  db-delete-key.js [FLAGS] STATEDIR KEY
+
+FLAGS may be:
+  --range - delete all keys starting with \`\${KEY}.\`
+`);
+}
+
+function fail(message, printUsage) {
+  if (message) {
+    log(message);
+  }
+  if (printUsage) {
+    usage();
+  }
+  process.exit(1);
+}
+
+function run() {
+  const argv = process.argv.slice(2);
+
+  let range = false;
+  while (argv[0] && argv[0].startsWith('-')) {
+    const flag = argv.shift();
+    switch (flag) {
+      case '--range':
+        range = true;
+        break;
+      default:
+        fail(`invalid flag ${flag}`, true);
+        break;
+    }
+  }
+  if (argv.length !== 2) {
+    fail('wrong number of args', true);
+  }
+  const stateDBDir = argv.shift();
+  const key = argv.shift();
+
+  const { storage, commit } = openSwingStore(stateDBDir);
+
+  if (range) {
+    for (const k of storage.getKeys(`${key}.`, `${key}/`)) {
+      storage.delete(k);
+    }
+  } else {
+    storage.delete(key);
+  }
+  commit();
+}
+
+run();
