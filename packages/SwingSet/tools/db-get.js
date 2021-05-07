7f73c3db2d01c751e75a3f30b94b8ee4d6e23df3
Chip Morningstar <chip@fudco.com>
2021-05-07 01:39:53 -0700

    feat: tools for fiddling with kernel DB

---
+++ packages/SwingSet/tools/db-get.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env -S node -r esm
+
+import '@agoric/install-ses';
+import process from 'process';
+import { openSwingStore } from '@agoric/swing-store-lmdb';
+
+const log = console.log;
+
+const out = process.stdout;
+function p(str) {
+  out.write(str);
+  out.write('\n');
+}
+
+function usage() {
+  log(`
+Command line:
+  db-get-key.js [FLAGS] STATEDIR KEY
+
+FLAGS may be:
+  --raw   - just output the value(s) without adornment or key info
+  --range - output values for all keys starting with \`\${KEY}.\`
+`);
+}
+
+function fail(message, printUsage) {
+  if (message) {
+    log(message);
+  }
+  if (printUsage) {
+    usage();
+  }
+  process.exit(1);
+}
+
+function run() {
+  const argv = process.argv.slice(2);
+
+  let raw = false;
+  let range = false;
+  while (argv[0] && argv[0].startsWith('-')) {
+    const flag = argv.shift();
+    switch (flag) {
+      case '--raw':
+        raw = true;
+        break;
+      case '--range':
+        range = true;
+        break;
+      default:
+        fail(`invalid flag ${flag}`, true);
+        break;
+    }
+  }
+  if (argv.length !== 2) {
+    fail('wrong number of args', true);
+  }
+  const stateDBDir = argv.shift();
+  const key = argv.shift();
+
+  const { storage } = openSwingStore(stateDBDir);
+
+  function pkv(k) {
+    const value = storage.get(k);
+    if (raw) {
+      p(value);
+    } else {
+      p(`${k} :: ${value}`);
+    }
+  }
+
+  if (range) {
+    for (const k of storage.getKeys(`${key}.`, `${key}/`)) {
+      pkv(k);
+    }
+  } else {
+    pkv(key);
+  }
+}
+
+run();
