4223e7e3f3d7d9cf367d48c2195141e499638a1d
Kris Kowal <kris@agoric.com>
2021-05-21 21:49:37 -0700

    SIFT fix(xsnap): Account for Uint8Array in Text shim WIP needs test

--- packages/xsnap/lib/text-shim.js
+++ packages/xsnap/lib/text-shim.js
@@ -7,14 +7,22 @@ const { fromString } = ArrayBuffer;
 const { fromArrayBuffer } = String;
 
 class TextEncoder {
-  encode(s) {
-    return new Uint8Array(fromString(s));
+  encode(text) {
+    return new Uint8Array(fromString(text));
   }
 }
 
 class TextDecoder {
-  decode(bs) {
-    return fromArrayBuffer(bs);
+  decode(bytes) {
+    if (bytes instanceof Uint8Array) {
+      // There is no guarantee that the ArrayBuffer underlying a Uint8Array is
+      // aligned at their respective 0 offsets, but the XS
+      // String.fromArrayBuffer insists on receiving an ArrayBuffer.
+      const temp = new Uint8Array(bytes.byteLength);
+      temp.set(bytes);
+      bytes = temp.buffer;
+    }
+    return fromArrayBuffer(bytes);
   }
 }
 
