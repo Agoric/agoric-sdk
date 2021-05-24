134cb15f956a24b15af66f06679ba8680116fa93
Kate Sills <kate@agoric.com>
2021-05-24 15:38:53 -0700

    refactor(zoe): separate offerHandlerStorage and delete entry after use (#3127)
    * refactor(zoe): separate offerHandlerStorage and delete entry after use
    
    * chore: Suggestion for #3127 better typing (#3158)
    
    Co-authored-by: Kate Sills <kate@agoric.com>
    
    * chore: type must be `any`, since `unknown` requires a value is returned, which is not a requirement of an offerHandler
    
    Co-authored-by: Mark S. Miller <erights@users.noreply.github.com>

---
+++ packages/zoe/src/contractFacet/offerHandlerStorage.js
@@ -0,0 +1,29 @@
+// @ts-check
+
+import { makeWeakStore as makeNonVOWeakStore } from '@agoric/store';
+
+import { makeHandle } from '../makeHandle';
+
+export const makeOfferHandlerStorage = () => {
+  /** @type {WeakStore<InvitationHandle, OfferHandler>} */
+  const invitationHandleToHandler = makeNonVOWeakStore('invitationHandle');
+
+  /** @type {(offerHandler: OfferHandler) => InvitationHandle} */
+  const storeOfferHandler = offerHandler => {
+    const invitationHandle = makeHandle('Invitation');
+    invitationHandleToHandler.init(invitationHandle, offerHandler);
+    return invitationHandle;
+  };
+
+  /** @type {(invitationHandle: InvitationHandle) => OfferHandler} */
+  const takeOfferHandler = invitationHandle => {
+    const offerHandler = invitationHandleToHandler.get(invitationHandle);
+    invitationHandleToHandler.delete(invitationHandle);
+    return offerHandler;
+  };
+
+  return harden({
+    storeOfferHandler,
+    takeOfferHandler,
+  });
+};
