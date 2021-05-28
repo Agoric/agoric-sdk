759d6abe4ec5f798dca15a88d3523c63808a8b30
Michael FIG <mfig@agoric.com>
2021-05-27 23:03:15 -0600

    feat: send AG_COSMOS_INIT supplyCoins instead of vpurse genesis

--- golang/cosmos/x/vpurse/types/genesis.pb.go
+++ golang/cosmos/x/vpurse/types/genesis.pb.go
@@ -5,7 +5,6 @@ package types
 
 import (
 	fmt "fmt"
-	github_com_cosmos_cosmos_sdk_types "github.com/cosmos/cosmos-sdk/types"
 	_ "github.com/gogo/protobuf/gogoproto"
 	proto "github.com/gogo/protobuf/proto"
 	io "io"
@@ -25,12 +24,6 @@ var _ = math.Inf
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 type GenesisState struct {
-	// Cosmos-SDK layer bech32 address to receive bootstrap_value urun.
-	BootstrapAddress string `protobuf:"bytes,1,opt,name=bootstrap_address,json=bootstrapAddress,proto3" json:"bootstrap_address,omitempty"`
-	// Number of urun to have the treasury send to bootstrap_address.
-	BootstrapValue github_com_cosmos_cosmos_sdk_types.Int `protobuf:"bytes,2,opt,name=bootstrap_value,json=bootstrapValue,proto3,customtype=github.com/cosmos/cosmos-sdk/types.Int" json:"bootstrap_value"`
-	// Number of urun to send from bootstrap_address to new ag-solo clients.
-	DonationValue github_com_cosmos_cosmos_sdk_types.Int `protobuf:"bytes,3,opt,name=donation_value,json=donationValue,proto3,customtype=github.com/cosmos/cosmos-sdk/types.Int" json:"donation_value"`
 }
 
 func (m *GenesisState) Reset()         { *m = GenesisState{} }
@@ -66,13 +59,6 @@ func (m *GenesisState) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_GenesisState proto.InternalMessageInfo
 
-func (m *GenesisState) GetBootstrapAddress() string {
-	if m != nil {
-		return m.BootstrapAddress
-	}
-	return ""
-}
-
 func init() {
 	proto.RegisterType((*GenesisState)(nil), "agoric.vpurse.GenesisState")
 }
@@ -80,24 +66,18 @@ func init() {
 func init() { proto.RegisterFile("agoric/vpurse/genesis.proto", fileDescriptor_5640d8342a3d07c5) }
 
 var fileDescriptor_5640d8342a3d07c5 = []byte{
-	// 272 bytes of a gzipped FileDescriptorProto
+	// 169 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x4e, 0x4c, 0xcf, 0x2f,
 	0xca, 0x4c, 0xd6, 0x2f, 0x2b, 0x28, 0x2d, 0x2a, 0x4e, 0xd5, 0x4f, 0x4f, 0xcd, 0x4b, 0x2d, 0xce,
 	0x2c, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x85, 0x48, 0xea, 0x41, 0x24, 0xa5, 0x44,
-	0xd2, 0xf3, 0xd3, 0xf3, 0xc1, 0x32, 0xfa, 0x20, 0x16, 0x44, 0x91, 0xd2, 0x47, 0x46, 0x2e, 0x1e,
-	0x77, 0x88, 0xb6, 0xe0, 0x92, 0xc4, 0x92, 0x54, 0x21, 0x6d, 0x2e, 0xc1, 0xa4, 0xfc, 0xfc, 0x92,
-	0xe2, 0x92, 0xa2, 0xc4, 0x82, 0xf8, 0xc4, 0x94, 0x94, 0xa2, 0xd4, 0xe2, 0x62, 0x09, 0x46, 0x05,
-	0x46, 0x0d, 0xce, 0x20, 0x01, 0xb8, 0x84, 0x23, 0x44, 0x5c, 0x28, 0x9c, 0x8b, 0x1f, 0xa1, 0xb8,
-	0x2c, 0x31, 0xa7, 0x34, 0x55, 0x82, 0x09, 0xa4, 0xd4, 0x49, 0xef, 0xc4, 0x3d, 0x79, 0x86, 0x5b,
-	0xf7, 0xe4, 0xd5, 0xd2, 0x33, 0x4b, 0x32, 0x4a, 0x93, 0xf4, 0x92, 0xf3, 0x73, 0xf5, 0x93, 0xf3,
-	0x8b, 0x73, 0xf3, 0x8b, 0xa1, 0x94, 0x6e, 0x71, 0x4a, 0xb6, 0x7e, 0x49, 0x65, 0x41, 0x6a, 0xb1,
-	0x9e, 0x67, 0x5e, 0x49, 0x10, 0x1f, 0xdc, 0x98, 0x30, 0x90, 0x29, 0x42, 0xa1, 0x5c, 0x7c, 0x29,
-	0xf9, 0x79, 0x89, 0x25, 0x99, 0xf9, 0x79, 0x50, 0x73, 0x99, 0xc9, 0x32, 0x97, 0x17, 0x66, 0x0a,
-	0xd8, 0x58, 0x2b, 0x96, 0x17, 0x0b, 0xe4, 0x19, 0x9c, 0x82, 0x4f, 0x3c, 0x92, 0x63, 0xbc, 0xf0,
-	0x48, 0x8e, 0xf1, 0xc1, 0x23, 0x39, 0xc6, 0x09, 0x8f, 0xe5, 0x18, 0x2e, 0x3c, 0x96, 0x63, 0xb8,
-	0xf1, 0x58, 0x8e, 0x21, 0xca, 0x12, 0xc9, 0x58, 0x47, 0x48, 0xd0, 0x42, 0x02, 0x11, 0x6c, 0x6c,
-	0x7a, 0x7e, 0x4e, 0x62, 0x5e, 0x3a, 0xcc, 0xbe, 0x0a, 0x58, 0xa8, 0x83, 0x6d, 0x4b, 0x62, 0x03,
-	0x87, 0xa7, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0x41, 0xba, 0x27, 0x6a, 0x93, 0x01, 0x00, 0x00,
+	0xd2, 0xf3, 0xd3, 0xf3, 0xc1, 0x32, 0xfa, 0x20, 0x16, 0x44, 0x91, 0x92, 0x08, 0x17, 0x8f, 0x3b,
+	0x44, 0x57, 0x70, 0x49, 0x62, 0x49, 0xaa, 0x15, 0xcb, 0x8b, 0x05, 0xf2, 0x0c, 0x4e, 0xc1, 0x27,
+	0x1e, 0xc9, 0x31, 0x5e, 0x78, 0x24, 0xc7, 0xf8, 0xe0, 0x91, 0x1c, 0xe3, 0x84, 0xc7, 0x72, 0x0c,
+	0x17, 0x1e, 0xcb, 0x31, 0xdc, 0x78, 0x2c, 0xc7, 0x10, 0x65, 0x99, 0x9e, 0x59, 0x92, 0x51, 0x9a,
+	0xa4, 0x97, 0x9c, 0x9f, 0xab, 0xef, 0x08, 0xb1, 0x1c, 0x62, 0x8d, 0x6e, 0x71, 0x4a, 0xb6, 0x7e,
+	0x7a, 0x7e, 0x4e, 0x62, 0x5e, 0xba, 0x7e, 0x72, 0x7e, 0x71, 0x6e, 0x7e, 0xb1, 0x7e, 0x05, 0xcc,
+	0x5d, 0x25, 0x95, 0x05, 0xa9, 0xc5, 0x49, 0x6c, 0x60, 0x1b, 0x8d, 0x01, 0x01, 0x00, 0x00, 0xff,
+	0xff, 0x06, 0x16, 0xf7, 0xbe, 0xb5, 0x00, 0x00, 0x00,
 }
 
 func (m *GenesisState) Marshal() (dAtA []byte, err error) {
@@ -120,33 +100,6 @@ func (m *GenesisState) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	{
-		size := m.DonationValue.Size()
-		i -= size
-		if _, err := m.DonationValue.MarshalTo(dAtA[i:]); err != nil {
-			return 0, err
-		}
-		i = encodeVarintGenesis(dAtA, i, uint64(size))
-	}
-	i--
-	dAtA[i] = 0x1a
-	{
-		size := m.BootstrapValue.Size()
-		i -= size
-		if _, err := m.BootstrapValue.MarshalTo(dAtA[i:]); err != nil {
-			return 0, err
-		}
-		i = encodeVarintGenesis(dAtA, i, uint64(size))
-	}
-	i--
-	dAtA[i] = 0x12
-	if len(m.BootstrapAddress) > 0 {
-		i -= len(m.BootstrapAddress)
-		copy(dAtA[i:], m.BootstrapAddress)
-		i = encodeVarintGenesis(dAtA, i, uint64(len(m.BootstrapAddress)))
-		i--
-		dAtA[i] = 0xa
-	}
 	return len(dAtA) - i, nil
 }
 
@@ -167,14 +120,6 @@ func (m *GenesisState) Size() (n int) {
 	}
 	var l int
 	_ = l
-	l = len(m.BootstrapAddress)
-	if l > 0 {
-		n += 1 + l + sovGenesis(uint64(l))
-	}
-	l = m.BootstrapValue.Size()
-	n += 1 + l + sovGenesis(uint64(l))
-	l = m.DonationValue.Size()
-	n += 1 + l + sovGenesis(uint64(l))
 	return n
 }
 
@@ -213,106 +158,6 @@ func (m *GenesisState) Unmarshal(dAtA []byte) error {
 			return fmt.Errorf("proto: GenesisState: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
-		case 1:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field BootstrapAddress", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowGenesis
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.BootstrapAddress = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field BootstrapValue", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowGenesis
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			if err := m.BootstrapValue.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
-		case 3:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field DonationValue", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowGenesis
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			if err := m.DonationValue.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipGenesis(dAtA[iNdEx:])
