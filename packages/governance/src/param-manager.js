a52f4c91cd27985da5e0e25c887bb9d5860ee388
Chris Hibbert <hibbert@agoric.com>
2021-06-09 17:10:54 -0700

    chore: add types for handle and any.

--- packages/governance/src/param-manager.js
+++ packages/governance/src/param-manager.js
@@ -7,12 +7,14 @@ import { assertIsRatio } from '@agoric/zoe/src/contractSupport';
 import { AmountMath, looksLikeBrand } from '@agoric/ertp';
 
 const ParamType = {
-  NAT: 'nat',
-  BIGINT: 'bigint',
-  STRING: 'string',
-  RATIO: 'ratio',
   AMOUNT: 'amount',
+  ANY: 'any',
+  BIGINT: 'bigint',
   BRAND: 'brand',
+  HANDLE: 'handle',
+  NAT: 'nat',
+  RATIO: 'ratio',
+  STRING: 'string',
 };
 harden(ParamType);
 
@@ -23,18 +25,6 @@ const assertType = (type, value, name) => {
   }
 
   switch (type) {
-    case ParamType.NAT:
-      Nat(value);
-      break;
-    case ParamType.BIGINT:
-      assert.typeof(value, 'bigint');
-      break;
-    case ParamType.STRING:
-      assert.typeof(value, 'string');
-      break;
-    case ParamType.RATIO:
-      assertIsRatio(value);
-      break;
     case ParamType.AMOUNT:
       // TODO(hibbert): is there a clean way to assert something is an amount?
       // An alternate approach would be to say the contract knows the brand and
@@ -44,11 +34,31 @@ const assertType = (type, value, name) => {
         X`value for ${name} must be an Amount, was ${value}`,
       );
       break;
+    case ParamType.ANY:
+      break;
+    case ParamType.BIGINT:
+      assert.typeof(value, 'bigint');
+      break;
     case ParamType.BRAND:
       assert(
         looksLikeBrand(value),
         X`value for ${name} must be a brand, was ${value}`,
       );
+      break;
+    case ParamType.HANDLE:
+      assert(
+        typeof value === 'object' && !Object.getOwnPropertyNames(value).length,
+        X`value for ${name} must be an empty object, was ${value}`,
+      );
+      break;
+    case ParamType.NAT:
+      Nat(value);
+      break;
+    case ParamType.RATIO:
+      assertIsRatio(value);
+      break;
+    case ParamType.STRING:
+      assert.typeof(value, 'string');
       break;
     default:
       assert.fail(X`unknown type guard ${type}`);
@@ -60,7 +70,6 @@ const parse = paramDesc => {
   const types = makeStore('name');
 
   paramDesc.forEach(({ name, value, type }) => {
-    assert.typeof(name, 'string');
     assertType(type, value, name);
     bindings.init(name, value);
     types.init(name, type);
