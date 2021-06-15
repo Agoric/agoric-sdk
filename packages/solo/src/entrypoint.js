131ae64188a55b53110ae0590ff5a0f82674da5f
Michael FIG <mfig@agoric.com>
2021-06-15 14:25:29 -0600

    refactor: separate entrypoint.cjs from entrypoint.js

---
+++ packages/solo/src/entrypoint.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+// import node-lmdb early to work around SES incompatibility
+import 'node-lmdb';
+
+// TODO Remove babel-standalone preinitialization
+// https://github.com/endojs/endo/issues/768
+import '@agoric/babel-standalone';
+
+// we need to enable Math.random as a workaround for 'brace-expansion' module
+// (dep chain: temp->glob->minimatch->brace-expansion)
+import '@agoric/install-metering-and-ses';
+
+import process from 'process';
+import path from 'path';
+
+// Configure logs.
+import '@agoric/cosmic-swingset/src/anylogger-agoric.js';
+import solo from './main.js';
+
+const baseprog = path.basename(process.argv[1]);
+solo(baseprog, process.argv.slice(2)).then(
+  res => process.exit(res || 0),
+  reason => {
+    console.log(`error running ag-solo:`, reason);
+    console.error(`\
+Maybe the chain has bumped and you need to rerun ag-setup-solo?`);
+    process.exit(1);
+  },
+);
