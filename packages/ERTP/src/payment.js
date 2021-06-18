8d26e8cab91f4299881951798883d31b61299fbb
Kate Sills <kate@agoric.com>
2021-06-18 11:40:59 -0700

    chore: payments are not virtual objects. payments are remotables

--- packages/ERTP/src/payment.js
+++ packages/ERTP/src/payment.js
@@ -1,24 +1,14 @@
 // @ts-check
-/* global makeKind */
 
 import { Far } from '@agoric/marshal';
 
 /**
  * @param {string} allegedName
  * @param {Brand} brand
- * @returns {() => Payment}
+ * @returns {Payment}
  */
-export const makePaymentMaker = (allegedName, brand) => {
-  const paymentVOFactory = () => {
-    return {
-      init: () => {},
-      self: Far(`${allegedName} payment`, {
-        getAllegedBrand: () => brand,
-      }),
-    };
-  };
-
-  const makePayment = makeKind(paymentVOFactory);
-
-  return makePayment;
+export const makePayment = (allegedName, brand) => {
+  return Far(`${allegedName} payment`, {
+    getAllegedBrand: () => brand,
+  });
 };
