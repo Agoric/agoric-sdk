f51d3270a8a59b4a5fdcac029f6f752fbad3ad59
Kate Sills <kate@agoric.com>
2021-06-18 10:59:05 -0700

    refactor(zoe)!: handle subtracting empty in a less fragile way (#3345)
    * chore: handle subtracting empty in a less fragile way

--- packages/zoe/src/contracts/newSwap/collectFees.js
+++ packages/zoe/src/contracts/newSwap/collectFees.js
@@ -1,13 +1,8 @@
 // @ts-check
 
-import { AmountMath } from '@agoric/ertp';
-
 export const makeMakeCollectFeesInvitation = (zcf, feeSeat, centralBrand) => {
   const collectFees = seat => {
-    // Ensure that the feeSeat has a stagedAllocation with a RUN keyword
-    feeSeat.incrementBy({ RUN: AmountMath.makeEmpty(centralBrand) });
     const amount = feeSeat.getAmountAllocated('RUN', centralBrand);
-
     seat.incrementBy(feeSeat.decrementBy({ RUN: amount }));
     zcf.reallocate(seat, feeSeat);
 
