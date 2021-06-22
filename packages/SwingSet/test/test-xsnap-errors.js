c27dc5c9da5d970de9b7e7250c9a10735e1e5934
Dan Connolly <connolly@agoric.com>
2021-06-22 13:18:51 -0500

    chore: annotate types in test-xsnap-errors.js
    ... sufficiently to detect `m.deliver is not a function` statically.

--- packages/SwingSet/test/test-xsnap-errors.js
+++ packages/SwingSet/test/test-xsnap-errors.js
@@ -1,4 +1,5 @@
 /* global require */
+// @ts-check
 // eslint-disable-next-line import/order
 import { test } from '../tools/prepare-test-env-ava.js';
 import path from 'path';
@@ -19,11 +20,15 @@ test('child termination during crank', async t => {
   );
   const bundles = [lockdown, supervisor];
   const snapstorePath = undefined; // good enough?
+  /** @type { Record<string, string> } */
   const env = {};
 
   const startXSnap = makeStartXSnap(bundles, { snapstorePath, env, spawn });
   const kernelKeeper = {}; // add just enough methods to not crash
+  /** @type { KernelKeeper } */
+  /** @type { KernelSlog } */
   const kernelSlog = {}; // same
+  /** @type { VatPowers } */
   const allVatPowers = {}; // probably safe to leave empty
   const xsWorkerFactory = makeXsSubprocessFactory({
     startXSnap,
@@ -36,11 +41,13 @@ test('child termination during crank', async t => {
   const vatID = 'v1';
   const fn = path.join(__dirname, 'vat-xsnap-hang.js');
   const bundle = await bundleSource(fn);
+  /** @type { ManagerOptions } */
   const managerOptions = {};
   const schandler = _vso => ['ok', null];
   const m = xsWorkerFactory.createFromBundle('v1', bundle, {}, schandler);
 
   const msg = { method: 'hang', args: capargs([]) };
+  /** @type { VatDeliveryObject } */
   const delivery = ['message', 'o+0', msg];
 
   // TODO: disable metering limit
