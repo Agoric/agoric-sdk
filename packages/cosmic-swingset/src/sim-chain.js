e16e7a77910e5c0af647be30cdaa360ce7bff0f8
Michael FIG <mfig@agoric.com>
2021-07-06 16:05:06 -0600

    fix(cosmic-swingset): use default batching parameters for sim-chain

--- packages/cosmic-swingset/src/sim-chain.js
+++ packages/cosmic-swingset/src/sim-chain.js
@@ -182,8 +182,6 @@ export async function connectToFakeChain(basedir, GCI, delay, inbound) {
   // Start the first pretend block.
   nextBlockTimeout = setTimeout(simulateBlock, maximumDelay);
 
-  // Only use 100ms batches for no specified inter-block delay.
-  // This makes for more deliveries but less overall waiting time.
-  const batchDelayMs = delay ? delay * 1000 : 100;
+  const batchDelayMs = delay ? delay * 1000 : undefined;
   return makeBatchedDeliver(deliver, batchDelayMs);
 }
