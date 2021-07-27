fd5bfe3c6ccaa098d82835309b813977ee251151
Kris Kowal <kris@agoric.com>
2021-07-26 17:56:22 -0700

    refactor(notifier): Convert RESM to NESM

---
+++ packages/notifier/test/prepare-test-env-ava.js
@@ -0,0 +1,10 @@
+// @ts-check
+import '@agoric/eventual-send/shim.js';
+import 'ses';
+import './lockdown.js';
+
+import { wrapTest } from '@endo/ses-ava';
+import rawTest from 'ava';
+
+/** @type {typeof rawTest} */
+export const test = wrapTest(rawTest);
