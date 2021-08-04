e48f2861d733430ab8b51e1a92aa3967137b7956
Mark S. Miller <erights@users.noreply.github.com>
2021-08-03 19:12:46 -0700

    Modularize passStyleOf (#3571)
    * fix: modularize passStyleOf

--- packages/same-structure/index.js
+++ packages/same-structure/index.js
@@ -1,7 +1,15 @@
+// This entire package is now deprecated, with its code migrated
+// to `@agoric/marshal` with the exports renamed to their modern
+// names.
+//
+// This package remains for now in order to
+// re-export the new names from `@agoric/marshal` under the old
+// deprecated names that this package used to export. Please update
+// uses to the new names as imported from `@agoric/marshal`.
+
 export {
-  sameValueZero,
-  allComparable,
   sameStructure,
-  mustBeSameStructure,
-  mustBeComparable,
-} from './src/sameStructure.js';
+  isStructure as isComparable, // deprecated
+  assertStructure as mustBeComparable, // deprecated
+  fulfillToStructure as allComparable, // deprecated
+} from '@agoric/marshal';
