f83b27be38c7e8270aa7a2d9ac24951a7da0cdb0
Kris Kowal <kris@agoric.com>
2021-08-09 17:07:49 -0700

    refactor(import-manager): Convert RESM to NESM

--- packages/import-manager/test/unitTests/goodImports.js
+++ packages/import-manager/test/unitTests/goodImports.js
@@ -1,7 +1,7 @@
 // Copyright (C) 2019 Agoric, under Apache License 2.0
 
-import { numIsEmpty, listIsEmpty } from './valueOps';
-import { importManager } from '../../src/importManager';
+import { numIsEmpty, listIsEmpty } from './valueOps.js';
+import { importManager } from '../../src/importManager.js';
 
 // This file models the way a module could export functionality to be run
 // locally within a vat, but with the behavior determined by remote objects.
