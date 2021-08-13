dc8d6dca5898890ef4d956c83685bc28eb189791
Chris Hibbert <hibbert@agoric.com>
2021-08-13 11:55:43 -0700

    feat!: add required rounding modes to ratio APIs
    deprecated multiply and divide operations that didn't specify rounding
    added more consistent type declarations.

--- packages/zoe/src/contractSupport/ratio.js
+++ packages/zoe/src/contractSupport/ratio.js
@@ -3,16 +3,17 @@
 import './types.js';
 import { assert, details as X, q } from '@agoric/assert';
 import { Nat } from '@agoric/nat';
+import { AmountMath } from '@agoric/ertp';
 import { natSafeMath } from './safeMath.js';
 
-const { multiply, floorDivide, add, subtract } = natSafeMath;
+const { multiply, floorDivide, ceilDivide, add, subtract } = natSafeMath;
 
 // make a Ratio, which represents a fraction. It is a pass-by-copy record.
 //
 // The natural syntax for the most common operations we want to support
 // are Amount * Ratio and Amount / Ratio. Since the operations want to adhere to
 // the ratio rather than the amount, we settled on a calling convention of
-// multiplyBy(Amount, Ratio) and divideBy(Amount, Ratio).
+// [ceil|floor]MultiplyBy(Amount, Ratio) and [ceil|floor]DivideBy(Amount, Ratio)
 //
 // The most common kind of Ratio can be applied to Amounts of a particular
 // brand, and produces results of the same brand. This represents a multiplier
@@ -22,6 +23,15 @@ const { multiply, floorDivide, add, subtract } = natSafeMath;
 // brand-checking helps us ensure that normal Ratios aren't applied to amounts
 // of the wrong brand, and that exchange rates are only used in the appropriate
 // direction.
+//
+// Since the ratios are represented by a numerator and a denominator, every
+// multiplication or division operation that produces an amount ends with a
+// division of the underlying bigIntegers, and bigInteger division requires that
+// the rounding mode (round up or round down) be specified. It would be a
+// mistake to hide this distinction from the caller, so we make it very visible
+// by using floorMultiplyBy, ceilMultiplyBy, floorDivideBy, and ceilDivideBy.
+// The operations without floor/ceil are still present, but won't be kept around
+// for long.
 
 const PERCENT = 100n;
 
@@ -43,13 +53,7 @@ export const assertIsRatio = ratio => {
   Nat(ratio.denominator.value);
 };
 
-/**
- * @param {bigint | number} numerator
- * @param {Brand} numeratorBrand
- * @param {bigint | number} denominator
- * @param {Brand} denominatorBrand
- * @returns {Ratio}
- */
+/** @type {MakeRatio} */
 export const makeRatio = (
   numerator,
   numeratorBrand,
@@ -61,32 +65,28 @@ export const makeRatio = (
     X`No infinite ratios! Denominator was 0/${q(denominatorBrand)}`,
   );
 
-  // TODO(https://github.com/Agoric/agoric-sdk/pull/2310) after the refactoring
-  //  use AmountMath's constructor here rather than building the record directly
   return harden({
-    numerator: { value: Nat(numerator), brand: numeratorBrand },
-    denominator: { value: Nat(denominator), brand: denominatorBrand },
+    numerator: AmountMath.make(numeratorBrand, Nat(numerator)),
+    denominator: AmountMath.make(denominatorBrand, Nat(denominator)),
   });
 };
 
+/** @type {MakeRatioFromAmounts} */
 export const makeRatioFromAmounts = (numeratorAmount, denominatorAmount) => {
-  // TODO(https://github.com/Agoric/agoric-sdk/pull/2310) after the refactoring
-  // coerce amounts using a native AmountMath operation.
-
+  AmountMath.coerce(numeratorAmount, numeratorAmount.brand);
+  AmountMath.coerce(denominatorAmount, denominatorAmount.brand);
   return makeRatio(
+    // @ts-ignore coerce ensures values are Nats
     Nat(numeratorAmount.value),
     numeratorAmount.brand,
+    // @ts-ignore coerce ensures values are Nats
     Nat(denominatorAmount.value),
     denominatorAmount.brand,
   );
 };
 
-export const multiplyBy = (amount, ratio) => {
-  // TODO(https://github.com/Agoric/agoric-sdk/pull/2310) after the refactoring
-  // coerce amount using a native AmountMath operation.
-  assert(amount.brand, X`Expected an amount: ${amount}`);
-  Nat(amount.value);
-
+const multiplyHelper = (amount, ratio, divideOp) => {
+  AmountMath.coerce(amount, amount.brand);
   assertIsRatio(ratio);
   assert(
     amount.brand === ratio.denominator.brand,
@@ -95,23 +95,35 @@ export const multiplyBy = (amount, ratio) => {
     )}`,
   );
 
-  // TODO(https://github.com/Agoric/agoric-sdk/pull/2310) after the refactoring
-  //  use AmountMath's constructor here rather than building the record directly
-  return harden({
-    value: floorDivide(
+  return AmountMath.make(
+    ratio.numerator.brand,
+    divideOp(
       multiply(amount.value, ratio.numerator.value),
       ratio.denominator.value,
     ),
-    brand: ratio.numerator.brand,
-  });
+  );
 };
 
-export const divideBy = (amount, ratio) => {
-  // TODO(https://github.com/Agoric/agoric-sdk/pull/2310) after the refactoring
-  // coerce amount using a native AmountMath operation.
-  assert(amount.brand, X`Expected an amount: ${amount}`);
-  Nat(amount.value);
+/** @type {FloorMultiplyBy} */
+export const floorMultiplyBy = (amount, ratio) => {
+  return multiplyHelper(amount, ratio, floorDivide);
+};
 
+/** @type {CeilMultiplyBy} */
+export const ceilMultiplyBy = (amount, ratio) => {
+  return multiplyHelper(amount, ratio, ceilDivide);
+};
+
+/**
+ * @deprecated use floorMultiplyBy() or ceilMultiplyBy()
+ * @type {MultiplyBy}
+ */
+export const multiplyBy = (amount, ratio) => {
+  return floorMultiplyBy(amount, ratio);
+};
+
+const divideHelper = (amount, ratio, divideOp) => {
+  AmountMath.coerce(amount, amount.brand);
   assertIsRatio(ratio);
   assert(
     amount.brand === ratio.numerator.brand,
@@ -120,28 +132,48 @@ export const divideBy = (amount, ratio) => {
     )}`,
   );
 
-  // TODO(https://github.com/Agoric/agoric-sdk/pull/2310) after the refactoring
-  //  use AmountMath's constructor here rather than building the record directly
-  return harden({
-    value: floorDivide(
+  return AmountMath.make(
+    divideOp(
       multiply(amount.value, ratio.denominator.value),
       ratio.numerator.value,
     ),
-    brand: ratio.denominator.brand,
-  });
+    ratio.denominator.brand,
+  );
 };
 
+/** @type {FloorDivideBy} */
+export const floorDivideBy = (amount, ratio) => {
+  return divideHelper(amount, ratio, floorDivide);
+};
+
+/** @type {CeilDivideBy} */
+export const ceilDivideBy = (amount, ratio) => {
+  return divideHelper(amount, ratio, ceilDivide);
+};
+
+/**
+ * @deprecated use floorDivideBy() or ceilDivideBy()
+ * @type {DivideBy}
+ */
+export const divideBy = (amount, ratio) => {
+  return floorDivideBy(amount, ratio);
+};
+
+/** @type {InvertRatio} */
 export const invertRatio = ratio => {
   assertIsRatio(ratio);
 
   return makeRatio(
+    // @ts-ignore assertIsRatio ensures values are Nats
     ratio.denominator.value,
     ratio.denominator.brand,
+    // @ts-ignore assertIsRatio ensures values are Nats
     ratio.numerator.value,
     ratio.numerator.brand,
   );
 };
 
+/** @type {AddRatios} */
 export const addRatios = (left, right) => {
   assertIsRatio(right);
   assertIsRatio(left);
@@ -162,6 +194,7 @@ export const addRatios = (left, right) => {
   );
 };
 
+/** @type {MultiplyRatios} */
 export const multiplyRatios = (left, right) => {
   assertIsRatio(right);
   assertIsRatio(left);
@@ -180,6 +213,7 @@ export const multiplyRatios = (left, right) => {
 };
 
 // If ratio is between 0 and 1, subtract from 1.
+/** @type {OneMinus} */
 export const oneMinus = ratio => {
   assertIsRatio(ratio);
   assert(
@@ -193,6 +227,7 @@ export const oneMinus = ratio => {
   return makeRatio(
     subtract(ratio.denominator.value, ratio.numerator.value),
     ratio.numerator.brand,
+    // @ts-ignore asserts ensure values are Nats
     ratio.denominator.value,
     ratio.numerator.brand,
   );
