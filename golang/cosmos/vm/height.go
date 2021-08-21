c4e472748b4eed4f7ad9650a5904a526e0c2e214
Michael FIG <mfig@agoric.com>
2021-08-21 16:29:56 -0600

    fix(cosmos): the bootstrap block is not a simulation, either

--- golang/cosmos/vm/height.go
+++ golang/cosmos/vm/height.go
@@ -8,6 +8,11 @@ var committedHeight int64 = 0
 
 // IsSimulation tells if we are simulating the transaction
 func IsSimulation(ctx sdk.Context) bool {
+	// If we haven't committed yet, we're still in bootstrap.
+	if committedHeight == 0 {
+		return false
+	}
+	// Otherwise, if we already committed this block, we're in simulation.
 	return committedHeight == ctx.BlockHeight()
 }
 
