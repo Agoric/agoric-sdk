1cecdcb7dd571146164aba1e8eda0b6ad91a975a
Michael FIG <mfig@agoric.com>
2021-09-09 18:24:00 -0600

    feat(solo): add `keys`, `init`, and `delete` to `home.scratch`

--- packages/solo/src/scratch.js
+++ packages/solo/src/scratch.js
@@ -2,27 +2,38 @@ import { Far } from '@agoric/marshal';
 
 export default function makeScratchPad() {
   const map = new Map();
+
+  const keys = async () => {
+    const keyList = [...map.keys()];
+    return harden(keyList.sort());
+  };
+
   return Far('scratchPad', {
-    get: async idP => {
-      const id = await idP;
-      return map.get(id);
+    delete: async keyP => {
+      const key = await keyP;
+      map.delete(key);
     },
-    set: async (idP, objP) => {
-      const [id, obj] = await Promise.all([idP, objP]);
-      map.set(id, obj);
-      return id;
+    get: async keyP => {
+      const key = await keyP;
+      return map.get(key);
     },
-    init: async (idP, objP) => {
-      const [id, obj] = await Promise.all([idP, objP]);
-      if (map.has(id)) {
-        throw Error(`Scratchpad already has id ${id}`);
+    // Initialize a key only if it doesn't already exist.  Needed for atomicity
+    // between multiple invocations.
+    init: async (keyP, objP) => {
+      const [key, obj] = await Promise.all([keyP, objP]);
+      if (map.has(key)) {
+        throw Error(`Scratchpad already has key ${key}`);
       }
-      map.set(id, obj);
-      return id;
+      map.set(key, obj);
+      return key;
     },
-    list: async () => {
-      const ids = [...map.keys()];
-      return harden(ids.sort());
+    keys,
+    // Legacy alias for `keys`.
+    list: keys,
+    set: async (keyP, objP) => {
+      const [key, obj] = await Promise.all([keyP, objP]);
+      map.set(key, obj);
+      return key;
     },
   });
 }
