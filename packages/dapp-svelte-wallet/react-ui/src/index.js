f66cc56f2009331d845b6fe998f6834d7175636f
samsiegart <samsiegart@gmail.com>
2021-09-24 11:31:59 -0700

    refactor(wallet): Streamline lockdown process using consoleTaming param

--- packages/dapp-svelte-wallet/react-ui/src/index.js
+++ packages/dapp-svelte-wallet/react-ui/src/index.js
@@ -1,8 +1,9 @@
-import './install-ses-lockdown.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App.js';
+
+Error.stackTraceLimit = Infinity;
 
 ReactDOM.render(<App />, document.getElementById('root'));
