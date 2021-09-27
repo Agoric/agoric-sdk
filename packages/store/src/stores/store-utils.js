d45c721508eb47ad53f7b7e86765ee6872d6beeb
Mark S. Miller <erights@gmail.com>
2021-09-26 17:52:52 -0700

    fix: kill cursor when stale

--- packages/store/src/stores/store-utils.js
+++ packages/store/src/stores/store-utils.js
@@ -1,6 +1,6 @@
 // @ts-check
 
-import { compareRank, Far, filterIterable } from '@agoric/marshal';
+import { compareRank, filterIterable } from '@agoric/marshal';
 import { matches } from '../patterns/patternMatchers.js';
 
 const { details: X, quote: q } = assert;
@@ -28,6 +28,12 @@ export const makeCursorKit = (
         },
 
     makeArray: (baseIterable, pattern = undefined) => {
+      const filter = pattern ? val => matches(harden(val), pattern) : harden;
+      const filtered = filterIterable(baseIterable, filter);
+      return harden([...filtered].sort(compareRank));
+    },
+
+    makeCursor: (baseIterable, pattern = undefined) => {
       const currentUpdateCount = updateCount;
       const notStaleFilter = () => {
         assert.equal(
@@ -37,27 +43,21 @@ export const makeCursorKit = (
         );
         return true;
       };
-      const filter = pattern
-        ? val => notStaleFilter() && matches(val, pattern)
-        : notStaleFilter;
 
       // TODO In an implementation where the baseIterable returns its data
-      // already rank sorted, `filtered` would be returned by `makeCursor`
-      // as the cursor and makeArray would be a snapshot of that. However,
+      // already rank sorted, `makeCursor` would use the following
+      // code to make a cursor, and makeArray would be a snapshot of that.
+      // However,
       // to get the correct external behavior on non-ordered representation,
-      // we sort here and then return a cursor built from it.
-      const filtered = filterIterable(baseIterable, filter);
-      const compareHardened = (x, y) => compareRank(harden(x), harden(y));
-      return harden([...filtered].sort(compareHardened));
-    },
+      // we sort in makeArray instead and then makeCursor return a cursor built
+      // from that.
+      // const filter = pattern
+      //   ? val => notStaleFilter() && matches(val, pattern)
+      //   : notStaleFilter;
+      // return filterIterable(baseIterable, filter);
 
-    makeCursor: (baseIterable, pattern = undefined) => {
       const sorted = cursorKit.makeArray(baseIterable, pattern);
-      return Far('cursor iterable', {
-        [Symbol.iterator]() {
-          return sorted[Symbol.iterator]();
-        },
-      });
+      return filterIterable(sorted, notStaleFilter);
     },
   });
   return cursorKit;
