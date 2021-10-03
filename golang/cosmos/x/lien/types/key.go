50a881be4971cd5c006867daca66eb6138276492
Jim Larson <jim@agoric.com>
2021-10-02 20:08:35 -0700

    fix: don't use manual key prefix

--- golang/cosmos/x/lien/types/key.go
+++ golang/cosmos/x/lien/types/key.go
@@ -10,20 +10,16 @@ const (
 	StoreKey = ModuleName
 )
 
-var (
-	LienByAddressPrefix = []byte{0x01}
-)
-
-// The lien-by-address space maps prefixed addresses to Lien messages.
-// The keys are raw address bytes with the appropriate prefix.
+// The lien-by-address space maps addresses to Lien messages.
+// The keys are raw address bytes.
 // THe data are protobuf-encoded Lien messages.
 
 // LienByAddressKey returns the lien lookup key for the addr.
 func LienByAddressKey(addr sdk.AccAddress) []byte {
-	return append(LienByAddressPrefix, addr.Bytes()...)
+	return addr.Bytes()
 }
 
 // LienByAddressDecodeKey returns the address for the lien lookup key.
 func LienByAddressDecodeKey(key []byte) sdk.AccAddress {
-	return sdk.AccAddress(key[len(LienByAddressPrefix):])
+	return sdk.AccAddress(key)
 }
