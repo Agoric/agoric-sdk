724d629184f4fd82b8ad95af7bb6c028f5c14ef0
Chris Hibbert <Chris-Hibbert@users.noreply.github.com>
2021-10-04 15:29:41 -0700

    docs: regenerate governance diagrams (#3927)
    * docs: regenerate governance diagrams
    
    Following #3888, add a shell script to generate the diagrams.
    Rename the files for better consistency.
    Check in new .pngs, since the .pumls were recently updated.
    
    * chore: add a final newline

---
+++ packages/governance/docs/gen.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+pumljar="${PLANTUML:-$HOME/plantuml/plantuml.jar}"
+page="index.md"
+java -jar "$pumljar" "./**/*.puml"
+echo "# Governance Diagrams" > "$page"
+for diag in $(find . -name "*.png" | sort); do
+    echo "## $diag\n" >> "$page"
+    echo "[![$diag]($diag)]($diag)\n" >> "$page"
+done
