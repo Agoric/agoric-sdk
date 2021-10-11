18807afea158f64eb0241b89cdafde3ec1847f4a
Samuel Siegart <samsiegart@gmail.com>
2021-10-11 11:22:19 -0700

    feat(react-wallet): Create app layout with left nav and app bar (#3941)
    * feat(react-wallet): Create app layout with left nav and app bar
    
    * feat(react-wallet): Fix font family quote usage

--- packages/dapp-svelte-wallet/react-ui/src/App.js
+++ packages/dapp-svelte-wallet/react-ui/src/App.js
@@ -1,22 +1,85 @@
 /* eslint-disable react/display-name */
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core';
+import {
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from '@material-ui/core/styles';
 
-import WalletConnection from './components/WalletConnection';
+import AppBar from './components/AppBar';
+import NavMenu from './components/NavMenu';
+import Contacts from './views/Contacts.js';
+import Dapps from './views/Dapps.js';
+import Dashboard from './views/Dashboard.js';
+import Purses from './views/Purses.js';
+import Issuers from './views/Issuers.js';
 
-import './App.css';
-import { withApplicationContext } from './contexts/Application';
+const appBarHeight = '64px';
+const navMenuWidth = '240px';
 
-const App = ({ connectionState }) => {
+const appTheme = createTheme({
+  palette: {
+    background: {
+      default: '#ffffff',
+    },
+  },
+  typography: {
+    fontFamily: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'].join(','),
+    fontWeightRegular: 500,
+    h1: {
+      fontFamily: ['Montserrat', 'Arial', 'sans-serif'].join(','),
+      fontSize: '32px',
+      fontWeight: '700',
+      letterSpacing: '-1.5px',
+      lineHeight: '48px',
+      margin: 0,
+    },
+  },
+  appBarHeight,
+  navMenuWidth,
+});
+
+const useStyles = makeStyles(_ => ({
+  main: {
+    boxSizing: 'border-box',
+    padding: '32px',
+    marginLeft: navMenuWidth,
+    position: 'absolute',
+    width: `calc(100vw - ${navMenuWidth})`,
+    top: appBarHeight,
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        Connection Status: {connectionState}
-      </header>
-      <WalletConnection></WalletConnection>
-    </div>
+    <ThemeProvider theme={appTheme}>
+      <CssBaseline />
+      <NavMenu />
+      <main className={classes.main}>
+        <Switch>
+          <Route path="/purses">
+            <Purses />
+          </Route>
+          <Route path="/dapps">
+            <Dapps />
+          </Route>
+          <Route path="/contacts">
+            <Contacts />
+          </Route>
+          <Route path="/issuers">
+            <Issuers />
+          </Route>
+          <Route path="/">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </main>
+      <AppBar />
+    </ThemeProvider>
   );
 };
 
-export default withApplicationContext(App, context => ({
-  connectionState: context.connectionState,
-}));
+export default App;
---
+++ packages/dapp-svelte-wallet/react-ui/src/views/Purses.js
@@ -0,0 +1,7 @@
+import { Typography } from '@material-ui/core';
+
+const Purses = () => {
+  return <Typography variant="h1">Purses</Typography>;
+};
+
+export default Purses;
