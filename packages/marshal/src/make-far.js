464bcc562aec18a4f4723d37c519c83e6150a915
Mark S. Miller <erights@gmail.com>
2021-10-14 01:24:01 -0700

    fix: not coherent yet

--- packages/marshal/src/make-far.js
+++ packages/marshal/src/make-far.js
@@ -88,25 +88,48 @@ export const pureCopy = val => {
 };
 harden(pureCopy);
 
+const assertCanBeRemotable = candidate =>
+  RemotableHelper.canBeValid(candidate, assertChecker);
+
+/**
+ * The tag record that remotables inherit from.
+ *
+ * @param {Object} oldProto
+ * The object that the new tag record should inherit from. This will typically
+ * be the object that the `remotable` argument inherited from before it was
+ * rewired to inherit from this tag record. The oldProto should be something
+ * with a `toString` method that can be inherited. In fact, it will always
+ * be either `Object.prototype` or one of `*Function.prototype`.
+ * @param {InterfaceSpec} iface
+ * @returns {Object}
+ */
+const makeRemotableTagRecord = (oldProto, iface) =>
+  harden(
+    create(oldProto, {
+      [PASS_STYLE]: { value: 'remotable' },
+      [Symbol.toStringTag]: { value: iface },
+    }),
+  );
+
 /**
  * Now that the remotableProto does not provide its own `toString` method,
  * ensure it always inherits from something. The original prototype of
  * `remotable` if there was one, or `Object.prototype` otherwise.
  *
  * @param {Object} remotable
- * @param {InterfaceSpec} iface
  * @returns {Object}
  */
-const makeRemotableProto = (remotable, iface) => {
+const getRemotableOldProto = remotable => {
   let oldProto = getPrototypeOf(remotable);
   if (typeof remotable === 'object') {
     if (oldProto === null) {
       oldProto = objectPrototype;
     }
     assert(
-      oldProto === objectPrototype || oldProto === null,
+      oldProto === objectPrototype,
       X`For now, remotables cannot inherit from anything unusual, in ${remotable}`,
     );
+    return oldProto;
   } else if (typeof remotable === 'function') {
     assert(
       oldProto !== null,
@@ -117,19 +140,83 @@ const makeRemotableProto = (remotable, iface) => {
         getPrototypeOf(oldProto) === functionPrototype,
       X`Far functions must originally inherit from Function.prototype, in ${remotable}`,
     );
+    return oldProto;
   } else {
     assert.fail(X`unrecognized typeof ${remotable}`);
   }
-  return harden(
-    create(oldProto, {
-      [PASS_STYLE]: { value: 'remotable' },
-      [Symbol.toStringTag]: { value: iface },
-    }),
-  );
 };
 
-const assertCanBeRemotable = candidate =>
-  RemotableHelper.canBeValid(candidate, assertChecker);
+/**
+ * Create and register a Remotable.  After this, getInterfaceOf(remotable)
+ * returns iface.
+ *
+ * // https://github.com/Agoric/agoric-sdk/issues/804
+ *
+ * @param {InterfaceSpec} [iface='Remotable'] The interface specification for
+ * the remotable. For now, a string iface must be "Remotable" or begin with
+ * "Alleged: ", to serve as the alleged name. More general ifaces are not yet
+ * implemented. This is temporary. We include the
+ * "Alleged" as a reminder that we do not yet have SwingSet or Comms Vat
+ * support for ensuring this is according to the vat hosting the object.
+ * Currently, Alice can tell Bob about Carol, where VatA (on Alice's behalf)
+ * misrepresents Carol's `iface`. VatB and therefore Bob will then see
+ * Carol's `iface` as misrepresented by VatA.
+ * @param {undefined} [props=undefined] Currently may only be undefined.
+ * That plan is that own-properties are copied to the remotable
+ * @param {Object=} oldProto
+ * @returns {(remotable?: object) => object}
+ */
+export const makeRemotableMaker = (
+  iface = 'Remotable',
+  props = undefined,
+  oldProto = Object.prototype,
+) => {
+  assertIface(iface);
+  iface = pureCopy(harden(iface));
+  assert(iface);
+  // TODO: When iface is richer than just string, we need to get the
+  // allegedName in a different way.
+  assert(props === undefined, X`Remotable props not yet implemented ${props}`);
+
+  const remotableProto = makeRemotableTagRecord(oldProto, iface);
+
+  return (remotable = {}) => {
+    // Fail fast: check that the unmodified object is able to become a Remotable.
+    assertCanBeRemotable(remotable);
+
+    // Ensure that the remotable isn't already marked.
+    assert(
+      !(PASS_STYLE in remotable),
+      X`Remotable ${remotable} is already marked as a ${q(
+        remotable[PASS_STYLE],
+      )}`,
+    );
+    // Ensure that the remotable isn't already frozen.
+    assert(!isFrozen(remotable), X`Remotable ${remotable} is already frozen`);
+    assert.equal(oldProto, getRemotableOldProto(remotable));
+
+    // Take a static copy of the enumerable own properties as data properties.
+    // const propDescs = getOwnPropertyDescriptors({ ...props });
+    const mutateHardenAndCheck = target => {
+      // defineProperties(target, propDescs);
+      setPrototypeOf(target, remotableProto);
+      harden(target);
+      assertCanBeRemotable(target);
+    };
+
+    // Fail fast: check a fresh remotable to see if our rules fit.
+    mutateHardenAndCheck({});
+
+    // Actually finish the new remotable.
+    mutateHardenAndCheck(remotable);
+
+    // COMMITTED!
+    // We're committed, so keep the interface for future reference.
+    assert(iface !== undefined); // To make TypeScript happy
+    return remotable;
+  };
+};
+harden(makeRemotableMaker);
 
 /**
  * Create and register a Remotable.  After this, getInterfaceOf(remotable)
@@ -156,48 +243,24 @@ export const Remotable = (
   props = undefined,
   remotable = {},
 ) => {
-  assertIface(iface);
-  iface = pureCopy(harden(iface));
-  assert(iface);
-  // TODO: When iface is richer than just string, we need to get the allegedName
-  // in a different way.
-  assert(props === undefined, X`Remotable props not yet implemented ${props}`);
-
-  // Fail fast: check that the unmodified object is able to become a Remotable.
-  assertCanBeRemotable(remotable);
-
-  // Ensure that the remotable isn't already marked.
-  assert(
-    !(PASS_STYLE in remotable),
-    X`Remotable ${remotable} is already marked as a ${q(
-      remotable[PASS_STYLE],
-    )}`,
-  );
-  // Ensure that the remotable isn't already frozen.
-  assert(!isFrozen(remotable), X`Remotable ${remotable} is already frozen`);
-  const remotableProto = makeRemotableProto(remotable, iface);
-
-  // Take a static copy of the enumerable own properties as data properties.
-  // const propDescs = getOwnPropertyDescriptors({ ...props });
-  const mutateHardenAndCheck = target => {
-    // defineProperties(target, propDescs);
-    setPrototypeOf(target, remotableProto);
-    harden(target);
-    assertCanBeRemotable(target);
-  };
-
-  // Fail fast: check a fresh remotable to see if our rules fit.
-  mutateHardenAndCheck({});
-
-  // Actually finish the new remotable.
-  mutateHardenAndCheck(remotable);
-
-  // COMMITTED!
-  // We're committed, so keep the interface for future reference.
-  assert(iface !== undefined); // To make TypeScript happy
-  return remotable;
+  const oldProto = getRemotableOldProto(remotable);
+  const makeRemotable = makeRemotableMaker(iface, props, oldProto);
+  return makeRemotable(remotable);
 };
 harden(Remotable);
+
+/**
+ * A concise convenience for the most common `makeRemotableMaker` use.
+ *
+ * @template T
+ * @param {string} farName
+ * @param {Object=} oldProto
+ * @returns {(remotable?: T) => T}
+ */
+export const makeFarMaker = (farName, oldProto = Object.prototype) => {
+  return makeRemotableMaker(`Alleged: ${farName}`, undefined, oldProto);
+};
+harden(makeFarMaker);
 
 /**
  * A concise convenience for the most common `Remotable` use.
@@ -205,12 +268,13 @@ harden(Remotable);
  * @template T
  * @param {string} farName This name will be prepended with `Alleged: `
  * for now to form the `Remotable` `iface` argument.
- * @param {T|undefined} [remotable={}] The object used as the remotable
+ * @param {T|{}} [remotable={}] The object used as the remotable
  * @returns {T} remotable, modified for debuggability
  */
-export const Far = (farName, remotable = undefined) => {
-  const r = remotable === undefined ? {} : remotable;
-  return Remotable(`Alleged: ${farName}`, undefined, r);
+export const Far = (farName, remotable = {}) => {
+  const oldProto = getRemotableOldProto(remotable);
+  const makeFar = makeFarMaker(farName, oldProto);
+  return makeFar(remotable);
 };
 harden(Far);
 
