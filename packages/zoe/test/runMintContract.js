3968af9acc1e88a80519f69862d1d3b971be542d
Kate Sills <kate@agoric.com>
2021-11-08 10:17:52 -0800

    chore: add swingset tests of multiple run mints (#4034)
    * chore: add swingset tests of multiple run mints
    
    * chore: switch back to xs-worker
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ packages/zoe/test/runMintContract.js
@@ -0,0 +1,24 @@
+// @ts-check
+
+import { E } from '@agoric/eventual-send';
+import { Far } from '@agoric/marshal';
+import { AmountMath } from '@agoric/ertp';
+
+/** @type {ContractStartFn} */
+const start = async (zcf, privateArgs) => {
+  const runMint = await zcf.registerFeeMint('RUN', privateArgs.feeMintAccess);
+
+  const run10 = AmountMath.make(runMint.getIssuerRecord().brand, 10n);
+
+  const creatorFacet = Far('creatorFacet', {
+    mintRun: () => {
+      const { zcfSeat, userSeat } = zcf.makeEmptySeatKit();
+      runMint.mintGains({ RUN: run10 }, zcfSeat);
+      zcfSeat.exit();
+      return E(userSeat).getPayout('RUN');
+    },
+  });
+  return harden({ creatorFacet });
+};
+harden(start);
+export { start };
