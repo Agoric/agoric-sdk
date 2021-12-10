f46e7cdc4e42d9531ef17575622d249a60b7a253
Dan Connolly <connolly@agoric.com>
2021-12-10 17:19:21 -0600

    feat(getRUN): close, uiNotifier, invitation fee + expiry
     - replace getInvitation with makeLoanInvitation
       patterned after vault.js
       - HIGH_FEE, LONG_EXP
       - return uiNotifier, invitationMakers, vault
       - factor out makeLineOfCreditKit
         - add close OfferHandler
         - track debtAmount, vaultState
         - keep attestation in vaultSeat
       - stub adjustBalances
     - factor out makeCreditPolicy
     - document close()
     - refactor: push more down into startAttestation
    
    test(runLoC): run tests from spreadsheet
    
     - ava tests have to be created synchronously, so
       turn the CSV data into a string constant
     - fix parsing of empty leading fields in CSV
     - add types to CSV parsing
    
    test(runLoC): test close
    
      10 tests passed
      15 tests skipped
    
    hush some logging

--- packages/treasury/test/csvParse.js
+++ packages/treasury/test/csvParse.js
@@ -1,13 +1,17 @@
+// @ts-check
 // ack: https://stackoverflow.com/a/18147076/7963
-export const fieldPattern = /(?:,|\n|^)("(?:(?:"")*[^"]*)*"|[^",\n]*|(?:\n|$))/g;
+export const fieldPattern = /("(?:(?:"")*[^"]*)*"|[^",\n]*)(?:,|\n|$)/g;
 
+/** @param { string } field */
 export const unquote = field =>
   field
     .replace(/""/g, '"')
     .replace(/^"/, '')
     .replace(/"$/, '');
 
+/** @param { string } line */
 export const splitFields = line =>
   [...line.matchAll(fieldPattern)].map(([_all, txt]) => unquote(txt));
 
-export const csvParse = content => content.split('\n').map(splitFields);
+/** @param { string } content */
+export const parse = content => content.split('\n').map(splitFields);
