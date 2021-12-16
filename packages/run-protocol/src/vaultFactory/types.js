3ba9179a006ff0ef95ad2a0bc739ee81c66d43c1
Dan Connolly <connolly@agoric.com>
2021-12-15 18:31:36 -0600

    feat(getRUN): partial payoff
      12 tests passed
      13 tests skipped
    
     - clarify close() offer handler a bit
     - test: clarify todo filter
     - never mind attenuating runMint
     - never mind burnSeat
    
    feat(getRUN): vault-compatible naming
    
     - match invitationMakers labels
     - disregard address; attMaker handles it
    
    test(getRUN): refactor stake reporter as mockBridge
    
    feat(getRUN): only RUN balance can be adjusted, for now
    
     - clean up test logging a bit
     - style: organize imports
    
    test(getRUN): misc refactor / clean-up
    
     - move contractRoots near imports;
       move test.before() bundle loading up
       - subordinate collection utilities in Collect
       - factor out theBundles type handling
     - move contract start types near usage
       - break lines in long type annotations
     - mockBridge: rename uBrand (underlying?) to stakingBrand
    
    feat(getRUN): align types with vaults
    
     - makeLineOfCreditKit: move creditPolicy check inside
       in order to make documentation / design simpler.
       - document connection between debtAmount and minted RUN
       - use return value from creditPolicy.checkBorrow
         to clarify adjustBalances (up)
       - burn before updating debtAmount
       - never mind zeroRun
     - factor BaseUIState out of UIState;
       liened -> locked
       - vaultState doesn't seem to be part of UIState, so
         reduce it to a boolean
         - never mind assertVaultIsOpen; didn't carry its weight
       - makeAdjustBalancesInvitation on vault wasn't exposed; skip it
     - document CreditTerms
     - annotate types for minAmt()

--- packages/run-protocol/src/vaultFactory/types.js
+++ packages/run-protocol/src/vaultFactory/types.js
@@ -4,10 +4,6 @@
 /** @typedef {import('./vault').VaultKit} VaultKit */
 
 /**
- * @typedef {ReturnType<typeof import('./runLoC.js').makeLineOfCreditKit>} LineOfCreditKit
- */
-
-/**
  * @typedef  {Object} AutoswapLocal
  * @property {(amount: Amount, brand: Brand) => Amount} getInputPrice
  * @property {() => Invitation} makeSwapInvitation
