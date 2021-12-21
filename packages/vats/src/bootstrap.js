69ed64fabca297c5740052e69fdb7c0b87c700ab
Dan Connolly <connolly@agoric.com>
2021-12-21 16:55:10 -0600

    feat(vats): don't await vatAdminSvc
    I can't imagine any need for it to be local.

--- packages/vats/src/bootstrap.js
+++ packages/vats/src/bootstrap.js
@@ -1028,7 +1028,7 @@ export function buildRootObject(vatPowers, vatParameters) {
       D(devices.mailbox).registerInboundHandler(vats.vattp);
       await E(vats.vattp).registerMailboxDevice(devices.mailbox);
 
-      const vatAdminSvc = await E(vats.vatAdmin).createVatAdminService(
+      const vatAdminSvc = E(vats.vatAdmin).createVatAdminService(
         devices.vatAdmin,
       );
 
