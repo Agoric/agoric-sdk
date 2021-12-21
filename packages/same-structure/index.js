75f88e3548245233d23ed0faa7967b63e9bd459d
Mark S. Miller <erights@gmail.com>
2021-12-20 17:48:15 -0800

    fix: comments

--- packages/same-structure/index.js
+++ packages/same-structure/index.js
@@ -1,11 +1,12 @@
 // This entire package is now deprecated, with its code migrated
-// to `@agoric/store` with the exports renamed to their modern
-// names.
+// to `@agoric/marshal` and `@agoric/store` with the exports renamed to their
+// modern names.
 //
 // This package remains for now in order to
-// re-export the new names from `@agoric/store` under the old
-// deprecated names that this package used to export. Please update
-// uses to the new names as imported from `@agoric/marshal`.
+// re-export the new names from `@agoric/marshal` and `@agoric/store` under
+// the old deprecated names that this package used to export. Please update
+// uses to the new names as imported from `@agoric/marshal`
+// and`@agoric/store`.
 
 import { keyEQ, isKey, assertKey } from '@agoric/store';
 import { deeplyFulfilled } from '@agoric/marshal';
@@ -19,5 +20,5 @@ export const isComparable = isKey;
 /** @deprecated Use `assertKey` from `'@agoric/store'` */
 export const mustBeComparable = assertKey;
 
-/** @deprecated Use `deeplyFulfilled` from `'@agoric/store'` */
+/** @deprecated Use `deeplyFulfilled` from `'@agoric/marshal'` */
 export const allComparable = deeplyFulfilled;
