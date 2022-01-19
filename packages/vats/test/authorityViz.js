8bf8da9a97d5997b1e943b3b1e60b36864fffeb2
Dan Connolly <connolly@agoric.com>
2022-01-19 14:32:31 -0600

    feat: visualize vatPowers

--- packages/vats/test/authorityViz.js
+++ packages/vats/test/authorityViz.js
@@ -1,24 +1,55 @@
 // @ts-check
 /* global Buffer */
 
-// https://stackoverflow.com/a/54565854/7963
+const { entries } = Object;
+
+/**
+ * @param {Object} graph
+ * @param { Set<string> } graph.nodes
+ * @param { Array<{ src: string, dest: string}> } graph.edges
+ * @yields { string }
+ */
+function* fmtGraph({ nodes, edges }) {
+  yield 'digraph G {\n';
+  for (const e of edges) {
+    yield `${e.src} -> ${e.dest}\n`; // TODO quoting / escaping
+  }
+  yield '}\n';
+}
+
 /**
  *
- * @param {Promise<NodeJS.ReadStream>} streamP
+ * @param {Record<string, Permit>} manifest
+ * @typedef {{
+ *   vatPowers?: Record<string, boolean>
+ * }} Permit
  */
-async function read(streamP) {
-  const chunks = [];
-  const stream = await streamP;
-  for await (const chunk of stream) chunks.push(chunk);
-  return Buffer.concat(chunks).toString('utf8');
-}
+const manifest2graph = manifest => {
+  const nodes = new Set();
+  const edges = [];
+  for (const [fnName, permit] of entries(manifest)) {
+    nodes.add(fnName);
+    if (permit.vatPowers) {
+      for (const [powerName, status] of entries(permit.vatPowers)) {
+        if (status) {
+          nodes.add('vatPowers');
+          nodes.add(powerName);
+          edges.push({ src: 'vatPowers', dest: powerName });
+          edges.push({ src: fnName, dest: powerName });
+        }
+      }
+    }
+  }
+  return { nodes, edges };
+};
 
 /**
  * @param { string[] } args
  * @param { Object } io
  * @param { typeof import('fs/promises').readFile } io.readFile
+ * @param { typeof import('process').stdout } io.stdout
  */
-const main = async (args, { readFile }) => {
+const main = async (args, { readFile, stdout }) => {
   const [fn] = args;
   if (!fn) throw Error('usage: authorityViz config.json');
   const txt = await readFile(fn, 'utf-8');
@@ -31,11 +62,21 @@ const main = async (args, { readFile }) => {
     },
   } = config;
 
-  console.log({ bootstrapManifest });
+  // console.log(JSON.stringify(bootstrapManifest, null, 2));
+  const g = manifest2graph(bootstrapManifest);
+  for (const chunk of fmtGraph(g)) {
+    stdout.write(chunk);
+  }
 };
 
 /* global process */
 (async () => {
-  const fsp = await import('fs/promises');
-  return main(process.argv.slice(2), { readFile: fsp.readFile });
+  const [process, fsp] = await Promise.all([
+    import('process'),
+    import('fs/promises'),
+  ]);
+  return main(process.argv.slice(2), {
+    readFile: fsp.readFile,
+    stdout: process.stdout,
+  });
 })().catch(console.error);
