d1f425f754731fbdd2dbdd13f99af3f4cc4d5fd7
Turadg Aleahmad <turadg@agoric.com>
2022-02-08 14:54:52 -0800

    more isolation between modules

--- packages/run-protocol/src/vaultFactory/orderedVaultStore.js
+++ packages/run-protocol/src/vaultFactory/orderedVaultStore.js
@@ -4,6 +4,12 @@ import { fromVaultKey, toVaultKey } from './storeUtils.js';
 
 /**
  * Used by prioritizedVaults to wrap the Collections API for this use case.
+ *
+ * Designed to be replaceable by naked Collections API when composite keys are available.
+ *
+ * In this module debts are encoded as the inverse quotient (collateral over debt) so that
+ * greater collaterization sorts after lower. (Higher debt-to-collateral come
+ * first.)
  */
 
 /** @typedef {import('./vault').VaultKit} VaultKit */
@@ -40,25 +46,30 @@ export const makeOrderedVaultStore = () => {
   };
 
   /**
-   * Have to define both tags until https://github.com/Microsoft/TypeScript/issues/23857
+   * Exposes vaultId contained in the key but not the ordering factor.
+   * That ordering factor is the inverse quotient of the debt ratio (collateral÷debt)
+   * but nothing outside this module should rely on that to be true.
+   *
+   * Redundant tags until https://github.com/Microsoft/TypeScript/issues/23857
    *
    * @yields {[[string, string], VaultKit]>}
-   * @returns {IterableIterator<[CompositeKey, VaultKit]>}
+   * @returns {IterableIterator<[VaultId, VaultKit]>}
    */
   // XXX need to make generator with const arrow definitions?
-  function* entriesWithCompositeKeys() {
+  // XXX can/should we avoid exposing the inverse debt quotient?
+  function* entriesWithId() {
     for (const [k, v] of store.entries()) {
-      const compositeKey = fromVaultKey(k);
+      const [_, vaultId] = fromVaultKey(k);
       /** @type {VaultKit} */
       const vaultKit = v;
-      yield [compositeKey, vaultKit];
+      yield [vaultId, vaultKit];
     }
   }
 
   return harden({
     addVaultKit,
     removeVaultKit,
-    entriesWithCompositeKeys,
+    entriesWithId,
     getSize: store.getSize,
     values: store.values,
   });
