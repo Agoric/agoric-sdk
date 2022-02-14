dd2e8fc46b4e4da5ae744b689ee410e2db05a004
Dan Connolly <connolly@agoric.com>
2022-02-14 14:38:17 -0600

    fixup(run-protocol): reconnect stake reporter in startGetRun

--- packages/run-protocol/src/econ-behaviors.js
+++ packages/run-protocol/src/econ-behaviors.js
@@ -13,6 +13,7 @@ import {
   agoricNamesReserved,
 } from '@agoric/vats/src/core/utils.js';
 import '@agoric/governance/exported.js';
+import { makeStakeReporter } from '@agoric/vats/src/my-lien.js';
 import '@agoric/vats/exported.js';
 import '@agoric/vats/src/core/types.js';
 
@@ -457,7 +458,7 @@ harden(startRewardDistributor);
  * @param {Ratio} terms.collateralizationRatio
  * @param {Issuer} stakeIssuer
  *
- * @typedef {Unpromise<ReturnType<typeof import('./getRUN.js').start>>} StartLineOfCredit
+ * @typedef {Unpromise<ReturnType<typeof import('./getRUN.js').start>>} StartGetRun
  */
 export const bootstrapRunLoC = async (
   zoe,
@@ -505,7 +506,7 @@ export const bootstrapRunLoC = async (
   );
 
   const governedInstance = await E(governorFacets.creatorFacet).getInstance();
-  /** @type {ERef<StartLineOfCredit['publicFacet']>} */
+  /** @type {ERef<StartGetRun['publicFacet']>} */
   const publicFacet = E(zoe).getPublicFacet(governedInstance);
   const creatorFacet = E(governorFacets.creatorFacet).getCreatorFacet();
 
@@ -521,13 +522,14 @@ export const startGetRun = async ({
     feeMintAccess: feeMintAccessP,
     getRUNBundle,
     agoricNames,
-    bridgeManager,
+    bridgeManager: bridgeP,
     client,
     chainTimerService,
     nameAdmins,
   },
 }) => {
   const stakeName = 'BLD'; // MAGIC STRING TODO TECHDEBT
+  const bridgeManager = await bridgeP;
 
   const bundle = await getRUNBundle;
   const [
@@ -570,7 +572,7 @@ export const startGetRun = async ({
   const attIssuer = E(publicFacet).getIssuer();
   const attBrand = await E(attIssuer).getBrand();
 
-  const reporter = makeStakeReporter(bridgeManager, bldBrand);
+  const reporter = bridgeManager && makeStakeReporter(bridgeManager, bldBrand);
 
   const [brandAdmin, issuerAdmin, installAdmin, instanceAdmin] =
     await collectNameAdmins(
@@ -588,10 +590,10 @@ export const startGetRun = async ({
     E(instanceAdmin).update(key, instance),
     E(brandAdmin).update(attKey, attBrand),
     E(issuerAdmin).update(attKey, attIssuer),
-    // @ts-ignore threading types thru governance is WIP
-    E(creatorFacet).addAuthority(reporter),
+    // @ts-expect-error threading types thru governance is WIP
+    ...(reporter ? [E(creatorFacet).addAuthority(reporter)] : []),
     E(client).assignBundle({
-      // @ts-ignore threading types thru governance is WIP
+      // @ts-expect-error threading types thru governance is WIP
       attMaker: address => E(creatorFacet).getAttMaker(address),
     }),
   ]);
