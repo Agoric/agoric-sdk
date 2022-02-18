363115d47652c43bec8514f624524e70e30f5b8e
Chris Hibbert <Chris-Hibbert@users.noreply.github.com>
2022-02-17 18:07:18 -0800

    Allow AMM liquidity provision at arbitrary rate (#4519)
    * feat: allow AMM liquidity provision at arbitrary rate
    
    Add a new invitation that allows adding liiquidity in an arbitrary ratio.
    
    The virtual pools get an internal facet that allows access to
    addLiquidityActual.
    
    In order to allow the rebalancing trade and liquidity addition to be
    done in a single transaction, pool methods deal with stagedAllocations
    rather than actual allocation.
    
    closes: 4249
    
    * fix: handle corner cases where pool is too small for desired ratio
    
    * chore: clarify contract for balancesToReachRatio, improve math
    
    use multiplication rather than division to avoid  coercion to zero.
    export imbalancedRequest for testing.
    
    * build(run-protocol): add fast-check in devDependencies
    
    * build: lock packages after adding fast-check
    
    * test(run-protocol): balancesToReachRatio calculations are to spec
    
    * test(run-protocol): practical limts on balancesToReachRatio test
    
    * test: create better parameter based tests to show problematic cases
    
    * test: clarlify test logic
    
    use imbalancedRequest() to filter bad inputs.
    use better behaved arithmetic in withinEpsilon
    
    * test: drop the max limit on the generated bigInts
    
    * chore: yarn.lock wants to change
    
    * chore: ts-ignore for amounts containing Nats.
    
    Co-authored-by: Dan Connolly <connolly@agoric.com>

---
+++ packages/run-protocol/test/amm/vpool-xyk-amm/test-amm-calc.js
@@ -0,0 +1,78 @@
+// @ts-check
+import { test } from '@agoric/swingset-vat/tools/prepare-test-env-ava.js';
+import { AmountMath as m, makeIssuerKit } from '@agoric/ertp';
+import { natSafeMath } from '@agoric/zoe/src/contractSupport/index.js';
+import fc from 'fast-check';
+import {
+  imbalancedRequest,
+  balancesToReachRatio,
+} from '../../../src/vpool-xyk-amm/addLiquidity.js';
+
+const { add, multiply } = natSafeMath;
+
+const { brand: brandX } = makeIssuerKit('X');
+const { brand: brandY } = makeIssuerKit('Y');
+
+const arbPoolX = fc.bigUint().map(value => m.make(brandX, 100n + value));
+const arbPoolY = fc.bigUint().map(value => m.make(brandY, 100n + value));
+const arbGiveX = fc.bigUint().map(value => m.make(brandX, value));
+const arbGiveY = fc.bigUint().map(value => m.make(brandY, value));
+
+// left and right are within 5% of each other.
+const withinEpsilon = (left, right) =>
+  multiply(right, 105) >= multiply(left, 100) &&
+  multiply(left, 105) >= multiply(right, 100);
+
+test('balancesToReachRatio calculations are to spec', t => {
+  fc.assert(
+    fc.property(
+      fc.record({
+        poolX: arbPoolX,
+        poolY: arbPoolY,
+        giveX: arbGiveX,
+        giveY: arbGiveY,
+      }),
+      ({ poolX, poolY, giveX, giveY }) => {
+        if (
+          imbalancedRequest(
+            // use floats so we don't convert all fractions less than 1 to 0.
+            Number(m.add(poolX, giveX).value) /
+              Number(m.add(poolY, giveY).value),
+            multiply(poolX.value, poolY.value),
+          )
+        ) {
+          t.pass();
+          return;
+        }
+        const { targetX, targetY } = balancesToReachRatio(
+          poolX,
+          poolY,
+          giveX,
+          giveY,
+        );
+
+        const targetWithinRangeOfK = withinEpsilon(
+          multiply(targetX.value, targetY.value),
+          multiply(poolX.value, poolY.value),
+        );
+        t.truthy(
+          targetWithinRangeOfK,
+          `with giveX=${giveX.value} giveY=${giveY.value}, targetX=${targetX.value} and targetY=${targetY.value} should have the same product as poolX=${poolX.value} * poolY=${poolY.value}.`,
+        );
+
+        // target X / targetY approximately equals poolXAfter / poolYAfter
+        const ratiosWithinRange = withinEpsilon(
+          // @ts-ignore targetX.value is a Nat
+          targetX.value * add(poolY.value, giveY.value),
+          // @ts-ignore targetY.value is a Nat
+          targetY.value * add(poolX.value, giveX.value),
+        );
+
+        t.truthy(
+          ratiosWithinRange,
+          `targetX ${targetX.value} and targetY ${targetY.value} should be in the same ratio as poolX + giveX / poolY + giveY`,
+        );
+      },
+    ),
+  );
+});
