5d8b4c14e798be5358530c5b0f7b5b59505431c9
Dan Connolly <connolly@agoric.com>
2022-02-18 01:57:07 -0600

    fix(run-protocol): harden results from collection utilities

--- packages/run-protocol/src/collect.js
+++ packages/run-protocol/src/collect.js
@@ -4,11 +4,11 @@ const { entries, fromEntries, keys, values } = Object;
 
 /** @type { <T, U>(obj: Record<string, T>, f: (t: T) => U) => Record<string, U>} */
 export const mapValues = (obj, f) =>
-  fromEntries(entries(obj).map(([p, v]) => [p, f(v)]));
+  harden(fromEntries(entries(obj).map(([p, v]) => [p, f(v)])));
 
 /** @type { <X, Y>(xs: X[], ys: Y[]) => [X, Y][]} */
-export const zip = (xs, ys) => xs.map((x, i) => [x, ys[i]]);
+export const zip = (xs, ys) => harden(xs.map((x, i) => [x, ys[i]]));
 
 /** @type { <V>(obj: Record<string, ERef<V>>) => Promise<Record<string, V>> } */
 export const allValues = async obj =>
-  fromEntries(zip(keys(obj), await Promise.all(values(obj))));
+  harden(fromEntries(zip(keys(obj), await Promise.all(values(obj)))));
