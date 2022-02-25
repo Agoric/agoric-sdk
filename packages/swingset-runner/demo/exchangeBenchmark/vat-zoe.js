5a20a2a1656af2f90adfa010f6f8c508fddd3dab
Brian Warner <warner@lothar.com>
2022-02-24 20:21:29 -0800

    fix(swingset-runner): update tests copied from zoe

--- packages/swingset-runner/demo/exchangeBenchmark/vat-zoe.js
+++ packages/swingset-runner/demo/exchangeBenchmark/vat-zoe.js
@@ -8,11 +8,9 @@ export function buildRootObject(vatPowers, vatParameters) {
   return Far('root', {
     buildZoe: vatAdminSvc => {
       const shutdownZoeVat = vatPowers.exitVatWithFailure;
-      const { zoeService: zoe } = makeZoeKit(
-        vatAdminSvc,
-        shutdownZoeVat,
-        vatParameters.zcfBundleName,
-      );
+      const { zoeService: zoe } = makeZoeKit(vatAdminSvc, shutdownZoeVat, {
+        name: vatParameters.zcfBundleName,
+      });
       return zoe;
     },
   });
