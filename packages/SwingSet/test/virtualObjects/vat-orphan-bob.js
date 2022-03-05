0696866ae04375b0f8680d55d4151dce1ed99b60
Chip Morningstar <chip@fudco.com>
2022-03-04 16:08:34 -0800

    feat: implement multifaceted virtual objects
    closes #3834

---
+++ packages/SwingSet/test/virtualObjects/vat-orphan-bob.js
@@ -0,0 +1,32 @@
+import { Far } from '@endo/marshal';
+import { defineKind } from '../../src/storeModule.js';
+
+export function buildRootObject(vatPowers) {
+  const { testLog } = vatPowers;
+
+  const makeThing = defineKind(
+    'thing',
+    () => ({ unused: 'uncared for' }),
+    () => ({
+      facetA: {
+        methodA: () => 0,
+      },
+      facetB: {
+        methodB: () => 0,
+      },
+    }),
+  );
+
+  let originalFacet;
+
+  return Far('root', {
+    getYourThing() {
+      originalFacet = makeThing().facetA;
+      makeThing();
+      return originalFacet;
+    },
+    isThingYourThing(thing) {
+      testLog(`compare originalFacet === thing : ${originalFacet === thing}`);
+    },
+  });
+}
