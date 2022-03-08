c6e8080b5fd0fb0776bdcd2037a47ecd768fb5b2
Turadg Aleahmad <turadg@agoric.com>
2022-03-07 16:05:52 -0800

    support plantuml from Markdown

--- docs/threat_models/gen.sh
+++ docs/threat_models/gen.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-pumljar="${PLANTUML:-$HOME/plantuml/plantuml.jar}"
+# Generate PNGs from PlantUML files and put put into the Markdown
+
+CMD="java -jar ${PLANTUML:-$HOME/plantuml/plantuml.jar}"
+if command -v plantuml; then
+    # e.g. from `brew install plantuml`
+    CMD=plantuml
+fi
+
+$CMD "./**/*.puml"
+
 page="index.md"
-java -jar "$pumljar" "./**/*.puml"
 echo "# Threat Model Diagrams" > "$page"
 for diag in $(find . -name "*.png" | sort); do
     echo "## $diag\n" >> "$page"
