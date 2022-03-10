92576daf716dd6a370506ec33ee5f297542f0149
Dean Tribble <tribble@agoric.com>
2022-03-09 23:04:52 -0800

    feat: fake governance
    * review feedback
    * add initial stub governance
    * rename "run" to "stable"
    * always decrement first in reallocate staging

--- packages/run-protocol/src/psm/psm.js
+++ packages/run-protocol/src/psm/psm.js
@@ -6,9 +6,7 @@ import '@agoric/zoe/src/contracts/exported.js';
 // import { E } from '@agoric/eventual-send';
 import '@agoric/governance/src/exported';
 
-import {
-  assertProposalShape,
-} from '@agoric/zoe/src/contractSupport/index.js';
+import { assertProposalShape } from '@agoric/zoe/src/contractSupport/index.js';
 import {
   ceilMultiplyBy,
   makeRatio,
@@ -18,51 +16,57 @@ import { Far } from '@endo/marshal';
 
 import { AmountMath } from '@agoric/ertp';
 import { makeMakeCollectFeesInvitation } from '../vaultFactory/collectRewardFees.js';
-import { makeParamManagerBuilder } from '@agoric/governance';
-// import { makeVaultParamManager, makeElectorateParamManager } from './params.js';
 
 const { details: X } = assert;
 
-const FEE_PARAM = 'FEE';
+const BASIS_POINTS = 10000n;
 
-const makeParamTerms = (number, invitationAmount) => {
-  return harden({
-    [FEE_PARAM]: makeGovernedNat(number),
-    // [CONTRACT_ELECTORATE]: makeGovernedInvitation(invitationAmount),
-  });
-};
-
-const makeParamManager = async (zoe, number, invitation) => {
-  const builder = makeParamManagerBuilder(zoe).addNat(FEE_PARAM, number);
-  // await builder.addInvitation(CONTRACT_ELECTORATE, invitation);
-  return builder.build();
-};
 /**
  * @param {ContractFacet} zcf
  * @param {{feeMintAccess: FeeMintAccess}} privateArgs
  */
 export const start = async (zcf, privateArgs) => {
   const {
-    main: {
-      [FEE_PARAM]: feeParam,
-    },
+    // main: {
+    //   [FEE_PARAM]: feeParam,
+    // },
     anchorBrand,
   } = zcf.getTerms();
 
   const { feeMintAccess } = privateArgs;
-  const baseMint = await zcf.registerFeeMint('RUN', feeMintAccess);
-  const { brand: runBrand } = baseMint.getIssuerRecord();
+  // TODO should this know that the name is 'Stable'
+  const stableMint = await zcf.registerFeeMint('Stable', feeMintAccess);
+  const { brand: stableBrand } = stableMint.getIssuerRecord();
   zcf.setTestJig(() => ({
-    runIssuerRecord: baseMint.getIssuerRecord(),
+    stableIssuerRecord: stableMint.getIssuerRecord(),
   }));
+
+  // HACK for simple governance API
+  const getGovernance = _zcf => {
+    // const FEE_PARAM = 'FEE';
+    // const {
+    //   main: { [FEE_PARAM]: feeBP },
+    // } = zcf.getTerms();
+    const FEE_BP = 2n;
+    const MINT_LIMIT = AmountMath.make(anchorBrand, 20_000_000n * 1_000_000n);
+    return {
+      getFeeBP: () => FEE_BP,
+      getMintLimit: () => MINT_LIMIT,
+    };
+  };
+
+  const gov = getGovernance(zcf);
 
   const { zcfSeat: anchorPool } = zcf.makeEmptySeatKit();
   const { zcfSeat: feePool } = zcf.makeEmptySeatKit();
   const { zcfSeat: stage } = zcf.makeEmptySeatKit();
-  const BASIS_POINTS = 10000n;
-  const FEE_BP = 2n;
-  // const withFee = makeRatio(BASIS_POINTS - FEE_BP, baseBrand, BASIS_POINTS);
-  const feeRate = makeRatio(FEE_BP, runBrand, BASIS_POINTS);
+
+  const assertUnderLimit = anchorAmount => {
+    assert(
+      AmountMath.isGTE(gov.getMintLimit(), anchorAmount),
+      X`Request would exceed mint limit`,
+    );
+  };
 
   const swapHook = seat => {
     assertProposalShape(seat, {
@@ -73,38 +77,47 @@ export const start = async (zcf, privateArgs) => {
       give: { In: given },
       want: { Out: wanted },
     } = seat.getProposal();
-    if (given.brand === runBrand) {
+    const feeRate = makeRatio(gov.getFeeBP(), stableBrand, BASIS_POINTS);
+
+    if (given.brand === stableBrand) {
       assert(wanted.brand === anchorBrand, X`Unexpected brand ${wanted}`);
       const fee = ceilMultiplyBy(given, feeRate);
       const afterFee = AmountMath.subtract(given, fee);
-      const maxAnchor = AmountMath.make(wanted.brand, afterFee.value);
+      const maxAnchor = AmountMath.make(anchorBrand, afterFee.value);
+      // TODO this prevents the reallocate from failing. Can this be tested otherwise?
       assert(
         AmountMath.isGTE(maxAnchor, wanted),
         X`wanted ${wanted} is more then ${given} minus fees ${fee}`,
       );
-      seat.decrementBy(stage.incrementBy({ RUN: afterFee }));
-      seat.decrementBy(feePool.incrementBy({ RUN: fee }));
+      stage.incrementBy(seat.decrementBy({ Stable: afterFee }));
+      feePool.incrementBy(seat.decrementBy({ Stable: fee }));
       seat.incrementBy(anchorPool.decrementBy({ Anchor: maxAnchor }));
       zcf.reallocate(seat, anchorPool, stage, feePool);
-      baseMint.burnLosses({ RUN: afterFee }, stage);
+      stableMint.burnLosses({ Stable: afterFee }, stage);
     } else if (given.brand === anchorBrand) {
-      assert(wanted.brand === runBrand, X`Unexpected brand ${wanted}`);
-      const asBase = AmountMath.make(wanted.brand, given.value);
-      const fee = ceilMultiplyBy(asBase, feeRate);
-      const afterFee = AmountMath.subtract(asBase, fee);
+      const anchorAfterTrade = AmountMath.add(
+        anchorPool.getAmountAllocated('Anchor'),
+        given,
+      );
+      assertUnderLimit(anchorAfterTrade);
+      assert(wanted.brand === stableBrand, X`Unexpected brand ${wanted}`);
+      const asStable = AmountMath.make(wanted.brand, given.value);
+      const fee = ceilMultiplyBy(asStable, feeRate);
+      const afterFee = AmountMath.subtract(asStable, fee);
       // TODO use the offer-safe check to be sure that reallocate will work?
       assert(
         AmountMath.isGTE(afterFee, wanted),
         X`wanted ${wanted} is more then ${given} minus fees ${fee}`,
       );
-      baseMint.mintGains({ RUN: asBase }, stage);
-      seat.decrementBy(anchorPool.incrementBy({ Anchor: given }));
-      seat.incrementBy(stage.decrementBy({ RUN: afterFee }));
-      feePool.incrementBy(stage.decrementBy({ RUN: fee }));
+      stableMint.mintGains({ Stable: asStable }, stage);
+      anchorPool.incrementBy(seat.decrementBy({ Anchor: given }));
+      seat.incrementBy(stage.decrementBy({ Stable: afterFee }));
+      feePool.incrementBy(stage.decrementBy({ Stable: fee }));
       try {
         zcf.reallocate(seat, anchorPool, stage, feePool);
       } catch (e) {
-        baseMint.burnLosses({ RUN: asBase }, stage);
+        // TODO reallocate should guarantee that this case cannot happen
+        stableMint.burnLosses({ Stable: asStable }, stage);
         throw e;
       }
     } else {
@@ -113,25 +126,20 @@ export const start = async (zcf, privateArgs) => {
   };
   const makeSwapInvitation = () => zcf.makeInvitation(swapHook, 'swap');
 
-  const { makeCollectFeesInvitation } = makeMakeCollectFeesInvitation(
-    zcf,
-    feePool,
-    runBrand,
-  );
-
   // Eventually the reward pool will live elsewhere. For now it's here for
   // bookkeeping. It's needed in tests.
   const getRewardAllocation = () => feePool.getCurrentAllocation();
 
   const publicFacet = Far('Parity Stability Module', {
     makeSwapInvitation,
-    getContractGovernor: () => electionManager,
+    // getContractGovernor: () => electionManager,
   });
 
   const creatorFacet = Far('Parity Stability Module', {
     getRewardAllocation,
-    makeCollectFeesInvitation,
-    getContractGovernor: () => electionManager,
+    makeCollectFeesInvitation: () =>
+      makeMakeCollectFeesInvitation(zcf, feePool, stableBrand),
+    // getContractGovernor: () => electionManager,
   });
 
   return { creatorFacet, publicFacet };
