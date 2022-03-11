9557d4ed4809eb3ae0688a2187b1f3f1a902c5fe
Turadg Aleahmad <turadg@agoric.com>
2022-03-11 15:51:53 -0800

    WIP typedParamManager

---
+++ packages/governance/src/paramGovernance/typedParamManager.js
@@ -0,0 +1,153 @@
+// @ts-check
+
+import { Far } from '@endo/marshal';
+import { assertIsRatio } from '@agoric/zoe/src/contractSupport/index.js';
+import { AmountMath } from '@agoric/ertp';
+import { assertKeywordName } from '@agoric/zoe/src/cleanProposal.js';
+import { Nat } from '@agoric/nat';
+import { makeSubscriptionKit } from '@agoric/notifier';
+import { makeStore } from '@agoric/store';
+
+import {
+  makeLooksLikeBrand,
+  makeAssertInstallation,
+  makeAssertInstance,
+} from './assertions.js';
+import { ParamType } from './paramManager.js';
+
+const { details: X } = assert;
+
+/**
+ *
+ * @param {Record<Keyword, ParamDescription>} spec
+ * @param {ERef<ZoeService>} [zoe]
+ */
+const makeParamManager = (spec, zoe) => {
+  const namesToParams = makeStore('Parameter Name');
+  const { publication, subscription } = makeSubscriptionKit();
+  const paramFunctions = {};
+
+  // support for parameters that are copy objects
+  const buildCopyParam = (name, value, assertion, type) => {
+    let current;
+    assertKeywordName(name);
+
+    const setParamValue = newValue => {
+      assertion(newValue);
+      current = newValue;
+      publication.updateState({ name, type, value: current });
+      return newValue;
+    };
+    setParamValue(value);
+
+    const getVisibleValue = proposed => {
+      assertion(proposed);
+      return proposed;
+    };
+
+    const publicMethods = Far(`Parameter ${name}`, {
+      getValue: () => current,
+      assertType: assertion,
+      makeDescription: () => ({ type, value: current }),
+      getVisibleValue,
+      getType: () => type,
+    });
+
+    // CRUCIAL: here we're creating the update functions that can change the
+    // values of the governed contract's parameters. We'll return the updateFns
+    // to our caller. They must handle them carefully to ensure that they end up
+    // in appropriate hands.
+    paramFunctions[`update${name}`] = setParamValue;
+    paramFunctions[`get${name}`] = () => current;
+    namesToParams.init(name, publicMethods);
+  };
+
+  /**
+   * Handlers for each parameter type
+   *
+   * @type {Record<ParamType, (Keyword, any) => void>}
+   */
+  const adders = {
+    amount: (name, value) => {
+      const assertAmount = a => {
+        assert(a.brand, `Expected an Amount for ${name}, got "${a}"`);
+        return AmountMath.coerce(a.brand, a);
+      };
+      buildCopyParam(name, value, assertAmount, ParamType.AMOUNT);
+    },
+
+    brand: (name, value) => {
+      const assertBrand = makeLooksLikeBrand(name);
+      buildCopyParam(name, value, assertBrand, ParamType.BRAND);
+    },
+
+    installation: (name, value) => {
+      const assertInstallation = makeAssertInstallation(name);
+      buildCopyParam(name, value, assertInstallation, ParamType.INSTALLATION);
+    },
+
+    instance: (name, value) => {
+      const assertInstance = makeAssertInstance(name);
+      buildCopyParam(name, value, assertInstance, ParamType.INSTANCE);
+    },
+
+    invitation: (name, value) => {
+      throw Error('Not yet implemented');
+    },
+
+    nat: (name, value) => {
+      const assertNat = v => {
+        assert.typeof(v, 'bigint');
+        Nat(v);
+        return true;
+      };
+      buildCopyParam(name, value, assertNat, ParamType.NAT);
+    },
+
+    ratio: (name, value) => {
+      buildCopyParam(name, value, assertIsRatio, ParamType.RATIO);
+    },
+
+    relativeTime: (name, value) => {
+      throw Error('Not yet implemented');
+    },
+
+    string: (name, value) => {
+      const assertString = v => assert.typeof(v, 'string');
+      buildCopyParam(name, value, assertString, ParamType.STRING);
+    },
+
+    unknown: (name, value) => {
+      const assertUnknown = _v => true;
+      buildCopyParam(name, value, assertUnknown, ParamType.UNKNOWN);
+    },
+  };
+
+  const getVisibleValue = (name, proposed) =>
+    namesToParams.get(name).getVisibleValue(proposed);
+
+  /**
+   * Should be exposed within contracts, and not externally, for invitations
+   *
+   * @param {keyof typeof spec} name
+   */
+  const getInternalParamValue = name =>
+    namesToParams.get(name).getInternalValue();
+
+  const mgr = { getParams: () => spec, getSubscription: () => subscription };
+  for (const [name, desc] of Object.entries(spec)) {
+    const { type, value } = desc;
+    const add = adders[type];
+    add(name, value);
+  }
+  return Far('param manager', {
+    getParams: () => spec, // FIXME
+    getSubscription: () => subscription,
+    getVisibleValue,
+    getInternalParamValue,
+    ...paramFunctions,
+  });
+};
+
+harden(makeParamManager);
+export { makeParamManager };
---
+++ packages/governance/test/unitTests/test-typedParamManager.js
@@ -0,0 +1,333 @@
+// @ts-check
+
+import { test } from '@agoric/zoe/tools/prepare-test-env-ava.js';
+import { makeIssuerKit, AmountMath, AssetKind } from '@agoric/ertp';
+import { makeRatio } from '@agoric/zoe/src/contractSupport/index.js';
+import { setupZCFTest } from '@agoric/zoe/test/unitTests/zcf/setupZcfTest.js';
+import { E } from '@agoric/eventual-send';
+import { Far } from '@endo/marshal';
+
+import { makeHandle } from '@agoric/zoe/src/makeHandle.js';
+import { ParamType } from '../../src/index.js';
+import { makeParamManager } from '../../src/paramGovernance/typedParamManager.js';
+
+test.only('two parameters', t => {
+  const drachmaKit = makeIssuerKit('drachma');
+
+  const drachmaBrand = drachmaKit.brand;
+  const drachmas = AmountMath.make(drachmaBrand, 37n);
+  const paramManager = makeParamManager({
+    Currency: { type: 'brand', value: drachmaBrand },
+    Amt: { type: 'amount', value: drachmas },
+  });
+
+  t.is(paramManager.getCurrency(), drachmaBrand);
+  t.deepEqual(paramManager.getAmt(), drachmas);
+  t.deepEqual(
+    paramManager.getParams(),
+    harden({
+      Currency: {
+        type: ParamType.BRAND,
+        value: drachmaBrand,
+      },
+      Amt: {
+        type: ParamType.AMOUNT,
+        value: drachmas,
+      },
+    }),
+  );
+});
+
+test.only('Amount', async t => {
+  const { brand } = makeIssuerKit('floor wax');
+  const { brand: brand2 } = makeIssuerKit('dessertTopping');
+  const paramManager = makeParamManager({
+    Shimmer: { type: 'amount', value: AmountMath.make(brand, 250n) },
+  });
+  t.deepEqual(paramManager.getShimmer(), AmountMath.make(brand, 250n));
+
+  paramManager.updateShimmer(AmountMath.make(brand2, 300n));
+  t.deepEqual(paramManager.getShimmer(), AmountMath.make(brand2, 300n));
+
+  t.throws(() => paramManager.updateShimmer('fear,loathing'), {
+    message: 'Expected an Amount for Shimmer, got "fear,loathing"',
+  });
+});
+
+test('Branded Amount', async t => {
+  const { brand: floorBrand } = makeIssuerKit('floor wax');
+  const { brand: dessertBrand } = makeIssuerKit('dessertTopping');
+  const paramManager = makeParamManagerBuilder()
+    .addBrandedAmount('Shimmer', AmountMath.make(floorBrand, 2n))
+    .build();
+  t.deepEqual(
+    paramManager.getAmount('Shimmer'),
+    AmountMath.make(floorBrand, 2n),
+  );
+
+  // @ts-ignore updateShimmer is a generated name
+  paramManager.updateShimmer(AmountMath.make(floorBrand, 5n));
+  t.deepEqual(
+    paramManager.getAmount('Shimmer'),
+    AmountMath.make(floorBrand, 5n),
+  );
+
+  // @ts-ignore updateShimmer is a generated name
+  t.throws(
+    // @ts-ignore updateShimmer is a generated name
+    () => paramManager.updateShimmer(AmountMath.make(dessertBrand, 20n)),
+    {
+      message:
+        'The brand in the allegedAmount {"brand":"[Alleged: dessertTopping brand]","value":"[20n]"} in \'coerce\' didn\'t match the specified brand "[Alleged: floor wax brand]".',
+    },
+  );
+
+  // @ts-ignore updateCandle is a generated name
+  t.throws(() => paramManager.updateShimmer('fear,loathing'), {
+    message: 'Expected an Amount for Shimmer, got "fear,loathing"',
+  });
+
+  t.throws(() => paramManager.getString('Shimmer'), {
+    message: '"Shimmer" is not "string"',
+  });
+});
+
+test('params one installation', async t => {
+  const installationKey = 'Installation';
+  // this is sufficient for the current type check. When we add
+  // isInstallation() (#3344), we'll need to make a mockZoe.
+  const installationHandle = Far('fake Installation', {
+    getBundle: () => ({ obfuscated: 42 }),
+  });
+
+  const paramManager = makeParamManagerBuilder()
+    .addInstallation('Installation', installationHandle)
+    .build();
+
+  t.deepEqual(
+    paramManager.getInstallation(installationKey),
+    installationHandle,
+  );
+  t.throws(
+    // @ts-ignore updateInstallation is a generated name
+    () => paramManager.updateInstallation(18.1),
+    {
+      message: 'value for "Installation" must be an Installation, was 18.1',
+    },
+    'value should be an installation',
+  );
+  const handle2 = Far('another fake Installation', {
+    getBundle: () => ({ condensed: '() => {})' }),
+  });
+  // @ts-ignore updateInstallation is a generated name
+  paramManager.updateInstallation(handle2);
+  t.deepEqual(paramManager.getInstallation(installationKey), handle2);
+
+  t.throws(() => paramManager.getNat('Installation'), {
+    message: '"Installation" is not "nat"',
+  });
+
+  t.deepEqual(
+    paramManager.getParams(),
+    harden({
+      Installation: {
+        type: ParamType.INSTALLATION,
+        value: handle2,
+      },
+    }),
+  );
+});
+
+test('params one instance', async t => {
+  const instanceKey = 'Instance';
+  // this is sufficient for the current type check. When we add
+  // isInstallation() (#3344), we'll need to make a mockZoe.
+  const instanceHandle = makeHandle(instanceKey);
+
+  const paramManager = makeParamManagerBuilder()
+    .addInstance(instanceKey, instanceHandle)
+    .build();
+
+  t.deepEqual(paramManager.getInstance(instanceKey), instanceHandle);
+  t.throws(
+    // @ts-ignore updateInstance is a generated name
+    () => paramManager.updateInstance(18.1),
+    {
+      message: 'value for "Instance" must be an Instance, was 18.1',
+    },
+    'value should be an instance',
+  );
+  const handle2 = makeHandle(instanceKey);
+  // @ts-ignore updateInstance is a generated name
+  paramManager.updateInstance(handle2);
+  t.deepEqual(paramManager.getInstance(instanceKey), handle2);
+
+  t.deepEqual(
+    paramManager.getParams(),
+    harden({
+      Instance: {
+        type: ParamType.INSTANCE,
+        value: handle2,
+      },
+    }),
+  );
+});
+
+test('Invitation', async t => {
+  const drachmaKit = makeIssuerKit('drachma');
+  const terms = harden({
+    mmr: makeRatio(150n, drachmaKit.brand),
+  });
+
+  const issuerKeywordRecord = harden({
+    Ignore: drachmaKit.issuer,
+  });
+
+  const { instance, zoe } = await setupZCFTest(issuerKeywordRecord, terms);
+
+  const invitation = await E(E(zoe).getPublicFacet(instance)).makeInvitation();
+
+  const invitationAmount = await E(E(zoe).getInvitationIssuer()).getAmountOf(
+    invitation,
+  );
+
+  const drachmaBrand = drachmaKit.brand;
+  const drachmaAmount = AmountMath.make(drachmaBrand, 37n);
+  const paramManagerBuilder = makeParamManagerBuilder(zoe)
+    .addBrand('Currency', drachmaBrand)
+    .addAmount('Amt', drachmaAmount);
+  // addInvitation is async, so it can't be part of the cascade.
+  await paramManagerBuilder.addInvitation('Invite', invitation);
+  const paramManager = paramManagerBuilder.build();
+
+  t.is(paramManager.getBrand('Currency'), drachmaBrand);
+  t.is(paramManager.getAmount('Amt'), drachmaAmount);
+  const invitationActualAmount =
+    paramManager.getInvitationAmount('Invite').value;
+  t.is(invitationActualAmount, invitationAmount.value);
+  // @ts-ignore invitationActualAmount's type is unknown
+  t.is(invitationActualAmount[0].description, 'simple');
+
+  t.is(await paramManager.getInternalParamValue('Invite'), invitation);
+
+  t.deepEqual(
+    paramManager.getParams(),
+    harden({
+      Amt: {
+        type: ParamType.AMOUNT,
+        value: drachmaAmount,
+      },
+      Currency: {
+        type: ParamType.BRAND,
+        value: drachmaBrand,
+      },
+      Invite: {
+        type: ParamType.INVITATION,
+        value: invitationAmount,
+      },
+    }),
+  );
+});
+
+test('two Nats', async t => {
+  const paramManager = makeParamManagerBuilder()
+    .addNat('Acres', 50n)
+    .addNat('SpeedLimit', 299_792_458n)
+    .build();
+
+  t.is(paramManager.getNat('Acres'), 50n);
+  t.is(paramManager.getNat('SpeedLimit'), 299_792_458n);
+
+  // @ts-ignore updateSpeedLimit is a generated name
+  t.throws(() => paramManager.updateSpeedLimit(300000000), {
+    message: '300000000 must be a bigint',
+  });
+
+  // @ts-ignore updateSpeedLimit is a generated name
+  t.throws(() => paramManager.updateSpeedLimit(-37n), {
+    message: '-37 is negative',
+  });
+});
+
+test('Ratio', async t => {
+  const unitlessBrand = makeIssuerKit('unitless').brand;
+
+  const ratio = makeRatio(16180n, unitlessBrand, 10_000n);
+  const paramManager = makeParamManagerBuilder()
+    .addNat('Acres', 50n)
+    .addRatio('GoldenRatio', ratio)
+    .build();
+  t.is(paramManager.getRatio('GoldenRatio'), ratio);
+
+  const morePrecise = makeRatio(1618033n, unitlessBrand, 1_000_000n);
+  // @ts-ignore updateGoldenRatio is a generated name
+  paramManager.updateGoldenRatio(morePrecise);
+  t.is(paramManager.getRatio('GoldenRatio'), morePrecise);
+  // @ts-ignore updateGoldenRatio is a generated name
+  t.throws(() => paramManager.updateGoldenRatio(300000000), {
+    message: '"ratio" 300000000 must be a pass-by-copy record, not "number"',
+  });
+});
+
+test('Branded Ratio', async t => {
+  const unitlessBrand = makeIssuerKit('unitless').brand;
+
+  const ratio = makeRatio(16180n, unitlessBrand, 10_000n);
+  const paramManager = makeParamManagerBuilder()
+    .addBrandedRatio('GoldenRatio', ratio)
+    .build();
+  t.is(paramManager.getRatio('GoldenRatio'), ratio);
+
+  const morePrecise = makeRatio(1618033n, unitlessBrand, 1_000_000n);
+  // @ts-ignore updateGoldenRatio is a generated name
+  paramManager.updateGoldenRatio(morePrecise);
+  t.is(paramManager.getRatio('GoldenRatio'), morePrecise);
+
+  const anotherBrand = makeIssuerKit('arbitrary').brand;
+
+  // @ts-ignore updateGoldenRatio is a generated name
+  t.throws(
+    () =>
+      // @ts-ignore updateGoldenRatio is a generated name
+      paramManager.updateGoldenRatio(makeRatio(16180n, anotherBrand, 10_000n)),
+    {
+      message:
+        'Numerator brand for "GoldenRatio" must be "[Alleged: unitless brand]"',
+    },
+  );
+});
+
+test('Strings', async t => {
+  const paramManager = makeParamManagerBuilder()
+    .addNat('Acres', 50n)
+    .addString('OurWeapons', 'fear')
+    .build();
+  t.is(paramManager.getString('OurWeapons'), 'fear');
+
+  // @ts-ignore updateOurWeapons is a generated name
+  paramManager.updateOurWeapons('fear,surprise');
+  t.is(paramManager.getString('OurWeapons'), 'fear,surprise');
+  // @ts-ignore updateOurWeapons is a generated name
+  t.throws(() => paramManager.updateOurWeapons(300000000), {
+    message: '300000000 must be a string',
+  });
+
+  t.throws(() => paramManager.getNat('OurWeapons'), {
+    message: '"OurWeapons" is not "nat"',
+  });
+});
+
+test('Unknown', async t => {
+  const paramManager = makeParamManagerBuilder()
+    .addString('Label', 'birthday')
+    .addUnknown('Surprise', 'party')
+    .build();
+  t.is(paramManager.getUnknown('Surprise'), 'party');
+
+  // @ts-ignore updateSurprise is a generated name
+  paramManager.updateSurprise('gift');
+  t.is(paramManager.getUnknown('Surprise'), 'gift');
+  // @ts-ignore updateSurprise is a generated name
+  paramManager.updateSurprise(['gift', 'party']);
+  t.deepEqual(paramManager.getUnknown('Surprise'), ['gift', 'party']);
+});
