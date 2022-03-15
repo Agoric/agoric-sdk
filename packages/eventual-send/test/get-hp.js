1f29b7646218897b4dfedf7efea7b133571d147c
Kris Kowal <kris@agoric.com>
2022-03-15 14:30:50 -0700

    chore: Remove moved eventual-send

--- packages/eventual-send/test/get-hp.js
+++
@@ -1,5 +0,0 @@
-import makeE from '../src/E.js';
-import { makeHandledPromise } from '../src/handled-promise.js';
-
-export const HandledPromise = makeHandledPromise(Promise);
-export const E = makeE(HandledPromise);
