af16312e9c79a32d7a5275146a8f401bd4608bfd
Dan Connolly <connolly@agoric.com>
2022-03-18 16:09:37 -0500

    style: update types in collect.js

--- packages/run-protocol/src/collect.js
+++ packages/run-protocol/src/collect.js
@@ -2,14 +2,15 @@
 
 const { entries, fromEntries, keys, values } = Object;
 
-/** @type { <T, U>(obj: Record<string, T>, f: (t: T) => U) => Record<string, U>} */
+/** @type { <K extends string, T, U>(obj: Record<K, T>, f: (t: T) => U) => Record<K, U>} */
 export const mapValues = (obj, f) =>
+  // @ts-ignore entries() loses the K type
   harden(fromEntries(entries(obj).map(([p, v]) => [p, f(v)])));
 
 /** @type { <X, Y>(xs: X[], ys: Y[]) => [X, Y][]} */
 export const zip = (xs, ys) => harden(xs.map((x, i) => [x, ys[+i]]));
 
-/** @type { <V>(obj: Record<string, ERef<V>>) => Promise<Record<string, V>> } */
+/** @type { <K extends string, V>(obj: Record<K, ERef<V>>) => Promise<Record<string, V>> } */
 export const allValues = async obj => {
   const resolved = await Promise.all(values(obj));
   return harden(fromEntries(zip(keys(obj), resolved)));
