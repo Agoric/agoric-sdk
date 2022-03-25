3c9ad7651bc31b8d5ca79a99ba16221460c6ea14
Michael FIG <michael+github@fig.org>
2022-03-25 14:08:40 -0600

    test(vats): test board.lookup

--- packages/vats/test/test-lib-board.js
+++ packages/vats/test/test-lib-board.js
@@ -7,17 +7,33 @@ import { makeBoard } from '../src/lib-board.js';
 test('makeBoard', async t => {
   const board = makeBoard();
 
-  const obj1 = Far('obj1', {});
+  const obj1 = Far('obj1', { lookup: async (...path) => path });
   const obj2 = Far('obj2', {});
 
   t.deepEqual(board.ids(), [], `board is empty to start`);
+  t.is(await board.lookup(), board, 'empty lookup returns board');
+
+  await t.throwsAsync(board.lookup('board0371'), {
+    message: /board does not have id: "board0371"/,
+  });
 
   const idObj1 = board.getId(obj1);
   t.is(idObj1, 'board0371');
   t.deepEqual(board.ids(), [idObj1], `board has one id`);
 
+  t.is(await board.lookup('board0371'), obj1, 'id lookup returns obj1');
+  t.deepEqual(
+    await board.lookup('board0371', 'a', 'b', 'c'),
+    ['a', 'b', 'c'],
+    'path lookup returns path',
+  );
+
   const idObj2 = board.getId(obj2);
   t.is(idObj2, 'board0592');
+  t.is(await board.lookup('board0592'), obj2, 'id lookup returns obj2');
+  await t.throwsAsync(board.lookup('board0592', 'a', 'b', 'c'), {
+    message: /target has no method "lookup"/,
+  });
   t.deepEqual(board.ids().length, 2, `board has two ids`);
 
   t.deepEqual(board.getValue(idObj1), obj1, `id matches value obj1`);
