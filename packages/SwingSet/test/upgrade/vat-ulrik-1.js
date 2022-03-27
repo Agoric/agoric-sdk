555db756b99f65178b1260f5c1e084abc9b41d52
Brian Warner <warner@lothar.com>
2022-03-26 18:28:50 -0700

    test(swingset): test that baggage and durables survive vat upgrade
    We create a durable Kind, and reattach behavior to it in v2. The
    handle must travel through baggage, demonstrating that baggage works.
    
    I'm still looking for the right way to use VatData these from with
    swingset tests.. other packages should import @agoric/vat-data, but
    that might be circular from here
    
    refs #1848

--- packages/SwingSet/test/upgrade/vat-ulrik-1.js
+++ packages/SwingSet/test/upgrade/vat-ulrik-1.js
@@ -1,10 +1,36 @@
+/* global VatData */
 import { Far } from '@endo/marshal';
 import { makePromiseKit } from '@endo/promise-kit';
+// import { makeKindHandle, defineDurableKind } from '@agoric/vat-data';
+const { makeKindHandle, defineDurableKind } = VatData;
 
-export function buildRootObject(_vatPowers, vatParameters) {
+const durandalHandle = makeKindHandle('durandal');
+function initializeDurandal(arg) {
+  return { arg };
+}
+function actualizeDurandal(state) {
+  return {
+    get() {
+      return state.arg;
+    },
+    set(arg) {
+      state.arg = arg;
+    },
+  };
+}
+const makeDurandal = defineDurableKind(
+  durandalHandle,
+  initializeDurandal,
+  actualizeDurandal,
+);
+
+export function buildRootObject(_vatPowers, vatParameters, baggage) {
   const { promise: p1 } = makePromiseKit();
   const { promise: p2 } = makePromiseKit();
   let heldPromise;
+
+  baggage.init('data', harden(['some', 'data']));
+  baggage.init('durandalHandle', durandalHandle);
 
   return Far('root', {
     getVersion() {
@@ -13,6 +39,20 @@ export function buildRootObject(_vatPowers, vatParameters) {
     getParameters() {
       return vatParameters;
     },
+
+    acceptPresence(pres) {
+      baggage.init('presence', pres);
+    },
+    getPresence() {
+      return baggage.get('presence');
+    },
+    getData() {
+      return baggage.get('data');
+    },
+    getDurandal(arg) {
+      return makeDurandal(arg);
+    },
+
     acceptPromise(p) {
       // stopVat will reject the promises that we decide, but should
       // not touch the ones we don't decide, so we hold onto this
--- packages/SwingSet/test/upgrade/vat-ulrik-2.js
+++ packages/SwingSet/test/upgrade/vat-ulrik-2.js
@@ -1,12 +1,38 @@
+/* global VatData */
 import { Far } from '@endo/marshal';
+// import { defineDurableKind } from '@agoric/vat-data';
+const { defineDurableKind } = VatData;
 
-export function buildRootObject(_vatPowers, vatParameters) {
+function initializeDurandal(arg) {
+  return { arg };
+}
+function actualizeDurandal(state) {
+  return {
+    get() {
+      return `new ${state.arg}`;
+    },
+    set(arg) {
+      state.arg = arg;
+    },
+  };
+}
+
+export function buildRootObject(_vatPowers, vatParameters, baggage) {
+  const durandalHandle = baggage.get('durandalHandle');
+  defineDurableKind(durandalHandle, initializeDurandal, actualizeDurandal);
+
   return Far('root', {
     getVersion() {
       return 'v2';
     },
     getParameters() {
       return vatParameters;
+    },
+    getPresence() {
+      return baggage.get('presence');
+    },
+    getData() {
+      return baggage.get('data');
     },
   });
 }
