40881a737fc21a10b6830edb90b9116c95fb1352
Michael FIG <michael+github@fig.org>
2022-04-15 13:58:17 -0600

    build: use `deploy-script-support` for `build-bundles.js`

--- packages/spawner/scripts/build-bundles.js
+++ packages/spawner/scripts/build-bundles.js
@@ -5,6 +5,8 @@ import url from 'url';
 
 const dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-const sourceToBundle = [[`../src/pegasus.js`, `../bundles/bundle-pegasus.js`]];
+const sourceToBundle = [
+  [`../src/vat-spawned.js`, `../bundles/bundle-spawn.js`],
+];
 
 createBundles(sourceToBundle, dirname);
