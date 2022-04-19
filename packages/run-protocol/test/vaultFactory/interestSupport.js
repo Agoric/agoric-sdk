2c9a5d0e07b0886ba6cbfd38ec6d321f1e42a4fb
Turadg Aleahmad <turadg@agoric.com>
2022-04-19 06:38:32 -0700

    fix(run-protocol): vault debt ratio ordering after interest charges (#5149)
    * test for ordering bug
    
    * correct ordering
    
    * ordering fix
    
    * test robust to parallelism
    
    * all green
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ packages/run-protocol/test/vaultFactory/interestSupport.js
@@ -0,0 +1,56 @@
+import { AmountMath } from '@agoric/ertp';
+import {
+  floorMultiplyBy,
+  makeRatio,
+} from '@agoric/zoe/src/contractSupport/ratio.js';
+import { Far } from '@endo/marshal';
+import { reverseInterest } from '../../src/interest-math.js';
+
+export const makeCompoundedInterestProvider = brand => {
+  let compoundedInterest = makeRatio(100n, brand);
+  return {
+    getCompoundedInterest: () => compoundedInterest,
+    chargeHundredPercentInterest: () => {
+      compoundedInterest = makeRatio(
+        compoundedInterest.numerator.value * 2n,
+        brand,
+      );
+    },
+  };
+};
+/**
+ * @param {VaultId} vaultId
+ * @param {Amount<'nat'>} initDebt
+ * @param {Amount<'nat'>} [initCollateral]
+ * @param {*} [manager]
+ * @returns {InnerVault & {setDebt: (Amount) => void}}
+ */
+
+export const makeFakeInnerVault = (
+  vaultId,
+  initDebt,
+  initCollateral = AmountMath.make(initDebt.brand, 100n),
+  manager = makeCompoundedInterestProvider(initDebt.brand),
+) => {
+  let normalizedDebt = reverseInterest(
+    initDebt,
+    manager.getCompoundedInterest(),
+  );
+  let collateral = initCollateral;
+  const vault = Far('Vault', {
+    getCollateralAmount: () => collateral,
+    getNormalizedDebt: () => normalizedDebt,
+    getCurrentDebt: () =>
+      floorMultiplyBy(normalizedDebt, manager.getCompoundedInterest()),
+    setDebt: newDebt =>
+      (normalizedDebt = reverseInterest(
+        newDebt,
+        manager.getCompoundedInterest(),
+      )),
+    setCollateral: newCollateral => (collateral = newCollateral),
+    getIdInManager: () => vaultId,
+    liquidate: () => {},
+  });
+  // @ts-expect-error cast
+  return vault;
+};
