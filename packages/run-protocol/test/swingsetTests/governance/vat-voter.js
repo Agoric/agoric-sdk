342e021778553a13da495e99a18dd1abeeed07ed
Turadg Aleahmad <turadg@agoric.com>
2022-04-20 12:08:49 -0700

    test(governance): fix some async validation (#5126)
    * types
    
    * dead code
    
    * test(governance): fix error in validate promise handling
    
    * run-protocol types
    
    * test(run-protocol): fix validation in governance swingset test
    
    * validateTimer on closingRule specifically
    
    * eref

--- packages/run-protocol/test/swingsetTests/governance/vat-voter.js
+++ packages/run-protocol/test/swingsetTests/governance/vat-voter.js
@@ -43,6 +43,7 @@ const build = async (log, zoe) => {
           const electionManagerP = E.get(governedTermsP).electionManager;
 
           const counterPublicP = E(zoe).getPublicFacet(counterInstance);
+          /** @type {Promise<QuestionSpec<ParamChangeIssue>>} */
           const ballotDetailsP = E(counterPublicP).getDetails();
 
           const [electionManager, ballotDetails] = await Promise.all([
