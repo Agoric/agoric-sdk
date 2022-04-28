e3f77ef788d7c85d55e07adfd52f24158d0eec85
Chris Hibbert <hibbert@agoric.com>
2022-04-28 15:11:35 -0700

    feature: add a contract that supports voting on economic contracts

---
+++ packages/run-protocol/src/voting.js
@@ -0,0 +1,64 @@
+// @ts-check
+
+import '@agoric/governance/src/exported.js';
+import '@agoric/zoe/exported.js';
+import '@agoric/zoe/src/contracts/exported.js';
+
+import { E } from '@endo/eventual-send';
+
+/**
+ * @file
+ *
+ * This extremely quick and dirty contract simplifies the process of
+ * calling for votes on changes to the economy by gathering the governor creator
+ * facets in one place and invokes `voteOnParamChanges` and
+ * `voteOnApiInvocation` on each when requested. A cleaner version would
+ * validate parameters, constrain deadlines and probably split the ability to
+ * call for vote into separate capabilities for finer grain encapsulation.
+ */
+
+/**
+ * @param {ZCF<{binaryVoteCounterInstallation:Installation}>} zcf
+ * @param {{reserve:GovernedContractFacetAccess<unknown>, amm:GovernedCreatorFacet<unknown>, vaults:GovernedCreatorFacet<unknown>}} privateArgs
+ */
+export const start = async (zcf, privateArgs) => {
+  const { binaryVoteCounterInstallation: counter } = zcf.getTerms();
+  const { reserve, amm, vaults } = privateArgs;
+
+  const voteOnParamChanges = (
+    target,
+    params,
+    path = { paramPath: { key: 'governedApi' } },
+    deadline,
+  ) => {
+    return E(target).voteOnParamChanges(counter, deadline, {
+      paramPath: path,
+      changes: params,
+    });
+  };
+
+  const voteOnApiInvocation = (contract, method, amounts, deadline) => {
+    return E(reserve).voteOnApiInvocation(method, amounts, counter, deadline);
+  };
+
+  const makeNullInvitation = () => {
+    return zcf.makeInvitation(() => {},
+    'identifies the voting contract instance');
+  };
+
+  const publicFacet = harden({
+    voteOnAmmParamChanges: params => voteOnParamChanges(amm, params),
+    voteOnReserveParamChanges: params => voteOnParamChanges(reserve, params),
+    // vote on param changes for a vaultManager
+    voteOnVaultParamChanges: (params, brand) =>
+      voteOnParamChanges(vaults, params, { paramPath: { key: brand } }),
+    // vote on param changes across the vaultFactory
+    voteOnVaultFactoryParamChanges: params =>
+      voteOnParamChanges(vaults, params),
+    voteOnReserveApiInvocation: (amounts, deadline) =>
+      voteOnApiInvocation(reserve, 'addLiquidityToAmmPool', amounts, deadline),
+    makeNullInvitation,
+  });
+
+  return { publicFacet };
+};
