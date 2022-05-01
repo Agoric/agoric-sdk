7d52883844af1a41a0b1095a290069de1470f4f5
Michael FIG <michael+github@fig.org>
2022-05-01 01:05:07 -0600

    feat: extract committee invitation as deploy script

---
+++ packages/run-protocol/scripts/invite-committee-core.js
@@ -0,0 +1,33 @@
+/* global process */
+// @ts-check
+import { makeHelpers } from '@agoric/deploy-script-support';
+
+import { getManifestForInviteCommittee } from '../src/committee-proposal.js';
+
+// Build proposal for sim-chain etc.
+export const defaultProposalBuilder = async ({ publishRef, install }) => {
+  const { ECON_COMMITTEE_ADDRESSES } = process.env;
+
+  assert(ECON_COMMITTEE_ADDRESSES, 'ECON_COMMITTEE_ADDRESES is required');
+  const voterAddresses = JSON.parse(ECON_COMMITTEE_ADDRESSES);
+
+  return harden({
+    sourceSpec: '../src/committee-proposal.js',
+    getManifestCall: [
+      getManifestForInviteCommittee.name,
+      {
+        voterAddresses,
+        votingRef: publishRef(
+          install('../src/voting.js', '../bundles/bundle-voting.js', {
+            persist: true,
+          }),
+        ),
+      },
+    ],
+  });
+};
+
+export default async (homeP, endowments) => {
+  const { writeCoreProposal } = await makeHelpers(homeP, endowments);
+  await writeCoreProposal('gov-invite-committee', defaultProposalBuilder);
+};
---
+++ packages/run-protocol/src/committee-proposal.js
@@ -0,0 +1,108 @@
+// @ts-check
+import { E } from '@endo/far';
+
+// must match packages/wallet/api/src/lib-wallet.js
+const DEPOSIT_FACET = 'depositFacet';
+
+const { values } = Object;
+
+/** @type { <X, Y>(xs: X[], ys: Y[]) => [X, Y][]} */
+const zip = (xs, ys) => xs.map((x, i) => [x, ys[i]]);
+
+/**
+ * @param { import('../src/econ-behaviors').EconomyBootstrapPowers } powers
+ * @param {object} root0
+ * @param {Record<string, string>} root0.voterAddresses
+ * @param root0.options
+ * @param root0.options.voterAddresses
+ */
+export const inviteCommitteeMembers = async (
+  {
+    consume: {
+      zoe,
+      agoricNames,
+      namesByAddress,
+      economicCommitteeCreatorFacet,
+      reserveGovernorCreatorFacet,
+      ammGovernorCreatorFacet,
+      vaultFactoryGovernorCreator,
+    },
+    installation: {
+      consume: { binaryVoteCounter: counterP },
+    },
+  },
+  { options: { voterAddresses } },
+) => {
+  /** @type {[Installation, Installation]} */
+  const [votingInstall, counterInstall] = await Promise.all([
+    E(agoricNames).lookup('installation', 'voting'),
+    counterP,
+  ]);
+  const terms = {
+    binaryVoteCounterInstallation: counterInstall,
+  };
+  const privateFacets = {
+    reserve: reserveGovernorCreatorFacet,
+    amm: ammGovernorCreatorFacet,
+    vaults: vaultFactoryGovernorCreator,
+  };
+  const { publicFacet: votingAPI } = E.get(
+    E(zoe).startInstance(votingInstall, undefined, terms, privateFacets),
+  );
+
+  const invitations = await E(
+    economicCommitteeCreatorFacet,
+  ).getVoterInvitations();
+  assert.equal(invitations.length, values(voterAddresses).length);
+
+  /** @param {[string, Promise<Invitation>]} entry */
+  const distributeInvitation = async ([addr, invitationP]) => {
+    const [voterInvitation, depositFacet] = await Promise.all([
+      invitationP,
+      E(namesByAddress).lookup(addr, DEPOSIT_FACET),
+    ]);
+
+    const nullInvitation = await E(votingAPI).makeNullInvitation();
+    await Promise.all([
+      E(depositFacet).receive(voterInvitation),
+      E(depositFacet).receive(nullInvitation),
+    ]);
+  };
+
+  await Promise.all(
+    zip(values(voterAddresses), invitations).map(distributeInvitation),
+  );
+};
+
+harden(inviteCommitteeMembers);
+
+export const getManifestForInviteCommittee = async (
+  { restoreRef },
+  { voterAddresses, votingRef },
+) => {
+  const t = true;
+  return {
+    manifest: {
+      inviteCommitteeMembers: {
+        consume: {
+          zoe: t,
+          agoricNames: t,
+          namesByAddress: t,
+          economicCommitteeCreatorFacet: t,
+          reserveGovernorCreatorFacet: t,
+          ammGovernorCreatorFacet: t,
+          vaultFactoryGovernorCreator: t,
+        },
+        installation: {
+          consume: { binaryVoteCounter: t },
+        },
+      },
+    },
+    installations: {
+      voting: restoreRef(votingRef),
+    },
+    options: {
+      voterAddresses,
+    },
+  };
+};
