5e84db2fd6ceaf7f11449ed4f630c1b24754ebb8
Dan Connolly <connolly@agoric.com>
2022-05-09 20:11:20 -0500

    fix(agoric-cli): RUN -> IST in chain-config, deploy
     - refactor CENTRAL_DENOM etc. to refer to DENOM_METADATA

--- packages/agoric-cli/src/deploy.js
+++ packages/agoric-cli/src/deploy.js
@@ -284,7 +284,7 @@ export { bootPlugin } from ${JSON.stringify(absPath)};
                * ['namesByAddress']
                * ['namesByAddress', 'agoric1...']
                * ['namesByAddress', 'agoric1...', 'depositFacet']
-               * ['wallet', 'issuer', 'RUN']
+               * ['wallet', 'issuer', 'IST']
                *
                * @param  {...string[]} namePath
                * @returns {Promise<any>}
