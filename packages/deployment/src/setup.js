c3bf7c347d0c998c7a93c626a4747312766f21da
Dan Connolly <connolly@agoric.com>
2022-05-12 22:03:17 -0500

    fix!(deployment): urun -> uist

--- packages/deployment/src/setup.js
+++ packages/deployment/src/setup.js
@@ -1,7 +1,7 @@
 import chalk from 'chalk';
 
 export const ACCOUNT_JSON = `account.json`;
-export const DEFAULT_BOOT_TOKENS = `1000000000000000ubld,10000000000000000urun`;
+export const DEFAULT_BOOT_TOKENS = `1000000000000000ubld,10000000000000000uist`;
 export const PLAYBOOK_WRAPPER = `./ansible-playbook.sh`;
 export const SSH_TYPE = 'ecdsa';
 
