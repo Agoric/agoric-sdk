7baed8ea1c7513d57bd33edb8c4b6a80dd5182ed
Michael FIG <mfig@agoric.com>
2022-05-17 19:33:09 -0600

    fix(slog-to-otel): ignore `vatstoreGetAfter` syscall

--- packages/telemetry/src/slog-to-otel.js
+++ packages/telemetry/src/slog-to-otel.js
@@ -499,6 +499,7 @@ export const makeSlogToOtelKit = (tracer, overrideAttrs = {}) => {
           case 'resolve':
           case 'subscribe':
           case 'vatstoreGet':
+          case 'vatstoreGetAfter':
           case 'vatstoreSet':
           case 'vatstoreDelete':
           case 'dropImports':
