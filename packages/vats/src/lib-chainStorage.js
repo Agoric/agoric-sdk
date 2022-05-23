6ead428a5d0987a4c44a5c18fa7cc63f68b342aa
Richard Gibson <richard.gibson@gmail.com>
2022-05-23 11:40:52 -0400

    chore: Improve child key checks and explanations

--- packages/vats/src/lib-chainStorage.js
+++ packages/vats/src/lib-chainStorage.js
@@ -13,7 +13,7 @@ const pathSegmentPattern = /^[a-zA-Z0-9_]{1,100}$/;
 /**
  * Create a root storage node for a given backing function and root key.
  *
- * @param {(message: any) => any} toStorage a function for sending a message to the storage implementation
+ * @param {(message: any) => any} toStorage a function for sending a storageMessage object to the storage implementation (cf. golang/cosmos/x/swingset/storage.go)
  * @param {string} rootKey
  */
 export function makeChainStorageRoot(toStorage, rootKey) {
@@ -34,26 +34,22 @@ export function makeChainStorageRoot(toStorage, rootKey) {
       },
       setValue(value) {
         assert.typeof(value, 'string');
-        // TODO: Fix on the chain side.
-        // https://github.com/Agoric/agoric-sdk/issues/5381
-        assert(value !== '');
         toStorage({ key, method: 'set', value });
       },
       async delete() {
         assert(key !== rootKey);
-        // A 'set' with no value deletes a key but leaves any descendants.
-        // We therefore want to disallow that (at least for now).
+        // A 'set' with no value deletes a key if it has no children, but
+        // otherwise sets data to the empty string and leaves all nodes intact.
+        // We want to reject silently incomplete deletes (at least for now).
         // This check is unfortunately racy (e.g., a vat could wake up
         // and set data for a child before _this_ vat receives an
-        // already-enqueued no-children response), but we can tolerate
-        // that once https://github.com/Agoric/agoric-sdk/issues/5381
-        // is fixed because then the 'set' message sent after erroneously
-        // passing the no-children check will be transformed into a
-        // set-to-empty which is at least *visibly* not deleted and
-        // indistinguishable from a valid deep-create scenario (in which
-        // parent keys are created automatically).
-        const childKeys = await toStorage({ key, method: 'keys' });
-        if (childKeys.length > 0) {
+        // already-enqueued response claiming no children), but we can tolerate
+        // that because transforming a deletion into a set-to-empty is
+        // effectively indistinguishable from a valid reordering where a fully
+        // successful 'delete' is followed by a child-key 'set' (for which
+        // absent parent keys are automatically created with empty-string data).
+        const childCount = await toStorage({ key, method: 'size' });
+        if (childCount > 0) {
           assert.fail(X`Refusing to delete node with children: ${key}`);
         }
         toStorage({ key, method: 'set' });
