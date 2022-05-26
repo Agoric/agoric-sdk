f95afa558698be8eeb1cf4de89baf08d13c6674e
Richard Gibson <richard.gibson@gmail.com>
2022-05-26 00:03:55 -0400

    feat: Add publishToChainNode finish/fail support

--- packages/vats/src/lib-chainPublish.js
+++ packages/vats/src/lib-chainPublish.js
@@ -1,11 +1,14 @@
 // @ts-check
 
+import { observeIteration } from '@agoric/notifier';
 import { E } from '@endo/far';
 
 /**
  * Publish results of an async iterable into a chain storage node as an array
  * serialized using JSON.stringify or an optionally provided function (e.g.,
  * leveraging a serialize function from makeMarshal).
+ * Array items are possibly-duplicated [index, value] pairs, where index is a
+ * string (ascending numeric or terminal "finish" or "fail").
  *
  * @param {AsyncIterator} iterable
  * @param {ReturnType<typeof import('./lib-chainStorage.js').makeChainStorageRoot>} chainStorageNode
@@ -20,27 +23,35 @@ export async function publishToChainNode(
   let isNewBlock = true;
   let oldResults = [];
   let results = [];
-  for await (const result of iterable) {
-    if (isNewBlock) {
-      isNewBlock = false;
-      oldResults = results;
-      results = [];
-      E(timerService)
-        .delay(1n)
-        .then(() => {
-          isNewBlock = true;
-        });
-    }
-    // To avoid loss when detecting the new block *after* already consuming
-    // results produced within it, we associate each result with an index and
-    // include "old" results in the batch.
-    // Downstream consumers are expected to deduplicate by index.
-    // We represent the index as a string to maintain compatibility with
-    // JSON.stringify and to avoid overly inflating the data size (e.g. with
-    // "@qclass" objects from makeMarshal serialize functions).
-    results.push([String(nextIndex), result]);
-    nextIndex += 1n;
-    const batch = harden(oldResults.slice().concat(results));
-    E(chainStorageNode).setValue(serialize(batch));
-  }
+  const makeAcceptor = forceIndex => {
+    return value => {
+      if (isNewBlock) {
+        isNewBlock = false;
+        oldResults = results;
+        results = [];
+        E(timerService)
+          .delay(1n)
+          .then(() => {
+            isNewBlock = true;
+          });
+      }
+      // To avoid loss when detecting the new block *after* already consuming
+      // results produced within it, we associate each result with an index and
+      // include "old" results in the batch.
+      // Downstream consumers are expected to deduplicate by index.
+      // We represent the index as a string to maintain compatibility with
+      // JSON.stringify and to avoid overly inflating the data size (e.g. with
+      // "@qclass" objects from makeMarshal serialize functions).
+      const index = forceIndex || String(nextIndex);
+      nextIndex += 1n;
+      results.push([index, value]);
+      const batch = harden(oldResults.slice().concat(results));
+      E(chainStorageNode).setValue(serialize(batch));
+    };
+  };
+  await observeIteration(iterable, {
+    updateState: makeAcceptor(),
+    finish: makeAcceptor('finish'),
+    fail: makeAcceptor('fail'),
+  });
 }
