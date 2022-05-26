392f67778dd282032510ff071575cdcdd470556b
Richard Gibson <richard.gibson@gmail.com>
2022-05-26 00:03:01 -0400

    test: Add publishToChainNode finish/fail tests

--- packages/vats/test/test-lib-chainPublish.js
+++ packages/vats/test/test-lib-chainPublish.js
@@ -18,9 +18,10 @@ test('publishToChainNode', async t => {
     supplyIterationResult,
   } = inputs;
   publishToChainNode(customAsyncIterable, storageNode, { timerService });
+  await E(Promise).resolve();
 
   supplyIterationResult('foo');
-  await E(Promise).resolve();
+  await E(Promise).resolve().then();
   t.is(nodeValueHistory.length, 1, 'first result is communicated promptly');
   t.deepEqual(
     nodeValueHistory.pop(),
@@ -29,7 +30,7 @@ test('publishToChainNode', async t => {
   );
 
   supplyIterationResult('bar');
-  await E(Promise).resolve();
+  await E(Promise).resolve().then();
   t.is(nodeValueHistory.length, 1, 'second result is communicated promptly');
   t.deepEqual(
     nodeValueHistory.pop(),
@@ -42,7 +43,7 @@ test('publishToChainNode', async t => {
 
   advanceTimer();
   supplyIterationResult('baz');
-  await E(Promise).resolve();
+  await E(Promise).resolve().then();
   t.is(
     nodeValueHistory.length,
     1,
@@ -62,7 +63,7 @@ test('publishToChainNode', async t => {
   advanceTimer();
   advanceTimer();
   supplyIterationResult('qux');
-  await E(Promise).resolve();
+  await E(Promise).resolve().then();
   t.is(
     nodeValueHistory.length,
     1,
@@ -78,7 +79,7 @@ test('publishToChainNode', async t => {
   );
 
   supplyIterationResult('quux');
-  await E(Promise).resolve();
+  await E(Promise).resolve().then();
   t.is(nodeValueHistory.length, 1);
   t.deepEqual(
     nodeValueHistory.pop(),
@@ -94,7 +95,7 @@ test('publishToChainNode', async t => {
   advanceTimer();
   advanceTimer();
   supplyIterationResult('corge');
-  await E(Promise).resolve();
+  await E(Promise).resolve().then();
   t.is(nodeValueHistory.length, 1);
   t.deepEqual(
     nodeValueHistory.pop(),
@@ -105,6 +106,120 @@ test('publishToChainNode', async t => {
     ],
     'the full previous batch is preserved after multiple timer advances',
   );
+});
+
+test('publishToChainNode captures finish value', async t => {
+  // eslint-disable-next-line no-use-before-define
+  const inputs = getMockInputs(t);
+  const {
+    // external facets
+    storageNode,
+    timerService,
+    customAsyncIterable,
+    // internal facets
+    nodeValueHistory,
+    supplyIterationResult,
+  } = inputs;
+  publishToChainNode(customAsyncIterable, storageNode, { timerService });
+  await E(Promise).resolve();
+
+  supplyIterationResult.finish('foo');
+  await E(Promise).resolve().then();
+  t.is(nodeValueHistory.length, 1, 'finish is communicated promptly');
+  t.deepEqual(
+    nodeValueHistory.pop(),
+    [['finish', 'foo']],
+    'result has index "finish"',
+  );
+});
+
+test('publishToChainNode captures fail value', async t => {
+  // eslint-disable-next-line no-use-before-define
+  const inputs = getMockInputs(t);
+  const {
+    // external facets
+    storageNode,
+    timerService,
+    customAsyncIterable,
+    // internal facets
+    nodeValueHistory,
+    supplyIterationResult,
+  } = inputs;
+  publishToChainNode(customAsyncIterable, storageNode, { timerService });
+  await E(Promise).resolve();
+
+  supplyIterationResult.fail('foo');
+  await E(Promise).resolve().then();
+  t.is(nodeValueHistory.length, 1, 'fail is communicated promptly');
+  t.deepEqual(
+    nodeValueHistory.pop(),
+    [['fail', 'foo']],
+    'result has index "fail"',
+  );
+});
+
+test('publishToChainNode custom serialization', async t => {
+  // eslint-disable-next-line no-use-before-define
+  const inputs = getMockInputs(t);
+  const {
+    // external facets
+    storageNode,
+    timerService,
+    customAsyncIterable,
+    // internal facets
+    nodeValueHistory,
+    supplyIterationResult,
+  } = inputs;
+  const serializeInputHistory = [];
+  const serialize = val => {
+    const length = serializeInputHistory.push(val);
+    return String(length);
+  };
+  publishToChainNode(customAsyncIterable, storageNode, {
+    timerService,
+    serialize,
+  });
+  await E(Promise).resolve();
+
+  supplyIterationResult('foo');
+  await E(Promise).resolve().then();
+  t.is(serializeInputHistory.length, 1, 'first result is serialized promptly');
+  t.deepEqual(
+    serializeInputHistory[0],
+    [['0', 'foo']],
+    'first result has index 0',
+  );
+  t.is(nodeValueHistory.length, 1, 'first result is communicated promptly');
+  t.is(nodeValueHistory[0], 1, 'first result is output from serialize()');
+
+  supplyIterationResult('bar');
+  await E(Promise).resolve().then();
+  t.is(serializeInputHistory.length, 2, 'second result is serialized promptly');
+  t.deepEqual(
+    serializeInputHistory[1],
+    [
+      ['0', 'foo'],
+      ['1', 'bar'],
+    ],
+    'second result is appended',
+  );
+  t.is(nodeValueHistory.length, 2, 'second result is communicated promptly');
+  t.is(nodeValueHistory[1], 2, 'second result is output from serialize()');
+
+  supplyIterationResult.finish('baz');
+  await E(Promise).resolve().then();
+  t.is(serializeInputHistory.length, 3, 'finish result is serialized promptly');
+  t.deepEqual(
+    serializeInputHistory[2],
+    [
+      ['0', 'foo'],
+      ['1', 'bar'],
+      ['finish', 'baz'],
+    ],
+    'finish result is appended',
+  );
+  t.is(nodeValueHistory.length, 3, 'finish result is communicated promptly');
+  t.is(nodeValueHistory[2], 3, 'finish result is output from serialize()');
 });
 
 // TODO: Move to a testing library.
@@ -134,24 +249,38 @@ function getMockInputs(t) {
   };
 
   // Create an async iterable with a function for supplying results.
-  let nextResultP;
-  let nextResultR = _result => {};
-  const supplyIterationResult = result => {
-    nextResultR(result);
-    nextResultP = new Promise(resolve => {
-      nextResultR = resolve;
+  const pendingResults = [{ resolve() {} }];
+  const supplyIterationResult = value => {
+    const nextDeferred = {};
+    nextDeferred.promise = new Promise((resolve, reject) => {
+      nextDeferred.resolve = resolve;
+      nextDeferred.reject = reject;
     });
+    const newLength = pendingResults.push(nextDeferred);
+    pendingResults[newLength - 2].resolve({ value });
+  };
+  supplyIterationResult.finish = value => {
+    pendingResults[pendingResults.length - 1].resolve({ value, done: true });
+  };
+  supplyIterationResult.fail = value => {
+    pendingResults[pendingResults.length - 1].reject(value);
   };
   const customAsyncIterable = (async function* makeCustomAsyncIterable() {
     while (true) {
       // eslint-disable-next-line no-await-in-loop
-      yield await nextResultP;
+      const iterationResult = await pendingResults[0].promise;
+      pendingResults.shift();
+      if (iterationResult.done) {
+        return iterationResult.value;
+      }
+      yield iterationResult.value;
     }
   })();
 
   // Initialize.
   advanceTimer();
   supplyIterationResult();
+  pendingResults.shift();
 
   return {
     // external facets
