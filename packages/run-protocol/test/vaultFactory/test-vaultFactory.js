0462363789d0a5a4e4432a9001170d8afa1813b1
Turadg Aleahmad <turadg@agoric.com>
2022-06-08 09:42:38 -0700

    chore: remove superfluous await

--- packages/run-protocol/test/vaultFactory/test-vaultFactory.js
+++ packages/run-protocol/test/vaultFactory/test-vaultFactory.js
@@ -2566,7 +2566,7 @@ test('manager notifiers', async t => {
     totalOverageReceived: { value: totalOverageReceived },
     totalProceedsReceived: { value: totalProceedsReceived },
   });
-  await m.assertFullyLiquidated();
+  m.assertFullyLiquidated();
 
   trace('4. Make another LOAN1 loan');
   vaultSeat = await E(services.zoe).offer(
@@ -2625,7 +2625,7 @@ test('manager notifiers', async t => {
     totalCollateral: { value: 0n },
     totalProceedsReceived: { value: totalProceedsReceived },
   });
-  await m.assertFullyLiquidated();
+  m.assertFullyLiquidated();
 
   trace('7. Make another LOAN2 loan');
   vaultSeat = await E(services.zoe).offer(
@@ -2725,7 +2725,7 @@ test('manager notifiers', async t => {
       value: DEBT1 + DEBT2 + interestAccrued - nextProceeds - 53n - 1n, // compensate for previous proceeds and rounding
     },
   });
-  await m.assertFullyLiquidated();
+  m.assertFullyLiquidated();
   const leftoverDebt = DEBT1 - nextProceeds + interestAccrued;
 
   trace('11. Create a loan with ample collateral');
