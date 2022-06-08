9cd8d405cb35f6baea83b0d8a8128e0bd87a0e2e
Michael FIG <mfig@agoric.com>
2022-06-08 17:39:37 -0600

    feat(web-components): provide `makeDefaultLeader` for Casting

--- packages/web-components/src/AgoricWalletConnection.js
+++ packages/web-components/src/AgoricWalletConnection.js
@@ -4,6 +4,7 @@ import { html, css, LitElement } from 'lit';
 
 import { assert, details as X } from '@agoric/assert';
 import { makeCache } from '@agoric/cache';
+import { makeLeader } from '@agoric/casting/src/leader-netconfig.js';
 import { makeCapTP as defaultMakeCapTP } from '@endo/captp';
 import { E } from '@endo/eventual-send';
 import { Far } from '@endo/marshal';
@@ -67,9 +68,17 @@ export const makeAgoricWalletConnection = (makeCapTP = defaultMakeCapTP) =>
       // Just make sure the reconnection logic is triggered.
       this._bridgePK = makePromiseKit();
       this._cache = undefined;
+      this._makeDefaultLeader = undefined;
 
       this.service.send({ type: 'reset' });
       this.isResetting = false;
+    }
+
+    get makeDefaultLeader() {
+      if (!this._makeDefaultLeader) {
+        this._makeDefaultLeader = leaderOptions => makeLeader(leaderOptions);
+      }
+      return this._makeDefaultLeader;
     }
 
     get cache() {
@@ -78,8 +87,7 @@ export const makeAgoricWalletConnection = (makeCapTP = defaultMakeCapTP) =>
         return this._cache;
       }
       const cache = makeCache(E(this._bridgePK.promise).getCacheCoordinator());
-      this._cache = (key, update, ...optGuardPattern) =>
-        cache(key, update, ...optGuardPattern);
+      this._cache = cache;
       return this._cache;
     }
 
@@ -152,6 +160,7 @@ export const makeAgoricWalletConnection = (makeCapTP = defaultMakeCapTP) =>
             state: this.machine.state.name,
             walletConnection: this.walletConnection,
             cache: this.cache,
+            makeDefaultLeader: this.makeDefaultLeader,
           },
         });
         this.dispatchEvent(ev);
