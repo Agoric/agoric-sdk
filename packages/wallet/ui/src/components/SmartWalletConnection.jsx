f3436f158f13a82dbd01721ff9425a40e7f50919
Michael FIG <mfig@agoric.com>
2022-06-14 13:38:48 -0600

    wip: follow casting from wallet ui

--- packages/wallet/ui/src/components/SmartWalletConnection.jsx
+++ packages/wallet/ui/src/components/SmartWalletConnection.jsx
@@ -1,3 +1,10 @@
+import {
+  iterateLatest,
+  makeFollower,
+  makeLeader,
+  makeCastingSpec,
+} from '@agoric/casting';
+
 import React, { useEffect, useState } from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
@@ -9,6 +16,8 @@ const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
+const DEFAULT_WALLET_CASTING_SPEC =
+  ':published.wallet.agoric1t7pdlnwwzf3yd62zajwpt7qc9ylz7dqs8x3zqd';
 const SmartWalletConnection = ({ walletConnection }) => {
   const [snackbarMessage, setSnackbarMessage] = useState(null);
 
@@ -27,32 +36,68 @@ const SmartWalletConnection = ({ walletConnection }) => {
     }
     setSnackbarMessage(message);
   };
+  const showReadOnlyError = (message, e) =>
+    showError(`READ-ONLY: ${message} for write access`, e);
 
   useEffect(() => {
     if (!walletConnection) {
       return () => {};
     }
     let outdated;
+
+    const followWallet = async () => {
+      // TODO: Persist this in localStorage from settings page.
+      const { url, walletSpec = DEFAULT_WALLET_CASTING_SPEC } =
+        walletConnection;
+
+      const leader = makeLeader(url);
+      const castingSpec = makeCastingSpec(walletSpec);
+      const follower = makeFollower(leader, castingSpec);
+
+      for await (const { value } of iterateLatest(follower)) {
+        if (outdated) {
+          return;
+        }
+        console.log(`here's a mailbox value`, value);
+      }
+      throw Error(`unexpected end of ${walletSpec} follower`);
+    };
+    followWallet().catch(e =>
+      showError('Cannot read Smart Wallet state casting', e),
+    );
+
+    return () => {
+      outdated = true;
+    };
+  }, [walletConnection]);
+
+  useEffect(() => {
+    if (!walletConnection || true) {
+      return () => {};
+    }
+    let outdated;
     const connect = async () => {
       if (!window.getOfflineSigner || !window.keplr) {
-        showError('Please install the Keplr extension');
+        showReadOnlyError('Please install the Keplr extension');
       } else if (window.keplr.experimentalSuggestChain) {
         try {
           const client = await suggestChain(walletConnection.url);
           console.log('got stargate client', client);
 
           // TODO: Would enable Keplr for transacting the Agoric client.
-          showError('Would enable transaction manager');
+          showReadOnlyError('Would enable transaction manager');
         } catch (e) {
           if (!outdated) {
-            showError('Failed to connect to Keplr', e);
+            showReadOnlyError('Failed to connect to Keplr', e);
           }
         }
       } else {
-        showError('Please use the most recent version of the Keplr extension');
+        showReadOnlyError(
+          'Please use the most recent version of the Keplr extension',
+        );
       }
     };
-    connect().catch(e => showError('Failed to connect to Keplr', e));
+    connect().catch(e => showReadOnlyError('Failed to connect to Keplr', e));
 
     return () => {
       outdated = true;
@@ -61,11 +106,7 @@ const SmartWalletConnection = ({ walletConnection }) => {
 
   return (
     <div>
-      <Snackbar
-        open={snackbarMessage !== null}
-        autoHideDuration={6000}
-        onClose={handleSnackbarClose}
-      >
+      <Snackbar open={snackbarMessage !== null} onClose={handleSnackbarClose}>
         <Alert
           onClose={handleSnackbarClose}
           severity="error"
