9c3bdfba27a2ce8cd8eff846d20ae33cde869a59
Michael FIG <mfig@agoric.com>
2022-06-16 09:07:45 -0600

    fixup! feat(wallet-ui): enable service worker caching

--- packages/wallet/ui/src/util/Icons.js
+++ packages/wallet/ui/src/util/Icons.js
@@ -1,9 +1,6 @@
-export const icons = {
-  RUN: 'tokens/RUN.svg',
-  BLD: 'tokens/BLD.svg',
-  ATOM: 'tokens/cosmos.svg',
-  LINK: 'tokens/chainlink.png',
-  USDC: 'tokens/usdc.svg',
-};
-
-export const defaultIcon = 'tokens/generic.ico';
+export { default as RUN } from '../tokens/RUN.svg';
+export { default as BLD } from '../tokens/BLD.svg';
+export { default as ATOM } from '../tokens/cosmos.svg';
+export { default as LINK } from '../tokens/chainlink.png';
+export { default as USDC } from '../tokens/usdc.svg';
+export { default } from '../tokens/generic.ico';
