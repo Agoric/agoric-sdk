1a4ebdf189b20313512d8459554cde8656414c4a
Michael FIG <mfig@agoric.com>
2022-06-17 06:31:13 -0600

    wip

--- packages/casting/src/leader.js
+++ packages/casting/src/leader.js
@@ -1,86 +1,44 @@
 // @ts-check
-import { E, Far } from '@endo/far';
+import { Far } from '@endo/far';
 import { DEFAULT_RETRY_CALLBACK, DEFAULT_JITTER } from './defaults.js';
-import { shuffle } from './shuffle.js';
 import { makePollingChangeFollower } from './change-follower.js';
+import { makeRoundRobinEndpointMapper } from './round-robin-mapper.js';
 
 /**
- * Create a chain leader that rotates through a list of endpoints.
  *
- * @param {string[]} endpoints
- * @param {import('./types.js').LeaderOptions} leaderOptions
+ * @param {import('./types').LeaderOptions} leaderOptions
+ * @returns {import('./types').Leader}
  */
-export const makeRoundRobinLeader = (endpoints, leaderOptions = {}) => {
+export const makeCosmJSLeader = leaderOptions => {
   const { retryCallback = DEFAULT_RETRY_CALLBACK, jitter = DEFAULT_JITTER } =
     leaderOptions;
 
-  // Shuffle the RPC addresses, so that we don't always hit the same one as all
-  // our peers.
-  shuffle(endpoints);
-
-  let lastRespondingEndpointIndex = 0;
-  let thisAttempt = 0;
-  let retrying;
-
   /** @type {import('./types.js').Leader} */
-  const leader = Far('round robin leader', {
+  const leader = Far('cosmjs leader', {
     getOptions: () => leaderOptions,
-    jitter: async where => jitter && jitter(where),
+    makeClient: clientOptions => {
+      const { mnemonic, keplr } = clientOptions;
+      if (mnemonic) {
+        return makeCosmJsMnemonicClient(leader, clientOptions);
+      }
+      if (keplr) {
+        return makeCosmJsKeplrClient(leader, clientOptions);
+      }
+      return makeCosmJsReadonlyClient(leader, clientOptions);
+    },
+    jitter: where => jitter && jitter(where),
     retry: async (where, err, attempt) => {
       if (retryCallback) {
         return retryCallback(where, err, attempt);
       }
       throw err;
     },
-    // eslint-disable-next-line no-use-before-define
-    watchCasting: _castingSpecP => pollingChangeFollower,
-    /**
-     * @template T
-     * @param {string} where
-     * @param {(endpoint: string) => Promise<T>} callback
-     */
-    mapEndpoints: async (where, callback) => {
-      where = `${where} (round-robin endpoints)`;
-      /** @type {Promise<T[]>} */
-      const p = new Promise((resolve, reject) => {
-        let endpointIndex = lastRespondingEndpointIndex;
-
-        const retry = err => {
-          if (!retrying) {
-            const attempt = thisAttempt;
-            retrying = E(leader)
-              .retry(where, err, attempt)
-              .then(() => {
-                endpointIndex = (endpointIndex + 1) % endpoints.length;
-                retrying = null;
-              });
-          }
-
-          retrying
-            .then(() => jitter && jitter(where))
-            // eslint-disable-next-line no-use-before-define
-            .then(applyOne, reject);
-          thisAttempt += 1;
-        };
-
-        const applyOne = () => {
-          Promise.resolve()
-            .then(() => callback(endpoints[endpointIndex]))
-            .then(res => {
-              resolve(harden([res]));
-              lastRespondingEndpointIndex = endpointIndex;
-              thisAttempt = 0;
-            }, retry);
-
-          // Don't return to prevent a promise chain.
-        };
-
-        applyOne();
-      });
-      return p;
+    makeWatcher: castingSpec => makePollingChangeFollower(leader, castingSpec),
+    mapEndpoints: (where, callback) => {
+      const mapper = makeRoundRobinEndpointMapper(leader, endpoints);
+      return mapper(callback);
     },
   });
 
-  const pollingChangeFollower = makePollingChangeFollower(leader);
   return leader;
 };
