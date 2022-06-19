59403d2e8ac28aa6dc325fa685f6efcab0d489e0
Chris Hibbert <hibbert@agoric.com>
2022-06-19 09:20:02 -0700

    test: add back the ertpService test

---
+++ packages/ERTP/test/swingsetTests/ertpService/test-ertp-service-upgrade.js
@@ -0,0 +1,53 @@
+// eslint-disable-next-line import/order
+import { test } from '@agoric/swingset-vat/tools/prepare-test-env-ava.js';
+
+// eslint-disable-next-line import/order
+import { assert } from '@agoric/assert';
+import { buildVatController } from '@agoric/swingset-vat';
+
+const bfile = name => new URL(name, import.meta.url).pathname;
+
+test('ertp service upgrade', async t => {
+  const config = {
+    // includeDevDependencies: true, // for vat-data
+    defaultManagerType: 'local', // 'xs-worker',
+    bootstrap: 'bootstrap',
+    // defaultReapInterval: 'never',
+    // defaultReapInterval: 1,
+    vats: {
+      bootstrap: { sourceSpec: bfile('bootstrap-ertp-service-upgrade.js') },
+    },
+    bundles: {
+      ertpService: { sourceSpec: bfile('vat-ertp-service.js') },
+    },
+    enableFakeDurable: true,
+  };
+
+  const c = await buildVatController(config);
+  c.pinVatRoot('bootstrap');
+  await c.run();
+
+  const run = async (name, args = []) => {
+    assert(Array.isArray(args));
+    const kpid = c.queueToVatRoot('bootstrap', name, args);
+    await c.run();
+    const status = c.kpStatus(kpid);
+    console.log(`${status}`);
+    const capdata = c.kpResolution(kpid);
+    return [status, capdata];
+  };
+
+  // create initial version
+  const [v1status] = await run('buildV1', []);
+  t.is(v1status, 'fulfilled');
+
+  // now perform the upgrade
+  console.log(`-- starting upgradeV2`);
+
+  const [v2status] = await run('upgradeV2', []);
+
+  // this will fail until we get rid of all dependence on fake durable, but the
+  // fact that v1status was 'fulfilled' means that we successfully saved state
+  t.is(v2status, 'rejected');
+  // t.is(v2status, 'fulfilled');
+});
