0110b4716dd6ba6bf3de8c84bc4542ec17711bbd
Dan Connolly <connolly@agoric.com>
2022-06-23 23:40:26 -0500

    refactor(wallet/ui): take advantage of ev type
     - JSON.stringify is more familiar

--- packages/wallet/ui/src/components/WalletConnection.jsx
+++ packages/wallet/ui/src/components/WalletConnection.jsx
@@ -87,18 +87,11 @@ const WalletConnection = ({
       removeEventListener,
     } = window; // WARNING: ambient
 
-    /** @param {StorageEvent} ev */
-    function handleStorageMessage(ev) {
-      const { parse, stringify } = JSON;
-      const { key, newValue } = ev;
-      // removeItem causes an event where newValue is null
-      if (!key || !newValue) {
-        return;
-      }
-      const keyParts = parse(key);
+    function handleStorageMessage(key, newValue) {
+      const keyParts = JSON.parse(key);
       assert(Array.isArray(keyParts));
       const [tag, origin, epoch, _ix] = /** @type {unknown[]} */ (keyParts);
-      const payload = parse(newValue);
+      const payload = JSON.parse(newValue);
       if (tag !== 'out' || !payload || typeof payload.type !== 'string') {
         return;
       }
@@ -108,7 +101,7 @@ const WalletConnection = ({
         ...payload,
         dappOrigin: origin,
       };
-      const dappKey = stringify([origin, epoch]);
+      const dappKey = JSON.stringify([origin, epoch]);
       /** @type {ReturnType<typeof makeCapTP>}  */
       let conn;
       /** @type {number} */
@@ -120,8 +113,8 @@ const WalletConnection = ({
         const send = payloadOut => {
           console.debug('WalletConnect: message -> storage', payloadOut);
           storage.setItem(
-            stringify(['in', origin, epoch, ix]),
-            stringify(payloadOut),
+            JSON.stringify(['in', origin, epoch, ix]),
+            JSON.stringify(payloadOut),
           );
           ix += 1; // ISSUE: overflow?
         };
@@ -136,7 +129,13 @@ const WalletConnection = ({
       conn.dispatch(obj);
       storage.removeItem(key);
     }
-    addEventListener('storage', handleStorageMessage);
+    addEventListener('storage', ev => {
+      const { key, newValue } = ev;
+      // removeItem causes an event where newValue is null
+      if (key && newValue) {
+        handleStorageMessage(key, newValue);
+      }
+    });
 
     return () => {
       cancelled = true;
