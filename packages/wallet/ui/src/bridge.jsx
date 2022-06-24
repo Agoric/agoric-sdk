e33468214e3a5a3830e21d47d30a4df0e8761f2a
Dan Connolly <connolly@agoric.com>
2022-06-23 23:41:32 -0500

    chore(wallet/ui): use guesture to grant storage access

--- packages/wallet/ui/src/bridge.jsx
+++ packages/wallet/ui/src/bridge.jsx
@@ -6,6 +6,8 @@ import './lockdown.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import logo from './Agoric-logo-color.svg';
+
 Error.stackTraceLimit = Infinity;
 
 /** ISSUE: where are these defined? */
@@ -19,37 +21,27 @@ const BridgeProtocol = /** @type {const} */ ({
  * the dApp are forwarded to localStorage and vice versa.
  *
  * @param {{
- *   sameParent: () => boolean,
  *   addEventListener: typeof window.addEventListener,
- *   storage: typeof window.localStorage,
  *   parentPost: typeof window.postMessage,
  *   t0: number,
  * }} io
  */
-const installDappConnection = async ({
-  addEventListener,
-  sameParent,
-  parentPost,
-  storage,
-  t0,
-}) => {
+const installDappConnection = ({ addEventListener, parentPost, t0 }) => {
   /** @type { string } */
   let origin;
+  let setItem;
+  const buffer = [];
 
-  console.debug('bridge: installDappConnection');
-
-  if (sameParent()) {
-    // console.debug('bridge: ignored message to self');
-    return;
-  }
-
-  parentPost({ type: BridgeProtocol.loaded }, '*');
-  parentPost({ type: BridgeProtocol.opened }, '*');
+  console.debug('bridge: installDappConnection', new Date(t0).toISOString());
 
   let outIx = 0; // TODO: persist index in storage
-  /** @param {Record<string, any>} payload */
-
   const { stringify, parse } = JSON;
+  /** @param {Record<string, any>} payload */
+  const send = payload => {
+    // console.debug('bridge: send', payload);
+    setItem(stringify(['out', origin, t0, outIx]), stringify(payload));
+    outIx += 1; // ISSUE: overflow?
+  };
 
   addEventListener('message', ev => {
     // console.debug('bridge: handling message:', ev.data);
@@ -66,36 +58,102 @@ const installDappConnection = async ({
       // console.debug('bridge: setting origin to', origin);
       origin = ev.origin;
     }
-    console.debug('bridge: message from dapp->localStorage', origin, ev.data);
-    storage.setItem(stringify(['out', origin, t0, outIx]), stringify(ev.data));
-    outIx += 1; // ISSUE: overflow?
+    if (setItem) {
+      console.debug('bridge: message from dapp->localStorage', origin, ev.data);
+      send(ev.data);
+    } else {
+      buffer.push(ev.data);
+      outIx += 1; // ISSUE: overflow?
+    }
   });
 
-  addEventListener('storage', ev => {
-    // console.debug('from storage', origin, ev.key, ev.newValue);
-    if (!ev.key || !ev.newValue) {
-      return;
-    }
-    const [tag, targetOrigin, targetT, _keyIx] = JSON.parse(ev.key); // or throw
-    if (tag !== 'in' || targetOrigin !== origin || targetT !== t0) {
-      return;
-    }
-    const payload = parse(ev.newValue); // or throw
-    storage.removeItem(ev.key);
-    console.debug('bridge: storage -> message to dapp', origin, payload);
-    parentPost(payload, '*');
-  });
+  // ISSUE: this fires much earlier than expected.
+  // addEventListener('beforeunload', () => {
+  //   const bye = { type: 'CTP_DISCONNECT', reason: `bye from ${origin}` };
+  //   parentPost(bye, '*');
+  // });
+
+  console.debug('bridge: Start the flow of messages.');
+  parentPost({ type: BridgeProtocol.loaded }, '*');
+  // ISSUE: wallet-bridge.html waits for the websocket side to open
+  // before sending this, but if we wait for the storage side to open,
+  // it doesn't seem to work.
+  parentPost({ type: BridgeProtocol.opened }, '*');
+
+  return {
+    /** @param {typeof window.localStorage} storage */
+    connectStorage: storage => {
+      if (setItem) {
+        return;
+      }
+
+      addEventListener('storage', ev => {
+        // console.debug('from storage', origin, ev.key, ev.newValue);
+        if (!ev.key || !ev.newValue) {
+          return;
+        }
+        const [tag, targetOrigin, targetT, _keyIx] = JSON.parse(ev.key); // or throw
+        if (tag !== 'in' || targetOrigin !== origin || targetT !== t0) {
+          return;
+        }
+        const payload = parse(ev.newValue); // or throw
+        storage.removeItem(ev.key);
+        console.debug('bridge: storage -> message to dapp', origin, payload);
+        parentPost(payload, '*');
+      });
+      setItem = (k, v) => storage.setItem(k, v);
+      if (buffer.length) {
+        console.debug('sending', buffer.length, 'queued messages from', origin);
+      }
+      while (buffer.length) {
+        send(buffer.shift());
+      }
+    },
+  };
 };
 
-installDappConnection({
+const conn = installDappConnection({
   addEventListener: window.addEventListener,
-  sameParent: () => window.parent === window,
-  parentPost: (payload, origin) => window.parent.postMessage(payload, origin),
-  storage: window.localStorage,
+  parentPost: (payload, origin) =>
+    window.parent !== window
+      ? window.parent.postMessage(payload, origin)
+      : undefined,
   t0: Date.now(),
 });
 
+const signalWallet = () => {
+  if ('requestStorageAccess' in document) {
+    document
+      .requestStorageAccess()
+      .then(() => {
+        console.debug('bridge: storage access granted');
+        conn.connectStorage(window.localStorage);
+      })
+      .catch(whyNot =>
+        console.error('bridge: requestStorageAccess rejected with', whyNot),
+      );
+  } else {
+    console.debug('bridge: SKIP document.requestStorageAccess');
+    conn.connectStorage(window.localStorage);
+  }
+
+  // TODO: get URL from local.agoric.com
+  // use 'locating' protocol from packages/web-components/src/AgoricWalletConnection.js
+  // onLocateMessage
+
+  // ISSUE: is this causing the wallet bridge to reload???
+  const walletAddress = 'http://localhost:8002/wallet/'; // ISSUE: how to get this???
+  window.open(walletAddress, 'target_agoric_wallet');
+};
+
+// TODO: pulse as state
+// TODO: logo centered; aspect ratio preserved; no iframe scrollbar
 ReactDOM.render(
-  <button id="signalBridge">Signal Bridge</button>,
+  <img
+    className="pulse logo expand100"
+    src={logo}
+    onClick={signalWallet}
+    alt="Agoric logo"
+  />,
   document.getElementById('root'),
 );
