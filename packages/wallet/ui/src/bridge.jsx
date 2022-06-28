49ad042ce2d5da99de665e258094b8b901f4b862
Dan Connolly <connolly@agoric.com>
2022-06-28 09:12:18 -0500

    chore: delay storage listener until user requests

--- packages/wallet/ui/src/bridge.jsx
+++ packages/wallet/ui/src/bridge.jsx
@@ -20,19 +20,18 @@ const BridgeProtocol = /** @type {const} */ ({
  *
  * @param {{
  *   addEventListener: typeof window.addEventListener,
- *   storage: typeof window.localStorage,
  *   parentPost: typeof window.postMessage,
  *   t0: number,
  * }} io
  */
-const installDappConnection = async ({
-  addEventListener,
-  parentPost,
-  storage,
-  t0,
-}) => {
+const installDappConnection = ({ addEventListener, parentPost, t0 }) => {
   /** @type { string } */
   let origin;
+
+  /** @type {(key: string, value: string) => void} */
+  let setItem;
+  /** @type {string[]} */
+  const buffer = [];
 
   console.debug('bridge: installDappConnection');
 
@@ -59,24 +58,44 @@ const installDappConnection = async ({
       // console.debug('bridge: setting origin to', origin);
       origin = ev.origin;
     }
-    console.debug('bridge: message from dapp->localStorage', origin, ev.data);
-    storage.setItem(stringify(['out', origin, t0, outIx]), stringify(ev.data));
+    if (setItem) {
+      console.debug('bridge: message from dapp->localStorage', origin, ev.data);
+      setItem(stringify(['out', origin, t0, outIx]), stringify(ev.data));
+    } else {
+      console.debug('bridge: message from dapp->buffer', origin, ev.data);
+      buffer.push(harden(ev.data));
+    }
     outIx += 1; // ISSUE: overflow?
   });
 
-  addEventListener('storage', ev => {
-    // console.debug('from storage', origin, ev.key, ev.newValue);
-    if (!ev.key || !ev.newValue) {
-      return;
-    }
-    const [tag, targetOrigin, targetT, _keyIx] = JSON.parse(ev.key); // or throw
-    if (tag !== 'in' || targetOrigin !== origin || targetT !== t0) {
-      return;
-    }
-    const payload = parse(ev.newValue); // or throw
-    storage.removeItem(ev.key);
-    console.debug('bridge: storage -> message to dapp', origin, payload);
-    parentPost(payload, '*');
+  return harden({
+    /** @param {typeof window.localStorage} storage */
+    connectStorage: storage => {
+      addEventListener('storage', ev => {
+        // console.debug('from storage', origin, ev.key, ev.newValue);
+        if (!ev.key || !ev.newValue) {
+          return;
+        }
+        const [tag, targetOrigin, targetT, _keyIx] = JSON.parse(ev.key); // or throw
+        if (tag !== 'in' || targetOrigin !== origin || targetT !== t0) {
+          return;
+        }
+        const payload = parse(ev.newValue); // or throw
+        storage.removeItem(ev.key);
+        console.debug('bridge: storage -> message to dapp', origin, payload);
+        parentPost(payload, '*');
+      });
+      setItem = (key, value) => storage.setItem(key, value);
+      if (buffer.length) {
+        console.debug('sending', buffer.length, 'queued messages from', origin);
+        while (buffer.length) {
+          setItem(
+            stringify(['out', origin, t0, outIx]),
+            stringify(buffer.shift()),
+          );
+        }
+      }
+    },
   });
 };
 
@@ -91,14 +110,19 @@ const installDappConnection = async ({
 //   return false;
 // };
 
-installDappConnection({
+const conn = installDappConnection({
   addEventListener: window.addEventListener,
   parentPost: (payload, origin) => window.parent.postMessage(payload, origin),
-  storage: window.localStorage,
   t0: Date.now(),
 });
 
+const signalBridge = () => {
+  conn.connectStorage(window.localStorage);
+};
+
 ReactDOM.render(
-  <button id="signalBridge">Signal Bridge</button>,
+  <button id="signalBridge" onClick={signalBridge}>
+    Signal Bridge
+  </button>,
   document.getElementById('root'),
 );
