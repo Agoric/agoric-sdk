3a856b67bc70cab980ccdcbf3de2fcbd48a58876
Mark S. Miller <erights@gmail.com>
2022-06-28 12:59:02 -0700

    fix: copy and use objectMap with note

--- packages/vat-data/src/kind-utils.js
+++ packages/vat-data/src/kind-utils.js
@@ -16,6 +16,29 @@ export const provideKindHandle = (baggage, kindName) =>
 harden(provideKindHandle);
 
 /**
+ * By analogy with how `Array.prototype.map` will map the elements of
+ * an array to transformed elements of an array of the same shape,
+ * `objectMap` will do likewise for the string-named own enumerable
+ * properties of an object.
+ *
+ * See the comment at
+ * https://github.com/Agoric/agoric-sdk/pull/5674#discussion_r908883510
+ * TODO need to move this to a common place.
+ *
+ * @template T, U
+ * @template {keyof T} K
+ * @param {{ [K2 in keyof T]: T[K2] }} original
+ * @param {(pair: [K, T[K]]) => [K, U]} mapPairFn
+ * @returns {Record<K, U>}
+ */
+export const objectMap = (original, mapPairFn) => {
+  const ents = /** @type {[K, T[K]][]} */ (Object.entries(original));
+  const mapEnts = ents.map(ent => mapPairFn(ent));
+  // @ts-expect-error TODO statically recognize harden
+  return /** @type {Record<K, U>} */ (harden(Object.fromEntries(mapEnts)));
+};
+
+/**
  * @template T
  * @param {import('@agoric/store').MapStore<string, unknown>} baggage
  * @param {string} kindName
@@ -25,9 +48,7 @@ harden(provideKindHandle);
  */
 export const ProvideFar = (baggage, kindName, methods, options = undefined) => {
   const kindHandle = provideKindHandle(baggage, kindName);
-  const behavior = fromEntries(
-    entries(methods).map(([k, m]) => [k, dropContext(m)]),
-  );
+  const behavior = objectMap(methods, ([k, m]) => [k, dropContext(m)]);
   const makeSingleton = defineDurableKind(
     kindHandle,
     () => ({}),
