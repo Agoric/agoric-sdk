766a220c60226e4446a9b37c7927b616de13f2d9
Dan Connolly <connolly@agoric.com>
2022-07-12 23:13:25 -0500

    feat(demo-signing): sign and broadcast WalletAction
    without ledger
    
     - toward keplr/ledger demo: copy SuggestChain.js
     - sign simple msg/doc with keplr/ledger (WIP)
       - ISSUE: started with cosmjs API integration but
         couldn't get it to work without skipping it
     - chore(demo-signing): figured out offlignSigner.signAmino() API
       no longer skipping all the way to keplr.signAmino()
     - feat(demo-signing): sign WALLET_ACTION with keplr, ledger
     - send signed transaction to chain (WIP)
       - can connect to chain and query for sequence
       - aminoTypes seem to work
       - TODO: generate protobuf codec for MsgWalletAction
     - pass connectWithSigner authority explicitly
     - integrate MsgWalletAction proto stub via registry
     - MsgWalletAction.owner is bytes, not string
    
    provide enough gas
    
    avoid:
    
    Error: Broadcasting transaction failed with code 11 (codespace: sdk). Log: out of gas in location: ReadFlat; gasWanted: 250, gasUsed: 1000: out of gas
        at SigningStargateClient.broadcastTx (stargateclient.ts:413:9)
        at async HTMLButtonElement.<anonymous> (keplr-ledger-demo.js:215:16)

---
+++ packages/wallet/ui/demo-signing/src/keplr-ledger-demo.js
@@ -0,0 +1,273 @@
+// @ts-check
+import {
+  AminoTypes,
+  SigningStargateClient,
+  defaultRegistryTypes,
+} from '@cosmjs/stargate';
+import { fromBech32, toBech32, fromBase64, toBase64 } from '@cosmjs/encoding';
+import { Registry } from '@cosmjs/proto-signing';
+import { html, render } from 'lit-html';
+import {
+  AGORIC_COIN_TYPE,
+  stakeCurrency,
+  stableCurrency,
+  bech32Config,
+  COSMOS_COIN_TYPE,
+} from './chainInfo.js';
+import { MsgWalletAction } from './gen/swingset/msgs';
+
+const { freeze } = Object;
+
+const check = {
+  /**
+   * @param {T?} x
+   * @param { string= } context
+   * @returns { T }
+   * @template T
+   */
+  notNull(x, context) {
+    if (!x) {
+      throw Error(`null/undefined ${context}`);
+    }
+    return x;
+  },
+
+  /** @type { (elt: unknown) => HTMLButtonElement } */
+  theButton(elt) {
+    if (!(elt instanceof HTMLButtonElement)) {
+      throw Error('not Button');
+    }
+    return elt;
+  },
+
+  /** @type { (elt: unknown) => HTMLInputElement } */
+  theInput(elt) {
+    if (!(elt instanceof HTMLInputElement)) {
+      throw Error('not input');
+    }
+    return elt;
+  },
+
+  /** @type { (elt: unknown) => HTMLTextAreaElement } */
+  theTextArea(elt) {
+    if (!(elt instanceof HTMLTextAreaElement)) {
+      throw Error('not input');
+    }
+    return elt;
+  },
+
+  theSelect(elt) {
+    if (!(elt instanceof HTMLSelectElement)) {
+      throw Error('not select');
+    }
+    return elt;
+  },
+};
+
+/**
+ * @typedef {{
+ *   typeUrl: '/agoric.swingset.MsgWalletAction',
+ *   value: {
+ *     owner: string, // base64 of raw bech32 data
+ *     action: string,
+ *   }
+ * }} WalletAction
+ */
+
+/**
+ * /agoric.swingset.XXX matches package agoric.swingset in swingset/msgs.go
+ */
+const SwingsetMsgs = {
+  MsgWalletAction: {
+    typeUrl: '/agoric.swingset.MsgWalletAction',
+    aminoType: 'swingset/WalletAction',
+  },
+};
+
+/** @type {(address: string) => Uint8Array} */
+export function toAccAddress(address) {
+  return fromBech32(address).data;
+}
+
+/** @type {import('@cosmjs/stargate').AminoConverters} */
+const SwingsetConverters = {
+  // '/agoric.swingset.MsgProvision': {
+  //   /* ... */
+  // },
+  [SwingsetMsgs.MsgWalletAction.typeUrl]: {
+    aminoType: SwingsetMsgs.MsgWalletAction.aminoType,
+    toAmino: proto => {
+      const { action, owner } = proto;
+      // NOTE: keep "dictionaries" sorted
+      const amino = {
+        action,
+        owner: toBech32(bech32Config.bech32PrefixAccAddr, fromBase64(owner)),
+      };
+      console.log('@@toAmino:', { proto, amino });
+      return amino;
+    },
+    fromAmino: amino => {
+      const { action, owner } = amino;
+      const proto = { action, owner: toBase64(toAccAddress(owner)) };
+      console.log('@@fromAmino:', { amino, proto });
+      return proto;
+    },
+  },
+};
+
+const aRegistry = new Registry([
+  ...defaultRegistryTypes,
+  [SwingsetMsgs.MsgWalletAction.typeUrl, MsgWalletAction],
+]);
+
+// agoric start local-chain
+const localChainInfo = {
+  rpc: 'http://localhost:26657',
+  // rest: api,
+  chainId: 'agoric',
+  chainName: 'Agoric local-chain',
+  stakeCurrency,
+  // walletUrlForStaking: `https://${network}.staking.agoric.app`,
+  bip44: {
+    coinType: COSMOS_COIN_TYPE,
+    // coinType: AGORIC_COIN_TYPE, // ISSUE: how do we switch on this before we know isNanoLedger?
+  },
+  bech32Config,
+  currencies: [stakeCurrency, stableCurrency],
+  feeCurrencies: [stableCurrency],
+  features: ['stargate', 'ibc-transfer'],
+};
+
+/**
+ * @param {ReturnType<typeof makeUI>} ui
+ * @param {*} keplr
+ * @param {typeof SigningStargateClient.connectWithSigner} connectWithSigner
+ */
+const makeSigner = async (ui, keplr, connectWithSigner) => {
+  // const chainId = ui.selectValue('select[name="chainId"]');
+  // console.log({ chainId });
+
+  const chainInfo = localChainInfo;
+  const { chainId } = chainInfo;
+  ui.setValue('*[name="chainId"]', chainId);
+  const { coinMinimalDenom: denom } = stakeCurrency;
+
+  await keplr.experimentalSuggestChain(chainInfo);
+  await keplr.enable(chainId);
+
+  // https://docs.keplr.app/api/#get-address-public-key
+  const key = await keplr.getKey(chainId);
+  console.log({ key });
+
+  ui.setChecked('*[name="isNanoLedger"]', key.isNanoLedger);
+
+  // const offlineSigner = await keplr.getOfflineSignerOnlyAmino(chainId);
+  const offlineSigner = await keplr.getOfflineSignerAuto(chainId);
+  console.log({ offlineSigner });
+
+  // Currently, Keplr extension manages only one address/public key pair.
+  const [account] = await offlineSigner.getAccounts();
+  const { address } = account;
+  ui.setValue('*[name="account"]', address);
+
+  const cosmJS = await connectWithSigner(chainInfo.rpc, offlineSigner, {
+    aminoTypes: new AminoTypes(SwingsetConverters),
+    registry: aRegistry,
+  });
+  console.log({ cosmJS });
+
+  ui.onClick('#sign', async _ev => {
+    const { accountNumber, sequence } = await cosmJS.getSequence(address);
+    console.log({ accountNumber, sequence });
+
+    const send1 = {
+      typeUrl: '/cosmos.bank.v1beta1.MsgSend',
+      value: {
+        amount: [
+          {
+            amount: '1234567',
+            denom,
+          },
+        ],
+        fromAddress: address,
+        toAddress: address,
+      },
+    };
+
+    /** @type {WalletAction} */
+    const act1 = {
+      typeUrl: '/agoric.swingset.MsgWalletAction',
+      value: {
+        owner: toBase64(toAccAddress(address)),
+        action: ui.textValue('*[name="action"]'),
+      },
+    };
+
+    const msgs = [act1];
+    const fee = {
+      amount: [{ amount: '100', denom }],
+      gas: '100000',
+    };
+    const memo = ui.inputValue('*[name="memo"]');
+
+    const signDoc = {
+      chain_id: chainId,
+      account_number: `${accountNumber}`,
+      sequence: `${sequence}`,
+      fee,
+      memo,
+      msgs,
+    };
+
+    // const tx = await cosmJS.signAmino(chainId, account.address, signDoc);
+    const signerData = { accountNumber, sequence, chainId };
+    console.log('sign', { address, msgs, fee, memo, signerData });
+
+    // const tx = await offlineSigner.signAmino(address, signDoc);
+
+    const tx = await cosmJS.signAndBroadcast(address, msgs, fee, memo);
+
+    console.log({ tx });
+  });
+};
+
+/** @param {typeof document} document */
+const makeUI = document => {
+  return freeze({
+    /** @param { string } selector */
+    inputValue: selector =>
+      check.theInput(document.querySelector(selector)).value,
+    textValue: selector =>
+      check.theTextArea(document.querySelector(selector)).value,
+    setValue: (selector, value) =>
+      (check.theInput(document.querySelector(selector)).value = value),
+    setChecked: (selector, value) =>
+      (check.theInput(document.querySelector(selector)).checked = value),
+    /** @param { string } selector */
+    selectValue: selector => {
+      const sel = check.theSelect(document.querySelector(selector));
+      return sel.options[sel.selectedIndex].value;
+    },
+    onClick: (selector, handler) =>
+      check
+        .theButton(document.querySelector(selector))
+        .addEventListener('click', handler),
+    showItems: (selector, items) =>
+      render(
+        html`<ul>
+          ${items.map(item => html`<li>${item}</li>`)}
+        </ul>`,
+        document.querySelector(selector),
+      ),
+  });
+};
+
+window.addEventListener('load', _ev => {
+  if (!('keplr' in window)) {
+    // eslint-disable-next-line no-alert
+    alert('Please install keplr extension');
+  }
+  // @ts-expect-error keplr is injected
+  const { keplr } = window;
+  makeSigner(makeUI(document), keplr, SigningStargateClient.connectWithSigner);
+});
