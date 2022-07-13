b04cb7da9ff8649292677976ed5a45e7f09c27ed
Dan Connolly <connolly@agoric.com>
2022-07-12 23:13:25 -0500

    feat(demo-signing): sign and broadcast WalletAction
    without ledger
    
     - toward keplr/ledger demo: copy SuggestChain.js
     - sign simple msg/doc with keplr/ledger (WIP)
       - ISSUE: started with cosmjs API integration but
         couldn't get it to work without skipping it
     - chore(demo-signing): figured out offlignSigner.signAmino() API
       no longer skipping all the way to keplr.signAmino()
     - feat(demo-signing): sign WALLET_ACTION with keplr, ledger
     - send signed transaction to chain (WIP)
       - can connect to chain and query for sequence
       - aminoTypes seem to work
       - TODO: generate protobuf codec for MsgWalletAction
     - pass connectWithSigner authority explicitly
     - integrate MsgWalletAction proto stub via registry
     - MsgWalletAction.owner is bytes, not string
    
    provide enough gas
    
    avoid:
    
    Error: Broadcasting transaction failed with code 11 (codespace: sdk). Log: out of gas in location: ReadFlat; gasWanted: 250, gasUsed: 1000: out of gas
        at SigningStargateClient.broadcastTx (stargateclient.ts:413:9)
        at async HTMLButtonElement.<anonymous> (keplr-ledger-demo.js:215:16)

---
+++ packages/wallet/ui/demo-signing/src/chainInfo.js
@@ -0,0 +1,119 @@
+import { SigningStargateClient } from '@cosmjs/stargate';
+
+export const NETWORK_CONFIGS = [
+  ['https://main.agoric.net/network-config', 'Agoric Mainnet'],
+  ['https://testnet.agoric.net/network-config', 'Agoric Testnet'],
+];
+
+export const AGORIC_COIN_TYPE = 564;
+export const COSMOS_COIN_TYPE = 118;
+
+export const stakeCurrency = {
+  coinDenom: 'BLD',
+  coinMinimalDenom: 'ubld',
+  coinDecimals: 6,
+  coinGeckoId: undefined,
+};
+export const stableCurrency = {
+  coinDenom: 'RUN',
+  coinMinimalDenom: 'urun',
+  coinDecimals: 6,
+  coinGeckoId: undefined,
+};
+
+export const bech32Config = {
+  bech32PrefixAccAddr: 'agoric',
+  bech32PrefixAccPub: 'agoricpub',
+  bech32PrefixValAddr: 'agoricvaloper',
+  bech32PrefixValPub: 'agoricvaloperpub',
+  bech32PrefixConsAddr: 'agoricvalcons',
+  bech32PrefixConsPub: 'agoricvalconspub',
+};
+
+const makeChainInfo = (networkConfig, rpcAddr, chainId, caption) => {
+  const coinType = Number(
+    new URL(networkConfig).searchParams.get('coinType') || AGORIC_COIN_TYPE,
+  );
+  const hostname = new URL(networkConfig).hostname;
+  const network = hostname.split('.')[0];
+  let rpc;
+  let api;
+  if (network !== hostname) {
+    rpc = `https://${network}.rpc.agoric.net`;
+    api = `https://${network}.api.agoric.net`;
+  } else {
+    rpc = `http://${rpcAddr}`;
+    api = rpc.replace(/(:\d+)?$/, ':1317');
+  }
+  return {
+    rpc,
+    rest: api,
+    chainId,
+    chainName: caption || `Agoric ${network}`,
+    stakeCurrency,
+    walletUrlForStaking: `https://${network}.staking.agoric.app`,
+    bip44: {
+      coinType,
+    },
+    bech32Config,
+    currencies: [stakeCurrency, stableCurrency],
+    feeCurrencies: [stableCurrency],
+    features: ['stargate', 'ibc-transfer'],
+  };
+};
+
+export async function suggestChain(
+  networkConfig,
+  /** @type {string} */ caption = undefined,
+  io = {},
+) {
+  const {
+    fetch = globalThis.fetch,
+    keplr = window.keplr,
+    SigningClient = SigningStargateClient,
+    random = Math.random,
+  } = io;
+
+  const res = await fetch(networkConfig);
+  if (!res.ok) {
+    throw Error(`Cannot fetch network: ${res.status}`);
+  }
+  const { chainName: chainId, rpcAddrs } = await res.json();
+  const rpcAddr = rpcAddrs[Math.floor(random() * rpcAddrs.length)];
+
+  const chainInfo = makeChainInfo(networkConfig, rpcAddr, chainId, caption);
+  await keplr.experimentalSuggestChain(chainInfo);
+  await keplr.enable(chainId);
+
+  const offlineSigner = keplr.getOfflineSigner(chainId);
+  const cosmJS = await SigningClient.connectWithSigner(
+    chainInfo.rpc,
+    offlineSigner,
+  );
+
+  /*
+  // Example transaction 
+  const amount = {
+    denom: 'ubld',
+    amount: '1234567',
+  };
+  const accounts = await offlineSigner.getAccounts();
+  await cosmJS.sendTokens(
+    accounts[0].address,
+    'agoric123456',
+    [amount],
+    {
+      amount: [
+        {
+          amount: '500000',
+          denom: 'urun',
+        },
+      ],
+      gas: '890000',
+    },
+    'enjoy!',
+  );
+ */
+
+  return cosmJS;
+}
