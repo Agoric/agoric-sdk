35b68062f69dc84041b98f670572683b572aa29e
Mathieu Hofman <86499+mhofman@users.noreply.github.com>
2022-07-27 00:49:47 +0200

    ci: ensure deployment test runs some loadgen cycles (#5829)

--- packages/deployment/scripts/integration-test.sh
+++ packages/deployment/scripts/integration-test.sh
@@ -54,7 +54,7 @@ then
     "$AG_SETUP_COSMOS_HOME/faucet-helper.sh" add-egress loadgen "$SOLO_ADDR"
   SDK_BUILD=0 SDK_SRC=/usr/src/agoric-sdk OUTPUT_DIR="$RESULTSDIR" ./start.sh \
     --stage.save-storage --trace kvstore swingstore xsnap \
-    --stages=3 --stage.duration=4 \
+    --stages=3 --stage.duration=10 --stage.loadgen.cycles=4 \
     --stage.loadgen.vault.interval=12 --stage.loadgen.vault.limit=2 \
     --stage.loadgen.amm.interval=12 --stage.loadgen.amm.wait=6 --stage.loadgen.amm.limit=2 \
     --profile=testnet "--testnet-origin=file://$RESULTSDIR" \
