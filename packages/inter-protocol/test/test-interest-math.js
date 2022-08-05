77996e94da3c360836d68d635b6a2779cf9a3f48
Turadg Aleahmad <turadg@agoric.com>
2022-08-05 09:51:01 -0700

    test(vaultFactory): refactor to macro

--- packages/inter-protocol/test/test-interest-math.js
+++ packages/inter-protocol/test/test-interest-math.js
@@ -6,18 +6,18 @@ import { makeIssuerKit, AmountMath } from '@agoric/ertp';
 import { makeRatio } from '@agoric/zoe/src/contractSupport/ratio.js';
 import { calculateCurrentDebt, reverseInterest } from '../src/interest-math.js';
 
-const runBrand = makeIssuerKit('run').brand;
+const brand = makeIssuerKit('foo').brand;
 
 /**
  * @param {*} t
  * @param {readonly [bigint, bigint, bigint]} input
  * @param {bigint} result
  */
-function checkDebt(t, input, result) {
+function checkDebt(t, [debt, interest, compounded], result) {
   /** @type {Amount<'nat'>} */
-  const debtSnapshot = AmountMath.make(runBrand, input[0]);
-  const interestSnapshot = makeRatio(100n + input[1], runBrand);
-  const currentCompoundedInterest = makeRatio(100n + input[2], runBrand);
+  const debtSnapshot = AmountMath.make(brand, debt);
+  const interestSnapshot = makeRatio(100n + interest, brand);
+  const currentCompoundedInterest = makeRatio(100n + compounded, brand);
   t.is(
     calculateCurrentDebt(
       debtSnapshot,
@@ -28,47 +28,39 @@ function checkDebt(t, input, result) {
   );
 }
 
-for (const [input, result] of /** @type {const} */ ([
-  // no debt
-  [[0n, 0n, 0n], 0n],
-  [[0n, 0n, 250n], 0n],
+test('no debt wo/compounding', checkDebt, [0n, 0n, 0n], 0n);
+test('no debt w/ compounding', checkDebt, [0n, 0n, 250n], 0n);
+test('some debt wo/compounding', checkDebt, [1_000_000n, 0n, 0n], 1_000_000n);
+test('some debt w/ compounding', checkDebt, [1_000_000n, 0n, 2n], 1_020_000n);
+test(
+  'previous interest wo/compounding',
+  checkDebt,
+  [1_000_000n, 2n, 2n],
+  1_000_000n,
+);
+test(
+  'previous interest w/ compounding',
+  checkDebt,
+  [1_000_000n, 2n, 4n],
+  1_019_608n,
+);
+test(
+  'previous interest, compounded down',
+  checkDebt,
+  [1_000_000n, 2n, 0n], // negative interest since snapshot
+  980_392n,
+);
 
-  // some debt
-  [[1_000_000n, 0n, 0n], 1_000_000n],
-  [[1_000_000n, 0n, 2n], 1_020_000n],
-
-  // some debt and previous interest
-  [[1_000_000n, 2n, 0n], 980_392n], // negative interest since snapshot
-  [[1_000_000n, 2n, 2n], 1_000_000n],
-  [[1_000_000n, 2n, 4n], 1_019_608n],
-])) {
-  test(
-    `calculateCurrentDebt ${input} returns ${result}`,
-    checkDebt,
-    input,
-    result,
-  );
+function checkReverse(t, [debt, interest], result) {
+  // /** @type {Amount<'nat'>} */
+  const debtSnap = AmountMath.make(brand, debt);
+  const interestApplied = makeRatio(100n + interest, brand);
+  t.deepEqual(reverseInterest(debtSnap, interestApplied).value, result);
 }
 
-function checkReverse(t, input, result) {
-  /** @type {Amount<'nat'>} */
-  const debt = AmountMath.make(runBrand, input[0]);
-  const interestApplied = makeRatio(100n + input[1], runBrand);
-  t.deepEqual(reverseInterest(debt, interestApplied).value, result);
-}
+test('none', checkReverse, [0n, 0n], 0n);
+test('none w/interest', checkReverse, [0n, 1n], 0n);
 
-for (const [input, result] of /** @type {const} */ ([
-  [[0n, 0n], 0n],
-  [[0n, 1n], 0n],
-
-  [[1_000_000n, 0n], 1_000_000n],
-  [[1_000_000n, 2n], 980_392n],
-  [[1_000_000n, 100n], 500_000n],
-])) {
-  test(
-    `reverseInterest ${input} returns ${result}`,
-    checkReverse,
-    input,
-    result,
-  );
-}
+test('some', checkReverse, [1_000_000n, 0n], 1_000_000n);
+test('some w/interest', checkReverse, [1_000_000n, 2n], 980_392n);
+test('some w/100% interest', checkReverse, [1_000_000n, 100n], 500_000n);
