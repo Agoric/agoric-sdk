088e1446a100932f801fb04d5881f80f5d94526f
Dan Connolly <connolly@agoric.com>
2022-08-10 13:37:23 -0500

    feat(wallet/api): marshal contexts for wallet, board (#5883)
    * feat(wallet/api): marshal contexts for wallet, board
    
    * test(wallet/api): makeLoggingPresence seems to work
    
    * fix(wallet/api): importContext has .serialize too
    
    * feat(wallet/api): makeLoggingPresence
    
    * feat(wallet/api): handle unregistered identites in serialization
    
    * chore: prune unused private brand, issuer maps
    
    * refactor: fromPart -> fromMyWallet
    
    * docs: makeExportContext
    
    * feat: ensureBoardId with test
    
    * docs: test names etc.
    
    * CHECKPOINT: tests pass. coverage is good.
    
    * chore(wallet/api): marshal stuff working (IOU words...)
    
    * refactor(wallet/api): JSON.stringify capData body for clarity
    
    * refactor(wallet/api): type safety for marshal-contexts
    
     - perhaps one bug fix around `purse:1` vs `1`?
    
    * docs(wallet/api): refine makeExportContext docstring
    
    * fixup! test(wallet/api): makeLoggingPresence seems to work
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ packages/wallet/api/test/test-wallet-marshal.js
@@ -0,0 +1,166 @@
+// @ts-check
+import { test } from '@agoric/zoe/tools/prepare-test-env-ava.js';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Far } from '@endo/far';
+import { makeBoard } from '@agoric/vats/src/lib-board.js';
+import {
+  makeExportContext,
+  makeImportContext,
+} from '../src/marshal-contexts.js';
+
+/** @param {ReturnType<typeof makeBoard>} board */
+const makeAMM = board => {
+  const atom = Far('ATOM brand', {});
+  const pub = board.getPublishingMarshaller();
+  return harden({ getMetrics: () => pub.serialize(harden([atom])) });
+};
+
+/** @param {ReturnType<typeof makeBoard>} board */
+const makeOnChainWallet = board => {
+  const context = makeExportContext();
+  let brand;
+  return harden({
+    suggestIssuer: (name, boardId) => {
+      brand = board.getValue(boardId);
+      const purse = Far(`${name} purse`, {
+        getCurrentAmount: () => harden({ brand, value: 100 }),
+      });
+      // only for private brands
+      //   context.initBrandId(boardId, brand);
+      context.initBoardId(boardId, brand);
+      // @ts-expect-error mock purse
+      context.initPurseId(name, purse); // TODO: strong id rather than name?
+    },
+    publish: () =>
+      context.serialize(
+        harden(
+          [...context.purseEntries()].map(([name, purse], id) => ({
+            meta: { id },
+            // displayInfo
+            // brandBoardId,
+            purse,
+            brand,
+            currentAmount: purse.getCurrentAmount(),
+            // actions
+            brandPetname: name,
+            pursePetname: `${name} purse`,
+          })),
+        ),
+      ),
+  });
+};
+
+test('makeImportContext preserves identity across AMM and wallet', t => {
+  const context = makeImportContext();
+
+  const board = makeBoard(0, { prefix: 'board' });
+  const amm = makeAMM(board);
+  const ammMetricsCapData = amm.getMetrics();
+
+  t.deepEqual(ammMetricsCapData, {
+    body: JSON.stringify([
+      { '@qclass': 'slot', iface: 'Alleged: ATOM brand', index: 0 },
+    ]),
+    slots: ['board011'],
+  });
+
+  /** @type {Brand[]} */
+  const [b1] = context.fromBoard.unserialize(ammMetricsCapData);
+  /** @type {Brand[]} */
+  const [b2] = context.fromBoard.unserialize(amm.getMetrics());
+  t.is(b1, b2, 'unserialization twice from same source');
+
+  const myWallet = makeOnChainWallet(board);
+  myWallet.suggestIssuer('ATOM', 'board011');
+  const walletCapData = myWallet.publish();
+  t.deepEqual(walletCapData, {
+    body: JSON.stringify([
+      {
+        brand: { '@qclass': 'slot', iface: 'Alleged: ATOM brand', index: 0 },
+        brandPetname: 'ATOM',
+        currentAmount: { brand: { '@qclass': 'slot', index: 0 }, value: 100 },
+        meta: { id: 0 },
+        purse: { '@qclass': 'slot', iface: 'Alleged: ATOM purse', index: 1 },
+        pursePetname: 'ATOM purse',
+      },
+    ]),
+    slots: ['board011', 'purse:ATOM'],
+  });
+
+  const walletState = context.fromMyWallet.unserialize(walletCapData);
+  t.is(walletState[0].brand, b1, 'unserialization across sources');
+
+  t.throws(
+    () => context.fromBoard.unserialize(walletCapData),
+    { message: /bad board slot/ },
+    'AMM cannot refer to purses',
+  );
+
+  t.throws(
+    () => context.fromMyWallet.serialize(Far('widget', {})),
+    {
+      message: /unregistered/,
+    },
+    'import context never exports unregistered objects',
+  );
+});
+
+test('ensureBoardId allows re-registration; initBoardId does not', t => {
+  const brandM = Far('Moola brand', {});
+  const brandS = Far('Semolean brand', {});
+
+  const context = makeExportContext();
+  context.initBoardId('board1', brandM);
+  t.throws(() => context.initBoardId('board1', brandM));
+  context.ensureBoardId('board1', brandM);
+  t.throws(() => context.ensureBoardId('board12', brandM));
+  context.ensureBoardId('board12', brandS);
+  t.throws(() => context.initBoardId('board12', brandM));
+});
+
+test('makeExportContext.serialize handles unregistered identites', t => {
+  const brand = Far('Zoe invitation brand', {});
+  const instance = Far('amm instance', {});
+  const invitationAmount = harden({ brand, value: [{ instance }] });
+
+  const context = makeExportContext();
+  context.initBoardId('board1', brand);
+  const actual = context.serialize(invitationAmount);
+
+  t.deepEqual(actual, {
+    body: JSON.stringify({
+      brand: {
+        '@qclass': 'slot',
+        iface: 'Alleged: Zoe invitation brand',
+        index: 0,
+      },
+      value: [
+        {
+          instance: {
+            '@qclass': 'slot',
+            iface: 'Alleged: amm instance',
+            index: 1,
+          },
+        },
+      ],
+    }),
+    slots: ['board1', 'unknown:1'],
+  });
+
+  t.deepEqual(context.unserialize(actual), invitationAmount);
+
+  const myPayment = /** @type {Payment} */ (
+    Far('payment', { getAllegedBrand: () => assert.fail('no impl') })
+  );
+  context.savePaymentActions(myPayment);
+  const cap2 = context.serialize(myPayment);
+  t.deepEqual(cap2, {
+    body: JSON.stringify({
+      '@qclass': 'slot',
+      iface: 'Alleged: payment',
+      index: 0,
+    }),
+    slots: ['payment:1'],
+  });
+  t.deepEqual(context.unserialize(cap2), myPayment);
+});
