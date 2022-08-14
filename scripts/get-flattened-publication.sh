0b97c2123098f89b34620a7e278057a9cfee5de3
Richard Gibson <richard.gibson@gmail.com>
2022-08-14 15:51:12 -0400

    refactor(scripts): Improve error reporting

--- scripts/get-flattened-publication.sh
+++ scripts/get-flattened-publication.sh
@@ -32,7 +32,7 @@ jq --version | awk '
   }
 '
 
-# Make the abci_query request and extract data from a response.
+# Make the abci_query request.
 # cf. https://docs.tendermint.com/master/rpc/#/ABCI/abci_query
 #
 #   {
@@ -46,29 +46,39 @@ jq --version | awk '
 #       }
 #     }
 #   }
+resp="$(
+  # Avoid the GET interface in case interpretation of `path` as JSON is ever fixed.
+  # https://github.com/tendermint/tendermint/issues/9164
+  # curl -sS "${URL_PREFIX%/}/abci_query?path=%22/custom/vstorage/data/$STORAGE_KEY%22" | \
+  curl -sS "$URL_PREFIX" --request POST --header 'Content-Type: application/json' --data "$(
+    printf '{
+      "jsonrpc": "2.0",
+      "id": -1,
+      "method": "abci_query",
+      "params": { "path": "/custom/vstorage/data/%s" }
+    }' "$STORAGE_KEY"
+  )"
+)"
 
-# Avoid the GET interface in case interpretation of `path` as JSON is ever fixed.
-# https://github.com/tendermint/tendermint/issues/9164
-# curl -sS "${URL_PREFIX%/}/abci_query?path=%22/custom/vstorage/data/$STORAGE_KEY%22" | \
-curl -sS "$URL_PREFIX" --request POST --header 'Content-Type: application/json' --data "$(
-  printf '{
-    "jsonrpc": "2.0",
-    "id": -1,
-    "method": "abci_query",
-    "params": { "path": "/custom/vstorage/data/%s" }
-  }' "$STORAGE_KEY"
-)" | \
-# Decode, simplify, flatten, and compact the output.
-jq -c '
-  # Capture response block height.
-  .result.response.height? as $responseHeight |
+# Decode the response
+response_height_json="$(printf '%s' "$resp" | jq '.result.response.height?' | grep -E '^"[0-9]+"$')"
+if [ ":$response_height_json" = : ]; then
+  printf 'Unable to read response block height:\n%s\n' "$resp" >&2
+  exit 1
+fi
+response_value_json="$(printf '%s' "$resp" | jq '.result.response.value | @base64d | fromjson')"
+if [ ":$response_value_json" = : ]; then
+  printf 'Unable to read response value as base64-encoded JSON text:\n%s\n' "$resp" >&2
+  exit 1
+fi
+unwrapped_value="$(printf '%s' "$response_value_json" | jq '.value | fromjson')"
+if [ ":$unwrapped_value" = : ]; then
+  printf 'Unable to unwrap response:\n%s\n' "$response_value_json" >&2
+  exit 1
+fi
 
-  # Decode `value` as base64, then decode that as JSON.
-  .result.response.value | @base64d | fromjson |
-
-  # Decode `value` as JSON.
-  .value | fromjson |
-
+# Simplify, flatten, and compact.
+printf '%s' "$unwrapped_value" | jq -c --arg responseHeightJson "$response_height_json" '
   # Upgrade a naked value to a stream cell if necessary.
   if has("height") and has("values") then . else { values: [ . | tojson ] } end |
 
@@ -105,5 +115,9 @@ jq -c '
 
   # Add block height information.
   (.dataBlockHeight |= $dataHeight) |
-  (.blockHeight |= $responseHeight)
-'
+  (.blockHeight |= ($responseHeightJson | fromjson))
+' || {
+  status=$?
+  printf 'Unable to process response value:\n%s\n' "$unwrapped_value" >&2
+  exit $status
+}
