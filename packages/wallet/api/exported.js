a6360fcf071053a714c881d7931cd8a1a91abe27
Turadg Aleahmad <turadg@agoric.com>
2022-08-16 10:00:49 -0700

    chore(wallet/api): WIP

--- packages/wallet/api/exported.js
+++ packages/wallet/api/exported.js
@@ -1,1 +1,6 @@
+// XXX without this types don't resolve
+// eslint-disable-next-line import/no-extraneous-dependencies -- TODO remove
+import '@agoric/governance/exported.js';
+import '@agoric/zoe/exported.js';
+
 import './src/types.js';
