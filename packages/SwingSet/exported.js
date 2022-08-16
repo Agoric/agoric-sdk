68a0b4a3d521ce53f4ebeddcac8cd7c53218b7e8
Turadg Aleahmad <turadg@agoric.com>
2022-08-16 09:47:29 -0700

    chore(swingset-vat): WIP remove maxNodeModuleJsDepth

--- packages/SwingSet/exported.js
+++ packages/SwingSet/exported.js
@@ -1,1 +1,8 @@
 /// <reference types="./src/vats/timer/types"/>
+
+// XXX without this types don't resolve
+// eslint-disable-next-line import/no-extraneous-dependencies -- TODO remove
+import '@agoric/governance/exported.js';
+
+// XXX https://github.com/endojs/endo/issues/1254
+import '@endo/captp/src/types.js';
