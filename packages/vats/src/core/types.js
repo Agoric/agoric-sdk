2f4110a3068eeec8fa72e6eea9e42074e860eade
Dan Connolly <connolly@agoric.com>
2022-08-22 15:44:56 -0500

    feat(vats): PSM-first bootstrap
     - reserve only appropriate names in agoricNames name hubs
     - install gov contracts
     - test that boot-psm puts an instance in agoricNames
     - PSM-only SwingSet config
     - publish appropriate top-level agoricNames in boot-psm
     - configurable top-level agoricNames
     - include bridgeCoreEval
     - don't crash on DeliverInbound
       - register a dummy mailbox handler
     - startWalletFactory without provisioning mailbox access
       - keep smartWalletStartResult in bootstrap space
       - include startWalletFactory in dynamic behaivior list
       - startWalletFactory types etc.
     - old-style installation
     - register walletFactory just once

--- packages/vats/src/core/types.js
+++ packages/vats/src/core/types.js
@@ -215,6 +215,7 @@
  *   priceAuthority: PriceAuthority,
  *   priceAuthorityAdmin: PriceAuthorityRegistryAdmin,
  *   provisioning: Awaited<ProvisioningVat>,
+ *   walletFactoryStartResult: import('./startWalletFactory').WalletFactoryStartResult,
  *   zoe: ZoeService,
  * }>} ChainBootstrapSpace
  *
--- packages/vats/test/devices.js
+++ packages/vats/test/devices.js
@@ -1,24 +1,31 @@
+import bundleCommittee from '@agoric/governance/bundles/bundle-committee.js';
+import bundleContractGovernor from '@agoric/governance/bundles/bundle-contractGovernor.js';
+import bundleBinaryVoteCounter from '@agoric/governance/bundles/bundle-binaryVoteCounter.js';
+import bundlePSM from '@agoric/inter-protocol/bundles/bundle-psm.js';
+
 import bundleCentralSupply from '../bundles/bundle-centralSupply.js';
 import bundleMintHolder from '../bundles/bundle-mintHolder.js';
 import bundleSingleWallet from '../bundles/bundle-singleWallet.js';
 import bundleWalletFactory from '../bundles/bundle-walletFactory.js';
 
+const bundles = {
+  centralSupply: bundleCentralSupply,
+  mintHolder: bundleMintHolder,
+  singleWallet: bundleSingleWallet,
+  walletFactory: bundleWalletFactory,
+  committee: bundleCommittee,
+  contractGovernor: bundleContractGovernor,
+  binaryVoteCounter: bundleBinaryVoteCounter,
+  psm: bundlePSM,
+};
+
 export const devices = {
   vatAdmin: {
     getNamedBundleCap: name => ({
       getBundle: () => {
-        switch (name) {
-          case 'centralSupply':
-            return bundleCentralSupply;
-          case 'mintHolder':
-            return bundleMintHolder;
-          case 'singleWallet':
-            return bundleSingleWallet;
-          case 'walletFactory':
-            return bundleWalletFactory;
-          default:
-            throw new Error(`unknown bundle ${name}`);
-        }
+        const bundle = bundles[name];
+        assert(bundle, `unknown bundle ${name}`);
+        return bundle;
       },
     }),
   },
