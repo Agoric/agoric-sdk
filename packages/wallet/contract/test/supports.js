cfc23416d9adc06621df607dd833ad2a51242e4f
Turadg Aleahmad <turadg@agoric.com>
2022-08-27 13:44:56 -0700

    refactor: makeFakeWalletBridgeManager helper

--- packages/wallet/contract/test/supports.js
+++ packages/wallet/contract/test/supports.js
@@ -2,6 +2,7 @@
 
 import { makeIssuerKit } from '@agoric/ertp';
 import { connectFaucet } from '@agoric/inter-protocol/src/proposals/demoIssuers.js';
+import * as BRIDGE_ID from '@agoric/vats/src/bridge-ids.js';
 import {
   installBootContracts,
   makeAddressNameHubs,
@@ -144,3 +145,89 @@ export const makeTestSpace = async log => {
 
   return space;
 };
+
+/** @param {import('ava').ExecutionContext} t */
+export const makeFakeWalletBridgeManager = t =>
+  Far('fakeWalletBridgeManager', {
+    register(srcID, handler) {
+      t.is(srcID, BRIDGE_ID.WALLET);
+      t.assert(handler);
+      t.assert(
+        !t.context.walletHandler,
+        'handler can only be registered once in test',
+      );
+    },
+    toBridge(dstID, obj) {
+      t.is(dstID, BRIDGE_ID.WALLET);
+      let ret;
+      switch (obj.type) {
+        case 'WALLET_ACTION': {
+          t.log('DEBUG toBridge', obj);
+          break;
+        }
+
+        case 'VBANK_GET_BALANCE': {
+          const { address, denom, type: _type, ...rest } = obj;
+          t.is(address, 'agoricfoo');
+          t.deepEqual(rest, {});
+          if (denom === 'ubld') {
+            ret = '11993';
+          } else if (denom === 'ufee') {
+            ret = '34';
+          } else {
+            t.fail(`unrecognized denomination ${denom}`);
+          }
+          break;
+        }
+
+        case 'VBANK_GIVE': {
+          const { amount, denom, recipient, type: _type, ...rest } = obj;
+          t.is(recipient, 'agoricfoo');
+          t.is(denom, 'ubld');
+          t.is(amount, '14');
+          t.deepEqual(rest, {});
+          ret = amount;
+          break;
+        }
+
+        case 'VBANK_GRAB': {
+          const { amount, denom, sender, type: _type, ...rest } = obj;
+          t.is(sender, 'agoricfoo');
+          t.deepEqual(rest, {});
+          if (denom === 'ubld') {
+            t.is(amount, '14');
+            ret = amount;
+          } else if (denom === 'ufee') {
+            if (BigInt(amount) > 35n) {
+              throw Error('insufficient ufee funds');
+            } else {
+              t.is(amount, '35');
+              ret = amount;
+            }
+          } else {
+            t.fail(`unrecognized denomination ${denom}`);
+          }
+          break;
+        }
+
+        case 'VBANK_GIVE_TO_REWARD_DISTRIBUTOR': {
+          const { amount, denom, type: _type, ...rest } = obj;
+          t.is(denom, 'ufee');
+          t.is(amount, '12');
+          t.deepEqual(rest, {});
+          ret = true;
+          break;
+        }
+
+        default: {
+          t.fail(`unknown bridge obj type '${obj.type}'`);
+          t.is(obj, null);
+        }
+      }
+      return ret;
+    },
+    unregister(srcID) {
+      t.is(srcID, BRIDGE_ID.WALLET);
+      t.fail('no expected unregister');
+    },
+  });
--- packages/wallet/contract/test/test-walletFactory.js
+++ packages/wallet/contract/test/test-walletFactory.js
@@ -2,16 +2,20 @@
 
 import { test as anyTest } from '@agoric/zoe/tools/prepare-test-env-ava.js';
 
-import '@agoric/vats/src/core/types.js';
-import '@agoric/zoe/exported.js';
-
 import { unsafeMakeBundleCache } from '@agoric/swingset-vat/tools/bundleTool.js';
 import * as BRIDGE_ID from '@agoric/vats/src/bridge-ids.js';
 import { makeStorageNodeChild } from '@agoric/vats/src/lib-chainStorage.js';
 import { makeNameHubKit } from '@agoric/vats/src/nameHub.js';
 import { E, Far } from '@endo/far';
 import path from 'path';
-import { makeTestSpace, subscriptionKey } from './supports.js';
+import {
+  makeFakeWalletBridgeManager,
+  makeTestSpace,
+  subscriptionKey,
+} from './supports.js';
+
+import '@agoric/vats/src/core/types.js';
+import '@agoric/zoe/exported.js';
 
 /** @type {import('ava').TestFn<Awaited<ReturnType<typeof makeTestContext>>>} */
 const test = anyTest;
@@ -65,89 +69,7 @@ const makeTestContext = async t => {
   );
 
   /** @type {BridgeManager} */
-  const bridgeManager = Far('fakeBridgeManager', {
-    register(srcID, handler) {
-      t.is(srcID, BRIDGE_ID.WALLET);
-      t.assert(handler);
-      t.assert(
-        !t.context.walletHandler,
-        'handler can only be registered once in test',
-      );
-    },
-    toBridge(dstID, obj) {
-      t.is(dstID, BRIDGE_ID.WALLET);
-      let ret;
-      switch (obj.type) {
-        case 'WALLET_ACTION': {
-          t.log('DEBUG toBridge', obj);
-          break;
-        }
-
-        case 'VBANK_GET_BALANCE': {
-          const { address, denom, type: _type, ...rest } = obj;
-          t.is(address, 'agoricfoo');
-          t.deepEqual(rest, {});
-          if (denom === 'ubld') {
-            ret = '11993';
-          } else if (denom === 'ufee') {
-            ret = '34';
-          } else {
-            t.fail(`unrecognized denomination ${denom}`);
-          }
-          break;
-        }
-
-        case 'VBANK_GIVE': {
-          const { amount, denom, recipient, type: _type, ...rest } = obj;
-          t.is(recipient, 'agoricfoo');
-          t.is(denom, 'ubld');
-          t.is(amount, '14');
-          t.deepEqual(rest, {});
-          ret = amount;
-          break;
-        }
-
-        case 'VBANK_GRAB': {
-          const { amount, denom, sender, type: _type, ...rest } = obj;
-          t.is(sender, 'agoricfoo');
-          t.deepEqual(rest, {});
-          if (denom === 'ubld') {
-            t.is(amount, '14');
-            ret = amount;
-          } else if (denom === 'ufee') {
-            if (BigInt(amount) > 35n) {
-              throw Error('insufficient ufee funds');
-            } else {
-              t.is(amount, '35');
-              ret = amount;
-            }
-          } else {
-            t.fail(`unrecognized denomination ${denom}`);
-          }
-          break;
-        }
-
-        case 'VBANK_GIVE_TO_REWARD_DISTRIBUTOR': {
-          const { amount, denom, type: _type, ...rest } = obj;
-          t.is(denom, 'ufee');
-          t.is(amount, '12');
-          t.deepEqual(rest, {});
-          ret = true;
-          break;
-        }
-
-        default: {
-          t.fail(`unknown bridge obj type '${obj.type}'`);
-          t.is(obj, null);
-        }
-      }
-      return ret;
-    },
-    unregister(srcID) {
-      t.is(srcID, BRIDGE_ID.WALLET);
-      t.fail('no expected unregister');
-    },
-  });
+  const bridgeManager = makeFakeWalletBridgeManager(t);
 
   const walletFactory = E(zoe).startInstance(
     installation,
