20307424e941938a438f49ca78eeb68e00abedcb
Dan Connolly <connolly@agoric.com>
2022-08-30 13:42:53 -0500

    WIP: bridge device does something

--- packages/vats/test/swingsetTests/flatPerf/bootstrap.js
+++ packages/vats/test/swingsetTests/flatPerf/bootstrap.js
@@ -1,7 +1,22 @@
-import { Far } from '@endo/far';
+import { E, Far } from '@endo/far';
 
 export const buildRootObject = () => {
   return Far('root', {
-    bootstrap: async (vats, devices) => {},
+    bootstrap: async (vats, { bridge }) => {
+      console.log(Object.keys(vats));
+      const { chainStorage } = vats;
+      const theBridgeId = 'swingset';
+      const rootPath = 'published'; // ?
+      const options = { sequence: true };
+
+      const storageRoot = E(chainStorage).makeBridgedChainStorageRoot(
+        bridge,
+        theBridgeId,
+        rootPath,
+        options,
+      );
+
+      await E(storageRoot).setValue('key1', 'value1');
+    },
   });
 };
--- packages/vats/test/swingsetTests/flatPerf/test-chainStorage-growth.js
+++ packages/vats/test/swingsetTests/flatPerf/test-chainStorage-growth.js
@@ -7,6 +7,7 @@ import {
   makeSwingsetController,
   buildKernelBundles,
   initializeSwingset,
+  buildBridge,
 } from '@agoric/swingset-vat';
 import { provideHostStorage } from '@agoric/swingset-vat/src/controller/hostStorage.js';
 
@@ -23,19 +24,27 @@ const log = label => x => {
 const bfile = name => log('url')(new URL(name, import.meta.url).pathname);
 
 test('run swingset', async t => {
+  const bd = buildBridge((bridgeId, message) => {
+    console.log('callOutbound', bridgeId);
+  });
+
   const config = {
     bootstrap: 'bootstrap',
     vats: {
       bootstrap: { sourceSpec: bfile('./bootstrap.js') },
       chainStorage: { sourceSpec: bfile('../../../src/vat-chainStorage.js') },
     },
-    devices: {},
+    devices: {
+      bridge: {
+        sourceSpec: new URL(bd.srcPath, import.meta.url).pathname,
+      },
+    },
   };
 
   const hostStorage = provideHostStorage();
-
+  const deviceEndowments = { bridge: bd.endowments };
   await initializeSwingset(config, [], hostStorage);
-  const c = await makeSwingsetController(hostStorage);
+  const c = await makeSwingsetController(hostStorage, deviceEndowments);
   t.teardown(c.shutdown);
 
   await c.run(); // allow timer device/vat messages to settle
