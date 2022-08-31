c7a9bfb69a72e80276c07fc2e6b4084e1f99f5da
Dan Connolly <connolly@agoric.com>
2022-08-30 22:05:54 -0500

    docs(wallet/contract): document notifier protocol

--- packages/wallet/contract/src/smartWallet.js
+++ packages/wallet/contract/src/smartWallet.js
@@ -50,9 +50,13 @@ export const makeSmartWallet = async (
   const notifierParts = {
     contacts: E(admin).getContactsNotifier(),
     dapps: E(admin).getDappsNotifier(),
+    /** all current issuers */
     issuers: E(admin).getIssuersNotifier(),
+    /** recently updated offers */
     offers: E(admin).getOffersNotifier(),
+    /** recently updated payments */
     payments: E(admin).getPaymentsNotifier(),
+    /** all current purses */
     purses: E(admin).getPursesNotifier(),
   };
   const mutableState = fromEntries(keys(notifierParts).map(key => [key, []]));
