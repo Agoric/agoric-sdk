ecb5ae047e9d571da88fade5daa818421dec52f5
Turadg Aleahmad <turadg@agoric.com>
2022-09-03 12:09:29 -0700

    refactor(governance): validate offerArgs

--- packages/governance/src/committee.js
+++ packages/governance/src/committee.js
@@ -65,8 +65,10 @@ const start = (zcf, privateArgs) => {
         }),
         invitationMakers: Far('invitation makers', {
           makeVoteInvitation: questionHandle => {
-            const continuingVoteHandler = (_seat, { positions }) => {
-              _seat.exit();
+            const continuingVoteHandler = (cseat, offerArgs) => {
+              assert(offerArgs, 'continuingVoteHandler missing offerArgs');
+              const { positions } = offerArgs;
+              cseat.exit();
               const { voteCap } = allQuestions.get(questionHandle);
               return E(voteCap).submitVote(voterHandle, positions, 1n);
             };
