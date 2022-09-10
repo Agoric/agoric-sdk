578aab35540089b100b6e61f7516bd381bd33894
Dan Connolly <connolly@agoric.com>
2022-09-10 13:22:57 -0500

    test(wallet/contract): add provisionPool

--- packages/wallet/contract/test/devices.js
+++ packages/wallet/contract/test/devices.js
@@ -2,6 +2,7 @@ import bundleCentralSupply from '@agoric/vats/bundles/bundle-centralSupply.js';
 import bundleMintHolder from '@agoric/vats/bundles/bundle-mintHolder.js';
 import bundleSingleWallet from '@agoric/vats/bundles/bundle-singleWallet.js';
 import bundleWalletFactory from '@agoric/vats/bundles/bundle-legacy-walletFactory.js';
+import bundleProvisionPool from '@agoric/vats/bundles/bundle-provisionPool.js';
 
 export const devices = {
   vatAdmin: {
@@ -16,6 +17,8 @@ export const devices = {
             return bundleSingleWallet;
           case 'walletFactory':
             return bundleWalletFactory;
+          case 'provisionPool':
+            return bundleProvisionPool;
           default:
             throw new Error(`unknown bundle ${name}`);
         }
