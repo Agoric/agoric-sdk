5d9f411a124e4028cd88764084999fb1fd791609
Mathieu Hofman <mathieu@agoric.com>
2022-09-16 23:43:31 +0000

    feat(internal): add async utils from loadgen

--- packages/internal/src/utils.js
+++ packages/internal/src/utils.js
@@ -223,3 +223,59 @@ export const makeMeasureSeconds = currentTimeMillisec => {
   };
   return measureSeconds;
 };
+
+/**
+ * @param {Error[]} errors
+ * @param {string} [message]
+ */
+const makeAggregateError = (errors, message) => {
+  const err = new Error(message);
+  Object.defineProperties(err, {
+    name: {
+      value: 'AggregateError',
+    },
+    errors: {
+      value: errors,
+    },
+  });
+  return err;
+};
+
+/**
+ * @template T
+ * @param {readonly (T | PromiseLike<T>)[]} values
+ * @returns {Promise<T[]>}
+ */
+export const PromiseAllOrErrors = async values => {
+  return Promise.allSettled(values).then(results => {
+    const errors = /** @type {PromiseRejectedResult[]} */ (
+      results.filter(({ status }) => status === 'rejected')
+    ).map(result => result.reason);
+    if (!errors.length) {
+      return /** @type {PromiseFulfilledResult<T>[]} */ (results).map(
+        result => result.value,
+      );
+    } else if (errors.length === 1) {
+      throw errors[0];
+    } else {
+      throw makeAggregateError(errors);
+    }
+  });
+};
+
+/**
+ * @type {<T>(
+ *   trier: () => Promise<T>,
+ *  finalizer: (error?: unknown) => Promise<void>,
+ * ) => Promise<T>}
+ */ export const aggregateTryFinally = async (trier, finalizer) =>
+  trier().then(
+    async result => finalizer().then(() => result),
+    async tryError =>
+      finalizer(tryError)
+        .then(
+          () => tryError,
+          finalizeError => makeAggregateError([tryError, finalizeError]),
+        )
+        .then(error => Promise.reject(error)),
+  );
