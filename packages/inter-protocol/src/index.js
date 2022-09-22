d68b350b4b64d4e741f7e07c11cd65e48fb93404
Turadg Aleahmad <turadg@agoric.com>
2022-09-22 10:34:27 -0700

    inter-protocol explicit

--- packages/inter-protocol/src/index.js
+++ packages/inter-protocol/src/index.js
@@ -1,1 +1,7 @@
+// @ts-check
+/// <reference types="ses"/>
+
+// Ambient types. Needed only for dev but this does a runtime import.
+import '@agoric/notifier/exported.js';
+
 export { calculateCurrentDebt } from './interest-math.js';
