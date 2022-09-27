4cbeb12fef2f28e63b5dafbeab618a369c7d6b77
Kris Kowal <kris@agoric.com>
2022-09-26 17:22:46 -0700

    chore: Bump xsnap meter type

--- packages/xsnap/test/test-xs-perf.js
+++ packages/xsnap/test/test-xs-perf.js
@@ -63,7 +63,7 @@ test('meter details', async t => {
     },
     'evaluate returns meter details',
   );
-  t.is(meterType, 'xs-meter-17');
+  t.is(meterType, 'xs-meter-18');
 });
 
 // test disabled until rewritten to tolerate fast CI hosts getting
