09c2d1c97449b66ed6ab9afcd3556e9e4792c7c2
Turadg Aleahmad <turadg@agoric.com>
2022-10-03 15:19:01 -0700

    fix(perf command): wait for numWantsSatisfied

--- packages/agoric-cli/src/commands/perf.js
+++ packages/agoric-cli/src/commands/perf.js
@@ -74,9 +74,10 @@ export const makePerfCommand = async logger => {
             if (status.error) {
               console.error(status.error);
               exit(1);
-            } else if (status.numWantsSatisfied)
+            } else if (status.numWantsSatisfied) {
               process.stdout.write(`satisfied: ${status.numWantsSatisfied}\n`);
-            exit(0);
+              exit(0);
+            }
           }
         }
       };
