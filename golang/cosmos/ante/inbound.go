4e1221767a2e01b091a8a3d198be975505ee37ca
Mathieu Hofman <mathieu@agoric.com>
2022-10-04 17:17:21 +0000

    fixup! feat: Cosmos-side ante handler, storage of allowed queue size
    Add comment to address feedback

--- golang/cosmos/ante/inbound.go
+++ golang/cosmos/ante/inbound.go
@@ -40,6 +40,21 @@ func (ia inboundAnte) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next
 			*swingtypes.MsgProvision,
 			*swingtypes.MsgWalletAction,
 			*swingtypes.MsgWalletSpendAction:
+			// Lazily compute the number of allowed messages when the transaction
+			// includes a SwingSet message. This number is the difference between
+			// the number of allowed messages initially returned by SwingSet, and
+			// the number of messages already added into the actionQueue by other
+			// transactions included in the block.
+			// We store the number of allowed messages locally since messages are
+			// added to the actionQueue after going through the ante handler (in
+			// non-simulate mode) and before the next transaction is processed.
+			// However in simulate mode (mempool admission check), no state is
+			// changed so it's possible for a set of transactions to exist which
+			// if all included in a block would push the actionQueue over, and thus
+			// end up in rejections instead of simply not admitting them in the
+			// mempool. To mitigate this, Swingset should compute the number of
+			// messages allowed for mempool admission as if its max queue length
+			// was lower (e.g. 50%).
 			if inboundsAllowed == -1 {
 				state := ia.sk.GetState(ctx)
 				entry := QueueInbound
