0b4b1aac42379c68aabe807904f5bfd6670009c5
Samuel Siegart <samsiegart@gmail.com>
2022-10-14 21:07:33 -0700

    feat(web-components): add 'makeAgoricKeplrConnection' util (#6452)
    * feat(web-components): add keplr smart wallet connection component
    
    * chore: add TODOs and better error messages
    
    * chore: prettier
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ packages/web-components/src/keplr-connection/onLoad.js
@@ -0,0 +1,10 @@
+// @ts-check
+export const onLoadP = /** @type {Promise<void>} */ (
+  new Promise(res => {
+    if (document.readyState === 'complete') {
+      res();
+    } else {
+      window.addEventListener('load', () => res());
+    }
+  })
+);
