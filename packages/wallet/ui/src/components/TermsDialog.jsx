9b27cebc9f1e44bcc2079d9fa3a14b11fab919e4
samsiegart <samsiegart@gmail.com>
2022-10-25 21:19:30 -0700

    feat(wallet-ui): link out to ToS

--- packages/wallet/ui/src/components/TermsDialog.jsx
+++ packages/wallet/ui/src/components/TermsDialog.jsx
@@ -3,10 +3,11 @@ import Checkbox from '@mui/material/Checkbox';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import { useState } from 'react';
+
+import styles from './TermsDialog.module.scss';
 
 const TermsDialog = ({ onClose, isOpen }) => {
   const [isChecked, setIsChecked] = useState(false);
@@ -19,20 +20,22 @@ const TermsDialog = ({ onClose, isOpen }) => {
     <Dialog open={isOpen}>
       <DialogTitle>Disclaimer</DialogTitle>
       <DialogContent>
-        <DialogContentText>
-          Inter Protocol is a fully decentralized stable token protocol. No
-          representation or warranty is made concerning any aspect of the Inter
-          Protocol, including its suitability, quality, availability,
-          accessibility, accuracy or safety. As more fully explained in the
-          Terms of Use (available here) and the Risk Statement (available here),
-          your access to and use of the Inter Protocol is entirely at your own
-          risk and could lead to substantial losses. You take full
-          responsibility for your use of the Inter Protocol, and acknowledge
-          that you use it on the basis of your own enquiry, without solicitation
-          or inducement by Contributors (as defined in the Terms of Use).
-        </DialogContentText>
+        {/* FIXME: GET REAL TOS LINK */}
         <FormControlLabel
-          label="I understand the risks and would like to proceed."
+          label={
+            <span>
+              By ticking the box and hitting &apos;Proceed&apos; you are
+              agreeing to the terms as described{' '}
+              <a
+                className={styles.link}
+                target="agoric_wallet_tos"
+                href="https://agoric.com"
+              >
+                here
+              </a>
+              .
+            </span>
+          }
           control={
             <Checkbox
               checked={isChecked}
