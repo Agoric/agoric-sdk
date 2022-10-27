23ed9174053936b8a22219ce5f56a357b52764d5
Turadg Aleahmad <turadg@agoric.com>
2022-10-27 10:39:24 -0700

    feat(makeChainInfo)!: rpc param as authoritative

--- packages/wallet/ui/src/util/SuggestChain.js
+++ packages/wallet/ui/src/util/SuggestChain.js
@@ -6,31 +6,29 @@ export const COSMOS_COIN_TYPE = 118;
 
 /**
  * @param {string} networkConfig URL
- * @param {string} rpcAddr URL or origin
+ * @param {object} addrs
+ * @param {string} addrs.rpcAddr URL or origin
+ * @param {string} addrs.apiAddr URL
  * @param {string} chainId
  * @param {string} [caption]
  * @returns {import('@keplr-wallet/types').ChainInfo}
  */
-const makeChainInfo = (networkConfig, rpcAddr, chainId, caption) => {
+const makeChainInfo = (
+  networkConfig,
+  { rpcAddr, apiAddr },
+  chainId,
+  caption,
+) => {
   const coinType = Number(
     new URL(networkConfig).searchParams.get('coinType') || AGORIC_COIN_TYPE,
   );
   const hostname = new URL(networkConfig).hostname;
   const network = hostname.split('.')[0];
-  let rpc;
-  let api;
-
-  if (network !== hostname) {
-    rpc = `https://${network}.rpc.agoric.net`;
-    api = `https://${network}.api.agoric.net`;
-  } else {
-    rpc = rpcAddr.match(/:\/\//) ? rpcAddr : `http://${rpcAddr}`;
-    api = rpc.replace(/(:\d+)?$/, ':1317');
-  }
+  const rpc = rpcAddr.match(/:\/\//) ? rpcAddr : `http://${rpcAddr}`;
 
   return {
     rpc,
-    rest: api,
+    rest: apiAddr,
     chainId,
     chainName: caption || `Agoric ${network}`,
     stakeCurrency,
@@ -63,10 +61,20 @@ export async function suggestChain(
   if (!res.ok) {
     throw Error(`Cannot fetch network: ${res.status}`);
   }
-  const { chainName: chainId, rpcAddrs } = await res.json();
-  const rpcAddr = rpcAddrs[Math.floor(random() * rpcAddrs.length)];
 
-  const chainInfo = makeChainInfo(networkConfig, rpcAddr, chainId, caption);
+  const { chainName: chainId, apiAddrs, rpcAddrs } = await res.json();
+  assert.equal(apiAddrs.length, rpcAddrs.length);
+  const index = Math.floor(random() * rpcAddrs.length);
+
+  const rpcAddr = rpcAddrs[index];
+  const apiAddr = apiAddrs[index];
+
+  const chainInfo = makeChainInfo(
+    networkConfig,
+    { rpcAddr, apiAddr },
+    chainId,
+    caption,
+  );
   console.log('chainInfo', chainInfo);
   await keplr.experimentalSuggestChain(chainInfo);
   await keplr.enable(chainId);
