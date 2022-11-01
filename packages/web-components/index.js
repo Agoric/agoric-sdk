12c7971273bad6232945c0d0be8625f42ea979aa
Turadg Aleahmad <turadg@agoric.com>
2022-11-01 11:45:11 -0700

    chore: TS ambient for network types

--- packages/web-components/index.js
+++ packages/web-components/index.js
@@ -1,3 +1,8 @@
+// @ts-check
+
+// Ambient types. https://github.com/Agoric/agoric-sdk/issues/6512
+import '@agoric/swingset-vat/src/vats/network/types.js';
+
 export { AgoricWalletConnection } from './src/AgoricWalletConnection.js';
 export { makeAgoricKeplrConnection } from './src/keplr-connection/KeplrConnection.js';
 export { Errors as AgoricKeplrConnectionErrors } from './src/keplr-connection/errors.js';
