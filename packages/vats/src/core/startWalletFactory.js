ef1d6b91036e4450563294c62236fbe453a7f532
Michael FIG <mfig@agoric.com>
2022-11-09 11:02:00 -0600

    feat(vats): make walletFactory a coreProposal

--- packages/vats/src/core/startWalletFactory.js
+++ packages/vats/src/core/startWalletFactory.js
@@ -294,3 +294,16 @@ export const WALLET_FACTORY_MANIFEST = {
     },
   },
 };
+
+export const getManifestForWalletFactory = (
+  { restoreRef },
+  { installKeys },
+) => {
+  return {
+    manifest: WALLET_FACTORY_MANIFEST,
+    installations: {
+      provisionPool: restoreRef(installKeys.provisionPool),
+      walletFactory: restoreRef(installKeys.walletFactory),
+    },
+  };
+};
