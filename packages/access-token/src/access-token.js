175e79778c25de55fa3cc95de80c0f004a7e53e7
Dan Connolly <connolly@agoric.com>
2022-12-09 16:48:04 -0600

    refactor(access-token): makeMyAgoricDir follows OCap Discipline

--- packages/access-token/src/access-token.js
+++ packages/access-token/src/access-token.js
@@ -1,3 +1,4 @@
+// @ts-check
 import fs from 'fs';
 import crypto from 'crypto';
 import os from 'os';
@@ -5,13 +6,20 @@ import path from 'path';
 
 import { openJSONStore } from './json-store.js';
 
+const { freeze: harden } = Object; // IOU
+
 // Adapted from https://stackoverflow.com/a/43866992/14073862
-export function generateAccessToken({
-  stringBase = 'base64url',
-  byteLength = 48,
-} = {}) {
+export function generateAccessToken(
+  {
+    stringBase = /** @type {const} */ ('base64url'),
+    byteLength = 48,
+    randomBytes,
+  } = {
+    randomBytes: crypto.randomBytes,
+  },
+) {
   return new Promise((resolve, reject) =>
-    crypto.randomBytes(byteLength, (err, buffer) => {
+    randomBytes(byteLength, (err, buffer) => {
       if (err) {
         reject(err);
       } else if (stringBase === 'base64url') {
@@ -26,26 +34,61 @@ export function generateAccessToken({
   );
 }
 
+/**
+ * @param {string=} sharedStateDir
+ * @param {object} io
+ * @param {typeof fs} io.fs
+ */
+export const makeMyAgoricDir = (
+  sharedStateDir = path.join(os.homedir(), '.agoric'),
+  { fs: fsSync } = { fs },
+) => {
+  const {
+    promises: { mkdir },
+  } = fsSync;
+  return harden({
+    /**
+     * @param {string | number} port
+     * @param {object} io
+     * @param {typeof import('crypto').randomBytes} io.randomBytes
+     */
+    getAccessToken: async (
+      port,
+      { randomBytes } = { randomBytes: crypto.randomBytes },
+    ) => {
+      if (typeof port === 'string') {
+        const match = port.match(/^(.*:)?(\d+)$/);
+        if (match) {
+          port = match[2];
+        }
+      }
+
+      // Ensure we're protected with a unique accessToken for this basedir.
+      await mkdir(sharedStateDir, { mode: 0o700, recursive: true });
+
+      // TODO: pass fsSync IO access explicitly to makeJSONStore
+      // Ensure an access token exists.
+      const { storage, commit, close } = openJSONStore(sharedStateDir);
+      const accessTokenKey = `accessToken/${port}`;
+      const stored = storage.has(accessTokenKey);
+      const accessToken = await (stored
+        ? storage.get(accessTokenKey)
+        : generateAccessToken({ randomBytes }));
+      await (stored ||
+        (async () => {
+          storage.set(accessTokenKey, accessToken);
+          await commit();
+        })());
+      await close();
+      return accessToken;
+    },
+  });
+};
+
+/**
+ * @param {number|string} port
+ * @deprecated use makeMyAgoricDir for OCap Discipline
+ */
 export async function getAccessToken(port) {
-  if (typeof port === 'string') {
-    const match = port.match(/^(.*:)?(\d+)$/);
-    if (match) {
-      port = match[2];
-    }
-  }
-
-  // Ensure we're protected with a unique accessToken for this basedir.
-  const sharedStateDir = path.join(os.homedir(), '.agoric');
-  await fs.promises.mkdir(sharedStateDir, { mode: 0o700, recursive: true });
-
-  // Ensure an access token exists.
-  const { storage, commit, close } = openJSONStore(sharedStateDir);
-  const accessTokenKey = `accessToken/${port}`;
-  if (!storage.has(accessTokenKey)) {
-    storage.set(accessTokenKey, await generateAccessToken());
-    await commit();
-  }
-  const accessToken = storage.get(accessTokenKey);
-  await close();
-  return accessToken;
+  return makeMyAgoricDir().getAccessToken(port);
 }
