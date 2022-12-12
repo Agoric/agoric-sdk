5e58585f2f2c5730c81991c99cc67ff35c1440ae
Turadg Aleahmad <turadg@agoric.com>
2022-12-12 14:52:24 -0800

    test: second oracle for smoketest

--- packages/agoric-cli/test/agops-oracle-smoketest.sh
+++ packages/agoric-cli/test/agops-oracle-smoketest.sh
@@ -24,6 +24,11 @@ if [ -z "$WALLET" ]; then
 fi
 set -x
 
+# this is in economy-template.json in the oracleAddresses list (agoric1dy0yegdsev4xvce3dx7zrz2ad9pesf5svzud6y)
+# to use it run `agd keys oracle2 --interactive` and enter this mnenomic:
+# dizzy scale gentle good play scene certain acquire approve alarm retreat recycle inch journey fitness grass minimum learn funny way unlock what buzz upon
+WALLET2=oracle2
+
 # Accept invitation to admin an oracle
 ORACLE_OFFER=$(mktemp -t agops.XXX)
 bin/agops oracle accept >|"$ORACLE_OFFER"
@@ -33,7 +38,14 @@ agoric wallet send --from "$WALLET" --offer "$ORACLE_OFFER"
 agoric wallet show --from "$WALLET"
 ORACLE_OFFER_ID=$(jq ".body | fromjson | .offer.id" <"$ORACLE_OFFER")
 
-### Now we have the continuing invitationMakers saved in the wallet
+# repeat for oracle2
+ORACLE_OFFER=$(mktemp -t agops.XXX)
+bin/agops oracle accept >|"$ORACLE_OFFER"
+jq ".body | fromjson" <"$ORACLE_OFFER"
+agoric wallet send --from "$WALLET2" --offer "$ORACLE_OFFER"
+ORACLE2_OFFER_ID=$(jq ".body | fromjson | .offer.id" <"$ORACLE_OFFER")
+
+### Now we have the continuing invitationMakers saved in the wallets
 
 # Use invitation result, with continuing invitationMakers to propose a vote
 PROPOSAL_OFFER=$(mktemp -t agops.XXX)
@@ -47,3 +59,18 @@ agoric wallet show --from "$WALLET"
 
 # verify that the round started
 agoric follow :published.priceFeed.ATOM-USD_price_feed.latestRound
+
+# submit another price in the round from the second oracle
+PROPOSAL_OFFER=$(mktemp -t agops.XXX)
+bin/agops oracle pushPriceRound --price 2.01 --roundId 1 --oracleAdminAcceptOfferId "$ORACLE2_OFFER_ID" >|"$PROPOSAL_OFFER"
+jq ".body | fromjson" <"$PROPOSAL_OFFER"
+agoric wallet send --from "$WALLET2" --offer "$PROPOSAL_OFFER"
+
+# second round, first oracle
+PROPOSAL_OFFER=$(mktemp -t agops.XXX)
+bin/agops oracle pushPriceRound --price 1.02 --roundId 2 --oracleAdminAcceptOfferId "$ORACLE_OFFER_ID" >|"$PROPOSAL_OFFER"
+agoric wallet send --from "$WALLET" --offer "$PROPOSAL_OFFER"
+# second round, second oracle
+PROPOSAL_OFFER=$(mktemp -t agops.XXX)
+bin/agops oracle pushPriceRound --price 2.01 --roundId 2 --oracleAdminAcceptOfferId "$ORACLE2_OFFER_ID" >|"$PROPOSAL_OFFER"
+agoric wallet send --from "$WALLET2" --offer "$PROPOSAL_OFFER"
