db292fd96d2cbb3b74c1b97d8fc042bfa35fe27d
Richard Gibson <richard.gibson@gmail.com>
2022-12-22 13:40:19 -0500

    test(zoe): Export AmountMath from the ERTP vat

--- packages/zoe/test/swingsetTests/upgradeCoveredCall/vat-ertp-service.js
+++ packages/zoe/test/swingsetTests/upgradeCoveredCall/vat-ertp-service.js
@@ -1,9 +1,12 @@
 import { Far } from '@endo/marshal';
+import { AmountMath } from '@agoric/ertp';
 import { vivifyErtpService } from '@agoric/ertp/test/swingsetTests/ertpService/vat-ertp-service.js';
 
 export const buildRootObject = async (vatPowers, _vatParams, baggage) => {
+  const exportableAmountMath = Far('AmountMath', { ...AmountMath });
   const ertpService = vivifyErtpService(baggage, vatPowers.exitVatWithFailure);
   return Far('root', {
     getErtpService: () => ertpService,
+    getAmountMath: () => exportableAmountMath,
   });
 };
