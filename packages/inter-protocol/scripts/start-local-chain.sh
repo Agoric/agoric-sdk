ca9a18d43ca7016cf06df17e58e271c4682abdc7
Mathieu Hofman <mathieu@agoric.com>
2022-12-29 07:14:03 +0000

    fix(inter-protocol): linux compat use test keyring backend mktemp needs X

--- packages/inter-protocol/scripts/start-local-chain.sh
+++ packages/inter-protocol/scripts/start-local-chain.sh
@@ -7,7 +7,7 @@
 # (the Makefile passes this to agd start)
 export AGC_START_ARGS="--pruning=nothing"
 
-CHAIN_LOG=$(mktemp -t Agoric-start-local-chain-log)
+CHAIN_LOG=$(mktemp -t Agoric-start-local-chain-log-XXXXXXX)
 if [[ $(uname) == "Darwin" ]]; then
     open -a /System/Applications/Utilities/Console.app "$CHAIN_LOG"
 fi
@@ -23,7 +23,7 @@ if [ -z "$WALLET" ]; then
     exit 1
 fi
 
-WALLET_BECH32=$(agd keys show "$WALLET" --output json | jq -r .address)
+WALLET_BECH32=$(agd keys --keyring-backend=test show "$WALLET" --output json | jq -r .address)
 
 echo CHAIN_LOG "$CHAIN_LOG"
 echo SDK "$SDK"
@@ -59,18 +59,18 @@ make ACCT_ADDR="$WALLET_BECH32" FUNDS=20000000ubld,20000000ibc/usdc1234 fund-acc
 agd query bank balances "$WALLET_BECH32" | grep ubld || exit 1
 
 echo "Provisioning your smart wallet..."
-agoric wallet provision --spend --account "$WALLET"
+agoric wallet --keyring-backend=test provision --spend --account "$WALLET"
 echo "waiting for blocks"
 sleep 15
 # verify
-agoric wallet list
-agoric wallet show --from "$WALLET"
+agoric wallet --keyring-backend=test list  
+agoric wallet --keyring-backend=test show --from "$WALLET"  
 
 echo "Repeating for oracle2 account..."
 # this is in economy-template.json in the oracleAddresses list (agoric1dy0yegdsev4xvce3dx7zrz2ad9pesf5svzud6y)
-# to use it run `agd keys oracle2 --interactive` and enter this mnenomic:
+# to use it run `agd keys --keyring-backend=test oracle2 --interactive` and enter this mnenomic:
 # dizzy scale gentle good play scene certain acquire approve alarm retreat recycle inch journey fitness grass minimum learn funny way unlock what buzz upon
 WALLET2=oracle2
-WALLET2_BECH32=$(agd keys show "$WALLET2" --output json | jq -r .address)
+WALLET2_BECH32=$(agd keys --keyring-backend=test show "$WALLET2" --output json | jq -r .address)
 make ACCT_ADDR="$WALLET2_BECH32" FUNDS=20000000ubld,20000000ibc/usdc1234 fund-acct
-agoric wallet provision --spend --account "$WALLET2"
+agoric wallet  --keyring-backend=test provision --spend --account "$WALLET2"
