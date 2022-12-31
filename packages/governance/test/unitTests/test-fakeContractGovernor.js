5ce06a326a5e64f7504879e2b33d06eb22a7bad3
Chris Hibbert <hibbert@agoric.com>
2022-12-30 16:29:05 -0800

    test: add a fakeContractGovernor that doesn't need an electorate

---
+++ packages/governance/test/unitTests/test-fakeContractGovernor.js
@@ -0,0 +1,237 @@
+import { test } from '@agoric/zoe/tools/prepare-test-env-ava.js';
+import '@agoric/zoe/exported.js';
+
+import { makeNotifierFromAsyncIterable } from '@agoric/notifier';
+import { makeZoeKit } from '@agoric/zoe';
+import bundleSource from '@endo/bundle-source';
+import buildManualTimer from '@agoric/zoe/tools/manualTimer.js';
+import { makeFakeVatAdmin } from '@agoric/zoe/tools/fakeVatAdmin.js';
+import { E } from '@endo/eventual-send';
+
+import { resolve as importMetaResolve } from 'import-meta-resolve';
+import { MALLEABLE_NUMBER } from '../swingsetTests/contractGovernor/governedContract.js';
+import { CONTRACT_ELECTORATE, ParamTypes } from '../../src/index.js';
+
+const governedRoot = '../swingsetTests/contractGovernor/governedContract.js';
+const contractGovernorRoot = '../../tools/fakeContractGovernor.js';
+const autoRefundRoot = '@agoric/zoe/src/contracts/automaticRefund.js';
+
+const makeBundle = async sourceRoot => {
+  const url = await importMetaResolve(sourceRoot, import.meta.url);
+  const path = new URL(url).pathname;
+  const contractBundle = await bundleSource(path);
+  return contractBundle;
+};
+
+// makeBundle is a slow step, so we do it once for all the tests.
+const contractGovernorBundleP = makeBundle(contractGovernorRoot);
+const governedBundleP = makeBundle(governedRoot);
+// could be called fakeCommittee. It's used as a source of invitations only
+const autoRefundBundleP = makeBundle(autoRefundRoot);
+
+const setUpZoeForTest = async setJig => {
+  const makeFar = o => o;
+
+  /**
+   * These properties will be assigned by `setJig` in the contract.
+   *
+   * @typedef {object} TestContext
+   * @property {ZCF} zcf
+   * @property {IssuerRecord} mintedIssuerRecord
+   * @property {IssuerRecord} govIssuerRecord
+   */
+  const { zoeService, feeMintAccess: nonFarFeeMintAccess } = makeZoeKit(
+    makeFakeVatAdmin(setJig, o => makeFar(o)).admin,
+  );
+  /** @type {ERef<ZoeService>} */
+  const zoe = makeFar(zoeService);
+  const feeMintAccess = await makeFar(nonFarFeeMintAccess);
+  return {
+    zoe,
+    feeMintAccess,
+  };
+};
+
+const installBundle = (zoe, contractBundle) => E(zoe).install(contractBundle);
+
+// contract governor wants a committee invitation. give it a random invitation
+async function getInvitation(zoe, autoRefundInstance) {
+  const autoRefundFacets = await E(zoe).startInstance(autoRefundInstance);
+  const invitationP = E(autoRefundFacets.publicFacet).makeInvitation();
+  const [fakeInvitationPayment, fakeInvitationAmount] = await Promise.all([
+    invitationP,
+    E(E(zoe).getInvitationIssuer()).getAmountOf(invitationP),
+  ]);
+  return { fakeInvitationPayment, fakeInvitationAmount };
+}
+
+const setUpGovernedContract = async (zoe, electorateTerms, timer) => {
+  const [contractGovernorBundle, autoRefundBundle, governedBundle] =
+    await Promise.all([
+      contractGovernorBundleP,
+      autoRefundBundleP,
+      governedBundleP,
+    ]);
+
+  const [governor, autoRefund, governed] = await Promise.all([
+    installBundle(zoe, contractGovernorBundle),
+    installBundle(zoe, autoRefundBundle),
+    installBundle(zoe, governedBundle),
+  ]);
+  const installs = { governor, autoRefund, governed };
+  const { fakeInvitationPayment, fakeInvitationAmount } = await getInvitation(
+    zoe,
+    autoRefund,
+  );
+
+  const governedTerms = {
+    governedParams: {
+      [MALLEABLE_NUMBER]: {
+        type: ParamTypes.NAT,
+        value: 602214090000000000000000n,
+      },
+      [CONTRACT_ELECTORATE]: {
+        type: ParamTypes.INVITATION,
+        value: fakeInvitationAmount,
+      },
+    },
+    governedApis: ['governanceApi'],
+  };
+  const governorTerms = {
+    timer,
+    governedContractInstallation: governed,
+    governed: {
+      terms: governedTerms,
+      issuerKeywordRecord: {},
+    },
+  };
+
+  const governorFacets = await E(zoe).startInstance(
+    governor,
+    {},
+    governorTerms,
+    {
+      governed: {
+        initialPoserInvitation: fakeInvitationPayment,
+      },
+    },
+  );
+
+  return { governorFacets, installs };
+};
+
+test('multiple params bad change', async t => {
+  const { zoe } = await setUpZoeForTest(() => {});
+  const timer = buildManualTimer(t.log);
+  const { governorFacets } = await setUpGovernedContract(
+    zoe,
+    { committeeName: 'Demos', committeeSize: 1 },
+    timer,
+  );
+
+  const paramChangesSpec = harden({
+    paramPath: { key: 'governedParams' },
+    changes: {
+      [CONTRACT_ELECTORATE]: 13n,
+      [MALLEABLE_NUMBER]: 42n,
+    },
+  });
+
+  await t.throwsAsync(
+    () => E(governorFacets.creatorFacet).changeParams(paramChangesSpec),
+    {
+      message:
+        'In "getAmountOf" method of (Zoe Invitation issuer): arg 0: bigint "[13n]" - Must be a remotable (Payment)',
+    },
+  );
+});
+
+test('change a param', async t => {
+  const { zoe } = await setUpZoeForTest(() => {});
+  const timer = buildManualTimer(t.log);
+  const { governorFacets, installs } = await setUpGovernedContract(
+    zoe,
+    { committeeName: 'Demos', committeeSize: 1 },
+    timer,
+  );
+
+  /** @type {GovernedPublicFacet<unknown>} */
+  const publicFacet = await E(governorFacets.creatorFacet).getPublicFacet();
+  const notifier = makeNotifierFromAsyncIterable(
+    await E(publicFacet).getSubscription(),
+  );
+  const update1 = await notifier.getUpdateSince();
+  t.is(
+    // @ts-expect-error reaching into unknown values
+    update1.value.current.Electorate.value.value[0].description,
+    'getRefund',
+  );
+  t.like(update1, {
+    value: {
+      current: {
+        MalleableNumber: { type: 'nat', value: 602214090000000000000000n },
+      },
+    },
+  });
+
+  // This is the wrong kind of invitation, but governance can't tell
+  const { fakeInvitationPayment, fakeInvitationAmount } = await getInvitation(
+    zoe,
+    installs.autoRefund,
+  );
+
+  const paramChangesSpec = harden({
+    paramPath: { key: 'governedParams' },
+    changes: {
+      [CONTRACT_ELECTORATE]: fakeInvitationPayment,
+      [MALLEABLE_NUMBER]: 42n,
+    },
+  });
+
+  E(governorFacets.creatorFacet).changeParams(paramChangesSpec);
+
+  const update2 = await notifier.getUpdateSince(update1.updateCount);
+  t.like(update2, {
+    value: {
+      current: {
+        MalleableNumber: { type: 'nat', value: 42n },
+      },
+    },
+  });
+
+  const paramsAfter = await E(publicFacet).getGovernedParams();
+  t.deepEqual(paramsAfter.Electorate.value, fakeInvitationAmount);
+  t.is(paramsAfter.MalleableNumber.value, 42n);
+});
+
+test('set offer Filter directly', async t => {
+  const { zoe } = await setUpZoeForTest(() => {});
+  const timer = buildManualTimer(t.log);
+  const { governorFacets } = await setUpGovernedContract(
+    zoe,
+    { committeeName: 'Demos', committeeSize: 1 },
+    timer,
+  );
+
+  await E(governorFacets.creatorFacet).setFilters(['whatever']);
+  t.deepEqual(
+    await E(zoe).getOfferFilter(governorFacets.creatorFacet.getInstance()),
+    ['whatever'],
+  );
+});
+
+test('call API directly', async t => {
+  const { zoe } = await setUpZoeForTest(() => {});
+  const timer = buildManualTimer(t.log);
+  const { governorFacets } = await setUpGovernedContract(
+    zoe,
+    { committeeName: 'Demos', committeeSize: 1 },
+    timer,
+  );
+
+  await E(governorFacets.creatorFacet).invokeAPI('governanceApi', []);
+  t.deepEqual(
+    await E(E(governorFacets.creatorFacet).getPublicFacet()).getApiCalled(),
+    1,
+  );
+});
---
+++ packages/governance/tools/fakeContractGovernor.js
@@ -0,0 +1,95 @@
+import '../src/types-ambient.js';
+
+import { E } from '@endo/eventual-send';
+import { Far } from '@endo/marshal';
+
+// eslint-disable-next-line no-unused-vars
+import { CONTRACT_ELECTORATE } from '../src/contractGovernance/governParam.js';
+
+// @file fakeContractGovernor is a simplified copy of ContractGovernor to
+// simplify testing. It has no electorate, and doesn't try to support
+// legibility. It does try to ensure that the governed contract can get complete
+// support for governance of parameters, apis, and filters. Tests should be able
+// to update parameters directly.
+
+/**
+ * @template {() => {creatorFacet: GovernorFacet<any>, publicFacet: GovernedPublicFacetMethods} } SF Start function of governed contract
+ * @param {ZCF<{
+ *   timer: TimerService,
+ *   governedContractInstallation: Installation<SF>,
+ *   governed: {
+ *     issuerKeywordRecord: IssuerKeywordRecord,
+ *     terms: {governedParams: {[CONTRACT_ELECTORATE]: Amount<'set'>}},
+ *     privateArgs: Record<string, unknown>,
+ *   }
+ * }>} zcf
+ * @param {{
+ *   governed: Record<string, unknown>
+ * }} privateArgs
+ */
+const start = async (zcf, privateArgs) => {
+  const zoe = zcf.getZoeService();
+  const {
+    governedContractInstallation,
+    governed: {
+      issuerKeywordRecord: governedIssuerKeywordRecord,
+      terms: contractTerms,
+    },
+  } = zcf.getTerms();
+
+  const augmentedTerms = harden({
+    ...contractTerms,
+  });
+
+  const {
+    creatorFacet: governedCF,
+    instance: governedInstance,
+    publicFacet: governedPF,
+    adminFacet,
+  } = await E(zoe).startInstance(
+    governedContractInstallation,
+    governedIssuerKeywordRecord,
+    augmentedTerms,
+    privateArgs.governed,
+  );
+
+  const paramMgrRetriever = E(governedCF).getParamMgrRetriever();
+
+  // In this fake, the ability to udpate params is not closely held.
+  const limitedCreatorFacet = E(governedCF).getLimitedCreatorFacet();
+
+  /** @param {ParamChangesSpec<any>} paramSpec */
+  const changeParams = paramSpec => {
+    const paramMgr = E(paramMgrRetriever).get(paramSpec.paramPath);
+    return E(paramMgr).updateParams(paramSpec.changes);
+  };
+
+  /** @param {Array<string>} strings */
+  const setFilters = strings => E(governedCF).setOfferFilter(strings);
+
+  /**
+   * @param {string} apiMethodName
+   * @param {unknown[]} methodArgs
+   */
+  const invokeAPI = (apiMethodName, methodArgs) =>
+    E(E(governedCF).getGovernedApis())[apiMethodName](...methodArgs);
+
+  const creatorFacet = Far('governor creatorFacet', {
+    changeParams,
+    invokeAPI,
+    setFilters,
+    getCreatorFacet: () => limitedCreatorFacet,
+    getAdminFacet: () => adminFacet,
+    getInstance: () => governedInstance,
+    getPublicFacet: () => governedPF,
+  });
+
+  const publicFacet = Far('contract governor public', {
+    getGovernedContract: () => governedInstance,
+  });
+
+  return { creatorFacet, publicFacet };
+};
+
+harden(start);
+export { start };
