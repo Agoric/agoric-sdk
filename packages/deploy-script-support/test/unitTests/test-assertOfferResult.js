1bdcd8c61bc4769ff63becc73b91e1a6d024ec52
Turadg Aleahmad <turadg@agoric.com>
2023-01-20 13:44:15 -0800

    test: drop dep on prepare-test-env-ava

--- packages/deploy-script-support/test/unitTests/test-assertOfferResult.js
+++ packages/deploy-script-support/test/unitTests/test-assertOfferResult.js
@@ -1,4 +1,5 @@
-import { test } from '@agoric/swingset-vat/tools/prepare-test-env-ava.js';
+import test from 'ava';
+import '@endo/init';
 
 import { assertOfferResult } from '../../src/assertOfferResult.js';
 
