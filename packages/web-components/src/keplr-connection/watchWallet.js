647ced9b2f9d063beef4fede9a60162e5995e18b
samsiegart <samsiegart@gmail.com>
2023-01-26 12:40:46 -0800

    feat(web-components): read purses from bank instead of smart wallet

--- packages/web-components/src/keplr-connection/watchWallet.js
+++ packages/web-components/src/keplr-connection/watchWallet.js
@@ -1,14 +1,16 @@
 // @ts-check
-import { makeFollower } from '@agoric/casting';
+import { makeFollower, iterateLatest } from '@agoric/casting';
 import { makeNotifierKit } from '@agoric/notifier';
-
-import { fetchCurrent } from './fetchCurrent';
-import { followLatest } from './followLatest';
+import { AmountMath } from '@agoric/ertp';
+import { assertHasData } from '@agoric/smart-wallet/src/utils';
 import { Errors } from './errors';
+import { queryBankBalances } from './queryBankBalances';
 
 /** @typedef {import('./fetchCurrent').PurseInfo} PurseInfo */
 
-export const watchWallet = async (leader, address, context) => {
+const POLL_INTERVAL_MS = 6000;
+
+export const watchWallet = async (leader, address, context, rpcs) => {
   const followPublished = path =>
     makeFollower(`:published.${path}`, leader, {
       unserializer: context.fromMyWallet,
@@ -30,21 +32,59 @@ export const watchWallet = async (leader, address, context) => {
     pursesNotifierKit.updater.updateState(harden(purses));
   };
 
-  const currentFollower = followPublished(`wallet.${address}.current`);
-  const { blockHeight, brandToPurse } = await fetchCurrent(
-    currentFollower,
-  ).catch(() => {
+  const currentFollower = await followPublished(`wallet.${address}.current`);
+  try {
+    // eslint-disable-next-line @jessie.js/no-nested-await
+    await assertHasData(currentFollower);
+  } catch {
     throw new Error(Errors.noSmartWallet);
-  });
-  updatePurses(brandToPurse);
+  }
 
-  const latestFollower = followPublished(`wallet.${address}`);
-  followLatest({
-    startingHeight: blockHeight,
-    latestFollower,
-    updatePurses,
-    brandToPurse,
-  });
+  const watchChainBalances = () => {
+    const brandToPurse = new Map();
+    let vbankAssets;
+    let bank;
+
+    const possiblyUpdateBankPurses = () => {
+      if (!vbankAssets || !bank) return;
+
+      const bankMap = new Map(bank.map(({ denom, amount }) => [denom, amount]));
+
+      vbankAssets.forEach(([denom, info]) => {
+        const amount = bankMap.get(denom) ?? 0n;
+        const purseInfo = {
+          brand: info.brand,
+          currentAmount: AmountMath.make(info.brand, BigInt(amount)),
+          brandPetname: info.issuerName,
+          pursePetname: info.issuerName,
+          displayInfo: info.displayInfo,
+        };
+        brandToPurse.set(info.brand, purseInfo);
+      });
+
+      updatePurses(brandToPurse);
+    };
+
+    const watchBank = async () => {
+      const balances = await queryBankBalances(address, rpcs[0]);
+      bank = balances;
+      possiblyUpdateBankPurses();
+      setTimeout(watchBank, POLL_INTERVAL_MS);
+    };
+
+    const watchVbankAssets = async () => {
+      const vbankAssetsFollower = followPublished('agoricNames.vbankAsset');
+      for await (const { value } of iterateLatest(vbankAssetsFollower)) {
+        vbankAssets = value;
+        possiblyUpdateBankPurses();
+      }
+    };
+
+    void watchVbankAssets();
+    void watchBank();
+  };
+
+  watchChainBalances();
 
   return {
     pursesNotifier: pursesNotifierKit.notifier,
