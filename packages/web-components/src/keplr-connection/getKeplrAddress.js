8766c265f51068d789b448518f764a2824008eb3
Turadg Aleahmad <turadg@agoric.com>
2023-01-28 07:17:20 -0800

    chore(types): getKeplrAddress

--- packages/web-components/src/keplr-connection/getKeplrAddress.js
+++ packages/web-components/src/keplr-connection/getKeplrAddress.js
@@ -1,15 +1,26 @@
+// @ts-check
 import { onLoadP } from './onLoad';
 import { Errors } from './errors';
 
+/**
+ * @param {string} chainId
+ */
 export const getKeplrAddress = async chainId => {
   await onLoadP;
+  if (!('keplr' in window)) {
+    throw new Error(Errors.noKeplr);
+  }
+  /** @type {import('@keplr-wallet/types').Keplr} */
+  // @ts-expect-error cast (checked above)
+  const keplr = window.keplr;
+
   try {
     // eslint-disable-next-line @jessie.js/no-nested-await
-    await window.keplr.enable(chainId);
+    await keplr.enable(chainId);
   } catch {
     throw new Error(Errors.enableKeplr);
   }
-  const offlineSigner = window.getOfflineSigner(chainId);
+  const offlineSigner = keplr.getOfflineSigner(chainId);
 
   const accounts = await offlineSigner.getAccounts();
 
