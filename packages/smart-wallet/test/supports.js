be5ecf8ef0a49d2da5cec49e03254850568178b6
Dan Connolly <connolly@agoric.com>
2023-02-02 09:20:08 -0600

    test(smart-wallet): provide baggage when creating board vat root

--- packages/smart-wallet/test/supports.js
+++ packages/smart-wallet/test/supports.js
@@ -21,6 +21,7 @@ import { eventLoopIteration } from '@agoric/zoe/tools/eventLoopIteration.js';
 import { makeFakeVatAdmin } from '@agoric/zoe/tools/fakeVatAdmin.js';
 import { makeLoopback } from '@endo/captp';
 import { E, Far } from '@endo/far';
+import { makeScalarBigMapStore } from '@agoric/vat-data';
 
 export { ActionType };
 
@@ -128,8 +129,10 @@ export const makeMockTestSpace = async log => {
     switch (name) {
       case 'mints':
         return mintsRoot();
-      case 'board':
-        return boardRoot();
+      case 'board': {
+        const baggage = makeScalarBigMapStore('baggage');
+        return boardRoot({}, {}, baggage);
+      }
       default:
         throw Error('unknown loadVat name');
     }
