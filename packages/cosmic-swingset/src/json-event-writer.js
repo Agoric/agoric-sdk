684c7511527c1768f6b3795a2ba5bf37ed049391
Mathieu Hofman <mathieu@agoric.com>
2023-02-05 19:02:37 +0000

    feat(cosmic-swingset): buffer chain transcript until commit

--- packages/cosmic-swingset/src/json-event-writer.js
+++ packages/cosmic-swingset/src/json-event-writer.js
@@ -1,5 +1,40 @@
 import { performance } from 'node:perf_hooks';
 
+/** @param {import('@agoric/internal').FsStreamWriter | undefined} stream */
+export const bufferStreamWrites = stream => {
+  if (!stream) {
+    return undefined;
+  }
+
+  /** @type {string[]} */
+  const buffer = [];
+
+  let closed = false;
+
+  const write = async data => {
+    if (closed) throw Error('Stream closed');
+
+    buffer.push(data);
+  };
+
+  const flush = async () => {
+    await buffer.splice(0).reduce(async (prev, data) => {
+      await prev;
+      await stream.write(data);
+    }, Promise.resolve());
+
+    await stream.flush();
+  };
+
+  const close = async () => {
+    closed = true;
+    await flush();
+    await stream.close();
+  };
+
+  return harden({ write, flush, close });
+};
+
 /** @param {{write: (data: string) => Promise<void>}} stream */
 export const makeJSONEventWriter = stream => {
   return harden({
