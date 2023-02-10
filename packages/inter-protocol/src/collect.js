98c5b62c3c99808894649194e8058f0e395807d6
Turadg Aleahmad <turadg@agoric.com>
2023-02-10 09:13:28 -0800

    chore(types): allValues support heterogenous mapping

--- packages/inter-protocol/src/collect.js
+++ packages/inter-protocol/src/collect.js
@@ -3,7 +3,7 @@ const { fromEntries, keys, values } = Object;
 /** @type { <X, Y>(xs: X[], ys: Y[]) => [X, Y][]} */
 export const zip = (xs, ys) => harden(xs.map((x, i) => [x, ys[+i]]));
 
-/** @type { <K extends string, V>(obj: Record<K, ERef<V>>) => Promise<Record<K, V>> } */
+/** @type { <T extends Record<string, ERef<any>>>(obj: T) => Promise<{ [K in keyof T]: Awaited<T[K]>}> } */
 export const allValues = async obj => {
   const resolved = await Promise.all(values(obj));
   // @ts-expect-error cast
