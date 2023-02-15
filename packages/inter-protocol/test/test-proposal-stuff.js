787f045584e74ae16de8014cfd0fb12021c08aff
Dan Connolly <connolly@agoric.com>
2023-02-15 13:54:41 -0600

    test(inter-protocol): DRY test config

--- packages/inter-protocol/test/test-proposal-stuff.js
+++ packages/inter-protocol/test/test-proposal-stuff.js
@@ -1,80 +1,107 @@
 // @ts-check
-import { test } from '@agoric/zoe/tools/prepare-test-env-ava.js';
+import { test as anyTest } from '@agoric/zoe/tools/prepare-test-env-ava.js';
+import * as ambientFs from 'fs';
+import { resolve as importMetaResolve } from 'import-meta-resolve';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { defaultProposalBuilder } from '@agoric/inter-protocol/scripts/init-core.js';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { defaultProposalBuilder as collateralBuilder } from '@agoric/inter-protocol/scripts/add-collateral-core.js';
+const configSpecifier = '@agoric/vats/decentral-test-vaults-config.json';
+const noop = harden(() => {});
 
-// XXX dynamic import??
-// lifted from packages/vats/decentral-test-vaults-config.json
-const prop2 = {
-  module: '@agoric/inter-protocol/scripts/init-core.js',
-  entrypoint: 'defaultProposalBuilder',
-  args: [
-    {
-      econCommitteeOptions: {
-        committeeSize: 3,
-      },
-      endorsedUi: 'deadbeef',
-      initialPricePct: 12_34,
-      minInitialPoolLiquidity: '0',
-    },
-  ],
+/** @type {import('ava').TestFn<ReturnType<typeof makeTestContext>>} */
+const test = anyTest;
+
+// #region test setup, isolating ambient authority
+const makeTestContext = t => {
+  /** @param {string} specifier */
+  const loadConfig = async specifier => {
+    const fullPath = await importMetaResolve(specifier, import.meta.url).then(
+      u => new URL(u).pathname,
+    );
+    t.is(typeof fullPath, 'string');
+    const txt = await ambientFs.promises.readFile(fullPath, 'utf-8');
+    t.is(typeof txt, 'string');
+    return JSON.parse(txt);
+  };
+
+  /** @param {{ module: string, entrypoint: string}} proposal */
+  const loadEntryPoint = async proposal => {
+    const { module, entrypoint } = proposal;
+    t.is(typeof module, 'string');
+    t.is(typeof entrypoint, 'string');
+    const modNS = await import(module);
+    t.is(typeof modNS, 'object');
+    const fn = modNS[entrypoint];
+    t.is(typeof fn, 'function');
+    return fn;
+  };
+
+  return { loadConfig, loadEntryPoint };
 };
 
-const collateralProp = {
-  module: '@agoric/inter-protocol/scripts/add-collateral-core.js',
-  entrypoint: 'defaultProposalBuilder',
-  args: [
-    {
-      interchainAssetOptions: {
-        denom: 'ibc/toyatom',
-        decimalPlaces: 4,
-        initialPricePct: 1234,
-        keyword: 'IbcATOM',
-        oracleBrand: 'ATOM',
-        proposedName: 'ATOM',
-      },
-    },
-  ],
-};
+test.before(async t => {
+  t.context = makeTestContext(t);
+});
+// #endregion
 
-test('inter-protocol proposal handles endorsedUi option', async t => {
-  const publishRef = () => {};
-  const install = () => {};
-  const actual = await defaultProposalBuilder(
-    { publishRef, install },
-    ...prop2.args,
+test('inter-protocol vaults proposal handles endorsedUi option', async t => {
+  const proposalModule = '@agoric/inter-protocol/scripts/init-core.js';
+  const endorsedUi =
+    'bafybeidvpbtlgefi3ptuqzr2fwfyfjqfj6onmye63ij7qkrb4yjxekdh3e';
+
+  t.log('loading', configSpecifier);
+  const { coreProposals } = await t.context.loadConfig(configSpecifier);
+
+  const vaultsProposal = coreProposals.find(p => p.module === proposalModule);
+
+  t.log('loading', vaultsProposal.module);
+  const builder = await t.context.loadEntryPoint(vaultsProposal);
+  const actual = await builder(
+    { publishRef: noop, install: noop },
+    ...vaultsProposal.args,
   );
+
   t.like(actual, {
     getManifestCall: {
       0: 'getManifestForInterProtocol',
-      1: { endorsedUi: 'deadbeef', minInitialPoolLiquidity: 0n },
+      1: {
+        endorsedUi,
+        minInitialPoolLiquidity: 0n,
+      },
     },
   });
 });
 
-test('inter-protocol proposal handles initialPricePct option', async t => {
-  const publishRef = () => {};
-  const install = () => {};
-  const actual = await collateralBuilder(
-    { publishRef, install, wrapInstall: undefined },
-    ...collateralProp.args,
+test('inter-protocol ATOM proposal handles initialPricePct option', async t => {
+  const proposalModule =
+    '@agoric/inter-protocol/scripts/add-collateral-core.js';
+  const interchainAssetOptions = {
+    decimalPlaces: 6,
+    denom: 'ibc/toyatom',
+    initialPricePct: 1234,
+    keyword: 'IbcATOM',
+    oracleBrand: 'ATOM',
+    proposedName: 'ATOM',
+  };
+
+  t.log('loading', configSpecifier);
+  const { coreProposals } = await t.context.loadConfig(configSpecifier);
+
+  const collateralProposal = coreProposals.find(
+    ({ module, entrypoint }) =>
+      module === proposalModule && entrypoint === 'defaultProposalBuilder',
   );
-  //   t.deepEqual(actual, {});
+
+  t.log('loading', collateralProposal.module);
+  const builder = await t.context.loadEntryPoint(collateralProposal);
+  const actual = await builder(
+    { publishRef: noop, install: noop, wrapInstall: undefined },
+    ...collateralProposal.args,
+  );
+
   t.like(actual, {
     getManifestCall: {
       0: 'getManifestForAddAssetToVault',
       1: {
-        interchainAssetOptions: {
-          decimalPlaces: 4,
-          denom: 'ibc/toyatom',
-          initialPricePct: 1234,
-          keyword: 'IbcATOM',
-          oracleBrand: 'ATOM',
-          proposedName: 'ATOM',
-        },
+        interchainAssetOptions,
       },
     },
   });
