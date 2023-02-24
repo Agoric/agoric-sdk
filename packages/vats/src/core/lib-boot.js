b639175147f16199805917dd680ae6c251c6d0ba
Dan Connolly <connolly@agoric.com>
2023-02-24 14:49:15 -0600

    chore(vats): CORE_EVAL expects utils module
    With an empty `modules` bootstrap power,
    
    `Cannot read properties of undefined (reading 'runModuleBehaviors')`
    
    ... was thrown in the code generated by makeWriteCoreProposal()
    in code-gen.js
    
    The initial symptoms were pretty obscure:
    
    ```
    KERNEL PANIC: kp40.policy panic: rejected {"body":"#{\"#error\":\"behavior: cannot coerce undefined to object\"
    ```
    
    These debug settings gave us a useful stack trace:
    
    `packages/solo/test$ DEBUG=SwingSet:ls,SwingSet:vat,track-turns ./startsolo.sh`
    
    The offending line is referring to `runModuleBehaviors` as a module global.
    So `Cannot read properties of undefined` seems to be SES-shim-speak for
    "Reference Error".

--- packages/vats/src/core/lib-boot.js
+++ packages/vats/src/core/lib-boot.js
@@ -32,6 +32,7 @@ const { Fail, quote: q } = assert;
 /**
  * @typedef {(powers: *, config?: *) => Promise<void>} BootBehavior
  * @typedef {Record<string, unknown>} ModuleNamespace
+ * @typedef {{ utils: typeof import('./utils.js') } & Record<string, Record<string, any>>} BootModules
  */
 
 /** @type {<X>(a: X[], b: X[]) => X[]} */
@@ -45,7 +46,7 @@ const setDiff = (a, b) => a.filter(x => !b.includes(x));
  * @param {Record<string, unknown>} vatParameters
  * @param {BootstrapManifest} bootManifest
  * @param {Record<string, BootBehavior>} behaviors
- * @param {Record<string, ModuleNamespace>} modules
+ * @param {BootModules} modules
  */
 export const makeBootstrap = (
   vatPowers,
