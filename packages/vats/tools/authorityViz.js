8c1a23cf427f73d7b9f8d16aaf8055a5689ebc0d
Dan Connolly <connolly@agoric.com>
2023-02-24 19:53:59 -0600

    chore(vats): restore authorityViz.js to working order
     - misc static types
     - note TODO ideas
     - make executable

--- packages/vats/tools/authorityViz.js
+++ packages/vats/tools/authorityViz.js
@@ -1,10 +1,14 @@
+#!/usr/bin/env node
 // @ts-check
 import '@endo/init';
 import process from 'process';
 
-import * as manifests from './core/manifest.js';
+const { entries, keys, values } = Object;
 
-const { entries } = Object;
+const logged = label => x => {
+  console.error(label, x);
+  return x;
+};
 
 const styles = {
   vatPowers: 'shape=star, style=filled, fillcolor=aqua',
@@ -62,17 +66,17 @@ function* fmtGraph(nodes, neighbors) {
 /**
  *
  * @param {Record<string, Permit>} manifest
- * @typedef {{
- *   vatParameters?: Record<string, unknown>,
- *   vatPowers?: Record<string, boolean>
- *   vats?: Record<string, boolean>
- *   devices?: Record<string, boolean>
+ * @typedef { true | {
+ *   vatParameters?: Record<string, Permit>,
+ *   vatPowers?: Record<string, true>
+ *   vats?: Record<string, Status>
+ *   devices?: Record<string, true>
  *   home?: PowerSpace,
  *   issuer?: PowerSpace,
  *   brand?: PowerSpace,
+ *   oracleBrand?: PowerSpace,
  *   installation?: PowerSpace,
  *   instance?: PowerSpace,
- *   runBehaviors: Function,
  * } & PowerSpace } Permit
  * @typedef {{produce?: Record<string, Status>, consume?: Record<string, Status>}} PowerSpace
  * @typedef { boolean | VatName } Status
@@ -89,6 +93,7 @@ const manifest2graph = manifest => {
    * @param {string} [style]
    */
   const addEdge = (src, dest, style = '') => {
+    logged('addEdge')({ src, dest });
     if (!neighbors.has(src)) {
       neighbors.set(src, new Set());
     }
@@ -98,89 +103,143 @@ const manifest2graph = manifest => {
   /**
    * @param {string} src
    * @param {string} ty
-   * @param {Record<string, Status> | undefined} item
+   * @param {Permit} item
    * @param {boolean} [reverse=false]
    */
   const level1 = (src, ty, item, reverse = false) => {
-    if (item) {
-      for (const [powerName, status] of entries(item)) {
-        // subsumed by permits for vat:, home:, ...
-        if (
-          [
-            'loadVat',
-            'loadCriticalVat',
-            'client',
-            'agoricNames',
-            'nameHubs',
-            'nameAdmins',
-          ].includes(powerName) &&
-          reverse
-        )
-          continue;
-        if (status) {
-          let cluster = {};
-          if (typeof status !== 'boolean') {
-            cluster = { cluster: status };
-          }
+    if (!item) return;
+    for (const [powerName, status] of entries(item)) {
+      // subsumed by permits for vat:, home:, ...
+      if (
+        [
+          'loadVat',
+          'loadCriticalVat',
+          'client',
+          'agoricNames',
+          'nameHubs',
+          'nameAdmins',
+        ].includes(powerName) &&
+        reverse
+      )
+        continue;
+      if (status) {
+        let cluster = {};
+        if (typeof status !== 'boolean') {
+          cluster = { cluster: status };
+        }
 
-          nodes.add({
-            id: `${ty}.${powerName}`,
-            label: powerName,
-            style: styles[ty],
-            ...cluster,
-          });
-          addEdge(src, `${ty}.${powerName}`, reverse ? 'dir=back' : '');
-          if (ty === 'home') {
-            nodes.add({ id: 'home', label: 'home', cluster: 'provisioning' });
-            addEdge('home', `${ty}.${powerName}`);
-          }
+        nodes.add({
+          id: `${ty}.${powerName}`,
+          label: powerName,
+          style: styles[ty],
+          ...cluster,
+        });
+        addEdge(src, `${ty}.${powerName}`, reverse ? 'dir=back' : '');
+        if (ty === 'home') {
+          nodes.add({ id: 'home', label: 'home', cluster: 'provisioning' });
+          addEdge('home', `${ty}.${powerName}`);
         }
       }
     }
   };
 
-  for (const [fnName, permit] of entries(manifest)) {
-    nodes.add({ id: fnName, label: fnName });
+  /** @type {<X>(xs: X[]) => X[]} */
+  const uniq = xs => [...new Set(xs)];
+  const spaces = uniq(
+    logged('vals')(values(manifest)).flatMap(permit =>
+      logged('keys?')(keys(permit)).flatMap(name => {
+        logged('name?')(name);
 
-    level1(fnName, 'vatPowers', permit.vatPowers, true);
-    level1(fnName, 'vats', permit.vats, true);
-    level1(fnName, 'devices', permit.devices, true);
-    level1(fnName, 'space', permit.produce);
-    level1(fnName, 'space', permit.consume, true);
-    level1(fnName, 'home', (permit.home || {}).produce);
-    level1(fnName, 'issuer', (permit.issuer || {}).produce);
-    level1(fnName, 'issuer', (permit.issuer || {}).consume, true);
-    level1(fnName, 'brand', (permit.brand || {}).produce);
-    level1(fnName, 'brand', (permit.brand || {}).consume, true);
-    level1(fnName, 'installation', (permit.installation || {}).produce);
-    level1(fnName, 'installation', (permit.installation || {}).consume, true);
-    level1(fnName, 'instance', (permit.instance || {}).produce);
-    level1(fnName, 'instance', (permit.instance || {}).consume, true);
-    if (permit.runBehaviors) {
-      nodes.add({
-        id: `runBehaviors`,
-        label: `runBehaviors`,
-        style: '',
-      });
-      addEdge(fnName, `runBehaviors`);
+        if (['produce', 'consume'].includes(name)) return [];
+        return [name];
+      }),
+    ),
+  );
+  console.error('@@@', { spaces });
+
+  for (const [fnName, permit] of entries(manifest)) {
+    if (permit === true) {
+      console.error('skipping wildcard permit:', fnName);
+      continue;
+    }
+    nodes.add({ id: logged('fn')(fnName), label: fnName });
+
+    permit.vatPowers && level1(fnName, 'vatPowers', permit.vatPowers, true);
+    permit.vats && level1(fnName, 'vats', permit.vats, true);
+    permit.devices && level1(fnName, 'devices', permit.devices, true);
+
+    const doPart = (name, part) => {
+      if (!part) return;
+      level1(fnName, name, part.produce || {});
+      level1(fnName, name, part.consume || {}, true);
+    };
+    doPart('space', permit);
+    spaces.forEach(s => doPart(s, permit[s]));
+
+    if ('runBehaviors' in permit) {
+      throw Error('not impl');
+      // nodes.add({
+      //   id: `runBehaviors`,
+      //   label: `runBehaviors`,
+      //   style: '',
+      // });
+      // addEdge(fnName, `runBehaviors`);
     }
   }
   return { nodes, neighbors };
+};
+
+const { Fail, quote: q } = assert;
+
+/**
+ * @param {string} specifier
+ * @param {object} io
+ * @param {Resolver} io.resolve
+ * @param {typeof import('fs/promises').readFile} io.readFile
+ */
+const loadConfig = async (specifier, { resolve, readFile }) => {
+  const fullPath = await resolve(specifier, import.meta.url).then(
+    u => new URL(u).pathname,
+  );
+  typeof fullPath === 'string' || Fail`${q(specifier)}`;
+  const txt = await readFile(fullPath, 'utf-8');
+  typeof txt === 'string' || Fail`readFile ${q(fullPath)}`;
+  return JSON.parse(txt);
 };
 
 /**
  * @param {string[]} args
  * @param {object} io
  * @param {typeof import('process').stdout} io.stdout
+ * @param {typeof import('fs/promises')} io.fsp
+ * @param {{
+ *   resolve: Resolver,
+ *   url: string,
+ *   load: (specifier: string) => Promise<Record<string, any>>,
+ * }} io.meta
+ *
+ * @typedef {(specifier: string, parent: string) => Promise<string>} Resolver
  */
-const main = async (args, { stdout }) => {
-  const [...opts] = args;
-  // if (!fn) throw Error('usage: authorityViz [--sim-chain]');
-  const [sim, gov] = ['--sim-chain', '--gov'].map(opt => opts.includes(opt));
-  const manifest = sim
-    ? manifests.SIM_CHAIN_BOOTSTRAP_MANIFEST
-    : manifests.CHAIN_BOOTSTRAP_MANIFEST;
+const main = async (args, { stdout, fsp, meta }) => {
+  const [specifier, ...opts] = args;
+  specifier || Fail`Usage: $0 @agoric/vats/decentral-...json`;
+
+  const config = await loadConfig(specifier, {
+    resolve: meta.resolve,
+    readFile: fsp.readFile,
+  });
+  // console.log(config);
+  const { bootstrap } = config.vats;
+
+  const { MANIFEST } = await meta
+    .resolve(bootstrap.sourceSpec, meta.url)
+    .then(p => meta.load(p));
+  // console.log('manifest keys:', Object.keys(MANIFEST));
+
+  const [gov] = ['--gov'].map(opt => opts.includes(opt));
+
   if (gov) {
+    throw Error('not impl');
     /*
     const postBoot = sim
       ? manifests.SIM_CHAIN_POST_BOOT_MANIFEST
@@ -189,15 +248,25 @@ const main = async (args, { stdout }) => {
     */
   }
 
-  // console.log(JSON.stringify(bootstrapManifest, null, 2));
-  const g = manifest2graph(/** @type {any} */ (manifest));
+  const g = manifest2graph(MANIFEST);
   for (const chunk of fmtGraph(g.nodes, g.neighbors)) {
     stdout.write(chunk);
   }
 };
 
 (async () => {
-  return main(process.argv.slice(2), {
-    stdout: process.stdout,
-  });
+  // eslint-disable-next-line import/no-extraneous-dependencies
+  Promise.all([import('fs/promises'), import('import-meta-resolve')]).then(
+    ([fsp, metaResolve]) => {
+      return main(process.argv.slice(2), {
+        stdout: process.stdout,
+        fsp,
+        meta: {
+          resolve: metaResolve.resolve,
+          url: import.meta.url,
+          load: specifier => import(specifier),
+        },
+      });
+    },
+  );
 })().catch(console.error);
