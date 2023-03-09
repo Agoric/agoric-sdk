16185e7699a253fffd5cceddfaae8e43179128df
Richard Gibson <richard.gibson@gmail.com>
2023-03-09 04:03:11 -0500

    chore(SwingSet): Move RunQueueEvent typedefs to types-internal

--- packages/SwingSet/src/kernel/kernel.js
+++ packages/SwingSet/src/kernel/kernel.js
@@ -1116,28 +1116,18 @@ export default function buildKernel(
    * @typedef { import('../types-internal.js').VatID } VatID
    * @typedef { import('../types-internal.js').InternalDynamicVatOptions } InternalDynamicVatOptions
    *
-   * @typedef { { type: 'notify', vatID: VatID, kpid: string } } RunQueueEventNotify
-   * @typedef { { type: 'send', target: string, msg: Message }} RunQueueEventSend
-   * @typedef { { type: 'create-vat', vatID: VatID,
-   *              source: { bundle: Bundle } | { bundleID: BundleID },
-   *              vatParameters: SwingSetCapData,
-   *              dynamicOptions: InternalDynamicVatOptions }
-   *          } RunQueueEventCreateVat
-   * @typedef { { type: 'upgrade-vat', vatID: VatID, upgradeID: string,
-   *              bundleID: BundleID, vatParameters: SwingSetCapData,
-   *              upgradeMessage: string } } RunQueueEventUpgradeVat
-   * @typedef { { type: 'changeVatOptions', vatID: VatID, options: Record<string, unknown> } } RunQueueEventChangeVatOptions
-   * @typedef { { type: 'startVat', vatID: VatID, vatParameters: SwingSetCapData } } RunQueueEventStartVat
-   * @typedef { { type: 'dropExports', vatID: VatID, krefs: string[] } } RunQueueEventDropExports
-   * @typedef { { type: 'retireExports', vatID: VatID, krefs: string[] } } RunQueueEventRetireExports
-   * @typedef { { type: 'retireImports', vatID: VatID, krefs: string[] } } RunQueueEventRetireImports
-   * @typedef { { type: 'negated-gc-action', vatID: VatID } } RunQueueEventNegatedGCAction
-   * @typedef { { type: 'bringOutYourDead', vatID: VatID } } RunQueueEventBringOutYourDead
-   * @typedef { RunQueueEventNotify | RunQueueEventSend | RunQueueEventCreateVat |
-   *            RunQueueEventUpgradeVat | RunQueueEventChangeVatOptions | RunQueueEventStartVat |
-   *            RunQueueEventDropExports | RunQueueEventRetireExports | RunQueueEventRetireImports |
-   *            RunQueueEventNegatedGCAction | RunQueueEventBringOutYourDead
-   *          } RunQueueEvent
+   * @typedef { import('../types-internal.js').RunQueueEventNotify } RunQueueEventNotify
+   * @typedef { import('../types-internal.js').RunQueueEventSend } RunQueueEventSend
+   * @typedef { import('../types-internal.js').RunQueueEventCreateVat } RunQueueEventCreateVat
+   * @typedef { import('../types-internal.js').RunQueueEventUpgradeVat } RunQueueEventUpgradeVat
+   * @typedef { import('../types-internal.js').RunQueueEventChangeVatOptions } RunQueueEventChangeVatOptions
+   * @typedef { import('../types-internal.js').RunQueueEventStartVat } RunQueueEventStartVat
+   * @typedef { import('../types-internal.js').RunQueueEventDropExports } RunQueueEventDropExports
+   * @typedef { import('../types-internal.js').RunQueueEventRetireExports } RunQueueEventRetireExports
+   * @typedef { import('../types-internal.js').RunQueueEventRetireImports } RunQueueEventRetireImports
+   * @typedef { import('../types-internal.js').RunQueueEventNegatedGCAction } RunQueueEventNegatedGCAction
+   * @typedef { import('../types-internal.js').RunQueueEventBringOutYourDead } RunQueueEventBringOutYourDead
+   * @typedef { import('../types-internal.js').RunQueueEvent } RunQueueEvent
    */
 
   /**
