34db7671daee5196c85f78ac82b90884e52fa555
Chip Morningstar <chip@fudco.com>
2023-03-14 18:38:03 -0700

    feat: implement bundleStore
    Closes #7089

---
+++ packages/swing-store/src/util.js
@@ -0,0 +1,16 @@
+import { Buffer } from 'buffer';
+
+/**
+ * This is a polyfill for the `buffer` function from Node's
+ * 'stream/consumers' package, which unfortunately only exists in newer versions
+ * of Node.
+ *
+ * @param {AsyncIterable<Buffer>} inStream
+ */
+export const buffer = async inStream => {
+  const chunks = [];
+  for await (const chunk of inStream) {
+    chunks.push(chunk);
+  }
+  return Buffer.concat(chunks);
+};
