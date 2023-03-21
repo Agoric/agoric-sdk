84335e90b6ffa8b8f6731ffee90645a62fb63830
Ari Rubinstein <ari@agoric.com>
2023-03-21 16:00:56 -0700

    ci: coverage

--- scripts/ci-collect-testruns.sh
+++ scripts/ci-collect-testruns.sh
@@ -12,3 +12,17 @@ do
         $SCRIPT_DIR/ava-etap-to-junit.mjs $pkg > $junit
     fi
 done
+
+for cov in ./packages/**/coverage
+do
+    # skip if unable to expand if there are no test files
+    if [[ $cov != "./packages/**/coverage" ]]; then
+        echo "processing $pkg"
+        (cd "$cov/../" && yarn run c8 report --reporter=lcov && find coverage -name "*.json" -delete)
+    fi
+done
+
+# if root coverage exists, ignore it
+if [[ -d "coverage" ]]; then
+    rm -rf coverage
+fi
