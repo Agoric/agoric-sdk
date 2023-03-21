5ed983e9b7d3088e724cc9ad7f4d75a225128f8b
Ari Rubinstein <ari@agoric.com>
2023-03-21 09:46:40 -0700

    ci: add simple smoketest to binaries for docker, and add docker build to the PR ci

--- scripts/ci-collect-testruns.sh
+++ scripts/ci-collect-testruns.sh
@@ -4,8 +4,11 @@ SCRIPT_DIR=$( cd ${0%/*} && pwd -P )
 
 for pkg in ./packages/*/_testoutput.txt
 do
-    echo "processing $pkg"
-    junit=$(dirname "$pkg")/junit.xml
-    echo "dest $junit"
-    $SCRIPT_DIR/ava-etap-to-junit.mjs $pkg > $junit
+    # skip if unable to expand if there are no test files
+    if [[ $pkg != "./packages/*/_testoutput.txt" ]]; then
+        echo "processing $pkg"
+        junit=$(dirname "$pkg")/junit.xml
+        echo "dest $junit"
+        $SCRIPT_DIR/ava-etap-to-junit.mjs $pkg > $junit
+    fi
 done
