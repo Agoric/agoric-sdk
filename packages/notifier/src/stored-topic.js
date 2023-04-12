5df6ed470952ae02c636fe569ef0cd912cb04572
Turadg Aleahmad <turadg@agoric.com>
2023-04-11 18:37:49 -0700

    chore!: remove deprecated pipeTopicToStorage

--- packages/notifier/src/stored-topic.js
+++ packages/notifier/src/stored-topic.js
@@ -1,9 +1,6 @@
-import { assertAllDefined } from '@agoric/internal';
-import { makeSerializeToStorage } from '@agoric/internal/src/lib-chainStorage.js';
 import { M } from '@agoric/store';
 import { E } from '@endo/far';
 import { SubscriberShape } from './publish-kit.js';
-import { forEachPublicationRecord } from './storesub.js';
 
 // XXX PublicTopic is a higher level concern
 export const PublicTopicShape = M.splitRecord(
@@ -30,31 +27,6 @@ export const TopicsRecordShape = M.recordOf(M.string(), PublicTopicShape);
  *   [topicName: string]: PublicTopic<unknown>,
  * }} TopicsRecord
  */
-/**
- * @deprecated uses forEachPublicationRecord which is [not sufficiently virtual](https://github.com/Agoric/agoric-sdk/issues/7302)
- *
- * NB: caller must ensure that `publisher.finish()` or `publisher.fail()` is
- * called when the publisher stores its final value.
- * Otherwise this watch is retained and can't be GCed.
- *
- * @param {Subscriber<any>} topic
- * @param {ERef<StorageNode>} storageNode
- * @param {ERef<ReturnType<import('@endo/marshal').makeMarshal>>} marshaller
- * @returns {void}
- */
-export const pipeTopicToStorage = (topic, storageNode, marshaller) => {
-  assertAllDefined({ topic, storageNode, marshaller });
-
-  const marshallToStorage = makeSerializeToStorage(storageNode, marshaller);
-
-  // Start publishing the source.
-  forEachPublicationRecord(topic, marshallToStorage).catch(err => {
-    // TODO: How should we handle and/or surface this failure?
-    // https://github.com/Agoric/agoric-sdk/pull/5766#discussion_r922498088
-    console.error('followAndStoreTopic failed to iterate', err);
-  });
-};
-harden(pipeTopicToStorage);
 
 /**
  * If the subscriber and storageNode are coming from a Recorder kit,
