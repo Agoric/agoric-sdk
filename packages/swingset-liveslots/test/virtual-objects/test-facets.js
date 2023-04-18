d36a5feee7619b8d38e7247a173b240934994f40
Brian Warner <warner@lothar.com>
2023-04-17 23:51:48 -0700

    add test of non-enumerable facet names
    refs #7446

--- packages/swingset-liveslots/test/virtual-objects/test-facets.js
+++ packages/swingset-liveslots/test/virtual-objects/test-facets.js
@@ -21,3 +21,33 @@ test('facets', async t => {
   thing1.mutable.set(1);
   t.is(thing1.mutable.getImmutable(), thing1.immutable);
 });
+
+// issue 7446: non-enumerable properties in the behavior record
+// ("hidden facets") yield facet/Representatives with no behavior:
+// their __proto__ is Object.prototype, not the special method-bearing
+// one from defendPrototype()
+
+test.failing('non-enumerable facet name', async t => {
+  const vom = makeFakeVirtualObjectManager();
+  const init = () => ({ count: 0 });
+  const behavior = {
+    incrementer: {
+      increment: ({ state }) => (state.count += 1),
+    },
+  };
+  // non-enumerable facet names should be ignored
+  Object.defineProperty(behavior, 'resetter', {
+    enumerable: false,
+    value: {
+      reset: ({ state }) => (state.count = 0),
+    },
+  });
+  harden(behavior);
+  const makeCounter = vom.defineKindMulti('counter', init, behavior);
+  const counter = makeCounter();
+  // Reflect.ownKeys yields all properties, including non-enumerable ones
+  t.deepEqual(Reflect.ownKeys(counter).sort(), ['incrementer']);
+
+  // console.log(counter.resetter);
+  // console.log(Object.getPrototypeOf(counter.resetter));
+});
