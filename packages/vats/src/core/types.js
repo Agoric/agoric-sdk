23e6a55deb9a92326e0e3fede6805e3650e16c7a
samsiegart <samsiegart@gmail.com>
2023-05-01 09:54:08 -0700

    chore: update snapshots

--- packages/vats/src/core/types.js
+++ packages/vats/src/core/types.js
@@ -190,7 +190,6 @@
  *       stakeFactory: Promise<Installation<import('@agoric/inter-protocol/src/stakeFactory/stakeFactory.js').start>>,
  *       VaultFactory: Promise<Installation<import('@agoric/inter-protocol/src/vaultFactory/vaultFactory.js').start>>,
  *       walletFactory: Promise<Installation<import('@agoric/smart-wallet/src/walletFactory.js').prepare>>,
- *       provisionPool: Promise<Installation<import('@agoric/vats/src/provisionPool.js').start>>,
  *     },
  *   },
  *   instance:{
