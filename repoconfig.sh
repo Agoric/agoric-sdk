7a2bfd94c63cf492ba63585b0aff25bd91cb7210
Jim Larson <jim@agoric.com>
2023-05-04 16:30:52 -0700

    fix: remove debugging modification

--- repoconfig.sh
+++ repoconfig.sh
@@ -10,6 +10,6 @@ function golang_version_check() {
   # Want 1.20.2+, 1.21+, or 2+
   [ $1 -eq 1 ] && {
     [ $2 -lt 20 ] && echo "need go version 1.20 or higher" 1>&2 && return 1
-    [ $2 -eq 20 ] && [ $3 -lt 222 ] && echo "need go version 1.20.2 or higher" 1>&2 && return 1
+    [ $2 -eq 20 ] && [ $3 -lt 2 ] && echo "need go version 1.20.2 or higher" 1>&2 && return 1
   } || true
 }
