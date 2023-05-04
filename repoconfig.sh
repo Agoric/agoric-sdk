588507d334a5d87e51373a593632348706c5d131
Jim Larson <jim@agoric.com>
2023-05-04 14:44:38 -0700

    feat: bin/agd installs go 1.20 and checks as minimum version

--- repoconfig.sh
+++ repoconfig.sh
@@ -1,6 +1,15 @@
 #! /bin/sh
 # shellcheck disable=SC2034
 NODEJS_VERSION=v16
-GOLANG_VERSION=1.19
+GOLANG_VERSION=1.20
 GOLANG_DIR=golang/cosmos
 GOLANG_DAEMON=$GOLANG_DIR/build/agd
+
+# Args are major, minor and patch version numbers
+function golang_version_check() {
+  # Want 1.20.2+, 1.21+, or 2+
+  [ $1 -eq 1 ] && {
+    [ $2 -lt 20 ] && fatal "need go version 1.20 or higher"
+    [ $2 -eq 20 ] && [ $3 -lt 2 ] && fatal "need go version 1.20.2 or higher"
+  }
+}
