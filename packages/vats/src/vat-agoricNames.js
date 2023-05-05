053e2612fdb449146a10fbfab6812462a374a088
Dan Connolly <connolly@agoric.com>
2023-05-05 00:25:10 -0500

    feat: provideInertBrand from agoricNames vat

--- packages/vats/src/vat-agoricNames.js
+++ packages/vats/src/vat-agoricNames.js
@@ -1,14 +1,53 @@
 // @ts-check
 // @jessy-check
 import { E, Far } from '@endo/far';
+import { BrandI } from '@agoric/ertp';
+import { provide } from '@agoric/vat-data';
+import { makeDurableZone } from '@agoric/zone/durable.js';
 import { makeNameHubKit } from './nameHub.js';
+
+/** @param {import('@agoric/zone').Zone} zone */
+const makeBrandStore = zone => {
+  const brandStore = zone.mapStore('Brand', { durable: true });
+
+  // XXX generalize past Nat; move into ERTP
+  /** @type {(name: string, d: DisplayInfo) => Brand} */
+  const makeNatBrand = zone.exoClass(
+    'Brand',
+    BrandI,
+    (name, displayInfo) => ({ name, displayInfo }),
+    {
+      async isMyIssuer(_allegedIssuer) {
+        return false;
+      },
+      getAllegedName() {
+        const { name } = this.state;
+        return name;
+      },
+      // Give information to UI on how to display the amount.
+      getDisplayInfo() {
+        const { displayInfo } = this.state;
+        return displayInfo;
+      },
+      getAmountShape() {
+        return undefined;
+      },
+    },
+  );
+
+  return {
+    provide: (keyword, displayInfo) =>
+      provide(brandStore, keyword, () => makeNatBrand(keyword, displayInfo)),
+  };
+};
 
 /**
  * @param {unknown} _vatPowers
  * @param {unknown} _vatParameters
- * @param {import('@agoric/vat-data').Baggage} _baggage
+ * @param {import('@agoric/vat-data').Baggage} baggage
  */
-export const buildRootObject = (_vatPowers, _vatParameters, _baggage) => {
+export const buildRootObject = (_vatPowers, _vatParameters, baggage) => {
+  const zone = makeDurableZone(baggage);
   const kit = makeNameHubKit(); // TODO: durable
   const { nameHub: agoricNames, nameAdmin: agoricNamesAdmin } = kit;
 
@@ -32,9 +71,21 @@ export const buildRootObject = (_vatPowers, _vatParameters, _baggage) => {
     );
   };
 
+  const brandStore = makeBrandStore(zone);
+
+  /**
+   * Provide a brand, with no mint nor issuer.
+   *
+   * @param {string} keyword
+   * @param {DisplayInfo} displayInfo
+   */
+  const provideInertBrand = (keyword, displayInfo) =>
+    brandStore.provide(keyword, displayInfo);
+
   return Far('vat-agoricNames', {
     getNameHub: () => agoricNames,
     getNameHubKit: () => kit,
     publishNameHubs,
+    provideInertBrand,
   });
 };
