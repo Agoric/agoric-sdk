66bfb0bc0e6a6b5f702db2526a02d067f0847aa6
Jim Larson <jim@agoric.com>
2023-05-05 13:40:01 -0700

    refactor: clean up based on comments

--- repoconfig.sh
+++ repoconfig.sh
@@ -8,8 +8,7 @@ GOLANG_DAEMON=$GOLANG_DIR/build/agd
 # Args are major, minor and patch version numbers
 function golang_version_check() {
   # Want 1.20.2+, 1.21+, or 2+
-  [ $1 -eq 1 ] && {
-    [ $2 -lt 20 ] && echo "need go version 1.20 or higher" 1>&2 && return 1
-    [ $2 -eq 20 ] && [ $3 -lt 2 ] && echo "need go version 1.20.2 or higher" 1>&2 && return 1
-  } || true
+  [ $1 -eq 1 ] && [ $2 -lt 20 ] && echo "need go version 1.20 or higher" 1>&2 && return 1
+  [ $1 -eq 1 ] && [ $2 -eq 20 ] && [ $3 -lt 2 ] && echo "need go version 1.20.2 or higher" 1>&2 && return 1
+  return 0
 }
