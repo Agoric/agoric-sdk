785950ac02dbff9c9948f11d38f35924b0f36a9b
Turadg Aleahmad <turadg@agoric.com>
2023-05-10 17:12:57 -0700

    fix(governance): governor adminFacet handling

--- packages/governance/src/types-ambient.js
+++ packages/governance/src/types-ambient.js
@@ -721,9 +721,10 @@
  * @template {GovernableStartFn} SF
  * @typedef GovernanceFacetKit
  * Akin to StartedInstanceKit but designed for the results of starting governed contracts. Used in bootstrap space.
- * @property {AdminFacet} adminFacet of the governing contract
+ * @property {AdminFacet} adminFacet of the governed contract
  * @property {LimitedCF<SF>} creatorFacet creator-like facet within the governed contract (without the powers the governor needs)
  * @property {GovernorCreatorFacet<SF>} governorCreatorFacet of the governing contract
+ * @property {AdminFacet} governorAdminFacet of the governing contract
  * @property {Awaited<ReturnType<SF>>['publicFacet']} publicFacet
  * @property {Instance} instance
  * @property {Instance} governor
