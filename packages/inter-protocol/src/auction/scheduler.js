bbf3d61e6507d703fcbd5340fa8276f325c456f8
Turadg Aleahmad <turadg@agoric.com>
2023-05-13 21:51:40 -0700

    chore(scheduler): enable tracing

--- packages/inter-protocol/src/auction/scheduler.js
+++ packages/inter-protocol/src/auction/scheduler.js
@@ -8,7 +8,7 @@ import { nextDescendingStepTime, computeRoundTiming } from './scheduleMath.js';
 
 const { Fail, quote: q } = assert;
 
-const trace = makeTracer('SCHED', false);
+const trace = makeTracer('SCHED');
 
 // If the startAuction wakeup is no more than 5 minutes late, go ahead with it.
 const MAX_LATE_TICK = 300n;
