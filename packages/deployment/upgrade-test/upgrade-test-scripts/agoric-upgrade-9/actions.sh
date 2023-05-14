a760ca1db7d6540ff17302455490a5ab30468011
Raphael Salas <raphdev@users.noreply.github.com>
2023-05-14 00:54:33 +0000

    test: dump parameters shouldnt be empty

--- packages/deployment/upgrade-test/upgrade-test-scripts/agoric-upgrade-9/actions.sh
+++ packages/deployment/upgrade-test/upgrade-test-scripts/agoric-upgrade-9/actions.sh
@@ -7,5 +7,8 @@
 
 # save somewhere we can access later
 echo "Dumping PSM gov params..."
-timeout 2 agoric follow :published.psm.IST.ToyUSD.metrics -o jsonlines | tee /root/.agoric/psm_metrics.json
-timeout 2 agoric follow :published.psm.IST.ToyUSD.governance -o jsonlines | tee /root/.agoric/psm_governance.json
+timeout 2 agoric follow -l :published.psm.IST.ToyUSD.metrics -o jsonlines | tee /root/.agoric/psm_metrics.json
+timeout 2 agoric follow -l :published.psm.IST.ToyUSD.governance -o jsonlines | tee /root/.agoric/psm_governance.json
+
+test_not_val "$(cat /root/.agoric/psm_metrics.json | wc -l)" "0" "psm metrics shouldnt be empty"
+test_not_val "$(cat /root/.agoric/psm_governance.json | wc -l)" "0" "psm gov params shouldnt be empty"
