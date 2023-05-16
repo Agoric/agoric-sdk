d8d26a918a2ce3354addf41a11ea315f8ecbe584
Jim Larson <jim@agoric.com>
2023-05-16 10:55:24 -0700

    fix: fetch go 1.20.3 but tolerate truncated semver
    Gvm cannot fetch the latest patch release of a given minor,
    so pick the latest 1.20 release that I've tested.
    Go releases can have truncated semver (might there be a "2" release?),
    so tolerate that in the regexp and flip the sense of the version check
    to fail closed instead of failing open.

--- repoconfig.sh
+++ repoconfig.sh
@@ -1,14 +1,15 @@
 #! /bin/sh
 # shellcheck disable=SC2034
 NODEJS_VERSION=v16
-GOLANG_VERSION=1.20
+GOLANG_VERSION=1.20.3
 GOLANG_DIR=golang/cosmos
 GOLANG_DAEMON=$GOLANG_DIR/build/agd
 
 # Args are major, minor and patch version numbers
 function golang_version_check() {
-  # Want 1.20.2+, 1.21+, or 2+
-  [ $1 -eq 1 ] && [ $2 -lt 20 ] && echo "need go version 1.20 or higher" 1>&2 && return 1
-  [ $1 -eq 1 ] && [ $2 -eq 20 ] && [ $3 -lt 2 ] && echo "need go version 1.20.2 or higher" 1>&2 && return 1
-  return 0
+  [ $1 -eq 1 ] && [ $2 -eq 20 ] && [ $3 -ge 2 ] && return 0
+  [ $1 -eq 1 ] && [ $2 -ge 21 ] && return 0
+  [ $1 -ge 2 ] && return 0
+  echo "need go version 1.20.2+, 1.21+, or 2+"
+  return 1
 }
