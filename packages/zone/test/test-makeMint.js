bf5e6ae2f154ebb328a155076655124d83735d4b
Dan Connolly <connolly@agoric.com>
2023-05-29 12:31:04 -0500

    test: refactor test-makeMint to qualify multiple implementations

--- packages/zone/test/test-makeMint.js
+++ packages/zone/test/test-makeMint.js
@@ -3,11 +3,15 @@ import { test } from '@agoric/swingset-vat/tools/prepare-test-env-ava.js';
 
 import { makeMint } from './makeMint.js';
 
-test(`makeMint: objects as closures implementation`, t => {
-  const mint = makeMint();
-  const p1 = mint.makePurse(10n);
-  const p2 = mint.makePurse(0n);
-  p2.deposit(3n, p1.withdraw(3n));
-  t.is(p1.getBalance(), 7n);
-  t.is(p2.getBalance(), 3n);
-});
+const qualifyImplementation = (label, maker) => {
+  test(`makeMint: ${label}`, t => {
+    const mint = maker();
+    const p1 = mint.makePurse(10n);
+    const p2 = mint.makePurse(0n);
+    p2.deposit(3n, p1.withdraw(3n));
+    t.is(p1.getBalance(), 7n);
+    t.is(p2.getBalance(), 3n);
+  });
+};
+
+qualifyImplementation('objects-as-closures', makeMint);
