39d50eea18de88b4980c8569b3e9a6f76c35ab8a
Dan Connolly <connolly@agoric.com>
2023-05-30 20:03:42 -0500

    test: upgrade zoe thru CoreEval

--- packages/deployment/upgrade-test/upgrade-test-scripts/agoric-upgrade-11/zoe-upgrade/vat-status.mjs
+++ packages/deployment/upgrade-test/upgrade-test-scripts/agoric-upgrade-11/zoe-upgrade/vat-status.mjs
@@ -1,10 +1,22 @@
-/* eslint-disable import/no-extraneous-dependencies */
+// @ts-check
 import processAmbient from 'process';
 import dbOpenAmbient from 'better-sqlite3';
 
+/**
+ * @file look up vat incarnation from kernel DB
+ * @see {main}
+ */
+
+const swingstorePath = '~/.agoric/data/agoric/swingstore.sqlite';
+
+/**
+ * SQL short-hand
+ *
+ * @param {import('better-sqlite3').Database} db
+ */
 const dbTool = db => {
   const prepare = (strings, ...params) => {
-    const dml = strings.join(' ? ');
+    const dml = strings.join('?');
     return { stmt: db.prepare(dml), params };
   };
   const sql = (strings, ...args) => {
@@ -18,30 +30,69 @@ const dbTool = db => {
   return sql;
 };
 
+/**
+ * @param {import('better-sqlite3').Database} db
+ */
+const makeSwingstore = db => {
+  const sql = dbTool(db);
+
+  /** @param {string} key */
+  const kvGet = key => sql.get`select * from kvStore where key = ${key}`.value;
+  /** @param {string} key */
+  const kvGetJSON = key => JSON.parse(kvGet(key));
+
+  /** @param {string} vatID */
+  const lookupVat = vatID => {
+    return Object.freeze({
+      source: () => kvGetJSON(`${vatID}.source`),
+      options: () => kvGetJSON(`${vatID}.options`),
+      currentSpan: () =>
+        sql.get`select * from transcriptSpans where isCurrent = 1 and vatID = ${vatID}`,
+    });
+  };
+
+  return Object.freeze({
+    /** @param {string} vatName */
+    findVat: vatName => {
+      /** @type {string[]} */
+      const dynamicIDs = kvGetJSON('vat.dynamicIDs');
+      const targetVat = dynamicIDs.find(
+        vatID => lookupVat(vatID).options().name === vatName,
+      );
+      if (!targetVat) throw Error(vatName);
+      return targetVat;
+    },
+    lookupVat,
+  });
+};
+
+/** @type {<T>(val: T | undefined) => T} */
+const NonNullish = val => {
+  if (!val) throw Error('required');
+  return val;
+};
+
+/**
+ * @param {string[]} argv
+ * @param {{ dbOpen: typeof import('better-sqlite3'), HOME?: string }} io
+ */
 const main = async (argv, { dbOpen, HOME }) => {
   const [_node, _script, vatName] = argv;
   if (!vatName) throw Error('vatName required');
 
-  const db = dbOpen(`${HOME}/.agoric/data/agoric/swingstore.sqlite`, {
-    readonly: true,
-  });
-  const sql = dbTool(db);
+  const fullPath = swingstorePath.replace(/^~/, NonNullish(HOME));
+  const kStore = makeSwingstore(dbOpen(fullPath, { readonly: true }));
 
-  const kvGet = key => sql.get`select * from kvStore where key = ${key}`.value;
+  const vatID = kStore.findVat(vatName);
+  const vatInfo = kStore.lookupVat(vatID);
 
-  const dynamicIDs = JSON.parse(kvGet('vat.dynamicIDs'));
-  const targetVat = dynamicIDs.find(vatID => {
-    const options = JSON.parse(kvGet(`${vatID}.options`));
-    return options.name === vatName;
-  });
+  const source = vatInfo.source();
+  const { incarnation } = vatInfo.currentSpan();
 
-  const source = JSON.parse(kvGet(`${targetVat}.source`));
-  const {
-    incarnation,
-  } = sql.get`select * from transcriptSpans where isCurrent = 1 and vatID = ${targetVat}`;
-  console.error(
-    JSON.stringify({ vatName, vatID: targetVat, incarnation, ...source }),
-  );
+  // misc info to stderr
+  console.error(JSON.stringify({ vatName, vatID, incarnation, ...source }));
+
+  // incarnation to stdout
   console.log(incarnation);
 };
 
