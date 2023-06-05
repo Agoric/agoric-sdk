a1d1666997c2f5b9c7bf14748f6d9603c0b3c5f9
Michael FIG <mfig@agoric.com>
2023-06-05 15:16:19 -0600

    feat(agd): try harder to find cosmic-swingset

--- golang/cosmos/cmd/agd/find_binary.go
+++ golang/cosmos/cmd/agd/find_binary.go
@@ -8,14 +8,19 @@ import (
 
 // FindBinaryNextToMe looks for binName next to the current executable.
 // It returns an absolute filename for binName, or an error.
-func FindBinaryNextToMe(binName string) (string, error) {
+func FindBinaryNextToMe(walkUp int, path... string) (string, error) {
 	ex, err := os.Executable()
 	if err != nil {
 		return "", err
 	}
 
 	// Calculate the binary's filename.
-	bin := filepath.Join(filepath.Dir(ex), binName)
+	dir := ex
+	for i := 0; i <= walkUp; i++ {
+		dir = filepath.Dir(dir)
+	}
+
+	bin := filepath.Join(append([]string{dir}, path...)...)
 
 	// Take the absolute path.
 	bin, err = filepath.Abs(bin)
@@ -24,20 +29,30 @@ func FindBinaryNextToMe(binName string) (string, error) {
 	}
 
 	// Check that the binary exists.
-	if _, err := os.Stat(bin); err != nil {
+	if _, err = os.Stat(bin); err != nil {
 		return "", err
 	}
+	
 	return bin, nil
 }
 
-// FindBinary looks for binName in the following locations:
+// FindCosmicSwingsetBinary looks for binName in the following locations:
 // 1. The executable's directory
-// 2. The system PATH
+// 2. Walking up to ../../cosmic-swingset/src/entrypoint.js
+// 3. Walking up to ../../../packages/cosmic-swingset/src/entrypoint.js
+// 4. The system PATH
 //
 // It returns the absolute filename for binName if it is found, otherwise an
 // error.
-func FindBinary(binName string) (string, error) {
-	if binary, err := FindBinaryNextToMe(binName); err == nil {
+func FindCosmicSwingsetBinary() (string, error) {
+	binName := "ag-chain-cosmos"
+	if binary, err := FindBinaryNextToMe(0, binName); err == nil {
+		return binary, nil
+	}
+	if binary, err := FindBinaryNextToMe(2, "cosmic-swingset", "src", "entrypoint.js"); err == nil {
+		return binary, nil
+	}
+	if binary, err := FindBinaryNextToMe(3, "packages", "cosmic-swingset", "src", "entrypoint.js"); err == nil {
 		return binary, nil
 	}
 
