639ec66b48af40f3771e72ae54288b7db92a9698
Turadg Aleahmad <turadg@agoric.com>
2023-06-12 15:28:44 -0700

    chore: move ui-components to ui-kit repo
    Can't be done atomically. Remove here and add there

--- packages/ui-components/src/display/index.js
+++
@@ -1,4 +0,0 @@
-export * from './display.js';
-export * from './natValue/stringifyRatioAsFraction.js';
-export * from './natValue/stringifyRatio.js';
-export * from './natValue/stringifyRatioAsPercent.js';
