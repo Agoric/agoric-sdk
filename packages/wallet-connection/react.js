5e49f9731a59f55af7a73dab248fcf9ba5a42754
Turadg Aleahmad <turadg@agoric.com>
2023-06-12 15:26:09 -0700

    chore: remove deprecated wallet-connection
    It is mothballed and doesn't need to be published anymore.

--- packages/wallet-connection/react.js
+++
@@ -1,4 +0,0 @@
-export { makeReactAgoricWalletConnection } from '@agoric/web-components/react.js';
-console.warn(
-  'This package is deprecated. Use @agoric/web-components instead of @agoric/wallet-connection.',
-);
