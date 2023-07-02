ec9931e03c235dfec493d060d51e64a2206901ba
Michael FIG <mfig@agoric.com>
2023-07-02 00:03:27 -0600

    fix(cosmic-swingset): detect `agvm` mode via envvars, not flags

--- packages/cosmic-swingset/src/agcc-shim.js
+++ packages/cosmic-swingset/src/agcc-shim.js
@@ -1,8 +1,17 @@
 /* global process */
+import { Fail } from '@agoric/assert';
+
 let agccP;
-const i = process.argv.indexOf('--vm-spec');
-if (i >= 0) {
-  const spec = process.argv[i + 1];
+const { AGVM_FROM_AGD, AGVM_TO_AGD } = process.env;
+if (AGVM_FROM_AGD && AGVM_TO_AGD) {
+  const fd = [parseInt(AGVM_FROM_AGD, 10), parseInt(AGVM_TO_AGD, 10)];
+  for (const f of fd) {
+    Number.isSafeInteger(f) || Fail(`fd ${f} must be a safe integer`);
+    f >= 0 || Fail(`fd ${f} must be non-negative`);
+  }
+  const spec = JSON.stringify({
+    agd: { fd },
+  });
   agccP = import('./agcc-agvm.js').then(ns => ns.makeAgccFromSpec(spec));
 } else {
   agccP = import('@agoric/cosmos').then(ns => ns.default);
