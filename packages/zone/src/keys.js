71278d0a1fc6120e3c077a9578a1ec0feb672f89
Michael FIG <mfig@agoric.com>
2023-07-03 19:16:03 -0600

    test(zone): add more tests

--- packages/zone/src/keys.js
+++ packages/zone/src/keys.js
@@ -3,10 +3,12 @@ const kind = label => `${label}_kindHandle`;
 /** @param {string} label */
 const singleton = label => `${label}_singleton`;
 
-/** @type {Record<'exoClass' | 'exoClassKit' | 'exo', (label: string) => string[]>} */
+/** @type {Record<'exoClass' | 'exoClassKit' | 'exo' | 'store' | 'zone', (label: string) => string[]>} */
 export const agoricVatDataKeys = {
   exoClass: label => harden([kind(label)]),
   exoClassKit: label => harden([kind(label)]),
   exo: label => harden([kind(label), singleton(label)]),
+  store: label => harden([label]),
+  zone: label => harden([label]),
 };
 harden(agoricVatDataKeys);
--- packages/zone/test/prepare-test-env-ava.js
+++ packages/zone/test/prepare-test-env-ava.js
@@ -1,1 +1,15 @@
 export { test } from '@agoric/swingset-vat/tools/prepare-test-env-ava.js';
+
+let incarnation;
+
+export const annihilate = () => {
+  incarnation = reincarnate({ relaxDurabilityRules: false });
+};
+
+export const getBaggage = () => {
+  return incarnation.fakeVomKit.cm.provideBaggage();
+};
+
+export const nextLife = () => {
+  incarnation = reincarnate(incarnation);
+};
