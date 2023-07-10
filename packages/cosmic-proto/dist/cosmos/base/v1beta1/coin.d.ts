87a2df6b037c44efdd42ad35a1d2e347e4efca22
Turadg Aleahmad <turadg@agoric.com>
2023-07-10 10:14:33 -0700

    build: yarn rebuild

--- packages/cosmic-proto/dist/cosmos/base/v1beta1/coin.d.ts
+++ packages/cosmic-proto/dist/cosmos/base/v1beta1/coin.d.ts
@@ -77,12 +77,12 @@ export declare const DecProto: {
         dec?: string | undefined;
     } & { [K in Exclude<keyof I, "dec">]: never; }>(object: I): DecProto;
 };
-declare type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
-export declare type DeepPartial<T> = T extends Builtin ? T : T extends Long ? string | number | Long : T extends Array<infer U> ? Array<DeepPartial<U>> : T extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>> : T extends {} ? {
+type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
+export type DeepPartial<T> = T extends Builtin ? T : T extends Long ? string | number | Long : T extends Array<infer U> ? Array<DeepPartial<U>> : T extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>> : T extends {} ? {
     [K in keyof T]?: DeepPartial<T[K]>;
 } : Partial<T>;
-declare type KeysOfUnion<T> = T extends T ? keyof T : never;
-export declare type Exact<P, I extends P> = P extends Builtin ? P : P & {
+type KeysOfUnion<T> = T extends T ? keyof T : never;
+export type Exact<P, I extends P> = P extends Builtin ? P : P & {
     [K in keyof P]: Exact<P[K], I[K]>;
 } & {
     [K in Exclude<keyof I, KeysOfUnion<P>>]: never;
