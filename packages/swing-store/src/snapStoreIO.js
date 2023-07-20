33f98b4d73de46a9a9429af31ddea35afb9cb543
Brian Warner <warner@lothar.com>
2023-07-19 19:03:25 -0700

    refactor swingstore into better pieces
    This creates new files to house `importSwingStore`,
    `makeSwingStoreExporter`, and the kvStore. All were previously in
    `swingStore.js`, which is now a shell that gathers together the other
    components. The module exports have been consolidated into
    `src/index.js`, and types are now exported by a `types.d.ts`. No
    behavioral changes.

---
+++ packages/swing-store/src/snapStoreIO.js
@@ -0,0 +1,8 @@
+import { performance } from 'perf_hooks';
+import { makeMeasureSeconds } from '@agoric/internal';
+
+export function makeSnapStoreIO() {
+  return {
+    measureSeconds: makeMeasureSeconds(performance.now),
+  };
+}
--- packages/swing-store/src/util.js
+++ packages/swing-store/src/util.js
@@ -5,7 +5,7 @@ import { Buffer } from 'buffer';
  * 'stream/consumers' package, which unfortunately only exists in newer versions
  * of Node.
  *
- * @param {import('./swingStore').AnyIterable<Uint8Array>} inStream
+ * @param {import('./exporter').AnyIterable<Uint8Array>} inStream
  */
 export const buffer = async inStream => {
   const chunks = [];
