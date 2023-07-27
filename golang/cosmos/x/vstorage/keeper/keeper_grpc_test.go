26d4b6cf70f6515634ce69700b9639b4865ad22f
Richard Gibson <richard.gibson@gmail.com>
2023-07-27 12:50:02 -0400

    chore(cosmos): Avoid special treatment of HTML characters in vstorage JSON encoding

--- golang/cosmos/x/vstorage/keeper/keeper_grpc_test.go
+++ golang/cosmos/x/vstorage/keeper/keeper_grpc_test.go
@@ -1,7 +1,6 @@
 package keeper
 
 import (
-	"encoding/json"
 	"fmt"
 	"reflect"
 	"strings"
@@ -11,7 +10,7 @@ import (
 	grpcStatus "google.golang.org/grpc/status"
 
 	agoric "github.com/Agoric/agoric-sdk/golang/cosmos/types"
-	//"github.com/Agoric/agoric-sdk/golang/cosmos/x/vstorage/capdata"
+	"github.com/Agoric/agoric-sdk/golang/cosmos/x/vstorage/capdata"
 	"github.com/Agoric/agoric-sdk/golang/cosmos/x/vstorage/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -22,7 +21,7 @@ func ptr[T any](v T) *T {
 }
 
 func mustJsonMarshal(val any) string {
-	jsonText, err := json.Marshal(val)
+	jsonText, err := capdata.JsonMarshal(val)
 	if err != nil {
 		panic(err)
 	}
