371f65ab623c3582129a130b07ce5688e86d3d82
Mathieu Hofman <mathieu@agoric.com>
2023-07-29 00:09:50 +0000

    fix(deployment): localize script to upgrade

--- packages/deployment/upgrade-test/upgrade-test-scripts/agoric-upgrade-11/test.sh
+++ packages/deployment/upgrade-test/upgrade-test-scripts/agoric-upgrade-11/test.sh
@@ -6,7 +6,7 @@ echo Wait for actions to settle
 waitForBlock 2
 
 # CWD is agoric-sdk
-upgradeScripts=packages/deployment/upgrade-test/upgrade-test-scripts
+upgrade11=./upgrade-test-scripts/agoric-upgrade-11
 
 # zoe vat is at incarnation 1
-test_val "$(yarn --silent node $upgradeScripts/vat-status.mjs zoe)" "1" "zoe vat incarnation"
+test_val "$(yarn --silent node $upgrade11/vat-status.mjs zoe)" "1" "zoe vat incarnation"
