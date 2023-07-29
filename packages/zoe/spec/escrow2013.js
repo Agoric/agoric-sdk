4c4c6820524f718ea3738c3104767be9a9dda9c5
Dan Connolly <dckc@madmode.com>
2023-07-29 14:59:26 -0500

    chore(escrow2013): migrate eventual send from ! to E()

--- packages/zoe/spec/escrow2013.js
+++ packages/zoe/spec/escrow2013.js
@@ -1,11 +1,13 @@
+import { E } from '@endo/far';
+
 var transfer = (decisionP, srcPurseP, dstPurseP, amount) => {
-  var makeEscrowPurseP = Q.join(srcPurseP ! makePurse,
-                                dstPurseP ! makePurse);
-  var escrowPurseP = makeEscrowPurseP ! ();
+  var makeEscrowPurseP = Q.join(E.get(srcPurseP).makePurse,
+                                E.get(dstPurseP).makePurse);
+  var escrowPurseP = E(makeEscrowPurseP)();
   Q(decisionP).then( // setup phase 2
-    _ => { dstPurseP ! deposit(amount, escrowPurseP); },
-    _ => { srcPurseP ! deposit(amount, escrowPurseP); });
-  return escrowPurseP ! deposit(amount, srcPurseP); // phase 1
+    _ => { E(dstPurseP).deposit(amount, escrowPurseP); },
+    _ => { E(srcPurseP).deposit(amount, escrowPurseP); });
+  return E(escrowPurseP).deposit(amount, srcPurseP); // phase 1
 };
 
 var failOnly = cancellationP => Q(cancellationP).then(
