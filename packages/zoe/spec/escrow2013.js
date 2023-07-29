5937e4db9872d95e53882d3d4a6dce37b08623ce
Dan Connolly <dckc@madmode.com>
2023-07-29 15:02:23 -0500

    chore(escrow2013): modern Agoric JS style
     - var -> let/const
     - linebreaks in ()

--- packages/zoe/spec/escrow2013.js
+++ packages/zoe/spec/escrow2013.js
@@ -1,8 +1,8 @@
 import { E } from '@endo/far';
 
-var Q = Promise;
+const Q = Promise;
 
-var Qjoin = (p1, p2) =>
+const Qjoin = (p1, p2) =>
   Q.all([p1, p2]).then(([r1, r2]) => {
     if (!Object.is(r1, r2)) {
       throw Error('join failed');
@@ -10,27 +10,44 @@ var Qjoin = (p1, p2) =>
     return r1;
   });
 
-var transfer = (decisionP, srcPurseP, dstPurseP, amount) => {
-  var makeEscrowPurseP = Qjoin(E.get(srcPurseP).makePurse,
-                               E.get(dstPurseP).makePurse);
-  var escrowPurseP = E(makeEscrowPurseP)();
-  Q(decisionP).then( // setup phase 2
-    _ => { E(dstPurseP).deposit(amount, escrowPurseP); },
-    _ => { E(srcPurseP).deposit(amount, escrowPurseP); });
+const transfer = (decisionP, srcPurseP, dstPurseP, amount) => {
+  const makeEscrowPurseP = Qjoin(
+    E.get(srcPurseP).makePurse,
+    E.get(dstPurseP).makePurse
+  );
+  const escrowPurseP = E(makeEscrowPurseP)();
+  // setup phase 2
+  Q(decisionP).then(
+    _ => {
+      E(dstPurseP).deposit(amount, escrowPurseP);
+    },
+    _ => {
+      E(srcPurseP).deposit(amount, escrowPurseP);
+    }
+  );
   return E(escrowPurseP).deposit(amount, srcPurseP); // phase 1
 };
 
-var failOnly = cancellationP => Q(cancellationP).then(
-  cancellation => { throw cancellation; });
+const failOnly = cancellationP =>
+  Q(cancellationP).then(cancellation => {
+    throw cancellation;
+  });
 
-var escrowExchange = (a, b) => { // a from Alice , b from Bob
-  var decide;
-  var decisionP = Q.promise(resolve => { decide = resolve; });
-  decide(Q.race([Q.all([
-      transfer(decisionP, a.moneySrcP, b.moneyDstP, b.moneyNeeded),
-      transfer(decisionP, b.stockSrcP, a.stockDstP, a.stockNeeded)
-    ]),
-    failOnly(a.cancellationP),
-    failOnly(b.cancellationP)]));
+// a from Alice , b from Bob
+const escrowExchange = (a, b) => {
+  let decide;
+  const decisionP = Q.promise(resolve => {
+    decide = resolve;
+  });
+  decide(
+    Q.race([
+      Q.all([
+        transfer(decisionP, a.moneySrcP, b.moneyDstP, b.moneyNeeded),
+        transfer(decisionP, b.stockSrcP, a.stockDstP, a.stockNeeded),
+      ]),
+      failOnly(a.cancellationP),
+      failOnly(b.cancellationP),
+    ])
+  );
   return decisionP;
 };
