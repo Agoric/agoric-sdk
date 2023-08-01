2bbf894e75780fe598df6725d62e9516b207eba4
Mark S. Miller <erights@gmail.com>
2023-07-31 19:57:16 -0700

    fix: make reorder tolerant

--- packages/inter-protocol/test/smartWallet/test-psm-integration.js
+++ packages/inter-protocol/test/smartWallet/test-psm-integration.js
@@ -465,7 +465,9 @@ test('agoricName invitation source errors', async t => {
     }),
     {
       message:
-        '{"instancePath":["psm-IST-AUSD"],"source":"agoricContract"} - Must have missing properties ["callPipe"]',
+        // TODO The pattern is here only as a temporary measure to tolerate
+        // the property order being sorted and not.
+        /\{("instancePath":\["psm-IST-AUSD"\]|,|"source":"agoricContract")*\} - Must have missing properties \["callPipe"\]/,
     },
   );
   t.is(await E.get(getBalanceFor(anchor.brand)).value, 0n);
