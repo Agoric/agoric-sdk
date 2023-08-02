8050e2aee2a745e44718afa000111b20b77d485e
Turadg Aleahmad <turadg@agoric.com>
2023-08-02 19:04:37 -0400

    refactor: move setup-vat-data to liveslots

--- packages/zone/test/prepare-test-env-ava.js
+++ packages/zone/test/prepare-test-env-ava.js
@@ -1,5 +1,5 @@
 import '@agoric/swingset-vat/tools/prepare-test-env-ava.js';
-import { reincarnate } from '@agoric/swingset-vat/tools/setup-vat-data.js';
+import { reincarnate } from '@agoric/swingset-liveslots/tools/setup-vat-data.js';
 
 export { test } from '@agoric/swingset-vat/tools/prepare-test-env-ava.js';
 
