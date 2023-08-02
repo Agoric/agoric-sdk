d1e7e3473cdb5298f816381a726077f2a4cb41d9
Turadg Aleahmad <turadg@agoric.com>
2023-08-02 18:26:24 -0400

    test: simpler presence test

--- packages/vat-data/test/present.test.js
+++ packages/vat-data/test/present.test.js
@@ -1,7 +1,12 @@
-import { test } from '@agoric/swingset-vat/tools/prepare-test-env-ava.js';
+import '@endo/init/debug.js';
+import test from 'ava';
+
+const mockDefineKind = /** @type {any} */ (harden({}));
+
+/* global globalThis */
+globalThis.VatData ||= { defineKind: mockDefineKind };
 
 test('methods available', async t => {
   const { defineKind } = await import('../src/index.js');
-  defineKind('test', () => {}, {});
-  t.pass();
+  t.is(defineKind, mockDefineKind);
 });
