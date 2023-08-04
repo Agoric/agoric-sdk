97d4ec5b709a1461faadeec9517a08ed1a22c7ed
Turadg Aleahmad <turadg@agoric.com>
2023-08-04 09:38:58 -0400

    build: don't publish agoric/boot

--- packages/boot/index.js
+++
@@ -1,6 +0,0 @@
-// @ts-check
-// Ambient types
-import '@agoric/vats/src/core/types.js';
-import '@agoric/zoe/src/contractFacet/types.js';
-import '@agoric/zoe/src/types.js';
-import '@agoric/zoe/tools/types-ambient.js';
