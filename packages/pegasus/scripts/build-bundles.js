1604b61d92c9cb7e8a6907b00d8f92ffece394b5
Turadg Aleahmad <turadg@agoric.com>
2023-08-10 16:36:02 -0400

    build: rm obsolete build-bundles

--- packages/pegasus/scripts/build-bundles.js
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env node
-import '@endo/init';
-import { extractProposalBundles } from '@agoric/deploy-script-support';
-import url from 'url';
-
-import { defaultProposalBuilder } from './init-core.js';
-
-const dirname = url.fileURLToPath(new URL('.', import.meta.url));
-extractProposalBundles([['.', defaultProposalBuilder]], dirname);
