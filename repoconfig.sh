ce7d183cd5f292a27863faac65831e2b781504aa
Michael FIG <mfig@agoric.com>
2023-08-25 13:44:56 -0600

    fix(agd): make script more robust

--- repoconfig.sh
+++ repoconfig.sh
@@ -7,10 +7,12 @@ GOLANG_DAEMON=$GOLANG_DIR/build/agd
 XSNAP_VERSION=agoric-upgrade-10
 
 # Args are major, minor and patch version numbers
-function golang_version_check() {
-  [ $1 -eq 1 ] && [ $2 -eq 20 ] && [ $3 -ge 2 ] && return 0
-  [ $1 -eq 1 ] && [ $2 -ge 21 ] && return 0
-  [ $1 -ge 2 ] && return 0
-  echo "need go version 1.20.2+, 1.21+, or 2+"
+golang_version_check() {
+  {
+    [ "$1" -eq 1 ] && [ "$2" -eq 20 ] && [ "$3" -ge 2 ] && return 0
+    [ "$1" -eq 1 ] && [ "$2" -ge 21 ] && return 0
+    [ "$1" -ge 2 ] && return 0
+  } 2>/dev/null
+  echo 1>&2 "need go version 1.20.2+, 1.21+, or 2+"
   return 1
 }
