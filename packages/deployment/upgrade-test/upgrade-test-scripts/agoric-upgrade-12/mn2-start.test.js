8b514ef6936c97d4f7e95b4a75efbe1ce90d7145
Dan Connolly <connolly@agoric.com>
2023-09-20 12:31:52 -0500

    chore: move mn2-start to test script

--- packages/deployment/upgrade-test/upgrade-test-scripts/agoric-upgrade-12/mn2-start.test.js
+++ packages/deployment/upgrade-test/upgrade-test-scripts/agoric-upgrade-12/mn2-start.test.js
@@ -43,6 +43,30 @@ export const getContractInfo = async (path, io = {}) => {
   return m.fromCapData({ body, slots });
 };
 
+const ensureMintLimit = async (targetNum, manager = 0, unit = 1_000_000) => {
+  const io = { agoric };
+  const [{ current }, metrics] = await Promise.all([
+    getContractInfo(`vaultFactory.managers.manager${manager}.governance`, io),
+    getContractInfo(`vaultFactory.managers.manager${manager}.metrics`, io),
+  ]);
+  const { totalDebt } = metrics;
+  const {
+    DebtLimit: { value: limit },
+  } = current;
+  const nums = {
+    total: Number(totalDebt.value) / unit,
+    limit: Number(limit.value) / unit,
+    target: targetNum,
+  };
+  nums.target += nums.total;
+  console.log(
+    nums,
+    nums.limit >= nums.target ? 'limit > target' : 'LIMIT TOO LOW',
+  );
+  if (nums.limit >= nums.target) return;
+  throw Error('raising mint limit not impl');
+};
+
 /**
  * @param {string[]} argv
  * @param {Record<string, string | undefined>} env
@@ -70,30 +94,6 @@ const main = async (argv, env, { execFileSync, stat }) => {
     const coin = coins.balances.find(a => a.denom === denom);
     console.log(denom, 'balance:', coin);
     return Number(coin.amount) / unit;
-  };
-
-  const ensureMintLimit = async (targetNum, manager = 0, unit = 1_000_000) => {
-    const io = { agoric };
-    const [{ current }, metrics] = await Promise.all([
-      getContractInfo(`vaultFactory.managers.manager${manager}.governance`, io),
-      getContractInfo(`vaultFactory.managers.manager${manager}.metrics`, io),
-    ]);
-    const { totalDebt } = metrics;
-    const {
-      DebtLimit: { value: limit },
-    } = current;
-    const nums = {
-      total: Number(totalDebt.value) / unit,
-      limit: Number(limit.value) / unit,
-      target: targetNum,
-    };
-    nums.target += nums.total;
-    console.log(
-      nums,
-      nums.limit >= nums.target ? 'limit > target' : 'LIMIT TOO LOW',
-    );
-    if (nums.limit >= nums.target) return;
-    throw Error('raising mint limit not impl');
   };
 
   const { round } = Math;
@@ -126,8 +126,9 @@ const main = async (argv, env, { execFileSync, stat }) => {
   await ensureInstallFunds();
 };
 
-// defensive copies of argv, env
 main(
+  // defensive copies of argv, env
+  // to avoid global mutable state
   [...process.argv],
   { ...process.env },
   {
