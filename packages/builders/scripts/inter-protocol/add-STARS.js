4f9a171b79cd2a3ca5576ea452a4c8c595d5deb8
Dan Connolly <dckc@madmode.com>
2023-10-13 10:32:15 -0500

    WIP: use stATOM params in place of STARS
    based on "Proposal Builder" section of
    https://github.com/0xpatrickdev/agoric-vault-collateral-proposal/pull/22

--- packages/builders/scripts/inter-protocol/add-STARS.js
+++ packages/builders/scripts/inter-protocol/add-STARS.js
@@ -6,13 +6,15 @@ import { defaultProposalBuilder as oraclesProposalBuilder } from './price-feed-c
 export const starsVaultProposalBuilder = async powers => {
   return vaultProposalBuilder(powers, {
     interchainAssetOptions: {
-      // Values for the Stargaze token on Osmosis
       denom:
-        'ibc/987C17B11ABC2B20019178ACE62929FE9840202CE79498E29FE8E5CB02B7C0A4',
+        'ibc/49C630713B2AB60653F76C0C58D43C2A64956803B4D422CACB6DD4AD016ED846',
       decimalPlaces: 6,
-      keyword: 'STARS',
-      oracleBrand: 'STARS',
-      proposedName: 'STARS',
+      initialPrice: undefined,
+      issuerBoardId: undefined,
+      issuerName: 'stATOM',
+      keyword: 'STATOM',
+      oracleBrand: 'stATOM',
+      proposedName: 'stATOM',
     },
   });
 };
@@ -20,24 +22,28 @@ export const starsVaultProposalBuilder = async powers => {
 /** @type {import('@agoric/deploy-script-support/src/externalTypes.js').ProposalBuilder} */
 export const starsOraclesProposalBuilder = async powers => {
   return oraclesProposalBuilder(powers, {
-    AGORIC_INSTANCE_NAME: `STARS-USD price feed`,
-    IN_BRAND_LOOKUP: ['agoricNames', 'oracleBrand', 'STARS'],
+    AGORIC_INSTANCE_NAME: `stATOM-USD price feed`,
+    IN_BRAND_LOOKUP: ['agoricNames', 'oracleBrand', 'stATOM'],
     IN_BRAND_DECIMALS: 6,
     OUT_BRAND_LOOKUP: ['agoricNames', 'oracleBrand', 'USD'],
     OUT_BRAND_DECIMALS: 4,
     oracleAddresses: [
-      // copied from decentral-main-vaults-config.json
-      'agoric1krunjcqfrf7la48zrvdfeeqtls5r00ep68mzkr',
-      'agoric19uscwxdac6cf6z7d5e26e0jm0lgwstc47cpll8',
-      'agoric144rrhh4m09mh7aaffhm6xy223ym76gve2x7y78',
-      'agoric19d6gnr9fyp6hev4tlrg87zjrzsd5gzr5qlfq2p',
-      'agoric1n4fcxsnkxe4gj6e24naec99hzmc4pjfdccy5nj',
+      'agoric1ldmtatp24qlllgxmrsjzcpe20fvlkp448zcuce',
+      'agoric140dmkrz2e42ergjj7gyvejhzmjzurvqeq82ang',
+      'agoric1w8wktaur4zf8qmmtn3n7x3r0jhsjkjntcm3u6h',
+      'agoric10vjkvkmpp9e356xeh6qqlhrny2htyzp8hf88fk',
+      'agoric1qj07c7vfk3knqdral0sej7fa6eavkdn8vd8etf',
+      'agoric1lw4e4aas9q84tq0q92j85rwjjjapf8dmnllnft',
+      'agoric1ra0g6crtsy6r3qnpu7ruvm7qd4wjnznyzg5nu4',
+      'agoric1zj6vrrrjq4gsyr9lw7dplv4vyejg3p8j2urm82',
+      'agoric15xddzse9lq74cyt6ev9d7wywxerenxdgxsdc3m',
+      'agoric1w5wmck6q2xrt20ax3njlk2k87m4t4l2y2xgw2d',
     ],
   });
 };
 
 export default async (homeP, endowments) => {
   const { writeCoreProposal } = await makeHelpers(homeP, endowments);
-  await writeCoreProposal('add-STARS', starsVaultProposalBuilder);
-  await writeCoreProposal('add-STARS-oracles', starsOraclesProposalBuilder);
+  await writeCoreProposal('add-stATOM', starsVaultProposalBuilder);
+  await writeCoreProposal('add-stATOM-oracles', starsOraclesProposalBuilder);
 };
