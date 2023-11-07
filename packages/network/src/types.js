555593233bd245bb9994ec9a4740550592a69e4e
Michael FIG <mfig@agoric.com>
2023-11-06 21:39:11 -0600

    feat(network): allow protocols to use Whenables

--- packages/network/src/types.js
+++ packages/network/src/types.js
@@ -1,3 +1,10 @@
+// @ts-check
+
+/**
+ * @template T
+ * @typedef {Promise<T | import('@agoric/whenable').Whenable<T>>} PromiseWhenable
+ */
+
 /**
  * @typedef {string | Buffer | ArrayBuffer} Data
  *
@@ -11,12 +18,12 @@
 
 /**
  * @typedef {object} Closable A closable object
- * @property {() => Promise<void>} close Terminate the object
+ * @property {() => PromiseWhenable<void>} close Terminate the object
  */
 
 /**
  * @typedef {object} Protocol The network Protocol
- * @property {(prefix: Endpoint) => Promise<Port>} bind Claim a port, or if
+ * @property {(prefix: Endpoint) => PromiseWhenable<Port>} bind Claim a port, or if
  *   ending in ENDPOINT_SEPARATOR, a fresh name
  */
 
@@ -24,14 +31,14 @@
  * @typedef {object} Port A port that has been bound to a protocol
  * @property {() => Endpoint} getLocalAddress Get the locally bound name of this
  *   port
- * @property {(acceptHandler: ListenHandler) => Promise<void>} addListener
+ * @property {(acceptHandler: ListenHandler) => PromiseWhenable<void>} addListener
  *   Begin accepting incoming connections
  * @property {(
  *   remote: Endpoint,
  *   connectionHandler?: ConnectionHandler,
- * ) => Promise<Connection>} connect
+ * ) => PromiseWhenable<Connection>} connect
  *   Make an outbound connection
- * @property {(acceptHandler: ListenHandler) => Promise<void>} removeListener
+ * @property {(acceptHandler: ListenHandler) => PromiseWhenable<void>} removeListener
  *   Remove the currently-bound listener
  * @property {() => void} revoke Deallocate the port entirely, removing all
  *   listeners and closing all active connections
@@ -39,25 +46,25 @@
 
 /**
  * @typedef {object} ListenHandler A handler for incoming connections
- * @property {(port: Port, l: ListenHandler) => Promise<void>} [onListen] The
+ * @property {(port: Port, l: ListenHandler) => PromiseWhenable<void>} [onListen] The
  *   listener has been registered
  * @property {(
  *   port: Port,
  *   localAddr: Endpoint,
  *   remoteAddr: Endpoint,
  *   l: ListenHandler,
- * ) => Promise<ConnectionHandler>} onAccept
+ * ) => PromiseWhenable<ConnectionHandler>} onAccept
  *   A new connection is incoming
  * @property {(
  *   port: Port,
  *   localAddr: Endpoint,
  *   remoteAddr: Endpoint,
  *   l: ListenHandler,
- * ) => Promise<void>} [onReject]
+ * ) => PromiseWhenable<void>} [onReject]
  *   The connection was rejected
- * @property {(port: Port, rej: any, l: ListenHandler) => Promise<void>} [onError]
+ * @property {(port: Port, rej: any, l: ListenHandler) => PromiseWhenable<void>} [onError]
  *   There was an error while listening
- * @property {(port: Port, l: ListenHandler) => Promise<void>} [onRemove] The
+ * @property {(port: Port, l: ListenHandler) => PromiseWhenable<void>} [onRemove] The
  *   listener has been removed
  */
 
@@ -66,9 +73,9 @@
  * @property {(
  *   packetBytes: Data,
  *   opts?: Record<string, any>,
- * ) => Promise<Bytes>} send
+ * ) => PromiseWhenable<Bytes>} send
  *   Send a packet on the connection
- * @property {() => Promise<void>} close Close both ends of the connection
+ * @property {() => PromiseWhenable<void>} close Close both ends of the connection
  * @property {() => Endpoint} getLocalAddress Get the locally bound name of this
  *   connection
  * @property {() => Endpoint} getRemoteAddress Get the name of the counterparty
@@ -85,16 +92,16 @@
  *   The connection has been opened
  * @property {(
  *   connection: Connection,
- *   packetBytes: Bytes,
+ *   ack: Bytes,
  *   c: ConnectionHandler,
  *   opts?: Record<string, any>,
- * ) => Promise<Data>} [onReceive]
+ * ) => PromiseWhenable<Data>} [onReceive]
  *   The connection received a packet
  * @property {(
  *   connection: Connection,
  *   reason?: CloseReason,
  *   c?: ConnectionHandler,
- * ) => Promise<void>} [onClose]
+ * ) => PromiseWhenable<void>} [onClose]
  *   The connection has been closed
  *
  * @typedef {any | null} CloseReason The reason a connection was closed
@@ -110,36 +117,36 @@
 /**
  * @typedef {object} ProtocolHandler A handler for things the protocol
  *   implementation will invoke
- * @property {(protocol: ProtocolImpl, p: ProtocolHandler) => Promise<void>} onCreate
+ * @property {(protocol: ProtocolImpl, p: ProtocolHandler) => PromiseWhenable<void>} onCreate
  *   This protocol is created
- * @property {(localAddr: Endpoint, p: ProtocolHandler) => Promise<string>} generatePortID
+ * @property {(localAddr: Endpoint, p: ProtocolHandler) => PromiseWhenable<string>} generatePortID
  *   Create a fresh port identifier for this protocol
  * @property {(
  *   port: Port,
  *   localAddr: Endpoint,
  *   p: ProtocolHandler,
- * ) => Promise<void>} onBind
+ * ) => PromiseWhenable<void>} onBind
  *   A port will be bound
  * @property {(
  *   port: Port,
  *   localAddr: Endpoint,
  *   listenHandler: ListenHandler,
  *   p: ProtocolHandler,
- * ) => Promise<void>} onListen
+ * ) => PromiseWhenable<void>} onListen
  *   A port was listening
  * @property {(
  *   port: Port,
  *   localAddr: Endpoint,
  *   listenHandler: ListenHandler,
  *   p: ProtocolHandler,
- * ) => Promise<void>} onListenRemove
+ * ) => PromiseWhenable<void>} onListenRemove
  *   A port listener has been reset
  * @property {(
  *   port: Port,
  *   localAddr: Endpoint,
  *   remote: Endpoint,
  *   p: ProtocolHandler,
- * ) => Promise<Endpoint>} [onInstantiate]
+ * ) => PromiseWhenable<Endpoint>} [onInstantiate]
  *   Return unique suffix for local address
  * @property {(
  *   port: Port,
@@ -147,36 +154,36 @@
  *   remote: Endpoint,
  *   c: ConnectionHandler,
  *   p: ProtocolHandler,
- * ) => Promise<AttemptDescription>} onConnect
+ * ) => PromiseWhenable<AttemptDescription>} onConnect
  *   A port initiates an outbound connection
  * @property {(
  *   port: Port,
  *   localAddr: Endpoint,
  *   p: ProtocolHandler,
- * ) => Promise<void>} onRevoke
+ * ) => PromiseWhenable<void>} onRevoke
  *   The port is being completely destroyed
  *
  * @typedef {object} InboundAttempt An inbound connection attempt
- * @property {(desc: AttemptDescription) => Promise<Connection>} accept
+ * @property {(desc: AttemptDescription) => PromiseWhenable<Connection>} accept
  *   Establish the connection
  * @property {() => Endpoint} getLocalAddress Return the local address for this
  *   attempt
  * @property {() => Endpoint} getRemoteAddress Return the remote address for
  *   this attempt
- * @property {() => Promise<void>} close Abort the attempt
+ * @property {() => PromiseWhenable<void>} close Abort the attempt
  *
  * @typedef {object} ProtocolImpl Things the protocol can do for us
- * @property {(prefix: Endpoint) => Promise<Port>} bind Claim a port, or if
+ * @property {(prefix: Endpoint) => PromiseWhenable<Port>} bind Claim a port, or if
  *   ending in ENDPOINT_SEPARATOR, a fresh name
  * @property {(
  *   listenAddr: Endpoint,
  *   remoteAddr: Endpoint,
- * ) => Promise<InboundAttempt>} inbound
+ * ) => PromiseWhenable<InboundAttempt>} inbound
  *   Make an attempt to connect into this protocol
  * @property {(
  *   port: Port,
  *   remoteAddr: Endpoint,
  *   connectionHandler: ConnectionHandler,
- * ) => Promise<Connection>} outbound
+ * ) => PromiseWhenable<Connection>} outbound
  *   Create an outbound connection
  */
