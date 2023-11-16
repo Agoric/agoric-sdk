b9e57435a02245f47c7c15e8a4a043f408d493c3
Dan Connolly <connolly@agoric.com>
2023-11-16 13:34:49 -0600

    refactor(gimix): clarify approach to rendering proposal

--- packages/zoe/test/unitTests/contracts/gimix/test-gimix-proposal.js
+++ packages/zoe/test/unitTests/contracts/gimix/test-gimix-proposal.js
@@ -8,28 +8,83 @@ import { permit } from '../../../../src/contracts/gimix/start-gimix.js';
 
 const asset = ref => fileURLToPath(new URL(ref, import.meta.url));
 
-const noImport = txt =>
-  txt.replace(/^\s*import\b\s*(.*)/gm, '// REDACTED: $1\n');
-const noExport = txt => txt.replace(/^\s*export\b\s*/gm, '');
-const defang = txt => txt.replace(/\bimport\b/g, 'XMPORT');
-
-test('gimix', async t => {
-  const { env } = process;
+/**
+ * If $SCRIPT is set, the test(s) below will write
+ * the rendered script to a file by that name.
+ * Likeiwse $PERMIT.
+ *
+ * If $ORACLE_ADDRESS is set, it is substituted into the script.
+ */
+test.before(async t => {
   const bundleCache = await unsafeMakeBundleCache('bundles/');
 
+  t.context = {
+    readFile: fsp.readFile,
+    env: process.env,
+    writeFile: fsp.writeFile,
+    bundle: await bundleCache.load(
+      asset('../../../../src/contracts/gimix/gimix.js'),
+      'gimix',
+    ),
+  };
+});
+
+const redactImportDecls = txt =>
+  txt.replace(/^\s*import\b\s*(.*)/gm, '// REDACTED: $1');
+const omitExportKewords = txt => txt.replace(/^\s*export\b\s*/gm, '');
+// cf. ses rejectImportExpressions
+// https://github.com/endojs/endo/blob/ebc8f66e9498f13085a8e64e17fc2f5f7b528faa/packages/ses/src/transforms.js#L143
+const hideImportExpr = txt => txt.replace(/\bimport\b/g, 'XMPORT');
+
+test('module to script: redact imports; omit export keywords', t => {
+  const modText = `
+import { E, Far } from '@endo/far';
+
+/** @param {import('wonderland').Carol} carol */
+export contst alice = (carol) => {
+    E(bob).greet(carol);
+};
+    `;
+
+  const expected = `
+// REDACTED: { E, Far } from '@endo/far';
+
+/** @param {XMPORT('wonderland').Carol} carol */
+contst alice = (carol) => {
+    E(bob).greet(carol);
+};
+    `;
+
+  const script = hideImportExpr(redactImportDecls(omitExportKewords(modText)));
+  t.is(script.trim(), expected.trim());
+});
+
+test('check / save gimix permit', async t => {
   t.notThrows(() => JSON.parse(permit));
 
-  const bundle = await bundleCache.load(
-    asset('../../../../src/contracts/gimix/gimix.js'),
-    'gimix',
-  );
+  // IDEA: check permit against a pattern
 
-  const modText = await fsp.readFile(
+  // @ts-expect-error TODO: types of t.context
+  const { env } = t.context;
+  if (env.PERMIT) {
+    // @ts-expect-error TODO: types of t.context
+    const { writeFile } = t.context;
+    const pretty = JSON.stringify(JSON.parse(permit), null, 2);
+    t.log('write permit to', env.PERMIT);
+    await t.notThrowsAsync(writeFile(env.PERMIT, pretty));
+  }
+});
+
+test('check / render gimix proposal', async t => {
+  // @ts-expect-error TODO: types of t.context
+  const { env, readFile, bundle } = t.context;
+
+  const modText = await readFile(
     asset('../../../../src/contracts/gimix/start-gimix.js'),
     'utf-8',
   );
 
-  let script = defang(noExport(noImport(modText)));
+  let script = hideImportExpr(redactImportDecls(omitExportKewords(modText)));
 
   script = script.replace(
     /bundleID = fail.*/,
@@ -42,16 +97,15 @@ test('gimix', async t => {
       `oracleAddress = ${JSON.stringify(env.ORACLE_ADDRESS)},`,
     );
   }
+
   const c = new Compartment({ E: () => {}, Far: () => {} });
   t.notThrows(() => c.evaluate(script));
 
-  if (env.PERMIT) {
-    const pretty = JSON.stringify(JSON.parse(permit), null, 2);
-    t.log('write permit to', env.PERMIT);
-    await t.notThrowsAsync(fsp.writeFile(env.PERMIT, pretty));
-  }
   if (env.SCRIPT) {
+    // @ts-expect-error TODO: types of t.context
+    const { writeFile } = t.context;
+
     t.log('write script to', env.SCRIPT);
-    await t.notThrowsAsync(fsp.writeFile(env.SCRIPT, script));
+    await t.notThrowsAsync(writeFile(env.SCRIPT, script));
   }
 });
