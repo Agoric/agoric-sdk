aa47e3e329f80052751c8585b4d64828332af1f3
Turadg Aleahmad <turadg@agoric.com>
2023-11-16 16:43:01 -0800

    fixup

--- packages/crowdfunding/src/proposals/startCrowdfunding.js
+++ packages/crowdfunding/src/proposals/startCrowdfunding.js
@@ -1,12 +1,45 @@
+import { Fail } from '@agoric/assert';
 import { makeTracer } from '@agoric/internal';
 import { makeStorageNodeChild } from '@agoric/internal/src/lib-chainStorage.js';
 import { Stable } from '@agoric/internal/src/tokens.js';
 import { E } from '@endo/far';
-import { publishAgoricBrandsDisplayInfo } from '@agoric/smart-wallet/src/proposals/upgrade-walletFactory-proposal.js';
+import { makeMarshal } from '@endo/marshal';
 
 const trace = makeTracer('StartCrowdfunding', true);
 
 export const CONTRACT_NAME = 'crowdfunding';
+
+//#region Copy-pasta from upgrade-walletFactory-proposal
+// XXX importing caused: Error#1: publishAgoricBrandsDisplayInfo not in CONTRACT_NAME,getManifestForCrowdfunding,startCrowdfunding
+const BOARD_AUX = 'boardAux';
+const marshalData = makeMarshal(_val => Fail`data only`);
+/**
+ * @param { BootstrapPowers } powers
+ */
+export const publishAgoricBrandsDisplayInfo = async ({
+  consume: { agoricNames, board, chainStorage },
+}) => {
+  // chainStorage type includes undefined, which doesn't apply here.
+  // @ts-expect-error UNTIL https://github.com/Agoric/agoric-sdk/issues/8247
+  const boardAux = E(chainStorage).makeChildNode(BOARD_AUX);
+  const publishBrandInfo = async brand => {
+    const [id, displayInfo, allegedName] = await Promise.all([
+      E(board).getId(brand),
+      E(brand).getDisplayInfo(),
+      E(brand).getAllegedName(),
+    ]);
+    const node = E(boardAux).makeChildNode(id);
+    const aux = marshalData.toCapData(harden({ allegedName, displayInfo }));
+    await E(node).setValue(JSON.stringify(aux));
+  };
+
+  /** @type {ERef<import('@agoric/vats').NameHub>} */
+  const brandHub = E(agoricNames).lookup('brand');
+  const brands = await E(brandHub).values();
+  // tolerate failure; in particular, for the timer brand
+  await Promise.allSettled(brands.map(publishBrandInfo));
+};
+harden(publishAgoricBrandsDisplayInfo);
 
 /**
  * @param {import('@agoric/inter-protocol/src/proposals/econ-behaviors').EconomyBootstrapPowers} powers
