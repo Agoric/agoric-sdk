6c4707dacf293ff3942a277b71e0e813f307721d
Dan Connolly <dckc@madmode.com>
2023-12-10 22:00:24 -0600

    WIP: upgradeable postalSvc WIP! WIP!
    Upgrading this to terminalIncarnation.js didn't work:
    
    ```
    🤞 UnhandledPromiseRejection: (Error#1)
    Nested error under Error#1
    Error#1: failed to reanimate Alleged: PostalSvc creatorFacet
    ```
    
    So I might roll it back.

--- packages/zoe/src/contracts/gimix/postalSvc.js
+++ packages/zoe/src/contracts/gimix/postalSvc.js
@@ -1,61 +1,78 @@
 // @ts-check
 import { E, Far } from '@endo/far';
 import { withdrawFromSeat } from '../../contractSupport/zoeHelpers.js';
+import { prepareExo, prepareExoClassKit, provide } from '@agoric/vat-data';
 
 const { keys, values } = Object;
+
+/** @type {ContractMeta} */
+export const meta = harden({
+  upgradability: 'canBeUpgraded',
+});
 
 /**
  * @typedef {object} PostalSvcTerms
  * @property {import('@agoric/vats').NameHub} namesByAddress
  */
 
-/** @param {ZCF<PostalSvcTerms>} zcf */
-export const start = zcf => {
+/**
+ * @param {ZCF<PostalSvcTerms>} zcf
+ * @param {unknown} _pf
+ * @param {Baggage} baggage
+ */
+export const start = (zcf, _pf, baggage) => {
   const { namesByAddress, issuers } = zcf.getTerms();
   console.log('postalSvc issuers', Object.keys(issuers));
 
-  /**
-   * @param {string} addr
-   * @returns {ERef<DepositFacet>}
-   */
-  const getDepositFacet = addr => {
-    assert.typeof(addr, 'string');
-    return E(namesByAddress).lookup(addr, 'depositFacet');
-  };
+  const make = prepareExoClassKit(baggage, 'PostalSvc', undefined, () => ({}), {
+    publicFacet: {
+      lookup(...path) {
+        return E(namesByAddress).lookup(...path);
+      },
+      /**
+       * @param {string} addr
+       * @returns {ERef<DepositFacet>}
+       */
+      getDepositFacet(addr) {
+        assert.typeof(addr, 'string');
+        return E(namesByAddress).lookup(addr, 'depositFacet');
+      },
 
-  /**
-   * @param {string} addr
-   * @param {Payment} pmt
-   */
-  const sendTo = (addr, pmt) => E(getDepositFacet(addr)).receive(pmt);
+      /**
+       * @param {string} addr
+       * @param {Payment} pmt
+       */
+      sendTo(addr, pmt) {
+        const { publicFacet } = this.facets;
+        return E(publicFacet.getDepositFacet(addr)).receive(pmt);
+      },
 
-  /** @param {string} recipient */
-  const makeSendInvitation = recipient => {
-    assert.typeof(recipient, 'string');
+      /** @param {string} recipient */
+      makeSendInvitation(recipient) {
+        assert.typeof(recipient, 'string');
+        const { publicFacet } = this.facets;
 
-    /** @type {OfferHandler} */
-    const handleSend = async seat => {
-      const { give } = seat.getProposal();
-      const depositFacet = await getDepositFacet(recipient);
-      const payouts = await withdrawFromSeat(zcf, seat, give);
+        /** @type {OfferHandler} */
+        const handleSend = async seat => {
+          const { give } = seat.getProposal();
+          const depositFacet = await publicFacet.getDepositFacet(recipient);
+          const payouts = await withdrawFromSeat(zcf, seat, give);
 
-      // XXX partial failure? return payments?
-      await Promise.all(
-        values(payouts).map(pmtP =>
-          Promise.resolve(pmtP).then(pmt => E(depositFacet).receive(pmt)),
-        ),
-      );
-      return `sent ${keys(payouts).join(', ')}`;
-    };
+          // XXX partial failure? return payments?
+          await Promise.all(
+            values(payouts).map(pmtP =>
+              Promise.resolve(pmtP).then(pmt => E(depositFacet).receive(pmt)),
+            ),
+          );
+          return `sent ${keys(payouts).join(', ')}`;
+        };
 
-    return zcf.makeInvitation(handleSend, 'send');
-  };
-
-  const publicFacet = Far('postalSvc', {
-    lookup: (...path) => E(namesByAddress).lookup(...path),
-    getDepositFacet,
-    sendTo,
-    makeSendInvitation,
+        return zcf.makeInvitation(handleSend, 'send');
+      },
+    },
+    creatorFacet: {},
   });
-  return { publicFacet };
+
+  const facets = provide(baggage, 'postalSvc', make);
+  return facets;
 };
