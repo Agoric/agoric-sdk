a0d8229524d8a940c56d28309f67881557bc1ad3
Richard Gibson <richard.gibson@gmail.com>
2023-12-24 01:46:03 -0500

    chore(golang): Clarify error messages (#8677)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- golang/cosmos/x/swingset/types/types.go
+++ golang/cosmos/x/swingset/types/types.go
@@ -3,6 +3,7 @@ package types
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 
 	vstoragetypes "github.com/Agoric/agoric-sdk/golang/cosmos/x/vstorage/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -25,18 +26,16 @@ func NewEgress(nickname string, peer sdk.AccAddress, powerFlags []string) *Egres
 	}
 }
 
-// FIXME: Should have @endo/nat
+// Nat is analogous to @endo/nat
+// https://github.com/endojs/endo/blob/master/packages/nat
 func Nat(num float64) (uint64, error) {
-	if num < 0 {
-		return 0, errors.New("Not a natural")
+	if 0 <= num && num < (1<<53) {
+		nat := uint64(num)
+		if float64(nat) == num {
+			return nat, nil
+		}
 	}
-
-	nat := uint64(num)
-	if float64(nat) != num {
-		return 0, errors.New("Not a precise integer")
-	}
-
-	return nat, nil
+	return 0, errors.New("Not a Nat")
 }
 
 type Messages struct {
@@ -60,11 +59,11 @@ func UnmarshalMessagesJSON(jsonString string) (ret *Messages, err error) {
 
 	ackFloat, ok := packet[1].(float64)
 	if !ok {
-		return nil, errors.New("Ack is not an integer")
+		return nil, errors.New("Ack is not a number")
 	}
 	ret.Ack, err = Nat(ackFloat)
 	if err != nil {
-		return nil, err
+		return nil, errors.New("Ack is not a Nat")
 	}
 
 	msgs, ok := packet[0].([]interface{})
@@ -77,21 +76,21 @@ func UnmarshalMessagesJSON(jsonString string) (ret *Messages, err error) {
 	for i, rawMsg := range msgs {
 		arrMsg, ok := rawMsg.([]interface{})
 		if !ok || len(arrMsg) != 2 {
-			return nil, errors.New("Message is not a pair")
+			return nil, fmt.Errorf("Messages[%d] is not a pair", i)
 		}
 
 		numFloat, ok := arrMsg[0].(float64)
 		if !ok {
-			return nil, errors.New("Message Num is not an integer")
+			return nil, fmt.Errorf("Messages[%d] Num is not a number", i)
 		}
 		ret.Nums[i], err = Nat(numFloat)
 		if err != nil {
-			return nil, errors.New("Message num is not a Nat")
+			return nil, fmt.Errorf("Messages[%d] Num is not a Nat", i)
 		}
 
 		ret.Messages[i], ok = arrMsg[1].(string)
 		if !ok {
-			return nil, errors.New("Message is not a string")
+			return nil, fmt.Errorf("Messages[%d] body is not a string", i)
 		}
 	}
 
