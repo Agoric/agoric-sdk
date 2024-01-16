a1602da2a7f9b90ae2356ea792f14f484eb94a31
Mathieu Hofman <mathieu@agoric.com>
2024-01-16 21:41:55 +0000

    Revert "ci: simulate network core proposal in upgrade test"
    This reverts commit 65d8c5b86b5e4a076185dca5297145cd113b4f4c.

--- a3p-integration/proposals/a:upgrade-14/post.test.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-
-import { getIncarnation } from '@agoric/synthetic-chain/src/lib/vat-status.js';
-
-test(`Ensure Network Vat was installed`, async t => {
-  const incarnation = await getIncarnation('network');
-  t.is(incarnation, 0);
-});
