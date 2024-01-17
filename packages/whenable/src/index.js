c816bf62f00dd402fa80e5d88873f3413296de7e
Michael FIG <mfig@agoric.com>
2024-01-17 14:18:43 -0600

    chore(whenable): remove hard dependency on `@agoric/internal`

--- packages/whenable/src/index.js
+++ packages/whenable/src/index.js
@@ -1,8 +1,5 @@
 // @ts-check
-export * from './when.js';
-export * from './whenable.js';
 export * from './module.js';
-export * from './heap.js';
 
 // eslint-disable-next-line import/export
 export * from './types.js';
