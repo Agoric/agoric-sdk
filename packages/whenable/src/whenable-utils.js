0d6af3acedfe653f5badae4d3f189fcba8c4453b
Michael FIG <mfig@agoric.com>
2024-01-17 12:23:12 -0600

    fixup! refactor(whenable): putting on some polish for reviewers

--- packages/whenable/src/whenable-utils.js
+++ packages/whenable/src/whenable-utils.js
@@ -15,7 +15,7 @@ export const getWhenablePayload = specimen =>
   specimen.payload;
 
 /** A unique object identity just for internal use. */
-const PUMPKIN = harden({});
+const ALREADY_WHENABLE = harden({});
 
 /**
  * @template T
@@ -24,17 +24,19 @@ const PUMPKIN = harden({});
  * @param {(specimen: Awaited<T>, payload: import('./types').Whenable<any>['payload']) => U} cb
  * @returns {Promise<Awaited<U>>}
  */
-export const getFirstWhenable = (specimenP, cb) =>
-  Promise.resolve().then(async () => {
-    let payload = getWhenablePayload(specimenP);
+export const getFirstWhenable = async (specimenP, cb) => {
+  let payload = getWhenablePayload(specimenP);
 
-    // Take exactly 1 turn to find the first whenable, if any.
-    let specimen = await (payload ? PUMPKIN : specimenP);
-    if (specimen === PUMPKIN) {
-      specimen = specimenP;
-    } else {
-      payload = getWhenablePayload(specimen);
-    }
+  // Take exactly 1 turn to find the first whenable, if any.
+  let specimen = await (payload ? ALREADY_WHENABLE : specimenP);
+  if (specimen === ALREADY_WHENABLE) {
+    // The fact that we have a whenable payload means it's not actually a
+    // promise.
+    specimen = specimenP;
+  } else {
+    // Check if the awaited specimen is a whenable.
+    payload = getWhenablePayload(specimen);
+  }
 
-    return cb(specimen, payload);
-  });
+  return cb(specimen, payload);
+};
