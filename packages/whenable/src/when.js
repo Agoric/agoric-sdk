57bb247ca08ffde6f663a3468ddcd08f53cbaffe
Michael FIG <mfig@agoric.com>
2024-01-18 18:52:10 -0600

    fixup! fix(whenable): properly chain `watch`ed whenables

--- packages/whenable/src/when.js
+++ packages/whenable/src/when.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { E } from '@endo/far';
 
-import { getFirstWhenable, getWhenablePayload } from './whenable-utils.js';
+import { unwrapPromise, getWhenablePayload } from './whenable-utils.js';
 
 /**
  * @param {import('@agoric/base-zone').Zone} zone
@@ -19,7 +19,7 @@ export const prepareWhen = (
   const when = specimenP => {
     const { settler, promise } = makeWhenablePromiseKit();
     // Ensure we have a presence that won't be disconnected later.
-    getFirstWhenable(specimenP, async (specimen, payload) => {
+    unwrapPromise(specimenP, async (specimen, payload) => {
       // Shorten the whenable chain without a watcher.
       await null;
       while (payload) {
