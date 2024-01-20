b152a52ac54d81030fa74a17fc1c16883c548b5a
Michael FIG <mfig@agoric.com>
2024-01-20 16:14:15 -0600

    fix(whenable): better fidelity of shimmed `watchPromise`

--- packages/whenable/test/test-watch.js
+++ packages/whenable/test/test-watch.js
@@ -21,32 +21,35 @@ const prepareAckWatcher = (zone, t) => {
   });
 };
 
-test('ack watcher', async t => {
+const runTests = async t => {
   const zone = makeHeapZone();
   const { watch, when, makeWhenableKit } = prepareWhenableModule(zone);
   const makeAckWatcher = prepareAckWatcher(zone, t);
 
   const packet = harden({ portId: 'port-1', channelId: 'channel-1' });
 
-  const connSend = Promise.resolve('ack');
-  t.is(await when(watch(connSend, makeAckWatcher(packet))), 'fulfilled');
+  const connSendP = Promise.resolve('ack');
+  t.is(await when(watch(connSendP, makeAckWatcher(packet))), 'fulfilled');
 
-  const connError = Promise.reject(Error('disconnected'));
-  t.is(await when(watch(connError, makeAckWatcher(packet))), 'rejected');
+  const connErrorP = Promise.reject(Error('disconnected'));
+  t.is(await when(watch(connErrorP, makeAckWatcher(packet))), 'rejected');
 
   const { whenable, settler } = makeWhenableKit();
-  const connWhenable = Promise.resolve(whenable);
+  const connWhenableP = Promise.resolve(whenable);
   settler.resolve('ack');
-  t.is(await when(watch(connWhenable, makeAckWatcher(packet))), 'fulfilled');
+  t.is(await when(watch(connWhenableP, makeAckWatcher(packet))), 'fulfilled');
+  t.is(await when(watch(whenable, makeAckWatcher(packet))), 'fulfilled');
 
   const { whenable: whenable2, settler: settler2 } = makeWhenableKit();
-  const connWhenable2 = Promise.resolve(whenable2);
+  const connWhenable2P = Promise.resolve(whenable2);
   settler2.resolve(whenable);
-  t.is(await when(watch(connWhenable2, makeAckWatcher(packet))), 'fulfilled');
+  t.is(await when(watch(connWhenable2P, makeAckWatcher(packet))), 'fulfilled');
 
   const { whenable: whenable3, settler: settler3 } = makeWhenableKit();
-  const connWhenable3 = Promise.resolve(whenable3);
+  const connWhenable3P = Promise.resolve(whenable3);
   settler3.reject(Error('disco2'));
   settler3.resolve(whenable2);
-  t.is(await when(watch(connWhenable3, makeAckWatcher(packet))), 'rejected');
-});
+  t.is(await when(watch(connWhenable3P, makeAckWatcher(packet))), 'rejected');
+};
+
+test('ack watcher - shim', runTests);
