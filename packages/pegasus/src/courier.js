4418aac7ee82700c199709934d553df24e6f91bb
Ikenna Omekam <ikenna@agoric.com>
2024-01-24 17:57:40 -0600

    fix: more linting

--- packages/pegasus/src/courier.js
+++ packages/pegasus/src/courier.js
@@ -46,89 +46,89 @@ export const getCourierPK = (key, keyToCourierPK) => {
  */
 export const makeCourierMaker =
   connection =>
-    ({
-      zcf,
-      board,
-      namesByAddress,
-      sendDenom,
-      localBrand,
-      retain,
-      redeem,
-      transferProtocol,
-      when,
-    }) => {
-      /** @type {Sender} */
-      const send = async (zcfSeat, depositAddress, memo, opts) => {
-        const tryToSend = async () => {
-          const amount = zcfSeat.getAmountAllocated('Transfer', localBrand);
-          const transferPacket = await E(transferProtocol).makeTransferPacket({
-            value: amount.value,
-            remoteDenom: sendDenom,
-            depositAddress,
-            memo,
-            opts,
-          });
-
-          // Retain the payment.  We must not proceed on failure.
-          retain(zcfSeat, { Transfer: amount });
-
-          // The payment is already escrowed, and proposed to retain, so try sending.
-          return when(E(connection).send(transferPacket))
-            .then(ack => E(transferProtocol).assertTransferPacketAck(ack))
-            .then(
-              _ => zcfSeat.exit(),
-              reason => {
-                // Return the payment to the seat, if possible.
-                redeem(zcfSeat, { Transfer: amount });
-                throw reason;
-              },
-            );
-        };
-
-        // Reflect any error back to the seat.
-        return tryToSend().catch(reason => {
-          zcfSeat.fail(reason);
+  ({
+    zcf,
+    board,
+    namesByAddress,
+    sendDenom,
+    localBrand,
+    retain,
+    redeem,
+    transferProtocol,
+    when,
+  }) => {
+    /** @type {Sender} */
+    const send = async (zcfSeat, depositAddress, memo, opts) => {
+      const tryToSend = async () => {
+        const amount = zcfSeat.getAmountAllocated('Transfer', localBrand);
+        const transferPacket = await E(transferProtocol).makeTransferPacket({
+          value: amount.value,
+          remoteDenom: sendDenom,
+          depositAddress,
+          memo,
+          opts,
         });
-      };
 
-      /** @type {Receiver} */
-      const receive = async ({ value, depositAddress }) => {
-        const localAmount = AmountMath.make(localBrand, value);
+        // Retain the payment.  We must not proceed on failure.
+        retain(zcfSeat, { Transfer: amount });
 
-        // Look up the deposit facet for this board address, if there is one.
-        /** @type {DepositFacet} */
-        const depositFacet = await E(board)
-          .getValue(depositAddress)
-          .catch(_ =>
-            E(namesByAddress).lookup(depositAddress, WalletName.depositFacet),
+        // The payment is already escrowed, and proposed to retain, so try sending.
+        return when(E(connection).send(transferPacket))
+          .then(ack => E(transferProtocol).assertTransferPacketAck(ack))
+          .then(
+            _ => zcfSeat.exit(),
+            reason => {
+              // Return the payment to the seat, if possible.
+              redeem(zcfSeat, { Transfer: amount });
+              throw reason;
+            },
           );
-
-        const { userSeat, zcfSeat } = zcf.makeEmptySeatKit();
-
-        // Redeem the backing payment.
-        try {
-          redeem(zcfSeat, { Transfer: localAmount });
-          zcfSeat.exit();
-        } catch (e) {
-          zcfSeat.fail(e);
-          throw e;
-        }
-
-        // Once we've gotten to this point, their payment is committed and
-        // won't be refunded on a failed receive.
-        const payout = await E(userSeat).getPayout('Transfer');
-
-        // Send the payout promise to the deposit facet.
-        //
-        // We don't want to wait for the depositFacet to return, so that
-        // it can't hang up (i.e. DoS) an ordered channel, which relies on
-        // us returning promptly.
-        E(depositFacet)
-          .receive(payout)
-          .catch(_ => { });
-
-        return E(transferProtocol).makeTransferPacketAck(true);
       };
 
-      return Far('courier', { send, receive });
+      // Reflect any error back to the seat.
+      return tryToSend().catch(reason => {
+        zcfSeat.fail(reason);
+      });
     };
+
+    /** @type {Receiver} */
+    const receive = async ({ value, depositAddress }) => {
+      const localAmount = AmountMath.make(localBrand, value);
+
+      // Look up the deposit facet for this board address, if there is one.
+      /** @type {DepositFacet} */
+      const depositFacet = await E(board)
+        .getValue(depositAddress)
+        .catch(_ =>
+          E(namesByAddress).lookup(depositAddress, WalletName.depositFacet),
+        );
+
+      const { userSeat, zcfSeat } = zcf.makeEmptySeatKit();
+
+      // Redeem the backing payment.
+      try {
+        redeem(zcfSeat, { Transfer: localAmount });
+        zcfSeat.exit();
+      } catch (e) {
+        zcfSeat.fail(e);
+        throw e;
+      }
+
+      // Once we've gotten to this point, their payment is committed and
+      // won't be refunded on a failed receive.
+      const payout = await E(userSeat).getPayout('Transfer');
+
+      // Send the payout promise to the deposit facet.
+      //
+      // We don't want to wait for the depositFacet to return, so that
+      // it can't hang up (i.e. DoS) an ordered channel, which relies on
+      // us returning promptly.
+      E(depositFacet)
+        .receive(payout)
+        .catch(_ => {});
+
+      return E(transferProtocol).makeTransferPacketAck(true);
+    };
+
+    return Far('courier', { send, receive });
+  };
