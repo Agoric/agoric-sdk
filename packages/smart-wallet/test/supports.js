9f6437774a91566d057dd905d891635544c541d6
Ikenna Omekam <ikenna@agoric.com>
2024-01-25 11:24:47 -0600

    fix: linting

--- packages/smart-wallet/test/supports.js
+++ packages/smart-wallet/test/supports.js
@@ -63,7 +63,7 @@ const makeFakeBridgeManager = () =>
     register(bridgeId, handler) {
       return Far('scopedBridgeManager', {
         fromBridge(_obj) {
-          asssert.fail(`expected fromBridge`);
+          assert.fail(`expected fromBridge`);
         },
         toBridge(obj) {
           if (!handler) {
@@ -134,7 +134,7 @@ export const makeMockTestSpace = async log => {
     Promise.resolve(
       Far(
         'mockBankManager',
-        /** @type {any} */({
+        /** @type {any} */ ({
           getBankForAddress: _a => fakeBankKit.bank,
         }),
       ),
