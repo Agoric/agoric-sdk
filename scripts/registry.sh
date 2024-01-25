8ec4ed33fe9e7e0a8cca2d4eb0d0dc35cb64ab1d
Luqi Pan <luqi@agoric.com>
2024-01-25 14:56:04 -0800

    fix(integration-test): set --dapp-template at registry.sh level

--- scripts/registry.sh
+++ scripts/registry.sh
@@ -107,7 +107,7 @@ integrationTest() {
     persistVar AGORIC_INSTALL_OPTIONS "[\"$DISTTAG\"]"
   }
   persistVar AGORIC_START_OPTIONS '["--rebuild"]'
-  persistVar AGORIC_INIT_OPTIONS "[\"--dapp-branch=$2\"]"
+  persistVar AGORIC_INIT_OPTIONS "[\"--dapp-template=dapp-fungible-faucet\"]"
 
   (
     cd "$thisdir/../packages/agoric-cli"
