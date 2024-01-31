b4c8cedd89a9740f61c311ff8e53167720f18d4d
Mathieu Hofman <mathieu@agoric.com>
2024-01-31 02:50:10 +0000

    chore: roll upgrade-15 tests into 14

--- a3p-integration/proposals/b:upgrade-15/post.test.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-import { getIncarnation } from '@agoric/synthetic-chain/src/lib/vat-status.js';
-
-test(`Zoe vat was upgraded`, async t => {
-  const incarnation = await getIncarnation('zoe');
-
-  t.is(incarnation, 1);
-});
