1a7a08d016675c43f122061856f921357d3a5cc2
Michael FIG <mfig@agoric.com>
2024-02-01 17:44:32 -0600

    chore(vat-data): adopt `@agoric/internal/whenable.js`

--- packages/vat-data/test/test-whenable.js
+++ packages/vat-data/test/test-whenable.js
@@ -1,33 +1,33 @@
 // @ts-check
 import test from 'ava';
-import { E as basicE, Far } from '@endo/far';
+import { E, Far } from '@endo/far';
 
-import { E, makeWhenableKit } from '../whenable.js';
+import { V, makeWhenableKit } from '../whenable.js';
 
 test('heap messages', async t => {
   const greeter = Far('Greeter', {
     hello: /** @param {string} name */ name => `Hello, ${name}!`,
   });
 
-  /** @type {import('@agoric/whenable').WhenableKit<typeof greeter>} */
+  /** @type {ReturnType<typeof makeWhenableKit<typeof greeter>>} */
   const { whenable, settler } = makeWhenableKit();
-  const retP = E(whenable).hello('World');
+  const retP = V(whenable).hello('World');
   settler.resolve(greeter);
 
-  // Happy path: E(whenable)[method](...args) calls the method.
+  // Happy path: WE(whenable)[method](...args) calls the method.
   t.is(await retP, 'Hello, World!');
 
-  // Sad path: basicE(whenable)[method](...args) rejects.
+  // Sad path: E(whenable)[method](...args) rejects.
   await t.throwsAsync(
     // @ts-expect-error hello is not accessible via basicE
-    () => basicE(whenable).hello('World'),
+    () => E(whenable).hello('World'),
     {
       message: /target has no method "hello"/,
     },
   );
 
-  // Happy path: await E.when unwraps the whenable.
-  t.is(await E.when(whenable), greeter);
+  // Happy path: await WE.when unwraps the whenable.
+  t.is(await V.when(whenable), greeter);
 
   // Sad path: await by itself gives the raw whenable.
   const w = await whenable;
