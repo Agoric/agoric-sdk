aaccf00ba99d9dde864da7e9ad7f6527ec22c3b8
Dan Connolly <connolly@agoric.com>
2024-02-13 17:35:09 -0600

    test(boot): override bridgeHandlers in makeSwingsetTestKit

--- packages/boot/tools/supports.ts
+++ packages/boot/tools/supports.ts
@@ -265,6 +265,7 @@ export const makeSwingsetTestKit = async (
     profileVats = [] as string[],
     debugVats = [] as string[],
     defaultManagerType = 'local' as ManagerType,
+    bridgeHandlers = {} as Record<string, (obj: any) => unknown>,
   } = {},
 ) => {
   console.time('makeBaseSwingsetTestKit');
@@ -298,6 +299,9 @@ export const makeSwingsetTestKit = async (
     }
     outboundMessages.get(bridgeId).push(obj);
 
+    if (bridgeId in bridgeHandlers) {
+      return bridgeHandlers[bridgeId](obj);
+    }
     switch (bridgeId) {
       case BridgeId.BANK: {
         trace(
