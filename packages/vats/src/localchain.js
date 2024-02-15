19e5aed4e8a2aad667c04023e0aea01712ff9b9c
Michael FIG <mfig@agoric.com>
2024-02-15 15:13:45 -0600

    feat(vats): provide init-localchain

--- packages/vats/src/localchain.js
+++ packages/vats/src/localchain.js
@@ -3,9 +3,10 @@ import { M } from '@endo/patterns';
 
 export const LocalChainAccountI = M.interface('LocalChainAccount', {
   getAddress: M.callWhen().returns(M.string()),
-  executeTx: M.callWhen(M.arrayOf(M.any())).returns(M.arrayOf(M.any())),
-  // A sample of adding specific queries.
-  allBalances: M.callWhen().returns(M.any()),
+  executeTx: M.callWhen(M.arrayOf(M.record())).returns(M.arrayOf(M.record())),
+  // An example of a high-level query.  TODO: Move this functionality to a higher
+  // layer that implements the Orchestration API.
+  allBalances: M.callWhen().returns(M.record()),
 });
 
 /** @param {import('@agoric/base-zone').Zone} zone */
@@ -13,6 +14,11 @@ const prepareLocalChainAccount = zone =>
   zone.exoClass(
     'LocalChainAccount',
     LocalChainAccountI,
+    /**
+     * @param {import('./types.js').ScopedBridgeManager} system
+     * @param {string} address
+     * @param {{ query: (messages: any[]) => Promise<any> }} chain
+     */
     (system, address, chain) => ({ system, address, chain }),
     {
       // Information that the account creator needs.
@@ -34,6 +40,9 @@ const prepareLocalChainAccount = zone =>
         const { system, address } = this.state;
         const obj = {
           type: 'VLOCALCHAIN_EXECUTE_TX',
+          // This address is the only one that `VLOCALCHAIN_EXECUTE_TX` will
+          // accept as a signer for the transaction.  If the messages have other
+          // addresses in signer positions, the transaction will be aborted.
           address,
           messages,
         };
@@ -44,7 +53,7 @@ const prepareLocalChainAccount = zone =>
 
 export const LocalChainI = M.interface('LocalChain', {
   createAccount: M.callWhen().returns(M.remotable('LocalChainAccount')),
-  query: M.callWhen(M.arrayOf(M.any())).returns(M.any()),
+  query: M.callWhen(M.arrayOf(M.record())).returns(M.arrayOf(M.record())),
 });
 
 /**
@@ -52,24 +61,32 @@ export const LocalChainI = M.interface('LocalChain', {
  * @param {ReturnType<typeof prepareLocalChainAccount>} createAccount
  */
 const prepareLocalChain = (zone, createAccount) =>
-  zone.exoClass('LocalChain', LocalChainI, system => ({ system }), {
-    async createAccount() {
-      const { system } = this.state;
-      // Allocate a fresh address that doesn't correspond with a public key,
-      // and follows the ICA guidelines to help reduce collisions.
-      const address = await E(system).toBridge({
-        type: 'VLOCALCHAIN_ALLOCATE_ADDRESS',
-      });
-      return createAccount(system, address, this.self);
+  zone.exoClass(
+    'LocalChain',
+    LocalChainI,
+    /** @param {import('./types.js').ScopedBridgeManager} system */
+    system => ({ system }),
+    {
+      async createAccount() {
+        const { system } = this.state;
+        // Allocate a fresh address that doesn't correspond with a public key,
+        // and follows the ICA guidelines to help reduce collisions.  See
+        // x/vlocalchain/keeper/keeper.go AllocateAddress for the use of the app
+        // hash and block data hash.
+        const address = await E(system).toBridge({
+          type: 'VLOCALCHAIN_ALLOCATE_ADDRESS',
+        });
+        return createAccount(system, address, this.self);
+      },
+      async query(messages) {
+        const { system } = this.state;
+        return E(system).toBridge({
+          type: 'VLOCALCHAIN_QUERY',
+          messages,
+        });
+      },
     },
-    async query(messages) {
-      const { system } = this.state;
-      return E(system).toBridge({
-        type: 'VLOCALCHAIN_QUERY',
-        messages,
-      });
-    },
-  });
+  );
 
 /** @param {import('@agoric/base-zone').Zone} zone */
 export const prepareLocalChainTools = zone => {
