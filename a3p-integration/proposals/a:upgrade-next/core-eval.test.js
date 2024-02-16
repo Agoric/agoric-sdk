0cd9160db314ee2334d6c213bc2d1e6319dfdd60
Turadg Aleahmad <turadg@agoric.com>
2024-02-15 16:46:38 -0800

    test: reduce race sensitivity

--- a3p-integration/proposals/a:upgrade-next/core-eval.test.js
+++ a3p-integration/proposals/a:upgrade-next/core-eval.test.js
@@ -1,7 +1,8 @@
+/* eslint-disable @jessie.js/safe-await-separator */
 import test from 'ava';
 import { readFile, writeFile } from 'node:fs/promises';
 
-import { agoric, evalBundles, waitForBlock } from '@agoric/synthetic-chain';
+import { agd, evalBundles, waitForBlock } from '@agoric/synthetic-chain';
 
 const SUBMISSION_DIR = 'core-eval-test-submission';
 
@@ -17,9 +18,26 @@ const replaceTemplateValuesInFile = async (fileName, replacements) => {
   await writeFile(`${fileName}.js`, script);
 };
 
+const readPublished = async path => {
+  const { value } = await agd.query(
+    'vstorage',
+    'data',
+    '--output',
+    'json',
+    `published.${path}`,
+  );
+  if (value === '') {
+    return undefined;
+  }
+  const obj = JSON.parse(value);
+  return obj.values[0];
+};
+
 test(`core eval works`, async t => {
   const nodePath = 'foo.bar';
   const nodeValue = 'baz';
+
+  t.falsy(await readPublished(nodePath));
 
   await replaceTemplateValuesInFile(`${SUBMISSION_DIR}/send-script`, {
     NODE_PATH: nodePath,
@@ -29,12 +47,6 @@ test(`core eval works`, async t => {
   await evalBundles(SUBMISSION_DIR);
 
   await waitForBlock(2); // enough time for core eval to execute ?
-  const chainStorageValue = await agoric.follow(
-    '-lF',
-    `:${nodePath}`,
-    '-o',
-    'text',
-  );
 
-  t.is(chainStorageValue, nodeValue);
+  t.is(await readPublished(nodePath), nodeValue);
 });
