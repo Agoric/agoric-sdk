27079f984779a6eb48ca43a6f1a1c4242b66ab2c
Mathieu Hofman <86499+mhofman@users.noreply.github.com>
2024-02-19 13:01:37 -0800

    refactor(a3p): reusable submission generation function (#8944)

--- scripts/generate-a3p-submission.sh
+++ scripts/generate-a3p-submission.sh
@@ -4,9 +4,16 @@ set -ueo pipefail
 sdkroot=$(cd -- "$(dirname "$0")/.." >/dev/null && pwd)
 
 cd $sdkroot
-yarn agoric run packages/builders/scripts/vats/restart-vats.js
 
-mkdir -p $sdkroot/a3p-integration/proposals/b:restart-vats/submission
-cp $(grep -oh '/.*b1-.*.json' $sdkroot/restart-vats*) a3p-integration/proposals/b:restart-vats/submission
-mv $sdkroot/restart-vats* a3p-integration/proposals/b:restart-vats/submission
+buildSubmission() {
+  proposalName=$1
+  a3pProposal=$2
 
+  yarn agoric run packages/builders/scripts/vats/$1.js
+
+  mkdir -p $sdkroot/a3p-integration/proposals/$2/submission
+  cp $(grep -oh '/.*b1-.*.json' $sdkroot/$1*) a3p-integration/proposals/$2/submission
+  mv $sdkroot/$1* a3p-integration/proposals/$2/submission
+}
+
+buildSubmission restart-vats "b:restart-vats"
