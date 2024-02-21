85e997da1a59963a6f56789d241f3db36fe45605
Mark S. Miller <erights@gmail.com>
2024-02-20 16:00:48 -0800

    fixup! format

--- packages/ERTP/src/index.js
+++ packages/ERTP/src/index.js
@@ -5,8 +5,8 @@ export * from './issuerKit.js';
 export * from './typeGuards.js';
 
 /**
- * Importing Baggage from `@agoric/ertp` is deprecated.
- * Import Baggage from `@agoric/vat-data` instead
+ * Importing Baggage from `@agoric/ertp` is deprecated. Import Baggage from
+ * `@agoric/vat-data` instead
  *
  * @typedef {import('@agoric/vat-data').Baggage} Baggage
  */
