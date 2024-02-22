3146886cbad75e773b0dd0520d0d88ef0f12540f
Turadg Aleahmad <turadg@agoric.com>
2024-02-22 13:29:40 -0800

    fix: handle promises in repairWalletForIncarnation2

--- packages/smart-wallet/src/smartWallet.js
+++ packages/smart-wallet/src/smartWallet.js
@@ -1102,9 +1102,9 @@ export const prepareSmartWallet = (baggage, shared) => {
             },
           });
         },
+        // TODO remove this and repairUnwatchedSeats once the repair has taken place.
         /**
-         * one-time use function. Remove this and repairUnwatchedSeats once the
-         * repair has taken place.
+         * To be called once ever per wallet.
          *
          * @param {object} key
          */
@@ -1115,8 +1115,12 @@ export const prepareSmartWallet = (baggage, shared) => {
             return;
           }
 
-          void facets.helper.repairUnwatchedSeats();
-          void facets.helper.repairUnwatchedPurses();
+          facets.helper.repairUnwatchedSeats().catch(e => {
+            console.error('repairUnwatchedSeats rejection', e);
+          });
+          facets.helper.repairUnwatchedPurses().catch(e => {
+            console.error('repairUnwatchedPurses rejection', e);
+          });
           trace(`repaired wallet ${state.address}`);
         },
       },
