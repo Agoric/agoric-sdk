d03eaa27d433585380ec1e201721fb15111e8196
Chris Hibbert <hibbert@agoric.com>
2024-02-29 13:06:05 -0800

    fixup! refactor: generate without invoking scripts from proposals

--- scripts/generate-a3p-submission.sh
+++ scripts/generate-a3p-submission.sh
@@ -6,14 +6,13 @@ sdkroot=$(cd -- "$(dirname "$0")/.." >/dev/null && pwd)
 cd "$sdkroot"
 
 proposalName=$1
-a3pProposal=$2
-output=${3:-$proposalName}
-submissionName=${4:-submission}
+outfileBase=${2:-$proposalName}
+submissionDirName=${3:-submission}
 
 yarn agoric run "packages/builders/scripts/vats/$proposalName.js"
 
-submissionDir="a3p-integration/proposals/$a3pProposal/$submissionName"
+submissionDir="./$submissionDirName"
 mkdir -p "$submissionDir"
-cp $(grep -oh '/.*b1-.*.json' "$output"*) "$submissionDir"
+cp $(grep -oh '/.*b1-.*.json' "$outfileBase"*) "$submissionDir"
 
-mv "$output"* "$submissionDir"
+mv "$outfileBase"* "$submissionDir"
