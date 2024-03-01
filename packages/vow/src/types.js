234d425b6363977d0f1cfc029c017af9d2192e12
Michael FIG <mfig@agoric.com>
2024-03-01 17:40:43 -0600

    fix(vow): clearer `Unwrap<T>` and `Remote<T>`

--- packages/vow/src/types.js
+++ packages/vow/src/types.js
@@ -1,23 +1,64 @@
 // @ts-check
 export {};
 
+/** @typedef {(...args: any[]) => any} Callable */
+
 /**
  * @template T
- * @typedef {PromiseLike<T | Vow<T>>} PromiseVow
+ * @typedef {Promise<T | Vow<T>>} PromiseVow Return type of a function that may
+ * return a promise or a vow.
  */
 
 /**
  * @template T
- * @typedef {import('./E').ERef<T>} ERef
+ * @typedef {T | PromiseLike<T>} ERef
  */
 
 /**
- * Creates a type that accepts both near and marshalled references that were
- * returned from `Remotable` or `Far`, and also promises for such references.
- *
+ * @template T
+ * @typedef {(
+ *   T extends bigint ? true :
+ *   T extends boolean ? true :
+ *   T extends null ? true :
+ *   T extends number ? true :
+ *   T extends string ? true :
+ *   T extends symbol ? true :
+ *   T extends undefined ? true :
+ *   false
+ * )} IsPrimitive Whether T is a primitive type.
+ */
+
+/**
+ * @template T
+ * @typedef {(
+ *   IsPrimitive<T> extends true ? T :
+ *   T extends Callable ? never :
+ *   { [P in keyof T as T[P] extends Callable ? never : P]: DataOnly<T[P]> }
+ * )} DataOnly Recursively extract the non-callable properties of T.
+ */
+
+/**
+ * Follow the chain of vow shortening to the end, returning the final value.
+ * This is used within E, so we must narrow the type to its remote form.
+ * @template T
+ * @typedef {(
+ *   T extends PromiseLike<infer U> ? Unwrap<U> :
+ *   T extends import('./types').Vow<infer U> ? Unwrap<U> :
+ *   IsPrimitive<T> extends true ? T :
+ *   T extends import('@endo/eventual-send').RemotableBrand<infer Local, infer Primary> ? Local & T :
+ *   T
+ * )} Unwrap
+ */
+
+/**
+ * A type that accepts both near and marshalled references that were
+ * returned from `Remotable` or `Far`.
  * @template Primary The type of the primary reference.
- * @template [Local=import('./E').DataOnly<Primary>] The local properties of the object.
- * @typedef {ERef<Local & import('@endo/eventual-send').RemotableBrand<Local, Primary>>} FarRef
+ * @template [Local=DataOnly<Primary>] The local properties of the object.
+ * @typedef {Primary |
+ *   import('@endo/eventual-send').RemotableBrand<Local, Primary>
+ * } Remote A type that doesn't assume its parameter is local, but is
+ * satisfied with both local and remote references.
  */
 
 /**
@@ -36,7 +77,7 @@ export {};
 /**
  * @template [T=any]
  * @typedef {object} VowPayload
- * @property {import('@endo/eventual-send').FarRef<VowV0<T>>} vowV0
+ * @property {Remote<VowV0<T>>} vowV0
  */
 
 /**
