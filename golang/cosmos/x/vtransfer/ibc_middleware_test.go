60d063f16a063ba1b82f3f1c55213ae1914e287c
Joe S <schnetzlerjoe@gmail.com>
2024-03-11 15:21:07 -0400

    Transfer test work

--- golang/cosmos/x/vtransfer/ibc_middleware_test.go
+++ golang/cosmos/x/vtransfer/ibc_middleware_test.go
@@ -3,6 +3,7 @@ package vtransfer_test
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	app "github.com/Agoric/agoric-sdk/golang/cosmos/app"
@@ -16,6 +17,7 @@ import (
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	ibctransfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
+	channeltypes "github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
 	ibctesting "github.com/cosmos/ibc-go/v6/testing"
 	"github.com/cosmos/ibc-go/v6/testing/simapp"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
@@ -43,6 +45,7 @@ func SetupAgoricTestingApp() (ibctesting.TestingApp, map[string]json.RawMessage)
 	db := dbm.NewMemDB()
 	encCdc := app.MakeEncodingConfig()
 	controller := func(ctx context.Context, needReply bool, str string) (string, error) {
+		fmt.Printf("controller got: %s\n", str)
 		// fmt.Fprintln(os.Stderr, "FIXME: Would upcall to controller with", str)
 		// FIXME: Unmarshal JSON and reply to the upcall.
 		jsonReply := `true`
@@ -139,11 +142,64 @@ func (s *IntegrationTestSuite) NewTransferPath() *ibctesting.Path {
 	return path
 }
 
+func (s *IntegrationTestSuite) SetupContract() *ibctesting.Path {
+	path := ibctesting.NewPath(s.chainA, s.chainB)
+	path.EndpointA.ChannelID = "channel-0"
+	path.EndpointB.ChannelID = "channel-0"
+	path.EndpointA.ChannelConfig.PortID = ibctesting.TransferPort
+	path.EndpointB.ChannelConfig.PortID = ibctesting.TransferPort
+	path.EndpointA.ChannelConfig.Version = "ics20-1"
+	path.EndpointB.ChannelConfig.Version = "ics20-1"
+
+	s.coordinator.Setup(path)
+
+	s.coordinator.CommitBlock(s.chainA, s.chainB)
+
+	return path
+}
+
 func (s *IntegrationTestSuite) TestOnAcknowledgementPacket() {
 	path := s.NewTransferPath()
 	s.Require().Equal(path.EndpointA.ChannelID, "channel-0")
 
 	s.Run("OnReceiveTransferWithCallMemo", func() {
+		// create a transfer packet
+		transfer := ibctransfertypes.NewFungibleTokenPacketData(
+			"uosmo",
+			"1000000",
+			s.chainA.SenderAccount.GetAddress().String(),
+			s.chainB.SenderAccount.GetAddress().String(),
+			"memo",
+		)
 
+		// send a transfer packet
+		timeoutHeight := s.chainA.GetTimeoutHeight()
+		sequence, err := path.EndpointA.SendPacket(timeoutHeight, 0, transfer.GetBytes())
+		s.Require().NoError(err)
+
+		// commit the send on chainA
+		s.coordinator.CommitBlock(s.chainA)
+
+		// Update Clients
+		path.EndpointA.UpdateClient()
+		path.EndpointB.UpdateClient()
+
+		// receive the transfer packet
+		packet := channeltypes.NewPacket(transfer.GetBytes(), sequence, path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, path.EndpointB.ChannelConfig.PortID, path.EndpointB.ChannelID, timeoutHeight, 0)
+		err = path.EndpointB.RecvPacket(packet)
+		s.Require().NoError(err)
+		// commit the receive on chainB
+		s.coordinator.CommitBlock(s.chainB)
+
+		// Update Clients
+		path.EndpointA.UpdateClient()
+		path.EndpointB.UpdateClient()
+
+		// acknowledge the transfer packet
+		ack := s.chainB.GetAcknowledgement(packet)
+		err = path.EndpointB.AcknowledgePacket(packet, ack)
+		s.Require().NoError(err)
+		// commit the receive on chainB
+		s.coordinator.CommitBlock(s.chainB)
 	})
 }
