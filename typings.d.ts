4f2eebfc3ea1943aefc81b28b9109da8de426f42
Turadg Aleahmad <turadg@agoric.com>
2024-03-21 14:05:05 -0700

    chore(deps): remove obsolete endo/nat patch

--- typings.d.ts
+++
@@ -1,6 +0,0 @@
-// XXX retrofits for type deficiencies in external packages
-
-declare module '@endo/nat' {
-  export function Nat(specimen: unknown): bigint;
-  export function isNat(specimen: unknown): boolean;
-}
