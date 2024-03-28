99b599e7cccdd6b35b3aed1f35f203811f6a808f
Turadg Aleahmad <turadg@agoric.com>
2024-03-27 17:04:16 -0700

    remove unnecessary proposals

--- a3p-integration/proposals/a:upgrade-next/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Place here any test that should be executed using the executed proposal.
-# The effects of this step are not persisted in further proposal layers.
-
-yarn ava post.test.js
-GLOBIGNORE=post.test.js
-yarn ava *.test.js
