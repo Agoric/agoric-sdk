1b0216652cc5e6fb35d8b71fad1795a367cb536a
Kris Kowal <kris@agoric.com>
2024-03-27 17:46:19 -0700

    debug

--- packages/xsnap/src/build.js
+++ packages/xsnap/src/build.js
@@ -316,6 +316,7 @@ async function main(args, { env, stdout, spawn, fs, os }) {
       `xsnap-native/xsnap/build/bin/${platform}/release/xsnap-worker`,
     )
   ) {
+    (await import('child_process')).spawnSync('find', ['.']);
     throw new Error(
       'XSnap has neither sources nor a pre-built binary. Docker? .dockerignore? npm files?',
     );
