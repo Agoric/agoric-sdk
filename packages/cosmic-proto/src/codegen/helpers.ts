7fa7e37629d668c681b8f3e5d05854c776e48740
Turadg Aleahmad <turadg@agoric.com>
2024-04-04 11:34:22 -0700

    feat: base64 without ambient authority

--- packages/cosmic-proto/src/codegen/helpers.ts
+++ packages/cosmic-proto/src/codegen/helpers.ts
@@ -5,41 +5,11 @@
  * and run the transpile command or yarn proto command to regenerate this bundle.
  */
 
-declare var self: any | undefined;
-declare var window: any | undefined;
-declare var global: any | undefined;
-var globalThis: any = (() => {
-  if (typeof globalThis !== 'undefined') return globalThis;
-  if (typeof self !== 'undefined') return self;
-  if (typeof window !== 'undefined') return window;
-  if (typeof global !== 'undefined') return global;
-  throw 'Unable to locate global object';
-})();
+import { encodeBase64, decodeBase64 } from '@endo/base64';
 
-const atob: (b64: string) => string =
-  globalThis.atob ||
-  (b64 => globalThis.Buffer.from(b64, 'base64').toString('binary'));
-
-export function bytesFromBase64(b64: string): Uint8Array {
-  const bin = atob(b64);
-  const arr = new Uint8Array(bin.length);
-  for (let i = 0; i < bin.length; ++i) {
-    arr[i] = bin.charCodeAt(i);
-  }
-  return arr;
-}
-
-const btoa: (bin: string) => string =
-  globalThis.btoa ||
-  (bin => globalThis.Buffer.from(bin, 'binary').toString('base64'));
-
-export function base64FromBytes(arr: Uint8Array): string {
-  const bin: string[] = [];
-  arr.forEach(byte => {
-    bin.push(String.fromCharCode(byte));
-  });
-  return btoa(bin.join(''));
-}
+// use Endo for broader compatibility
+export const bytesFromBase64: (input: string) => Uint8Array = decodeBase64;
+export const base64FromBytes: (input: Uint8Array) => string = encodeBase64;
 
 export interface AminoHeight {
   readonly revision_number?: string;
