39b73d56944685a8a77b8f6e8e8d7dd0e459164e
0xPatrick <patrick@0xpatrick.dev>
2024-04-04 10:06:14 -0400

    feat(orchestration): add makeICAConnectionAddress and parseAddress utils

--- packages/orchestration/index.js
+++ packages/orchestration/index.js
@@ -1,2 +1,3 @@
 // eslint-disable-next-line import/export
 export * from './src/types.js';
+export * from './src/utils/address.js';
---
+++ packages/orchestration/src/types.d.ts
@@ -0,0 +1,1 @@
+export type ConnectionId = `connection-${number}`;
