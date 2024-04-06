0ce6d5d8ac73c2f76c595d19f3bbb86bfa93315c
Turadg Aleahmad <turadg@agoric.com>
2024-04-06 15:43:05 -0700

    test: remove upgrade-14 content from upgrade-next

--- golang/cosmos/app/app.go
+++ golang/cosmos/app/app.go
@@ -895,11 +895,8 @@ func unreleasedUpgradeHandler(app *GaiaApp, targetUpgrade string) func(sdk.Conte
 			// Each CoreProposalStep runs sequentially, and can be constructed from
 			// one or more modules executing in parallel within the step.
 			CoreProposalSteps = []vm.CoreProposalStep{
-				// First, upgrade wallet factory
-				vm.CoreProposalStepForModules("@agoric/builders/scripts/smart-wallet/build-wallet-factory2-upgrade.js"),
-				// Then, upgrade Zoe and ZCF
-				vm.CoreProposalStepForModules("@agoric/builders/scripts/vats/replace-zoe.js"),
-				// Then, upgrade the provisioning vat
+				/* upgrade-15 evals */
+				// upgrade the provisioning vat
 				vm.CoreProposalStepForModules("@agoric/builders/scripts/vats/replace-provisioning.js"),
 				// Enable low-level Orchestration.
 				vm.CoreProposalStepForModules(
