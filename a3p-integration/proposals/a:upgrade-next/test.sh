c3853dc55f397e914e498ba49e1265d64b235406
Turadg Aleahmad <turadg@agoric.com>
2024-04-06 15:43:24 -0700

    test: vat details after upgrade

--- a3p-integration/proposals/a:upgrade-next/test.sh
+++ a3p-integration/proposals/a:upgrade-next/test.sh
@@ -3,6 +3,9 @@
 # Place here any test that should be executed using the executed proposal.
 # The effects of this step are not persisted in further proposal layers.
 
-yarn ava post.test.js
-GLOBIGNORE=post.test.js
-yarn ava *.test.js
+# test the state right after upgrade
+yarn ava initial.test.js
+
+# test more, in ways that changes system state
+GLOBIGNORE=initial.test.js
+yarn ava ./*.test.js
