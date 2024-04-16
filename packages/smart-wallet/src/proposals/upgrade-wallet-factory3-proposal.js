1c1cd4c4b3b4babdcfc48f291ec72f59b3361a1a
Dan Connolly <connolly@agoric.com>
2024-04-16 17:41:01 -0500

    chore(smart-wallet): update ZCF too (WIP)
    ZCF has an await in the 1st crank that prevents upgrading walletFactory
    
    WIP: includes debug logging

--- packages/smart-wallet/src/proposals/upgrade-wallet-factory3-proposal.js
+++ packages/smart-wallet/src/proposals/upgrade-wallet-factory3-proposal.js
@@ -8,15 +8,18 @@ const toRecover = [
   // },
 ];
 
+const exitOffers = async () => {};
+
 /**
  * @param {BootstrapPowers & NonNullChainStorage} powers
  * @typedef {PromiseSpaceOf<{ chainStorage: StorageNode }>} NonNullChainStorage
  * @param {object} options
- * @param {{ walletRef: VatSourceRef }} options.options
+ * @param {{ walletRef: VatSourceRef, zcfRef: VatSourceRef }} options.options
  */
 export const upgradeWalletFactory = async (
   {
     consume: {
+      vatStore,
       walletFactoryStartResult,
       provisionPoolStartResult,
       chainStorage,
@@ -27,47 +30,64 @@ export const upgradeWalletFactory = async (
   },
   options,
 ) => {
+  console.log('@@@ upgradeWalletFactory');
   const WALLET_STORAGE_PATH_SEGMENT = 'wallet';
 
-  const { walletRef } = options.options;
+  const { walletRef, zcfRef } = options.options;
 
-  const [walletBridgeManager, walletStorageNode, ppFacets] = await Promise.all([
-    walletBridgeManagerP,
-    E(chainStorage).makeChildNode(WALLET_STORAGE_PATH_SEGMENT),
-    provisionPoolStartResult,
-  ]);
-  const walletReviver = await E(ppFacets.creatorFacet).getWalletReviver();
+  const upgradeZcf = async () => {
+    const { root: zoeRoot } = await E(vatStore).get('zoe');
 
-  const privateArgs = {
-    storageNode: walletStorageNode,
-    walletBridgeManager,
-    walletReviver,
+    const zoeConfigFacet = await E(zoeRoot).getZoeConfigFacet();
+    await E(zoeConfigFacet).updateZcfBundleId(zcfRef.bundleID);
+    console.log(`ZCF BUNDLE ID: `, zcfRef.bundleID);
   };
+  await upgradeZcf();
 
-  const { adminFacet } = await walletFactoryStartResult;
+  const upgradeWalletFactory = async () => {
+    const [walletBridgeManager, walletStorageNode, ppFacets] =
+      await Promise.all([
+        walletBridgeManagerP,
+        E(chainStorage).makeChildNode(WALLET_STORAGE_PATH_SEGMENT),
+        provisionPoolStartResult,
+      ]);
+    const walletReviver = await E(ppFacets.creatorFacet).getWalletReviver();
 
-  assert(walletRef.bundleID);
-  await E(adminFacet).upgradeContract(walletRef.bundleID, privateArgs);
+    const privateArgs = {
+      storageNode: walletStorageNode,
+      walletBridgeManager,
+      walletReviver,
+    };
 
-  console.log(`Successfully upgraded WalletFactory`);
+    const { adminFacet } = await walletFactoryStartResult;
+
+    assert(walletRef.bundleID);
+    console.log('@@@ upgradeWalletFactory', walletRef.bundleID);
+    await E(adminFacet).upgradeContract(walletRef.bundleID, privateArgs);
+
+    console.log(`Successfully upgraded WalletFactory`);
+  };
+  await upgradeWalletFactory();
 
   const { creatorFacet } = await walletFactoryStartResult;
 
-  await Promise.allSettled(
-    toRecover.map(async ({ address, offerId }) => {
-      const bank = await E(bankManager).getBankForAddress(address);
-      const [wallet, _x] = await E(creatorFacet).provideSmartWallet(
-        address,
-        bank,
-        namesByAddressAdmin,
-      );
-      await E(wallet)
-        .tryExitOffer(offerId)
-        .catch(e => {
-          console.log(`Failed to exit offer ${offerId} for ${address}: ${e}`);
-        });
-    }),
-  );
+  const exitOffers = () =>
+    Promise.allSettled(
+      toRecover.map(async ({ address, offerId }) => {
+        const bank = await E(bankManager).getBankForAddress(address);
+        const [wallet, _x] = await E(creatorFacet).provideSmartWallet(
+          address,
+          bank,
+          namesByAddressAdmin,
+        );
+        await E(wallet)
+          .tryExitOffer(offerId)
+          .catch(e => {
+            console.log(`Failed to exit offer ${offerId} for ${address}: ${e}`);
+          });
+      }),
+    );
+  await exitOffers();
 };
 harden(upgradeWalletFactory);
 
@@ -76,6 +96,8 @@ export const main = upgradeWalletFactory;
 
 export const permit = {
   consume: {
+    vatAdminSvc: true,
+    vatStore: true,
     walletFactoryStartResult: 'walletFactoryStartResult',
     provisionPoolStartResult: 'provisionPoolStartResult',
     chainStorage: 'chainStorage',
@@ -84,11 +106,12 @@ export const permit = {
     namesByAddressAdmin: 'provisioning',
   },
 };
+
 export const manifest = {
   [upgradeWalletFactory.name]: permit,
 };
 
-export const getManifestForUpgradeWallet = (_powers, { walletRef }) => ({
+export const getManifestForUpgradeWallet = (_powers, options) => ({
   manifest,
-  options: { walletRef },
+  options,
 });
