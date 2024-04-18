4cde92de632c11590f97c6ea0b7df2083fbe98a8
Turadg Aleahmad <turadg@agoric.com>
2024-04-18 12:59:04 -0700

    test: move core-eval to new acceptance suite

---
+++ a3p-integration/proposals/z:acceptance/README.md
@@ -0,0 +1,6 @@
+# Inert proposal to test acceptance of the history of upgrades
+
+Its `type` is `/cosmos.params.v1beta1.ParameterChangeProposal` because that is
+the lightest form of actual proposal to execute. It runs the `eval.sh` script,
+which does nothing.
+
---
+++ a3p-integration/proposals/z:acceptance/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Place here any test that should be executed using the executed proposal.
+# The effects of this step are not persisted in further proposal layers.
+
+# test the state right after the previous proposals
+yarn ava initial.test.js
+
+# test more, in ways that change system state
+GLOBIGNORE=initial.test.js
+yarn ava ./*.test.js
