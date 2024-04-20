06cca8c00c3dbd750437104c46136bcc2dd0cace
Richard Gibson <richard.gibson@gmail.com>
2024-04-20 12:57:51 -0400

    style(scripts): Align gen-upgrade-proposal.sh with community forum release posts
    Fixes #9270
    
    cf. https://community.agoric.com/t/proposal-71-agoric-upgrade-14-interchain-stack-smart-wallet-upgrades/633#creating-an-emerynet-upgrade-proposal-for-rc1-3

--- scripts/gen-upgrade-proposal.sh
+++ scripts/gen-upgrade-proposal.sh
@@ -1,43 +1,45 @@
 #! /bin/bash
 set -ueo pipefail
 
-cat << EOF 1>&2
-------------------------------------------------------------
+cat << 'EOF' 1>&2
+------------------------------------------------------------------------
 This script shows a command for a software upgrade proposal which is
 compatible with the Cosmovisor found at:
 https://github.com/agoric-labs/cosmos-sdk/tree/Agoric/cosmovisor#readme
-------------------------------------------------------------
+------------------------------------------------------------------------
 EOF
 
 COMMIT_ID=$(git rev-parse HEAD)
-ZIPURL=https://github.com/Agoric/agoric-sdk/archive/$COMMIT_ID.zip
+ZIP_URL="https://github.com/Agoric/agoric-sdk/archive/${COMMIT_ID}.zip"
 
-echo "Verifying archive is at $ZIPURL..." 1>&2
+echo "Verifying archive is at $ZIP_URL..." 1>&2
 zipfile=$(mktemp)
 trap 'rm -f "$zipfile"' EXIT
-curl -L "$ZIPURL" -o "$zipfile"
+curl -L "$ZIP_URL" -o "$zipfile"
 
 echo "Generating SHA-256 checksum..." 1>&2
-checksum=sha256:$(shasum -a 256 "$zipfile" | cut -d' ' -f1)
+CHECKSUM=sha256:$(shasum -a 256 "$zipfile" | cut -d' ' -f1)
 
-info="{\"binaries\":{\"any\":\"$ZIPURL//agoric-sdk-$COMMIT_ID?checksum=$checksum\"},\"source\":\"$ZIPURL?checksum=$checksum\"}"
+UPGRADE_INFO="{\"binaries\":{\"any\":\"$ZIP_URL//agoric-sdk-${COMMIT_ID}?checksum=$CHECKSUM\"},\"source\":\"$ZIP_URL?checksum=$CHECKSUM\"}"
 
-cat << EOF 1>&2
-------------------------------------------------------------
+cat << 'EOF' 1>&2
+------------------------------------------------------------------------
 Here is the skeleton of the recomended upgrade proposal command.
-You'll need to fill in the details.
+You'll need to fill in the details and add arguments such as
+`--chain-id=<chain id>` and `--from=<wallet>`.
 
-Try \`agd tx submit-proposal software-upgrade --help\` for more info.
+Try `agd tx submit-proposal software-upgrade --help` for more info.
 
 Also, take a look at existing on-chain software upgrade proposals for
-examples.
-------------------------------------------------------------
+examples: https://ping.pub/agoric/gov
+------------------------------------------------------------------------
 EOF
 
 cat << EOF
 agd tx submit-proposal software-upgrade <UPGRADE-NAME> \\
-  --upgrade-info='$info' \\
+  --upgrade-info='$UPGRADE_INFO' \\
   --upgrade-height <HEIGHT> \\
-  --title '<TITLE>' --description '<DESCRIPTION>' \\
+  --title '<TITLE>' \\
+  --description '<DESCRIPTION>' \\
   <TXOPTS>...
 EOF
