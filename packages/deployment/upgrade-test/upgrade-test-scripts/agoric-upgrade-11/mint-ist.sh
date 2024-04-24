f2d2378d3d74c6706f07414907c6294f6d382f5f
Chris Hibbert <hibbert@agoric.com>
2024-04-24 13:31:40 -0700

    test: drop obsolete deployment  upgrade-test

--- packages/deployment/upgrade-test/upgrade-test-scripts/agoric-upgrade-11/mint-ist.sh
+++
@@ -1,6 +0,0 @@
-agd tx bank send validator $GOV1ADDR 20123000000ibc/BA313C4A19DFBF943586C0387E6B11286F9E416B4DD27574E6909CABE0E342FA --keyring-backend=test --chain-id=agoriclocal --yes -bblock
-agops vaults open --giveCollateral 5000 --wantMinted 20000 > /tmp/offer.json
-agops perf satisfaction --executeOffer /tmp/offer.json --from gov1 --keyring-backend=test
-
-
-# ibc/BA313C4A19DFBF943586C0387E6B11286F9E416B4DD27574E6909CABE0E342FA
