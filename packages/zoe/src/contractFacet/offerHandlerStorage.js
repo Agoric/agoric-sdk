5ac71a17fdf06310586d24546f9253ec0a081828
Turadg Aleahmad <turadg@agoric.com>
2024-04-24 13:32:26 -0700

    chore(types): Zoe accomodations

--- packages/zoe/src/contractFacet/offerHandlerStorage.js
+++ packages/zoe/src/contractFacet/offerHandlerStorage.js
@@ -6,9 +6,13 @@ import { canBeDurable, provideDurableWeakMapStore } from '@agoric/vat-data';
 
 import { defineDurableHandle } from '../makeHandle.js';
 
+/**
+ * @typedef {RemotableBrand & RemotableObject & OfferHandler} PassableOfferHandler
+ */
+
 export const makeOfferHandlerStorage = zcfBaggage => {
   const makeInvitationHandle = defineDurableHandle(zcfBaggage, 'Invitation');
-  /** @type {WeakMapStore<InvitationHandle, OfferHandler>} */
+  /** @type {WeakMapStore<InvitationHandle, PassableOfferHandler>} */
 
   // ZCF needs to ephemerally hold on to ephemeral handlers, and durably hold
   // onto handlers that are intended to be durable. We keep two stores and store
@@ -18,13 +22,12 @@ export const makeOfferHandlerStorage = zcfBaggage => {
   const invitationHandleToEphemeralHandler = makeScalarWeakMapStore(
     'invitationHandleToEphemeralHandler',
   );
-  /** @type {WeakMapStore<InvitationHandle, OfferHandler>} */
+  /** @type {WeakMapStore<InvitationHandle, PassableOfferHandler>} */
   const invitationHandleToDurableHandler = provideDurableWeakMapStore(
     zcfBaggage,
     'invitationHandleToDurableHandler',
   );
 
-  /** @type {(offerHandler: OfferHandler) => InvitationHandle} */
   const storeOfferHandler = offerHandler => {
     if (typeof offerHandler === 'function') {
       offerHandler = ToFarFunction('offerHandler', offerHandler);
