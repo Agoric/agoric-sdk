86da7b72852becba4be4263489ccb5e2d0b5650e
Chris Hibbert <hibbert@agoric.com>
2024-04-25 09:51:19 -0700

    chore: review suggestions

--- a3p-integration/proposals/a:upgrade-next/priceFeed-follower-auction.test.js
+++ a3p-integration/proposals/a:upgrade-next/priceFeed-follower-auction.test.js
@@ -3,6 +3,6 @@ import { getDetailsMatchingVats } from './vatDetails.js';
 
 test('new auction vat', async t => {
   const details = await getDetailsMatchingVats('auctioneer');
-  // This query matches both the auction and its governor
-  t.true(Object.keys(details).length > 2);
+  // This query matches both the auction and its governor, so 2*2
+  t.is(Object.keys(details).length, 4);
 });
