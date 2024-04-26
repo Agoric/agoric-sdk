ddb7703eef9622b81b76dc02abdb5a6e4e949e03
Turadg Aleahmad <turadg@agoric.com>
2024-04-26 11:45:50 -0700

    build: update replace-packages for npm build

--- scripts/replace-packages.sh
+++ scripts/replace-packages.sh
@@ -14,6 +14,14 @@ DSTDIR=${2-$PWD/node_modules}
 pushd "$SRCDIR"
 yarn install
 yarn build
+
+# use lerna to ensure these run in topological order, whereas npm query
+# sorts in order of the array and lexically for the glob
+# https://github.com/npm/cli/issues/4139#issuecomment-1730186418
+yarn lerna run prepack
+# Convention used in Endo
+yarn lerna run build:types
+
 npm query .workspace | jq -r '.[].location' | while read -r dir; do
   # Skip private packages.
   test "$(jq .private < "$dir/package.json")" != true || continue
@@ -21,10 +29,10 @@ npm query .workspace | jq -r '.[].location' | while read -r dir; do
   # Create the tarball.
   pushd "$dir"
   name=$(jq -r .name < package.json)
-  stem=$(echo "$name" | sed -e 's!^@!!; s!/!-!g;')
-  rm -f "${stem}"-*.tgz
-  yarn pack
-  tar -xvf "${stem}"-*.tgz
+  rm -f package.tgz
+  npm pack
+  mv ./*.tgz package.tgz
+  tar -xvf package.tgz
 
   # Replace the destination package.
   rm -rf "${DSTDIR:?}/$name"
