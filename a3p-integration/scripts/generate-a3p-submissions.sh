7c43b436db1d669cb32cc9e1532522c4a16de65d
Turadg Aleahmad <turadg@agoric.com>
2024-05-08 10:22:29 -0700

    style: format scripts

--- a3p-integration/scripts/generate-a3p-submissions.sh
+++ a3p-integration/scripts/generate-a3p-submissions.sh
@@ -2,15 +2,15 @@
 set -ueo pipefail
 
 # cd prints its target, so without the redirect, we get two copies
-SCRIPT_DIR=$( cd ${0%/*} > /dev/null && pwd -P )
+SCRIPT_DIR=$(cd ${0%/*} > /dev/null && pwd -P)
 
 IFS=$'\n'
 
 for proposal in ./proposals/?:*; do
   cd $proposal
   while read -r line; do
-    IFS=' ' parts=( $line )
+    IFS=' ' parts=($line)
     $SCRIPT_DIR/generate-a3p-submission.sh $proposal ${parts[@]}
-  done < <(jq -r < package.json '.agoricProposal["sdk-generate"][]')
+  done < <(jq -r '.agoricProposal["sdk-generate"][]' < package.json)
   cd -
 done
