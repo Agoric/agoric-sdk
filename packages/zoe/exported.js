24897154d2303658c864577aa79c11255b38ce61
Turadg Aleahmad <turadg@agoric.com>
2024-05-17 10:56:31 -0700

    chore(types): rm empty 'exported' module

--- packages/zoe/exported.js
+++ packages/zoe/exported.js
@@ -8,4 +8,3 @@ import './src/types-ambient.js';
 import '@agoric/notifier/exported.js';
 import '@agoric/ertp/exported.js';
 import '@agoric/store/exported.js';
-import '@agoric/swingset-vat/exported.js';
