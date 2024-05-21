c63d7c34c3009a4689e2475e64ea294366483dce
Mathieu Hofman <mathieu@agoric.com>
2024-05-21 00:16:07 +0000

    chore(cosmic-swingset): default artifact mode to operational

--- packages/cosmic-swingset/src/export-kernel-db.js
+++ packages/cosmic-swingset/src/export-kernel-db.js
@@ -42,10 +42,10 @@ export const ExportManifestFileName = 'export-manifest.json';
  */
 export const getEffectiveArtifactMode = artifactMode => {
   switch (artifactMode) {
+    case undefined:
     case 'none':
     case 'operational':
       return 'operational';
-    case undefined:
     case 'replay':
       return 'replay';
     case 'archival':
