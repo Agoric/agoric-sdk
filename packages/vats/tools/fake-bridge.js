9abd0931182993545504f9f264440052128756e8
Turadg Aleahmad <turadg@agoric.com>
2024-05-23 10:58:08 -0700

    feat: fake bank bridge

--- packages/vats/tools/fake-bridge.js
+++ packages/vats/tools/fake-bridge.js
@@ -1,10 +1,116 @@
 import { Fail } from '@agoric/assert';
+import { makeTracer, VBankAccount } from '@agoric/internal';
 import assert from 'node:assert/strict';
+import { E } from '@endo/far';
 
 /**
  * @import {MsgDelegateResponse} from '@agoric/cosmic-proto/cosmos/staking/v1beta1/tx.js';
- * @import {ScopedBridgeManager} from '../src/types.js';
+ * @import {BridgeHandler, ScopedBridgeManager} from '../src/types.js';
+ * @import {Remote} from '@agoric/vow';
  */
+const trace = makeTracer('FakeBridge', false);
+
+/** @typedef {{ [address: string]: { [denom: string]: bigint } }} Balances */
+
+export const FAUCET_ADDRESS = 'faucet';
+const INFINITE_AMOUNT = 99999999999n;
+
+/**
+ * You can withdraw from the `faucet` address infinitely because its balance is
+ * always huge. When you withdraw, it's as if it is topped up again by a Cosmos
+ * transaction outside the Agoric VM. (Similarly for deposits.)
+ *
+ * @param {import('@agoric/zone').Zone} zone
+ * @param {object} opts
+ * @param {Balances} opts.balances initial balances
+ * @returns {ScopedBridgeManager<'bank'>}
+ */
+export const makeFakeBankBridge = (zone, opts = { balances: {} }) => {
+  const { balances } = opts;
+
+  const currentBalance = ({ address, denom }) =>
+    address === FAUCET_ADDRESS
+      ? INFINITE_AMOUNT
+      : Nat((balances[address] && balances[address][denom]) ?? 0n);
+
+  let lastNonce = 0n;
+  /** @type {Remote<BridgeHandler>} */
+  let hndlr;
+  return zone.exo('Fake Bank Bridge Manager', undefined, {
+    getBridgeId: () => 'bank',
+    toBridge: async obj => {
+      const { method, type, ...params } = obj;
+      trace('toBridge', type, method, params);
+      switch (obj.type) {
+        case 'VBANK_GET_MODULE_ACCOUNT_ADDRESS': {
+          const { moduleName } = obj;
+          const moduleDescriptor = Object.values(VBankAccount).find(
+            ({ module }) => module === moduleName,
+          );
+          if (!moduleDescriptor) {
+            return 'undefined';
+          }
+          return moduleDescriptor.address;
+        }
+
+        // Observed message:
+        // address: 'agoric1megzytg65cyrgzs6fvzxgrcqvwwl7ugpt62346',
+        // denom: 'ibc/toyatom',
+        // type: 'VBANK_GET_BALANCE'
+        case 'VBANK_GET_BALANCE': {
+          return String(currentBalance(obj));
+        }
+
+        case 'VBANK_GRAB':
+        case 'VBANK_GIVE': {
+          const { amount, denom } = obj;
+          const address = type === 'VBANK_GRAB' ? obj.sender : obj.recipient;
+          balances[address] ||= {};
+          balances[address][denom] ||= 0n;
+
+          if (type === 'VBANK_GRAB') {
+            balances[address][denom] = Nat(
+              currentBalance({ address, denom }) - BigInt(amount),
+            );
+          } else {
+            balances[address][denom] = Nat(
+              currentBalance({ address, denom }) + BigInt(amount),
+            );
+          }
+
+          lastNonce += 1n;
+          // Also empty balances.
+          return harden({
+            type: 'VBANK_BALANCE_UPDATE',
+            nonce: `${lastNonce}`,
+            updated: [
+              {
+                address,
+                denom,
+                amount: String(currentBalance({ address, denom })),
+              },
+            ],
+          });
+        }
+        default:
+          Fail`unknown type ${type}`;
+      }
+    },
+    fromBridge: async obj => {
+      if (!hndlr) throw Error('no handler!');
+      await E(hndlr).fromBridge(obj);
+      return E(hndlr).fromBridge(obj);
+    },
+    initHandler: h => {
+      if (hndlr) throw Error('already init');
+      hndlr = h;
+    },
+    setHandler: h => {
+      if (!hndlr) throw Error('must init first');
+      hndlr = h;
+    },
+  });
+};
 
 /**
  * @param {import('@agoric/zone').Zone} zone
@@ -13,6 +119,7 @@ import assert from 'node:assert/strict';
  * @returns {ScopedBridgeManager<'dibc'>}
  */
 export const makeFakeIbcBridge = (zone, onToBridge, onFromBridge) => {
+  /** @type {Remote<BridgeHandler>} */
   let hndlr;
   return zone.exo('Fake IBC Bridge Manager', undefined, {
     getBridgeId: () => 'dibc',
@@ -29,6 +136,7 @@ export const makeFakeIbcBridge = (zone, onToBridge, onFromBridge) => {
     fromBridge: async obj => {
       if (!hndlr) throw Error('no handler!');
       await onFromBridge(hndlr, obj);
+      return E(hndlr).fromBridge(obj);
     },
     initHandler: h => {
       if (hndlr) throw Error('already init');
@@ -52,6 +160,7 @@ export const makeFakeLocalchainBridge = (
   onToBridge = () => {},
   onFromBridge = () => {},
 ) => {
+  /** @type {Remote<BridgeHandler>} */
   let hndlr;
   let lcaExecuteTxSequence = 0;
   return zone.exo('Fake Localchain Bridge Manager', undefined, {
@@ -59,7 +168,7 @@ export const makeFakeLocalchainBridge = (
     toBridge: async obj => {
       onToBridge(obj);
       const { method, type, ...params } = obj;
-      console.info('toBridge', type, method, params);
+      trace('toBridge', type, method, params);
       switch (type) {
         case 'VLOCALCHAIN_ALLOCATE_ADDRESS':
           return 'agoric1fakeLCAAddress';
@@ -97,6 +206,7 @@ export const makeFakeLocalchainBridge = (
     fromBridge: async obj => {
       if (!hndlr) throw Error('no handler!');
       await onFromBridge(hndlr, obj);
+      return E(hndlr).fromBridge(obj);
     },
     initHandler: h => {
       if (hndlr) throw Error('already init');
