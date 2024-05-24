30e2f1e8512b3bf8ad9ceed195fb310d77e40c18
Turadg Aleahmad <turadg@agoric.com>
2024-05-24 12:52:08 -0700

    test: common orcSetup

--- packages/orchestration/test/examples/unbondExample.test.ts
+++ packages/orchestration/test/examples/unbondExample.test.ts
@@ -1,16 +1,9 @@
 import { test } from '@agoric/zoe/tools/prepare-test-env-ava.js';
 
-import { makeIssuerKit } from '@agoric/ertp';
-import { makeFakeStorageKit } from '@agoric/internal/src/storage-test-utils.js';
-import { prepareLocalChainTools } from '@agoric/vats/src/localchain.js';
-import { buildRootObject as buildBankVatRoot } from '@agoric/vats/src/vat-bank.js';
 import { setUpZoeForTest } from '@agoric/zoe/tools/setup-zoe.js';
-import { withAmountUtils } from '@agoric/zoe/tools/test-utils.js';
-import { makeHeapZone } from '@agoric/zone';
 import { E } from '@endo/far';
 import path from 'path';
-import { makeFakeBankManagerKit } from '@agoric/vats/tools/bank-utils.js';
-import { makeFakeLocalchainBridge } from '../supports.js';
+import { orcSetup } from '../supports.js';
 
 const dirname = path.dirname(new URL(import.meta.url).pathname);
 
@@ -19,42 +12,26 @@ type StartFn =
   typeof import('@agoric/orchestration/src/examples/unbondExample.contract.js').start;
 
 test('start', async t => {
-  const issuerKit = makeIssuerKit('IST');
-  const stable = withAmountUtils(issuerKit);
+  const {
+    bootstrap,
+    brands: { ist },
+  } = await orcSetup(t);
+
   const { zoe, bundleAndInstall } = await setUpZoeForTest();
   const installation: Installation<StartFn> =
     await bundleAndInstall(contractFile);
 
-  const zone = makeHeapZone();
-  const { makeLocalChain } = prepareLocalChainTools(zone.subZone('localchain'));
-  const { bankManager } = await makeFakeBankManagerKit();
-
-  await E(bankManager).addAsset('uist', 'IST', 'Inter Stable Token', issuerKit);
-
-  const localchainBridge = makeFakeLocalchainBridge(zone);
-
-  const localchain = makeLocalChain({
-    bankManager,
-    system: localchainBridge,
-  });
-
-  const storage = makeFakeStorageKit('mockChainStorageRoot', {
-    sequence: false,
-  });
-
-  const privateArgs = {
-    localchain,
-    orchestrationService: null as any,
-    storageNode: storage.rootNode,
-    timerService: null as any,
-    zone,
-  };
-
   const { publicFacet } = await E(zoe).startInstance(
     installation,
-    { Stable: stable.issuer },
+    { Stable: ist.issuer },
     {},
-    privateArgs,
+    {
+      localchain: bootstrap.localchain,
+      orchestrationService: null as any,
+      storageNode: bootstrap.storage.rootNode,
+      timerService: bootstrap.timer,
+      zone: bootstrap.rootZone,
+    },
   );
 
   const inv = E(publicFacet).makeUnbondAndLiquidStakeInvitation();
--- packages/orchestration/test/supports.ts
+++ packages/orchestration/test/supports.ts
@@ -1,1 +1,62 @@
+import { makeIssuerKit } from '@agoric/ertp';
+import { makeFakeStorageKit } from '@agoric/internal/src/storage-test-utils.js';
+import { prepareLocalChainTools } from '@agoric/vats/src/localchain.js';
+import { makeFakeBankManagerKit } from '@agoric/vats/tools/bank-utils.js';
+import { makeFakeBoard } from '@agoric/vats/tools/board-utils.js';
+import { makeFakeLocalchainBridge } from '@agoric/vats/tools/fake-bridge.js';
+import type { Installation } from '@agoric/zoe/src/zoeService/utils.js';
+import buildManualTimer from '@agoric/zoe/tools/manualTimer.js';
+import { withAmountUtils } from '@agoric/zoe/tools/test-utils.js';
+import { makeHeapZone } from '@agoric/zone';
+import { E } from '@endo/far';
+
 export { makeFakeLocalchainBridge } from '@agoric/vats/tools/fake-bridge.js';
+
+export const orcSetup = async t => {
+  t.log('bootstrap vat dependencies');
+  const rootZone = makeHeapZone();
+  const bld = withAmountUtils(makeIssuerKit('BLD'));
+  const ist = withAmountUtils(makeIssuerKit('IST'));
+
+  const { bankManager, pourPayment } = await makeFakeBankManagerKit();
+  await E(bankManager).addAsset('ubld', 'BLD', 'Staking Token', bld.issuerKit);
+  await E(bankManager).addAsset(
+    'uist',
+    'IST',
+    'Inter Stable Token',
+    ist.issuerKit,
+  );
+
+  const localchainBridge = makeFakeLocalchainBridge(rootZone);
+  const localchain = prepareLocalChainTools(
+    rootZone.subZone('localchain'),
+  ).makeLocalChain({
+    bankManager,
+    system: localchainBridge,
+  });
+  const timer = buildManualTimer(t.log);
+  const marshaller = makeFakeBoard().getReadonlyMarshaller();
+  const storage = makeFakeStorageKit('mockChainStorageRoot', {
+    sequence: false,
+  });
+  return {
+    bootstrap: {
+      bankManager,
+      timer,
+      localchain,
+      marshaller,
+      rootZone,
+      storage,
+    },
+    brands: {
+      // TODO consider omitting `issuer` to prevent minting, which the bank can't observe
+      bld,
+      ist,
+    },
+    utils: {
+      pourPayment,
+    },
+  };
+};
+
+export const makeDefaultContext = <SF>(contract: Installation<SF>) => {};
