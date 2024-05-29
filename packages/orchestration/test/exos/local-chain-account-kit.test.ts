6a7e887c0d6e66a250f4aee2fb5694288df1ab71
Turadg Aleahmad <turadg@agoric.com>
2024-05-29 14:46:00 -0700

    test: withdrawal

--- packages/orchestration/test/exos/local-chain-account-kit.test.ts
+++ packages/orchestration/test/exos/local-chain-account-kit.test.ts
@@ -9,6 +9,74 @@ import { prepareMockChainInfo } from '../../src/utils/mockChainInfo.js';
 import { NANOSECONDS_PER_SECOND } from '../../src/utils/time.js';
 import { commonSetup } from '../supports.js';
 
+test('deposit, withdraw', async t => {
+  const { bootstrap, brands, utils } = await commonSetup(t);
+
+  const { bld: stake } = brands;
+
+  const { timer, localchain, marshaller, rootZone, storage } = bootstrap;
+
+  t.log('chainInfo mocked via `prepareMockChainInfo` until #8879');
+  const agoricChainInfo = prepareMockChainInfo(rootZone.subZone('chainInfo'));
+
+  t.log('exo setup - prepareLocalChainAccountKit');
+  const { makeRecorderKit } = prepareRecorderKitMakers(
+    rootZone.mapStore('recorder'),
+    marshaller,
+  );
+  const makeLocalChainAccountKit = prepareLocalChainAccountKit(
+    rootZone,
+    makeRecorderKit,
+    // @ts-expect-error mocked zcf. use `stake-bld.contract.test.ts` to test LCA with offer
+    Far('MockZCF', {}),
+    timer,
+    timer.getTimerBrand(),
+    agoricChainInfo,
+  );
+
+  t.log('request account from vat-localchain');
+  const lca = await E(localchain).makeAccount();
+  const address = await E(lca).getAddress();
+
+  t.log('make a LocalChainAccountKit');
+  const { holder: account } = makeLocalChainAccountKit({
+    account: lca,
+    address,
+    storageNode: storage.rootNode.makeChildNode('lcaKit'),
+  });
+
+  t.regex(await E(account).getAddress(), /agoric1/);
+
+  const oneHundredStakePmt = await utils.pourPayment(stake.units(100));
+
+  t.log('deposit 100 bld to account');
+  const depositResp = await E(account).deposit(oneHundredStakePmt);
+  t.true(AmountMath.isEqual(depositResp, stake.units(100)), 'deposit');
+
+  const withdrawal1 = await E(account).withdraw(stake.units(50));
+  t.true(
+    AmountMath.isEqual(
+      await stake.issuer.getAmountOf(withdrawal1),
+      stake.units(50),
+    ),
+  );
+
+  await t.throwsAsync(
+    E(account).withdraw(stake.units(51)),
+    undefined,
+    'fails to overwithdraw',
+  );
+  await t.notThrowsAsync(
+    E(account).withdraw(stake.units(50)),
+    'succeeeds at exactly empty',
+  );
+  await t.throwsAsync(
+    E(account).withdraw(stake.make(1n)),
+    undefined,
+    'fails to overwithdraw',
+  );
+});
+
 test('transfer', async t => {
   const { bootstrap, brands, utils } = await commonSetup(t);
 
