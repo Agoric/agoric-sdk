1906a7689dce773dc3a28e41336908dd4cd82f41
Turadg Aleahmad <turadg@agoric.com>
2024-05-29 13:48:32 -0700

    test: factor out LCA/stakeBld

--- packages/boot/test/bootstrapTests/orchestration.test.ts
+++ packages/boot/test/bootstrapTests/orchestration.test.ts
@@ -4,10 +4,10 @@ import type { TestFn } from 'ava';
 
 import { Fail } from '@agoric/assert';
 import { AmountMath } from '@agoric/ertp';
-import type { start as stakeBldStart } from '@agoric/orchestration/src/examples/stakeBld.contract.js';
 import type { Instance } from '@agoric/zoe/src/zoeService/utils.js';
 import { M, matches } from '@endo/patterns';
 import type { CosmosValidatorAddress } from '@agoric/orchestration';
+import type { start as startStakeAtom } from '@agoric/orchestration/src/examples/stakeAtom.contract.js';
 import { makeWalletFactoryContext } from './walletFactory.ts';
 
 type DefaultTestContext = Awaited<ReturnType<typeof makeWalletFactoryContext>>;
@@ -17,85 +17,13 @@ const test: TestFn<DefaultTestContext> = anyTest;
 test.before(async t => (t.context = await makeWalletFactoryContext(t)));
 test.after.always(t => t.context.shutdown?.());
 
-test.serial('stakeBld', async t => {
-  const {
-    agoricNamesRemotes,
-    buildProposal,
-    evalProposal,
-    refreshAgoricNamesRemotes,
-  } = t.context;
-  // TODO move into a vm-config for u15
-  await evalProposal(
-    buildProposal('@agoric/builders/scripts/vats/init-localchain.js'),
-  );
-  // start-stakeBld depends on this. Sanity check in case the context changes.
-  const { BLD } = agoricNamesRemotes.brand;
-  BLD || Fail`BLD missing from agoricNames`;
-  await evalProposal(
-    buildProposal('@agoric/builders/scripts/orchestration/init-stakeBld.js'),
-  );
-  // update now that stakeBld is instantiated
-  refreshAgoricNamesRemotes();
-  const stakeBld = agoricNamesRemotes.instance.stakeBld as Instance<
-    typeof stakeBldStart
-  >;
-  t.truthy(stakeBld);
-
-  const wd = await t.context.walletFactoryDriver.provideSmartWallet(
-    'agoric1testStakeBld',
-  );
-
-  await wd.executeOffer({
-    id: 'request-stake',
-    invitationSpec: {
-      source: 'agoricContract',
-      instancePath: ['stakeBld'],
-      callPipe: [['makeStakeBldInvitation']],
-    },
-    proposal: {
-      give: {
-        // @ts-expect-error XXX BoardRemote
-        In: { brand: BLD, value: 10n },
-      },
-    },
-  });
-
-  const current = await wd.getCurrentWalletRecord();
-  const latest = await wd.getLatestUpdateRecord();
-  t.like(current, {
-    offerToPublicSubscriberPaths: [
-      // TODO publish something useful
-      ['request-stake', { account: 'published.stakeBld' }],
-    ],
-  });
-  t.like(latest, {
-    status: { id: 'request-stake', numWantsSatisfied: 1 },
-  });
-
-  await wd.executeOffer({
-    id: 'request-delegate',
-    invitationSpec: {
-      source: 'continuing',
-      previousOffer: 'request-stake',
-      invitationMakerName: 'Delegate',
-      invitationArgs: ['agoric1validator1', { brand: BLD, value: 10n }],
-    },
-    proposal: {
-      give: {
-        // @ts-expect-error XXX BoardRemote
-        In: { brand: BLD, value: 10n },
-      },
-    },
-  });
-});
-
 test.serial('stakeAtom - repl-style', async t => {
   const {
     buildProposal,
     evalProposal,
     runUtils: { EV },
   } = t.context;
-  // TODO move into a vm-config for u15
+  // TODO move into a vm-config
   await evalProposal(
     buildProposal('@agoric/builders/scripts/vats/init-network.js'),
   );
@@ -107,10 +35,12 @@ test.serial('stakeAtom - repl-style', async t => {
   );
 
   const agoricNames = await EV.vat('bootstrap').consumeItem('agoricNames');
-  const instance = await EV(agoricNames).lookup('instance', 'stakeAtom');
+  const instance: Instance<typeof startStakeAtom> = await EV(
+    agoricNames,
+  ).lookup('instance', 'stakeAtom');
   t.truthy(instance, 'stakeAtom instance is available');
 
-  const zoe = await EV.vat('bootstrap').consumeItem('zoe');
+  const zoe: ZoeService = await EV.vat('bootstrap').consumeItem('zoe');
   const publicFacet = await EV(zoe).getPublicFacet(instance);
   t.truthy(publicFacet, 'stakeAtom publicFacet is available');
 
@@ -122,7 +52,7 @@ test.serial('stakeAtom - repl-style', async t => {
     'account is a remotable',
   );
 
-  const atomBrand = await EV(agoricNames).lookup('brand', 'ATOM');
+  const atomBrand: Brand = await EV(agoricNames).lookup('brand', 'ATOM');
   const atomAmount = AmountMath.make(atomBrand, 10n);
 
   const validatorAddress: CosmosValidatorAddress = {
