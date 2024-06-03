89b50a082c63e236356f33d521cad5f15a4e80d2
Michael FIG <mfig@agoric.com>
2024-06-02 20:59:14 -0600

    feat(vtransfer): save watched addresses to genesis

--- golang/cosmos/x/vtransfer/genesis.go
+++ golang/cosmos/x/vtransfer/genesis.go
@@ -1,9 +1,9 @@
-package vbank
+package vtransfer
 
 import (
 	"fmt"
 
-	"github.com/Agoric/agoric-sdk/golang/cosmos/x/vbank/types"
+	"github.com/Agoric/agoric-sdk/golang/cosmos/x/vtransfer/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 )
@@ -14,30 +14,26 @@ func NewGenesisState() *types.GenesisState {
 
 func ValidateGenesis(data *types.GenesisState) error {
 	if data == nil {
-		return fmt.Errorf("vbank genesis data cannot be nil")
-	}
-	if err := data.Params.ValidateBasic(); err != nil {
-		return err
+		return fmt.Errorf("vtransfer genesis data cannot be nil")
 	}
 	return nil
 }
 
 func DefaultGenesisState() *types.GenesisState {
-	return &types.GenesisState{
-		Params: types.DefaultParams(),
-		// State is defaulted to empty.
-	}
+	return &types.GenesisState{}
 }
 
 func InitGenesis(ctx sdk.Context, keeper Keeper, data *types.GenesisState) []abci.ValidatorUpdate {
-	keeper.SetParams(ctx, data.GetParams())
-	keeper.SetState(ctx, data.GetState())
+	keeper.SetWatchedAddresses(ctx, data.GetWatchedAddresses())
 	return []abci.ValidatorUpdate{}
 }
 
 func ExportGenesis(ctx sdk.Context, k Keeper) *types.GenesisState {
 	var gs types.GenesisState
-	gs.Params = k.GetParams(ctx)
-	gs.State = k.GetState(ctx)
+	addresses, err := k.GetWatchedAddresses(ctx)
+	if err != nil {
+		panic(err)
+	}
+	gs.WatchedAddresses = addresses
 	return &gs
 }
