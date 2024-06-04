bacefd13a1a781ddf308b470072ecb61b68d40b4
0xPatrick <patrick@0xpatrick.dev>
2024-06-04 10:56:37 -0400

    test(vlocalchain): keeper.DeserializeTxMessages behavior

--- golang/cosmos/x/vlocalchain/keeper/keeper_test.go
+++ golang/cosmos/x/vlocalchain/keeper/keeper_test.go
@@ -1,6 +1,15 @@
 package keeper
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/Agoric/agoric-sdk/golang/cosmos/app/params"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+)
+
 
 func TestKeeper_ParseRequestTypeURL(t *testing.T) {
 	testCases := []struct {
@@ -26,6 +35,62 @@ func TestKeeper_ParseRequestTypeURL(t *testing.T) {
 			}
 			if responseURL != tc.responseURL {
 				t.Errorf("%v response expected %v, got %v", tc.typeUrl, tc.responseURL, responseURL)
+			}
+		})
+	}
+}
+
+func TestKeeper_DeserializeTxMessages(t *testing.T) {
+	encodingConfig := params.MakeEncodingConfig()
+	cdc := encodingConfig.Marshaler
+
+	banktypes.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+
+	keeper := NewKeeper(cdc, nil, nil, nil)
+
+	expectedMsgSend := []sdk.Msg{
+		&banktypes.MsgSend{
+			FromAddress: "cosmos1abc",
+			ToAddress:   "cosmos1xyz",
+			Amount:      sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(100))),
+		},
+	}
+
+	testCases := []struct {
+		name     string
+		json     string
+		expected []sdk.Msg
+		wantErr  bool
+	}{
+		{
+			name: "camelCase keys",
+			json: `{"messages":[{"@type":"/cosmos.bank.v1beta1.MsgSend","fromAddress":"cosmos1abc","toAddress":"cosmos1xyz","amount":[{"denom":"stake","amount":"100"}]}]}`,
+			expected: expectedMsgSend,
+			wantErr: false,
+		},
+		{
+			name: "snake_case keys",
+			json: `{"messages":[{"@type":"/cosmos.bank.v1beta1.MsgSend","from_address":"cosmos1abc","to_address":"cosmos1xyz","amount":[{"denom":"stake","amount":"100"}]}]}`,
+			expected: expectedMsgSend,
+			wantErr: false,
+		},
+		{
+			name: "misspelled key",
+			json: `{"messages":[{"@type":"/cosmos.bank.v1beta1.MsgSend","from_addresss":"cosmos1abc","to_address":"cosmos1xyz","amount":[{"denom":"stake","amount":"100"}]}]}`,
+			expected: expectedMsgSend,
+			wantErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			msgs, err := keeper.DeserializeTxMessages([]byte(tc.json))
+
+			if tc.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.expected, msgs)
 			}
 		})
 	}
