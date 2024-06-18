a1538a86942346b73b989e92fc24f7e74da964ad
Mathieu Hofman <mathieu@agoric.com>
2024-06-18 00:57:40 +0000

    Revert "fix(a3p-integration): workaround zoe issues"
    This reverts commit fb74bbd9e1c407f788dc53604fa17d6df06c3843.

--- a3p-integration/proposals/a:upgrade-16/probeZcfBundleCap.test.js
+++ a3p-integration/proposals/a:upgrade-16/probeZcfBundleCap.test.js
@@ -24,4 +24,13 @@ test('upgrade Zoe to verify ZcfBundleCap endures', async t => {
     detailsBefore.incarnation + 2,
     'wf incarnation must increase by 2',
   );
+
+  // The test restarts the WalletFactory, so it'll use the recently assigned
+  // ZCF bundle. It then restarts Zoe, so it'll revert to whichever ZCF bundle
+  // made it to persistent store. We then restart the Wallet Factory and see if
+  // it's gone back to the ZCF that Zoe initially knew about. If we could get the
+  // ZCF bundleID here from the probe, we'd explicitly check for that. Instead,
+  // we have to be content that it did indeed use the newly assigned bundle in
+  // manual tests.
+  t.not(detailsAfter.bundleID, detailsBefore.bundleID);
 });
