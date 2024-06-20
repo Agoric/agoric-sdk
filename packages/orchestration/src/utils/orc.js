74ecb346486bc0b51e8752ff7fc47d1926ef3a8c
Dan Connolly <connolly@agoric.com>
2024-06-20 14:44:07 -0500

    WIP: makeOsmosisSwap based on XCSv2

--- packages/orchestration/src/utils/orc.js
+++ packages/orchestration/src/utils/orc.js
@@ -1,4 +1,53 @@
-/** @import {AfterAction, SwapExact, SwapMaxSlippage, TransferMsg} from '../types.js' */
+/** @import {AfterAction, Denom, SwapExact, SwapMaxSlippage, TransferMsg} from '../types.js' */
+
+/**
+ * @template M
+ * @typedef {{
+ *   wasm: {
+ *     contract: WASMContractAddress;
+ *     msg: M;
+ *   };
+ * }} IBCHook
+ */
+/**
+ * @typedef {string} WASMContractAddress
+ *
+ * @typedef {string} Percentage
+ */
+/**
+ * @template M
+ * @typedef {{
+ *   osmosis_swap: {
+ *     output_denom: Denom;
+ *     slippage: {
+ *       twap: { slippage_percentage: Percentage; window_seconds: number };
+ *     };
+ *     receiver: string;
+ *     on_failed_delivery: 'do_nothing';
+ *     next_memo: M;
+ *   };
+ * }} XCSv2Msg
+ */
+/**
+ * cf.
+ * https://github.com/osmosis-labs/osmosis/tree/main/cosmwasm/contracts/crosschain-swaps#via-ibc
+ *
+ * @satisfies {IBCHook<XCSv2Msg<null>>}
+ */
+const XCSv2example = {
+  wasm: {
+    contract: 'osmo1crosschainswapscontract',
+    msg: {
+      osmosis_swap: {
+        output_denom: 'token1',
+        slippage: { twap: { slippage_percentage: '20', window_seconds: 10 } },
+        receiver: 'juno1receiver',
+        on_failed_delivery: 'do_nothing',
+        next_memo: null,
+      },
+    },
+  },
+};
 
 export const orcUtils = {
   /**
@@ -21,18 +70,28 @@ export const orcUtils = {
    * SwapExact or SwapMaxSlippage, with optional AfterAction
    *
    * @param {(SwapExact | SwapMaxSlippage) &
-   *   (AfterAction | Record<string, never>)} _args
+   *   (AfterAction | Record<string, never>)} opts
    *
    * @returns {TransferMsg}
    */
-  makeOsmosisSwap(_args) {
-    // FIXME mocked, so typescript is happy
-    return {
-      toAccount: {
-        chainId: 'osmosis-test',
-        value: 'osmo1234',
-        encoding: 'bech32',
+  makeOsmosisSwap(opts) {
+    assert('slippage' in opts, 'SwapExact is TODO');
+    assert('destAddress' in opts, 'TODO: optional destAddress');
+    /** @type {XCSv2Msg<null>} */
+    const it = {
+      osmosis_swap: {
+        output_denom: '// @@@TODO',
+        slippage: {
+          twap: {
+            slippage_percentage: `${opts.slippage * 100}`,
+            window_seconds: 10,
+          },
+        },
+        receiver: opts.destAddress.value,
+        on_failed_delivery: 'do_nothing',
+        next_memo: null,
       },
     };
+    return it;
   },
 };
