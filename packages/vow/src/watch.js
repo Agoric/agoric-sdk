dd1a7dbb56c2d7bfebaba632b9e0a7f6c39bb48e
Mathieu Hofman <86499+mhofman@users.noreply.github.com>
2024-06-21 22:20:50 -0700

    fix(vow): handle resolution loops in vows (#9561)
    closes: #9560
    
    ## Description
    Add a weak set of previously seen vowV0 payloads when shortening during `watch` or `when`
    
    ### Security Considerations
    None
    
    ### Scaling Considerations
    Potential churn in storage just for shortening loop detection. Every `watch` will cause a new WeakSetStore to be allocated.
    
    ### Documentation Considerations
    None
    
    ### Testing Considerations
    New unit tests
    
    ### Upgrade Considerations
    To avoid state migration concerns, this needs to be deployed before we start using `watch` more broadly.

--- packages/vow/src/watch.js
+++ packages/vow/src/watch.js
@@ -65,8 +65,23 @@ const settle = (resolver, watcher, wcb, value, watcherArgs = []) => {
  * @param {IsRetryableReason} isRetryableReason
  * @param {ReturnType<typeof makeWatchNextStep>} watchNextStep
  */
-const preparePromiseWatcher = (zone, isRetryableReason, watchNextStep) =>
-  zone.exoClass(
+const preparePromiseWatcher = (zone, isRetryableReason, watchNextStep) => {
+  // We use an ephemeral WeakSet for the previously seen vows in a watch operation
+  // While watch is durable, it suffices to detect the cycle in a single incarnation
+  /** @type {WeakMap<PromiseWatcher, WeakSet<any>>} */
+  const watcherSeenPayloads = new WeakMap();
+
+  /** @param {PromiseWatcher} watcher */
+  const getSeenPayloads = watcher => {
+    let seenPayloads = watcherSeenPayloads.get(watcher);
+    if (!seenPayloads) {
+      seenPayloads = new WeakSet();
+      watcherSeenPayloads.set(watcher, seenPayloads);
+    }
+    return seenPayloads;
+  };
+
+  return zone.exoClass(
     'PromiseWatcher',
     PromiseWatcherI,
     /**
@@ -91,12 +106,20 @@ const preparePromiseWatcher = (zone, isRetryableReason, watchNextStep) =>
       /** @type {Required<PromiseWatcher>['onFulfilled']} */
       onFulfilled(value) {
         const { watcher, watcherArgs, resolver } = this.state;
-        if (getVowPayload(value)) {
+        const payload = getVowPayload(value);
+        if (payload) {
+          const seenPayloads = getSeenPayloads(this.self);
+          // TODO: rely on endowed helper to get storable cap from payload
+          if (seenPayloads.has(payload.vowV0)) {
+            return this.self.onRejected(Error('Vow resolution cycle detected'));
+          }
+          seenPayloads.add(payload.vowV0);
           // We've been shortened, so reflect our state accordingly, and go again.
           this.state.vow = value;
           watchNextStep(value, this.self);
           return;
         }
+        watcherSeenPayloads.delete(this.self);
         this.state.priorRetryValue = undefined;
         this.state.watcher = undefined;
         this.state.resolver = undefined;
@@ -115,6 +138,7 @@ const preparePromiseWatcher = (zone, isRetryableReason, watchNextStep) =>
             return;
           }
         }
+        watcherSeenPayloads.delete(this.self);
         this.state.priorRetryValue = undefined;
         this.state.resolver = undefined;
         this.state.watcher = undefined;
@@ -122,6 +146,7 @@ const preparePromiseWatcher = (zone, isRetryableReason, watchNextStep) =>
       },
     },
   );
+};
 
 /**
  * @param {Zone} zone
