572e46c2fa8837ea00da68d16c0f8abbc52da34b
Halimao <1065621723@qq.com>
2024-06-26 11:50:13 +0800

    feat: support set http proxy from env

--- packages/casting/node-fetch-shim.js
+++ packages/casting/node-fetch-shim.js
@@ -2,6 +2,8 @@
 
 /* global globalThis */
 import fetch from 'node-fetch';
+import { bootstrap } from 'global-agent';
 
 // @ts-expect-error node-fetch does not exactly match W3C Fetch
 globalThis.fetch = fetch;
+bootstrap();
