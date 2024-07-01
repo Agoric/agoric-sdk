57aa7e60fa9f88f33502fab2dabebbbde6d235b3
Michael FIG <mfig@agoric.com>
2024-06-30 21:02:48 -0600

    fix(vow): clearer stored/non-stored values

--- packages/vow/src/types.js
+++ packages/vow/src/types.js
@@ -34,6 +34,11 @@ export {};
  */
 
 /**
+ * @template T
+ * @typedef {ERef<T | Vow<T>>} EVow
+ */
+
+/**
  * Follow the chain of vow shortening to the end, returning the final value.
  * This is used within E, so we must narrow the type to its remote form.
  * @template T
