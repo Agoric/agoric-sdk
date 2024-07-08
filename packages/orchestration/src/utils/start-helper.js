f5772bda2338d705112187a83f1c34ed3eefd2fa
Turadg Aleahmad <turadg@agoric.com>
2024-07-08 14:27:35 -0700

    refactor: consolidate subzones

--- packages/orchestration/src/utils/start-helper.js
+++ packages/orchestration/src/utils/start-helper.js
@@ -48,16 +48,27 @@ export const provideOrchestration = (
     baggage,
   }));
 
-  const zone = makeDurableZone(baggage);
+  // separate zones
+  const zones = (() => {
+    const zone = makeDurableZone(baggage);
+    return {
+      asyncFlow: zone.subZone('asyncFlow'),
+      /** for contract-provided names */
+      contract: zone.subZone('contract'),
+      orchestration: zone.subZone('orchestration'),
+      vows: zone.subZone('vows'),
+    };
+  })();
+
   const { agoricNames, timerService } = remotePowers;
 
-  const vowTools = prepareVowTools(zone.subZone('vows'));
+  const vowTools = prepareVowTools(zones.vows);
 
   const chainHub = makeChainHub(agoricNames, vowTools);
 
   const { makeRecorderKit } = prepareRecorderKitMakers(baggage, marshaller);
   const makeLocalOrchestrationAccountKit = prepareLocalOrchestrationAccountKit(
-    zone,
+    zones.orchestration,
     makeRecorderKit,
     zcf,
     timerService,
@@ -65,19 +76,18 @@ export const provideOrchestration = (
     chainHub,
   );
 
-  const asyncFlowTools = prepareAsyncFlowTools(zone.subZone('asyncFlow'), {
+  const asyncFlowTools = prepareAsyncFlowTools(zones.asyncFlow, {
     vowTools,
   });
 
   const makeCosmosOrchestrationAccount = prepareCosmosOrchestrationAccount(
-    // FIXME what zone?
-    zone,
+    zones.orchestration,
     makeRecorderKit,
     vowTools,
     zcf,
   );
 
-  const makeRemoteChainFacade = prepareRemoteChainFacade(zone, {
+  const makeRemoteChainFacade = prepareRemoteChainFacade(zones.orchestration, {
     makeCosmosOrchestrationAccount,
     orchestration: remotePowers.orchestrationService,
     storageNode: remotePowers.storageNode,
@@ -85,7 +95,7 @@ export const provideOrchestration = (
     vowTools,
   });
 
-  const makeLocalChainFacade = prepareLocalChainFacade(zone, {
+  const makeLocalChainFacade = prepareLocalChainFacade(zones.orchestration, {
     makeLocalOrchestrationAccountKit,
     localchain: remotePowers.localchain,
     // FIXME what path?
@@ -97,7 +107,7 @@ export const provideOrchestration = (
 
   const facade = makeOrchestrationFacade({
     zcf,
-    zone,
+    zone: zones.orchestration,
     chainHub,
     makeLocalOrchestrationAccountKit,
     makeRecorderKit,
@@ -108,6 +118,6 @@ export const provideOrchestration = (
     vowTools,
     ...remotePowers,
   });
-  return { ...facade, chainHub, vowTools, zone };
+  return { ...facade, chainHub, vowTools, zone: zones.contract };
 };
 harden(provideOrchestration);
