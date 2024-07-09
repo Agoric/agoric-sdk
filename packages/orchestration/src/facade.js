e9988c9ae3abc92a4040e150d74afdef56c3b4d1
Turadg Aleahmad <turadg@agoric.com>
2024-07-09 16:47:23 -0700

    feat: orchestrateAll
    Co-Authored-By: Dean Tribble <dtribble@users.noreply.github.com>

--- packages/orchestration/src/facade.js
+++ packages/orchestration/src/facade.js
@@ -54,46 +54,65 @@ export const makeOrchestrationFacade = ({
 
   const { when } = vowTools;
 
+  /**
+   * @template GuestReturn
+   * @template HostReturn
+   * @template GuestContext
+   * @template HostContext
+   * @template {any[]} GuestArgs
+   * @template {any[]} HostArgs
+   * @param {string} durableName - the orchestration flow identity in the zone
+   *   (to resume across upgrades)
+   * @param {HostContext} hostCtx - values to pass through the async flow
+   *   membrane
+   * @param {(
+   *   guestOrc: Orchestrator,
+   *   guestCtx: GuestContext,
+   *   ...args: GuestArgs
+   * ) => Promise<GuestReturn>} guestFn
+   * @returns {(...args: HostArgs) => Promise<HostReturn>} TODO returns a
+   *   Promise for now for compat before use of asyncFlow. But really should be
+   *   `Vow<HostReturn>`
+   */
+  const orchestrate = (durableName, hostCtx, guestFn) => {
+    const subZone = zone.subZone(durableName);
+
+    const hostOrc = makeOrchestrator();
+
+    const [wrappedOrc, wrappedCtx] = prepareEndowment(subZone, 'endowments', [
+      hostOrc,
+      hostCtx,
+    ]);
+
+    const hostFn = asyncFlow(subZone, 'asyncFlow', guestFn);
+
+    const orcFn = (...args) =>
+      // TODO remove the `when` after fixing the return type
+      // to `Vow<HostReturn>`
+      when(hostFn(wrappedOrc, wrappedCtx, ...args));
+    return harden(orcFn);
+  };
+
+  /**
+   * Orchestrate all the guest functions.
+   *
+   * NOTE multiple calls to this with the same guestFn name will fail
+   *
+   * @param {{ [durableName: string]: (...args: any[]) => any }} guestFns
+   * @param {any} hostCtx
+   */
+  const orchestrateAll = (guestFns, hostCtx) =>
+    Object.fromEntries(
+      Object.entries(guestFns).map(([name, guestFn]) => [
+        name,
+        orchestrate(name, hostCtx, guestFn),
+      ]),
+    );
+
   return {
-    /**
-     * @template GuestReturn
-     * @template HostReturn
-     * @template GuestContext
-     * @template HostContext
-     * @template {any[]} GuestArgs
-     * @template {any[]} HostArgs
-     * @param {string} durableName - the orchestration flow identity in the zone
-     *   (to resume across upgrades)
-     * @param {HostContext} hostCtx - values to pass through the async flow
-     *   membrane
-     * @param {(
-     *   guestOrc: Orchestrator,
-     *   guestCtx: GuestContext,
-     *   ...args: GuestArgs
-     * ) => Promise<GuestReturn>} guestFn
-     * @returns {(...args: HostArgs) => Promise<HostReturn>} TODO returns a
-     *   Promise for now for compat before use of asyncFlow. But really should
-     *   be `Vow<HostReturn>`
-     */
-    orchestrate(durableName, hostCtx, guestFn) {
-      const subZone = zone.subZone(durableName);
-
-      const hostOrc = makeOrchestrator();
-
-      const [wrappedOrc, wrappedCtx] = prepareEndowment(subZone, 'endowments', [
-        hostOrc,
-        hostCtx,
-      ]);
-
-      const hostFn = asyncFlow(subZone, 'asyncFlow', guestFn);
-
-      const orcFn = (...args) =>
-        // TODO remove the `when` after fixing the return type
-        // to `Vow<HostReturn>`
-        when(hostFn(wrappedOrc, wrappedCtx, ...args));
-      return harden(orcFn);
-    },
     adminAsyncFlow,
+    orchestrate,
+    orchestrateAll,
   };
 };
 harden(makeOrchestrationFacade);
