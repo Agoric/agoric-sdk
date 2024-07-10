ff94d3c42f9111de792ba1500d111eebbd7b8bfd
Turadg Aleahmad <turadg@agoric.com>
2024-07-10 11:05:53 -0700

    chore(types): retriable

--- packages/vow/src/tools.js
+++ packages/vow/src/tools.js
@@ -10,6 +10,22 @@ import { makeWhen } from './when.js';
  * @import {IsRetryableReason, AsPromiseFunction, EVow, Vow, ERef} from './types.js';
  */
 
+// TODO find a good home, DRY with orchestration package.
+/**
+ * Converts a function type that returns a Promise to a function type that
+ * returns a Vow. If the input is not a function returning a Promise, it
+ * preserves the original type.
+ *
+ * @template T - The type to transform
+ * @typedef {T extends (
+ * ...args: infer Args
+ * ) => Promise<infer R>
+ * ? (...args: Args) => Vow<R>
+ * : T extends (...args: infer Args) => infer R
+ * ? (...args: Args) => R
+ * : T} PromiseToVow
+ */
+
 /**
  * NB: Not to be used in a Vat. It doesn't know what an upgrade is. For that you
  * need `prepareVowTools` from `vat.js`.
@@ -40,14 +56,15 @@ export const prepareVowTools = (zone, powers = {}) => {
    *
    * The internal functions
    *
-   * @template T
+   * @template {(...args: any[]) => any} F
    * @param {Zone} fnZone - the zone for the named function
    * @param {string} name
-   * @param {(...args: unknown[]) => ERef<T>} fn
-   * @returns {(...args: unknown[]) => Vow<T>}
+   * @param {F} fn
+   * @returns {PromiseToVow<F>}
    */
   const retriable =
     (fnZone, name, fn) =>
+    // @ts-expect-error cast
     (...args) => {
       return watch(fn(...args));
     };
