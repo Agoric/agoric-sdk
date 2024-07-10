d91d11d3e8ff68407af0ff4552e9051341418611
Turadg Aleahmad <turadg@agoric.com>
2024-07-10 13:23:10 -0700

    refactor: types to .d.ts

---
+++ packages/async-flow/src/types.d.ts
@@ -0,0 +1,157 @@
+import type { Passable } from '@endo/pass-style';
+import type { Vow, VowTools } from '@agoric/vow';
+import type { LogStore } from './log-store.js';
+import type { Bijection } from './bijection.js';
+import type { EndowmentTools } from './endowments.js';
+
+export type FlowState =
+  | 'Running'
+  | 'Sleeping'
+  | 'Replaying'
+  | 'Failed'
+  | 'Done';
+/**
+ * `T` defaults to `any`, not `Passable`, because unwrapped guests include
+ * non-passables, like unwrapped functions and unwrapped state records.
+ * (Unwrapped functions could be made into Remotables,
+ * but since they still could not be made durable, in this context
+ * it'd be pointless.)
+ */
+export type Guest<T extends unknown = any> = T;
+export type Host<T extends Passable = Passable> = T;
+/**
+ * A HostVow must be durably storable. It corresponds to an
+ * ephemeral guest promise.
+ */
+export type HostVow<T extends Passable = Passable> = Host<Vow<T>>;
+export type GuestAsyncFunc = (
+  ...activationArgs: Guest[]
+) => Guest<Promise<any>>;
+export type HostAsyncFuncWrapper = (...activationArgs: Host[]) => HostVow;
+export type PreparationOptions = {
+  vowTools?: VowTools;
+  makeLogStore?: (() => LogStore) | undefined;
+  makeBijection?: (() => Bijection) | undefined;
+  endowmentTools?: EndowmentTools;
+};
+export type OutcomeKind = 'return' | 'throw';
+export type Outcome =
+  | {
+      kind: 'return';
+      result: any;
+    }
+  | {
+      kind: 'throw';
+      problem: any;
+    };
+export type Ephemera<S extends WeakKey = WeakKey, V extends unknown = any> = {
+  for: (self: S) => V;
+  resetFor: (self: S) => void;
+};
+
+/**
+ * This is the type alias for the membrane log entries we currently implement.
+ *
+ * @see {FutureLogEntry} below for the full membrane log entry, which we do not
+ * yet support.
+ */
+export type LogEntry =
+  | [
+      // ///////////////// From Host to Guest /////////////////////////
+      op: 'doFulfill',
+      vow: HostVow,
+      fulfillment: Host,
+    ]
+  | [op: 'doReject', vow: HostVow, reason: Host]
+  | [op: 'doReturn', callIndex: number, result: Host]
+  | [op: 'doThrow', callIndex: number, problem: Host]
+  | [
+      // ///////////////////// From Guest to Host /////////////////////////
+      op: 'checkCall',
+      target: Host,
+      optVerb: PropertyKey | undefined,
+      args: Host[],
+      callIndex: number,
+    ]
+  | [
+      op: 'checkSendOnly',
+      target: Host,
+      optVerb: PropertyKey | undefined,
+      args: Host[],
+      callIndex: number,
+    ]
+  | [
+      op: 'checkSend',
+      target: Host,
+      optVerb: PropertyKey | undefined,
+      args: Host[],
+      callIndex: number,
+    ];
+
+/**
+ * This would be the type alias for the full membrane log, if we supported:
+ * - the guest sending guest-promises and guest-remotables to the host
+ * - the guest using `E` to eventual-send to guest wrappers of the host
+ *   vows and remotables.
+ */
+export type FutureLogEntry =
+  | [
+      // ///////////////// From Host to Guest ///////////////////////
+      op: 'doFulfill',
+      vow: HostVow,
+      fulfillment: Host,
+    ]
+  | [op: 'doReject', vow: HostVow, reason: Host]
+  | [
+      op: 'doCall',
+      target: Host,
+      optVerb: PropertyKey | undefined,
+      args: Host[],
+      callIndex: number,
+    ]
+  | [
+      op: 'doSendOnly',
+      target: Host,
+      optVerb: PropertyKey | undefined,
+      args: Host[],
+      callIndex: number,
+    ]
+  | [
+      op: 'doSend',
+      target: Host,
+      optVerb: PropertyKey | undefined,
+      args: Host[],
+      callIndex: number,
+    ]
+  | [op: 'doReturn', callIndex: number, result: Host]
+  | [op: 'doThrow', callIndex: number, problem: Host]
+  | [
+      // ///////////////////// From Guest to Host /////////////////////////
+      op: 'checkFulfill',
+      vow: HostVow,
+      fulfillment: Host,
+    ]
+  | [op: 'checkReject', vow: HostVow, reason: Host]
+  | [
+      op: 'checkCall',
+      target: Host,
+      optVerb: PropertyKey | undefined,
+      args: Host[],
+      callIndex: number,
+    ]
+  | [
+      op: 'checkSendOnly',
+      target: Host,
+      optVerb: PropertyKey | undefined,
+      args: Host[],
+      callIndex: number,
+    ]
+  | [
+      op: 'checkSend',
+      target: Host,
+      optVerb: PropertyKey | undefined,
+      args: Host[],
+      callIndex: number,
+    ]
+  | [op: 'checkReturn', callIndex: number, result: Host]
+  | [op: 'checkThrow', callIndex: number, problem: Host];
