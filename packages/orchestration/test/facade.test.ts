3c81747165ae4f177f49c6aa21806d769c341407
Turadg Aleahmad <turadg@agoric.com>
2024-07-12 08:38:16 -0700

    test: chain info lacks staking denom

--- packages/orchestration/test/facade.test.ts
+++ packages/orchestration/test/facade.test.ts
@@ -2,6 +2,7 @@ import { test as anyTest } from '@agoric/zoe/tools/prepare-test-env-ava.js';
 
 import { prepareSwingsetVowTools } from '@agoric/vow/vat.js';
 import { setupZCFTest } from '@agoric/zoe/test/unitTests/zcf/setupZcfTest.js';
+import { reincarnate } from '@agoric/swingset-liveslots/tools/setup-vat-data.js';
 import type { CosmosChainInfo, IBCConnectionInfo } from '../src/cosmos-api.js';
 import type { Chain } from '../src/orchestration-api.js';
 import { provideOrchestration } from '../src/utils/start-helper.js';
@@ -39,7 +40,7 @@ export const mockChainConnection: IBCConnectionInfo = {
   },
 };
 
-test('chain info', async t => {
+test.serial('chain info', async t => {
   const { bootstrap, facadeServices, commonPrivateArgs } = await commonSetup(t);
 
   const { zcf } = await setupZCFTest();
@@ -77,6 +78,53 @@ test('chain info', async t => {
 
   const result = (await handle()) as Chain<any>;
   t.deepEqual(await vt.when(result.getChainInfo()), mockChainInfo);
+});
+
+test.serial('faulty chain info', async t => {
+  const { facadeServices, commonPrivateArgs } = await commonSetup(t);
+
+  // XXX relax again so setupZCFTest can run. This is also why the tests are serial.
+  reincarnate({ relaxDurabilityRules: true });
+  const { zcf } = await setupZCFTest();
+
+  // After setupZCFTest because this disables relaxDurabilityRules
+  // which breaks Zoe test setup's fakeVatAdmin
+  const zone = provideDurableZone('test');
+  const vt = prepareSwingsetVowTools(zone);
+
+  const orchKit = provideOrchestration(
+    zcf,
+    zone.mapStore('test'),
+    {
+      agoricNames: facadeServices.agoricNames,
+      timerService: facadeServices.timerService,
+      storageNode: commonPrivateArgs.storageNode,
+      orchestrationService: facadeServices.orchestrationService,
+      localchain: facadeServices.localchain,
+    },
+    commonPrivateArgs.marshaller,
+  );
+
+  const { chainHub, orchestrate } = orchKit;
+
+  const { stakingTokens, ...sansStakingTokens } = mockChainInfo;
+
+  chainHub.registerChain('mock', sansStakingTokens);
+  chainHub.registerConnection(
+    'agoric-3',
+    mockChainInfo.chainId,
+    mockChainConnection,
+  );
+
+  const handle = orchestrate('mock', {}, async orc => {
+    const chain = await orc.getChain('mock');
+    const account = await chain.makeAccount();
+    return account;
+  });
+
+  await t.throwsAsync(handle(), {
+    message: 'chain info lacks staking denom',
+  });
 });
 
 test.todo('contract upgrade');
