9fc41b85902273b5bed377237b36316c7a762a7e
Turadg Aleahmad <turadg@agoric.com>
2024-07-19 15:04:10 -0700

    BREAKING CHANGE: promote /vatsafe to top module

--- packages/cosmic-proto/test/vatsafe.test.js
+++ packages/cosmic-proto/test/vatsafe.test.js
@@ -1,30 +1,8 @@
 // @ts-check
 /* eslint-disable import/no-extraneous-dependencies -- requiring the package itself to check exports map */
 import test from '@endo/ses-ava/prepare-endo.js';
-import { cosmos } from '@agoric/cosmic-proto';
 
 import { MsgDelegate } from '@agoric/cosmic-proto/cosmos/staking/v1beta1/tx.js';
-
-// @ts-expect-error
-cosmos.staking.v1beta1.MsgDelegate.missing;
-// @ts-expect-error
-MsgDelegate.missing;
-
-test('imports', t => {
-  t.is(MsgDelegate, cosmos.staking.v1beta1.MsgDelegate);
-
-  t.deepEqual(cosmos.staking.v1beta1.MsgDelegate.fromPartial({}), {
-    amount: undefined,
-    delegatorAddress: '',
-    validatorAddress: '',
-  });
-
-  t.deepEqual(MsgDelegate.fromPartial({}), {
-    amount: undefined,
-    delegatorAddress: '',
-    validatorAddress: '',
-  });
-});
 
 test('proto encoding', t => {
   const contents = {
