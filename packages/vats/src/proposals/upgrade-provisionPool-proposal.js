c69cc3c88d284f5f113feac5c818f9d43a9515b8
Kris Kowal <kris@agoric.com>
2024-07-22 15:07:24 -0700

    WIP TODO deeplyFulfilled type in inter-protocol

--- packages/vats/src/proposals/upgrade-provisionPool-proposal.js
+++ packages/vats/src/proposals/upgrade-provisionPool-proposal.js
@@ -32,7 +32,8 @@ export const upgradeProvisionPool = async (
   const { adminFacet, instance } = provisionPoolStartResult;
 
   const [originalPrivateArgs, poserInvitation] = await Promise.all([
-    // @ts-expect-error
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore Local tsc sees this as an error but typedoc does not
     deeplyFulfilled(instancePrivateArgs.get(instance)),
     E(electorateCreatorFacet).getPoserInvitation(),
   ]);
