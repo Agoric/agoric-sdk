0890182356db2afd5fcea2edbfe1b8336ddc703c
Dan Connolly <connolly@agoric.com>
2024-07-31 09:45:46 -0500

    chore(swapExample): complete makeOsmosisSwap
     - pass orch to makeOsmosisSwap
     - parameterize brands.Out

--- packages/orchestration/src/examples/swapExample.contract.js
+++ packages/orchestration/src/examples/swapExample.contract.js
@@ -1,15 +1,13 @@
 import { StorageNodeShape } from '@agoric/internal';
 import { TimerServiceShape } from '@agoric/time';
 import { M } from '@endo/patterns';
+import { assertIssuerKeywords } from '@agoric/zoe/src/contractSupport/zoeHelpers.js';
+import { E } from '@endo/far';
 import { withOrchestration } from '../utils/start-helper.js';
 import * as flows from './swapExample.flows.js';
+import fetchedChainInfo from '../fetched-chain-info.js';
 
 /**
- * @import {TimerService} from '@agoric/time';
- * @import {LocalChain} from '@agoric/vats/src/localchain.js';
- * @import {Remote} from '@agoric/internal';
- * @import {CosmosInterchainService} from '../exos/cosmos-interchain-service.js';
- * @import {NameHub} from '@agoric/vats';
  * @import {Zone} from '@agoric/zone';
  * @import {OrchestrationTools} from '../utils/start-helper.js';
  */
@@ -40,13 +38,8 @@ export const makeNatAmountShape = (brand, min) =>
 /**
  * Orchestration contract to be wrapped by withOrchestration for Zoe
  *
- * @param {ZCF} zcf
- * @param {{
- *   agoricNames: Remote<NameHub>;
- *   localchain: Remote<LocalChain>;
- *   orchestrationService: Remote<CosmosInterchainService>;
- *   storageNode: Remote<StorageNode>;
- *   timerService: Remote<TimerService>;
+ * @param {ZCF<{ baseDenom: string; baseName: string }>} zcf
+ * @param {import('../utils/start-helper.js').OrchestrationPowers & {
  *   marshaller: Marshaller;
  * }} privateArgs
  * @param {Zone} zone
@@ -56,13 +49,28 @@ const contract = async (
   zcf,
   privateArgs,
   zone,
-  { zoeTools, orchestrateAll },
+  { zoeTools, orchestrateAll, chainHub },
 ) => {
-  const { brands } = zcf.getTerms();
+  assertIssuerKeywords(zcf, ['Stable', 'Out']);
+  const { brands, baseDenom, baseName } = zcf.getTerms();
 
   const handlers = orchestrateAll(flows, {
     zcf,
     localTransfer: zoeTools.localTransfer,
+    brandOut: brands.Out,
+  });
+
+  const assets = await E(
+    E(privateArgs.agoricNames).lookup('vbankAsset'),
+  ).values();
+  const info = assets.find(a => a.brand === brands.Out);
+  for (const chainName of ['agoric', 'omniflixhub']) {
+    chainHub.registerChain(chainName, fetchedChainInfo[chainName]);
+  }
+  chainHub.registerAsset(info.denom, {
+    baseDenom,
+    baseName,
+    chainName: 'agoric',
   });
 
   const publicFacet = zone.exo('publicFacet', undefined, {
--- packages/orchestration/src/examples/swapExample.flows.js
+++ packages/orchestration/src/examples/swapExample.flows.js
@@ -4,12 +4,12 @@ import { orcUtils } from '../utils/orc.js';
  * @import {LocalTransfer} from '../utils/zoe-tools.js';
  * @import {Orchestrator, CosmosValidatorAddress, OrchestrationFlow} from '../types.js'
  */
-
 /**
  * @satisfies {OrchestrationFlow}
  * @param {Orchestrator} orch
  * @param {object} ctx
  * @param {LocalTransfer} ctx.localTransfer
+ * @param {Brand} ctx.brandOut
  * @param {ZCFSeat} seat
  * @param {object} offerArgs
  * @param {Amount<'nat'>} offerArgs.staked
@@ -17,7 +17,7 @@ import { orcUtils } from '../utils/orc.js';
  */
 export const swapAndStake = async (
   orch,
-  { localTransfer },
+  { localTransfer, brandOut },
   seat,
   offerArgs,
 ) => {
@@ -38,13 +38,16 @@ export const swapAndStake = async (
   seat.exit();
 
   // build swap instructions with orcUtils library
-  const transferMsg = orcUtils.makeOsmosisSwap({
-    destChain: 'omniflixhub',
-    destAddress: omniAddress,
-    amountIn: give.Stable,
-    brandOut: /** @type {any} */ ('FIXME'),
-    slippage: 0.03,
-  });
+  const transferMsg = await orcUtils.makeOsmosisSwap(
+    {
+      destChain: 'omniflixhub',
+      destAddress: omniAddress,
+      amountIn: give.Stable,
+      brandOut,
+      slippage: 0.03,
+    },
+    orch,
+  );
 
   try {
     await localAccount.transferSteps(give.Stable, transferMsg);
