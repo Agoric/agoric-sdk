69b8234e2cd8c21cb95467e81034ae747cd4a8db
Mudassir Shabbir <mudassir@agoric.com>
2024-08-05 21:35:45 +0500

    chore: stakeBld partially updated with withOrchestration

--- packages/orchestration/src/examples/stakeBld.contract.js
+++ packages/orchestration/src/examples/stakeBld.contract.js
@@ -1,54 +1,81 @@
 /**
  * @file Stake BLD contract
  */
-import { makeTracer } from '@agoric/internal';
-import { prepareRecorderKitMakers } from '@agoric/zoe/src/contractSupport/recorder.js';
-import { withdrawFromSeat } from '@agoric/zoe/src/contractSupport/zoeHelpers.js';
-import { InvitationShape } from '@agoric/zoe/src/typeGuards.js';
-import { makeDurableZone } from '@agoric/zone/durable.js';
-import { prepareVowTools, heapVowE as E } from '@agoric/vow/vat.js';
-import { deeplyFulfilled } from '@endo/marshal';
-import { M } from '@endo/patterns';
-import { prepareLocalOrchestrationAccountKit } from '../exos/local-orchestration-account.js';
-import { makeChainHub } from '../exos/chain-hub.js';
+import { makeTracer } from "@agoric/internal";
+import { prepareRecorderKitMakers } from "@agoric/zoe/src/contractSupport/recorder.js";
+import { withdrawFromSeat } from "@agoric/zoe/src/contractSupport/zoeHelpers.js";
+import { InvitationShape } from "@agoric/zoe/src/typeGuards.js";
+import { makeDurableZone } from "@agoric/zone/durable.js";
+import { prepareVowTools, heapVowE as E } from "@agoric/vow/vat.js";
+import { deeplyFulfilled } from "@endo/marshal";
+import { M } from "@endo/patterns";
+import { prepareLocalOrchestrationAccountKit } from "../exos/local-orchestration-account.js";
+import { makeChainHub } from "../exos/chain-hub.js";
+
+import { withOrchestration } from "../utils/start-helper.js";
 
 /**
- * @import {NameHub} from '@agoric/vats';
- * @import {Remote} from '@agoric/internal';
+ * @import {GuestOf} from '@agoric/async-flow';
+ * @import {Orchestrator, LocalAccountMethods, OrchestrationAccountI, OrchestrationFlow} from '../types.js';
  * @import {TimerService} from '@agoric/time';
  * @import {LocalChain} from '@agoric/vats/src/localchain.js';
+ * @import {NameHub} from '@agoric/vats';
+ * @import {Remote, Vow} from '@agoric/vow';
+ * @import {Zone} from '@agoric/zone';
+ * @import {VBankAssetDetail} from '@agoric/vats/tools/board-utils.js';
+ * @import {CosmosInterchainService} from '../exos/cosmos-interchain-service.js';
+ * @import {OrchestrationTools} from '../utils/start-helper.js';
  */
-
-const trace = makeTracer('StakeBld');
 
 /**
- * @param {ZCF} zcf
- * @param {{
- *   agoricNames: Remote<NameHub>;
+ * @typedef {{
  *   localchain: Remote<LocalChain>;
- *   marshaller: Marshaller;
- *   storageNode: StorageNode;
- *   timerService: TimerService;
- * }} privateArgs
- * @param {import('@agoric/vat-data').Baggage} baggage
+ *   orchestrationService: Remote<CosmosInterchainService>;
+ *   storageNode: Remote<StorageNode>;
+ *   timerService: Remote<TimerService>;
+ *   agoricNames: Remote<NameHub>;
+ * }} OrchestrationPowers
  */
-export const start = async (zcf, privateArgs, baggage) => {
-  const zone = makeDurableZone(baggage);
 
-  const { makeRecorderKit } = prepareRecorderKitMakers(
-    baggage,
-    privateArgs.marshaller,
-  );
-  const vowTools = prepareVowTools(zone.subZone('vows'));
+const trace = makeTracer("StakeBld");
+/**
+ *
+ * @satisfies {OrchestrationFlow}
+ * @param {Orchestrator} orch
+ * @param {object} ctx
+ * @param { ZCF } ctx.zcf
+ * @param {ZCFSeat} seat
+ * @param {Object} offerArgs
+ */
 
-  const makeLocalOrchestrationAccountKit = prepareLocalOrchestrationAccountKit(
-    zone,
-    makeRecorderKit,
-    zcf,
-    privateArgs.timerService,
-    vowTools,
-    makeChainHub(privateArgs.agoricNames, vowTools),
-  );
+const flow1 = async (orch, { zcf }, seat, offerArgs) => {
+  const { give } = seat.getProposal();
+  trace("makeStakeBldInvitation", give);
+  const holder = await orch.makeLocalAccount();
+  const { In } = await deeplyFulfilled(withdrawFromSeat(zcf, seat, give));
+  await E(holder).deposit(In);
+  seat.exit();
+  return holder.asContinuingOffer();
+};
+
+/**
+ * Orchestration contract to be wrapped by withOrchestration for Zoe
+ *
+ * @param {ZCF} zcf
+ * @param {OrchestrationPowers & {
+ *   marshaller: Marshaller;
+ * }} privateArgs
+ * @param {Zone} zone
+ * @param {OrchestrationTools} tools
+ */
+export const contract = async (
+  zcf,
+  privateArgs,
+  zone,
+  { chainHub, orchestrate, vowTools, zoeTools }
+) => {
+  
+  const handle = orchestrate("flow1", { zcf }, flow1);
 
   // ----------------
   // All `prepare*` calls should go above this line.
@@ -56,25 +83,10 @@ export const start = async (zcf, privateArgs, baggage) => {
   const BLD = zcf.getTerms().brands.In;
   const bldAmountShape = await E(BLD).getAmountShape();
 
-  async function makeLocalAccountKit() {
-    const account = await E(privateArgs.localchain).makeAccount();
-    const address = await E(account).getAddress();
-    // FIXME 'address' is implied by 'account'; use an async maker that get the value itself
-    return makeLocalOrchestrationAccountKit({
-      account,
-      address: harden({
-        value: address,
-        encoding: 'bech32',
-        chainId: 'local',
-      }),
-      storageNode: privateArgs.storageNode,
-    });
-  }
-
   const publicFacet = zone.exo(
-    'StakeBld',
-    M.interface('StakeBldI', {
-      makeAccount: M.callWhen().returns(M.remotable('LocalChainAccountHolder')),
+    "StakeBld",
+    M.interface("StakeBldI", {
+      makeAccount: M.callWhen().returns(M.remotable("LocalChainAccountHolder")),
       makeAccountInvitationMaker: M.callWhen().returns(InvitationShape),
       makeStakeBldInvitation: M.callWhen().returns(InvitationShape),
     }),
@@ -84,26 +96,16 @@ export const start = async (zcf, privateArgs, baggage) => {
        */
       makeStakeBldInvitation() {
         return zcf.makeInvitation(
-          async seat => {
-            const { give } = seat.getProposal();
-            trace('makeStakeBldInvitation', give);
-            const { holder } = await makeLocalAccountKit();
-            const { In } = await deeplyFulfilled(
-              withdrawFromSeat(zcf, seat, give),
-            );
-            await E(holder).deposit(In);
-            seat.exit();
-            return holder.asContinuingOffer();
-          },
-          'wantStake',
+          handle,
+          "wantStake",
           undefined,
           M.splitRecord({
             give: { In: bldAmountShape },
-          }),
+          })
         );
       },
       async makeAccount() {
-        trace('makeAccount');
+        trace("makeAccount");
         const { holder } = await makeLocalAccountKit();
         return holder;
       },
@@ -111,15 +113,17 @@ export const start = async (zcf, privateArgs, baggage) => {
        * Invitation to make an account, without any funds
        */
       makeAccountInvitationMaker() {
-        trace('makeCreateAccountInvitation');
-        return zcf.makeInvitation(async seat => {
+        trace("makeCreateAccountInvitation");
+        return zcf.makeInvitation(async (seat) => {
           seat.exit();
           const { holder } = await makeLocalAccountKit();
           return holder.asContinuingOffer();
-        }, 'wantLocalChainAccount');
+        }, "wantLocalChainAccount");
       },
-    },
+    }
   );
 
   return { publicFacet };
 };
+
+export const start = withOrchestration(contract);
