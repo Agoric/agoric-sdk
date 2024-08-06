e8e92cffa01a1735fa1a5546c8f2d972296af2ad
rabi-siddique <rabisiddique27@gmail.com>
2024-08-06 16:08:08 +0500

    ci: ignore yarn.lock file when running with master

--- .github/actions/restore-node/check-git-status.sh
+++ .github/actions/restore-node/check-git-status.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
+
 # Set verbose execution
 set -x
+
 # Navigate to the specified directory
 cd "$1" || exit $?
-# Check for unexpected changes
-# Fail if git status detects changes
+
+# Get the value of IGNORE_ENDO_BRANCH
+ignore_endo_branch=$2 || exit $?
+
+# Get the status of the repository
 changes=$(git status --porcelain)
+
+if [ "$ignore_endo_branch" = false ]; then
+  # Remove yarn.lock from the changes list
+  changes=$(echo "$changes" | grep -v " yarn.lock")
+fi
+
 if [ -n "$changes" ]; then
   git status
   echo "Unexpected dirty git status in Agoric SDK path"
   exit 1
 fi
+
+echo "Git status is clean or only yarn.lock is changed on master branch"
