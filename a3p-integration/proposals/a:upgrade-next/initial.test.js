4402c5c998fa68bd7cf7f305fbb81eb90b8eec97
Chris Hibbert <hibbert@agoric.com>
2024-08-16 09:54:19 -0700

    chore: update incarnation numbers for Zoe vat

--- a3p-integration/proposals/a:upgrade-next/initial.test.js
+++ a3p-integration/proposals/a:upgrade-next/initial.test.js
@@ -7,7 +7,7 @@ const vats = {
   ibc: { incarnation: 0 },
   localchain: { incarnation: 0 },
   walletFactory: { incarnation: 3 },
-  zoe: { incarnation: 2 },
+  zoe: { incarnation: 3 },
 };
 
 test(`vat details`, async t => {
