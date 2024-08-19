5a1c729286dbabf2c7449a6beeb28be6df100bea
Chris Hibbert <hibbert@agoric.com>
2024-08-19 10:17:38 -0700

    docs: clarify comments in test.sh

--- a3p-integration/proposals/a:vaults-auctions/test.sh
+++ a3p-integration/proposals/a:vaults-auctions/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Place here any test that should be executed using the executed proposal.
+# Place here any test that should be run using the executed proposal.
 # The effects of this step are not persisted in further proposal layers.
 
-# test more, in ways that change system state
 yarn ava ./*.test.js
