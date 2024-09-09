a7e19175458b807e53775ca678e02622d43ae275
Turadg Aleahmad <turadg@agoric.com>
2024-09-09 11:32:09 -0700

    docs: OrchestrationFlow

--- packages/orchestration/src/orchestration-api.ts
+++ packages/orchestration/src/orchestration-api.ts
@@ -234,6 +234,26 @@ export interface OrchestrationAccountI {
   getPublicTopics: () => Promise<Record<string, ResolvedPublicTopic<unknown>>>;
 }
 
+/**
+ * Flows to orchestrate are regular Javascript functions but have some
+ * constraints to fulfill the requirements of resumability after termination of
+ * the enclosing vat. Some requirements for each orchestration flow:
+ * - must not close over any values that could change between invocations
+ * - must satisfy the `OrchestrationFlow` interface
+ * - must be hardened
+ * - must not use `E()` (eventual send)
+ *
+ * The call to `orchestrate` using a flow function in reincarnations of the vat
+ * must have the same `durableName` as before. To help enforce these
+ * constraints, we recommend:
+ *
+ * - keeping flows in a `.flows.js` module
+ * - importing them all with `import * as flows` to get a single object keyed by
+ *   the export name
+ * - using `orchestrateAll` to treat each export name as the `durableName` of
+ *   the flow
+ * - adopting `@agoric/eslint-config` that has rules to help detect problems
+ */
 export interface OrchestrationFlow<CT = unknown> {
   (orc: Orchestrator, ctx: CT, ...args: Passable[]): Promise<unknown>;
 }
