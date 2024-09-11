5f50d9208ccf6fac95c70a7f7eb565816175fbab
0xPatrick <patrick@0xpatrick.dev>
2024-09-11 12:34:16 -0400

    feat: use orchFns.autoStake as target for receiveUpcall
    - uses an async-flow function as the handler for a `.monitorTransfers()` tap
    - allows guest develoeprs to write handlers as flows instead of with vows and watchers

--- packages/orchestration/src/examples/auto-stake-it.contract.js
+++ packages/orchestration/src/examples/auto-stake-it.contract.js
@@ -2,19 +2,56 @@ import {
   EmptyProposalShape,
   InvitationShape,
 } from '@agoric/zoe/src/typeGuards.js';
+import { makeTracer } from '@agoric/internal';
 import { M } from '@endo/patterns';
 import { prepareChainHubAdmin } from '../exos/chain-hub-admin.js';
 import { preparePortfolioHolder } from '../exos/portfolio-holder-kit.js';
 import { withOrchestration } from '../utils/start-helper.js';
-import { prepareStakingTap } from './auto-stake-it-tap-kit.js';
 import * as flows from './auto-stake-it.flows.js';
 import { registerChainsAndAssets } from '../utils/chain-hub-helper.js';
+import { ChainAddressShape } from '../typeGuards.js';
+
+const trace = makeTracer('AutoStakeIt');
 
 /**
+ * @import {GuestInterface} from '@agoric/async-flow';
  * @import {Zone} from '@agoric/zone';
+ * @import {IBCChannelID, VTransferIBCEvent} from '@agoric/vats';
+ * @import {TargetApp} from '@agoric/vats/src/bridge-target.js';
+ * @import {ChainAddress, CosmosValidatorAddress, Denom, CosmosChainInfo, DenomDetail} from '@agoric/orchestration';
+ * @import {Passable} from '@endo/marshal';
+ * @import {CosmosOrchestrationAccount} from '../exos/cosmos-orchestration-account.js';
+ * @import {LocalOrchestrationAccount} from '../exos/local-orchestration-account.js';
  * @import {OrchestrationPowers, OrchestrationTools} from '../utils/start-helper.js';
- * @import {CosmosChainInfo, Denom, DenomDetail} from '../types.js';
  */
+
+/**
+ * @typedef {{
+ *   stakingAccount: GuestInterface<CosmosOrchestrationAccount>;
+ *   localAccount: GuestInterface<LocalOrchestrationAccount>;
+ *   config: {
+ *     validator: CosmosValidatorAddress;
+ *     localChainAddress: ChainAddress;
+ *     remoteChainAddress: ChainAddress;
+ *     sourceChannel: IBCChannelID;
+ *     remoteDenom: Denom;
+ *     localDenom: Denom;
+ *   };
+ * }} StakingTapState
+ */
+
+const StakingTapStateShape = harden({
+  stakingAccount: M.remotable('CosmosOrchestrationAccount'),
+  localAccount: M.remotable('LocalOrchestrationAccount'),
+  config: {
+    validator: ChainAddressShape,
+    localChainAddress: ChainAddressShape,
+    remoteChainAddress: ChainAddressShape,
+    sourceChannel: M.string(),
+    remoteDenom: M.string(),
+    localDenom: M.string(),
+  },
+});
 
 /**
  * AutoStakeIt allows users to to create an auto-forwarding address that
@@ -37,16 +74,42 @@ const contract = async (
   zone,
   { chainHub, orchestrateAll, vowTools },
 ) => {
-  const makeStakingTap = prepareStakingTap(
-    zone.subZone('stakingTap'),
-    vowTools,
-  );
   const makePortfolioHolder = preparePortfolioHolder(
     zone.subZone('portfolio'),
     vowTools,
   );
 
-  const { makeAccounts } = orchestrateAll(flows, {
+  /**
+   * Provides a {@link TargetApp} that reacts to an incoming IBC transfer.
+   */
+  const makeStakingTap = zone.exoClass(
+    'StakingTap',
+    M.interface('AutoStakeItTap', {
+      receiveUpcall: M.call(M.record()).returns(M.undefined()),
+    }),
+    /** @param {StakingTapState} initialState */
+    initialState => harden(initialState),
+    {
+      /**
+       * Transfers from localAccount to stakingAccount, then delegates from the
+       * stakingAccount to `validator` if the expected token (remoteDenom) is
+       * received.
+       *
+       * @param {VTransferIBCEvent & Passable} event
+       */
+      receiveUpcall(event) {
+        trace('receiveUpcall', event);
+        const { localAccount, stakingAccount, config } = this.state;
+
+        orchFns.autoStake(localAccount, stakingAccount, config, event);
+      },
+    },
+    {
+      stateShape: StakingTapStateShape,
+    },
+  );
+
+  const orchFns = orchestrateAll(flows, {
     makeStakingTap,
     makePortfolioHolder,
     chainHub,
@@ -60,7 +123,7 @@ const contract = async (
     {
       makeAccountsInvitation() {
         return zcf.makeInvitation(
-          makeAccounts,
+          orchFns.makeAccounts,
           'Make Accounts',
           undefined,
           EmptyProposalShape,
