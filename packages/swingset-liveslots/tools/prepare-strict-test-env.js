6c15cc99d1c8084b2d4e88e757c78dbd9c8497bd
Mathieu Hofman <mathieu@agoric.com>
2024-09-21 00:59:44 +0000

    feat(liveslots-tools): return incarnation in test tools

--- packages/swingset-liveslots/tools/prepare-strict-test-env.js
+++ packages/swingset-liveslots/tools/prepare-strict-test-env.js
@@ -10,48 +10,68 @@
 import '@agoric/internal/src/install-ses-debug.js';
 
 // eslint-disable-next-line import/order
-import { reincarnate } from './setup-vat-data.js';
+import { reincarnate, flushIncarnation } from './setup-vat-data.js';
 
 import { makePromiseKit } from '@endo/promise-kit';
 import { Fail } from '@endo/errors';
 import { eventLoopIteration } from '@agoric/internal/src/testing-utils.js';
 import { makeUpgradeDisconnection } from '@agoric/internal/src/upgrade-api.js';
 
+export { flushIncarnation };
 export { eventLoopIteration as nextCrank };
 
-/** @type {ReturnType<typeof reincarnate>} */
+/**
+ * @import { ReincarnateOptions } from './setup-vat-data.js'
+ */
+
+/** @type {ReincarnateOptions} */
 let incarnation;
 let incarnationNumber = 0;
 
-export const annihilate = () => {
-  incarnation = reincarnate({ relaxDurabilityRules: false });
+/** @param {Omit<ReincarnateOptions, 'fakeVomKit' | 'fakeStore'>} [options] */
+export const annihilate = (options = {}) => {
+  // @ts-expect-error fakeStore and fakeVomKit don't exist on type, but drop them if they do at runtime
+  const { fakeStore: _fs, fakeVomKit: _fvk, ...opts } = options;
+  incarnation = reincarnate({ relaxDurabilityRules: false, ...opts });
   incarnationNumber = 0;
+  return incarnation;
 };
 
 export const getBaggage = () => {
   return incarnation.fakeVomKit.cm.provideBaggage();
 };
 
-export const nextLife = () => {
-  incarnation = reincarnate(incarnation);
+/**
+ * @param {ReincarnateOptions} [fromIncarnation]
+ */
+export const nextLife = (fromIncarnation = incarnation) => {
+  incarnation = reincarnate(fromIncarnation);
   incarnationNumber += 1;
+  return incarnation;
 };
 
 /**
  * @template {(baggage: import('@agoric/swingset-liveslots').Baggage) => Promise<any> | any} B
  * @param {B} build
  * @param {(tools: Awaited<ReturnType<B>>) => Promise<void> | void} [run]
+ * @param {object} [options]
+ * @param {ReincarnateOptions} [options.fromIncarnation]
+ * @param {boolean} [options.cleanStart]
  */
-export const startLife = async (build, run) => {
+export const startLife = async (
+  build,
+  run,
+  { fromIncarnation, cleanStart } = {},
+) => {
   await eventLoopIteration();
+  if (cleanStart) annihilate();
   const oldIncarnationNumber = incarnationNumber;
   const oldIncarnation = incarnation;
   const disconnectionObject = makeUpgradeDisconnection(
     'vat upgraded',
     oldIncarnationNumber,
   );
-  nextLife();
-  const { fakeVomKit } = incarnation;
+  const { fakeVomKit } = nextLife(fromIncarnation);
   /** @type {Map<string, import('@endo/promise-kit').PromiseKit<any>>} */
   const previouslyWatchedPromises = new Map();
   let buildTools;
@@ -93,6 +113,8 @@ export const startLife = async (build, run) => {
     await run(buildTools);
     await eventLoopIteration();
   }
+
+  return incarnation;
 };
 
 // Setup the initial incarnation
