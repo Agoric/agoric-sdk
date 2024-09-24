99767e251f570538ddd4f75cd27c17bab28ae403
Siarhei Liakh <siarhei@agoric.com>
2024-09-24 13:26:36 +0000

    fix(xsnap): refactor, add more tests to xsnap-eof

--- packages/xsnap/test/xsnap-eof.test.js
+++ packages/xsnap/test/xsnap-eof.test.js
@@ -5,11 +5,9 @@ import fs from 'fs';
 import { fileURLToPath, URL } from 'url';
 import { tmpName } from 'tmp';
 import { text } from 'stream/consumers';
-import { makeNetstringWriter } from '@endo/netstring';
-import { makeNodeWriter } from '@endo/stream-node';
 import { makePromiseKit } from '@endo/promise-kit';
 import { options } from './message-tools.js';
-import { xsnap, QUERY_RESPONSE_BUF } from '../src/xsnap.js';
+import { xsnap } from '../src/xsnap.js';
 import { ExitCode } from '../api.js';
 
 /**
@@ -20,9 +18,9 @@ import { ExitCode } from '../api.js';
  * @typedef {Omit<import("child_process").ChildProcess, 'stdio'> &
  *   {
  *     stdin: null;
- *     stdout: null;
+ *     stdout: Readable;
  *     stderr: Readable;
- *     readonly stdio: [null, null, Readable, Duplex, Duplex, undefined, undefined, Duplex, Duplex];
+ *     readonly stdio: [null, Readable, Readable, Duplex, Duplex, undefined, undefined, Duplex, Duplex];
  *   }
  * } XsnapChildProcess
  */
@@ -81,7 +79,12 @@ async function spawnReflectiveWorker(handleCommand) {
   };
 
   const io = { spawn: spawnSpy, os: os.type(), fs, tmpName };
-  const vat = await xsnap({ ...options(io), handleCommand, stderr: 'pipe' });
+  const vat = await xsnap({
+    ...options(io),
+    handleCommand,
+    stdout: 'pipe',
+    stderr: 'pipe',
+  });
   await vat.evaluate(
     'function handleCommand(message) { issueCommand(new Uint8Array().buffer); };',
   );
@@ -91,6 +94,7 @@ async function spawnReflectiveWorker(handleCommand) {
   const toXsnap = xsnapProcess.stdio[3];
   const fromXsnap = xsnapProcess.stdio[4];
   const stderrP = text(xsnapProcess.stderr);
+  const stdoutP = text(xsnapProcess.stdout);
 
   xsnapProcess.on('error', err => {
     exitedPKit.reject(err);
@@ -100,100 +104,187 @@ async function spawnReflectiveWorker(handleCommand) {
     exitedPKit.resolve({ code, signal });
   });
 
-  return { vat, xsnapProcess, toXsnap, fromXsnap, stderrP, exitedPKit };
+  return {
+    vat,
+    xsnapProcess,
+    toXsnap,
+    fromXsnap,
+    stdoutP,
+    stderrP,
+    exitedP: exitedPKit.promise,
+  };
 }
 
-async function issueCommandAndWait(worker) {
-  let issueCommandError;
-  let vatCloseError;
-  const { vat, stderrP, exitedPKit } = worker;
+async function issueCommandAndWait(worker, beforeWait, afterWait) {
+  let beforeWaitError;
+  let afterWaitError;
+  const { stdoutP, stderrP, exitedP } = worker;
 
   try {
-    // this will error out since we are intentionally
+    // this may error out since we are intentionally
     // breaking communications with the worker
-    await vat.issueStringCommand('0');
+    await beforeWait(worker); // vat.issueStringCommand('0');
   } catch (err) {
-    issueCommandError = err;
+    beforeWaitError = err;
   }
 
   // wait for the worker to exit
-  const [strErr, { code: vatExitCode, signal: vatExitSignal }] =
-    await Promise.all([stderrP, exitedPKit.promise]);
+  const [strOut, strErr, { code: vatExitCode, signal: vatExitSignal }] =
+    await Promise.all([stdoutP, stderrP, exitedP]);
 
   try {
-    // this will error out since vat has died.
-    await vat.close();
+    // this may error out since vat has died.
+    await afterWait(worker); // vat.close();
   } catch (err) {
-    vatCloseError = err;
+    afterWaitError = err;
   }
   return {
-    issueCommandError,
-    vatCloseError,
+    beforeWaitError,
+    afterWaitError,
+    strOut,
     strErr,
     vatExitCode,
     vatExitSignal,
   };
 }
 
-const testInterruption = test.macro(async (t, onRequest, expectedStderr) => {
-  const handleCommand = message => {
-    // @ts-expect-error onRequest is untyped
-    // eslint-disable-next-line no-use-before-define
-    return onRequest(worker, message);
-  };
-  const worker = await spawnReflectiveWorker(handleCommand);
+async function issueCommandZero(worker) {
+  await worker.vat.issueCommand('0');
+}
 
+async function closeVat(worker) {
+  await worker.vat.close();
+}
+
+async function verifyStdError(t, results, expectedStderr) {
   const {
-    issueCommandError,
-    vatCloseError,
+    beforeWaitError,
+    afterWaitError,
+    strOut,
     strErr,
     vatExitCode,
     vatExitSignal,
-  } = await issueCommandAndWait(worker);
+  } = results;
 
-  t.not(issueCommandError, undefined, 'issueCommand() must produce an error');
-  t.not(vatCloseError, 'vat.close() must produce an error');
+  t.not(beforeWaitError, undefined, 'issueCommand() must produce an error');
+  t.not(afterWaitError, undefined, 'vat.close() must produce an error');
   t.is(vatExitSignal, null, 'vat exit signal must be null');
   t.is(
     vatExitCode,
     ExitCode.E_IO_ERROR,
     'vat exit code must indicate an IO error',
   );
+  t.is(strOut, '', 'stdout must be empty');
   t.is(strErr, expectedStderr, 'stderr must indicate expected error');
-});
+}
+
+const testInterruption = test.macro(
+  async (t, onRequest, beforeWait, afterWait, verifyResults) => {
+    const handleCommand = message => {
+      // @ts-expect-error onRequest is untyped
+      // eslint-disable-next-line no-use-before-define
+      return onRequest(worker, message);
+    };
+    const worker = await spawnReflectiveWorker(handleCommand);
+    const results = await issueCommandAndWait(worker, beforeWait, afterWait);
+    // @ts-expect-error verifyResults is untyped
+    await verifyResults(t, results);
+  },
+);
 
 test(
   'xsnap-worker complains while trying to READ an answer when pipes are closed',
   testInterruption,
   async function onRequest(worker, _message) {
-    // the worker is blocked on read here, so we should close "fromXsnap" pipe first
-    worker.fromXsnap.end();
-    worker.fromXsnap.destroy();
+    // the worker is blocked on read here, so closing "toXsnap" pipe
+    // should cause an immediate read error on worker side.
     worker.toXsnap.end();
     worker.toXsnap.destroy();
     return new Uint8Array();
   },
-  'Got EOF on netstring read. Has parent died?\n',
+  issueCommandZero,
+  closeVat,
+  async function verifyResults(t, results) {
+    await verifyStdError(
+      t,
+      results,
+      'Got EOF on netstring read. Has parent died?\n',
+    );
+  },
 );
 
 test(
   'xsnap-worker complains while trying to WRITE an answer when pipes are closed',
   testInterruption,
   async function onRequest(worker, _message) {
-    // the worker is blocked on read here, so we should close "fromXsnap" pipe first
+    // The worker is blocked on read here, so closing "fromXsnap" pipe
+    // does not cause an immediate exit. However, an attempt to send an
+    // answer back to us will cause a write error with a SIGPIPE.
     worker.fromXsnap.end();
     worker.fromXsnap.destroy();
-    // write a fake but valid response into a pipe buffer,
-    // forcing the worker to move to the next state where it would
-    // attempt to write out return value from its own "handleCommand()"
-    // into a pipe which we've just closed above.
-    await makeNetstringWriter(makeNodeWriter(worker.toXsnap)).next([
-      QUERY_RESPONSE_BUF,
-      new Uint8Array(),
-    ]);
-    worker.toXsnap.end();
-    worker.toXsnap.destroy();
     return new Uint8Array();
   },
-  'Caught SIGPIPE. Has parent died?\n',
+  issueCommandZero,
+  closeVat,
+  async function verifyResults(t, results) {
+    await verifyStdError(t, results, 'Caught SIGPIPE. Has parent died?\n');
+  },
+);
+
+test(
+  'xsnap-worker exits quietly when pipes are closed in quiescent state',
+  testInterruption,
+  async function onRequest(_worker, _message) {
+    return new Uint8Array();
+  },
+  async function beforeWait(worker) {
+    // Simulating an orderly shutdown in quiescent state by closing both pipes.
+    // The worker is blocked on read here, so we should close "fromXsnap" pipe first
+    worker.fromXsnap.end();
+    worker.fromXsnap.destroy();
+    worker.toXsnap.end();
+    worker.toXsnap.destroy();
+  },
+  closeVat,
+  async function verifyResults(t, results) {
+    const {
+      beforeWaitError,
+      afterWaitError,
+      strOut,
+      strErr,
+      vatExitCode,
+      vatExitSignal,
+    } = results;
+
+    t.is(beforeWaitError, undefined, 'worker pipes must close without error');
+    t.is(afterWaitError, undefined, 'vat.close() must not produce an error');
+    t.is(vatExitSignal, null, 'vat exit signal must be null');
+    t.is(
+      vatExitCode,
+      ExitCode.E_SUCCESS,
+      'vat exit code must indicate success',
+    );
+    t.is(strOut, '', 'stdout must be empty');
+    t.is(strErr, '', 'stderr must be empty');
+  },
+);
+
+test(
+  'xsnap-worker complains while trying to WRITE a query when pipes are closed',
+  testInterruption,
+  async function onRequest(_worker, _message) {
+    return new Uint8Array();
+  },
+  async function beforeWait(worker) {
+    // The worker is blocked on read here, so we close "fromXsnap" pipe before
+    // issuing a command. This will trigger worker's handleCommand(), which will
+    // attempt to issueCommand() (query) back to us.
+    worker.fromXsnap.end();
+    worker.fromXsnap.destroy();
+    await issueCommandZero(worker);
+  },
+  closeVat,
+  async function verifyResults(t, results) {
+    await verifyStdError(t, results, 'Caught SIGPIPE. Has parent died?\n');
+  },
 );
