46d1ecf51a1b9910dc8458da74d249d709243767
Jorge-Lopes <jorge.lopes@bytepitch.com>
2024-09-24 19:59:10 +0100

    fix(a3p): replace assertions that were using t.true()
    - Address review comments from #10123
    - Comments:
      - https://github.com/Agoric/agoric-sdk/pull/10123/files#r1772306184
      - https://github.com/Agoric/agoric-sdk/pull/10123/files#r1772310395
      - https://github.com/Agoric/agoric-sdk/pull/10123/files#r1772311673
      - https://github.com/Agoric/agoric-sdk/pull/10123/files#r1772072506

--- a3p-integration/proposals/z:acceptance/vaults.test.js
+++ a3p-integration/proposals/z:acceptance/vaults.test.js
@@ -17,12 +17,14 @@ import {
 test.serial('attempt to open vaults under the minimum amount', async t => {
   const activeVaultsBefore = await agops.vaults('list', '--from', USER1ADDR);
   await bankSend(USER1ADDR, `20000000${ATOM_DENOM}`);
+  t.log('active vaults before:', activeVaultsBefore);
 
   const mint = '3.0';
   const collateral = '5.0';
   await t.throwsAsync(() => openVault(USER1ADDR, mint, collateral));
 
   const activeVaultsAfter = await agops.vaults('list', '--from', USER1ADDR);
+  t.log('active vaults after:', activeVaultsAfter);
 
   t.true(
     activeVaultsAfter.length === activeVaultsBefore.length,
@@ -33,6 +35,8 @@ test.serial('attempt to open vaults under the minimum amount', async t => {
 test.serial('open new vault', async t => {
   const istBalanceBefore = await getISTBalance(USER1ADDR);
   const activeVaultsBefore = await agops.vaults('list', '--from', USER1ADDR);
+  t.log('uist balance before:', istBalanceBefore);
+  t.log('active vaults before:', activeVaultsBefore);
 
   await bankSend(USER1ADDR, `20000000${ATOM_DENOM}`);
 
@@ -43,13 +47,17 @@ test.serial('open new vault', async t => {
 
   const istBalanceAfter = await getISTBalance(USER1ADDR);
   const activeVaultsAfter = await agops.vaults('list', '--from', USER1ADDR);
+  t.log('uist balance after:', istBalanceAfter);
+  t.log('active vaults after:', activeVaultsAfter);
 
-  t.true(
-    istBalanceBefore + 5 === istBalanceAfter,
+  t.is(
+    istBalanceBefore + 5,
+    istBalanceAfter,
     'The IST balance should increase by the minted amount',
   );
-  t.true(
-    activeVaultsAfter.length > activeVaultsBefore.length,
+  t.is(
+    activeVaultsAfter.length,
+    activeVaultsBefore.length + 1,
     `The number of active vaults should increase after opening a new vault.`,
   );
 });
@@ -61,16 +69,19 @@ test.serial('remove collateral', async t => {
 
   let vaultData = await getContractInfo(vaultPath, { agoric, prefix: '' });
   const collateralBefore = vaultData.locked.value;
+  t.log('vault collateral before:', collateralBefore);
 
   await adjustVault(USER1ADDR, vaultID, { wantCollateral: 1.0 });
   await waitForBlock();
 
   vaultData = await getContractInfo(vaultPath, { agoric, prefix: '' });
-  
-  const collateralAfter = vaultData.locked.value;
 
-  t.true(
-    Number(collateralBefore) > Number(collateralAfter),
+  const collateralAfter = vaultData.locked.value;
+  t.log('vault collateral after:', collateralAfter);
+
+  t.is(
+    Number(collateralBefore),
+    Number(collateralAfter) + 1000000,
     'The vault Collateral should decrease after removing some ATOM',
   );
 });
@@ -82,15 +93,18 @@ test.serial('remove IST', async t => {
 
   let vaultData = await getContractInfo(vaultPath, { agoric, prefix: '' });
   const debtBefore = vaultData.debtSnapshot.debt.value;
+  t.log('vault debt before:', debtBefore);
 
   await adjustVault(USER1ADDR, vaultID, { wantMinted: 1.0 });
   await waitForBlock();
 
   vaultData = await getContractInfo(vaultPath, { agoric, prefix: '' });
   const debtAfter = vaultData.debtSnapshot.debt.value;
+  t.log('vault debt after:', debtAfter);
 
-  t.true(
-    Number(debtAfter) > Number(debtBefore),
+  t.is(
+    Number(debtAfter),
+    Number(debtBefore) + 1005000,
     'The vault Debt should increase after removing some IST',
   );
 });
@@ -105,6 +119,7 @@ test.serial('close vault', async t => {
 
   const vaultData = await getContractInfo(vaultPath, { agoric, prefix: '' });
   const vaultState = vaultData.vaultState;
+  t.log('vault state:', vaultState);
 
   t.is(vaultState, 'closed', 'The vault should be in the "closed" state.');
 });
@@ -114,6 +129,7 @@ test.serial('open second vault', async t => {
   await bankSend(user2Address, `20000000${ATOM_DENOM}`);
 
   const activeVaultsBefore = await agops.vaults('list', '--from', user2Address);
+  t.log('active vaults before:', activeVaultsBefore);
 
   const mint = '7.0';
   const collateral = '11.0';
@@ -121,9 +137,11 @@ test.serial('open second vault', async t => {
   await waitForBlock();
 
   const activeVaultsAfter = await agops.vaults('list', '--from', user2Address);
+  t.log('active vaults after:', activeVaultsAfter);
 
-  t.true(
-    activeVaultsAfter.length > activeVaultsBefore.length,
+  t.is(
+    activeVaultsAfter.length,
+    activeVaultsBefore.length + 1,
     `The number of active vaults should increase after opening a new vault.`,
   );
 });
@@ -136,15 +154,18 @@ test.serial('add collateral', async t => {
 
   let vaultData = await getContractInfo(vaultPath, { agoric, prefix: '' });
   const collateralBefore = vaultData.locked.value;
+  t.log('vault collateral before:', collateralBefore);
 
   await adjustVault(user2Address, vaultID, { giveCollateral: 1.0 });
   await waitForBlock();
 
   vaultData = await getContractInfo(vaultPath, { agoric, prefix: '' });
   const collateralAfter = vaultData.locked.value;
+  t.log('vault collateral after:', collateralAfter);
 
-  t.true(
-    Number(collateralBefore) < Number(collateralAfter),
+  t.is(
+    Number(collateralBefore),
+    Number(collateralAfter) - 1000000,
     'The vault Collateral should increase after adding some ATOM',
   );
 });
@@ -157,15 +178,18 @@ test.serial('add IST', async t => {
 
   let vaultData = await getContractInfo(vaultPath, { agoric, prefix: '' });
   const debtBefore = vaultData.debtSnapshot.debt.value;
+  t.log('vault debt before:', debtBefore);
 
   await adjustVault(user2Address, vaultID, { giveMinted: 1.0 });
   await waitForBlock();
 
   vaultData = await getContractInfo(vaultPath, { agoric, prefix: '' });
   const debtAfter = vaultData.debtSnapshot.debt.value;
+  t.log('vault debt after:', debtAfter);
 
-  t.true(
-    Number(debtAfter) < Number(debtBefore),
+  t.is(
+    Number(debtAfter),
+    Number(debtBefore) - 1000000,
     'The vault Debt should decrease after adding some IST',
   );
 });
@@ -181,6 +205,7 @@ test.serial('close second vault', async t => {
 
   const vaultData = await getContractInfo(vaultPath, { agoric, prefix: '' });
   const vaultState = vaultData.vaultState;
+  t.log('vault state:', vaultState);
 
   t.is(vaultState, 'closed', 'The vault should be in the "closed" state.');
 });
