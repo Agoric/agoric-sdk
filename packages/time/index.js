9cdcd5bee688255f8c64f065fdd3e208fc433805
Turadg Aleahmad <turadg@agoric.com>
2024-09-25 14:53:25 -0700

    chore(types): types-index time

--- packages/time/index.js
+++ packages/time/index.js
@@ -1,4 +1,4 @@
 export * from './src/timeMath.js';
 export * from './src/typeGuards.js';
 // eslint-disable-next-line import/export -- just types
-export * from './src/types.js';
+export * from './src/types-index.js';
