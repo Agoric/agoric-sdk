41efbad8b5cecd22b8c8737761330158b86d1806
rabi-siddique <rabisiddique27@gmail.com>
2024-09-27 16:21:53 +0500

    chore: dynamically determine config for electorate

--- packages/builders/scripts/inter-protocol/replace-electorate-core.js
+++ packages/builders/scripts/inter-protocol/replace-electorate-core.js
@@ -20,7 +20,79 @@ export const defaultProposalBuilder = async ({ publishRef, install }, opts) => {
   });
 };
 
+const configurations = {
+  MAINNET: {
+    committeeName: 'Economic Committee',
+    // TODO: Update the addresses after confirmation
+    voterAddresses: {
+      gov1: 'agoric1gx9uu7y6c90rqruhesae2t7c2vlw4uyyxlqxrx',
+      gov2: 'agoric1d4228cvelf8tj65f4h7n2td90sscavln2283h5',
+      gov3: 'agoric14543m33dr28x7qhwc558hzlj9szwhzwzpcmw6a',
+    },
+    highPrioritySendersConfig: {
+      addressesToAdd: [],
+      addressesToRemove: [
+        'agoric13p9adwk0na5npfq64g22l6xucvqdmu3xqe70wq',
+        'agoric1el6zqs8ggctj5vwyukyk4fh50wcpdpwgugd5l5',
+        'agoric1zayxg4e9vd0es9c9jlpt36qtth255txjp6a8yc',
+      ],
+    },
+  },
+  DEVNET: {
+    committeeName: 'Economic Committee',
+    // TODO: Update the addresses after confirmation
+    voterAddresses: {
+      gov1: 'agoric1ldmtatp24qlllgxmrsjzcpe20fvlkp448zcuce',
+      gov2: 'agoric140dmkrz2e42ergjj7gyvejhzmjzurvqeq82ang',
+    },
+    highPrioritySendersConfig: {
+      addressesToAdd: [],
+      addressesToRemove: ['agoric1w8wktaur4zf8qmmtn3n7x3r0jhsjkjntcm3u6h'],
+    },
+  },
+  A3P_INTEGRATION: {
+    committeeName: 'Economic Committee',
+    voterAddresses: {
+      gov1: 'agoric1ee9hr0jyrxhy999y755mp862ljgycmwyp4pl7q',
+    },
+    highPrioritySendersConfig: {
+      addressesToAdd: [],
+      addressesToRemove: [],
+    },
+  },
+  BOOTSTRAP_TEST: {
+    committeeName: 'Economic Committee',
+    voterAddresses: {
+      gov1: 'agoric1gx9uu7y6c90rqruhesae2t7c2vlw4uyyxlqxrx',
+      gov2: 'agoric1d4228cvelf8tj65f4h7n2td90sscavln2283h5',
+      gov3: 'agoric14543m33dr28x7qhwc558hzlj9szwhzwzpcmw6a',
+    },
+    highPrioritySendersConfig: {
+      addressesToAdd: [],
+      addressesToRemove: [
+        'agoric13p9adwk0na5npfq64g22l6xucvqdmu3xqe70wq',
+        'agoric1el6zqs8ggctj5vwyukyk4fh50wcpdpwgugd5l5',
+        'agoric1zayxg4e9vd0es9c9jlpt36qtth255txjp6a8yc',
+      ],
+    },
+  },
+};
+
+const { keys } = Object;
+const Usage = `agoric run replace-electorate-core.js ${keys(configurations).join(' | ')}`;
 export default async (homeP, endowments) => {
+  const { scriptArgs } = endowments;
+  const key = scriptArgs?.[0] || process.env.CONFIG_KEY;
+  const config = configurations[key];
+  if (!config) {
+    console.error(Usage);
+    process.exit(1);
+  }
+  console.log('replace-committee', scriptArgs, config);
+
   const { writeCoreEval } = await makeHelpers(homeP, endowments);
-  await writeCoreEval('replace-committee', defaultProposalBuilder);
+
+  await writeCoreEval('replace-committee', (utils, opts) =>
+    defaultProposalBuilder(utils, { ...opts, ...config }),
+  );
 };
