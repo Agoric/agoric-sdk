afca2f9b9cf0cb3aa6684ba5f9464bb429c34a8c
Mathieu Hofman <mathieu@agoric.com>
2024-10-03 06:12:42 +0000

    test(async-flow): test helpers for async flow Rewrite wake test to use helper

--- packages/async-flow/test/async-flow-wake.test.js
+++ packages/async-flow/test/async-flow-wake.test.js
@@ -1,90 +1,48 @@
 // The purpose of this test file is to demonstrate
 // https://github.com/Agoric/agoric-sdk/issues/9377
-// as the `test.serial.failing` test at the end.
+// as the `.failing` test at the end.
 
-// eslint-disable-next-line import/order
-import {
-  test,
-  getBaggage,
-  annihilate,
-  nextLife,
-} from './prepare-test-env-ava.js';
-
-import { eventLoopIteration } from '@agoric/internal/src/testing-utils.js';
-import { makeDurableZone } from '@agoric/zone/durable.js';
-
-import { prepareTestAsyncFlowTools } from './_utils.js';
-
-/**
- * @import {Zone} from '@agoric/base-zone'
- */
+import { testAsyncLife } from './prepare-test-env-ava.js';
 
 const neverSettlesP = new Promise(() => {});
 
-/**
- * @param {any} t
- * @param {Zone} zone
- */
-const testPlay1 = async (t, zone) => {
-  const { asyncFlow } = prepareTestAsyncFlowTools(t, zone);
+testAsyncLife(
+  'test durable first-crank hazard 1',
+  async (t, { zone, asyncFlow }) => {
+    const guestFunc = async () => neverSettlesP;
 
-  const guestFunc = async () => neverSettlesP;
+    const wrapperFunc = asyncFlow(zone, 'guestFunc', guestFunc);
 
-  const wrapperFunc = asyncFlow(zone, 'guestFunc', guestFunc);
+    return { wrapperFunc };
+  },
+  async (t, { wrapperFunc }) => {
+    wrapperFunc();
+    t.pass();
+  },
+);
 
-  wrapperFunc();
-  t.pass();
-};
+testAsyncLife(
+  'test durable first-crank hazard 2',
+  async (t, { zone, asyncFlow }) => {
+    const guestFunc = async () => neverSettlesP;
 
-/**
- * @param {any} t
- * @param {Zone} zone
- */
-const testPlay2 = async (t, zone) => {
-  const { asyncFlow } = prepareTestAsyncFlowTools(t, zone);
+    t.notThrows(() => asyncFlow(zone, 'guestFunc', guestFunc));
+  },
+);
 
-  const guestFunc = async () => neverSettlesP;
+testAsyncLife.failing(
+  'test durable first-crank hazard 3',
+  async (t, { zone, asyncFlow, allWokenP }) => {
+    await null;
 
-  t.notThrows(() => asyncFlow(zone, 'guestFunc', guestFunc));
-};
-
-/**
- * @param {any} t
- * @param {Zone} zone
- */
-const testPlay3 = async (t, zone) => {
-  const { asyncFlow, allWokenP } = prepareTestAsyncFlowTools(t, zone);
-
-  await null;
-
-  const guestFunc = async () => neverSettlesP;
-  t.notThrows(() => asyncFlow(zone, 'guestFunc', guestFunc));
-  t.notThrowsAsync(
-    () => allWokenP,
-    'will actually throw due to crank bug #9377',
-  );
-};
-
-test.serial('test durable first-crank hazard 1', async t => {
-  annihilate();
-  const zone1 = makeDurableZone(getBaggage(), 'durableRoot');
-  await testPlay1(t, zone1);
-
-  await eventLoopIteration();
-});
-
-test.serial('test durable first-crank hazard 2', async t => {
-  nextLife();
-  const zone2 = makeDurableZone(getBaggage(), 'durableRoot');
-  await testPlay2(t, zone2);
-
-  await eventLoopIteration();
-});
-
-test.serial.failing('test durable first-crank hazard 3', async t => {
-  nextLife();
-  const zone3 = makeDurableZone(getBaggage(), 'durableRoot');
-  await testPlay3(t, zone3);
-
-  return eventLoopIteration();
-});
+    const guestFunc = async () => neverSettlesP;
+    t.notThrows(() => asyncFlow(zone, 'guestFunc', guestFunc));
+    return { allWokenP };
+  },
+  async (t, { allWokenP }) => {
+    await t.notThrowsAsync(
+      () => allWokenP,
+      'will actually throw due to crank bug #9377',
+    );
+  },
+);
--- packages/async-flow/test/prepare-test-env-ava.js
+++ packages/async-flow/test/prepare-test-env-ava.js
@@ -1,6 +1,16 @@
-import '@agoric/swingset-vat/tools/prepare-strict-test-env-ava.js';
+import {
+  startLife,
+  test as avaTest,
+} from '@agoric/swingset-vat/tools/prepare-strict-test-env-ava.js';
 
 import { environmentOptionsListHas } from '@endo/env-options';
+import { makeDurableZone } from '@agoric/zone/durable.js';
+import { prepareTestAsyncFlowTools } from './_utils.js';
+
+/**
+ * @import {ExecutionContext, SerialFn, TodoFn} from 'ava'
+ * @import {Zone} from '@agoric/base-zone'
+ */
 
 export * from '@agoric/swingset-vat/tools/prepare-strict-test-env-ava.js';
 
@@ -8,3 +18,93 @@ export const asyncFlowVerbose = () => {
   // TODO figure out how we really want to control this.
   return environmentOptionsListHas('DEBUG', 'async-flow-verbose');
 };
+
+/**
+ * @typedef {{ zone: Zone; } & ReturnType<typeof prepareTestAsyncFlowTools>} AsyncLifeTools
+ */
+
+/**
+ * @typedef {Parameters<typeof startLife>[2] & {
+ *   panicHandler?: import('./_utils.js').TestAsyncFlowPanicHandler;
+ * }} StartAsyncLifeOptions
+ */
+
+/**
+ * @template {any} RT
+ * @param {ExecutionContext} t
+ * @param {(tools: AsyncLifeTools) => RT} build
+ * @param {(tools: Awaited<RT>) => Promise<void> | void} [run]
+ * @param {StartAsyncLifeOptions} [options]
+ */
+export const startAsyncLife = async (
+  t,
+  build,
+  run,
+  { panicHandler, ...startOptions } = {},
+) =>
+  startLife(
+    async baggage => {
+      const rootZone = makeDurableZone(baggage, 'durableRoot');
+      const asyncFlowTools = prepareTestAsyncFlowTools(
+        t,
+        rootZone.subZone('admin'),
+        { panicHandler },
+      );
+      const tools = await build({
+        zone: rootZone.subZone('contract'),
+        ...asyncFlowTools,
+      });
+      return tools;
+    },
+    run,
+    startOptions,
+  );
+
+/**
+ * @param {SerialFn} test
+ */
+const makeTestAsyncLifeFn =
+  test =>
+  /**
+   * @template {any} RT
+   * @param {string} title
+   * @param {(t: ExecutionContext, tools: AsyncLifeTools) => RT} build
+   * @param {(t: ExecutionContext, tools: Awaited<RT>) => Promise<void> | void} [run]
+   * @param {StartAsyncLifeOptions} [options]
+   */
+  (title, build, run, options) => {
+    return test(title, async t =>
+      startAsyncLife(
+        t,
+        tools => build(t, tools),
+        typeof run === 'function' ? tools => run(t, tools) : undefined,
+        options,
+      ),
+    );
+  };
+
+/**
+ * @param {SerialFn} test
+ * @returns {ReturnType<typeof makeTestAsyncLifeFn> & {
+ *   failing: ReturnType<typeof makeTestAsyncLifeFn>;
+ *   only: ReturnType<typeof makeTestAsyncLifeFn>;
+ *   skip: ReturnType<typeof makeTestAsyncLifeFn>;
+ *   todo: TodoFn;
+ * }}
+ */
+const makeTestAsyncLife = test => {
+  const testAsyncLife = makeTestAsyncLifeFn(test);
+  for (const subTestName of ['failing', 'only', 'skip', 'todo']) {
+    Object.defineProperty(testAsyncLife, subTestName, {
+      value: makeTestAsyncLifeFn(test[subTestName]),
+      writable: true,
+      enumerable: true,
+      configurable: true,
+    });
+  }
+  harden(testAsyncLife);
+  // @ts-expect-error define
+  return testAsyncLife;
+};
+
+export const testAsyncLife = makeTestAsyncLife(avaTest.serial);
