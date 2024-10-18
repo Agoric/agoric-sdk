3cefe08af2cb4fc18128d96d9056926232bb3910
Jorge-Lopes <jorge.lopes@bytepitch.com>
2024-10-18 18:35:46 +0100

    chore(a3p): create helper functions for Vaults tests
    rel: https://github.com/Agoric/BytePitchPartnerEng/issues/22

--- a3p-integration/proposals/z:acceptance/test-lib/utils.js
+++ a3p-integration/proposals/z:acceptance/test-lib/utils.js
@@ -1,6 +1,8 @@
-import { makeAgd, agops } from '@agoric/synthetic-chain';
+import '@endo/init';
+import { makeAgd, agops, agoric } from '@agoric/synthetic-chain';
 import { execFileSync } from 'node:child_process';
 import { readFile, writeFile } from 'node:fs/promises';
+import { boardSlottingMarshaller, makeFromBoard } from './rpc.js';
 
 /**
  * @param {string} fileName base file name without .tjs extension
@@ -65,4 +67,23 @@ export const makeTimerUtils = ({ setTimeout }) => {
     delay,
     waitUntil,
   };
+};
+
+const fromBoard = makeFromBoard();
+const marshaller = boardSlottingMarshaller(fromBoard.convertSlotToVal);
+
+export const getAgoricNamesBrands = async () => {
+  const brands = await agoric
+    .follow('-lF', ':published.agoricNames.brand', '-o', 'text')
+    .then(res => Object.fromEntries(marshaller.fromCapData(JSON.parse(res))));
+
+  return brands;
+};
+
+export const getAgoricNamesInstances = async () => {
+  const instances = await agoric
+    .follow('-lF', ':published.agoricNames.instance', '-o', 'text')
+    .then(res => Object.fromEntries(marshaller.fromCapData(JSON.parse(res))));
+
+  return instances;
 };
