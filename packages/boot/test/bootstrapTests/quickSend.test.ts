e1b5ee32e5253be4bef03ff7dbc0702a18bcbc2e
Dan Connolly <connolly@agoric.com>
2024-10-20 00:14:17 -0500

    test(quickSend): measure advance computrons (WIP)
    currently: IBC mocks lack ack for this transfer
    
      ✘ [fail]: watcher: accept, report Rejected promise returned by test
        ℹ start metering
        ℹ metered cost (beans) undefined
        ℹ REJECTED from ava test.serial("watcher: accept, report"): (Error#1)
        ℹ Error#1: unsettled value for kp1546
    
    wallet agoric1watcher offerStatus {
      id: 'report-2',
      invitationSpec: {
        invitationMakerName: 'ReportCCTPCall',
        previousOffer: 'accept-1',
        source: 'continuing'
      },
      numWantsSatisfied: 1,
      offerArgs: {
        amount: 1234n,
        dest: { chainId: 'osmosis-1', encoding: 'bech32', value: 'osmo1333' },
        nobleFwd: 'noble1301333'
      },
      proposal: {},
      payouts: {}
    }
    watchPacketPattern onFulfilled makeTagged("match:or", [
      makeTagged("match:splitRecord", [
        {
          event: "acknowledgementPacket",
          ...
        },
      ]),
    ])
    No match yet. Save the pattern for later.

--- packages/boot/test/bootstrapTests/quickSend.test.ts
+++ packages/boot/test/bootstrapTests/quickSend.test.ts
@@ -1,12 +1,19 @@
 import { test as anyTest } from '@agoric/zoe/tools/prepare-test-env-ava.js';
 
+import type { CallDetails } from '@agoric/orchestration/src/examples/quickSend.flows.js';
+import { makePromiseKit } from '@endo/promise-kit';
 import type { TestFn } from 'ava';
+import type { OfferId } from '@agoric/smart-wallet/src/offers.js';
+import {
+  AgoricCalc,
+  NobleCalc,
+} from '@agoric/orchestration/src/utils/address.js';
+import { computronCounter } from '../../tools/computron-counter.js';
+import { WalletFactoryDriver } from '../../tools/drivers.js';
 import {
   makeWalletFactoryContext,
   type WalletFactoryTestContext,
 } from './walletFactory.js';
-import { WalletFactoryDriver } from '../../tools/drivers.js';
-import { computronCounter } from '../../tools/computron-counter.js';
 
 const makeMeter = () => {
   const beansPer = 100n;
@@ -26,7 +33,7 @@ const makeMeter = () => {
       return policy;
     },
     setMetering: x => (metering = x),
-    getValue: () => policy.remainingBeans(),
+    getValue: () => policy?.remainingBeans(),
   });
   return meter;
 };
@@ -40,7 +47,8 @@ test.before('bootstrap', async t => {
   const ctx = await makeWalletFactoryContext(
     t,
     '@agoric/vm-config/decentral-itest-orchestration-config.json',
-    { defaultManagerType: 'xsnap', meter },
+    // for perf testing: { defaultManagerType: 'xsnap', meter },
+    { defaultManagerType: 'local' },
   );
   t.context = { ...ctx, meter };
 
@@ -70,31 +78,62 @@ type SmartWallet = Awaited<
   ReturnType<WalletFactoryDriver['provideSmartWallet']>
 >;
 
-test.serial('accept watcher invitation', async t => {
-  const { agoricNamesRemotes, meter } = t.context;
+const makeWatcher = (sw: SmartWallet, instance) => {
+  let seq = 0;
+  const accepted: PromiseKit<OfferId> = makePromiseKit();
 
-  const william = async (sw: SmartWallet) => {
-    await sw.executeOffer({
-      id: 'accept',
-      invitationSpec: {
-        source: 'purse',
-        instance: agoricNamesRemotes.instance.quickSend,
-        description: 'initAccounts',
-      },
-      proposal: {},
-    });
-    const update = sw.getLatestUpdateRecord();
-    t.like(update, {
-      updated: 'offerStatus',
-      status: { id: 'accept', numWantsSatisfied: 1, result: 'UNPUBLISHED' },
-    });
-  };
+  return harden({
+    accept: async () => {
+      const id = `accept-${(seq += 1)}`;
+      accepted.resolve(id);
+      await sw.executeOffer({
+        id,
+        invitationSpec: {
+          source: 'purse',
+          instance,
+          description: 'initAccounts',
+        },
+        proposal: {},
+      });
+      return sw.getLatestUpdateRecord();
+    },
 
-  const wd =
-    await t.context.walletFactoryDriver.provideSmartWallet('agoric1watcher');
+    report: async (callDetails: CallDetails) => {
+      await sw.executeOffer({
+        id: `report-${(seq += 1)}`,
+        invitationSpec: {
+          source: 'continuing',
+          invitationMakerName: 'ReportCCTPCall',
+          previousOffer: await accepted.promise,
+        },
+        proposal: {},
+        offerArgs: callDetails,
+      });
+      return sw.getLatestUpdateRecord();
+    },
+  });
+};
+
+test.serial('watcher: accept, report', async t => {
+  const { agoricNamesRemotes, meter, walletFactoryDriver } = t.context;
+
+  const settlementBase = 'agoric1fakeLCAAddress'; // TODO: read from vstorage
+
+  const wd = await walletFactoryDriver.provideSmartWallet('agoric1watcher');
+  const william = makeWatcher(wd, agoricNamesRemotes.instance.quickSend);
 
   t.log('start metering');
   meter.setMetering(true);
-  await william(wd);
+  const update = await william.accept();
   t.log('metered cost (beans)', meter.getValue());
+  t.like(update, { status: { id: 'accept-1', numWantsSatisfied: 1 } });
+
+  const encoding = 'bech32' as const;
+  const dest = { chainId: 'osmosis-1', encoding, value: 'osmo1333' };
+  const vAddr = AgoricCalc.virtualAddressFor(settlementBase, dest.value);
+  const nobleFwd = NobleCalc.fwdAddressFor(vAddr);
+  const s2 = await william.report({ amount: 1234n, dest, nobleFwd });
+  t.log('@@@', s2);
+  t.log('metered cost (beans)', meter.getValue());
+  t.like(s2, {});
 });
