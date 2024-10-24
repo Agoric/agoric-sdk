0d0ffef337973c8488a4292a618d816a98867bdd
Dan Connolly <connolly@agoric.com>
2024-10-24 16:56:59 -0500

    refactor: simplify io in quick-load.test

--- a3p-integration/proposals/d:quick-send/quick-load.test.js
+++ a3p-integration/proposals/d:quick-send/quick-load.test.js
@@ -1,29 +1,17 @@
 // @ts-check
 /* global globalThis */
-import anyTest from 'ava';
+import test from 'ava';
 import { extractStreamCellValue } from '@agoric/synthetic-chain';
 import { localAPI, makeLCD } from './test-lib/cosmos-api.js';
 import { makeVStorage } from './test-lib/vstorage-client.js';
 
-/**
- * @import {TestFn} from 'ava';
- */
-/** @type {TestFn<Awaited<ReturnType<typeof makeTestContext>>>} */
-// @ts-expect-error XXX something weird about test.after
-const test = anyTest;
-
-const makeTestContext = async t => {
-  const api = makeLCD(localAPI, { fetch: globalThis.fetch });
-  return { api };
-};
-
-test.before('IO setup', async t => (t.context = await makeTestContext(t)));
+const io = { api: makeLCD(localAPI, { fetch: globalThis.fetch }) };
 
 test('quickSend is in agoricNames.instance', async t => {
-  const { api } = t.context;
-  const vs = makeVStorage(api);
+  const vs = makeVStorage(io.api);
   const data = await vs.readStorage('published.agoricNames.instance');
   const value = extractStreamCellValue(data);
+  assert.typeof(value, 'string');
   const capData = JSON.parse(value);
   const encoding = JSON.parse(capData?.body.replace(/^#/, ''));
   const byName = Object.fromEntries(encoding);
