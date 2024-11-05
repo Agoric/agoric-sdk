e03b85877a4d77255d20e16b2c4b200753ff50d3
Turadg Aleahmad <turadg@agoric.com>
2024-11-05 15:39:51 -0800

    build: codegen

---
+++ packages/client-utils/src/codegen/agoric/bundle.ts
@@ -0,0 +1,65 @@
+//@ts-nocheck
+import * as _0 from "./lien/genesis.js";
+import * as _1 from "./lien/lien.js";
+import * as _2 from "./swingset/genesis.js";
+import * as _3 from "./swingset/msgs.js";
+import * as _4 from "./swingset/query.js";
+import * as _5 from "./swingset/swingset.js";
+import * as _6 from "./vbank/genesis.js";
+import * as _7 from "./vbank/msgs.js";
+import * as _8 from "./vbank/query.js";
+import * as _9 from "./vbank/vbank.js";
+import * as _10 from "./vibc/msgs.js";
+import * as _11 from "./vlocalchain/vlocalchain.js";
+import * as _12 from "./vstorage/genesis.js";
+import * as _13 from "./vstorage/query.js";
+import * as _14 from "./vstorage/vstorage.js";
+import * as _15 from "./vtransfer/genesis.js";
+import * as _209 from "./swingset/query.rpc.Query.js";
+import * as _210 from "./vbank/query.rpc.Query.js";
+import * as _211 from "./vstorage/query.rpc.Query.js";
+import * as _212 from "./swingset/msgs.rpc.msg.js";
+import * as _213 from "./vibc/msgs.rpc.msg.js";
+import * as _266 from "./rpc.query.js";
+import * as _267 from "./rpc.tx.js";
+export namespace agoric {
+  export const lien = {
+    ..._0,
+    ..._1
+  };
+  export const swingset = {
+    ..._2,
+    ..._3,
+    ..._4,
+    ..._5,
+    ..._209,
+    ..._212
+  };
+  export const vbank = {
+    ..._6,
+    ..._7,
+    ..._8,
+    ..._9,
+    ..._210
+  };
+  export const vibc = {
+    ..._10,
+    ..._213
+  };
+  export const vlocalchain = {
+    ..._11
+  };
+  export const vstorage = {
+    ..._12,
+    ..._13,
+    ..._14,
+    ..._211
+  };
+  export const vtransfer = {
+    ..._15
+  };
+  export const ClientFactory = {
+    ..._266,
+    ..._267
+  };
+}
---
+++ packages/client-utils/src/codegen/stride/bundle.ts
@@ -0,0 +1,91 @@
+//@ts-nocheck
+import * as _170 from "./epochs/genesis.js";
+import * as _171 from "./epochs/query.js";
+import * as _172 from "./records/callbacks.js";
+import * as _173 from "./records/genesis.js";
+import * as _174 from "./records/params.js";
+import * as _175 from "./records/query.js";
+import * as _176 from "./records/records.js";
+import * as _177 from "./stakedym/genesis.js";
+import * as _178 from "./stakedym/query.js";
+import * as _179 from "./stakedym/stakedym.js";
+import * as _180 from "./stakedym/tx.js";
+import * as _181 from "./stakeibc/address_unbonding.js";
+import * as _182 from "./stakeibc/callbacks.js";
+import * as _183 from "./stakeibc/epoch_tracker.js";
+import * as _184 from "./stakeibc/genesis.js";
+import * as _185 from "./stakeibc/gov.js";
+import * as _186 from "./stakeibc/host_zone.js";
+import * as _187 from "./stakeibc/ica_account.js";
+import * as _188 from "./stakeibc/packet.js";
+import * as _189 from "./stakeibc/params.js";
+import * as _190 from "./stakeibc/query.js";
+import * as _191 from "./stakeibc/trade_route.js";
+import * as _192 from "./stakeibc/tx.js";
+import * as _193 from "./stakeibc/validator.js";
+import * as _194 from "./staketia/genesis.js";
+import * as _195 from "./staketia/query.js";
+import * as _196 from "./staketia/staketia.js";
+import * as _197 from "./staketia/tx.js";
+import * as _258 from "./epochs/query.rpc.Query.js";
+import * as _259 from "./records/query.rpc.Query.js";
+import * as _260 from "./stakedym/query.rpc.Query.js";
+import * as _261 from "./stakeibc/query.rpc.Query.js";
+import * as _262 from "./staketia/query.rpc.Query.js";
+import * as _263 from "./stakedym/tx.rpc.msg.js";
+import * as _264 from "./stakeibc/tx.rpc.msg.js";
+import * as _265 from "./staketia/tx.rpc.msg.js";
+import * as _276 from "./rpc.query.js";
+import * as _277 from "./rpc.tx.js";
+export namespace stride {
+  export const epochs = {
+    ..._170,
+    ..._171,
+    ..._258
+  };
+  export const records = {
+    ..._172,
+    ..._173,
+    ..._174,
+    ..._175,
+    ..._176,
+    ..._259
+  };
+  export const stakedym = {
+    ..._177,
+    ..._178,
+    ..._179,
+    ..._180,
+    ..._260,
+    ..._263
+  };
+  export const stakeibc = {
+    ..._181,
+    ..._182,
+    ..._183,
+    ..._184,
+    ..._185,
+    ..._186,
+    ..._187,
+    ..._188,
+    ..._189,
+    ..._190,
+    ..._191,
+    ..._192,
+    ..._193,
+    ..._261,
+    ..._264
+  };
+  export const staketia = {
+    ..._194,
+    ..._195,
+    ..._196,
+    ..._197,
+    ..._262,
+    ..._265
+  };
+  export const ClientFactory = {
+    ..._276,
+    ..._277
+  };
+}
