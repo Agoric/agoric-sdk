c8916b25289540ccd92c81a11fb4ae8c2efb9bac
anilhelvaci <anilhelvaci96@gmail.com>
2024-11-06 16:43:15 +0300

    chore(pp-upgrade): create helper core-evals
    Refs: https://github.com/Agoric/agoric-sdk/issues/10395

---
+++ a3p-integration/proposals/p:upgrade-19/depositUSD-LEMONS/deposit-usd-lemons.js
@@ -0,0 +1,49 @@
+const PROVISIONING_POOL_ADDR = 'agoric1megzytg65cyrgzs6fvzxgrcqvwwl7ugpt62346';
+
+const depositUsdLemons = async powers => {
+  const {
+    consume: { contractKits: contractKitsP, namesByAddressAdmin: namesByAddressAdminP , agoricNames },
+    // instance: { consume: { ['psm-IST-USD_LEMONS']: usdLemonsPsmInstanceP }}
+  } = powers;
+
+  const namesByAddressAdmin = await namesByAddressAdminP;
+
+  const getDepositFacet = async address => {
+    const admin = await E(namesByAddressAdmin).lookupAdmin(address);
+    console.log('ADMIN', admin)
+
+    const nameHub = await E(admin).readonly();
+    console.log('NAME_HUB', nameHub);
+    const hub = E(E(namesByAddressAdmin).lookupAdmin(address)).readonly();
+    return E(hub).lookup('depositFacet');
+  };
+
+  const [contractKits, usdLemonsIssuer, usdLemonsBrand, ppDepositFacet] = await Promise.all([
+    contractKitsP,
+    E(agoricNames).lookup('issuer', 'USD_LEMONS'),
+    E(agoricNames).lookup('brand', 'USD_LEMONS'),
+    getDepositFacet(PROVISIONING_POOL_ADDR),
+  ]);
+
+  console.log('[CONTRACT_KITS]', contractKits);
+  console.log('[ISSUER]', usdLemonsIssuer);
+
+  let usdLemonsMint;
+  for (const { publicFacet, creatorFacet: mint } of contractKits.values()) {
+    if (publicFacet === usdLemonsIssuer) {
+      usdLemonsMint = mint;
+      console.log('BINGO', mint)
+      break;
+    }
+  }
+
+  console.log('Minting USD_LEMONS');
+  const helloPayment = await E(usdLemonsMint).mintPayment(harden({ brand: usdLemonsBrand, value: 500000n }));
+
+  console.log('Funding provision pool...');
+  await E(ppDepositFacet).receive(helloPayment);
+
+  console.log('Done.');
+};
+
+depositUsdLemons;
