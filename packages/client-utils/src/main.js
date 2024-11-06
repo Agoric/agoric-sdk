4b4844d2b07fb983f0db34ee708d92fc13c15523
Turadg Aleahmad <turadg@agoric.com>
2024-11-05 16:05:22 -0800

    refactor: fetchSwingsetParams wo spawn

--- packages/client-utils/src/main.js
+++ packages/client-utils/src/main.js
@@ -2,13 +2,14 @@ import { VBankAccount } from '@agoric/internal/src/config.js';
 
 export { VBankAccount };
 export * from './cli.js';
+export * from './clients.js';
 export * from './marshalTables.js';
 export * from './network-config.js';
 export * from './rpc.js';
 export * from './smart-wallet-kit.js';
 export * from './sync-tools.js';
-export * from './vstorage.js';
 export * from './vstorage-kit.js';
+export * from './vstorage.js';
 export * from './wallet-utils.js';
 
 // eslint-disable-next-line import/export -- just types
