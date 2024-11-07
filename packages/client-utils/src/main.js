88939bfb60e2e3480b1f8f6d4dca7f64cd668b4a
Turadg Aleahmad <turadg@agoric.com>
2024-11-07 10:06:11 -0800

    feat(types): TypedPublished

--- packages/client-utils/src/main.js
+++ packages/client-utils/src/main.js
@@ -2,3 +2,6 @@ export * from './rpc.js';
 export * from './sync-tools.js';
 export * from './vstorage-kit.js';
 export * from './wallet-utils.js';
+
+// eslint-disable-next-line import/export -- just types
+export * from './types.js';
