8eb7dee8f70facf8fb0b36c36d630c4153f4c722
Turadg Aleahmad <turadg@agoric.com>
2024-11-11 13:01:02 -0800

    feat: TransactionFeedKit

--- packages/fast-usdc/test/fast-usdc.contract.test.ts
+++ packages/fast-usdc/test/fast-usdc.contract.test.ts
@@ -3,6 +3,8 @@ import { test } from '@agoric/zoe/tools/prepare-test-env-ava.js';
 import { setUpZoeForTest } from '@agoric/zoe/tools/setup-zoe.js';
 import { E } from '@endo/far';
 import path from 'path';
+import fetchedChainInfo from '@agoric/orchestration/src/fetched-chain-info.js';
+import { MockCctpTxEvidences } from './fixtures.js';
 import { commonSetup } from './supports.js';
 
 const dirname = path.dirname(new URL(import.meta.url).pathname);
@@ -12,17 +14,20 @@ type StartFn = typeof import('../src/fast-usdc.contract.js').start;
 
 test('start', async t => {
   const {
-    bootstrap,
     brands: { poolShares, usdc },
     commonPrivateArgs,
-    utils,
   } = await commonSetup(t);
 
-  const { zoe, bundleAndInstall } = await setUpZoeForTest();
+  const { zoe, bundleAndInstall } = await setUpZoeForTest({
+    setJig: jig => {
+      jig.chainHub.registerChain('osmosis', fetchedChainInfo.osmosis);
+    },
+  });
+
   const installation: Installation<StartFn> =
     await bundleAndInstall(contractFile);
 
-  const { creatorFacet } = await E(zoe).startInstance(
+  const { creatorFacet, publicFacet } = await E(zoe).startInstance(
     installation,
     { PoolShares: poolShares.issuer, USDC: usdc.issuer },
     {
@@ -32,4 +37,16 @@ test('start', async t => {
     commonPrivateArgs,
   );
   t.truthy(creatorFacet);
+
+  const e1 = await E(MockCctpTxEvidences.AGORIC_NO_PARAMS)();
+
+  const inv = await E(publicFacet).makeTestPushInvitation(e1);
+  // the invitation maker itself pushes the evidence
+
+  // the offer is still safe to make
+  const seat = await E(zoe).offer(inv);
+  t.is(
+    await E(seat).getOfferResult(),
+    'noop; evidence was pushed in the invitation maker call',
+  );
 });
