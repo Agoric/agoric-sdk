784feb23cad5f10808727e56c5c03586199ee36c
Ikenna Omekam <ikenna@agoric.com>
2024-11-11 17:03:23 -0600

    add pfmEnabled to shape

--- packages/orchestration/src/typeGuards.js
+++ packages/orchestration/src/typeGuards.js
@@ -99,6 +99,7 @@ export const CosmosChainInfoShape = M.splitRecord(
     stakingTokens: M.arrayOf({ denom: M.string() }),
     // UNTIL https://github.com/Agoric/agoric-sdk/issues/9326
     icqEnabled: M.boolean(),
+    pfmEnabled: M.boolean(),
   },
 );
 
