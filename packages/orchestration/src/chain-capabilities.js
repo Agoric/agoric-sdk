b11b3e6e440ed991cb5d8703d85eb69927d58db1
0xPatrick <patrick@0xpatrick.dev>
2024-11-13 17:26:20 -0500

    feat: chain-capabilities.js constants
    - adds `ICQ_ENABLED` and `PFM_ENABLED` constants to support `CosmosChainInfo`
    - this data is not available via a well-known registry like cosmos/chain-registry,
      but necessary for the Orchestration API

---
+++ packages/orchestration/src/chain-capabilities.js
@@ -0,0 +1,45 @@
+/**
+ * @file Contains ChainInfo that not available from a well-known chain registry.
+ *
+ *   This is used to seed `agoricNames` and `ChainHub`. However, the data is
+ *   dynamic and can change over time. (Remote chains can choose to add or
+ *   remove ibc application modules with chain software upgrades and change
+ *   allowlist settings.)
+ *
+ *   Developers should use `ChainHubAdmin` as part of their creator or governed
+ *   facet if they wish to have fine-grained control over the data in their
+ *   local `ChainHub`.
+ *
+ *   Last Updated November 11, 2024
+ */
+
+/** @import {KnownChains} from '@agoric/orchestration'; */
+
+/**
+ * Chains with the async-icq (icq-1) module available.
+ *
+ * @satisfies {(keyof KnownChains)[]}
+ */
+export const ICQ_ENABLED = /** @type {const} } */ ['omniflixhub', 'osmosis'];
+harden(ICQ_ENABLED);
+
+/**
+ * Chains with the packet-forward-middleware module available.
+ *
+ * @satisfies {(keyof KnownChains)[]}
+ */
+export const PFM_ENABLED = /** @type {const} } */ [
+  'agoric',
+  'celestia',
+  'cosmoshub',
+  'juno',
+  'neutron',
+  'noble',
+  'omniflixhub',
+  'osmosis',
+  'secretnetwork',
+  'stargaze',
+  'stride',
+  'umee',
+];
+harden(PFM_ENABLED);
