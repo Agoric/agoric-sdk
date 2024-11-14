292d2f36bfd39b30290f67371c3d4d22ca57f30d
Turadg Aleahmad <turadg@agoric.com>
2024-11-13 17:33:29 -0800

    chore(types): fix Buffer usage in TS 5.7

--- packages/internal/src/netstring.js
+++ packages/internal/src/netstring.js
@@ -89,6 +89,7 @@ export function decode(data, optMaxChunkSize) {
  */
 // input is a byte pipe, output is a sequence of Buffers
 export function netstringDecoderStream(optMaxChunkSize) {
+  /** @type {Buffer<ArrayBufferLike>} */
   let buffered = Buffer.from('');
   /**
    * @param {Buffer} chunk
