555c29b3bac4666e8228c14d585ee2949b455db1
Ikenna Omekam <ikenna@agoric.com>
2024-11-14 10:08:02 -0500

    test: fixing basic-flows

--- packages/orchestration/src/examples/basic-flows.contract.js
+++ packages/orchestration/src/examples/basic-flows.contract.js
@@ -4,9 +4,13 @@
  */
 import { InvitationShape } from '@agoric/zoe/src/typeGuards.js';
 import { M } from '@endo/patterns';
+import { E } from '@endo/far';
 import { preparePortfolioHolder } from '../exos/portfolio-holder-kit.js';
 import { withOrchestration } from '../utils/start-helper.js';
 import * as flows from './basic-flows.flows.js';
+import { prepareChainHubAdmin } from '../exos/chain-hub-admin.js';
+import { registerKnownChainsAndAssets } from '../utils/chain-hub-helper.js';
+import fetchedChainInfo from '../fetched-chain-info.js';
 
 /**
  * @import {Zone} from '@agoric/zone';
@@ -17,20 +21,59 @@ import * as flows from './basic-flows.flows.js';
  * @param {ZCF} zcf
  * @param {OrchestrationPowers & {
  *   marshaller: Marshaller;
- * }} _privateArgs
+ * }} privateArgs
  * @param {Zone} zone
  * @param {OrchestrationTools} tools
  */
 const contract = async (
   zcf,
-  _privateArgs,
+  privateArgs,
   zone,
-  { orchestrateAll, vowTools },
+  { chainHub, orchestrateAll, vowTools, baggage },
 ) => {
   const makePortfolioHolder = preparePortfolioHolder(
     zone.subZone('portfolio'),
     vowTools,
   );
+
+  const creatorFacet = prepareChainHubAdmin(zone, chainHub);
+
+  const incarnationKey = 'incarnation1';
+
+  await null;
+
+  if (!baggage.has(incarnationKey)) {
+    baggage.init(incarnationKey, true);
+
+    // register assets in ChainHub ourselves,
+    // UNTIL https://github.com/Agoric/agoric-sdk/issues/9752
+    const assets =
+      /** @type {import('@agoric/vats/src/vat-bank.js').AssetInfo[]} */ (
+        await E(E(privateArgs.agoricNames).lookup('vbankAsset')).values()
+      );
+
+    /** @type {Record<string, Brand<'nat'>>} */
+    const brands = {};
+
+    for (const asset of assets) {
+      brands[asset.issuerName] = /** @type {Brand<'nat'>} */ (asset.brand);
+    }
+
+    await registerKnownChainsAndAssets(
+      {
+        vowTools,
+        chainHubAdmin: creatorFacet,
+      },
+      fetchedChainInfo,
+      brands,
+    );
+
+    await E(creatorFacet).registerAsset(`ibc/uusdchash`, {
+      chainName: 'agoric',
+      baseName: 'agoric',
+      baseDenom: 'uusdc',
+    });
+  }
 
   const orchFns = orchestrateAll(flows, { makePortfolioHolder });
 
