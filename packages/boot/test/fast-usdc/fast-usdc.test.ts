9b295150d58aacc2efd245e7bb572101e16b705b
Dan Connolly <connolly@agoric.com>
2024-11-13 23:48:01 -0600

    test(boot): fast-usdc core eval

---
+++ packages/boot/test/fast-usdc/fast-usdc.test.ts
@@ -0,0 +1,100 @@
+import { test as anyTest } from '@agoric/zoe/tools/prepare-test-env-ava.js';
+
+import type { TestFn } from 'ava';
+import type { FastUSDCKit } from '@agoric/fast-usdc/src/fast-usdc.start.js';
+import { Fail } from '@endo/errors';
+import { unmarshalFromVstorage } from '@agoric/internal/src/marshal.js';
+import { makeMarshal } from '@endo/marshal';
+import {
+  makeWalletFactoryContext,
+  type WalletFactoryTestContext,
+} from '../bootstrapTests/walletFactory.js';
+
+const test: TestFn<WalletFactoryTestContext> = anyTest;
+
+test.before('bootstrap', async t => {
+  const config = '@agoric/vm-config/decentral-itest-orchestration-config.json';
+  t.context = await makeWalletFactoryContext(t, config);
+});
+test.after.always(t => t.context.shutdown?.());
+
+test.serial('oracles provision before contract deployment', async t => {
+  const { walletFactoryDriver: wd } = t.context;
+  const watcherWallet = await wd.provideSmartWallet('agoric1watcher1');
+  t.truthy(watcherWallet);
+});
+
+test.serial(
+  'contract starts; adds to agoricNames; sends invitation',
+  async t => {
+    const {
+      agoricNamesRemotes,
+      evalProposal,
+      buildProposal,
+      refreshAgoricNamesRemotes,
+      storage,
+      walletFactoryDriver: wd,
+    } = t.context;
+
+    const watcherWallet = await wd.provideSmartWallet('agoric1watcher1');
+
+    const materials = buildProposal(
+      '@agoric/builders/scripts/fast-usdc/init-fast-usdc.js',
+      ['--oracle', 'a:agoric1watcher1'],
+    );
+    await evalProposal(materials);
+
+    // update now that fastUSDC is instantiated
+    refreshAgoricNamesRemotes();
+    t.truthy(agoricNamesRemotes.instance.fastUSDC);
+    t.truthy(agoricNamesRemotes.brand.FastLP);
+
+    const { EV } = t.context.runUtils;
+    const agoricNames = await EV.vat('bootstrap').consumeItem('agoricNames');
+    const board = await EV.vat('bootstrap').consumeItem('board');
+    const getBoardAux = async name => {
+      const brand = await EV(agoricNames).lookup('brand', name);
+      const id = await EV(board).getId(brand);
+      t.truthy(storage.data.get(`published.boardAux.${id}`));
+      return unmarshalFromVstorage(
+        storage.data,
+        `published.boardAux.${id}`,
+        makeMarshal().fromCapData,
+        -1,
+      );
+    };
+    t.like(
+      await getBoardAux('FastLP'),
+      {
+        allegedName: 'PoolShares', // misnomer, in some contexts
+        displayInfo: {
+          assetKind: 'nat',
+          decimalPlaces: 6,
+        },
+      },
+      'brand displayInfo available in boardAux',
+    );
+
+    const current = watcherWallet.getCurrentWalletRecord();
+
+    // XXX We should be able to compare objects by identity like this:
+    //
+    // const invitationPurse = current.purses.find(
+    //   p => p.brand === agoricNamesRemotes.brand.Invitation,
+    // );
+    //
+    // But agoricNamesRemotes and walletFactoryDriver
+    // don't share a marshal context.
+    // We should be able to map between them using
+    // const walletStuff = w.fromCapData(a.toCapData(aStuff))
+    // but the marshallers don't even preserve identity within themselves.
+
+    current.purses.length === 1 || Fail`test limited to 1 purse`;
+    const [thePurse] = current.purses;
+    const details = thePurse.balance.value as Array<any>;
+    Array.isArray(details) || Fail`expected SET value`;
+    t.is(details.length, 1, 'oracle wallet has 1 invitation');
+    t.is(details[0].description, 'oracle operator invitation');
+    // XXX t.is(details.instance, agoricNames.instance.fastUSDC) should work
+  },
+);
