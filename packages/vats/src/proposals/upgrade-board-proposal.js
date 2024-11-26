e80ed6b1fab31f380b8bfccd39285c4034479ae8
Chris Hibbert <hibbert@agoric.com>
2024-11-26 14:37:33 -0800

    chore: verify that the boards list of keys doesn't change

--- packages/vats/src/proposals/upgrade-board-proposal.js
+++ packages/vats/src/proposals/upgrade-board-proposal.js
@@ -14,7 +14,7 @@ import { E } from '@endo/far';
  * @param {{ boardRef: VatSourceRef }} options.options
  */
 export const upgradeBoard = async (
-  { consume: { vatAdminSvc, vatStore } },
+  { consume: { board, vatAdminSvc, vatStore } },
   options,
 ) => {
   const { boardRef } = options.options;
@@ -22,16 +22,25 @@ export const upgradeBoard = async (
   assert(boardRef.bundleID);
   const boardBundleCap = await E(vatAdminSvc).getBundleCap(boardRef.bundleID);
   console.log(`Board BUNDLE ID: `, boardRef.bundleID);
+  const idsBefore = await E(board).ids();
 
   const { adminNode } = await E(vatStore).get('board');
-
   await E(adminNode).upgrade(boardBundleCap, {});
+
+  const idsAfter = await E(board).ids();
+  const same =
+    idsBefore.length === idsAfter.length &&
+    idsBefore.every((element, index) => element === idsAfter[index]);
+  assert(same, 'keys must stay the same');
+
+  console.log('Board upgrade complete');
 };
 
 export const getManifestForUpgradingBoard = (_powers, { boardRef }) => ({
   manifest: {
     [upgradeBoard.name]: {
       consume: {
+        board: 'board',
         vatAdminSvc: 'vatAdminSvc',
         vatStore: 'vatStore',
       },
