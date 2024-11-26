ae2cf1e461a63deb39dc01c35cea564cf1d5527b
Turadg Aleahmad <turadg@agoric.com>
2024-11-25 20:50:38 -0800

    feat: operator accept cmd

--- packages/fast-usdc/src/cli/operator-commands.js
+++ packages/fast-usdc/src/cli/operator-commands.js
@@ -1,6 +1,13 @@
+/* eslint-env node */
 /**
  * @import {Command} from 'commander';
+ * @import {OfferSpec} from '@agoric/smart-wallet/src/offers.js';
+ * @import {ExecuteOfferAction} from '@agoric/smart-wallet/src/smartWallet.js';
  */
+
+import { fetchEnvNetworkConfig, makeVstorageKit } from '@agoric/client-utils';
+import { INVITATION_MAKERS_DESC } from '../exos/transaction-feed.js';
+import { outputActionAndHint } from './bridge-action.js';
 
 /**
  * @param {Command} program
@@ -13,9 +20,46 @@ export const addOperatorCommands = program => {
   operator
     .command('accept')
     .description('Accept invitation to be an operator')
-    .action(async options => {
-      const { requestId } = options;
-      console.error('TODO: Implement accept logic for request:', requestId);
+    .addHelpText(
+      'after',
+      '\nPipe the STDOUT to a file such as accept.json, then use the Agoric CLI to broadcast it:\n  agoric wallet send --offer accept.json --from gov1 --keyring-backend="test"',
+    )
+    .option(
+      '--offerId <string>',
+      'Offer id',
+      String,
+      `operatorAccept-${Date.now()}`,
+    )
+    .action(async opts => {
+      const networkConfig = await fetchEnvNetworkConfig({
+        env: process.env,
+        fetch,
+      });
+      const vsk = await makeVstorageKit({ fetch }, networkConfig);
+      const instance = vsk.agoricNames.instance.fastUsdc;
+      assert(instance, 'fastUsdc instance not in agoricNames');
+
+      /** @type {OfferSpec} */
+      const offer = {
+        id: opts.offerId,
+        invitationSpec: {
+          source: 'purse',
+          instance,
+          description: INVITATION_MAKERS_DESC,
+        },
+        proposal: {},
+      };
+
+      /** @type {ExecuteOfferAction} */
+      const bridgeAction = {
+        method: 'executeOffer',
+        offer,
+      };
+
+      outputActionAndHint(bridgeAction, {
+        stderr: process.stderr,
+        stdout: process.stdout,
+      });
     });
 
   operator
