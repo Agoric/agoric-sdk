4319ca45e8583a54ae81bfc20470030349f34665
Jorge-Lopes <jorge.lopes@bytepitch.com>
2024-11-28 19:34:12 +0000

    test(a3p): changes execution order to trigger error

--- a3p-integration/proposals/z:acceptance/test.sh
+++ a3p-integration/proposals/z:acceptance/test.sh
@@ -4,38 +4,39 @@ set -ueo pipefail
 # Place here any test that should be executed using the executed proposal.
 # The effects of this step are not persisted in further proposal layers.
 
-# test readFully method
-# XXX to be removed
-echo ACCEPTANCE TESTING vstorage
-yarn ava vstorage.test.js
-
 # test the state right after the previous proposals
-yarn ava initial.test.js
+# yarn ava initial.test.js
 
-# XXX some of these tests have path dependencies so no globs
-yarn ava core-eval.test.js
+# # XXX some of these tests have path dependencies so no globs
+# yarn ava core-eval.test.js
 
-npm install -g tsx
-scripts/test-vaults.mts
+# npm install -g tsx
+# scripts/test-vaults.mts
 
-echo ACCEPTANCE TESTING kread
-yarn ava kread.test.js
+# echo ACCEPTANCE TESTING kread
+# yarn ava kread.test.js
 
-echo ACCEPTANCE TESTING valueVow
-yarn ava valueVow.test.js
+# echo ACCEPTANCE TESTING valueVow
+# yarn ava valueVow.test.js
+
+echo ACCEPTANCE TESTING passing vstorage
+yarn ava vstorage.test.js
 
 echo ACCEPTANCE TESTING state sync
 ./state-sync-snapshots-test.sh
 ./genesis-test.sh
 
-echo ACCEPTANCE TESTING wallet
-yarn ava wallet.test.js
+echo ACCEPTANCE TESTING failing vstorage
+yarn ava vstorage.test.js
 
-echo ACCEPTANCE TESTING psm
-yarn ava psm.test.js
+# echo ACCEPTANCE TESTING wallet
+# yarn ava wallet.test.js
 
-echo ACCEPTANCE TESTING governance
-yarn ava governance.test.js
+# echo ACCEPTANCE TESTING psm
+# yarn ava psm.test.js
 
-echo ACCEPTANCE TESTING vaults
-yarn ava vaults.test.js
+# echo ACCEPTANCE TESTING governance
+# yarn ava governance.test.js
+
+# echo ACCEPTANCE TESTING vaults
+# yarn ava vaults.test.js
