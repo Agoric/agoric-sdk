8e23534544686780a1858ba8928bb6afd1678e05
Turadg Aleahmad <turadg@agoric.com>
2024-12-02 11:46:56 -0800

    test: skip extra endo/init

--- multichain-testing/ava.main.config.js
+++ multichain-testing/ava.main.config.js
@@ -2,7 +2,8 @@ export default {
   extensions: {
     ts: 'module',
   },
-  require: ['@endo/init/debug.js'],
+  // Each test imports `@endo/ses-ava/prepare-endo.js` which does its own @endo/init
+  // require: ['@endo/init/debug.js'],
   nodeArguments: ['--import=ts-blank-space/register'],
   files: ['test/**/*.test.ts', '!test/fast-usdc/**/*.test.ts'],
   concurrency: 1,
