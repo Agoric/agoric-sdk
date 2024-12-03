11df834abbc903ca49dc42501dd89aea034aaaaa
Dan Connolly <connolly@agoric.com>
2024-12-03 01:48:06 -0600

    chore: avoid magic strings in attest test

--- packages/fast-usdc/test/cli/operator-commands.test.ts
+++ packages/fast-usdc/test/cli/operator-commands.test.ts
@@ -6,37 +6,29 @@ import { MockCctpTxEvidences } from '../fixtures.js';
 import { makeMarshal } from '@endo/marshal';
 
 export const flags = (
-  record: Record<string, string | number | undefined>,
+  record: Record<string, string | number | bigint | undefined>,
 ): string[] => {
   // @ts-expect-error undefined is filtered out
   const skipUndef: [string, string][] = Object.entries(record).filter(
     ([_k, v]) => v !== undefined,
   );
-  return skipUndef.map(([k, v]) => [`--${k}`, v]).flat();
+  return skipUndef.map(([k, v]) => [`--${k}`, `${v}`]).flat();
 };
 
 const marshalData = makeMarshal(_v => assert.fail('data only'));
 
 test('fast-usdc operator attest sub-command', async t => {
-  const evidence = harden(
-    MockCctpTxEvidences.AGORIC_PLUS_DYDX(),
-  ) as unknown as Passable;
+  const evidence = harden(MockCctpTxEvidences.AGORIC_PLUS_DYDX());
+  const { aux, tx, ...flat } = evidence;
   const argv = [
     ...`node fast-usdc operator attest`.split(' '),
     ...flags({
       previousOfferId: 123,
-      forwardingChannel: 'channel-21',
-      recipientAddress:
-        'agoric16kv2g7snfc4q24vg3pjdlnnqgngtjpwtetd2h689nz09lcklvh5s8u37ek?EUD=dydx183dejcnmkka5dzcu9xw6mywq0p2m5peks28men',
-      blockHash:
-        '0x80d7343e04f8160892e94f02d6a9b9f255663ed0ac34caca98544c8143fee699',
-      blockNumber: 21037669,
-      blockTimestamp: 1730762099,
-      chainId: 1,
-      amount: 300000000,
-      forwardingAddress: 'noble1x0ydg69dh6fqvr27xjvp6maqmrldam6yfelktz',
-      txHash:
-        '0xd81bc6105b60a234c7c50ac17816ebcd5561d366df8bf3be59ff387552761799',
+      forwardingChannel: aux.forwardingChannel,
+      recipientAddress: aux.recipientAddress,
+      amount: tx.amount,
+      forwardingAddress: tx.forwardingAddress,
+      ...flat,
     }),
   ];
   const program = new Command();
@@ -70,26 +62,7 @@ test('fast-usdc operator attest sub-command', async t => {
     offer: {
       id: 'operatorAttest-1234',
       invitationSpec: {
-        invitationArgs: [
-          {
-            aux: {
-              forwardingChannel: 'channel-21',
-              recipientAddress:
-                'agoric16kv2g7snfc4q24vg3pjdlnnqgngtjpwtetd2h689nz09lcklvh5s8u37ek?EUD=dydx183dejcnmkka5dzcu9xw6mywq0p2m5peks28men',
-            },
-            blockHash:
-              '0x80d7343e04f8160892e94f02d6a9b9f255663ed0ac34caca98544c8143fee699',
-            blockNumber: 21037669n,
-            blockTimestamp: 1730762099n,
-            chainId: 1,
-            tx: {
-              amount: 300000000n,
-              forwardingAddress: 'noble1x0ydg69dh6fqvr27xjvp6maqmrldam6yfelktz',
-            },
-            txHash:
-              '0xd81bc6105b60a234c7c50ac17816ebcd5561d366df8bf3be59ff387552761799',
-          },
-        ],
+        invitationArgs: [evidence],
         invitationMakerName: 'SubmitEvidence',
         previousOffer: '123',
         source: 'continuing',
