985c202935e5a1c8a40836419be3fe11c8dd7df2
Jorge-Lopes <jorge.lopes@bytepitch.com>
2024-12-04 10:44:09 +0000

    chore(mintHolder): add core-eval for USDC mintHolder null upgrade

---
+++ packages/builders/scripts/vats/upgrade-mintHolder-usdc.js
@@ -0,0 +1,21 @@
+import { makeHelpers } from '@agoric/deploy-script-support';
+import { getManifestForUpgradingMintHolder } from '@agoric/vats/src/proposals/upgrade-mintHolder-usdc-proposal.js';
+
+/** @type {import('@agoric/deploy-script-support/src/externalTypes.js').CoreEvalBuilder} */
+export const defaultProposalBuilder = async ({ publishRef, install }) =>
+  harden({
+    sourceSpec:
+      '@agoric/vats/src/proposals/upgrade-mintHolder-usdc-proposal.js',
+    getManifestCall: [
+      getManifestForUpgradingMintHolder.name,
+      {
+        contractRef: publishRef(install('@agoric/vats/src/mintHolder.js')),
+      },
+    ],
+  });
+
+/** @type {import('@agoric/deploy-script-support/src/externalTypes.js').DeployScriptFunction} */
+export default async (homeP, endowments) => {
+  const { writeCoreEval } = await makeHelpers(homeP, endowments);
+  await writeCoreEval('upgrade-mintHolder-usdc', defaultProposalBuilder);
+};
