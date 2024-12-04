3695ed5a5f1493b8cec003fddd75e883847c69bc
Jorge-Lopes <jorge.lopes@bytepitch.com>
2024-12-04 20:57:47 +0000

    test(a3p): test evalBundle after genesis-test

--- a3p-integration/proposals/z:acceptance/test.sh
+++ a3p-integration/proposals/z:acceptance/test.sh
@@ -5,31 +5,33 @@ set -ueo pipefail
 # The effects of this step are not persisted in further proposal layers.
 
 # test the state right after the previous proposals
-yarn ava initial.test.js
+# yarn ava initial.test.js
 
-# XXX some of these tests have path dependencies so no globs
-yarn ava core-eval.test.js
+# # XXX some of these tests have path dependencies so no globs
+# yarn ava core-eval.test.js
 
-scripts/test-vaults.ts
+# scripts/test-vaults.ts
 
-echo ACCEPTANCE TESTING kread
-yarn ava kread.test.js
+# echo ACCEPTANCE TESTING kread
+# yarn ava kread.test.js
 
-echo ACCEPTANCE TESTING valueVow
-yarn ava valueVow.test.js
+# echo ACCEPTANCE TESTING valueVow
+# yarn ava valueVow.test.js
 
 echo ACCEPTANCE TESTING state sync
-./state-sync-snapshots-test.sh
+# ./state-sync-snapshots-test.sh
 ./genesis-test.sh
 
-echo ACCEPTANCE TESTING wallet
-yarn ava wallet.test.js
+yarn ava bundle.test.js
 
-echo ACCEPTANCE TESTING psm
-yarn ava psm.test.js
+# echo ACCEPTANCE TESTING wallet
+# yarn ava wallet.test.js
 
-echo ACCEPTANCE TESTING governance
-yarn ava governance.test.js
+# echo ACCEPTANCE TESTING psm
+# yarn ava psm.test.js
 
-echo ACCEPTANCE TESTING vaults
-yarn ava vaults.test.js
+# echo ACCEPTANCE TESTING governance
+# yarn ava governance.test.js
+
+# echo ACCEPTANCE TESTING vaults
+# yarn ava vaults.test.js
