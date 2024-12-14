9bb2d40fc2ef371b7dda513f2f671f9570db4fec
Dan Connolly <connolly@agoric.com>
2024-12-13 18:15:49 -0600

    chore: inject RNG into generateMnemonic

--- multichain-testing/tools/wallet.ts
+++ multichain-testing/tools/wallet.ts
@@ -1,14 +1,16 @@
 import { Bip39, Random } from '@cosmjs/crypto';
 import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
 
-export function generateMnemonic(): string {
-  return Bip39.encode(Random.getBytes(16)).toString();
+export function generateMnemonic(getBytes = Random.getBytes): string {
+  return Bip39.encode(getBytes(16)).toString();
 }
 
 export const createWallet = async (
   bech32Prefix: string,
-  mnemonic: string = generateMnemonic(),
+  mnemonic?: string,
+  { getBytes = Random.getBytes } = {},
 ) => {
+  mnemonic ||= generateMnemonic(getBytes);
   return DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
     prefix: bech32Prefix,
   });
