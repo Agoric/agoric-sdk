72e6fdfef7c6fc076df12a833dd93fd180f2635a
Richard Gibson <richard.gibson@gmail.com>
2024-12-16 12:30:28 -0500

    feat(internal): Add `pick` utility

--- packages/internal/src/index.js
+++ packages/internal/src/index.js
@@ -14,7 +14,3 @@ export * from './typeGuards.js';
 
 // eslint-disable-next-line import/export -- just types
 export * from './types-index.js';
-
-export { objectMap } from '@endo/common/object-map.js';
-export { objectMetaMap } from '@endo/common/object-meta-map.js';
-export { fromUniqueEntries } from '@endo/common/from-unique-entries.js';
