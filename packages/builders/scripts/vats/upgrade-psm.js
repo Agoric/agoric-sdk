a78340415d7d118eab1746e5e465db48d0015529
Ikenna Omekam <ikenna@agoric.com>
2024-12-18 11:07:12 -0500

    feat(psm): add upgrade proposal

---
+++ packages/builders/scripts/vats/upgrade-psm.js
@@ -0,0 +1,21 @@
+import { makeHelpers } from '@agoric/deploy-script-support';
+
+/** @type {import('@agoric/deploy-script-support/src/externalTypes.js').CoreEvalBuilder} */
+export const defaultProposalBuilder = async ({ publishRef, install }) =>
+  harden({
+    sourceSpec: '@agoric/vats/src/proposals/upgrade-psm-proposal.js',
+    getManifestCall: [
+      'getManifestForUpgradingPSM',
+      {
+        psmRef: publishRef(
+          install('@agoric/inter-protocol/src/psm/psm.js'),
+        ),
+      },
+    ],
+  });
+
+/** @type {import('@agoric/deploy-script-support/src/externalTypes.js').DeployScriptFunction} */
+export default async (homeP, endowments) => {
+  const { writeCoreProposal } = await makeHelpers(homeP, endowments);
+  await writeCoreProposal('upgrade-psm', defaultProposalBuilder);
+};
