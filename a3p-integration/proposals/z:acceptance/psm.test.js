5e9b3bc2816c0b8dcc66241de6214a8314b12a9f
anilhelvaci <anilhelvaci96@gmail.com>
2024-12-19 21:15:20 +0300

    fix(acceptance): fix flakey psm test
    Refs: #10728

--- a3p-integration/proposals/z:acceptance/psm.test.js
+++ a3p-integration/proposals/z:acceptance/psm.test.js
@@ -34,7 +34,6 @@ import {
   logRecord,
   maxMintBelowLimit,
   psmSwap,
-  sendOfferAgd,
 } from './test-lib/psm-lib.js';
 import { getBalances } from './test-lib/utils.js';
 
@@ -163,7 +162,7 @@ test.serial('swap into IST using agd with default gas', async t => {
       '--feePct',
       wantMintedFeeVal,
     ],
-    { ...psmSwapIo, sendOffer: sendOfferAgd },
+    psmSwapIo,
   );
 
   await checkSwapSucceeded(t, metricsBefore, balancesBefore, {
