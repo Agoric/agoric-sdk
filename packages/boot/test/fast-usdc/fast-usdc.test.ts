7776298772a6e5178573fc3078ab0a5fbfea9f8a
Dan Connolly <connolly@agoric.com>
2025-01-08 23:52:23 -0600

    feat(boot): distribute FastUSDC contract fee builder, test

--- packages/boot/test/fast-usdc/fast-usdc.test.ts
+++ packages/boot/test/fast-usdc/fast-usdc.test.ts
@@ -5,15 +5,11 @@ import { encodeAddressHook } from '@agoric/cosmic-proto/address-hooks.js';
 import { configurations } from '@agoric/fast-usdc/src/utils/deploy-config.js';
 import { MockCctpTxEvidences } from '@agoric/fast-usdc/test/fixtures.js';
 import { documentStorageSchema } from '@agoric/governance/tools/storageDoc.js';
-import {
-  BridgeId,
-  deeplyFulfilledObject,
-  NonNullish,
-  objectMap,
-} from '@agoric/internal';
+import { BridgeId, NonNullish } from '@agoric/internal';
 import { unmarshalFromVstorage } from '@agoric/internal/src/marshal.js';
 import { defaultSerializer } from '@agoric/internal/src/storage-test-utils.js';
 import { eventLoopIteration } from '@agoric/internal/src/testing-utils.js';
+import { buildVTransferEvent } from '@agoric/orchestration/tools/ibc-mocks.js';
 import { Fail } from '@endo/errors';
 import { makeMarshal } from '@endo/marshal';
 import {
@@ -288,7 +284,9 @@ test.serial('makes usdc advance', async t => {
 
   const EUD = 'dydx1anything';
   const lastNodeValue = storage.getValues('published.fastUsdc').at(-1);
-  const { settlementAccount } = JSON.parse(NonNullish(lastNodeValue));
+  const { settlementAccount, poolAccount } = JSON.parse(
+    NonNullish(lastNodeValue),
+  );
   const evidence = MockCctpTxEvidences.AGORIC_PLUS_OSMO(
     // mock with the real settlementAccount address
     encodeAddressHook(settlementAccount, { EUD }),
@@ -317,15 +315,52 @@ test.serial('makes usdc advance', async t => {
     );
   harness?.resetRunPolicy();
 
-  t.deepEqual(
+  const getTxStatus = txHash =>
     storage
-      .getValues(`published.fastUsdc.txns.${evidence.txHash}`)
-      .map(defaultSerializer.parse),
-    [
-      { evidence, status: 'OBSERVED' }, // observation includes evidence observed
-      { status: 'ADVANCING' },
-    ],
+      .getValues(`published.fastUsdc.txns.${txHash}`)
+      .map(defaultSerializer.parse);
+
+  t.deepEqual(getTxStatus(evidence.txHash), [
+    { evidence, status: 'OBSERVED' }, // observation includes evidence observed
+    { status: 'ADVANCING' },
+  ]);
+
+  const { runInbound } = t.context.bridgeUtils;
+  await runInbound(
+    BridgeId.VTRANSFER,
+    buildVTransferEvent({
+      sender: poolAccount,
+      target: poolAccount,
+      sourceChannel: 'channel-62',
+      sequence: '1',
+    }),
   );
+
+  // in due course, minted USDC arrives
+  await runInbound(
+    BridgeId.VTRANSFER,
+
+    buildVTransferEvent({
+      sequence: '1', // arbitrary; not used
+      amount: evidence.tx.amount,
+      denom: 'uusdc',
+      sender: evidence.tx.forwardingAddress,
+      target: settlementAccount,
+      receiver: encodeAddressHook(settlementAccount, { EUD }),
+      sourceChannel: evidence.aux.forwardingChannel,
+      destinationChannel: 'channel-62', // fetchedChainInfo
+      // destinationChannel: evidence.aux.forwardingChannel,
+      // sourceChannel: 'channel-62', // fetchedChainInfo
+    }),
+  );
+
+  await eventLoopIteration();
+  t.like(getTxStatus(evidence.txHash), [
+    { status: 'OBSERVED' },
+    { status: 'ADVANCING' },
+    { status: 'ADVANCED' },
+    { status: 'DISBURSED', split: { ContractFee: { value: 302000n } } },
+  ]);
 
   const doc = {
     node: `fastUsdc.txns`,
@@ -343,6 +378,32 @@ test.serial('writes pool metrics to vstorage', async t => {
     showValue: defaultSerializer.parse,
   };
   await documentStorageSchema(t, storage, doc);
+});
+
+test.serial('distributes fees per BLD staker decision', async t => {
+  const { walletFactoryDriver: wd, buildProposal, evalProposal } = t.context;
+
+  const ContractFee = 302000n; // see split above
+  t.is(((ContractFee - 250000n) * 5n) / 10n, 26000n);
+  const cases = [
+    { dest: 'agoric1a', args: ['--fixedFees', '0.25'], rxd: '250000' },
+    { dest: 'agoric1b', args: ['--feePortion', '0.5'], rxd: '26000' },
+  ];
+  for (const { dest, args, rxd } of cases) {
+    await wd.provideSmartWallet(dest);
+    const materials = buildProposal(
+      '@agoric/builders/scripts/fast-usdc/fast-usdc-fees.build.js',
+      ['--destinationAddress', dest, ...args],
+    );
+    await evalProposal(materials);
+
+    const { getOutboundMessages } = t.context.bridgeUtils;
+    const found = getOutboundMessages(BridgeId.BANK).find(
+      msg => msg.recipient === dest && msg.type === 'VBANK_GIVE',
+    );
+    t.log('dest vbank msg', found);
+    t.like(found, { amount: rxd });
+  }
 });
 
 test.serial('skips usdc advance when risks identified', async t => {
