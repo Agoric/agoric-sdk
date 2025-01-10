e0d86fdb545ed7ede637cdc67459c957a4d19695
rabi-siddique <rabisiddique27@gmail.com>
2025-01-10 18:10:58 +0500

    fixup! chore: replace cometbft/libs/log with cosmossdk.io/log where required

--- golang/cosmos/daemon/cmd/root.go
+++ golang/cosmos/daemon/cmd/root.go
@@ -10,10 +10,10 @@ import (
 
 	serverconfig "github.com/cosmos/cosmos-sdk/server/config"
 
+	"cosmossdk.io/log"
 	dbm "github.com/cometbft/cometbft-db"
 	tmcfg "github.com/cometbft/cometbft/config"
 	tmcli "github.com/cometbft/cometbft/libs/cli"
-	"github.com/cometbft/cometbft/libs/log"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/config"
 	"github.com/cosmos/cosmos-sdk/client/debug"
