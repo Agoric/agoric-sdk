2409e8e4ef7d9dfe60add89c37dce576b03d56c0
Mujahid Khan <106528609+mujahidkay@users.noreply.github.com>
2025-01-13 18:27:50 +0500

    test(a3p-integration): update to agoric-upgrade-18

--- a3p-integration/proposals/n:upgrade-next/test.sh
+++ a3p-integration/proposals/n:upgrade-next/test.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-set -ueo pipefail
-source /usr/src/upgrade-test-scripts/env_setup.sh
 
-# Place here any test that should be executed using the executed proposal.
-# The effects of this step are not persisted in further proposal layers.
+yarn ava terminateGovernor.test.js
+yarn ava replaceFeeDistributor.test.js
+yarn ava mintHolder.test.js
+yarn ava provisionPool.test.js
 
-test_val \
-  "$(agd q swingset params -o json | jq -Sc .vat_cleanup_budget)" \
-  '[{"key":"default","value":"5"},{"key":"kv","value":"50"}]' \
-  'vat cleanup budget'
+yarn ava agoricNames.test.js
 
-# suppress file names from glob that run earlier
-GLOBIGNORE=initial.test.js
+yarn ava assetReserve.test.js
+yarn ava psm.test.js
 
-# test the state right after upgrade
-yarn ava initial.test.js
-
-# test more, in ways that change system state
-yarn ava ./*.test.js
+yarn ava registry.test.js
