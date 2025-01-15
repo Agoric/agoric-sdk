8eae20c8ea5d387f6fd1cb4f51cc087959af5aff
anilhelvaci <anilhelvaci96@gmail.com>
2025-01-15 10:26:39 +0300

    chore(acceptance): document misnaming validator address
    refs: https://github.com/Agoric/agoric-3-proposals/issues/212
    refs: #10818

--- a3p-integration/proposals/z:acceptance/test.sh
+++ a3p-integration/proposals/z:acceptance/test.sh
@@ -35,6 +35,7 @@ yarn ava governance.test.js
 
 echo ACCEPTANCE TESTING stake BLD
 # XXX correct misnaming
+# UNTIL https://github.com/Agoric/agoric-3-proposals/issues/212
 DELEGATOR_ADDRRESS=$VALIDATORADDR
 VALIDATOR_ADDRESS=$(agd query staking delegations "$DELEGATOR_ADDRRESS" --output json | jq -r ".delegation_responses[0].delegation.validator_address")
 export VALIDATOR_ADDRESS
