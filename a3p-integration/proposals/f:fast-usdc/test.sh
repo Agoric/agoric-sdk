84fd9c2059b0271521a9b7e68c9a11737d28d320
Turadg Aleahmad <turadg@agoric.com>
2025-01-16 06:56:23 -0800

    test: omit from use image for f:fast-usdc

--- a3p-integration/proposals/f:fast-usdc/test.sh
+++ a3p-integration/proposals/f:fast-usdc/test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+# segregate so changing these does not invalidate the proposal image
+# à la https://github.com/Agoric/agoric-3-proposals/pull/213
+cd test
+
 echo AVAILABLE WALLETs
 agd query vstorage children published.wallet
 
