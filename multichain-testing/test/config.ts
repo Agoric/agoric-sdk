rabi-siddique/rs-agoric-sdk

feat: support go-relayer `rly` commands

multichain-testing/test/config.ts
@@ -41,3 +41,8 @@ export const MAKE_ACCOUNT_AND_QUERY_BALANCE_TIMEOUT: RetryOptions = {
   retryIntervalMs: 5000,
   maxRetries: 24,
 };
+
+export const TWO_MINUTES: RetryOptions = {
+  retryIntervalMs: 5000,
+  maxRetries: 24,
+};

Agoric/agoric-sdk

multichain-testing/test/config.ts
@@ -41,3 +41,8 @@ export const MAKE_ACCOUNT_AND_QUERY_BALANCE_TIMEOUT: RetryOptions = {
   retryIntervalMs: 5000,
   maxRetries: 24,
 };
+
+export const ICA_CHANNEL_CLOSE_TIMEOUT: RetryOptions = {
+  retryIntervalMs: 5000,
+  maxRetries: 24,
+};