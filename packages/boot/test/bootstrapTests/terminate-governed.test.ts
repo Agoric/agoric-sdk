a0849f32b32b7a9c6de43331b598b40cad36a465
Siarhei Liakh <siarhei@agoric.com>
2025-01-17 17:29:16 -0500

    feat(bilders): add terminate-governed-instance.js proposal
    refs: #10725
    
    This change includes:
    * A single-file core-eval builder-proposal to terminate governed contracts and their
    governors by boardID.
    * A bootstrap test which:
    ** creates an instance of a simple governed contract via its own core-eval
    ** confirms that the contract accessible and operational
    ** kill the contract instance by boardID
    ** confirms that the contract is no longer operational

--- packages/boot/test/bootstrapTests/terminate-governed.test.ts
+++ packages/boot/test/bootstrapTests/terminate-governed.test.ts
@@ -1,126 +1,22 @@
 import { test as anyTest } from '@agoric/swingset-vat/tools/prepare-test-env-ava.js';
 import type { TestFn } from 'ava';
-import path from 'path';
-import bundleSource from '@endo/bundle-source';
-import { CONTRACT_ELECTORATE, ParamTypes } from '@agoric/governance';
-import { MALLEABLE_NUMBER } from '@agoric/governance/test/swingsetTests/contractGovernor/governedContract.js';
-import { makeAgoricNamesRemotesFromFakeStorage } from '@agoric/vats/tools/board-utils.js';
-
 import { makeSwingsetTestKit } from '../../tools/supports.js';
-import {
-    makeGovernanceDriver,
-    makeWalletFactoryDriver,
-} from '../../tools/drivers.js';
-
-const dirname = path.dirname(new URL(import.meta.url).pathname);
-
-const GOVERNED_CONTRACT_SRC = '../../../governance/test/swingsetTests/contractGovernor/governedContract.js';
-
-const setUpGovernedContract = async (zoe, timer, EV, controller) => {
-    const installBundle = contractBundle => EV(zoe).install(contractBundle);
-    //const installBundleToVatAdmin = contractBundle =>
-    //    controller.validateAndInstallBundle(contractBundle);
-    const source = `${dirname}/${GOVERNED_CONTRACT_SRC}`;
-    const governedContractBundle = await bundleSource(source);
-
-    const agoricNames = await EV.vat('bootstrap').consumeItem('agoricNames');
-    const governorInstallation = await EV(agoricNames).lookup(
-        'installation',
-        'contractGovernor',
-    );
-    const voteCounterInstallation = await EV(agoricNames).lookup(
-        'installation',
-        'binaryVoteCounter',
-    );
-
-    const electorateCreatorFacet = await EV.vat('bootstrap').consumeItem(
-        'economicCommitteeCreatorFacet',
-    );
-    const poserInvitation = await EV(electorateCreatorFacet).getPoserInvitation();
-    const poserInvitation2 = await EV(
-        electorateCreatorFacet,
-    ).getPoserInvitation();
-
-    const invitationIssuer = await EV(zoe).getInvitationIssuer();
-    const invitationAmount =
-        await EV(invitationIssuer).getAmountOf(poserInvitation);
-
-    const governedTerms = {
-        governedParams: {
-            [MALLEABLE_NUMBER]: {
-                type: ParamTypes.NAT,
-                value: 602214090000000000000000n,
-            },
-            [CONTRACT_ELECTORATE]: {
-                type: ParamTypes.INVITATION,
-                value: invitationAmount,
-            },
-        },
-    };
-
-    const governedInstallation = await installBundle(governedContractBundle);
-    const governorTerms = {
-        timer,
-        governedContractInstallation: governedInstallation,
-        governed: {
-            terms: governedTerms,
-            issuerKeywordRecord: {},
-        },
-    };
-
-    const governorFacets = await EV(zoe).startInstance(
-        governorInstallation,
-        {},
-        governorTerms,
-        {
-            governed: {
-                initialPoserInvitation: poserInvitation,
-            },
-        },
-    );
-
-    return {
-        governorFacets,
-        invitationAmount,
-        voteCounterInstallation,
-        poserInvitation2,
-    };
-};
 
 // A more minimal set would be better. We need governance, but not econ vats.
 const PLATFORM_CONFIG = '@agoric/vm-config/decentral-main-vaults-config.json';
 
 const makeDefaultTestContext = async t => {
-    console.time('DefaultTestContext');
     const swingsetTestKit = await makeSwingsetTestKit(t.log, undefined, {
         configSpecifier: PLATFORM_CONFIG,
     });
-
-    const { runUtils, storage, controller } = swingsetTestKit;
-    console.timeLog('DefaultTestContext', 'swingsetTestKit');
+    const { runUtils } = swingsetTestKit;
     const { EV } = runUtils;
+
+    // We need to poke at bootstrap vat and wait for results to allow
+    // SwingSet to finish its boot process before we start the test
     const zoe: ZoeService = await EV.vat('bootstrap').consumeItem('zoe');
-    const timer = await EV.vat('bootstrap').consumeItem('chainTimerService');
 
-    // has to be late enough for agoricNames data to have been published
-    const agoricNamesRemotes = makeAgoricNamesRemotesFromFakeStorage(storage);
-
-    const walletFactoryDriver = await makeWalletFactoryDriver(
-        runUtils,
-        storage,
-        agoricNamesRemotes,
-    );
-
-    const governanceDriver = await makeGovernanceDriver(
-        swingsetTestKit,
-        agoricNamesRemotes,
-        walletFactoryDriver,
-        [],
-    );
-
-    const facets = await setUpGovernedContract(zoe, timer, EV, controller);
-
-    return { ...swingsetTestKit, facets, governanceDriver };
+    return { ...swingsetTestKit };
 };
 
 const test = anyTest as TestFn<
@@ -135,65 +31,42 @@ test.after.always(t => {
     return t.context.shutdown && t.context.shutdown();
 });
 
-/*
-test(`start contract; verify`, async t => {
-    const { runUtils, facets } = t.context;
-    const {
-        governorFacets: { creatorFacet },
-    } = facets;
+test(`Create a contract via core-eval and kill it via core-eval by boardID `, async t => {
+
+    const TEST_CONTRACT_LABEL = 'testContractLabel';
+    const { runUtils, buildProposal, evalProposal } = t.context;
     const { EV } = runUtils;
-    const contractPublicFacet = await EV(creatorFacet).getPublicFacet();
 
-    const avogadro = await EV(contractPublicFacet).getNum();
-    t.is(await EV(contractPublicFacet).getApiCalled(), 0);
-    t.is(avogadro, 602214090000000000000000n);
-});
-*/
+    // create a contract via core-eval
+    const testContractProposalArgs = [TEST_CONTRACT_LABEL,];
+    const creatorProposalMaterials = buildProposal(
+        '@agoric/governance/test/swingsetTests/contractGovernor/add-governedContract.js',
+        testContractProposalArgs,
+    );
+    await evalProposal(creatorProposalMaterials);
 
-test(`Create a contract and acquire a boardId`, async t => {
-    const { runUtils, facets, governanceDriver, storage } = t.context;
-    const {
-        governorFacets: { creatorFacet, instance, adminFacet }, // this is governor's adminFacet
-        voteCounterInstallation: vci,
-    } = facets;
+    const { boardID, publicFacet } = await EV.vat('bootstrap').consumeItem(TEST_CONTRACT_LABEL);
+    console.log({ boardID });
 
-    const { EV } = runUtils;
-    const contractPublicFacet = await EV(creatorFacet).getPublicFacet();
-
-    // set a cookie value in the contract
-    const avogadro = await EV(contractPublicFacet).getNum();
-    t.is(await EV(contractPublicFacet).getApiCalled(), 0);
-    t.is(avogadro, 602214090000000000000000n);
-
-    //const committee = governanceDriver.ecMembers;
-
-    const agoricNamesAdmin =
-        await EV.vat('bootstrap').consumeItem('agoricNamesAdmin');
-    // await EV(agoricNamesAdmin).reserve('governedContract');
-    const instanceAdmin = await EV(agoricNamesAdmin).lookupAdmin('instance');
-    console.log('UPGA ', { instanceAdmin });
-    await EV(instanceAdmin).update('governedContract', instance);
-    const agoricNamesRemotes = makeAgoricNamesRemotesFromFakeStorage(storage);
-
-    const { governedContract } = agoricNamesRemotes.instance;
-    //const board = await EV.vat('bootstrap').consumeItem('board');
-    const boardId = await governedContract.getBoardId();
-
-    console.log({ boardId });
-    // t.context.boardId = boardId;
-
-    // verify the contract works and the cookie matches
-    const num = await EV(contractPublicFacet).getNum();
+    // confirming the contract actually works
+    const num = await EV(publicFacet).getNum();
     t.is(num, 602214090000000000000000n);
 
-    // terminate -- replace with a coreEval
-    console.log({ adminFacet });
-    const contractAdminFacet = await EV(creatorFacet).getAdminFacet();
-    await EV(contractAdminFacet).terminateContract(Error('Reasons'));
+    // killing via terminate-governed-instance
+    const targets = [
+        `${boardID}:${TEST_CONTRACT_LABEL}`,
+    ];
+    console.log({ targets });
+
+    const terminatorProposalMaterials = buildProposal(
+        '@agoric/builders/scripts/vats/terminate-governed-instance.js',
+        targets,
+    );
+    await evalProposal(terminatorProposalMaterials);
 
     // confirm the contract is no longer there
     await t.throwsAsync(
-        () => EV(contractPublicFacet).getNum(),
+        () => EV(publicFacet).getNum(),
         {
             message: 'vat terminated',
         }
---
+++ packages/internal/src/module-utils.js
@@ -0,0 +1,23 @@
+import { readFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { search } from '@endo/compartment-mapper';
+import { Fail } from '@endo/errors';
+
+/**
+ * Get a deep-import specifier for a file, starting with the specifier for its containing module.
+ *
+ * @param {string} fileUrl
+ * @returns {Promise<string>}
+ */
+export const getSpecifier = async fileUrl => {
+    const read = async location => {
+        const path = fileURLToPath(new URL(location, fileUrl));
+        return readFile(path);
+    };
+    const searchResult = await search(read, fileUrl);
+    const { packageDescriptorText, moduleSpecifier } = searchResult;
+    const { name } = JSON.parse(packageDescriptorText);
+    name || Fail`file package has no name`;
+    return `${name}/${moduleSpecifier}`;
+};
+harden(getSpecifier);
