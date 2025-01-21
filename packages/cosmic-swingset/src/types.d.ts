4c7a89e7f0856b04e22467ce6085d1a149d5efae
Turadg Aleahmad <turadg@agoric.com>
2025-01-21 10:07:03 -0800

    feat(types): BridgeMessage ActionContext

--- packages/cosmic-swingset/src/types.d.ts
+++ packages/cosmic-swingset/src/types.d.ts
@@ -4,35 +4,41 @@ import type {
   CoreEvalSDKType,
 } from '@agoric/cosmic-proto/agoric/swingset/swingset.js';
 
-interface BlockInfo {
-  blockHeight: number;
-  blockTime: number;
+// TODO move `walletFlags.js` from @agoric/vats to @agoric/cosmic-proto
+type PowerFlag = 'SMART_WALLET' | 'REMOTE_WALLET';
+
+// NB: keep these manually synced until we build these types out of the Golang
+// structs or use some other shared type truth
+// https://github.com/Agoric/agoric-sdk/issues/8545
+
+interface ActionContext<T extends Uppercase<string>> {
+  type: T;
+  blockHeight: string;
+  blockTime: string;
 }
 
 /**
  * @see coreEvalAction in proposal.go
  */
-export type CoreEvalAction = BlockInfo & {
-  type: 'CORE_EVAL';
+export type CoreEvalAction = ActionContext<'CORE_EVAL'> & {
   evals: CoreEvalSDKType[];
 };
 
 /**
  * @see provisionAction in msg_server.go
  */
-export type PleaseProvisionAction = {
-  type: 'PLEASE_PROVISION';
+export type PleaseProvisionAction = ActionContext<'PLEASE_PROVISION'> & {
   address: string;
   nickname: string;
-  powerFlags: PowerFlags.SMART_WALLET;
+  powerFlags: PowerFlag[];
   submitter: string;
+  autoProvision: boolean;
 };
 
 /**
  * @see VbankBalanceUpdate in vbank.go
  */
-export type VbankBalanceUpdateAction = {
-  type: 'VBANK_BALANCE_UPDATE';
+export type VbankBalanceUpdateAction = ActionContext<'VBANK_BALANCE_UPDATE'> & {
   nonce: string;
   updated: Array<{ address: string; denom: string; amount: string }>;
 };
