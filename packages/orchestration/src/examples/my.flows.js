5f5c743744cbf4945b744c226bd07bb61fcac781
Dan Connolly <connolly@agoric.com>
2025-01-27 22:43:57 -0600

    WIP: makePosition flow stub

--- packages/orchestration/src/examples/my.flows.js
+++ packages/orchestration/src/examples/my.flows.js
@@ -1,13 +1,22 @@
 /**
- * @import {Orchestrator, OrchestrationFlow} from '@agoric/orchestration';
+ * @import {Orchestrator, OrchestrationFlow} from '@agoric/orchestration'
+ * @import {TargetApp} from '@agoric/vats/src/bridge-target'
+ * @import {Passable} from '@endo/pass-style'
  */
 
 /**
  * @satisfies {OrchestrationFlow}
  * @param {Orchestrator} orch
+ * @param {unknown} _ctx
+ * @param {TargetApp & Passable} tap
  */
-export const makeHookAccount = async orch => {
+export const makeHookAccount = async (orch, _ctx, tap) => {
   const agoricChain = await orch.getChain('agoric');
-  return agoricChain.makeAccount();
+  const hookAccount = await agoricChain.makeAccount();
+
+  const registration = hookAccount.monitorTransfers(tap);
+  console.warn('TODO: keep registration', registration);
+
+  return hookAccount;
 };
 harden(makeHookAccount);
