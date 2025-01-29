1c02c60d29c069ec3ecead278fa91660a8e4ff94
Turadg Aleahmad <turadg@agoric.com>
2025-01-28 16:32:40 -0800

    test: include elementShape for type resolution

--- packages/deploy-script-support/test/unitTests/depositInvitation.test.js
+++ packages/deploy-script-support/test/unitTests/depositInvitation.test.js
@@ -3,7 +3,13 @@ import { test } from '@agoric/zoe/tools/prepare-test-env-ava.js';
 
 import { makeIssuerKit, AssetKind, AmountMath } from '@agoric/ertp';
 
+import { InvitationElementShape } from '@agoric/zoe/src/typeGuards.js';
+import { makeHandle } from '@agoric/zoe/src/makeHandle.js';
 import { makeDepositInvitation } from '../../src/depositInvitation.js';
+
+const instance = makeHandle('Instance');
+const installation = makeHandle('Installation');
+const handle = makeHandle('Invitation');
 
 test('depositInvitation', async t => {
   const { mint, issuer, brand } = makeIssuerKit(
@@ -11,9 +17,15 @@ test('depositInvitation', async t => {
     AssetKind.SET,
     undefined,
     undefined,
+    {
+      elementShape: InvitationElementShape,
+    },
   );
   const purse = issuer.makeEmptyPurse();
-  const paymentAmount = AmountMath.make(brand, harden([{ instance: {} }]));
+  const paymentAmount = AmountMath.make(
+    brand,
+    harden([{ description: 'foo', instance, installation, handle }]),
+  );
   const payment = mint.mintPayment(paymentAmount);
   const depositInvitation = makeDepositInvitation(purse);
   const result = await depositInvitation(payment);
