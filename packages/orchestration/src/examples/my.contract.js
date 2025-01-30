c69946448dad4ccec9b4b4c63de2947ea2ee81ff
Dan Connolly <connolly@agoric.com>
2025-01-30 11:57:12 -0600

    WIP: cctp flows foo

--- packages/orchestration/src/examples/my.contract.js
+++ packages/orchestration/src/examples/my.contract.js
@@ -1,20 +1,16 @@
+import { E } from '@endo/far';
 import { M } from '@endo/patterns';
 import { OrchestrationPowersShape } from '../typeGuards.js';
 import { withOrchestration } from '../utils/start-helper.js';
 import * as flows from './my.flows.js';
-import { E } from '@endo/far';
-import { decodeAddressHook } from '@agoric/cosmic-proto/address-hooks.js';
 
 /**
- * @import {FungibleTokenPacketData} from '@agoric/cosmic-proto/ibc/applications/transfer/v2/packet.js';
  * @import {VTransferIBCEvent} from '@agoric/vats'
- */
-const interfaceTODO = undefined;
-
-/**
  * @import {Zone} from '@agoric/zone';
  * @import {OrchestrationTools} from '../types.js';
  */
+
+const interfaceTODO = undefined;
 
 export const meta = M.splitRecord({
   privateArgsShape: {
@@ -26,23 +22,6 @@ export const meta = M.splitRecord({
 harden(meta);
 
 /**
- * cf. CCTP docs
- */
-const domains = {
-  ethereum: 0,
-  solana: 5,
-};
-
-// Left pad the mint recipient address with 0's to 32 bytes
-const frobEthThingy = rawMintRecipient => {
-  const cleanedMintRecipient = rawMintRecipient.replace(/^0x/, '');
-  const zeroesNeeded = 64 - cleanedMintRecipient.length;
-  const mintRecipient = '0'.repeat(zeroesNeeded) + cleanedMintRecipient;
-  const buffer = Buffer.from(mintRecipient, 'hex');
-  return new Uint8Array(buffer);
-};
-
-/**
  * @param {any} _zcf
  * @param {any} _privateArgs
  * @param {Zone} zone
@@ -51,64 +30,43 @@ const frobEthThingy = rawMintRecipient => {
  */
 export const contract = async (_zcf, _privateArgs, zone, tools) => {
   const { orchestrateAll } = tools;
-  const { makeHookAccount, sendToEth } = orchestrateAll(flows, {});
-
-  const hookAccountV = zone.makeOnce('hookAccount', _key =>
-    makeHookAccount(tap),
+  const { makeHookAccount, makeNobleAccount, sendToEth } = orchestrateAll(
+    flows,
+    {},
   );
-
-  const nobleAccountV = zone.makeOnce('nobleAccount', _key =>
-    makeNobleAccount(),
-  );
-
-  const { vowTools } = tools;
-  const hookAccountP = vowTools.when(hookAccountV);
 
   const tap = zone.makeOnce('tapPosition', _key => {
     console.log('making tap');
     /** @satisfies {import('@agoric/vats/src/bridge-target.js').TargetApp} */
     const tap = zone.exo('tap', interfaceTODO, {
-      /** @param {VTransferIBCEvent} event */
+      /** @param {VTransferIBCEvent & import('@endo/marshal').Passable} event */
       async receiveUpcall(event) {
+        harden(event);
         console.log(event);
 
-        const { packet } = event;
-        // IOU: if (packet.source_channel !== sourceChannel)
-
-        const tx = /** @type {FungibleTokenPacketData} */ (
-          JSON.parse(atob(packet.data))
-        );
-        console.log('@@@@tx', tx);
-
-        const { query } = decodeAddressHook(tx.receiver);
-        const { dest } = query;
-        assert.typeof(dest, 'string');
-
-        const { amount, denom: burnToken } = tx;
-        assert.equal(burnToken, 'uusdc');
-        const { value: from } = await E(hookAccountP).getAddress();
-
-        const msg = {
-          typeUrl: '/circle.cctp.v1.MsgDepositForBurn',
-          value: {
-            from,
-            amount,
-            destinationDomain: domains.ethereum,
-            mintRecipient: frobEthThingy(dest),
-            burnToken,
-          },
-        };
-
-        // we have an issue about this, no?
-        const sent = await E(hookAccountP).executeTx([msg]);
+        // TODO: vowTools.watch(sendTo..., handler)
+        await vowTools.when(sendToEth(event, accounts));
       },
     });
     return tap;
   });
 
+  const { vowTools } = tools;
+  const hookAccountV = zone.makeOnce('hookAccount', _key =>
+    makeHookAccount(tap),
+  );
+  const nobleAccountV = zone.makeOnce('nobleAccount', _key =>
+    makeNobleAccount(),
+  );
+  const accounts = {
+    hook: await vowTools.when(hookAccountV),
+    noble: await vowTools.when(nobleAccountV),
+  };
+  const hookAddress = await E(accounts.hook).getAddress();
+
   return {
     publicFacet: zone.exo('MyPub', undefined, {
-      getHookAddress: () => E(hookAccountP).getAddress(),
+      getHookAddress: () => hookAddress,
     }),
     creatorFacet: zone.exo('MyCreator', undefined, {}),
   };
--- packages/orchestration/src/examples/my.flows.js
+++ packages/orchestration/src/examples/my.flows.js
@@ -1,8 +1,12 @@
 /**
- * @import {Orchestrator, OrchestrationFlow, ChainAddress} from '@agoric/orchestration'
+ * @import {FungibleTokenPacketData} from '@agoric/cosmic-proto/ibc/applications/transfer/v2/packet.js';
+ * @import {AgoricChainMethods, ChainAddress, IcaAccountMethods, LocalAccountMethods, Orchestrator, OrchestrationFlow, OrchestrationAccount} from '@agoric/orchestration'
  * @import {TargetApp} from '@agoric/vats/src/bridge-target'
- * @import {Passable} from '@endo/pass-style'
+ * @import {CopyRecord, Passable} from '@endo/pass-style'
  */
+
+import { Nat } from '@endo/nat';
+import { decodeAddressHook } from '@agoric/cosmic-proto/address-hooks.js';
 
 /**
  * @satisfies {OrchestrationFlow}
@@ -21,25 +25,72 @@ export const makeHookAccount = async (orch, _ctx, tap) => {
 };
 harden(makeHookAccount);
 
-const destinationDomain = 0;
 /**
  * @satisfies {OrchestrationFlow}
  * @param {Orchestrator} orch
  * @param {unknown} _ctx
- * @param {ChainAddress['value']} from
- * @param {{ denom: string; amount: string }} howMuch
- * @param {import('@agoric/cosmic-proto/address-hooks.js').HookQuery} hookStuff
  */
-export const sendToEth = async (
-  orch,
-  _ctx,
-  from,
-  { denom, amount },
-  hookStuff,
-) => {
-  console.log('@@@', { denom, amount });
+export const makeNobleAccount = async (orch, _ctx) => {
+  const chainNoble = await orch.getChain('noble');
+  const nobleAccount = await chainNoble.makeAccount();
+  return nobleAccount;
+};
 
+/**
+ * @typedef {CopyRecord & {
+ *   hook: OrchestrationAccount<any> & LocalAccountMethods;
+ *   noble: OrchestrationAccount<any> & IcaAccountMethods;
+ * }} Accounts
+ */
+
+/**
+ * cf. CCTP docs
+ */
+const domains = {
+  ethereum: 0,
+  solana: 5,
+};
+
+// Left pad the mint recipient address with 0's to 32 bytes
+const frobEthThingy = rawMintRecipient => {
+  const cleanedMintRecipient = rawMintRecipient.replace(/^0x/, '');
+  const zeroesNeeded = 64 - cleanedMintRecipient.length;
+  const mintRecipient = '0'.repeat(zeroesNeeded) + cleanedMintRecipient;
+  const buffer = Buffer.from(mintRecipient, 'hex');
+  return new Uint8Array(buffer);
+};
+
+/**
+ * @satisfies {OrchestrationFlow}
+ * @param {Orchestrator} orch
+ * @param {unknown} _ctx
+ * @param {import('@agoric/vats').VTransferIBCEvent & Passable} event
+ * @param {Accounts} accounts
+ */
+export const sendToEth = async (orch, _ctx, event, accounts) => {
+  const { packet } = event;
+  // IOU: if (packet.source_channel !== sourceChannel)
+
+  const tx = /** @type {FungibleTokenPacketData} */ (
+    JSON.parse(atob(packet.data))
+  );
+  console.log('@@@@tx', tx);
+
+  const { amount, denom: denom } = tx;
   assert.equal(denom, 'uusdc');
+
+  const hookAcct = await accounts.hook;
+  const nobleAcct = await accounts.noble;
+  const nobleAddr = await nobleAcct.getAddress();
+  await hookAcct.transfer(nobleAddr, {
+    denom: denom,
+    value: Nat(BigInt(amount)),
+  });
+  const { value: from } = await hookAcct.getAddress();
+
+  const { query } = decodeAddressHook(tx.receiver);
+  const { dest } = query;
+  assert.typeof(dest, 'string');
 
   const msg = {
     typeUrl: '/circle.cctp.v1.MsgDepositForBurn',
@@ -47,7 +98,7 @@ export const sendToEth = async (
       from,
       amount,
       destinationDomain: domains.ethereum,
-      mintRecipient: mintRecipientBytes,
+      mintRecipient: frobEthThingy(dest),
       burnToken: denom,
     },
   };
