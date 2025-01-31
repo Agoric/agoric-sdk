b4475db81dc19917433c9db69b3ad2cb84f33b07
Dan Connolly <connolly@agoric.com>
2025-01-31 15:16:55 -0600

    test: Fast USDC chain impact test (WIP)
     - access kernel stats post bootstrap

---
+++ packages/boot/test/fast-usdc/chain-impact.test.ts
@@ -0,0 +1,31 @@
+import { test as anyTest } from '@agoric/zoe/tools/prepare-test-env-ava.js';
+import type { TestFn } from 'ava';
+
+import {
+  makeWalletFactoryContext,
+  type WalletFactoryTestContext,
+} from '../bootstrapTests/walletFactory.js';
+
+const test: TestFn<WalletFactoryTestContext> = anyTest;
+
+const config = '@agoric/vm-config/decentral-itest-orchestration-config.json';
+
+test.before(async t => {
+  const { env } = globalThis.process;
+  const {
+    SLOGFILE: slogFile,
+    SWINGSET_WORKER_TYPE: defaultManagerType = 'local', // or 'xs-worker',
+  } = env;
+  t.context = await makeWalletFactoryContext(t, config, {
+    slogFile,
+    defaultManagerType,
+  });
+});
+test.after.always(t => t.context.shutdown?.());
+
+test('access kernel stats after bootstrap', async t => {
+  const { controller } = t.context;
+  const stats = controller.getStats();
+  t.log('kernel stats', stats);
+  t.truthy(stats);
+});
