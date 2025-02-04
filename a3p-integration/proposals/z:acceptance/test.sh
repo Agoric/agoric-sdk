341faa7cfa0b305584861e5808890c3afe519bc5
Thomas Greco <6646552+tgrecojs@users.noreply.github.com>
2025-02-04 16:07:18 -0500

    Discard changes to a3p-integration/proposals/z:acceptance/test.sh

--- a3p-integration/proposals/z:acceptance/test.sh
+++ a3p-integration/proposals/z:acceptance/test.sh
@@ -12,37 +12,41 @@ cd test
 yarn ava initial.test.js
 
 # XXX some of these tests have path dependencies so no globs
+yarn ava core-eval.test.js
 
-yarn ava airdrop.test.js
+scripts/test-vaults.ts
 
-# echo ACCEPTANCE TESTING kread
-# yarn ava kread.test.js
+echo ACCEPTANCE TESTING recorded instances
+yarn ava recorded-retired.test.js
 
-# echo ACCEPTANCE TESTING valueVow
-# yarn ava valueVow.test.js
+echo ACCEPTANCE TESTING kread
+yarn ava kread.test.js
 
-# echo ACCEPTANCE TESTING wallet
-# yarn ava wallet.test.js
+echo ACCEPTANCE TESTING valueVow
+yarn ava valueVow.test.js
 
-# echo ACCEPTANCE TESTING psm
-# yarn ava psm.test.js
+echo ACCEPTANCE TESTING wallet
+yarn ava wallet.test.js
 
-# echo ACCEPTANCE TESTING vaults
-# yarn ava vaults.test.js
+echo ACCEPTANCE TESTING psm
+yarn ava psm.test.js
 
-# echo ACCEPTANCE TESTING governance
-# yarn ava governance.test.js
+echo ACCEPTANCE TESTING vaults
+yarn ava vaults.test.js
 
-# echo ACCEPTANCE TESTING stake BLD
-# # XXX correct misnaming
-# # UNTIL https://github.com/Agoric/agoric-3-proposals/issues/212
-# DELEGATOR_ADDRRESS=$VALIDATORADDR
-# VALIDATOR_ADDRESS=$(agd query staking delegations "$DELEGATOR_ADDRRESS" --output json | jq -r ".delegation_responses[0].delegation.validator_address")
-# export VALIDATOR_ADDRESS
-# echo "VALIDATOR_ADDRESS: $VALIDATOR_ADDRESS from delegator $DELEGATOR_ADDRRESS (named 'VALIDATORADDR' in env)"
+echo ACCEPTANCE TESTING governance
+yarn ava governance.test.js
 
-# yarn ava stakeBld.test.js
+echo ACCEPTANCE TESTING stake BLD
+# XXX correct misnaming
+# UNTIL https://github.com/Agoric/agoric-3-proposals/issues/212
+DELEGATOR_ADDRRESS=$VALIDATORADDR
+VALIDATOR_ADDRESS=$(agd query staking delegations "$DELEGATOR_ADDRRESS" --output json | jq -r ".delegation_responses[0].delegation.validator_address")
+export VALIDATOR_ADDRESS
+echo "VALIDATOR_ADDRESS: $VALIDATOR_ADDRESS from delegator $DELEGATOR_ADDRRESS (named 'VALIDATORADDR' in env)"
 
-# echo ACCEPTANCE TESTING state sync
-# ./state-sync-snapshots-test.sh
-# ./genesis-test.sh
+yarn ava stakeBld.test.js
+
+echo ACCEPTANCE TESTING state sync
+./state-sync-snapshots-test.sh
+./genesis-test.sh
