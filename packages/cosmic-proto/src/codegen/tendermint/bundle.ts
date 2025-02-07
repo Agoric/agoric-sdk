e3304b91f9cee852a81bb7c3e8e18853d58278ca
0xPatrick <patrick@0xpatrick.dev>
2025-02-06 20:43:54 -0500

    chore: `circle/cctp/v1` protos
    - sourced from https://github.com/circlefin/noble-cctp/tree/38595d10898712dd03c848662be42cd038e25d56/proto
    - includes `yarn codegen`
    - mainly for `/circle.cctp.v1.MsgDepositForBurn`

--- packages/cosmic-proto/src/codegen/tendermint/bundle.ts
+++ packages/cosmic-proto/src/codegen/tendermint/bundle.ts
@@ -1,39 +1,39 @@
 //@ts-nocheck
-import * as _125 from './abci/types.js';
-import * as _126 from './crypto/keys.js';
-import * as _127 from './crypto/proof.js';
-import * as _128 from './libs/bits/types.js';
-import * as _129 from './p2p/types.js';
-import * as _130 from './types/block.js';
-import * as _131 from './types/evidence.js';
-import * as _132 from './types/params.js';
-import * as _133 from './types/types.js';
-import * as _134 from './types/validator.js';
-import * as _135 from './version/types.js';
+import * as _140 from './abci/types.js';
+import * as _141 from './crypto/keys.js';
+import * as _142 from './crypto/proof.js';
+import * as _143 from './libs/bits/types.js';
+import * as _144 from './p2p/types.js';
+import * as _145 from './types/block.js';
+import * as _146 from './types/evidence.js';
+import * as _147 from './types/params.js';
+import * as _148 from './types/types.js';
+import * as _149 from './types/validator.js';
+import * as _150 from './version/types.js';
 export namespace tendermint {
   export const abci = {
-    ..._125,
+    ..._140,
   };
   export const crypto = {
-    ..._126,
-    ..._127,
+    ..._141,
+    ..._142,
   };
   export namespace libs {
     export const bits = {
-      ..._128,
+      ..._143,
     };
   }
   export const p2p = {
-    ..._129,
+    ..._144,
   };
   export const types = {
-    ..._130,
-    ..._131,
-    ..._132,
-    ..._133,
-    ..._134,
+    ..._145,
+    ..._146,
+    ..._147,
+    ..._148,
+    ..._149,
   };
   export const version = {
-    ..._135,
+    ..._150,
   };
 }
