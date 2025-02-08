fc2526642f9a6b2c78a013c5f289718250375c0f
Chris Hibbert <hibbert@agoric.com>
2025-02-07 16:15:33 -0800

    chore: gofmt  upgrade.go

--- golang/cosmos/app/upgrade.go
+++ golang/cosmos/app/upgrade.go
@@ -183,8 +183,8 @@ func unreleasedUpgradeHandler(app *GaiaApp, targetUpgrade string) func(sdk.Conte
 			// Each CoreProposalStep runs sequentially, and can be constructed from
 			// one or more modules executing in parallel within the step.
 			CoreProposalSteps = append(CoreProposalSteps,
-            	// because of #10794, we need to do at least a null upgrade of
-            	// the walletFactory on every software upgrade
+				// because of #10794, we need to do at least a null upgrade of
+				// the walletFactory on every software upgrade
 				vm.CoreProposalStepForModules(
 					"@agoric/builders/scripts/smart-wallet/build-wallet-factory2-upgrade.js",
 				),
