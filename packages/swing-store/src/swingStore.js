02b70f4b49ff2fcca6548f879b03bd53c82e3f03
Richard Gibson <richard.gibson@gmail.com>
2025-02-10 21:18:48 -0500

    feat(internal): Generalize single-level `pick` utility to recursive `attenuate`

--- packages/swing-store/src/swingStore.js
+++ packages/swing-store/src/swingStore.js
@@ -7,7 +7,8 @@ import sqlite3 from 'better-sqlite3';
 
 import { Fail, q } from '@endo/errors';
 
-import { pick } from '@agoric/internal';
+import { attenuate } from '@agoric/internal';
+import { TRUE } from '@agoric/internal/src/js-utils.js';
 
 import { dbFileInDirectory } from './util.js';
 import { makeKVStore, getKeyType } from './kvStore.js';
@@ -565,41 +566,32 @@ export function makeSwingStore(path, forceReset, options = {}) {
     return db;
   }
 
-  const transcriptStore = pick(
-    transcriptStoreInternal,
-    /** @type {const} */ ({
-      initTranscript: true,
-      rolloverSpan: true,
-      rolloverIncarnation: true,
-      getCurrentSpanBounds: true,
-      addItem: true,
-      readSpan: true,
-      stopUsingTranscript: true,
-      deleteVatTranscripts: true,
-    }),
-  );
+  const transcriptStore = attenuate(transcriptStoreInternal, {
+    initTranscript: TRUE,
+    rolloverSpan: TRUE,
+    rolloverIncarnation: TRUE,
+    getCurrentSpanBounds: TRUE,
+    addItem: TRUE,
+    readSpan: TRUE,
+    stopUsingTranscript: TRUE,
+    deleteVatTranscripts: TRUE,
+  });
 
-  const snapStore = pick(
-    snapStoreInternal,
-    /** @type {const} */ ({
-      loadSnapshot: true,
-      saveSnapshot: true,
-      deleteAllUnusedSnapshots: true,
-      deleteVatSnapshots: true,
-      stopUsingLastSnapshot: true,
-      getSnapshotInfo: true,
-    }),
-  );
+  const snapStore = attenuate(snapStoreInternal, {
+    loadSnapshot: TRUE,
+    saveSnapshot: TRUE,
+    deleteAllUnusedSnapshots: TRUE,
+    deleteVatSnapshots: TRUE,
+    stopUsingLastSnapshot: TRUE,
+    getSnapshotInfo: TRUE,
+  });
 
-  const bundleStore = pick(
-    bundleStoreInternal,
-    /** @type {const} */ ({
-      addBundle: true,
-      hasBundle: true,
-      getBundle: true,
-      deleteBundle: true,
-    }),
-  );
+  const bundleStore = attenuate(bundleStoreInternal, {
+    addBundle: TRUE,
+    hasBundle: TRUE,
+    getBundle: TRUE,
+    deleteBundle: TRUE,
+  });
 
   const kernelStorage = {
     kvStore: kernelKVStore,
