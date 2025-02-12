916a907c18e2bc0de3fdd3538cc9936d0b44e898
Siarhei Liakh <siarhei@agoric.com>
2025-02-12 16:27:39 -0500

    feat(cosmos:vstorage): report vstorage size delta for set() operations
    refs: #10938

--- golang/cosmos/x/vstorage/vstorage.go
+++ golang/cosmos/x/vstorage/vstorage.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	agoric "github.com/Agoric/agoric-sdk/golang/cosmos/types"
@@ -39,6 +40,49 @@ func unmarshalSinglePathFromArgs(args []json.RawMessage, path *string) error {
 		return fmt.Errorf("extra arguments after 'path'")
 	}
 	return json.Unmarshal(args[0], path)
+}
+
+const vstorageMetricKey = "vstorage_size_delta"
+
+// reportKVEntrySizeDelta reports the size delta of a KVEntry to the telemetry
+// system. The size delta is the change in size of the key and value of the
+// KVEntry. The label is used to identify the operation that caused the size
+// delta.
+// The function uses simplified accounting for the size of the key: a full size
+// of the key is used regardless of whether it shares a prefix with aby other key
+// This tends to overestimate the total storage size consumed by the keys, but
+// balances out when the keys are deleted by setting them with nil values.
+func reportKVEntrySizeDelta(ctx sdk.Context, keeper Keeper, newEntry agoric.KVEntry, label string) {
+	var oldEntry agoric.KVEntry
+	var valueSizeDelta int = 0
+	var keySizeDelta int = 0
+
+	if !newEntry.IsValidKey() {
+		// invalid key - no change in size
+		return
+	}
+
+	// get the old entry to compare
+	oldEntry = keeper.GetEntry(ctx, newEntry.Key())
+
+	if !oldEntry.HasValue() && !newEntry.HasValue() {
+		// There is no change in size if both the old and new entries have no value
+		return
+	} else if oldEntry.HasValue() && newEntry.HasValue() {
+		// Updating the existing value, no change in key size
+		valueSizeDelta = len(*newEntry.Value()) - len(*oldEntry.Value())
+	} else if oldEntry.HasValue() && !newEntry.HasValue() {
+		// Deleting the value, removing the key
+		valueSizeDelta = -len(*oldEntry.Value())
+		keySizeDelta = -len(oldEntry.Key())
+	} else {
+		// !oldEntry.HasValue() && newEntry.HasValue()
+		// Adding a new value, adding the key
+		valueSizeDelta = len(*newEntry.Value())
+		keySizeDelta = len(newEntry.Key())
+	}
+
+	telemetry.IncrCounter(float32(keySizeDelta + valueSizeDelta), vstorageMetricKey, label)
 }
 
 func (sh vstorageHandler) Receive(cctx context.Context, str string) (ret string, err error) {
@@ -75,6 +119,7 @@ func (sh vstorageHandler) Receive(cctx context.Context, str string) (ret string,
 			if err != nil {
 				return
 			}
+			reportKVEntrySizeDelta(ctx, keeper, entry, msg.Method)
 			keeper.SetStorageAndNotify(ctx, entry)
 		}
 		return "true", nil
@@ -90,6 +135,7 @@ func (sh vstorageHandler) Receive(cctx context.Context, str string) (ret string,
 				return
 			}
 			//fmt.Printf("giving Keeper.SetStorage(%s) %s\n", entry.Path(), entry.Value())
+			reportKVEntrySizeDelta(ctx, keeper, entry, msg.Method)
 			keeper.LegacySetStorageAndNotify(ctx, entry)
 		}
 		return "true", nil
@@ -101,6 +147,7 @@ func (sh vstorageHandler) Receive(cctx context.Context, str string) (ret string,
 			if err != nil {
 				return
 			}
+			reportKVEntrySizeDelta(ctx, keeper, entry, msg.Method)
 			keeper.SetStorage(ctx, entry)
 		}
 		return "true", nil
