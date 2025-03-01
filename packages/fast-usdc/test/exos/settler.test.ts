169d88fcfa1df4057844b143e8cb92af2a2fca83
Chris Hibbert <hibbert@agoric.com>
2025-02-28 17:37:07 -0800

    test: update tests for loss of 'Observed' state, etc.

--- packages/fast-usdc/test/exos/settler.test.ts
+++ packages/fast-usdc/test/exos/settler.test.ts
@@ -171,18 +171,6 @@ const makeTestContext = async t => {
         return cctpTxEvidence;
       },
       /**
-       * slow path - e.g. insufficient pool funds
-       * @param evidence
-       */
-      observe: (evidence?: CctpTxEvidence) => {
-        const cctpTxEvidence = makeEvidence(evidence);
-        t.log('Mock CCTP Evidence:', cctpTxEvidence);
-        t.log('Pretend we `OBSERVED` (did not advance)');
-        statusManager.observe(cctpTxEvidence);
-
-        return cctpTxEvidence;
-      },
-      /**
        * mint early path. caller must simulate tap before calling
        * @param evidence
        */
@@ -307,7 +295,7 @@ test('happy path: disburse to LPs; StatusManager removes tx', async t => {
   await eventLoopIteration();
   const { storage } = t.context;
   t.deepEqual(storage.getDeserialized(`fun.txns.${cctpTxEvidence.txHash}`), [
-    { evidence: cctpTxEvidence, status: 'OBSERVED' },
+    { evidence: cctpTxEvidence, status: undefined },
     { status: 'ADVANCING' },
     { status: 'ADVANCED' },
     { split: expectedSplit, status: 'DISBURSED' },
@@ -338,15 +326,7 @@ test('slow path: forward to EUD; remove pending tx', async t => {
     ...defaultSettlerParams,
   });
   const simulate = makeSimulate(settler.notifier);
-  const cctpTxEvidence = simulate.observe();
-  t.deepEqual(
-    statusManager.lookupPending(
-      cctpTxEvidence.tx.forwardingAddress,
-      cctpTxEvidence.tx.amount,
-    ),
-    [{ ...cctpTxEvidence, status: PendingTxStatus.Observed }],
-    'statusManager shows this tx is only observed',
-  );
+  const cctpTxEvidence = simulate.skipAdvance(['TOO_LARGE_AMOUNT']);
 
   t.log('Simulate incoming IBC settlement');
   void settler.tap.receiveUpcall(MockVTransferEvents.AGORIC_PLUS_OSMO());
@@ -383,7 +363,8 @@ test('slow path: forward to EUD; remove pending tx', async t => {
   accounts.settlement.transferVResolver.resolve(undefined);
   await eventLoopIteration();
   t.deepEqual(storage.getDeserialized(`fun.txns.${cctpTxEvidence.txHash}`), [
-    { evidence: cctpTxEvidence, status: 'OBSERVED' },
+    { evidence: cctpTxEvidence, status: undefined },
+    { risksIdentified: ['TOO_LARGE_AMOUNT'], status: 'ADVANCE_SKIPPED' },
     { status: 'FORWARDED' },
   ]);
 
@@ -458,7 +439,7 @@ test('skip advance: forward to EUD; remove pending tx', async t => {
   );
   const { storage } = t.context;
   t.deepEqual(storage.getDeserialized(`fun.txns.${cctpTxEvidence.txHash}`), [
-    { evidence: cctpTxEvidence, status: 'OBSERVED' },
+    { evidence: cctpTxEvidence, status: undefined },
     { status: 'ADVANCE_SKIPPED', risksIdentified: ['TOO_LARGE_AMOUNT'] },
     { status: 'FORWARDED' },
   ]);
@@ -533,7 +514,7 @@ test('Settlement for unknown transaction (minted early)', async t => {
   accounts.settlement.transferVResolver.resolve(undefined);
   await eventLoopIteration();
   t.deepEqual(storage.getDeserialized(`fun.txns.${evidence.txHash}`), [
-    { evidence, status: 'OBSERVED' },
+    { evidence, status: undefined },
     { status: 'FORWARDED' },
   ]);
 });
@@ -628,7 +609,7 @@ test('Multiple minted early transactions with same address and amount', async t 
   ]);
 
   // Simulate a third transaction and verify no more are tracked as minted early
-  simulate.observe({
+  simulate.observeLate({
     ...MockCctpTxEvidences.AGORIC_PLUS_OSMO(),
     txHash:
       '0x0000000000000000000000000000000000000000000000000000000000000001',
@@ -700,7 +681,7 @@ test('Settlement for Advancing transaction (advance succeeds)', async t => {
   simulate.finishAdvance(cctpTxEvidence, true);
   await eventLoopIteration();
   t.deepEqual(storage.getDeserialized(`fun.txns.${cctpTxEvidence.txHash}`), [
-    { evidence: cctpTxEvidence, status: 'OBSERVED' },
+    { evidence: cctpTxEvidence, status: undefined },
     { status: 'ADVANCING' },
     { status: 'ADVANCED' },
     { split: expectedSplit, status: 'DISBURSED' },
@@ -764,7 +745,7 @@ test('Settlement for Advancing transaction (advance fails)', async t => {
   accounts.settlement.transferVResolver.resolve(undefined);
   await eventLoopIteration();
   t.deepEqual(storage.getDeserialized(`fun.txns.${cctpTxEvidence.txHash}`), [
-    { evidence: cctpTxEvidence, status: 'OBSERVED' },
+    { evidence: cctpTxEvidence, status: undefined },
     { status: 'ADVANCING' },
     { status: 'ADVANCE_FAILED' },
     { status: 'FORWARDED' },
@@ -775,12 +756,11 @@ test('slow path, and forward fails (terminal state)', async t => {
   const {
     common,
     makeSettler,
-    statusManager,
     defaultSettlerParams,
     repayer,
     makeSimulate,
+    inspectLogs,
     accounts,
-    peekCalls,
     storage,
   } = t.context;
   const { usdc } = common.brands;
@@ -790,43 +770,32 @@ test('slow path, and forward fails (terminal state)', async t => {
     settlementAccount: accounts.settlement.account,
     ...defaultSettlerParams,
   });
-  const simulate = makeSimulate(settler.notifier);
-  const cctpTxEvidence = simulate.observe();
-  t.deepEqual(
-    statusManager.lookupPending(
-      cctpTxEvidence.tx.forwardingAddress,
-      cctpTxEvidence.tx.amount,
-    ),
-    [{ ...cctpTxEvidence, status: PendingTxStatus.Observed }],
-    'statusManager shows this tx is only observed',
-  );
-
-  t.log('Simulate incoming IBC settlement');
-  void settler.tap.receiveUpcall(MockVTransferEvents.AGORIC_PLUS_OSMO());
-  await eventLoopIteration();
-
-  t.log('funds are forwarded; no interaction with LP');
-  t.like(accounts.settlement.callLog, [
-    [
-      'transfer',
-      'cosmos:osmosis-1:osmo183dejcnmkka5dzcu9xw6mywq0p2m5peks28men',
-      usdc.units(150),
-      {
-        forwardOpts: {
-          intermediateRecipient: {
-            value: 'noble1test',
-          },
-        },
-      },
-    ],
-  ]);
 
   t.log('simulating forward failure (e.g. unknown route)');
   const mockE = Error('no connection info found');
   accounts.settlement.transferVResolver.reject(mockE);
   await eventLoopIteration();
-  t.deepEqual(storage.getDeserialized(`fun.txns.${cctpTxEvidence.txHash}`), [
-    { evidence: cctpTxEvidence, status: 'OBSERVED' },
+
+  t.log('Simulate incoming IBC settlement');
+  void settler.tap.receiveUpcall(MockVTransferEvents.AGORIC_PLUS_OSMO());
+  await eventLoopIteration();
+  const simulate = makeSimulate(settler.notifier);
+
+  const evidence = simulate.observeLate();
+
+  const rawLogs = inspectLogs();
+  const penultimateLog = rawLogs.slice(rawLogs.length - 2, rawLogs.length - 1);
+  await eventLoopIteration();
+  t.deepEqual(penultimateLog, [
+    [
+      '⚠️ tap: minted before observed',
+      'noble1x0ydg69dh6fqvr27xjvp6maqmrldam6yfelqkd',
+      150000000n,
+    ],
+  ]);
+
+  t.deepEqual(storage.getDeserialized(`fun.txns.${evidence.txHash}`), [
+    { evidence, status: undefined },
     { status: 'FORWARD_FAILED' },
   ]);
 });
