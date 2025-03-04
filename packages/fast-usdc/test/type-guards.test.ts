1169211a9b2342a973db031c9946e430ed273f67
Chris Hibbert <hibbert@agoric.com>
2025-03-04 13:02:07 -0800

    chore: revert type in test

--- packages/fast-usdc/test/type-guards.test.ts
+++ packages/fast-usdc/test/type-guards.test.ts
@@ -22,7 +22,7 @@ test('CctpTxEvidenceShape', t => {
 test('PendingTxShape', t => {
   const specimen: CctpTxEvidence & { status: TxStatus } = harden({
     ...MockCctpTxEvidences.AGORIC_PLUS_OSMO(),
-    status: PendingTxStatus.AdvanceFailed,
+    status: PendingTxStatus.Observed,
   });
 
   t.notThrows(() => mustMatch(specimen, PendingTxShape));
