2a07b65949eb6fa051a728854a34a70ba47a64a8
rabi-siddique <rabisiddique27@gmail.com>
2025-03-13 14:04:45 +0500

    chore: update contract and flows

---
+++ packages/orchestration/src/examples/axelar-evm.flows.js
@@ -0,0 +1,70 @@
+import { Fail } from '@endo/errors';
+import { denomHash } from '@agoric/orchestration/src/utils/denomHash.js';
+
+/**
+ * @import {GuestInterface} from '@agoric/async-flow';
+ * @import {Orchestrator, OrchestrationFlow} from '@agoric/orchestration';
+ * @import {MakeEvmTap} from './evm-tap-kit.js';
+ * @import {MakePortfolioHolder} from '@agoric/orchestration/src/exos/portfolio-holder-kit.js';
+ * @import {ChainHub} from '@agoric/orchestration/src/exos/chain-hub.js';
+ */
+
+/**
+ * @satisfies {OrchestrationFlow}
+ * @param {Orchestrator} orch
+ * @param {{
+ *   makeEvmTap: MakeEvmTap;
+ *   makePortfolioHolder: MakePortfolioHolder;
+ *   chainHub: GuestInterface<ChainHub>;
+ * }} ctx
+ * @param {ZCFSeat} seat
+ * @param {{
+ *   chainName: string;
+ * }} offerArgs
+ */
+export const createAndMonitorLCA = async (
+  orch,
+  { makeEvmTap, chainHub },
+  seat,
+  { chainName },
+) => {
+  seat.exit();
+  const [agoric, remoteChain] = await Promise.all([
+    orch.getChain('agoric'),
+    orch.getChain(chainName),
+  ]);
+  const { chainId, stakingTokens } = await remoteChain.getChainInfo();
+  const remoteDenom = stakingTokens[0].denom;
+  remoteDenom ||
+    Fail`${chainId || chainName} does not have stakingTokens in config`;
+
+  const localAccount = await agoric.makeAccount();
+  const localChainAddress = await localAccount.getAddress();
+  console.log('Local Chain Address:', localChainAddress);
+
+  const agoricChainId = (await agoric.getChainInfo()).chainId;
+  const { transferChannel } = await chainHub.getConnectionInfo(
+    agoricChainId,
+    chainId,
+  );
+  assert(transferChannel.counterPartyChannelId, 'unable to find sourceChannel');
+
+  const localDenom = `ibc/${denomHash({
+    denom: remoteDenom,
+    channelId: transferChannel.channelId,
+  })}`;
+
+  const tap = makeEvmTap({
+    localAccount,
+    localChainAddress,
+    sourceChannel: transferChannel.counterPartyChannelId,
+    remoteDenom,
+    localDenom,
+  });
+  // XXX consider storing appRegistration, so we can .revoke() or .updateTargetApp()
+  // @ts-expect-error tap.receiveUpcall: 'Vow<void> | undefined' not assignable to 'Promise<any>'
+  await localAccount.monitorTransfers(tap);
+
+  return localChainAddress.value;
+};
+harden(createAndMonitorLCA);
