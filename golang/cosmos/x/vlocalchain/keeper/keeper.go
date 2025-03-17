5cf1f522d319434ec30cfb7961e2f3e607326b5f
Michael FIG <mfig@agoric.com>
2025-03-16 19:48:32 -0600

    fix(cosmos): add some missing pieces

--- golang/cosmos/x/vlocalchain/keeper/keeper.go
+++ golang/cosmos/x/vlocalchain/keeper/keeper.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"unicode"
 
-	"github.com/gogo/protobuf/jsonpb"
-	"github.com/gogo/protobuf/proto"
+	"github.com/cosmos/gogoproto/jsonpb"
+	"github.com/cosmos/gogoproto/proto"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
