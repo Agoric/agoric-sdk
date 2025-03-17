87e8052b63d71ef9f888bc1a690291924b16fd09
rabi-siddique <rabisiddique27@gmail.com>
2025-03-17 16:23:50 +0500

    changes

--- packages/orchestration/src/proposals/start-axelar-gmp.js
+++ packages/orchestration/src/proposals/start-axelar-gmp.js
@@ -89,15 +89,15 @@ export const startAxelarGmp = async (
     E(agoricNames).lookup('issuer', 'AUSDC'),
   );
 
-  const wavaxIssuer = await safeFulfill(() =>
-    E(agoricNames).lookup('issuer', 'WAVAX'),
-  );
+  // const wavaxIssuer = await safeFulfill(() =>
+  //   E(agoricNames).lookup('issuer', 'WAVAX'),
+  // );
 
   const issuerKeywordRecord = harden({
     BLD: await BLD,
     IST: await IST,
     ...(ausdcIssuer && { AUSDC: ausdcIssuer }),
-    ...(wavaxIssuer && { WAVAX: wavaxIssuer }),
+    // ...(wavaxIssuer && { WAVAX: wavaxIssuer }),
   });
   trace('issuerKeywordRecord', issuerKeywordRecord);
 
