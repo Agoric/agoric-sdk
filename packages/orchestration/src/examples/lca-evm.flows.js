f5e2850c208106fd9a753558a1603e9e35513c1a
rabi-siddique <rabisiddique27@gmail.com>
2025-03-17 16:20:42 +0500

    changes

--- packages/orchestration/src/examples/lca-evm.flows.js
+++ packages/orchestration/src/examples/lca-evm.flows.js
@@ -1,5 +1,7 @@
+// @ts-check
 import { Fail } from '@endo/errors';
 import { denomHash } from '../utils/denomHash.js';
+import { ethers } from 'ethers';
 
 /**
  * @import {GuestInterface} from '@agoric/async-flow';
@@ -8,6 +10,32 @@ import { denomHash } from '../utils/denomHash.js';
  * @import {MakePortfolioHolder} from '../../src/exos/portfolio-holder-kit.js';
  * @import {ChainHub} from '../../src/exos/chain-hub.js';
  */
+
+const test = () => {
+  const abiCoder = ethers.utils.defaultAbiCoder;
+
+  const message = 'Hello, Blockchain!';
+  const sourceChain = 'Ethereum';
+  const sourceAddress = '0x0830c9d8f05D1dcAE3406102420C29bBb287C199';
+  const payload = abiCoder.encode(['string'], ['fraz']);
+
+  const payload2 = abiCoder.encode(
+    ['string', 'string', 'bytes'],
+    [sourceChain, sourceAddress, payload],
+  );
+  console.log('Encoded payload2:', payload2);
+
+  const payload3 = abiCoder.encode(
+    ['string', 'string[]', 'string[]', 'bytes'],
+    [
+      'receive_message_evm',
+      ['source_chain', 'source_address', 'payload'],
+      ['string', 'string', 'bytes'],
+      payload2,
+    ],
+  );
+  console.log('Encoded payload3:', payload3);
+};
 
 /**
  * @satisfies {OrchestrationFlow}
@@ -33,6 +61,7 @@ export const createAndMonitorLCA = async (
     orch.getChain('agoric'),
     orch.getChain(chainName),
   ]);
+  test();
   const { chainId, stakingTokens } = await remoteChain.getChainInfo();
   const remoteDenom = stakingTokens[0].denom;
   remoteDenom ||
