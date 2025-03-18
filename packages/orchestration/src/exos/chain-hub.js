5703c02a20acd6e81eb888efadf925145f58d5ec
0xPatrick <patrick@0xpatrick.dev>
2025-03-18 18:01:59 -0400

    chore: chain-hub js doc

--- packages/orchestration/src/exos/chain-hub.js
+++ packages/orchestration/src/exos/chain-hub.js
@@ -234,10 +234,10 @@ const ChainHubI = M.interface('ChainHub', {
 /**
  * Make a new ChainHub in the zone.
  *
- * The resulting object is an Exo singleton. It has no precious state. It's only
+ * The resulting object is an Exo singleton. It has no precious state. Its only
  * state is a cache of queries to agoricNames and whatever info was provided in
  * registration calls. When you need a newer version you can simply make a hub
- * hub and repeat the registrations.
+ * and repeat the registrations.
  *
  * @param {Zone} zone
  * @param {Remote<NameHub>} agoricNames
