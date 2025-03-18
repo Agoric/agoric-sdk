9e32617a0551b67e1af85b97a42cc9d798968188
rabi-siddique <rabisiddique27@gmail.com>
2025-03-18 10:26:19 +0500

    chore: working version with deep imports

--- packages/builders/scripts/testing/init-axelar-gmp.js
+++ packages/builders/scripts/testing/init-axelar-gmp.js
@@ -3,17 +3,107 @@ import {
   getManifest,
   startAxelarGmp,
 } from '@agoric/orchestration/src/proposals/start-axelar-gmp.js';
-import { parseArgs } from 'node:util';
 
 /**
  * @import {ParseArgsConfig} from 'node:util'
  */
 
-/** @type {ParseArgsConfig['options']} */
-const parserOpts = {
-  chainInfo: { type: 'string' },
-  assetInfo: { type: 'string' },
-};
+const chainInfo = JSON.stringify({
+  agoric: {
+    chainId: 'agoriclocal',
+    stakingTokens: [
+      {
+        denom: 'ubld',
+      },
+    ],
+    connections: {
+      'osmo-test-5': {
+        id: 'connection-0',
+        client_id: '07-tendermint-0',
+        counterparty: {
+          client_id: '07-tendermint-4486',
+          connection_id: 'connection-3921',
+        },
+        state: 3,
+        transferChannel: {
+          channelId: 'channel-0',
+          portId: 'transfer',
+          counterPartyChannelId: 'channel-10226',
+          counterPartyPortId: 'transfer',
+          ordering: 0,
+          state: 3,
+          version: 'ics20-1',
+        },
+      },
+    },
+  },
+
+  osmosis: {
+    chainId: 'osmo-test-5',
+    stakingTokens: [
+      {
+        denom: 'uosmo',
+      },
+    ],
+    connections: {
+      agoriclocal: {
+        id: 'connection-3921',
+        client_id: '07-tendermint-4486',
+        counterparty: {
+          client_id: '07-tendermint-0',
+          connection_id: 'connection-0',
+        },
+        state: 3,
+        transferChannel: {
+          channelId: 'channel-10226',
+          portId: 'transfer',
+          counterPartyChannelId: 'channel-0',
+          counterPartyPortId: 'transfer',
+          ordering: 0,
+          state: 3,
+          version: 'ics20-1',
+        },
+      },
+    },
+  },
+});
+
+const assetInfo = JSON.stringify([
+  [
+    'uist',
+    {
+      baseDenom: 'uist',
+      baseName: 'agoric',
+      chainName: 'agoric',
+    },
+  ],
+  [
+    'uosmo',
+    {
+      baseDenom: 'uosmo',
+      baseName: 'osmosis',
+      chainName: 'osmosis',
+    },
+  ],
+  [
+    'ibc/94EB1E9A676004E74ECF47F8E4BF183F4017CE0630A4D1AC7C7D9EB9CD6A3D53',
+    {
+      baseDenom: 'uausdc',
+      baseName: 'osmosis',
+      chainName: 'agoric',
+      brandKey: 'AUSDC',
+    },
+  ],
+  [
+    'ibc/3C870A71004EAD01A29709B779FECBB9F150559B1276825584E149596BD450DE',
+    {
+      baseDenom: 'wavax-wei',
+      baseName: 'osmosis',
+      chainName: 'agoric',
+      brandKey: 'WAVAX',
+    },
+  ],
+]);
 
 /** @type {import('@agoric/deploy-script-support/src/externalTypes.js').CoreEvalBuilder} */
 export const defaultProposalBuilder = async (
@@ -35,15 +125,6 @@ export const defaultProposalBuilder = async (
 
 /** @type {import('@agoric/deploy-script-support/src/externalTypes.js').DeployScriptFunction} */
 export default async (homeP, endowments) => {
-  const { scriptArgs } = endowments;
-
-  const {
-    values: { chainInfo, assetInfo },
-  } = parseArgs({
-    args: scriptArgs,
-    options: parserOpts,
-  });
-
   const parseChainInfo = () => {
     if (typeof chainInfo !== 'string') return undefined;
     return JSON.parse(chainInfo);
