df84c88b5f3a339badab744c03912e2f1e4f4f07
rabi-siddique <rabisiddique27@gmail.com>
2025-03-20 06:47:59 +0500

    chore: update contract builder script and log messages

--- packages/orchestration/src/examples/axelar-gmp.contract.js
+++ packages/orchestration/src/examples/axelar-gmp.contract.js
@@ -11,7 +11,6 @@ import * as evmFlows from './lca-evm.flows.js';
 import { prepareEvmTap } from './evm-tap-kit.js';
 import { EmptyProposalShape } from '@agoric/zoe/src/typeGuards';
 import { E } from '@endo/far';
-import { buildGMPPayload } from '../utils/gmp.js';
 
 /**
  * @import {Remote, Vow} from '@agoric/vow';
@@ -53,21 +52,8 @@ export const contract = async (
   console.log('Channel Info Agoric:');
   console.log(privateArgs.chainInfo.agoric.connections);
 
-  console.log('Channel Info Osmosis:');
-  console.log(privateArgs.chainInfo.osmosis.connections);
-
-  // const dummyParams = {
-  //   type: 3,
-  //   evmContractAddress: '0x1234567890abcdef1234567890abcdef12345678',
-  //   functionSelector: '0xa9059cbb', // Example 4-byte function selector
-  //   encodedArgs:
-  //     '0x0000000000000000000000000000000000000000000000000000000000000001', // Example ABI encoded arguments
-  //   deadline: 3600, // 1 hour from now
-  //   nonce: 42,
-  // };
-
-  // const result = buildGMPPayload(dummyParams);
-  // console.log(result);
+  console.log('Channel Info Axelar:');
+  console.log(privateArgs.chainInfo.axelar.connections);
 
   console.log('Registering Chain and Assets....');
   registerChainsAndAssets(
