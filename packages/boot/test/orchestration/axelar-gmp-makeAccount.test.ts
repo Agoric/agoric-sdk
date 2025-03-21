6d612d59b37349b75ef05a6b94201cdcf64c3073
rabi-siddique <rabisiddique27@gmail.com>
2025-03-21 08:46:38 +0500

    test: make an LCA with a receiveUpCall listener

---
+++ packages/boot/test/orchestration/axelar-gmp-makeAccount.test.ts
@@ -0,0 +1,78 @@
+// @ts-check
+/** @file Bootstrap test of restarting contracts using orchestration */
+import { test as anyTest } from '@agoric/zoe/tools/prepare-test-env-ava.js';
+import type { TestFn } from 'ava';
+import {
+  makeWalletFactoryContext,
+  type WalletFactoryTestContext,
+} from '../bootstrapTests/walletFactory.js';
+import fetchedChainInfo from '@agoric/orchestration/src/fetched-chain-info.js';
+
+const test: TestFn<WalletFactoryTestContext> = anyTest;
+test.before(async t => {
+  t.context = await makeWalletFactoryContext(
+    t,
+    '@agoric/vm-config/decentral-itest-orchestration-config.json',
+  );
+});
+test.after.always(t => t.context.shutdown?.());
+
+test('makeAccount via axelarGmp', async t => {
+  const {
+    walletFactoryDriver,
+    bridgeUtils: { runInbound },
+    buildProposal,
+    evalProposal,
+    storage,
+  } = t.context;
+
+  const { BLD } = t.context.agoricNamesRemotes.brand;
+
+  t.log('start axelarGmp');
+
+  await evalProposal(
+    buildProposal('@agoric/builders/scripts/testing/init-axelar-gmp.js', [
+      '--chainInfo',
+      JSON.stringify({
+        agoric: fetchedChainInfo.agoric,
+        axelar: fetchedChainInfo.axelar,
+      }),
+      '--assetInfo',
+      JSON.stringify([
+        [
+          'ubld',
+          {
+            baseDenom: 'ubld',
+            brandKey: 'BLD',
+            baseName: 'agoric',
+            chainName: 'agoric',
+          },
+        ],
+      ]),
+    ]),
+  );
+
+  t.log('making offer');
+
+  const wallet = await walletFactoryDriver.provideSmartWallet('agoric1test');
+  await wallet.sendOffer({
+    id: 'axelarMakeAccountCall',
+    invitationSpec: {
+      source: 'agoricContract',
+      instancePath: ['axelarGmp'],
+      callPipe: [['createAndMonitorLCA']],
+    },
+    proposal: {},
+  });
+
+  const getLogged = () =>
+    JSON.parse(storage.data.get('published.axelarGmp.log')!).values;
+
+  // Flow started but IBC Transfer promise not resolved
+  t.deepEqual(getLogged(), [
+    'Inside createAndMonitorLCA',
+    'localAccount created successfully',
+    'tap created successfully',
+    'Monitoring transfers setup successfully',
+  ]);
+});
