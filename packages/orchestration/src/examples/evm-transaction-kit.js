e0d7aea3c1eed2157ac4987fbaf765342536ae40
rabi-siddique <rabisiddique27@gmail.com>
2025-03-24 11:35:50 +0500

    feat: initial setup for continuing invitations with dummy methods

---
+++ packages/orchestration/src/examples/evm-transaction-kit.js
@@ -0,0 +1,56 @@
+import { M, prepareExoClassKit } from '@agoric/vat-data';
+import { defineDurableHandle } from '@agoric/zoe/src/makeHandle.js';
+
+const EVMI = M.interface('evmTransaction', {
+  printName: M.call(M.any(), M.any()).returns(M.any()),
+});
+
+const InvitationMakerI = M.interface('invitationMaker', {
+  makeEvmTransactionInvitation: M.call(M.any(), M.any()).returns(M.any()),
+});
+
+/**
+ * Make a kit suitable for returning to a EVM Transaction Kit.
+ *
+ * @param {import('@agoric/vat-data').Baggage} baggage
+ * @param {object} powers
+ */
+export const prepareEVMTransactionKit = (baggage, { zcf }) => {
+  const makeEVMTransactionHandle = defineDurableHandle(
+    baggage,
+    'EVMTransaction',
+  );
+  const makeEvmTransactionKit = prepareExoClassKit(
+    baggage,
+    'EVMTransactionKit',
+    { evm: EVMI, invitationMakers: InvitationMakerI },
+    id => {
+      const evmTransactionHandle = makeEVMTransactionHandle();
+      return { id, evmTransactionHandle };
+    },
+    {
+      evm: {
+        printName: (firstName, lastName) => {
+          console.log(`Hi ${firstName} ${lastName}`);
+          return `Hi ${firstName} ${lastName}`;
+        },
+      },
+      invitationMakers: {
+        makeEvmTransactionInvitation(firstName, lastName) {
+          const { evm } = this.facets;
+          const continuingEVMTransactionHandler = cSeat => {
+            cSeat.exit();
+            return evm.printName(firstName, lastName);
+          };
+
+          return zcf.makeInvitation(
+            continuingEVMTransactionHandler,
+            'evmTransaction',
+          );
+        },
+      },
+    },
+  );
+  return makeEvmTransactionKit;
+};
+harden(prepareEVMTransactionKit);
