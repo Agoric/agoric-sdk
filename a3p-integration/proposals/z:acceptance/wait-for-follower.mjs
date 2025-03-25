79e902613630807dfbce106febd4f3ccacbd357b
usmanmani1122 <usman.sbf@gmail.com>
2025-03-25 15:07:17 +0000

    fix: use event based watcher

--- a3p-integration/proposals/z:acceptance/wait-for-follower.mjs
+++ a3p-integration/proposals/z:acceptance/wait-for-follower.mjs
@@ -1,6 +1,7 @@
 /* eslint-env node */
 
-import { readFile, watch } from 'fs/promises';
+import { readFile } from 'fs/promises';
+import { watch } from 'fs';
 
 const FILE_ENCODING = 'utf-8';
 const FILE_PATH = process.env.MESSAGE_FILE_PATH;
@@ -16,12 +17,63 @@ const checkFileContent = async (filePath, message) => {
 };
 
 /**
+ * @template T
+ * @returns {{promise: Promise<T>; reject: (reason: Error) => void; resolve: (value: T) => void; }}
+ */
+const createDeferredPromise = () => {
+  /**
+   * @type {(value: T) => void}
+   */
+  let resolve = _ => {};
+  /**
+   * @type {(reason: Error) => void}
+   */
+  let reject = _ => {};
+
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  return { promise, resolve, reject };
+};
+
+/**
+ * @param {string} filePath
+ */
+async function* createFileWatcher(filePath) {
+  let { promise, resolve, reject } = createDeferredPromise();
+  let closed = false;
+
+  const watcher = watch(filePath, FILE_ENCODING, (eventType, fileName) =>
+    resolve({ eventType, fileName }),
+  );
+
+  watcher.addListener('close', () => (closed = true));
+
+  watcher.on('error', error => reject(error));
+
+  try {
+    yield /** @type {import('node:fs/promises').FileChangeInfo<string>} */ ({
+      eventType: 'change',
+      filename: filePath,
+    });
+    while (!closed) {
+      yield await promise;
+      ({ promise, resolve, reject } = createDeferredPromise());
+    }
+  } finally {
+    watcher.close();
+  }
+}
+
+/**
  * @param {string} filePath
  */
 const watchSharedFile = async filePath => {
   const [, , message] = process.argv;
 
-  for await (const { eventType } of wrappedWatch(filePath)) {
+  for await (const { eventType } of createFileWatcher(filePath)) {
     if (eventType === 'change') {
       const possibleContent = await checkFileContent(filePath, message);
       if (possibleContent) return possibleContent;
@@ -30,19 +82,6 @@ const watchSharedFile = async filePath => {
 
   return undefined;
 };
-
-/**
- * @param {string} filePath
- */
-async function* wrappedWatch(filePath) {
-  const watchIter = watch(filePath);
-
-  yield /** @type {import('node:fs/promises').FileChangeInfo<string>} */ ({
-    eventType: 'change',
-    filename: filePath,
-  });
-  yield* watchIter;
-}
 
 FILE_PATH &&
   watchSharedFile(FILE_PATH).then(
