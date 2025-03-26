afef2d7ac029e0c72058e3206479d47927ec491a
usmanmani1122 <usman.sbf@gmail.com>
2025-03-26 06:33:38 +0000

    remove async iterator

--- a3p-integration/proposals/z:acceptance/wait-for-follower.mjs
+++ a3p-integration/proposals/z:acceptance/wait-for-follower.mjs
@@ -1,7 +1,6 @@
 /* eslint-env node */
 
-import { readFile } from 'fs/promises';
-import { watch } from 'fs';
+import { watch, readFileSync } from 'fs';
 
 const FILE_ENCODING = 'utf-8';
 const FILE_PATH = process.env.MESSAGE_FILE_PATH;
@@ -10,77 +9,46 @@ const FILE_PATH = process.env.MESSAGE_FILE_PATH;
  * @param {string} filePath
  * @param {string} message
  */
-const checkFileContent = async (filePath, message) => {
-  const fileContent = (await readFile(filePath, FILE_ENCODING)).trim();
+const checkFileContent = (filePath, message) => {
+  const fileContent = readFileSync(filePath, FILE_ENCODING).trim();
   if (!new RegExp(message).test(fileContent)) return '';
   return fileContent;
 };
 
 /**
- * @template T
- * @returns {{promise: Promise<T>; reject: (reason: Error) => void; resolve: (value: T) => void; }}
- */
-const createDeferredPromise = () => {
-  /**
-   * @type {(value: T) => void}
-   */
-  let resolve = _ => {};
-  /**
-   * @type {(reason: Error) => void}
-   */
-  let reject = _ => {};
-
-  const promise = new Promise((res, rej) => {
-    resolve = res;
-    reject = rej;
-  });
-
-  return { promise, resolve, reject };
-};
-
-/**
  * @param {string} filePath
  */
-async function* createFileWatcher(filePath) {
-  let { promise, resolve, reject } = createDeferredPromise();
-  let closed = false;
-
-  const watcher = watch(filePath, FILE_ENCODING, (eventType, fileName) =>
-    resolve({ eventType, fileName }),
-  );
-
-  watcher.addListener('close', () => (closed = true));
-
-  watcher.on('error', error => reject(error));
-
-  try {
-    yield /** @type {import('node:fs/promises').FileChangeInfo<string>} */ ({
-      eventType: 'change',
-      filename: filePath,
-    });
-    while (!closed) {
-      yield await promise;
-      ({ promise, resolve, reject } = createDeferredPromise());
-    }
-  } finally {
-    watcher.close();
-  }
-}
-
-/**
- * @param {string} filePath
- */
-const watchSharedFile = async filePath => {
+const watchSharedFile = filePath => {
   const [, , message] = process.argv;
 
-  for await (const { eventType } of createFileWatcher(filePath)) {
-    if (eventType === 'change') {
-      const possibleContent = await checkFileContent(filePath, message);
-      if (possibleContent) return possibleContent;
-    }
-  }
+  return /** @type {Promise<string>} */ (
+    new Promise((resolve, reject) => {
+      /**
+       * @type {import('fs').FSWatcher}
+       */
+      let watcher;
 
-  return undefined;
+      /**
+       * @param {import('fs').WatchEventType} eventType
+       */
+      const listener = eventType => {
+        if (eventType === 'change') {
+          const possibleContent = checkFileContent(filePath, message);
+          if (possibleContent) {
+            watcher.close();
+            resolve(possibleContent);
+          }
+        }
+      };
+
+      watcher = watch(filePath, FILE_ENCODING, listener);
+
+      watcher.on('close', () => reject(Error('Watcher closed')));
+      watcher.on('error', error => reject(error));
+
+      listener('change');
+    })
+  );
 };
 
 FILE_PATH &&
