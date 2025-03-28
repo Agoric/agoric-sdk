97c54c57bbe133335f2ddff16738db11b24aaf9a
Kris Kowal <kris@agoric.com>
2025-03-28 16:23:47 -0700

    feat(cosmic-proto): Minify distributed JavaScript

---
+++ packages/cosmic-proto/scripts/minify.cjs
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+/* eslint-env node */
+
+const { sync: glob } = require('glob');
+const { minify_sync: minify } = require('terser');
+const { readFileSync: readFile, writeFileSync: writeFile } = require('fs');
+
+const sources = glob('dist/**/*.js');
+for (const path of sources) {
+  const code = readFile(path, 'utf-8');
+  const { code: minCode } = minify(code, {
+    module: true,
+  });
+  writeFile(path, minCode, 'utf-8');
+}
+console.log('🍰 code minified by Terser');
