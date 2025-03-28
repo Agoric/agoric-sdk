9a7f5a1d12b7f30bd1695aa15bd232cd8a3e8cf5
Richard Gibson <richard.gibson@gmail.com>
2025-03-28 14:19:09 -0400

    feat(telemetry): Update slog sender JSON serialization of error instances
    `{ error, message, ... }` rather than `{}`

--- packages/telemetry/src/serialize-slog-obj.js
+++ packages/telemetry/src/serialize-slog-obj.js
@@ -1,4 +1,26 @@
-export const serializeSlogObj = slogObj =>
-  JSON.stringify(slogObj, (_, arg) =>
-    typeof arg === 'bigint' ? Number(arg) : arg,
-  );
+const { hasOwn } = Object;
+
+const replacer = (_key, value) => {
+  switch (typeof value) {
+    case 'object': {
+      if (value instanceof Error) {
+        const obj = { error: value.name, message: value.message };
+        if (hasOwn(value, 'cause')) obj.cause = replacer('', value.cause);
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore TS2339 property "errors" is only on AggregateError
+        if (hasOwn(value, 'errors')) obj.errors = replacer('', value.errors);
+        const stack = value.stack;
+        if (stack) obj.stack = stack;
+        return obj;
+      }
+      break;
+    }
+    case 'bigint':
+      return Number(value);
+    default:
+      break;
+  }
+  return value;
+};
+
+export const serializeSlogObj = slogObj => JSON.stringify(slogObj, replacer);
