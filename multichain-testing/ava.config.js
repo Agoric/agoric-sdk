1436fe27f49f9f7d3bca3d9d3125bdc691d9e620
Turadg Aleahmad <turadg@agoric.com>
2025-03-30 08:38:29 -0700

    test: default Ava config

--- multichain-testing/ava.config.js
+++ multichain-testing/ava.config.js
@@ -5,7 +5,7 @@ export default {
   // Each test imports `@endo/ses-ava/prepare-endo.js` which does its own @endo/init
   // require: ['@endo/init/debug.js'],
   nodeArguments: ['--import=ts-blank-space/register'],
-  files: ['test/**/*.test.ts', '!test/fast-usdc/**/*.test.ts'],
+  files: ['test/**/*.test.*'],
   concurrency: 1,
   serial: true,
   timeout: '125s',
