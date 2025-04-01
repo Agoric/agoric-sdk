21b6edd3346882350ebfee51fa5189d22f4dcb0f
Dan Connolly <connolly@agoric.com>
2025-04-01 17:35:07 -0500

    chore(snap-store): export type SnapStoreDebug

--- packages/swing-store/src/types-index.d.ts
+++ packages/swing-store/src/types-index.d.ts
@@ -5,7 +5,12 @@ export type {
 } from './swingStore.js';
 export type { KVStore } from './kvStore.js';
 export type { BundleStore } from './bundleStore.js';
-export type { SnapStore, SnapshotResult, SnapshotInfo } from './snapStore.js';
+export type {
+  SnapStore,
+  SnapshotResult,
+  SnapshotInfo,
+  SnapStoreDebug,
+} from './snapStore.js';
 export type { TranscriptStore } from './transcriptStore.js';
 export type { ArtifactMode } from './internal.js';
 export type { ImportSwingStoreOptions } from './importer.js';
