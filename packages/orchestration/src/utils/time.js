c41ca519c17183069f2a5149f9c3e77238309838
0xPatrick <patrick@0xpatrick.dev>
2025-04-01 17:28:01 -0400

    feat(orchestration): simplify `getTimeoutTimestampNS` parameter
    - change the `getTimeoutTimestampNS` function to accept a simple bigint parameter (`secondsInFuture`) instead of a `RelativeTimeRecord` object
    - this simplifies the API by eliminating the need for users to obtain a timer brand

--- packages/orchestration/src/utils/time.js
+++ packages/orchestration/src/utils/time.js
@@ -2,9 +2,10 @@ import { E } from '@endo/far';
 import { TimeMath } from '@agoric/time';
 
 /**
- * @import {RelativeTimeRecord, TimerBrand, TimerService} from '@agoric/time';
+ * @import {TimerService} from '@agoric/time';
  * @import {Remote} from '@agoric/internal';
  * @import {EReturn} from '@endo/far';
+ * @import {MsgTransfer} from '@agoric/cosmic-proto/ibc/applications/transfer/v1/tx.js';
  */
 
 export const SECONDS_PER_MINUTE = 60n;
@@ -22,22 +23,20 @@ export function makeTimestampHelper(timer) {
     /**
      * XXX do this need to be resumable / use Vows?
      *
-     * Takes the current time from ChainTimerService and adds a relative time to
-     * determine a timeout timestamp in nanoseconds. Useful for
-     * {@link MsgTransfer.timeoutTimestamp}.
+     * Takes the current time from ChainTimerService and adds a relative time
+     * (`secondsInFuture`) to determine a timeout timestamp in nanoseconds.
+     * Useful for {@link MsgTransfer.timeoutTimestamp}.
      *
-     * @param {RelativeTimeRecord} [relativeTime] defaults to 5 minutes
+     * @param {bigint} [secondsInFuture] defaults to 300n (5 minutes)
      * @returns {Promise<bigint>} Timeout timestamp in absolute nanoseconds
      *   since unix epoch
      */
-    async getTimeoutTimestampNS(relativeTime) {
+    async getTimeoutTimestampNS(secondsInFuture = SECONDS_PER_MINUTE * 5n) {
       const currentTime = await E(timer).getCurrentTimestamp();
-      const timeout =
-        relativeTime ||
-        TimeMath.coerceRelativeTimeRecord(
-          SECONDS_PER_MINUTE * 5n,
-          currentTime.timerBrand,
-        );
+      const timeout = TimeMath.coerceRelativeTimeRecord(
+        secondsInFuture,
+        currentTime.timerBrand,
+      );
       return (
         TimeMath.addAbsRel(currentTime, timeout).absValue *
         NANOSECONDS_PER_SECOND
