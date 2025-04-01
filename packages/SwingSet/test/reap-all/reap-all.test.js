3eaf4cea8b44cf00c4b172581b274018917b8132
Mathieu Hofman <mathieu@agoric.com>
2025-04-01 03:56:35 +0000

    chore(SwingSet): refactor reap-all test

--- packages/SwingSet/test/reap-all/reap-all.test.js
+++ packages/SwingSet/test/reap-all/reap-all.test.js
@@ -43,41 +43,48 @@ test('reap all vats', async t => {
   const dynamicRoots = kunser(c.kpResolution(kpid));
   for (let i = 0; i < 3; i += 1) {
     for (let j = 0; j < i + 1; j += 1) {
-      c.queueToVatRoot(`staticDumbo${i + 1}`, 'doSomething', [
-        `staticDumbo${i + 1} #${j + 1}`,
-      ]);
+      c.queueToVatRoot(
+        `staticDumbo${i + 1}`,
+        'doSomething',
+        [`staticDumbo${i + 1} #${j + 1}`],
+        'none',
+      );
     }
   }
   for (let i = 0; i < 3; i += 1) {
     for (let j = 0; j < i + 1; j += 1) {
-      c.queueToVatObject(dynamicRoots[i], 'doSomething', [
-        `dynamicDumbo${i + 1} #${j + 1}`,
-      ]);
+      c.queueToVatObject(
+        dynamicRoots[i],
+        'doSomething',
+        [`dynamicDumbo${i + 1} #${j + 1}`],
+        'none',
+      );
     }
   }
   // Note: no call to c.run() here, so all the above messages are still enqueued
 
-  const dumpBefore = c.dump();
-  t.is(dumpBefore.acceptanceQueue.length, 12);
-  t.is(dumpBefore.reapQueue.length, 0);
-  t.is(dumpBefore.runQueue.length, 0);
+  const { activeVats } = c.getStatus();
+  t.log(activeVats.map(({ id, options }) => `${id}: ${options.name}`));
+  const reapable = activeVats.map(({ id }) => id);
+  t.true(reapable.length >= 7); // bootstrap, staticDumbo{1,2,3}, dyn{1,2,3}
+
+  /**
+   * @param {{ reap?: string[], acceptanceLength?: number, runLength?: number }} [expectations]
+   */
+  const checkQueues = (expectations = {}) => {
+    const { reap = [], acceptanceLength = 0, runLength = 0 } = expectations;
+    const dump = c.dump();
+    t.is(dump.acceptanceQueue.length, acceptanceLength);
+    t.is(dump.runQueue.length, runLength);
+    t.is(dump.reapQueue.length, reap.length);
+    t.deepEqual(new Set(dump.reapQueue), new Set(reap));
+  };
+
+  checkQueues({ acceptanceLength: 12 });
 
   c.reapAllVats();
-  const dumpPreReap = c.dump();
-  t.is(dumpPreReap.acceptanceQueue.length, 12);
-  t.is(dumpPreReap.reapQueue.length, 11);
-  t.is(dumpBefore.runQueue.length, 0);
-  // prettier-ignore
-  const reapQueueReference =
-    new Set(['v1', 'v3', 'v6', 'v7', 'v8', 'v5', 'v2', 'v4', 'v9', 'v10', 'v11'])
-  const reapQueueActual = new Set(dumpPreReap.reapQueue);
-  t.deepEqual(reapQueueActual, reapQueueReference);
+  checkQueues({ reap: reapable, acceptanceLength: 12 });
 
   await c.run();
-  const dumpPostReap = c.dump();
-  t.is(dumpPostReap.acceptanceQueue.length, 0);
-  t.is(dumpPostReap.reapQueue.length, 0);
-  t.is(dumpBefore.runQueue.length, 0);
-
-  t.pass();
+  checkQueues();
 });
