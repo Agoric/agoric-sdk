44eb66849c6095d96d371cb2067b5f9769b164a7
Richard Gibson <richard.gibson@gmail.com>
2025-04-04 00:04:12 -0400

    test(swingset-liveslots): Extend durable stateShape test for field additions (and mark failing)

--- packages/swingset-liveslots/test/virtual-objects/state-shape.test.js
+++ packages/swingset-liveslots/test/virtual-objects/state-shape.test.js
@@ -44,24 +44,29 @@ const dispatchForResult = async (ls, syscallLog, message) => {
 const eph1 = Far('ephemeral1');
 const eph2 = Far('ephemeral2');
 
-const initHolder = value => ({ value });
+const initHolder = fields => ({ ...fields });
 const holderMethods = {
-  get: ({ state }) => state.value,
-  set: ({ state }, value) => (state.value = value),
+  get: ({ state }, ...fields) =>
+    // We require fields to be explicit because they are currently defined on
+    // the state *prototype*.
+    Object.fromEntries(
+      fields.flatMap(key => (key in state ? [[key, state[key]]] : [])),
+    ),
+  set: ({ state }, fields) => {
+    Object.assign(state, fields);
+  },
 };
 /**
- * Define a virtual or durable kind for getting and setting a single value
- * constrained by the provided shape.
+ * Define a virtual or durable kind for getting and setting state constrained
+ * by the provided shape.
  *
  * @template {VatData.defineKind | VatData.defineDurableKind} D
  * @param {D} defineKind
  * @param {Parameters<D>[0]} kindIdentifier
- * @param {import('@endo/patterns').Pattern} valueShape
+ * @param {import('@endo/patterns').Pattern} stateShape
  */
-const defineHolder = (defineKind, kindIdentifier, valueShape) =>
-  defineKind(kindIdentifier, initHolder, holderMethods, {
-    stateShape: { value: valueShape },
-  });
+const defineHolder = (defineKind, kindIdentifier, stateShape) =>
+  defineKind(kindIdentifier, initHolder, holderMethods, { stateShape });
 
 // virtual/durable Kinds can specify a 'stateShape', which should be
 // enforced at both initialization and subsequent state changes
@@ -69,14 +74,14 @@ const testStateShape = test.macro((t, valueShape, config) => {
   const { goodValues, badValues, throwsExpectation } = config;
   const { vom } = makeFakeVirtualStuff();
   const { defineKind } = vom;
-  const makeHolder = defineHolder(defineKind, 'kindTag', valueShape);
-  const instance = goodValues.map(value => makeHolder(value)).at(-1);
+  const makeHolder = defineHolder(defineKind, 'kindTag', { value: valueShape });
+  const instance = goodValues.map(value => makeHolder({ value })).at(-1);
   for (const value of goodValues) {
-    instance.set(value);
+    instance.set({ value });
   }
   for (const value of badValues || []) {
-    t.throws(() => makeHolder(value), throwsExpectation);
-    t.throws(() => instance.set(value), throwsExpectation);
+    t.throws(() => makeHolder({ value }), throwsExpectation);
+    t.throws(() => instance.set({ value }), throwsExpectation);
   }
   t.pass();
 });
@@ -115,11 +120,11 @@ test('durable state shape', t => {
   const defineNextKind = valueShape => {
     kindNumber += 1;
     const kh = makeKindHandle(`kind${kindNumber}`);
-    return defineHolder(defineDurableKind, kh, valueShape);
+    return defineHolder(defineDurableKind, kh, { value: valueShape });
   };
 
   const makeKind1 = defineNextKind();
-  makeKind1();
+  makeKind1({ value: undefined });
 
   const makeKind2 = defineNextKind();
   makeKind2();
@@ -128,19 +133,19 @@ test('durable state shape', t => {
   const obj3 = makeKind3();
 
   const makeKind4 = defineNextKind(M.string());
-  const obj4 = makeKind4('string');
+  const obj4 = makeKind4({ value: 'string' });
 
   const makeKind5 = defineNextKind(M.remotable());
   const durableValueFail = { message: /value for "value" is not durable/ };
-  t.throws(() => makeKind5(eph1), durableValueFail);
+  t.throws(() => makeKind5({ value: eph1 }), durableValueFail);
 
   const durableShapeFail = { message: /stateShape.*is not durable: slot 0 of/ };
   t.throws(() => defineNextKind(eph1), durableShapeFail);
 
   const makeKind7 = defineNextKind(obj4); // obj4 is durable
-  makeKind7(obj4);
+  makeKind7({ value: obj4 });
   const specificRemotableFail = { message: /kind3.*Must be:.*kind4/ };
-  t.throws(() => makeKind7(obj3), specificRemotableFail);
+  t.throws(() => makeKind7({ value: obj3 }), specificRemotableFail);
 });
 
 // durable Kinds maintain refcounts on their serialized stateShape
@@ -159,7 +164,7 @@ test('durable stateShape refcounts', async t => {
         defineDurableKind: valueShape => {
           const kh = makeKindHandle('shaped');
           baggage.init('kh', kh);
-          defineHolder(defineDurableKind, kh, valueShape);
+          defineHolder(defineDurableKind, kh, { value: valueShape });
         },
       });
     };
@@ -193,7 +198,7 @@ test('durable stateShape refcounts', async t => {
       const { defineDurableKind } = VatData;
       const { valueShape } = vatParameters;
       const kh = baggage.get('kh');
-      defineHolder(defineDurableKind, kh, valueShape);
+      defineHolder(defineDurableKind, kh, { value: valueShape });
 
       return Far('root', {});
     };
@@ -210,11 +215,12 @@ test('durable stateShape refcounts', async t => {
   t.is(ls2.testHooks.getReachableRefCount(vref2), 1);
 });
 
-test('durable stateShape must match', async t => {
-  const kvStore = new Map();
-  const { syscall: sc1, log: log1 } = buildSyscall({ kvStore });
+test.failing('durable stateShape must match or extend', async t => {
+  const store1 = new Map();
+  const { syscall: sc1, log: log1 } = buildSyscall({ kvStore: store1 });
   const gcTools = makeMockGC();
 
+  const fields = ['x', 'x2', 'y']; // but "x2" is not initially present
   const ls1 = makeLiveSlots(sc1, 'vatA', {}, {}, gcTools, undefined, () => {
     const buildRootObject = (vatPowers, _vatParameters, baggage) => {
       const { VatData } = vatPowers;
@@ -236,33 +242,39 @@ test('durable stateShape must match', async t => {
 
   const vref1 = 'o-1';
   const vref2 = 'o-2';
+  const passables = new Map([
+    [vref1, kslot(vref1, vref1)],
+    [vref2, kslot(vref2, vref2)],
+  ]);
   const instance1 = await dispatchForResult(ls1, log1, [
     root,
     'makeShapedDurable',
-    [kslot(vref1), kslot(vref2)],
+    [passables.get(vref1), passables.get(vref2)],
   ]);
-  const instance1Ref = instance1.getKref();
-  const state1 = await dispatchForResult(ls1, log1, [instance1Ref, 'get']);
-  t.deepEqual(kser(state1), kser({ x: kslot(vref1), y: kslot(vref2) }));
+  const objRef = instance1.getKref();
+  const state1 = await dispatchForResult(ls1, log1, [objRef, 'get', fields]);
+  t.deepEqual(
+    kser(state1),
+    kser({ x: passables.get(vref1), y: passables.get(vref2) }),
+  );
 
   // ------
 
   // Simulate upgrade by starting from the non-empty kvStore.
-  const clonedStore = new Map(kvStore);
-  const { syscall: sc2, log: log2 } = buildSyscall({ kvStore: clonedStore });
+  const store2 = new Map(store1);
+  const { syscall: sc2, log: log2 } = buildSyscall({ kvStore: store2 });
 
   // we do *not* override allowStateShapeChanges
   // const opts = { allowStateShapeChanges: true };
   const opts = undefined;
+  const stateShapeMismatch = { message: /durable Kind stateShape mismatch/ };
   const ls2 = makeLiveSlots(sc2, 'vatA', {}, opts, gcTools, undefined, () => {
     const buildRootObject = (vatPowers, vatParameters, baggage) => {
       const { VatData } = vatPowers;
-      const { defineDurableKind } = VatData;
       const { x, y } = vatParameters;
       const kh = baggage.get('kh');
-      const redefineDurableKind = valueShape => {
-        defineHolder(defineDurableKind, kh, valueShape);
-      };
+      const redefineDurableKind = stateShape =>
+        defineHolder(VatData.defineDurableKind, kh, stateShape);
       // several shapes that are not compatible
       const badShapes = [
         { x, y: M.any() },
@@ -271,9 +283,8 @@ test('durable stateShape must match', async t => {
         { x: M.or(x, M.string()), y },
         { x: y, y: x }, // wrong slots
       ];
-      const expectation = { message: /durable Kind stateShape mismatch/ };
-      for (const valueShape of badShapes) {
-        t.throws(() => redefineDurableKind(valueShape), expectation);
+      for (const stateShape of badShapes) {
+        t.throws(() => redefineDurableKind(stateShape), stateShapeMismatch);
       }
       // the correct shape
       redefineDurableKind({ x, y });
@@ -283,9 +294,96 @@ test('durable stateShape must match', async t => {
     return { buildRootObject };
   });
 
-  const vatParameters = { x: kslot(vref1), y: kslot(vref2) };
-  await ls2.dispatch(makeStartVat(kser(vatParameters)));
+  const vatParameters2 = { x: passables.get(vref1), y: passables.get(vref2) };
+  await ls2.dispatch(makeStartVat(kser(vatParameters2)));
 
-  const state2 = await dispatchForResult(ls2, log2, [instance1Ref, 'get']);
+  const state2 = await dispatchForResult(ls2, log2, [objRef, 'get', fields]);
   t.deepEqual(kser(state2), kser(state1));
+
+  // ------
+
+  // Now upgrade again, first to add a new optional stateShape field x2 and then
+  // to preserve the new shape.
+  const store3 = new Map(store2);
+  const { syscall: sc3, log: log3 } = buildSyscall({ kvStore: store3 });
+  const ls3 = makeLiveSlots(sc3, 'vatA', {}, {}, gcTools, undefined, () => {
+    const buildRootObject = ({ VatData }, vatParameters, baggage) => {
+      const { x, y } = vatParameters;
+      const kh = baggage.get('kh');
+      const redefineDurableKind = stateShape =>
+        defineHolder(VatData.defineDurableKind, kh, stateShape);
+      const badShapes = [{ x, x2: x, y }];
+      for (const stateShape of badShapes) {
+        t.throws(() => redefineDurableKind(stateShape), stateShapeMismatch);
+      }
+      redefineDurableKind({ x, x2: M.or(M.undefined(), x), y });
+      return Far('root', {});
+    };
+    return { buildRootObject };
+  });
+  const vatParameters3 = { x: passables.get(vref1), y: passables.get(vref2) };
+  await ls3.dispatch(makeStartVat(kser(vatParameters3)));
+
+  const state3 = await dispatchForResult(ls3, log3, [objRef, 'get', fields]);
+  t.deepEqual(kser(state3), kser({ ...state1, x2: undefined }));
+  const makeVerificationCase = (x, x2, y, throwsMessage) => [
+    `{ x: ${x}, x2: ${x2}, y: ${y} }`,
+    { x: passables.get(x), x2: passables.get(x2), y: passables.get(y) },
+    throwsMessage,
+  ];
+  const verifications = [
+    makeVerificationCase(vref1, vref2, vref2, 'rejected'),
+    makeVerificationCase(vref1, vref1, vref2),
+    makeVerificationCase(vref1, undefined, vref2),
+  ];
+  let lastState = state3;
+  for (const [label, value, message] of verifications) {
+    const tryUpdate = () =>
+      dispatchForResult(ls3, log3, [objRef, 'set', [value]]);
+    await (message
+      ? t.throwsAsync(tryUpdate, { message }, `${label} ${message}`)
+      : tryUpdate());
+    const state = await dispatchForResult(ls3, log3, [objRef, 'get', fields]);
+    const expectState = message ? lastState : value;
+    t.deepEqual(kser(state), kser(expectState), `${label}`);
+    lastState = state;
+  }
+
+  const store4 = new Map(store3);
+  const { syscall: sc4, log: log4 } = buildSyscall({ kvStore: store4 });
+  const ls4 = makeLiveSlots(sc4, 'vatA', {}, {}, gcTools, undefined, () => {
+    const buildRootObject = ({ VatData }, vatParameters, baggage) => {
+      const { x, y } = vatParameters;
+      const kh = baggage.get('kh');
+      const redefineDurableKind = stateShape =>
+        defineHolder(VatData.defineDurableKind, kh, stateShape);
+      const badShapes = [
+        // Removing optionality from x2.
+        { x, x2: x, y },
+        // Equivalent but not *equal* to M.or(M.undefined(), x).
+        { x, x2: M.or(undefined, x), y },
+        { x, x2: M.or(x, M.undefined()), y },
+      ];
+      for (const stateShape of badShapes) {
+        t.throws(() => redefineDurableKind(stateShape), stateShapeMismatch);
+      }
+      redefineDurableKind({ x, x2: M.or(M.undefined(), x), y });
+      return Far('root', {});
+    };
+    return { buildRootObject };
+  });
+  const vatParameters4 = { x: passables.get(vref1), y: passables.get(vref2) };
+  await ls4.dispatch(makeStartVat(kser(vatParameters4)));
+
+  for (const [label, value, message] of verifications) {
+    const tryUpdate = () =>
+      dispatchForResult(ls4, log4, [objRef, 'set', [value]]);
+    await (message
+      ? t.throwsAsync(tryUpdate, { message }, `${label} ${message}`)
+      : tryUpdate());
+    const state = await dispatchForResult(ls4, log4, [objRef, 'get', fields]);
+    const expectState = message ? lastState : value;
+    t.deepEqual(kser(state), kser(expectState), `${label}`);
+    lastState = state;
+  }
 });
