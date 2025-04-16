fbc439f4d884b04ac7970ac84a44e66a6be1b76c
Turadg Aleahmad <turadg@agoric.com>
2025-04-16 13:31:02 -0700

    feat: validate SwingSetCapData

--- packages/swingset-liveslots/src/capdata.js
+++ packages/swingset-liveslots/src/capdata.js
@@ -5,15 +5,18 @@ import { Fail } from '@endo/errors';
  * actually is.  A capdata object should have a .body property that's a string
  * and a .slots property that's an array of strings.
  *
- * @param {any} capdata  The object to be tested
+ * @param {any} specimen  The object to be tested
  * @throws {Error} if, upon inspection, the parameter does not satisfy the above
  *   criteria.
- * @returns {asserts capdata is import('./types.js').SwingSetCapData}
+ * @returns {asserts specimen is import('./types.js').SwingSetCapData}
  */
-export function insistCapData(capdata) {
-  typeof capdata.body === 'string' ||
-    Fail`capdata has non-string .body ${capdata.body}`;
-  Array.isArray(capdata.slots) ||
-    Fail`capdata has non-Array slots ${capdata.slots}`;
-  // TODO check that the .slots array elements are actually strings
+export function insistCapData(specimen) {
+  typeof specimen.body === 'string' ||
+    Fail`capdata has non-string .body ${specimen.body}`;
+  Array.isArray(specimen.slots) ||
+    Fail`capdata has non-Array slots ${specimen.slots}`;
+  assert(
+    specimen.slots.every(slot => typeof slot === 'string'),
+    'All slots must be strings',
+  );
 }
