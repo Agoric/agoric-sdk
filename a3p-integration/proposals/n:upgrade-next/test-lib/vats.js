7e89aa8cba7c300cbab33ef6da57887b65051696
Richard Gibson <richard.gibson@gmail.com>
2025-04-17 21:44:30 -0400

    chore(a3p-integration): Use a local function for natural comparison

--- a3p-integration/proposals/n:upgrade-next/test-lib/vats.js
+++ a3p-integration/proposals/n:upgrade-next/test-lib/vats.js
@@ -1,12 +1,19 @@
 import { getDetailsMatchingVats } from '@agoric/synthetic-chain';
-import { naturalCompare } from '@agoric/internal';
+
+// Poor man's naturalCompare, since that function cannot be imported from
+// @agoric/internal at time of writing.
+const compareVatIDs = (a, b) => {
+  const [_a, aSuffix] = a.vatID.match(/^v([0-9]+)$/);
+  const [_b, bSuffix] = b.vatID.match(/^v([0-9]+)$/);
+  return aSuffix - bSuffix;
+};
 
 export const getGovernedVatDetails = async (nameCommon, includeTerminated) => {
   const detailsArray = await getDetailsMatchingVats(nameCommon);
   return (
     detailsArray
       // the governor launches the governed, and therefore has a lower vatID
-      .sort((a, b) => naturalCompare(a.vatID, b.vatID))
+      .sort(compareVatIDs)
       .flatMap(({ vatName, terminated, ...details }) => {
         if (terminated && !includeTerminated) return [];
         const nameSuffix = vatName.slice(
