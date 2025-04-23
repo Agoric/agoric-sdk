1c76e82fc09a5c7277e05732692b26baa945f527
anilhelvaci <anilhelvaci96@gmail.com>
2025-04-23 16:13:43 +0300

    chore: implement swap-anything.contract.js with zoe offers
    chore: WIP swap anything
    
    Refs: #8863
    
    chore: unit test for happy path
    
    Refs: #8863
    
    chore: init multichain testing work
    
    Refs: #8863
    
    chore: WIP basic use case multichain testing
    
    Refs: #8863
    
    chore: improve assertions, add README
    
    Refs: #8863

--- packages/orchestration/src/examples/swap-anything.flows.js
+++ packages/orchestration/src/examples/swap-anything.flows.js
@@ -1,13 +1,67 @@
+import { NonNullish, makeTracer } from '@agoric/internal';
+import { Fail, makeError, q } from '@endo/errors';
+import { M, mustMatch } from '@endo/patterns';
+
+const trace = makeTracer('SwapAnything');
+
+const { entries } = Object;
+
 /**
+ * @typedef {{
+ *   destAddr: string;
+ *   receiverAddr: string;
+ *   outDenom: Denom;
+ *   slippage: { slippagePercentage: string; windowSeconds: number };
+ *   onFailedDelivery: string;
+ *   nextMemo?: string;
+ * }} SwapInfo
+ *
  * @import {GuestInterface, GuestOf} from '@agoric/async-flow';
- * @import {Invitation, ZCF, ZCFSeat} from '@agoric/zoe';
- * @import {Brand} from '@agoric/ertp';
+ * @import {Denom} from '@agoric/orchestration';
+ * @import {ZCFSeat} from '@agoric/zoe';
  * @import {Vow} from '@agoric/vow';
  * @import {LocalOrchestrationAccountKit} from '../exos/local-orchestration-account.js';
  * @import {ZoeTools} from '../utils/zoe-tools.js';
- * @import {Orchestrator, OrchestrationFlow, LocalAccountMethods, ChainHub, ChainInfo} from '../types.js';
- * @import {AccountIdArg} from '../orchestration-api.ts';
+ * @import {Orchestrator, OrchestrationFlow, ChainHub, ChainInfo, CosmosChainInfo} from '../types.js';
  */
+
+const denomForBrand = async (orch, brand) => {
+  const agoric = await orch.getChain('agoric');
+  const assets = await agoric.getVBankAssetInfo();
+  const { denom } = NonNullish(
+    assets.find(a => a.brand === brand),
+    `${brand} not registered in ChainHub`,
+  );
+  return denom;
+};
+
+/**
+ * @param {SwapInfo} swapInfo
+ * @returns {string}
+ */
+const buildXCSMemo = swapInfo => {
+  const memo = {
+    wasm: {
+      contract: swapInfo.destAddr,
+      msg: {
+        osmosis_swap: {
+          output_denom: swapInfo.outDenom,
+          slippage: {
+            twap: {
+              window_seconds: swapInfo.slippage.windowSeconds,
+              slippage_percentage: swapInfo.slippage.slippagePercentage,
+            },
+          },
+          receiver: swapInfo.receiverAddr,
+          on_failed_delivery: swapInfo.onFailedDelivery,
+          nextMemo: swapInfo.nextMemo,
+        },
+      },
+    },
+  };
+
+  return JSON.stringify(memo);
+};
 
 /**
  * @satisfies {OrchestrationFlow}
@@ -15,22 +69,10 @@
  * @param {object} ctx
  * @param {GuestInterface<ChainHub>} ctx.chainHub
  * @param {Promise<GuestInterface<LocalOrchestrationAccountKit['holder']>>} ctx.sharedLocalAccountP
- * @param {Promise<
- *   GuestInterface<
- *     import('../exos/cosmos-orchestration-account.js').CosmosOrchestrationAccountKit['holder']
- *   >
- * >} ctx.nobleAccountP
  * @param {GuestInterface<ZoeTools>} ctx.zoeTools
  * @param {GuestOf<(msg: string) => Vow<void>>} ctx.log
- * @param {Brand} ctx.USDC
  * @param {ZCFSeat} seat
- * @param {{
- *   destAddr: AccountIdArg;
- *   destDenom: Denom;
- *   slippage: { slippageRatio: Ratio; windowSeconds: Nat };
- *   onFailedDelivery: string;
- *   nextMemo: string;
- * }} offerArgs
+ * @param {SwapInfo} offerArgs
  */
 
 // Given USDC, swap to desired token with slippage
@@ -41,11 +83,81 @@ export const swapIt = async (
     chainHub,
     sharedLocalAccountP,
     log,
-    nobleAccountP,
-    USDC,
     zoeTools: { localTransfer, withdrawToSeat },
   },
   seat,
   offerArgs,
-) => {};
+) => {
+  mustMatch(
+    offerArgs,
+    M.splitRecord(
+      {
+        destAddr: M.string(),
+        receiverAddr: M.string(),
+        outDenom: M.string(),
+        onFailedDelivery: M.string(),
+        slippage: { slippagePercentage: M.string(), windowSeconds: M.number() },
+      },
+      { nextMemo: M.string() },
+    ),
+  );
+
+  const { receiverAddr, destAddr } = offerArgs;
+  // NOTE the proposal shape ensures that the `give` is a single asset
+  const { give } = seat.getProposal();
+  const [[_kw, amt]] = entries(give);
+  void log(`sending {${amt.value}} from osmosis to ${receiverAddr}`);
+  const denom = await denomForBrand(orch, amt.brand);
+
+  /**
+   * @type {any} XXX methods returning vows
+   *   https://github.com/Agoric/agoric-sdk/issues/9822
+   */
+  const sharedLocalAccount = await sharedLocalAccountP;
+
+  /**
+   * Helper function to recover if IBC Transfer fails
+   *
+   * @param {Error} e
+   */
+  const recoverFailedTransfer = async e => {
+    await withdrawToSeat(sharedLocalAccount, seat, give);
+    const errorMsg = `IBC Transfer failed ${q(e)}`;
+    void log(`ERROR: ${errorMsg}`);
+    seat.fail(errorMsg);
+    throw makeError(errorMsg);
+  };
+
+  const [_a, osmosisChainInfo, connection] =
+    await chainHub.getChainsAndConnection('agoric', 'osmosis');
+
+  connection.counterparty || Fail`No IBC connection to Osmosis`;
+
+  void log(`got info for chain: osmosis ${osmosisChainInfo}`);
+  trace(osmosisChainInfo);
+
+  await localTransfer(seat, sharedLocalAccount, give);
+  void log(`completed transfer to localAccount`);
+
+  try {
+    const memo = buildXCSMemo(offerArgs);
+    trace(memo);
+
+    await sharedLocalAccount.transfer(
+      {
+        value: destAddr,
+        encoding: 'bech32',
+        chainId: /** @type {CosmosChainInfo} */ (osmosisChainInfo).chainId,
+      },
+      { denom, value: amt.value },
+      { memo },
+    );
+    void log(`completed transfer to ${destAddr}`);
+  } catch (e) {
+    return recoverFailedTransfer(e);
+  }
+
+  seat.exit();
+  void log(`transfer complete, seat exited`);
+};
 harden(swapIt);
---
+++ packages/orchestration/test/examples/swap-anything.test.ts
@@ -0,0 +1,141 @@
+import { test } from '@agoric/zoe/tools/prepare-test-env-ava.js';
+import { setUpZoeForTest } from '@agoric/zoe/tools/setup-zoe.js';
+import { E } from '@endo/far';
+import * as contractExports from '../../src/examples/swap-anything.contract.js';
+import { commonSetup } from '../supports.js';
+
+const contractName = 'swap-anything';
+type StartFn = typeof contractExports.start;
+
+const config = {
+  xcsInformation: {
+    // A message that we know to be working
+    rawMsgNoNextMemo:
+      '{"wasm":{"contract":"osmo17p9rzwnnfxcjp32un9ug7yhhzgtkhvl9jfksztgw5uh69wac2pgs5yczr8","msg":{"osmosis_swap":{"output_denom":"uosmo","slippage":{"twap":{"window_seconds":10,"slippage_percentage":"20"}},"receiver":"agoric1elueec97as0uwavlxpmj75u5w7yq9dgphq47zx","on_failed_delivery":"do_nothing"}}}}',
+  },
+};
+
+const bootstrapOrchestration = async t => {
+  t.log('bootstrap, orchestration core-eval');
+  const {
+    bootstrap,
+    commonPrivateArgs,
+    brands: { bld },
+    utils: { inspectLocalBridge, pourPayment, transmitTransferAck },
+  } = await commonSetup(t);
+  const vt = bootstrap.vowTools;
+
+  const { zoe, bundleAndInstall } = await setUpZoeForTest();
+
+  t.log('contract coreEval', contractName);
+
+  const installation: Installation<StartFn> =
+    await bundleAndInstall(contractExports);
+
+  const storageNode = await E(bootstrap.storage.rootNode).makeChildNode(
+    contractName,
+  );
+
+  const swapKit = await E(zoe).startInstance(
+    installation,
+    { BLD: bld.issuer },
+    {},
+    { ...commonPrivateArgs, storageNode },
+  );
+
+  return {
+    bootstrap,
+    commonPrivateArgs,
+    bld,
+    inspectLocalBridge,
+    pourPayment,
+    transmitTransferAck,
+    vt,
+    zoe,
+    installation,
+    storageNode,
+    swapKit,
+  };
+};
+
+const buildOfferArgs = rawMsg => {
+  const {
+    wasm: {
+      contract,
+      msg: {
+        osmosis_swap: {
+          receiver,
+          output_denom: outDenom,
+          slippage: {
+            twap: {
+              slippage_percentage: slippagePercentage,
+              window_seconds: windowSeconds,
+            },
+          },
+
+          on_failed_delivery: onFailedDelivery,
+          next_memo: nextMemo,
+        },
+      },
+    },
+  } = JSON.parse(rawMsg);
+
+  return {
+    destAddr: contract,
+    receiverAddr: receiver,
+    outDenom,
+    slippage: { slippagePercentage, windowSeconds },
+    onFailedDelivery,
+    nextMemo,
+  };
+};
+
+test('swap BLD for Osmo, receiver on Agoric', async t => {
+  const {
+    vt,
+    swapKit,
+    zoe,
+    pourPayment,
+    bld,
+    transmitTransferAck,
+    inspectLocalBridge,
+  } = await bootstrapOrchestration(t);
+
+  const publicFacet = await E(zoe).getPublicFacet(swapKit.instance);
+  const inv = E(publicFacet).makeSendInvitation();
+  const amt = await E(zoe).getInvitationDetails(inv);
+  t.is(amt.description, 'swap');
+
+  const anAmt = bld.units(3.5);
+  const Send = await pourPayment(anAmt);
+  const offerArgs = buildOfferArgs(config.xcsInformation.rawMsgNoNextMemo);
+
+  const userSeat = await E(zoe).offer(
+    inv,
+    { give: { Send: anAmt } },
+    { Send },
+    offerArgs,
+  );
+  await transmitTransferAck();
+  await vt.when(E(userSeat).getOfferResult());
+
+  const history = inspectLocalBridge();
+  t.log(history);
+  t.like(history, [
+    { type: 'VLOCALCHAIN_ALLOCATE_ADDRESS' },
+    { type: 'VLOCALCHAIN_EXECUTE_TX' },
+  ]);
+
+  t.like(
+    history.find(x => x.type === 'VLOCALCHAIN_EXECUTE_TX')?.messages?.[0],
+    {
+      '@type': '/ibc.applications.transfer.v1.MsgTransfer',
+      receiver: offerArgs.destAddr, // XCS contract has to be the one receiving the IBC message
+      sender: 'agoric1fakeLCAAddress',
+      memo: config.xcsInformation.rawMsgNoNextMemo,
+    },
+    'crosschain swap sent',
+  );
+});
+
+test.todo('should throw when Osmosis not connected');
