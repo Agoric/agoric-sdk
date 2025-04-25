0fe4e71fdefd18d87aa973599190cb21c882b178
Turadg Aleahmad <turadg@agoric.com>
2025-04-24 19:29:20 -0700

    test(contract): forwarding

---
+++ packages/fast-usdc-contract/test/forwarding.test.ts
@@ -0,0 +1,147 @@
+import { test as anyTest } from '@agoric/zoe/tools/prepare-test-env-ava.js';
+import type { TestFn } from 'ava';
+
+import { eventLoopIteration } from '@agoric/internal/src/testing-utils.js';
+import { buildVTransferEvent } from '@agoric/orchestration/tools/ibc-mocks.js';
+import { E, type EReturn } from '@endo/far';
+import { makePublishKit } from '@agoric/notifier';
+import {
+  makeCustomer,
+  makeOracleOperator,
+  makeTestContext,
+} from './contract-setup.ts';
+
+const test = anyTest as TestFn<EReturn<typeof makeTestContext>>;
+test.before(async t => {
+  t.context = await makeTestContext(t);
+  const {
+    startKit: { zoe, invitations },
+    evm: { txPub },
+    sync,
+  } = t.context;
+  // start oracles in this before() hook since this test doesn't test their behavior
+  sync.ocw.resolve(
+    await Promise.all(
+      invitations.operator.map(async opInv => {
+        const op = makeOracleOperator(opInv, txPub.subscriber, zoe, t);
+        await E(op).watch();
+        return op;
+      }),
+    ),
+  );
+});
+
+test.serial('Observed after mint: Forward', async t => {
+  const {
+    bridges: { snapshot, since },
+    evm: { cctp, txPub },
+    common: {
+      bootstrap: { storage },
+      commonPrivateArgs: { feeConfig },
+      facadeServices: { chainHub },
+      mocks: { transferBridge },
+      utils: { transmitVTransferEvent },
+    },
+    mint,
+    addresses,
+  } = t.context;
+
+  const nullPublishKit = makePublishKit();
+  const user = makeCustomer(
+    'Otto',
+    cctp,
+    nullPublishKit.publisher, // so it's not attested
+    feeConfig,
+    chainHub,
+  );
+
+  const EUD = 'osmo10tt0';
+  const mintAmt = 5_000_000n;
+  const evidence = await user.sendFast(t, mintAmt, EUD);
+
+  await mint(evidence);
+
+  t.throws(
+    () => storage.getDeserialized(`fun.txns.${evidence.txHash}`),
+    undefined,
+    'no record of txn until observed',
+  );
+  await eventLoopIteration();
+  const bridgePos = snapshot();
+
+  // now observe, which triggers forward()
+  txPub.publisher.publish({ evidence, isRisk: false });
+
+  // and make the forward() succeed
+  await transmitVTransferEvent('acknowledgementPacket');
+
+  const [outgoingForward] = since(bridgePos).local;
+  t.like(outgoingForward, {
+    type: 'VLOCALCHAIN_EXECUTE_TX',
+    address: addresses.settlementAccount,
+    messages: [
+      {
+        '@type': '/ibc.applications.transfer.v1.MsgTransfer',
+      },
+    ],
+  });
+  const [outgoingForwardMessage] = outgoingForward.messages;
+  t.is(
+    outgoingForwardMessage.token.amount,
+    String(evidence.tx.amount),
+    'full amount is transferred via `.forward()`',
+  );
+
+  const forwardInfo = JSON.parse(outgoingForwardMessage.memo).forward;
+  t.is(forwardInfo.receiver, EUD, 'receiver is osmo10tt0');
+
+  // in lieu of transmitTransferAck so we can set a nonce that matches our initial Advance
+  await E(transferBridge).fromBridge(
+    buildVTransferEvent({
+      receiver: outgoingForwardMessage.receiver,
+      sender: outgoingForwardMessage.sender,
+      target: outgoingForwardMessage.sender,
+      sourceChannel: outgoingForwardMessage.sourceChannel,
+      sequence: BigInt(Number(evidence.txHash.slice(2))),
+      denom: outgoingForwardMessage.token.denom,
+      amount: BigInt(outgoingForwardMessage.token.amount),
+    }),
+  );
+  await eventLoopIteration();
+
+  t.deepEqual(storage.getDeserialized(`fun.txns.${evidence.txHash}`), [
+    { evidence, status: 'OBSERVED' },
+    { status: 'FORWARDED' },
+  ]);
+});
+
+test.serial('Observed after mint: Forward failed', async t => {
+  const {
+    evm: { cctp, txPub },
+    common: {
+      bootstrap: { storage },
+      commonPrivateArgs: { feeConfig },
+      facadeServices: { chainHub },
+      utils: { transmitVTransferEvent },
+    },
+    mint,
+  } = t.context;
+
+  const user = makeCustomer('Otto', cctp, txPub.publisher, feeConfig, chainHub);
+
+  // publish a risky transaction, so the Advance is skipped and forward() instead
+  const evidence = await user.sendFast(t, 5_000_000n, 'osmo10tt0', true);
+
+  // and make the forward() timeout
+  await mint(evidence);
+  await transmitVTransferEvent('timeoutPacket');
+
+  t.deepEqual(storage.getDeserialized(`fun.txns.${evidence.txHash}`), [
+    { evidence, status: 'OBSERVED' },
+    {
+      risksIdentified: ['RISK1'],
+      status: 'ADVANCE_SKIPPED',
+    },
+    { status: 'FORWARD_FAILED' },
+  ]);
+});
