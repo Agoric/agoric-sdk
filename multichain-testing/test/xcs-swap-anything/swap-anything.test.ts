915fd4d697580c6dd73da5084a549be790b17e5f
anilhelvaci <anilhelvaci96@gmail.com>
2025-04-28 22:44:54 +0300

    chore: WIP basic use case multichain testing
    Refs: #8863

---
+++ multichain-testing/test/xcs-swap-anything/swap-anything.test.ts
@@ -0,0 +1,97 @@
+import anyTest from '@endo/ses-ava/prepare-endo.js';
+import type { TestFn } from 'ava';
+import { makeDoOffer } from '../../tools/e2e-tools.js';
+import {
+  commonSetup,
+  type SetupContextWithWallets,
+} from '../support.js';
+import { AmountMath } from '@agoric/ertp';
+import { makeQueryClient } from '../../tools/query.js';
+
+const test = anyTest as TestFn<SetupContextWithWallets>;
+
+const accounts = ['agoricSender', 'agoricReceiver'];
+
+const contractName = 'swapAnything';
+const contractBuilder =
+  '../packages/builders/scripts/testing/init-swap-anything.js';
+
+test.before(async t => {
+  const { setupTestKeys, ...common } = await commonSetup(t);
+  const { commonBuilderOpts, deleteTestKeys, startContract } = common;
+  await deleteTestKeys(accounts).catch();
+  const wallets = await setupTestKeys(accounts);
+  console.log('WALLETS', wallets);
+  t.context = { ...common, wallets };
+  await startContract(contractName, contractBuilder, commonBuilderOpts);
+});
+
+test('BLD for OSMO, receiver on Agoric', async t => {
+  const {
+    wallets,
+    provisionSmartWallet,
+    vstorageClient,
+    retryUntilCondition,
+    useChain,
+  } = t.context;
+
+  // Provision the Agoric smart wallet
+  const agoricAddr = wallets.agoricSender;
+  const wdUser = await provisionSmartWallet(agoricAddr, {
+    BLD: 1000n,
+    IST: 1000n,
+  });
+  t.log(`Provisioned Agoric smart wallet for ${agoricAddr}`);
+
+  // const osmosisChainId = useChain('osmosis').chain.chain_id;
+
+  // const {
+  //   transferChannel: { counterPartyChannelId, channelId },
+  // } = starshipChainInfo.agoric.connections[osmosisChainId];
+
+  const doOffer = makeDoOffer(wdUser);
+
+  // Verify deposit
+  const apiUrl = await useChain('agoric').getRestEndpoint();
+  const queryClient = makeQueryClient(apiUrl);
+
+  const brands = await vstorageClient.queryData('published.agoricNames.brand');
+  const bldBrand = Object.fromEntries(brands).BLD;
+  const swapInAmount = AmountMath.make(bldBrand, 125n);
+  const { balances: balancesBefore } = await queryClient.queryBalances(
+    wallets.agoricReceiver,
+  );
+
+  // Send swap offer
+  const makeAccountOfferId = `swap-ubld-uosmo-${Date.now()}`;
+  await doOffer({
+    id: makeAccountOfferId,
+    invitationSpec: {
+      source: 'agoricContract',
+      instancePath: [contractName],
+      callPipe: [['makeSendInvitation']],
+    },
+    offerArgs: {
+      destAddr: 'osmo1ufs3tlq4umljk0qfe8k5ya0x6hpavn897u2cnf9k0en9jr7qarqq7fzxcr',
+      receiverAddr: wallets.agoricReceiver,
+      outDenom: 'uosmo',
+      slippage: { slippagePercentage: '20', windowSeconds: 10 },
+      onFailedDelivery: 'do_nothing',
+    },
+    proposal: { give: { Send: swapInAmount } },
+  });
+
+  const agoricReceiverBalance = await retryUntilCondition(
+    () => queryClient.queryBalances(wallets.agoricReceiver),
+    ({ balances }) => balances.length > balancesBefore.length,
+    'Deposit reflected in localOrchAccount balance',
+  );
+  t.log(agoricReceiverBalance);
+
+  t.pass();
+});
+
+test.after(async t => {
+  const { deleteTestKeys } = t.context;
+  deleteTestKeys(accounts);
+});
