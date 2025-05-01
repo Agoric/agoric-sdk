cd2e0d7afb4fa2d41b87dfb988534740153879e6
Jeancarlo Barrios <JeancarloBarrios@users.noreply.github.com>
2025-05-01 16:38:14 -0600

    wip

--- packages/payments/src/my.flows.ts
+++ packages/payments/src/my.flows.ts
@@ -5,17 +5,24 @@ import type {
   OrchestrationFlow,
   Orchestrator,
 } from '@agoric/orchestration';
-import type { TargetApp } from '@agoric/vats/src/bridge-target.js';
-import type { Passable } from '@endo/pass-style';
 import type { GuestInterface, GuestOf } from '@agoric/async-flow';
 import type { LocalOrchestrationAccountKit } from '@agoric/orchestration/src/exos/local-orchestration-account';
 import type { Vow } from '@agoric/vow';
 import { Fail } from '@endo/errors';
 import { makeTracer } from '@agoric/internal';
-import type { ZCFSeat } from '@agoric/zoe';
-
+import type { Denom } from '@agoric/orchestration';
+import type { TargetApp } from '@agoric/vats/src/bridge-target';
 
 const trace = makeTracer('SwapAndSend');
+
+type SwapInfo = {
+  destAddr: string;
+  receiverAddr: string;
+  outDenom: Denom;
+  slippage: { slippagePercentage: string; windowSeconds: number };
+  onFailedDelivery: string;
+  nextMemo?: string;
+};
 
 export const makeHookAccount = (async (
   orch: Orchestrator,
@@ -34,7 +41,6 @@ export const makeHookAccount = (async (
   return hookAccount;
 }) satisfies OrchestrationFlow;
 harden(makeHookAccount);
-
 
 /**
  * @param {SwapInfo} swapInfo
@@ -70,13 +76,18 @@ type SendContext = {
     GuestInterface<LocalOrchestrationAccountKit['holder']>
   >;
   log: GuestOf<(msg: string) => Vow<void>>;
-  tools: { localTransfer, withdrawToSeat };
+  tools: { localTransfer; withdrawToSeat };
 };
 
+/**
+ * @satisfies {OrchestrationFlow}
+ * @param {Orchestrator} orch
+ * @param {SendContext} ctx
+ * @param {amount: bigint; denom: string; swapDenom: string; sender: string; receiver: string} args
+ */
 export const swapAndSend = async (
   orch: Orchestrator,
   ctx: SendContext,
-  seat: ZCFSeat,
   {
     amount,
     denom,
@@ -85,32 +96,18 @@ export const swapAndSend = async (
     receiver,
   }: {
     amount: bigint;
-    denom: string;
-    swapDenom: string;
+    denom: Denom;
+    swapDenom: Denom;
     sender: string;
     receiver: string;
   },
 ) => {
+  
   // FIXME: this is a placeholder
   console.log('swapAndSend', { amount, denom, swapDenom, sender, receiver });
   const { chainHub, localAccountHolder: localAccountHolderP, log, tools } = ctx;
 
   const sharedLocalAccount = await localAccountHolderP;
-
-  const { give } = seat.getProposal();
-
-   /**
-   * Helper function to recover if IBC Transfer fails
-   *
-   * @param {Error} e
-   */
-  //  const recoverFailedTransfer = async e => {
-  //   await withdrawToSeat(sharedLocalAccount, seat, give);
-  //   const errorMsg = `IBC Transfer failed ${q(e)}`;
-  //   void log(`ERROR: ${errorMsg}`);
-  //   seat.fail(errorMsg);
-  //   throw makeError(errorMsg);
-  // };
 
   void log(`Initiating send: ${amount} ${denom} to ${receiver} on Osmosis`);
 
@@ -127,33 +124,25 @@ export const swapAndSend = async (
     receiverAddr: receiver,
     outDenom: swapDenom,
     slippage: {
-      slippagePercentage: '1.0', // Default 1% slippage
+      slippagePercentage: '1.0', // Default 1% slippage | Default Value as of now
       windowSeconds: 30, // Default 30 second window
     },
     onFailedDelivery: sender, // Return funds to sender if delivery fails
-  };
+  } as SwapInfo;
 
- 
+  const memo = buildXCSMemo(offerArgs);
+  trace(memo);
 
-  try {
-    const memo = buildXCSMemo(offerArgs);
-    trace(memo)
+  await sharedLocalAccount.transfer(
+    {
+      value: receiver as `${string}1${string}`,
+      encoding: 'bech32',
+      chainId: /** @type {CosmosChainInfo} */ cosmosOsmosisChainInfo.chainId,
+    },
+    { denom, value: BigInt(amount) },
+    { memo },
+  );
+  void log(`completed transfer to ${receiver}`);
 
-    await sharedLocalAccount.transfer(
-      {
-        value: receiver as `${string}1${string}`,
-        encoding: 'bech32',
-        chainId: /** @type {CosmosChainInfo} */ (cosmosOsmosisChainInfo).chainId,
-      },
-      { denom, value: amount },
-      { memo },
-    );
-  } catch (e) {
-  }
-
-
-
-
-  const result = await Promise.resolve('FIXME: swapAndSend result');
-  return result;
+  return 'Done';
 };
