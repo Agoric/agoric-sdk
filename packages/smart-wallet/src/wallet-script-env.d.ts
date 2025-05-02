30620d71f5521312e62ec16b8f91dad66ba2544e
Turadg Aleahmad <turadg@agoric.com>
2025-05-02 12:55:01 -0700

    feat(types): wallet-script-env

---
+++ packages/smart-wallet/src/wallet-script-env.d.ts
@@ -0,0 +1,33 @@
+/* eslint-disable */
+/**
+ * @file typedefs for the Smart Wallet script environment
+ *
+ *   To use add this to the top of the proposal:
+ *
+ *   /// <reference types="@agoric/smart-wallet/src/wallet-script-env" />
+ *
+ *   That directive has to be before imports, but this one's only useful in
+ *   modules that have no imports or named exports.
+ */
+
+import type * as far from '@endo/far';
+import type { Assert } from 'ses';
+import type { AllPowers } from './smartWallet.js';
+
+// Provided by 'CORE_EVAL' handler in chain-behaviors.js
+declare global {
+  // powers the script may be permitted to use
+  var powers: AllPowers;
+
+  // @endo/far exports
+  var E: typeof far.E;
+
+  // This is correctly the `Assert` type from `'ses'`, not from @endo/errors
+  // See https://github.com/Agoric/agoric-sdk/issues/9515
+  var assert: Assert;
+
+  // console is a VirtualConsole but this directive fails to override the extant global `console`
+  // var console: VirtualConsole;
+
+  // Base64 and URL are not available in all environments
+}
