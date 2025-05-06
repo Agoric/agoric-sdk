Agoric/agoric

chore(types): codegen with Endo base64Lib

packages/cosmic-proto/src/codegen/agoric/swingset/query.ts
@@ -7,7 +7,9 @@ import {
 } from './swingset.js';
 import { BinaryReader, BinaryWriter } from '../../binary.js';
 import { type JsonSafe } from '../../json-safe.js';
-import { isSet, bytesFromBase64, base64FromBytes } from '../../helpers.js';
+import { isSet } from '../../helpers.js';
+import { encodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { decodeBase64 as base64FromBytes } from '@endo/base64';
 /** QueryParamsRequest is the request type for the Query/Params RPC method. */
 export interface QueryParamsRequest {}
 export interface QueryParamsRequestProtoMsg {

packages/cosmic-proto/src/codegen/agoric/swingset/swingset.ts
@@ -1,8 +1,10 @@
 //@ts-nocheck
 import { Coin, type CoinSDKType } from '../../cosmos/base/v1beta1/coin.js';
 import { BinaryReader, BinaryWriter } from '../../binary.js';
-import { isSet, bytesFromBase64, base64FromBytes } from '../../helpers.js';
+import { isSet } from '../../helpers.js';
 import { type JsonSafe } from '../../json-safe.js';
+import { encodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { decodeBase64 as base64FromBytes } from '@endo/base64';
 /**
  * CoreEvalProposal is a gov Content type for evaluating code in the SwingSet
  * core.

packages/cosmic-proto/src/codegen/cosmos/mint/v1beta1/query.ts
@@ -2,7 +2,9 @@
 import { Params, type ParamsSDKType } from './mint.js';
 import { BinaryReader, BinaryWriter } from '../../../binary.js';
 import { type JsonSafe } from '../../../json-safe.js';
-import { isSet, bytesFromBase64, base64FromBytes } from '../../../helpers.js';
+import { isSet } from '../../../helpers.js';
+import { encodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { decodeBase64 as base64FromBytes } from '@endo/base64';
 /** QueryParamsRequest is the request type for the Query/Params RPC method. */
 export interface QueryParamsRequest {}
 export interface QueryParamsRequestProtoMsg {

packages/cosmic-proto/src/codegen/google/protobuf/descriptor.ts
@@ -1,7 +1,9 @@
 //@ts-nocheck
 import { BinaryReader, BinaryWriter } from '../../binary.js';
 import { type JsonSafe } from '../../json-safe.js';
-import { isSet, bytesFromBase64, base64FromBytes } from '../../helpers.js';
+import { isSet } from '../../helpers.js';
+import { encodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { decodeBase64 as base64FromBytes } from '@endo/base64';
 export enum FieldDescriptorProto_Type {
   /**
    * TYPE_DOUBLE - 0 is reserved for errors.

packages/cosmic-proto/src/codegen/ibc/applications/interchain_accounts/v1/packet.ts
@@ -1,11 +1,9 @@
 //@ts-nocheck
 import { Any, type AnySDKType } from '../../../../google/protobuf/any.js';
 import { BinaryReader, BinaryWriter } from '../../../../binary.js';
-import {
-  isSet,
-  bytesFromBase64,
-  base64FromBytes,
-} from '../../../../helpers.js';
+import { isSet } from '../../../../helpers.js';
+import { encodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { decodeBase64 as base64FromBytes } from '@endo/base64';
 import { type JsonSafe } from '../../../../json-safe.js';
 /**
  * Type defines a classification of message issued from a controller chain to its associated interchain accounts

packages/cosmic-proto/src/codegen/ibc/core/commitment/v1/commitment.ts
@@ -4,11 +4,9 @@ import {
   type CommitmentProofSDKType,
 } from '../../../../proofs.js';
 import { BinaryReader, BinaryWriter } from '../../../../binary.js';
-import {
-  isSet,
-  bytesFromBase64,
-  base64FromBytes,
-} from '../../../../helpers.js';
+import { isSet } from '../../../../helpers.js';
+import { encodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { decodeBase64 as base64FromBytes } from '@endo/base64';
 import { type JsonSafe } from '../../../../json-safe.js';
 /**
  * MerkleRoot defines a merkle root hash.

packages/cosmic-proto/src/codegen/ibc/lightclients/solomachine/v1/solomachine.ts
@@ -9,12 +9,10 @@ import {
   type ChannelSDKType,
 } from '../../../core/channel/v1/channel.js';
 import { BinaryReader, BinaryWriter } from '../../../../binary.js';
-import {
-  isSet,
-  bytesFromBase64,
-  base64FromBytes,
-} from '../../../../helpers.js';
+import { isSet } from '../../../../helpers.js';
 import { type JsonSafe } from '../../../../json-safe.js';
+import { encodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { decodeBase64 as base64FromBytes } from '@endo/base64';
 /**
  * DataType defines the type of solo machine proof being created. This is done
  * to preserve uniqueness of different data sign byte encodings.

packages/cosmic-proto/src/codegen/proofs.ts
@@ -1,6 +1,8 @@
 //@ts-nocheck
 import { BinaryReader, BinaryWriter } from './binary.js';
-import { isSet, bytesFromBase64, base64FromBytes } from './helpers.js';
+import { isSet } from './helpers.js';
+import { encodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { decodeBase64 as base64FromBytes } from '@endo/base64';
 import { type JsonSafe } from './json-safe.js';
 export enum HashOp {
   /** NO_HASH - NO_HASH is the default if no data passed. Note this is an illegal argument some places. */

Agoric/agoric-sdk

packages/cosmic-proto/src/codegen/agoric/swingset/query.ts
@@ -7,7 +7,9 @@ import {
 } from './swingset.js';
 import { BinaryReader, BinaryWriter } from '../../binary.js';
 import { type JsonSafe } from '../../json-safe.js';
-import { isSet, bytesFromBase64, base64FromBytes } from '../../helpers.js';
+import { isSet } from '../../helpers.js';
+import { decodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { encodeBase64 as base64FromBytes } from '@endo/base64';
 /** QueryParamsRequest is the request type for the Query/Params RPC method. */
 export interface QueryParamsRequest {}
 export interface QueryParamsRequestProtoMsg {

packages/cosmic-proto/src/codegen/agoric/swingset/swingset.ts
@@ -1,8 +1,10 @@
 //@ts-nocheck
 import { Coin, type CoinSDKType } from '../../cosmos/base/v1beta1/coin.js';
 import { BinaryReader, BinaryWriter } from '../../binary.js';
-import { isSet, bytesFromBase64, base64FromBytes } from '../../helpers.js';
+import { isSet } from '../../helpers.js';
 import { type JsonSafe } from '../../json-safe.js';
+import { decodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { encodeBase64 as base64FromBytes } from '@endo/base64';
 /**
  * CoreEvalProposal is a gov Content type for evaluating code in the SwingSet
  * core.

packages/cosmic-proto/src/codegen/cosmos/mint/v1beta1/query.ts
@@ -2,7 +2,9 @@
 import { Params, type ParamsSDKType } from './mint.js';
 import { BinaryReader, BinaryWriter } from '../../../binary.js';
 import { type JsonSafe } from '../../../json-safe.js';
-import { isSet, bytesFromBase64, base64FromBytes } from '../../../helpers.js';
+import { isSet } from '../../../helpers.js';
+import { decodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { encodeBase64 as base64FromBytes } from '@endo/base64';
 /** QueryParamsRequest is the request type for the Query/Params RPC method. */
 export interface QueryParamsRequest {}
 export interface QueryParamsRequestProtoMsg {

packages/cosmic-proto/src/codegen/google/protobuf/descriptor.ts
@@ -1,7 +1,9 @@
 //@ts-nocheck
 import { BinaryReader, BinaryWriter } from '../../binary.js';
 import { type JsonSafe } from '../../json-safe.js';
-import { isSet, bytesFromBase64, base64FromBytes } from '../../helpers.js';
+import { isSet } from '../../helpers.js';
+import { decodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { encodeBase64 as base64FromBytes } from '@endo/base64';
 export enum FieldDescriptorProto_Type {
   /**
    * TYPE_DOUBLE - 0 is reserved for errors.

packages/cosmic-proto/src/codegen/ibc/applications/interchain_accounts/v1/packet.ts
@@ -1,11 +1,9 @@
 //@ts-nocheck
 import { Any, type AnySDKType } from '../../../../google/protobuf/any.js';
 import { BinaryReader, BinaryWriter } from '../../../../binary.js';
-import {
-  isSet,
-  bytesFromBase64,
-  base64FromBytes,
-} from '../../../../helpers.js';
+import { isSet } from '../../../../helpers.js';
+import { decodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { encodeBase64 as base64FromBytes } from '@endo/base64';
 import { type JsonSafe } from '../../../../json-safe.js';
 /**
  * Type defines a classification of message issued from a controller chain to its associated interchain accounts

packages/cosmic-proto/src/codegen/ibc/core/commitment/v1/commitment.ts
@@ -4,11 +4,9 @@ import {
   type CommitmentProofSDKType,
 } from '../../../../proofs.js';
 import { BinaryReader, BinaryWriter } from '../../../../binary.js';
-import {
-  isSet,
-  bytesFromBase64,
-  base64FromBytes,
-} from '../../../../helpers.js';
+import { isSet } from '../../../../helpers.js';
+import { decodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { encodeBase64 as base64FromBytes } from '@endo/base64';
 import { type JsonSafe } from '../../../../json-safe.js';
 /**
  * MerkleRoot defines a merkle root hash.

packages/cosmic-proto/src/codegen/ibc/lightclients/solomachine/v1/solomachine.ts
@@ -9,12 +9,10 @@ import {
   type ChannelSDKType,
 } from '../../../core/channel/v1/channel.js';
 import { BinaryReader, BinaryWriter } from '../../../../binary.js';
-import {
-  isSet,
-  bytesFromBase64,
-  base64FromBytes,
-} from '../../../../helpers.js';
+import { isSet } from '../../../../helpers.js';
 import { type JsonSafe } from '../../../../json-safe.js';
+import { decodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { encodeBase64 as base64FromBytes } from '@endo/base64';
 /**
  * DataType defines the type of solo machine proof being created. This is done
  * to preserve uniqueness of different data sign byte encodings.

packages/cosmic-proto/src/codegen/proofs.ts
@@ -1,6 +1,8 @@
 //@ts-nocheck
 import { BinaryReader, BinaryWriter } from './binary.js';
-import { isSet, bytesFromBase64, base64FromBytes } from './helpers.js';
+import { isSet } from './helpers.js';
+import { decodeBase64 as bytesFromBase64 } from '@endo/base64';
+import { encodeBase64 as base64FromBytes } from '@endo/base64';
 import { type JsonSafe } from './json-safe.js';
 export enum HashOp {
   /** NO_HASH - NO_HASH is the default if no data passed. Note this is an illegal argument some places. */