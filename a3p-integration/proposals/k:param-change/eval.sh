d8eb9e813d54a1d6fdff848f8b552a998502aa28
Richard Gibson <richard.gibson@gmail.com>
2025-05-14 15:21:34 -0400

    test(a3p-integration): Fund the proposal submitter account with BLD

--- a3p-integration/proposals/k:param-change/eval.sh
+++ a3p-integration/proposals/k:param-change/eval.sh
@@ -5,9 +5,16 @@ set -e
 
 source /usr/src/upgrade-test-scripts/env_setup.sh
 
+if [ -z "$SIGN_BROADCAST_OPTS" ]; then
+  echo >&2 'Missing SIGN_BROADCAST_OPTS!'
+  exit 1
+fi
+
+# After the param change, proposals (including install-bundle) will require BLD.
+agd tx bank send validator "$GOV1ADDR" 10000000000ubld $SIGN_BROADCAST_OPTS
+
 # shellcheck disable=SC2086
-agd tx gov submit-proposal param-change drop-ist.json \
-  ${SIGN_BROADCAST_OPTS="--missing-env-setup"}
+agd tx gov submit-proposal param-change drop-ist.json $SIGN_BROADCAST_OPTS
 
 voteLatestProposalAndWait
 
