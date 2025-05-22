f0b48bc8f85ca514cb18137a4f6b58cfe21c6daf
Turadg Aleahmad <turadg@agoric.com>
2025-05-22 16:50:02 -0700

    chore: drop support for Node 18

--- repoconfig.sh
+++ repoconfig.sh
@@ -20,10 +20,9 @@ golang_version_check() {
 nodejs_version_check() {
   # Keep synchronized with README.md section "Prerequisites".
   {
-    [ "$1" -eq 18 ] && [ "$2" -ge 12 ] && return 0
     [ "$1" -eq 20 ] && [ "$2" -ge 9 ] && return 0
     [ "$1" -eq 22 ] && [ "$2" -ge 11 ] && return 0
   } 2> /dev/null
-  echo 1>&2 "need Node.js LTS version ^18.12 or ^20.9 or ^22.11, found $4"
+  echo 1>&2 "need Node.js LTS version ^20.9 or ^22.11, found $4"
   return 1
 }
