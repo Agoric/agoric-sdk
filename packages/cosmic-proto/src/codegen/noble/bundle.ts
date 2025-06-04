27db9554fc034b4e7f2a1e3c44db8b22b453e1b2
Dan Connolly <connolly@agoric.com>
2025-06-03 22:07:29 -0500

    chore(cosmic-swingset): codegen noble

---
+++ packages/cosmic-proto/src/codegen/noble/bundle.ts
@@ -0,0 +1,23 @@
+//@ts-nocheck
+import * as _170 from './dollar/vaults/v1/tx.js';
+import * as _171 from './dollar/vaults/v1/vaults.js';
+import * as _172 from './swap/v1/algorithm.js';
+import * as _173 from './swap/v1/swap.js';
+import * as _174 from './swap/v1/tx.js';
+export namespace noble {
+  export namespace dollar {
+    export namespace vaults {
+      export const v1 = {
+        ..._170,
+        ..._171,
+      };
+    }
+  }
+  export namespace swap {
+    export const v1 = {
+      ..._172,
+      ..._173,
+      ..._174,
+    };
+  }
+}
