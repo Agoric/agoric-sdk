28f81f82134a871f1e5573bb5eeea829e252c4f7
Dan Connolly <connolly@agoric.com>
2025-06-04 17:08:49 -0500

    chore: portfolio/ymax permit

--- packages/portfolio-contract/src/portfolio.contract.meta.ts
+++ packages/portfolio-contract/src/portfolio.contract.meta.ts
@@ -8,6 +8,7 @@ import type { CopyRecord } from '@endo/pass-style';
 import { M } from '@endo/patterns';
 
 export const meta = {
+  name: 'ymax0',
   privateArgsShape: {
     ...(OrchestrationPowersShape as CopyRecord),
     marshaller: M.remotable('marshaller'),
