05d64169a6a6d550bd478abd3b24382359dea4e2
Dan Connolly <connolly@agoric.com>
2025-06-05 03:51:14 -0500

    WIP: multichain-test for portfolio

--- multichain-testing/test/portfolio/portfolio-e2e.test.ts
+++ multichain-testing/test/portfolio/portfolio-e2e.test.ts
@@ -1,44 +1,126 @@
 import anyTest from '@endo/ses-ava/prepare-endo.js';
 
-import { makeTracer } from '@agoric/internal';
+import { makeTracer, mustMatch } from '@agoric/internal';
 import type { ExecutionContext } from 'ava';
 import { commonSetup } from '../support.ts';
 
-import { meta } from '../../../packages/portfolio-contract/src/portfolio.contract.meta.ts';
+import { AmountMath } from '@agoric/ertp';
+import { ChainInfoShape } from '@agoric/orchestration';
+import type {
+  CurrentWalletRecord,
+  UpdateRecord,
+} from '@agoric/smart-wallet/src/smartWallet.js';
+import { M } from '@endo/patterns';
 import type { TestFn } from 'ava';
+import { meta } from '../../../packages/portfolio-contract/src/portfolio.contract.meta.ts';
+import starshipChainInfo from '../../starship-chain-info.js';
+import { seatLike } from '../../tools/e2e-tools.js';
 
 const trace = makeTracer('MCYMX');
 
+const { fromEntries, keys, values } = Object;
+const { make } = AmountMath;
+
 const test = anyTest as TestFn<Awaited<ReturnType<typeof makeTestContext>>>;
+
+const keyMaterial = {
+  trader1:
+    'cause eight cattle slot course mail more aware vapor slab hobby match',
+} as const;
+harden(keyMaterial);
 
 const makeTestContext = async (t: ExecutionContext) => {
   const common = await commonSetup(t, {
     // TODO: config with just agoric, noble
     config: `../config.fusdc.yaml`,
   });
-  const { startContract } = common;
+  const { deleteTestKeys, setupTestKeys, faucetTools, startContract } = common;
+
+  await deleteTestKeys(keys(keyMaterial)); // XXX???
+  const accounts = (await setupTestKeys(
+    keys(keyMaterial),
+    values(keyMaterial),
+  )) as Record<keyof typeof keyMaterial, string>;
+  t.log('setupTestKeys:', accounts);
+
   const { chainInfo, assetInfo } = common.commonBuilderOpts;
   await startContract(
     meta.name,
     '../packages/portfolio-contract/scripts/portfolio.build.js',
     { chainInfo, assetInfo },
+    // { skipInstanceCheck: true }, // XXX
   );
 
-  return common;
+  return { ...common, accounts };
 };
 
 test.before(async t => (t.context = await makeTestContext(t)));
 test.after(async t => {
-  trace('TODO: delete test keys?');
+  const { deleteTestKeys } = t.context;
+  deleteTestKeys(keys(keyMaterial));
 });
 
-test.serial('contract instance is in vstorage', async t => {
-  const { vstorageClient } = t.context;
-
-  // XXX this is already checked by startContract
-  const instance = Object.fromEntries(
-    await vstorageClient.queryData('published.agoricNames.instance'),
+test.serial('starship chain info shape', t => {
+  t.notThrows(() =>
+    mustMatch(starshipChainInfo, M.recordOf(M.string(), ChainInfoShape)),
   );
-  t.log('instances', Object.keys(instance));
-  t.true('ymax0' in instance);
+});
+
+type VStorageClient = Awaited<ReturnType<typeof commonSetup>>['vstorageClient'];
+
+// TODO: factor out of fu-actors?
+export const agoricNamesQ = (vsc: VStorageClient) =>
+  harden({
+    brands: <K extends AssetKind>(_assetKind: K) =>
+      vsc
+        .queryData('published.agoricNames.brand')
+        .then(pairs => fromEntries(pairs) as Record<string, Brand<K>>),
+    instance: (name: string) =>
+      vsc
+        .queryData('published.agoricNames.instance')
+        .then(pairs => fromEntries(pairs)[name] as Instance),
+  });
+const walletQ = (vsc: VStorageClient) => {
+  const self = harden({
+    current: (addr: string) =>
+      vsc.queryData(
+        `published.wallet.${addr}.current`,
+      ) as Promise<CurrentWalletRecord>,
+    update: (addr: string) =>
+      vsc.queryData(`published.wallet.${addr}`) as Promise<UpdateRecord>,
+  });
+  return self;
+};
+
+test.serial('open a USDN position', async t => {
+  const { provisionSmartWallet, accounts, faucetTools } = t.context;
+
+  await faucetTools.fundFaucet([['noble', 'uusdc']]);
+
+  const wallet = await provisionSmartWallet(accounts.trader1, {
+    USDC: 8_000n, // XXX user story calls for 10k, but faucet doesn't provide quite that much
+    BLD: 100n,
+  });
+
+  const { vstorageClient } = t.context;
+  const { USDC } = await agoricNamesQ(vstorageClient).brands('nat');
+  const give = { USDN: make(USDC, 3_333n * 1_000_000n) };
+  t.log('opening portfolio', give);
+  const updates = wallet.offers.executeOffer({
+    id: `open-${Date.now()}`, // XXX ambient
+    invitationSpec: {
+      source: 'agoricContract',
+      instancePath: ['ymax0'],
+      callPipe: [['makeOpenPortfolioInvitation']],
+    },
+    proposal: { give },
+  });
+  const seat = seatLike(updates);
+  const payoutAmounts = await seat.getPayoutAmounts();
+  t.log('payouts', payoutAmounts);
+  t.truthy(payoutAmounts);
+
+  const current = await walletQ(vstorageClient).current(accounts.trader1);
+  t.log('trader1 current', current);
+  t.truthy(current);
 });
