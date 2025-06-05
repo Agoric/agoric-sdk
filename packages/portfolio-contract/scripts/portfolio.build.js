28e5dadb87d9e82dd299110e5ad742654b336b64
Dan Connolly <connolly@agoric.com>
2025-06-05 02:16:45 -0500

    WIP: portfolio-build: supply config

--- packages/portfolio-contract/scripts/portfolio.build.js
+++ packages/portfolio-contract/scripts/portfolio.build.js
@@ -1,39 +1,108 @@
 import { makeHelpers } from '@agoric/deploy-script-support';
+import { parseArgs } from 'node:util';
+import { M } from '@endo/patterns';
+import {
+  ChainInfoShape,
+  DenomDetailShape,
+  DenomShape,
+} from '@agoric/orchestration';
+import { toExternalConfig } from '../src/config-marshal.js';
+
+// TODO: import { PortfolioConfigShape } from '../src/type-guards.js';
+const PortfolioConfigShape = M.splitRecord({
+  chainInfo: M.recordOf(M.string(), ChainInfoShape),
+  assetInfo: M.arrayOf([DenomShape, DenomDetailShape]),
+});
 
 /**
+ * @import {ParseArgsConfig} from 'node:util'
+ * @import {PortfolioConfig} from '../src/type-guards.js';
  * @import { CoreEvalBuilder, DeployScriptFunction } from '@agoric/deploy-script-support/src/externalTypes.js';
  */
 
+/** @type {ParseArgsConfig['options']} */
+const options = {
+  chainInfo: { type: 'string' },
+  assetInfo: { type: 'string' },
+};
+
+/**
+ * @typedef {{
+ *   chainInfo?: string;
+ *   assetInfo?: string;
+ * }} PortfolioOpts
+ */
+
+const DBG = (label, x) => {
+  console.log(label, x);
+  return x;
+};
+
+// XXX copied from portfolio.contract.meta.ts
+const meta = { name: 'ymax0' };
+
+const crossVatContext = /** @type {const} */ ({});
+
 /**
  * @param {Parameters<CoreEvalBuilder>[0]} tools
- * @param {unknown} options
+ * @param {PortfolioConfig} config
  * @satisfies {CoreEvalBuilder}
  */
-const defaultProposalBuilder = async ({ publishRef, install }, options) => {
-  return harden({
-    sourceSpec: '../dist/portfolio-start.core.bundle.js',
-    getManifestCall: [
-      'getManifestForPortfolio', // TODO: unit test agreemnt with getManifestForPortfolio.name
-      {
-        installKeys: {
-          queryFlows: publishRef(
-            install('../dist/portfolio.contract.bundle.js'),
+const defaultProposalBuilder = async ({ publishRef, install }, config) => {
+  return DBG(
+    '@@@defaultProposalBuilder returns',
+    harden({
+      // XXX: using .ts for the core script introduces a build step
+      // that multichain-testing doesn't always do when it's needed.
+      // TODO: eliminate build step by moving to .js? or something
+      sourceSpec: '../dist/portfolio-start.core.bundle.js',
+      getManifestCall: [
+        'getManifestForPortfolio', // TODO: unit test agreemnt with getManifestForPortfolio.name
+        {
+          options: toExternalConfig(
+            config,
+            crossVatContext,
+            PortfolioConfigShape,
           ),
+
+          installKeys: {
+            [meta.name]: publishRef(
+              install('../dist/portfolio.contract.bundle.js'),
+            ),
+          },
         },
-        options,
-      },
-    ],
-  });
+      ],
+    }),
+  );
 };
 
 /** @type {DeployScriptFunction} */ 0;
 const build = async (homeP, endowments) => {
-  // TODO: const { scriptArgs } = endowments;
-  const opts = {};
+  const { scriptArgs } = endowments;
+
+  /** @type {{ values: PortfolioOpts }} */
+  const {
+    values: { chainInfo, assetInfo },
+  } = parseArgs({ args: scriptArgs, options });
+
+  const parseChainInfo = () => {
+    if (!chainInfo) return {};
+    return JSON.parse(chainInfo);
+  };
+  const parseAssetInfo = () => {
+    if (!assetInfo) return [];
+    return JSON.parse(assetInfo);
+  };
+
+  /** @type {PortfolioConfig} */
+  const config = harden({
+    chainInfo: parseChainInfo(),
+    assetInfo: parseAssetInfo(),
+  });
   const { writeCoreEval } = await makeHelpers(homeP, endowments);
   // TODO: unit test agreement with startPortfolio.name
   await writeCoreEval('startPortfolio', utils =>
-    defaultProposalBuilder(utils, opts),
+    defaultProposalBuilder(utils, config),
   );
 };
 
