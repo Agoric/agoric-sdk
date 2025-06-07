ae44a4470d98ceabfebf399146536cb2cee88287
Dan Connolly <connolly@agoric.com>
2025-06-07 16:09:39 -0500

    feat(multichain): noble-usdn-lab swap works on testnet
    $ ./scripts/noble-usdn-lab.ts
    noble18qlqfelxhe7tszqqprm2eqdpzt9s6ry025y3j5 has { denom: 'uusdc', amount: '9416668' }
    [16:08:37.007] [0] $ nobled --node 'https://noble-testnet-rpc.polkachu.com:443' tx swap swap 266666uusdc '{"pool_id":0,"denom_to":"uusdn"}' 263999uusdn '--chain-id=grand-1' '--from=trader1' '--keyring-backend=test' --yes '--fees=25000uusdc' '--gas=auto' '--gas-adjustment=1.3' -o json
    [16:08:37.958] [0] ✔ (done in 951ms)
    {
      txhash: '64689B18834BF0C57EA9E6727905011B655FAD2C3AF67E01B4AB9EC9E5DB2595',
      url: 'https://www.mintscan.io/noble-testnet/tx/64689B18834BF0C57EA9E6727905011B655FAD2C3AF67E01B4AB9EC9E5DB2595'
    }

--- multichain-testing/scripts/noble-usdn-lab.ts
+++ multichain-testing/scripts/noble-usdn-lab.ts
@@ -15,6 +15,15 @@ const configs = {
       rpc: 'http://localhost:26654',
     },
   },
+  testnet: {
+    noble: {
+      faucet: 'https://faucet.circle.com/',
+      rpc: 'https://noble-testnet-rpc.polkachu.com:443',
+      api: 'https://noble-testnet-api.polkachu.com:443',
+      chainId: 'grand-1',
+      explorer: 'https://www.mintscan.io/noble-testnet',
+    },
+  },
 };
 
 const keyMaterial = {
@@ -46,11 +55,12 @@ const main = async ({
   );
   const [{ address }] = await wallet.getAccounts();
 
-  const nobleChain = useChain('noble');
-  const apiUrl = await nobleChain.getRestEndpoint();
+  //   const nobleChain = useChain('noble');
+  //   const apiUrl = await nobleChain.getRestEndpoint();
+  const { api: apiUrl } = configs.testnet.noble;
   const queryClient = makeQueryClient(apiUrl); // XXX pass fetch
 
-  const fundsWanted = 10_000_000_000;
+  const fundsWanted = 4_000_000n;
   for (;;) {
     const { balance } = await queryClient.queryBalance(address, 'uusdc');
     if (balance) {
@@ -59,13 +69,34 @@ const main = async ({
       if (amount >= fundsWanted) break;
     }
     console.log('getting creditFromFaucet for', address);
-    await nobleChain.creditFromFaucet(address);
+    throw Error(`use ${configs.testnet.noble.faucet}`);
+    // await nobleChain.creditFromFaucet(address);
   }
 
-  const route = `pool_id: 0 denom_to: "uusdn"`; // protobuf text format
-  const actual =
-    await $`nobled tx swap swap 1000000uusdc ${route} 950000uusdn --from=trader1 --keyring-backend=test`;
-  console.log(actual);
+  const toTrade = fundsWanted / 15n; // 15 tries
+  const [poolId, denom, denomTo] = [0, 'uusdc', 'uusdn']; // cf. .flows.ts
+  //   const route = `pool_id: ${poolId} denom_to: "${denomTo}"`; // protobuf text format
+  const route = JSON.stringify({ pool_id: poolId, denom_to: denomTo });
+  const { rpc, chainId, explorer } = configs.testnet.noble;
+  const signArgs = [
+    `--chain-id=${chainId}`,
+    // '--broadcast-mode=block',
+    '--from=trader1',
+    '--keyring-backend=test',
+    '--yes',
+    '--fees=25000uusdc',
+    '--gas=auto',
+    '--gas-adjustment=1.3',
+  ];
+  const actual = await $({
+    verbose: 'short',
+  })`nobled --node ${rpc} tx swap swap ${toTrade.toString()}${denom} ${route} ${((toTrade * 99n) / 100n).toString()}${denomTo} ${signArgs} -o json`;
+  //   console.log(actual);
+  const { code, raw_log, txhash } = JSON.parse(actual.stdout);
+  if (code !== 0) {
+    throw Error(raw_log);
+  }
+  console.log({ txhash, url: `${explorer}/tx/${txhash}` });
 };
 
 main().catch(err => {
