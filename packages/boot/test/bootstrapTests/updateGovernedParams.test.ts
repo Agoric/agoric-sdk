809941530503a6b9dcbade7f2304d903b2858f1d
usmanmani1122 <usman.sbf@gmail.com>
2025-06-10 10:09:25 +0000

    replace usage in updateGovernedParams.test

--- packages/boot/test/bootstrapTests/updateGovernedParams.test.ts
+++ packages/boot/test/bootstrapTests/updateGovernedParams.test.ts
@@ -7,27 +7,68 @@
  * 2. force prices to drop so a vault liquidates
  * 3. verify that the bidder gets the liquidated assets.
  */
-import { test as anyTest } from '@agoric/zoe/tools/prepare-test-env-ava.js';
+
+import { createRequire } from 'node:module';
 
 import type { TestFn } from 'ava';
-import { makeAgoricNamesRemotesFromFakeStorage } from '@agoric/vats/tools/board-utils.js';
-import { Fail } from '@endo/errors';
 
-import { makeSwingsetTestKit } from '../../tools/supports.js';
-import {
-  makeGovernanceDriver,
-  makeWalletFactoryDriver,
-} from '../../tools/drivers.js';
 import {
   updateVaultDirectorParams,
   updateVaultManagerParams,
-} from '../tools/changeVaultParams.js';
+} from '@aglocal/boot/test/tools/changeVaultParams.js';
+import {
+  makeGovernanceDriver,
+  makeWalletFactoryDriver,
+} from '@aglocal/boot/tools/drivers.js';
+import {
+  makeCosmicSwingsetTestKit,
+  makeMockBridgeKit,
+} from '@agoric/cosmic-swingset/tools/test-kit.js';
+import { buildProposal } from '@agoric/cosmic-swingset/tools/test-proposal-utils.ts';
+import { NonNullish } from '@agoric/internal';
+import { unmarshalFromVstorage } from '@agoric/internal/src/marshal.js';
+import { makeFakeStorageKit } from '@agoric/internal/src/storage-test-utils.js';
+import { loadSwingsetConfigFile } from '@agoric/swingset-vat';
+import {
+  boardSlottingMarshaller,
+  makeAgoricNamesRemotesFromFakeStorage,
+  slotToBoardRemote,
+} from '@agoric/vats/tools/board-utils.js';
+import { test as anyTest } from '@agoric/zoe/tools/prepare-test-env-ava.js';
+import { Fail } from '@endo/errors';
 
-const makeDefaultTestContext = async t => {
+const { fromCapData } = boardSlottingMarshaller(slotToBoardRemote);
+const { resolve: resolvePath } = createRequire(import.meta.url);
+
+const makeDefaultTestContext = async () => {
   console.time('DefaultTestContext');
-  const swingsetTestKit = await makeSwingsetTestKit(t.log);
 
-  const { runUtils, storage } = swingsetTestKit;
+  const storage = makeFakeStorageKit('bootstrapTests');
+  const swingsetTestKit = await makeCosmicSwingsetTestKit({
+    configOverrides: NonNullish(
+      await loadSwingsetConfigFile(
+        resolvePath('@agoric/vm-config/decentral-itest-vaults-config.json'),
+      ),
+    ),
+    mockBridgeReceiver: makeMockBridgeKit({ storageKit: storage }),
+  });
+
+  const { runNextBlock, runUtils } = swingsetTestKit;
+  await runNextBlock();
+
+  const readLatestEntryFromStorage = (path: string) => {
+    let data;
+    try {
+      data = unmarshalFromVstorage(storage.data, path, fromCapData, -1);
+    } catch {
+      // fall back to regular JSON
+      const raw = storage.getValues(path).at(-1);
+      assert(raw, `No data found for ${path}`);
+      data = JSON.parse(raw);
+    }
+    return data;
+  };
+
   console.timeLog('DefaultTestContext', 'swingsetTestKit');
   const { EV } = runUtils;
 
@@ -36,9 +77,7 @@ const makeDefaultTestContext = async t => {
   console.timeLog('DefaultTestContext', 'vaultFactoryKit');
 
   // has to be late enough for agoricNames data to have been published
-  const agoricNamesRemotes = makeAgoricNamesRemotesFromFakeStorage(
-    swingsetTestKit.storage,
-  );
+  const agoricNamesRemotes = makeAgoricNamesRemotesFromFakeStorage(storage);
   agoricNamesRemotes.brand.ATOM || Fail`ATOM missing from agoricNames`;
   console.timeLog('DefaultTestContext', 'agoricNamesRemotes');
 
@@ -52,7 +91,12 @@ const makeDefaultTestContext = async t => {
   console.timeEnd('DefaultTestContext');
 
   const gd = await makeGovernanceDriver(
-    swingsetTestKit,
+    // @ts-expect-error
+    {
+      ...swingsetTestKit,
+      readPublished: (subPath: string) =>
+        readLatestEntryFromStorage(`published.${subPath}`),
+    },
     agoricNamesRemotes,
     walletFactoryDriver,
     [
@@ -69,20 +113,20 @@ const test = anyTest as TestFn<
   Awaited<ReturnType<typeof makeDefaultTestContext>>
 >;
 
-test.before(async t => {
-  t.context = await makeDefaultTestContext(t);
-});
-test.after.always(t => {
-  return t.context.shutdown && t.context.shutdown();
-});
+test.before(async t => (t.context = await makeDefaultTestContext()));
+test.after.always(t => t.context.shutdown?.());
 
 test('modify manager & director params; update vats, check', async t => {
-  const { agoricNamesRemotes, evalProposal, buildProposal, gd } = t.context;
+  const {
+    agoricNamesRemotes,
+    evaluateProposal,
+    gd,
+    runUtils: { EV },
+  } = t.context;
 
   const { ATOM } = agoricNamesRemotes.brand;
   ATOM || Fail`ATOM missing from agoricNames`;
 
-  const { EV } = t.context.runUtils;
   const zoe: ZoeService = await EV.vat('bootstrap').consumeItem('zoe');
   const vaultFactoryKit =
     await EV.vat('bootstrap').consumeItem('vaultFactoryKit');
@@ -95,7 +139,6 @@ test('modify manager & director params; update vats, check', async t => {
       collateralBrand: brands.ATOM,
     });
 
-    // @ts-expect-error getGovernedParams doesn't declare these fields
     return params.DebtLimit.value.value;
   };
 
@@ -143,7 +186,7 @@ test('modify manager & director params; update vats, check', async t => {
     bundles: coreEvals.flatMap(e => e.bundles),
   };
   t.log('evaluating', coreEvals.length, 'scripts');
-  await evalProposal(combined);
+  await evaluateProposal(combined);
 
   // verify manager params restored to latest value
   t.is(await getDebtLimitValue(), 50_000_000n);
