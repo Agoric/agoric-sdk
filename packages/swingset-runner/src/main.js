06ba84adde6952ecced8b6f0087d09f9504924d2
usmanmani1122 <usman.sbf@gmail.com>
2025-06-18 12:36:07 +0500

    address @gibson042 comments

--- packages/swingset-runner/src/main.js
+++ packages/swingset-runner/src/main.js
@@ -44,50 +44,51 @@ Command line:
   runner [FLAGS...] CMD [{BASEDIR|--} [ARGS...]]
 
 FLAGS may be:
-  --resume                  - resume execution using existing saved state
-  --initonly                - initialize the swingset but exit without running it
-  --sqlite                  - runs using Sqlite3 as the data store (default)
-  --memdb                   - runs using the non-persistent in-memory data store
-  --usexs                   - run vats using the XS engine
-  --usebundlecache          - cache bundles created by swingset loader
-  --dbdir DIR               - specify where the data store should go (default BASEDIR)
-  --blockmode               - run in block mode (checkpoint every BLOCKSIZE blocks)
-  --blocksize N             - set BLOCKSIZE to N cranks (default 200)
-  --logtimes                - log block execution time stats while running
-  --logmem                  - log memory usage stats after each block
-  --logdisk                 - log disk space usage stats after each block
-  --logstats                - log kernel stats after each block
-  --logall                  - log kernel stats, block times, memory use, and disk space
-  --logtag STR              - tag for stats log file (default "runner")
-  --slog FILE               - write swingset slog to FILE
-  --teleslog                - transmit a slog feed to the local otel collector
-  --forcegc                 - run garbage collector after each block
-  --batchsize N             - set BATCHSIZE to N cranks (default 200)
-  --verbose                 - output verbose debugging messages as it runs
-  --activityhash            - print out the current activity hash after each crank
-  --audit                   - audit kernel promise reference counts after each crank
-  --dump                    - dump a kernel state store snapshot after each crank
-  --dumpdir DIR             - place kernel state dumps in directory DIR (default ".")
-  --dumptag STR             - prefix kernel state dump filenames with STR (default "t")
-  --raw                     - perform kernel state dumps in raw mode
-  --stats                   - print a performance stats report at the end of a run
-  --statsfile FILE          - output performance stats to FILE as a JSON object
-  --benchmark N             - perform an N round benchmark after the initial run
-  --sbench FILE             - run a whole-swingset benchmark with the driver vat in FILE
-  --chain                   - emulate the behavior of the Cosmos-based chain
-  --indirect                - launch swingset from a vat instead of launching directly
-  --config FILE             - read swingset config from FILE instead of inferring it
-  --cpu-profile-output      - specify where the cpu profile will be dumped (generated using engine's native inspector)
-  --heap-profile-output     - specify where the heap profile will be dumped (generated using engine's native inspector sampler)
-  --heap-sampling-interval  - override the sampling heap inteval (default value is 1024)
+  --resume                    - resume execution using existing saved state
+  --initonly                  - initialize the swingset but exit without running it
+  --sqlite                    - runs using Sqlite3 as the data store (default)
+  --memdb                     - runs using the non-persistent in-memory data store
+  --usexs                     - run vats using the XS engine
+  --usebundlecache            - cache bundles created by swingset loader
+  --dbdir DIR                 - specify where the data store should go (default BASEDIR)
+  --blockmode                 - run in block mode (checkpoint every BLOCKSIZE blocks)
+  --blocksize N               - set BLOCKSIZE to N cranks (default 200)
+  --logtimes                  - log block execution time stats while running
+  --logmem                    - log memory usage stats after each block
+  --logdisk                   - log disk space usage stats after each block
+  --logstats                  - log kernel stats after each block
+  --logall                    - log kernel stats, block times, memory use, and disk space
+  --logtag STR                - tag for stats log file (default "runner")
+  --slog FILE                 - write swingset slog to FILE
+  --teleslog                  - transmit a slog feed to the local otel collector
+  --forcegc                   - run garbage collector after each block
+  --batchsize N               - set BATCHSIZE to N cranks (default 200)
+  --verbose                   - output verbose debugging messages as it runs
+  --activityhash              - print out the current activity hash after each crank
+  --audit                     - audit kernel promise reference counts after each crank
+  --dump                      - dump a kernel state store snapshot after each crank
+  --dumpdir DIR               - place kernel state dumps in directory DIR (default ".")
+  --dumptag STR               - prefix kernel state dump filenames with STR (default "t")
+  --raw                       - perform kernel state dumps in raw mode
+  --stats                     - print a performance stats report at the end of a run
+  --statsfile FILE            - output performance stats to FILE as a JSON object
+  --benchmark N               - perform an N round benchmark after the initial run
+  --sbench FILE               - run a whole-swingset benchmark with the driver vat in FILE
+  --chain                     - emulate the behavior of the Cosmos-based chain
+  --indirect                  - launch swingset from a vat instead of launching directly
+  --config FILE               - read swingset config from FILE instead of inferring it
+  --cpu-profile-output FILE   - write an implementation-specific CPU profile to FILE
+  --heap-profile-output FILE  - write an implementation-specific heap profile to FILE
+  --heap-sampling-interval N  - set heap profiling sampling interval to N units (for Node.js, bytes)
+                                (default 1024)
 
 CMD is one of:
-  help                      - print this helpful usage information
-  run                       - launches or resumes the configured vats, which run to completion.
-  batch                     - launch or resume, then run BATCHSIZE cranks or until completion
-  step                      - steps the configured swingset one crank.
-  shell                     - starts a simple CLI allowing the swingset to be run or stepped or
-                              interrogated interactively.
+  help                        - print this helpful usage information
+  run                         - launches or resumes the configured vats, which run to completion.
+  batch                       - launch or resume, then run BATCHSIZE cranks or until completion
+  step                        - steps the configured swingset one crank.
+  shell                       - starts a simple CLI allowing the swingset to be run or stepped or
+                                interrogated interactively.
 
 BASEDIR is the base directory for locating the swingset's vat definitions and
   for storing the swingset's state.  If BASEDIR is omitted or '--' it defaults
@@ -140,14 +141,16 @@ function generateSwingsetBenchmarkConfig(
       `you can't have a vat named benchmarkBootstrap in a benchmark swingset`,
     );
   }
-  if (baseConfig.vats.benchmarkDriver) {
+  if (baseConfig.vats.benchmarkDriver)
     fail(`you can't have a vat named benchmarkDriver in a benchmark swingset`);
-  }
-  // eslint-disable-next-line prefer-const
-  let { benchmarkDriver, ...baseConfigOptions } = baseConfig;
-  if (!benchmarkDriver) {
-    benchmarkDriver = { sourceSpec: swingsetBenchmarkDriverPath };
-  }
+
+  const {
+    benchmarkDriver = { sourceSpec: swingsetBenchmarkDriverPath },
+    bootstrap: baseBootstrap,
+    vats: baseVats,
+    ...baseConfigOptions
+  } = baseConfig;
+
   const config = {
     ...baseConfigOptions,
     bootstrap: 'benchmarkBootstrap',
@@ -156,27 +159,30 @@ function generateSwingsetBenchmarkConfig(
       benchmarkBootstrap: {
         sourceSpec: new URL('vat-benchmarkBootstrap.js', import.meta.url)
           .pathname,
-        parameters: { config: { bootstrap: baseConfig.bootstrap } },
+        parameters: { config: { bootstrap: baseBootstrap } },
       },
       benchmarkDriver,
-      ...baseConfig.vats,
+      ...baseVats,
     },
   };
-  if (!config.vats[baseConfig.bootstrap].parameters) {
-    config.vats[baseConfig.bootstrap].parameters = {};
-  }
-  config.vats[baseConfig.bootstrap].parameters.argv = bootstrapArgv;
+
+  if (!config.vats[baseBootstrap].parameters)
+    config.vats[baseBootstrap].parameters = {};
+
+  config.vats[baseBootstrap].parameters.argv = bootstrapArgv;
   return config;
 }
 
 function generateIndirectConfig(baseConfig) {
+  const launcherConfig = { bootstrap: baseConfig.bootstrap, vats: {} };
+
   const config = {
     bootstrap: 'launcher',
     bundles: {},
     vats: {
       launcher: {
         sourceSpec: new URL('vat-launcher.js', import.meta.url).pathname,
-        parameters: { config: { bootstrap: baseConfig.bootstrap, vats: {} } },
+        parameters: { config: launcherConfig },
       },
     },
   };
@@ -200,7 +206,7 @@ function generateIndirectConfig(baseConfig) {
         fail(`this can't happen`);
       }
       baseVat.bundleName = newBundleName;
-      config.vats.launcher.parameters.config.vats[vatName] = baseVat;
+      launcherConfig.vats[vatName] = baseVat;
     }
   }
   if (baseConfig.bundles) {
