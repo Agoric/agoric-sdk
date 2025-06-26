13a7b129ee76a9968a8d2f4fde9a6182227db891
Michael FIG <mfig@agoric.com>
2025-06-26 17:45:32 -0600

    test(localchain): update in a3p's `z:acceptance`

--- packages/vats/src/proposals/localchain-test.js
+++ packages/vats/src/proposals/localchain-test.js
@@ -1,4 +1,5 @@
 // @ts-check
+import { Far } from '@endo/far';
 import { heapVowE as E } from '@agoric/vow/vat.js';
 import { typedJson } from '@agoric/cosmic-proto';
 
@@ -12,7 +13,7 @@ import { typedJson } from '@agoric/cosmic-proto';
  * @param {{ testResultPath: string }} options.options
  */
 export const testLocalChain = async (
-  { consume: { chainStorage, localchain } },
+  { consume: { board, chainStorage, localchain } },
   { options: { testResultPath } },
 ) => {
   console.warn('=== localchain test started (result in', testResultPath, ')!');
@@ -23,24 +24,35 @@ export const testLocalChain = async (
     throw Error('no chainStorage');
   }
 
+  for (const nodeName of testResultPath.split('.')) {
+    node = E(node).makeChildNode(nodeName);
+  }
+
+  /** @type {PromiseSettledResult<any>} */
   let result;
   try {
-    const lca = await E(localchain).makeAccount();
-    console.info('created account', lca);
-    const address = await E(lca).getAddress();
-    console.info('address', address);
+    const lcaSender = await E(localchain).makeAccount();
+    const lcaReceiver = await E(localchain).makeAccount();
+    const senderAddress = await E(lcaSender).getAddress();
+    const receiverAddress = await E(lcaReceiver).getAddress();
+    console.info('created accounts', {
+      lcaSender,
+      lcaReceiver,
+      senderAddress,
+      receiverAddress,
+    });
 
     const queryMsg = typedJson('/cosmos.bank.v1beta1.QueryAllBalancesRequest', {
-      address,
+      address: receiverAddress,
     });
     const balances = await E(localchain).query(queryMsg);
     console.info('balances', balances);
 
-    await E(lca)
+    await E(lcaReceiver)
       .executeTx([
         typedJson('/cosmos.bank.v1beta1.MsgSend', {
-          fromAddress: address,
-          toAddress: address,
+          fromAddress: receiverAddress,
+          toAddress: receiverAddress,
           amount: [{ denom: 'ucosm', amount: '1' }],
         }),
       ])
@@ -61,16 +73,58 @@ export const testLocalChain = async (
       throw Error('emptyQuery results should be empty');
     }
 
-    result = { success: true };
+    const tap = Far(
+      'Localchain Tap',
+      /** @type {import('../bridge-target.js').TargetApp} */ ({
+        async receiveUpcall(obj) {
+          console.info('=== localchain test tap received upcall', obj);
+          await E(E(node).makeChildNode('tap')).setValue(JSON.stringify(obj));
+        },
+      }),
+    );
+    await E(lcaReceiver).monitorTransfers(tap);
+
+    const testMore = Far('Localchain Test More', {
+      async sendTo(toAddress, amount) {
+        /** @type {PromiseSettledResult<any>} */
+        let sendToResult;
+        await null;
+        try {
+          console.info('=== localchain test sendTo called');
+          const execResult = await E(lcaSender).executeTx([
+            typedJson('/cosmos.bank.v1beta1.MsgSend', {
+              fromAddress: senderAddress,
+              toAddress,
+              amount,
+            }),
+          ]);
+          sendToResult = {
+            status: 'fulfilled',
+            value: { toAddress, amount, execResult },
+          };
+        } catch (e) {
+          console.error('=== localchain test sendTo failed', e);
+          sendToResult = {
+            status: 'rejected',
+            reason: { toAddress, amount, error: String(e) },
+          };
+        }
+        console.info('== localchain sendTo result', sendToResult);
+        await E(node).setValue(JSON.stringify(sendToResult));
+      },
+    });
+    const testMoreBoardId = await E(board).getId(testMore);
+
+    result = {
+      status: 'fulfilled',
+      value: { receiverAddress, senderAddress, testMoreBoardId },
+    };
   } catch (e) {
     console.error('test failed with', e);
-    result = { success: false, error: String(e) };
+    result = { status: 'rejected', reason: String(e) };
   }
 
   console.warn('=== localchain test done, setting', { result });
-  for (const nodeName of testResultPath.split('.')) {
-    node = E(node).makeChildNode(nodeName);
-  }
   await E(node).setValue(JSON.stringify(result));
 };
 
@@ -78,8 +132,12 @@ export const getManifestForLocalChainTest = (_powers, { testResultPath }) => ({
   manifest: {
     [testLocalChain.name]: {
       consume: {
+        board: 'vats',
         chainStorage: 'bridge',
         localchain: 'localchain',
+      },
+      produce: {
+        testLocalChain: 'localchain-test',
       },
     },
   },
