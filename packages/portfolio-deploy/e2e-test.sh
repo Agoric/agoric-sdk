f4293f87acd6c37502c24c6f98034edfb3010e32
Fraz Arshad <fraz.arshad12@gmail.com>
2025-07-02 10:24:02 +0200

    fixup! test: e2e tests for axelar

--- packages/portfolio-deploy/e2e-test.sh
+++ packages/portfolio-deploy/e2e-test.sh
@@ -4,13 +4,13 @@ CHAIN1_ID="agoriclocal"
 CHAIN2_ID="noble-1"
 CONFIG_FILE=$HOME/.hermes/config.toml
 
-cd $DIR/test/e2e/agoric-to-axelar-local && npm i 
-cd $DIR/test/e2e/agoric-to-axelar-local/packages/axelar-local-dev &&  npm run build
-cd $DIR/test/e2e/agoric-to-axelar-local/packages/axelar-local-dev-cosmos &&  npm run build
+# cd $DIR/test/e2e/agoric-to-axelar-local && npm i 
+# cd $DIR/test/e2e/agoric-to-axelar-local/packages/axelar-local-dev &&  npm run build
+# cd $DIR/test/e2e/agoric-to-axelar-local/packages/axelar-local-dev-cosmos &&  npm run build
 
-cd $DIR/test/e2e/agoric-to-axelar-local/packages/axelar-local-dev-cosmos && npm run start && \
+# cd $DIR/test/e2e/agoric-to-axelar-local/packages/axelar-local-dev-cosmos && npm run start && \
 
-cd $DIR && yarn build && scripts/deploy-cli.ts src/chain-info.build.js --net=local --from=gov1 -- net=local peer=axelar:connection-0:channel-0:uaxl && \
-cd $DIR && yarn build && scripts/deploy-cli.ts src/portfolio.build.js --net=local --from=gov1  && \
-sleep 15 && \
+# cd $DIR && yarn build && scripts/deploy-cli.ts src/chain-info.build.js --net=local --from=gov1 -- net=local peer=axelar:connection-0:channel-0:uaxl && \
+# cd $DIR && yarn build && scripts/deploy-cli.ts src/portfolio.build.js --net=local --from=gov1  && \
+# sleep 15 && \
 cd $DIR && yarn e2e:relay
