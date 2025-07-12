c217539406ff9f5e47c6cc9fc7eec86da086bdd7
Dan Connolly <connolly@agoric.com>
2025-07-11 23:25:01 -0500

    WIP moar pruning

--- packages/portfolio-contract/src/pos-usdn-shapes.ts
+++
@@ -1,7 +0,0 @@
-import type { TypedPattern } from '@agoric/internal';
-
-// #region Proposal Shapes
-
-export const makeUSDNGiveFields = (
-  usdcAmountShape: TypedPattern<Brand<'nat'>>,
-) => ({ USDN: usdcAmountShape, NobleFees: usdcAmountShape });
