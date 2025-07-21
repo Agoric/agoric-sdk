b7773d864fd4018450d1d61e6c2a02e0896dca24
Dan Connolly <connolly@agoric.com>
2025-07-21 15:50:08 -0500

    test(a3p-integration): refine logging in ymax test

--- a3p-integration/proposals/g:resolve-usdc/test/ymax.test.js
+++ a3p-integration/proposals/g:resolve-usdc/test/ymax.test.js
@@ -1,28 +1,35 @@
+// @ts-check
 import test from 'ava';
 import '@endo/init/debug.js';
 import { queryVstorage } from '@agoric/synthetic-chain';
 
+const getCapDataStructure = cell => {
+  const { body, slots } = JSON.parse(cell);
+  const structure = JSON.parse(body.replace(/^#/, ''));
+  return { structure, slots };
+};
+
+const getCellValues = ({ value }) => {
+  return JSON.parse(value).values;
+};
+
 test('ymax is deployed and appears in vstorage instance', async t => {
-  // Check if ymax is in vstorage
   const instancePath = 'published.agoricNames.instance';
   const instanceRaw = await queryVstorage(instancePath);
-  const instance = JSON.parse(instanceRaw.value);
+  const instance = Object.fromEntries(
+    getCapDataStructure(getCellValues(instanceRaw).at(-1)).structure,
+  );
 
   const chainInfoPath = 'published.agoricNames.chain.axelar';
   const chainInfoRaw = await queryVstorage(chainInfoPath);
-  const chainInfo = JSON.parse(chainInfoRaw.value);
+  const chainInfo = getCapDataStructure(
+    getCellValues(chainInfoRaw).at(-1),
+  ).structure;
 
-  t.log(instancePath, instance);
+  t.log(instancePath, Object.keys(instance).join(', '));
   t.log(chainInfoPath, chainInfo);
 
-  t.truthy(
-    instance.values.some(v => {
-      const parsedV = JSON.parse(v);
-      const body = JSON.parse(parsedV.body.replace(/^#/, ''));
-      return body.some(entry => entry[0] === 'ymax0');
-    }),
-    'ymax installation should exist in vstorage',
-  );
+  t.truthy('ymax0' in instance, 'ymax installation should exist in vstorage');
 
   t.truthy(chainInfo, 'axelar chain info should exist in vstorage');
 });
