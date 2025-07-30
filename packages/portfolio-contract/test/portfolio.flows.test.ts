022466a545973f3a5e9b5e270722f6316de98c3e
Dan Connolly <connolly@agoric.com>
2025-07-30 16:50:58 -0500

    test(portfolio-contract): refactor evmNamingDistinction
    to clarify contract tests

--- packages/portfolio-contract/test/portfolio.flows.test.ts
+++ packages/portfolio-contract/test/portfolio.flows.test.ts
@@ -28,11 +28,7 @@ import buildZoeManualTimer from '@agoric/zoe/tools/manualTimer.js';
 import { makeHeapZone } from '@agoric/zone';
 import { Far, passStyleOf } from '@endo/pass-style';
 import { makePromiseKit } from '@endo/promise-kit';
-import {
-  AxelarChain,
-  RebalanceStrategy,
-  YieldProtocol,
-} from '../src/constants.js';
+import { RebalanceStrategy, YieldProtocol } from '../src/constants.js';
 import {
   preparePortfolioKit,
   type PortfolioKit,
@@ -53,33 +49,22 @@ import {
   type OfferArgsFor,
 } from '../src/type-guards-steps.ts';
 import { makeProposalShapes, type ProposalType } from '../src/type-guards.ts';
-import { axelarIdsMock, contractsMock, gmpAddresses } from './mocks.ts';
 import { makePortfolioSteps } from '../tools/portfolio-actors.ts';
+import { decodeFunctionCall } from './abi-utils.ts';
+import {
+  axelarIdsMock,
+  contractsMock,
+  evmNamingDistinction,
+  gmpAddresses,
+} from './mocks.ts';
 import {
   axelarCCTPConfig,
   makeIncomingEVMEvent,
   makeIncomingVTransferEvent,
 } from './supports.ts';
-import { decodeFunctionCall } from './abi-utils.ts';
-import type { GmpAddresses } from '../src/portfolio.contract.ts';
 
-/**
- * Use Arbitrum or any other EVM chain whose Axelar chain ID (`axelarId`) differs
- * from the chain name. For example, Arbitrum's `axelarId` is "arbitrum", while
- * Ethereum’s is "Ethereum" (case-sensitive). The challenge is that if a mismatch
- * occurs, it may go undetected since the `axelarId` is passed via the IBC memo
- * and not validated automatically.
- *
- * To ensure proper testing, it's best to use a chain where the `chainName` and
- * `axelarId` are not identical. This increases the likelihood of catching issues
- * with misconfigured or incorrectly passed `axelarId` values.
- *
- * To see the `axelarId` for a given chain, refer to:
- * @see {@link https://github.com/axelarnetwork/axelarjs-sdk/blob/f84c8a21ad9685091002e24cac7001ed1cdac774/src/chains/supported-chains-list.ts | supported-chains-list.ts}
- */
-const destinationEVMChain: AxelarChain = 'Arbitrum';
-// Must be axelarId of destinationEVMChain
-const sourceChain = 'arbitrum';
+// Use an EVM chain whose axelar ID differs from its chain name
+const { sourceChain } = evmNamingDistinction;
 
 const theExit = harden(() => {}); // for ava comparison
 // @ts-expect-error mock
