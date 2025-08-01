be5aa127ccf40a811edb1e6cfc4070b012920ad0
Dan Connolly <connolly@agoric.com>
2025-08-01 12:21:45 -0500

    test: test for new boardId (and update docs)

--- a3p-integration/proposals/g:3-ymax-alpha2/test/ymax.test.js
+++ a3p-integration/proposals/g:3-ymax-alpha2/test/ymax.test.js
@@ -2,37 +2,24 @@
 import test from 'ava';
 import '@endo/init/debug.js';
 import { queryVstorage } from '@agoric/synthetic-chain';
+import { boardSlottingMarshaller } from '@agoric/client-utils';
 
-const getCapDataStructure = cell => {
-  const { body, slots } = JSON.parse(cell);
-  const structure = JSON.parse(body.replace(/^#/, ''));
-  return { structure, slots };
-};
+const oldBoardId = 'board013515'; // from mainnet proposal 100
 
 const getCellValues = ({ value }) => {
   return JSON.parse(value).values;
 };
 
-test('ymax deployed incompletely', async t => {
+test('ymax is in vstorage instance with a new boardId', async t => {
   const instancePath = 'published.agoricNames.instance';
   const instanceRaw = await queryVstorage(instancePath);
-  const instance = Object.fromEntries(
-    getCapDataStructure(getCellValues(instanceRaw).at(-1)).structure,
-  );
+  const capData = getCellValues(instanceRaw).at(-1);
 
-  const chainInfoPath = 'published.agoricNames.chain.axelar';
-  const chainInfoRaw = await queryVstorage(chainInfoPath);
-  const chainInfo = getCapDataStructure(
-    getCellValues(chainInfoRaw).at(-1),
-  ).structure;
+  const m = boardSlottingMarshaller();
+  const instance = Object.fromEntries(m.fromCapData(capData));
 
-  t.log(instancePath, Object.keys(instance).join(', '));
-  t.log(chainInfoPath, chainInfo);
-
-  t.falsy(
-    'ymax0' in instance,
-    'ymax installation does not yet exist in vstorage',
-  );
-
-  t.truthy(chainInfo, 'axelar chain info should exist in vstorage');
+  const { slots } = m.toCapData(instance.ymax0);
+  const [newBoardId] = slots;
+  t.log('old, new boarId', oldBoardId, newBoardId);
+  t.not(newBoardId, oldBoardId);
 });
