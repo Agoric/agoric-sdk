983566002ab943ecc57ddc2048489bcb45a1f0a0
Richard Gibson <richard.gibson@gmail.com>
2025-08-01 15:44:24 -0400

    fixup! test: test for new boardId (and update docs)

--- a3p-integration/proposals/g:3-ymax-alpha2/test/ymax.test.js
+++ a3p-integration/proposals/g:3-ymax-alpha2/test/ymax.test.js
@@ -15,7 +15,7 @@ const getCellValues = ({ value }) => {
 test('ymax is in vstorage instance with a new boardId', async t => {
   const instancePath = 'published.agoricNames.instance';
   const instanceRaw = await queryVstorage(instancePath);
-  const capData = getCellValues(instanceRaw).at(-1);
+  const capData = JSON.parse(getCellValues(instanceRaw).at(-1));
 
   const m = boardSlottingMarshaller((slot, iface) => {
     return Far('SlotReference', {
