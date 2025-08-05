bb79f8ee3146c0e6b3e86cc6a8b68365eac795bb
Richard Gibson <richard.gibson@gmail.com>
2025-08-05 14:16:20 -0700

    chore(portfolio-planner): Tweak console output

--- packages/portfolio-planner/src/engine.ts
+++ packages/portfolio-planner/src/engine.ts
@@ -8,17 +8,25 @@ export const startEngine = async ({ agd, rpc }: {
   rpc: CosmosRPCClient;
 }) => {
   await null;
-  console.log('Appd status:', (await agd.exec([`status`])).stdout);
+  let status = await (await agd.exec([`status`])).stdout;
+  try {
+    const statusObj = JSON.parse(status);
+    if (JSON.stringify(statusObj) === status.trim()) status = statusObj;
+    else console.log({ status, rt: JSON.stringify(statusObj) });
+  } catch (err) {
+    console.error(err);
+  }
+  console.warn('agd status', status);
 
   // TODO: This is a more expected way to use the RPC client.
   await rpc.opened();
   try {
-    // console.log('RPC client opened:', rpc);
+    // console.warn('RPC client opened:', rpc);
     const sub = rpc.subscribeAll([
       `tm.event = 'NewBlockHeader'`, // All BEGIN_BLOCK, END_BLOCK activity
       // `tm.event = 'Tx'`, // All transactions within the block
     ]);
-    // console.log('consuming events');
+    // console.warn('consuming events');
     for await (const response of sub) {
       console.log(
         'Received subscription response:',
@@ -26,6 +34,7 @@ export const startEngine = async ({ agd, rpc }: {
       );
     }
   } finally {
+    console.warn('Terminating...');
     rpc.close();
   }
 };
