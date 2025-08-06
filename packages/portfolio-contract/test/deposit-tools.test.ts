e06b4582ed7a7c6ca6bcf998c208ff6dceef89bc
Dan Connolly <dckc@madmode.com>
2025-08-05 17:01:14 -0700

    test: add deposit tools test file for portfolio contract

---
+++ packages/portfolio-contract/test/deposit-tools.test.ts
@@ -0,0 +1,6 @@
+/** @file test for deposit tools */
+import { test } from '@agoric/zoe/tools/prepare-test-env-ava.js';
+
+test('planDepositTransfers works in a handful of cases', t => {
+  // write this test AI!
+});
