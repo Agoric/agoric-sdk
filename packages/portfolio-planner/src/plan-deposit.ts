eca002b2dffa53919c922c38cfc82b1d08011c67
Dan Connolly <dckc@madmode.com>
2025-08-05 21:31:39 -0700

    feat: handleDeposit (WIP NEEDSTEST)

---
+++ packages/portfolio-planner/src/plan-deposit.ts
@@ -0,0 +1,104 @@
+import {
+  PoolPlaces,
+  type PoolKey,
+} from '@aglocal/portfolio-contract/src/type-guards.ts';
+import {
+  makePortfolioQuery,
+  planDepositTransfers,
+  planTransfer,
+} from '@aglocal/portfolio-contract/tools/portfolio-actors.ts';
+import { VstorageKit } from '@agoric/client-utils';
+import { AmountMath } from '@agoric/ertp/src/amountMath.js';
+import type { Brand, NatAmount } from '@agoric/ertp/src/types.ts';
+import { assert } from '@endo/errors';
+import { X, q } from '@endo/errors';
+import type { CosmosRestClient } from './cosmos-rest-client.ts';
+import type { Chain, Pool, SpectrumClient } from './spectrum-client.ts';
+
+// XXX add to PoolPlaces info?
+const USDNconfig = {
+  denom: 'usdn',
+  chainName: 'noble',
+};
+
+const addressOfAccountId = (aid: `${string}:${string}:${string}`) =>
+  aid.split(':', 3)[2];
+const { make } = AmountMath;
+
+export const getCurrentBalances = async (
+  portfolioKey: `${string}.portfolios.portfolio${number}`,
+  readPublished: VstorageKit['readPublished'],
+  spectrum: SpectrumClient,
+  cosmosRest: CosmosRestClient,
+  brand: Brand<'nat'>,
+) => {
+  const pq = makePortfolioQuery(portfolioKey, readPublished);
+  const status = await pq.getPortfolioStatus();
+  const balances: Partial<Record<PoolKey, NatAmount>> = {};
+
+  for (const posKey of status.positionKeys) {
+    if (!(posKey in PoolPlaces)) continue; // XXX warn?
+    const info = PoolPlaces[posKey as PoolKey];
+    switch (info.protocol) {
+      case 'USDN': {
+        const { chainName, denom } = USDNconfig;
+        const addr = addressOfAccountId(status.accountIdByChain[chainName]);
+        const { amount } = await cosmosRest.getAccountBalance(
+          USDNconfig.chainName,
+          addr,
+          denom,
+        );
+        balances[posKey] = make(brand, amount);
+        break;
+      }
+      case 'Aave':
+      case 'Compound': {
+        const { chainName } = info;
+        const pool = info.protocol.toLocaleLowerCase() as Pool;
+        const chain = chainName.toLocaleLowerCase() as Chain;
+        const addr = addressOfAccountId(status.accountIdByChain[chainName]);
+        const { balance } = await spectrum.getPoolBalance(chain, pool, addr);
+        assert(Number.isSafeInteger(balance.supplyBalance));
+        balances[posKey] = make(brand, BigInt(balance.supplyBalance));
+        break;
+      }
+      default:
+        // TODO: Beefy
+        assert.fail(X`unknown protocol: ${q(posKey)}`);
+    }
+  }
+  return harden(balances);
+};
+
+export const handleDeposit = async (
+  amount: NatAmount,
+  portfolioKey: `${string}.portfolios.portfolio${number}`,
+  readPublished: VstorageKit['readPublished'],
+  spectrum: SpectrumClient,
+  cosmosRest: CosmosRestClient,
+) => {
+  const currentBalances = await getCurrentBalances(
+    portfolioKey,
+    readPublished,
+    spectrum,
+    cosmosRest,
+    amount.brand,
+  );
+  const pq = makePortfolioQuery(portfolioKey, readPublished);
+  const { targetAllocation } = await pq.getPortfolioStatus();
+  if (!targetAllocation) {
+    // XXX warn?
+    return;
+  }
+  const txfrs = planDepositTransfers(amount, currentBalances, targetAllocation);
+  const steps = [
+    { src: '<Deposit>', dest: '@agoric', amount },
+    { src: '@agoric', dest: '@noble', amount },
+    ...Object.entries(txfrs)
+      // XXX Object.entries() type is goofy
+      .map(([dest, amt]) => planTransfer(dest as PoolKey, amt))
+      .flat(),
+  ];
+  console.log('TODO: submit steps', steps);
+  throw Error('moar TODO');
+};
