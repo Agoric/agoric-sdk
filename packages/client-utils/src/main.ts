ee635edfe86834802276affe1c2c13d17fcc3fd8
Turadg Aleahmad <turadg@agoric.com>
2025-08-18 12:42:57 -0700

    chore!: drop types-index export
    incompatible with `dist` build

--- packages/client-utils/src/main.ts
+++ packages/client-utils/src/main.ts
@@ -1,6 +1,4 @@
-import { VBankAccount } from '@agoric/internal/src/config.js';
-
-export { VBankAccount };
+export { VBankAccount } from '@agoric/internal/src/config.js';
 export * from './cli.js';
 export * from './clients.js';
 export * from './marshalTables.js';
@@ -12,5 +10,5 @@ export * from './vstorage-kit.js';
 export * from './vstorage.js';
 export * from './wallet-utils.js';
 
-// eslint-disable-next-line import/export -- just types
-export * from './types-index.js';
+// Does not use types-index because this is built to a dist folder
+export * from './types.js';
