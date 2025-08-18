72c0280a4c006ba166bc8963c3c9f615648f04d6
Dan Connolly <connolly@agoric.com>
2025-08-18 15:15:43 -0500

    chore: core eval to produce getDepositFacet

---
+++ packages/portfolio-deploy/scripts/attenuated-deposit.core.js
@@ -0,0 +1,47 @@
+/**
+ * @file Get a promise for a deposit facet without
+ * access to all of namesByAddressAdmin.
+ *
+ * script version of ../src/attenuated-deposit.core.js
+ * for ad-hoc deploy on devnet
+ */
+
+const trace = (...args) => console.log('---- AttD', ...args);
+
+/**
+ * @import {DepositFacet} from '@agoric/ertp';
+ */
+const depositFacetKey = 'depositFacet';
+
+/**
+ * XXX move this into BootstrapPowers
+ * @typedef {PromiseSpaceOf<{
+ *   getDepositFacet: (addr: string, debugName?: string) => Promise<DepositFacet>
+ * }>} AttenuatedDepositPowers
+ */
+
+/**
+ * @param {BootstrapPowers & AttenuatedDepositPowers} powers
+ */
+const produceAttenuatedDeposit = powers => {
+  const { namesByAddress, namesByAddressAdmin } = powers.consume;
+  /**
+   *
+   * @param {string} addr
+   * @param {string} [debugName]
+   */
+  const getDepositFacet = async (addr, debugName = 'party') => {
+    trace('reserve nameHub for', debugName, addr);
+    await E(namesByAddressAdmin).reserve(addr);
+    trace('lookup depositFacet for', debugName, addr);
+    const df = await E(namesByAddress).lookup(addr, depositFacetKey);
+    trace('got depositFacet for', debugName, addr);
+    return df;
+  };
+  harden(getDepositFacet);
+
+  powers.produce.getDepositFacet.resolve(getDepositFacet);
+};
+
+// "export" it
+produceAttenuatedDeposit;
