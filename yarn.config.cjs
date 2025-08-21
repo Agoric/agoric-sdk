700602783142ba81d4f746b300cd644607997e18
Turadg Aleahmad <turadg@agoric.com>
2025-08-21 15:03:17 -0700

    ci: enforce workspace: protocol

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -4,6 +4,20 @@ const { defineConfig } = require('@yarnpkg/types');
 module.exports = defineConfig({
   async constraints({ Yarn }) {
     for (const workspace of Yarn.workspaces()) {
+      for (const version of workspace.pkg.dependencies.values()) {
+        if (
+          version.ident.startsWith('@aglocal/') ||
+          version.ident.startsWith('@agoric/') ||
+          version.ident == 'agoric'
+        ) {
+          // If the dependency is not a workspace and is not the special case '@agoric/wallet-ui',
+          // then it's an error because all Agoric dependencies must be resolved via the workspace protocol.
+          if (!version.workspace && version.ident != '@agoric/wallet-ui')
+            workspace.error(
+              'Agoric dependencies must resolve by the workspace: protocol',
+            );
+        }
+      }
       if (workspace.cwd.startsWith('packages/')) {
         workspace.set('engines.node', '^20.9 || ^22.11');
         if (workspace.ident?.startsWith('@aglocal/')) {
