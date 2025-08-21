72c4b4cd823fa91bf66b4a05bb6ca5b4ed0d5139
Turadg Aleahmad <turadg@agoric.com>
2025-08-21 14:36:37 -0700

    build: enforce package privacy

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -1,11 +1,19 @@
 // see https://yarnpkg.com/features/constraints
+const { defineConfig } = require('@yarnpkg/types');
 
-module.exports = {
+module.exports = defineConfig({
   async constraints({ Yarn }) {
     for (const workspace of Yarn.workspaces()) {
       if (workspace.cwd.startsWith('packages/')) {
         workspace.set('engines.node', '^20.9 || ^22.11');
+        if (workspace.ident?.startsWith('@aglocal/')) {
+          workspace.set('private', true);
+        } else {
+          workspace.unset('private');
+        }
+      } else if (workspace.cwd.startsWith('services/')) {
+        workspace.set('private', true);
       }
     }
   },
-};
+});
