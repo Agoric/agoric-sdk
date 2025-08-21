2680bf931eda4ba01ee1f46cbdbbc85be5c936ee
Turadg Aleahmad <turadg@agoric.com>
2025-08-21 14:51:26 -0700

    build: enforce package naming

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -8,10 +8,22 @@ module.exports = defineConfig({
         workspace.set('engines.node', '^20.9 || ^22.11');
         if (workspace.ident?.startsWith('@aglocal/')) {
           workspace.set('private', true);
-        } else {
+        } else if (
+          workspace.ident?.startsWith('@agoric/') ||
+          workspace.ident == 'agoric'
+        ) {
           workspace.unset('private');
+        } else {
+          workspace.error(
+            'Workspaces in the packages directory must be under the @agoric or @aglocal scope',
+          );
         }
       } else if (workspace.cwd.startsWith('services/')) {
+        if (!workspace.cwd.endsWith('-service')) {
+          workspace.error(
+            'Workspaces in the services directory must end with "-service"',
+          );
+        }
         workspace.set('private', true);
       }
     }
