6307e5fd8b76fa389190a83111f39b2b8d375f24
Mathieu Hofman <mathieu@agoric.com>
2025-08-21 00:34:00 +0000

    fix: handle agd status sync_info

--- a3p-integration/proposals/z:acceptance/setup-test.sh
+++ a3p-integration/proposals/z:acceptance/setup-test.sh
@@ -12,8 +12,8 @@ CHAIN_ID="$(jq --raw-output '.chain_id' < "$AGORIC_HOME/config/genesis.json")"
 IP_ADDRESS="$(hostname --ip-address)"
 NODE_ID="$(agd tendermint show-node-id)"
 
-LATEST_BLOCK_HEIGHT="$(jq --raw-output '.SyncInfo.latest_block_height' < "$STATUS_FILE")"
-LATEST_BLOCK_HASH="$(jq --raw-output '.SyncInfo.latest_block_hash' < "$STATUS_FILE")"
+LATEST_BLOCK_HEIGHT="$(jq -e --raw-output '.sync_info // .SyncInfo | .latest_block_height' < "$STATUS_FILE")"
+LATEST_BLOCK_HASH="$(jq -e --raw-output '.sync_info // .SyncInfo | .latest_block_hash' < "$STATUS_FILE")"
 
 echo -n "{\"chainName\": \"$CHAIN_ID\", \"rpcAddrs\": [\"$IP_ADDRESS:26657\"], \"gci\": \"$IP_ADDRESS:26657/genesis\", \"peers\":[\"$NODE_ID@$IP_ADDRESS:26656\"], \"seeds\":[], \"trustedBlockInfo\": {\"height\": \"$LATEST_BLOCK_HEIGHT\", \"hash\": \"$LATEST_BLOCK_HASH\"}}" > "$MESSAGE_FILE_PATH"
 
