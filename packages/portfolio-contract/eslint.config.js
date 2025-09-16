cd8a6cd05c4206899a025bc2d9827255b942a18e
Dan Connolly <connolly@agoric.com>
2025-09-16 13:43:53 -0500

    style: move vendor/ exclusion to top level
    package-local eslint.config.js overrides normal rules

--- packages/portfolio-contract/eslint.config.js
+++
@@ -1,9 +0,0 @@
-// packages/portfolio-contract/eslint.config.js
-export default [
-  {
-    ignores: [
-      'src/vendor/viem/viem-abi.bundle.js', // ignore this specific file
-    ],
-  },
-  // You may already have additional config objects here
-];
