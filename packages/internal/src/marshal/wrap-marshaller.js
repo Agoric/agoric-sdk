eda6ab68a7deea305a3b3acb7728d8f683697fc8
Mathieu Hofman <mathieu@agoric.com>
2025-10-10 07:20:31 +0000

    feat(internal): stub wrapRemoteMarshaller

--- packages/internal/src/marshal/wrap-marshaller.js
+++ packages/internal/src/marshal/wrap-marshaller.js
@@ -1,9 +1,12 @@
 // @ts-check
+import { E } from '@endo/eventual-send';
+import { Far } from '@endo/far';
 
 /**
  * @import {EOnly} from '@endo/eventual-send';
  * @import {Simplify} from '@endo/pass-style';
- * @import {Marshal} from '@endo/marshal';
+ * @import {CapData, Passable, Marshal} from '@endo/marshal';
+ * @import {ERemote} from '../types.js';
  */
 
 /**
@@ -14,4 +17,34 @@
  * @typedef {Simplify<EOnly<Marshal<Slot>>>} EMarshaller
  */
 
-export {};
+/**
+ * @template [Slot=unknown]
+ * @param {ERemote<Pick<EMarshaller<Slot>, 'fromCapData' | 'toCapData'>>} marshaller
+ * @returns {ReturnType<typeof Far<EMarshaller<Slot>>>}
+ */
+export const wrapRemoteMarshallerDirectSend = marshaller => {
+  /**
+   * @param {Passable} val
+   * @returns {Promise<CapData<Slot>>}
+   */
+  const toCapData = val => E(marshaller).toCapData(val);
+
+  /**
+   * @param {CapData<Slot>} data
+   * @returns {Promise<Passable>}
+   */
+  const fromCapData = data => E(marshaller).fromCapData(data);
+
+  return Far('wrapped remote marshaller', {
+    toCapData,
+    fromCapData,
+
+    // for backwards compatibility
+    /** @deprecated use toCapData */
+    serialize: toCapData,
+    /** @deprecated use fromCapData */
+    unserialize: fromCapData,
+  });
+};
+
+export const wrapRemoteMarshaller = wrapRemoteMarshallerDirectSend;
