e347c6b910c4a7357be6b214fda45685f7e67c22
Turadg Aleahmad <turadg@agoric.com>
2025-10-17 18:28:51 -0700

    refactor: use cachingMarshaller

--- packages/fast-usdc-contract/src/fast-usdc.contract.ts
+++ packages/fast-usdc-contract/src/fast-usdc.contract.ts
@@ -44,10 +44,7 @@ import type {
   Marshaller,
   StorageNode,
 } from '@agoric/internal/src/lib-chainStorage.js';
-import {
-  wrapRemoteMarshaller,
-  type EMarshaller,
-} from '@agoric/internal/src/marshal/wrap-marshaller.js';
+import { type EMarshaller } from '@agoric/internal/src/marshal/wrap-marshaller.js';
 import type { ContractMeta, Invitation, ZCF } from '@agoric/zoe';
 import type { Zone } from '@agoric/zone';
 import { prepareAdvancer } from './exos/advancer.ts';
@@ -122,11 +119,9 @@ export const contract = async (
   assert('USDC' in terms.brands, 'no USDC brand');
   assert('usdcDenom' in terms, 'no usdcDenom');
 
-  const { feeConfig, marshaller: remoteMarshaller, storageNode } = privateArgs;
+  const { feeConfig, storageNode } = privateArgs;
 
-  // TODO(https://github.com/Agoric/agoric-sdk/issues/12109):
-  // once withOrchestration provides a wrapped marshaller, don't re-wrap.
-  const cachingMarshaller = wrapRemoteMarshaller(remoteMarshaller);
+  const { cachingMarshaller } = tools;
   const { makeRecorderKit } = prepareRecorderKitMakers(
     zone.mapStore('vstorage'),
     cachingMarshaller,
