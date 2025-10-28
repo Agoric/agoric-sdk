36104b7cf5b1817b0ad198fc7e38b46237848898
Turadg Aleahmad <turadg@agoric.com>
2025-10-28 15:23:07 -0700

    fix: bad .ts export in portfolio-api

--- packages/portfolio-api/src/main.js
+++ packages/portfolio-api/src/main.js
@@ -1,4 +1,6 @@
 export * from './constants.js';
 export * from './instruments.js';
 export * from './resolver.js';
-export * from './types.js';
+
+// eslint-disable-next-line import/export -- just types
+export * from './types-index.js';
