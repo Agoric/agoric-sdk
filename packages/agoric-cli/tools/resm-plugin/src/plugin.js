8ada618076bc64d21aa6a793178bf8b39e0042ec
Turadg Aleahmad <turadg@agoric.com>
2025-11-14 13:14:43 -0800

    chore(type): nodenext for agoric-cli

--- packages/agoric-cli/tools/resm-plugin/src/plugin.js
+++ packages/agoric-cli/tools/resm-plugin/src/plugin.js
@@ -3,6 +3,7 @@
 import { Far } from '@endo/marshal';
 import { start } from './output.js';
 
+// @ts-expect-error TS1287: A top-level 'export' modifier cannot be used on value declarations in a CommonJS module when 'verbatimModuleSyntax' is enabled.
 export const bootPlugin = () => {
   return Far('plugin', {
     start(_opts) {
