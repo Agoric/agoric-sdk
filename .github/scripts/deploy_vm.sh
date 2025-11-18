394f7bb5e21eecd69c261cf13d70babc16abde25
Muneeb Zia <31257991+Muneeb147@users.noreply.github.com>
2025-11-18 16:21:00 +0500

    fix: Remove env and mount flags in deployment;

--- .github/scripts/deploy_vm.sh
+++ .github/scripts/deploy_vm.sh
@@ -16,6 +16,6 @@ echo "Deploying to $GCE_INSTANCE: $IMAGE"
 
 gcloud compute instances update-container "$GCE_INSTANCE" \
   --zone "$GCE_ZONE" \
-  --container-env-file "$ENV_FILE" \
-  --container-image "$IMAGE" \
-  --container-mount-host-path=mount-path=/db_data,host-path=/var/lib/kv-store,mode=rw
+  --container-image "$IMAGE"
+  # --container-env-file "$ENV_FILE" \
+  # --container-mount-host-path=mount-path=/db_data,host-path=/var/lib/kv-store,mode=rw
