1e4309bf2c05c00a6f84418ce79a9dc4126876a7
rabi-siddique <rabisiddique27@gmail.com>
2025-11-18 10:41:42 +0500

    chore: update values in create2 test

--- packages/portfolio-contract/test/utils/create2.test.ts
+++ packages/portfolio-contract/test/utils/create2.test.ts
@@ -12,8 +12,35 @@ import { contractsMock } from '../mocks.ts';
 const nodeRequire = createRequire(import.meta.url);
 const asset = (spec: string) => readFile(nodeRequire.resolve(spec), 'utf8');
 
+const AVALANCHE_FACTORY_ADDRESS =
+  '0x9F9684d7FA7318698a0030ca16ECC4a01944836b' as const;
+const GATEWAY_ADDRESS = '0xC249632c2D40b9001FE907806902f63038B737Ab' as const;
+const GASSERVICE_ADDRESS =
+  '0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6' as const;
+
 test('predictWalletAddress computes expected address', async t => {
   const cases = [
+    {
+      // See actual tx: https://testnet.snowtrace.io/tx/0x8de043788a3beff7b0940c7e6eab83adcdf0646d8282b466dd8bc59940b4d32d/eventlog?chainid=43113
+      contracts: {
+        factory: AVALANCHE_FACTORY_ADDRESS,
+        gasService: GASSERVICE_ADDRESS,
+        gateway: GATEWAY_ADDRESS,
+      },
+      owner: 'agoric1u4yhqp9tp58hx3kkg2ktj4d9shpukg2q3cx8nv',
+      walletAddress: '0xed49fc80bfe226f6652f556de53b88ca81f22637',
+    },
+
+    {
+      // See actual tx: https://testnet.snowtrace.io/tx/0xd5672e95d47ec2b7d19a54fe296cb345322fd416191633500a141a224e3f3a2d/eventlog?chainid=43113
+      contracts: {
+        factory: AVALANCHE_FACTORY_ADDRESS,
+        gasService: GASSERVICE_ADDRESS,
+        gateway: GATEWAY_ADDRESS,
+      },
+      owner: 'agoric1wrfh296eu2z34p6pah7q04jjuyj3mxu9v98277',
+      walletAddress: '0x68ffd110d64bc38773f2cfd12b5ff46cd4700bca',
+    },
     {
       // from computeAddr.js https://gist.github.com/rabi-siddique/385c92179e5380ef1ef3677a53ffef9d
       //     === CREATE2 Address Calculator ===
@@ -30,16 +57,15 @@ test('predictWalletAddress computes expected address', async t => {
       //   Salt: 0xc588432f2a0ef17c1e3e1eef64e0181fba5fc6cf443dd71f422e65282867fb88
       //   Init Code Hash: 0xd18a438c2dc8ba0f3764d8036e3684fa64b8cae656150d29106189daa729d2fc
       owner: 'agoric10utru593dspjwfewcgdak8lvp9tkz0xttvcnxv',
-      walletAddress: '0x1750ac3a5b2fa7217c9b413a4d0bc37ee6440b70',
+      walletAddress: '0x3433aebfc13e92d0fd808e42d9585dc4792810c2',
     },
-
     {
       // from https://github.com/Agoric/agoric-sdk/commit/de3b64ab64f0055c7106a4011276567b05ae200e
       // which has '0x8fcc8340520552c3cc861acaaa752e2d38bff2bb'
       // how did it get that???
       contracts: contractsMock.Arbitrum,
       owner: 'agoric1qyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqc09z0g',
-      walletAddress: '0x1de1c2e9465120d54dc0ad03da31bdef4ff85a66',
+      walletAddress: '0x68ffd110d64bc38773f2cfd12b5ff46cd4700bca',
     },
   ];
 
