cd29e8450fb04c65defd0049104f81220f015381
Dan Connolly <connolly@agoric.com>
2025-12-08 15:45:49 -0600

    WIP: humanHash stuff

--- packages/portfolio-ui-playground/src/humanHash.ts
+++ packages/portfolio-ui-playground/src/humanHash.ts
@@ -1,35 +1,57 @@
-type FormatString = string; // e.g. ???
+import { fail } from 'node:assert';
+import { EIP712Domain, EIP712Types } from './types.ts';
 
-type ProtobufMsg = { type: string }; // ...
+type EIP712Packet<T = unknown> = {
+  domain: EIP712Domain;
+  types: EIP712Types;
+  message: T;
+}; // ...
 
-type MsgType = string;
-type Formatter = (msg: ProtobufMsg) => string;
-const formats: Record<MsgType, Formatter> = {
+/**
+ * wallet store key, as used in invokeEntry
+ *
+ * TODO: consider confusion risk screw cases
+ */
+type StoreKey = string;
+
+type Formatter = <T>(msg: EIP712Packet<T>) => string;
+const formats: Record<StoreKey, Formatter> = {
   'ymax.OpenPortfolio': m => 'open sesame!',
+};
+/** @throws on not found */
+const getFormatter = (name: string): Formatter => {
+  const formatter = formats[name] || fail(`no formatter for :${name}`);
+  return formatter;
 };
 
 type Bytes = Uint8Array;
 
-const sign = (msg: ProtobufMsg, magic: string) => Promise<Bytes>;
-const broadcast = (stuff: unknown) => Promise<void>;
+const EVMProvider = {
+  /** @throws on signature failure */
+  sign: <T>(msg: EIP712Packet<T>, magic: string) => Promise<Bytes>,
+};
 
-const DApp = {
-  signModeTextual: async (msg: ProtobufMsg) => {
-    const formatter = formats[msg.type];
+const AppServer = {
+  post: (body: unknown) => Promise<void>,
+};
+
+const YmaxUI = {
+  signModeTextual: async <T>(msg: EIP712Packet<T>) => {
+    const formatter = getFormatter(msg.domain.name);
     const magic: string = formatter(msg);
-    const sig = await sign(msg, magic);
-    await broadcast({ msg, sig, mode: 'TEXTUAL' });
+    const sig = await EVMProvider.sign(msg, magic);
+    await AppServer.post({ msg, sig });
   },
 };
 
-const assertSigOk = (sig: Bytes, msg: ProtobufMsg, magic: string) => {
+const assertSigOk = (sig: Bytes, msg: EIP712Packet, magic: string) => {
   // ...
 };
 
 //
 const Validator = {
-  checkSig: ({ msg, sig }: { msg: ProtobufMsg; sig: Bytes }) => {
-    const formatter = formats[msg.type];
+  checkSig: ({ msg, sig }: { msg: EIP712Packet; sig: Bytes }) => {
+    const formatter = getFormatter(msg.domain.name);
     const magic: string = formatter(msg);
     assertSigOk(sig, msg, magic);
   },
