91d6c6873154f128467ce82bde2ab58e4b3d46bf
Waqas Shaukat <waqas.lhr.pnjb@gmail.com>
2025-12-09 21:00:09 +0500

    feat(ci): test-all-packages-integrate Mergify flaky test detection

--- scripts/ci/collect-testruns.sh
+++ scripts/ci/collect-testruns.sh
@@ -2,6 +2,18 @@
 set -o xtrace
 SCRIPT_DIR=$(cd ${0%/*} && pwd -P)
 
+# Convert TAP to JUnit for each package
+for pkg in ./packages/*/_testoutput.txt; do
+  # skip if unable to expand if there are no test files
+  if [[ $pkg != "./packages/*/_testoutput.txt" ]]; then
+    echo "processing $pkg"
+    junit=$(dirname "$pkg")/junit.xml
+    echo "dest $junit"
+    $SCRIPT_DIR/../ava-etap-to-junit.mjs $pkg > $junit
+  fi
+done
+
+# Generate coverage reports
 for cov in ./packages/**/coverage; do
   # skip if unable to expand if there are no test files
   if [[ $cov != "./packages/**/coverage" ]]; then
