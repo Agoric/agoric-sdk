50b319e3a833f72ece94fb3d8f205a3417740555
Turadg Aleahmad <turadg@agoric.com>
2026-01-07 15:59:09 -0800

    test: rely on global VatData

--- packages/zoe/test/unitTests/zoe-env.test.js
+++ packages/zoe/test/unitTests/zoe-env.test.js
@@ -1,7 +1,6 @@
-import {
-  test,
-  VatData,
-} from '@agoric/swingset-vat/tools/prepare-test-env-ava.js';
+import { test } from '@agoric/swingset-vat/tools/prepare-test-env-ava.js';
+
+const VatData = global.VatData;
 
 test('harden from SES is in the zoe contract environment', t => {
   // @ts-expect-error testing existence of function only
