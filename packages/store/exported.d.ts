5e474feea8211886cf81464aa1f39cbb7d7cd250
Turadg Aleahmad <turadg@agoric.com>
2026-01-08 16:35:21 -0800

    chore!: rm store exported.js
    all that was left is ERef

--- packages/store/exported.d.ts
+++
@@ -1,8 +0,0 @@
-/** @file Global definitions of foundational types */
-/** @see {@link /docs/typescript.md} */
-
-import { ERef as _ERef } from '@endo/far';
-
-declare global {
-  export { _ERef as ERef };
-}
