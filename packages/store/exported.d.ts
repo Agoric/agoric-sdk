30bfac554e0f29c5546c449d201f19af948aa32d
Turadg Aleahmad <turadg@agoric.com>
2026-01-08 16:18:50 -0800

    chore(types): unglobal Agoric store types

--- packages/store/exported.d.ts
+++ packages/store/exported.d.ts
@@ -2,21 +2,7 @@
 /** @see {@link /docs/typescript.md} */
 
 import { ERef as _ERef } from '@endo/far';
-import {
-  MapStore as _MapStore,
-  SetStore as _SetStore,
-  StoreOptions as _StoreOptions,
-  WeakMapStore as _WeakMapStore,
-  WeakSetStore as _WeakSetStore,
-} from './src/types.js';
 
 declare global {
-  export {
-    _ERef as ERef,
-    _MapStore as MapStore,
-    _SetStore as SetStore,
-    _StoreOptions as StoreOptions,
-    _WeakMapStore as WeakMapStore,
-    _WeakSetStore as WeakSetStore,
-  };
+  export { _ERef as ERef };
 }
