e7cea5ccd0388f891ba614b527d17759b4422fb6
Turadg Aleahmad <turadg@agoric.com>
2026-02-05 14:38:34 -0800

    chore(types): push down MustMatch in package graph

--- packages/store/src/types-index.d.ts
+++ packages/store/src/types-index.d.ts
@@ -1,2 +1,17 @@
 // eslint-disable-next-line import/export -- just types
 export type * from './types.js';
+
+// TODO make Endo's mustMatch do this
+/**
+ * Returning normally indicates success. Match failure is indicated by
+ * throwing.
+ *
+ * Note: remotables can only be matched as "remotable", not the specific kind.
+ *
+ * @see {endoMustMatch} for the implementation. This one has a type annotation to narrow if the pattern is a TypedPattern.
+ */
+export declare type MustMatch = <P extends Pattern>(
+  specimen: unknown,
+  pattern: P,
+  label?: string | number,
+) => asserts specimen is P extends TypedPattern<any> ? PatternType<P> : unknown;
