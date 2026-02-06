7bbe4e31d657128a51f0c9b97cfdaaa3f63e4780
Turadg Aleahmad <turadg@agoric.com>
2026-02-05 21:02:42 -0800

    Revert "refactor(time): remove notifier type-edge from timer API"
    This reverts commit 2402ada6a1147b17b3e186fcb2584388a127dd3d.

--- packages/time/src/types.ts
+++ packages/time/src/types.ts
@@ -100,15 +100,6 @@ export type RelativeTime = RelativeTimeRecord | RelativeTimeValue;
  */
 export type CancelToken = object;
 
-export type TimeUpdateRecord<T> = {
-  value: T;
-  updateCount?: bigint | undefined;
-};
-
-export type TimeNotifier<T> = {
-  getUpdateSince: (updateCount?: bigint | number) => Promise<TimeUpdateRecord<T>>;
-};
-
 /**
  * Gives the ability to get the current time,
  * schedule a single wake() call, create a repeater that will allow scheduling
@@ -174,7 +165,7 @@ export interface TimerServiceCommon {
     delay: RelativeTime,
     interval: RelativeTime,
     cancelToken?: CancelToken,
-  ) => TimeNotifier<TimestampRecord>;
+  ) => import('@agoric/notifier').Notifier<TimestampRecord>;
   /**
    * Cancel a previously-established wakeup or repeater.
    */
