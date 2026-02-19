67212ce58f8a34e1e3b309c20ab1a173482e46bb
Kris Kowal <kris@agoric.com>
2026-02-18 20:52:48 -0800

    fixup shellcheck codegen

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
+SCRIPT_DIR=$(CDPATH='' cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
 ROOT_DIR=$(cd -- "$SCRIPT_DIR/.." && pwd)
 
 cd "$ROOT_DIR"
