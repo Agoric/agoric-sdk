3df9426b06d5f8dd84734e71baa806f3e7bf7598
Turadg Aleahmad <turadg@agoric.com>
2026-02-25 12:39:43 -0800

    fixup! refactor(internal): add build-cache utilities and adopt in bundle tooling

--- packages/internal/src/build-cache.js
+++ packages/internal/src/build-cache.js
@@ -28,6 +28,35 @@ export const makeDirectoryLock = powers => {
     }
   };
 
+  /** @param {string} dpath */
+  const mkdirUnlessExists = async dpath => {
+    await null; // avoid accidentally catching synchronous exceptions
+    try {
+      await fs.mkdir(dpath);
+      return true;
+    } catch (err) {
+      const e = /** @type {NodeJS.ErrnoException} */ (err);
+      if (e.code === 'EEXIST') {
+        return false;
+      }
+      throw err;
+    }
+  };
+
+  /** @param {string} fpath */
+  const statUnlessMissing = async fpath => {
+    await null; // avoid accidentally catching synchronous exceptions
+    try {
+      return await fs.stat(fpath);
+    } catch (err) {
+      const e = /** @type {NodeJS.ErrnoException} */ (err);
+      if (e.code === 'ENOENT') {
+        return undefined;
+      }
+      throw err;
+    }
+  };
+
   /**
    * @param {string} key
    * @param {() => Promise<any>} body
@@ -37,35 +66,6 @@ export const makeDirectoryLock = powers => {
     const ownerPath = path.join(lockPath, 'owner.json');
     await fs.mkdir(lockRoot, { recursive: true });
     const started = now();
-
-    /** @param {string} dpath */
-    const mkdirUnlessExists = async dpath => {
-      await null; // avoid accidentally catching synchronous exceptions
-      try {
-        await fs.mkdir(dpath);
-        return true;
-      } catch (err) {
-        const e = /** @type {NodeJS.ErrnoException} */ (err);
-        if (e.code === 'EEXIST') {
-          return false;
-        }
-        throw err;
-      }
-    };
-
-    /** @param {string} fpath */
-    const statUnlessMissing = async fpath => {
-      await null; // avoid accidentally catching synchronous exceptions
-      try {
-        return await fs.stat(fpath);
-      } catch (err) {
-        const e = /** @type {NodeJS.ErrnoException} */ (err);
-        if (e.code === 'ENOENT') {
-          return undefined;
-        }
-        throw err;
-      }
-    };
 
     const maybeBreakStaleLock = async () => {
       const ownerText = await fs.readFile(ownerPath, 'utf8').catch(() => '');
