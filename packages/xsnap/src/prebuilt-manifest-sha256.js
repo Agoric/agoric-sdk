8fa6f3428b18e8c5a79d394cd7ead667fbf502ca
Michael FIG <mfig@agoric.com>
2026-04-08 22:51:55 -0600

    chore(xsnap): update 0.15.0 manifestHash

--- packages/xsnap/src/prebuilt-manifest-sha256.js
+++ packages/xsnap/src/prebuilt-manifest-sha256.js
@@ -1,3 +1,4 @@
 export const manifestHashes = {
   '0.14.2': '377030e37b10554e73d0aff655639051d33281c9e17e7da98275fb9efcaae6f2',
+  '0.15.0': 'b481d7d5e66ed17a00624c9fea2f666b097e3bbef0d244201dd26a09e34064f1',
 };
