8ef07145037bf29196553f61c135e18856d45904
Dan Connolly <connolly@agoric.com>
2026-04-15 16:15:44 -0500

    chore(wallet-admin): support upgrade
     - increase timeout to give upgrades 10min to replay async flow logs
     - handle absolute paths in cwd

--- packages/portfolio-deploy/src/wallet-admin-types.ts
+++ packages/portfolio-deploy/src/wallet-admin-types.ts
@@ -7,18 +7,33 @@ import type {
   SigningSmartWalletKit,
   SmartWalletKit,
   reflectWalletStore,
+  makeSigningSmartWalletKit,
 } from '@agoric/client-utils';
+import type { DeliverTxResponse } from '@cosmjs/stargate';
 import type { FileRW } from '@agoric/pola-io/src/file.js';
 import type { E } from '@endo/far';
 
 export type SigningSmartWalletKitWithStore = SigningSmartWalletKit & {
   store: ReturnType<typeof reflectWalletStore>;
+  lastTx?: DeliverTxResponse;
+  txHistory?: readonly DeliverTxResponse[];
 };
+
+type ClientUtilsConnect = Parameters<
+  typeof makeSigningSmartWalletKit
+>[0]['connectWithSigner'];
+
+type ConnectOpts = Parameters<ClientUtilsConnect>[2];
 
 export interface RunTools {
   scriptArgs: string[];
   walletKit: SmartWalletKit;
-  makeAccount(name: string): Promise<SigningSmartWalletKitWithStore>;
+  makeAccount(
+    name: string,
+    opts?: ConnectOpts,
+  ): Promise<SigningSmartWalletKitWithStore>;
+  fetch: typeof globalThis.fetch;
+  setTimeout: typeof globalThis.setTimeout;
   E: typeof E;
   harden: typeof harden;
   cwd: FileRW;
