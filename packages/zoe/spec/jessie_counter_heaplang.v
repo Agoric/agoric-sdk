104251beb6ac5d021942e26620cba13adfebc312
Dan Connolly <dckc@madmode.com>
2026-04-20 10:00:22 -0500

    spec: generalize HeapLang counter proof over monotone attackers
    Refactor the direct HeapLang counter proof so the checked-client theorem is no
    longer hard-coded to the hole and single-incr attackers.
    
    This commit:
    - factors the counter methods as incr_cap / decr_cap helpers
    - proves checked_client_from_mono_attacker_wp, a higher-order theorem saying
      any attacker that preserves the counter cell's lower bound when given incr_cap
      also preserves the final checked-client assertion
    - re-expresses the hole and single-incr proofs as instances of that theorem
    - restores the active HeapLang build to green
    
    This is still not the full OCPL-style all-contexts theorem, but it isolates the
    remaining gap cleanly: derive the monotone-attacker premise from the allowed
    attacker-context class.

--- packages/zoe/spec/jessie_counter_heaplang.v
+++ packages/zoe/spec/jessie_counter_heaplang.v
@@ -1,4 +1,5 @@
 (* Direct HeapLang encoding of makeCounter, for OCPL/Iris-style reuse. *)
+From Coq Require Import ZArith Lia.
 From stdpp Require Import gmap.
 From iris.proofmode Require Import proofmode.
 From iris.base_logic.lib Require Export invariants.
@@ -9,6 +10,12 @@ From iris.heap_lang.lib Require Import assert.
 
 Module JessieCounterHeapLang.
   Open Scope expr_scope.
+
+  Definition incr_cap (l : loc) : val :=
+    λ: <>, #l <- (! #l + #1);; ! #l.
+
+  Definition decr_cap (l : loc) : val :=
+    λ: <>, #l <- (! #l - #1);; ! #l.
 
   Definition makeCounter : val :=
     λ: <>,
@@ -39,47 +46,105 @@ Module JessieCounterHeapLang.
   Section proof.
     Context `{!heapGS Σ}.
 
-    Lemma checked_client_hole_wp :
-      {{{ True }}} checked_client_body hole_attacker {{{ RET #(); True }}}.
+    Lemma checked_client_from_mono_attacker_wp (attacker : val) :
+      (forall (l : loc) (n : Z),
+        {{{ l ↦ #n }}}
+          attacker (incr_cap l)
+        {{{ (v : val) (m : Z), RET v; ⌜(n <= m)%Z⌝ ∗ l ↦ #m }}}) ->
+      {{{ True }}} checked_client_body attacker {{{ RET #(); True }}}.
     Proof.
-      iIntros (Φ) "_ HΦ".
-      rewrite /checked_client_body /hole_attacker /cUp_of /makeCounter.
+      iIntros (Hatt Φ) "_ HΦ".
+      rewrite /checked_client_body /cUp_of /makeCounter.
       wp_lam.
       wp_alloc l as "Hl".
       wp_pures.
-      wp_rec.
+      wp_bind (attacker _).
+      rewrite /incr_cap.
+      specialize (Hatt l 0%Z).
+      wp_apply (Hatt with "Hl").
+      iIntros (v m) "[%Hmono Hl]".
       wp_pures.
+      wp_apply wp_assert.
       wp_load.
       wp_op.
       wp_store.
       wp_load.
       wp_op.
-      wp_if.
-      by iApply "HΦ".
+      iModIntro.
+      iSplit.
+      - iPureIntro.
+        do 2 f_equal.
+        apply bool_decide_eq_true_2.
+        lia.
+      - iNext. by iApply "HΦ".
+    Qed.
+
+    Lemma hole_attacker_mono_wp l n :
+      {{{ l ↦ #n }}}
+        hole_attacker (incr_cap l)
+      {{{ RET #(); ⌜True⌝ ∗ l ↦ #n }}}.
+    Proof.
+      iIntros (Φ) "Hl HΦ".
+      rewrite /hole_attacker /incr_cap.
+      wp_lam.
+      iApply "HΦ".
+      iFrame.
+      done.
+    Qed.
+
+    Lemma single_incr_attacker_mono_wp (l : loc) (n : Z) :
+      {{{ l ↦ #n }}}
+        single_incr_attacker (incr_cap l)
+      {{{ (v : val), RET v; ∃ m : Z, ⌜v = #m⌝ ∗ ⌜(n <= m)%Z⌝ ∗ l ↦ #m }}}.
+    Proof.
+      iIntros (Φ) "Hl HΦ".
+      rewrite /single_incr_attacker /incr_cap.
+      wp_lam.
+      wp_pures.
+      wp_load.
+      wp_op.
+      wp_store.
+      wp_load.
+      iApply ("HΦ" $! #(n + 1)).
+      iExists (n + 1)%Z.
+      iFrame.
+      iPureIntro.
+      split; first reflexivity.
+      lia.
+    Qed.
+
+    Lemma checked_client_hole_wp :
+      {{{ True }}} checked_client_body hole_attacker {{{ RET #(); True }}}.
+    Proof.
+      iIntros (Φ) "_ HΦ".
+      iApply (checked_client_from_mono_attacker_wp hole_attacker).
+      - iIntros (l n Φ') "Hl HΦ'".
+        iApply (hole_attacker_mono_wp with "Hl").
+        iNext.
+        iIntros "[_ Hl]".
+        iApply ("HΦ'" $! #() n).
+        iSplit.
+        + iPureIntro. lia.
+        + iFrame.
+      - done.
+      - iExact "HΦ".
     Qed.
 
     Lemma checked_client_single_incr_wp :
       {{{ True }}} checked_client_body single_incr_attacker {{{ RET #(); True }}}.
     Proof.
       iIntros (Φ) "_ HΦ".
-      rewrite /checked_client_body /single_incr_attacker /cUp_of /makeCounter.
-      wp_lam.
-      wp_alloc l as "Hl".
-      wp_pures.
-      wp_load.
-      wp_op.
-      wp_store.
-      wp_load.
-      wp_pures.
-      wp_rec.
-      wp_pures.
-      wp_load.
-      wp_op.
-      wp_store.
-      wp_load.
-      wp_op.
-      wp_if.
-      by iApply "HΦ".
+      iApply (checked_client_from_mono_attacker_wp single_incr_attacker).
+      - iIntros (l n Φ') "Hl HΦ'".
+        iApply (single_incr_attacker_mono_wp with "Hl").
+        iNext.
+        iIntros (v) "(%m & -> & %Hmono & Hl)".
+        iApply ("HΦ'" $! #m m).
+        iSplit.
+        + iPureIntro. done.
+        + iFrame.
+      - done.
+      - iExact "HΦ".
     Qed.
   End proof.
 
