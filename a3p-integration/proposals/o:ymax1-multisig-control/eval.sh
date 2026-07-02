11be30d1653109c4ce08ab037dac2bdeb4450d37
Dan Connolly <connolly@agoric.com>
2026-07-02 12:52:46 -0500

    test(a3p): start ymax1 contract in persistent history

--- a3p-integration/proposals/o:ymax1-multisig-control/eval.sh
+++ a3p-integration/proposals/o:ymax1-multisig-control/eval.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+# from g:ymax1/use.sh
+export YMAX1_CONTROL_MNEMONIC="swing matrix country boring segment void similar cliff illness any pulse object quantum viable unveil carbon gap thunder merge screen combine core dog control"
+
+pkg=/usr/src/agoric-sdk/packages/portfolio-deploy
+$pkg/scripts/wallet-admin.ts $pkg/test/start-ymax1.ts
+
 cp /usr/src/upgrade-test-scripts/eval_submission.js .
 yarn node ./eval_submission.js
