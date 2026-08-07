aa07d79559628cef0e0ef0472279837722c2229f
Dan Connolly <connolly@agoric.com>
2026-08-07 15:25:32 -0500

    test(a3p): exercise instrument oracle upgrade

--- a3p-integration/proposals/f:ymax0-restart/eval.sh
+++ a3p-integration/proposals/f:ymax0-restart/eval.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "no eval"
+source /usr/src/upgrade-test-scripts/env_setup.sh
+
+bundle_file=./bundle-ymax0-2605.json
+private_args_file=./privateArgsOverrides-2604.json
+
+curl --fail --location --silent --show-error \
+  --output "$bundle_file" \
+  https://github.com/Agoric/agoric-sdk/releases/download/ymax-v0.3.2605-beta1/bundle-ymax0.json
+curl --fail --location --silent --show-error \
+  --output "$private_args_file" \
+  https://github.com/Agoric/agoric-sdk/releases/download/ymax-v0.3.2604-beta1/privateArgsOverrides.json
+
+sha256sum --check <<EOF
+6eb97a6237622ffdc1b4bebb28c0b104ae8adba6b961b5189bd551b5002d5da6  $bundle_file
+1c9643ddc5d508fc1fa8298493f9767ceaffa685eca2685096f4583c03836e61  $private_args_file
+EOF
+
+test "$(jq -r .endoZipBase64Sha512 "$bundle_file")" = \
+  03d5ff17d1f29f8d1993525d0a9e82e6cd74b117a64cff64d3ca7e246bc69428d8e7d6947b69ee6bf37c024d16920490e684f83f263d6fed0c6ba875d57bf621
+
+# shellcheck disable=SC2086
+agd tx swingset install-bundle @"$bundle_file" \
+  $SIGN_BROADCAST_OPTS --from validator
---
+++ a3p-integration/proposals/f:ymax0-restart/use.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -euo pipefail
+
+./restart-with-release.js
