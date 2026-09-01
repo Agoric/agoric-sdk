283caf25e7e1c91fe7a08748cf35188a86e9c23c
Richard Gibson <richard.gibson@gmail.com>
2026-09-01 18:37:05 -0400

    test(ymax-planner): Add more `scaleToNat` test cases

--- services/ymax-planner/test/ses-utils.test.ts
+++ services/ymax-planner/test/ses-utils.test.ts
@@ -15,10 +15,20 @@ test('scaleToNat input validation', t => {
 test('scaleToNat fixed place count', t => {
   t.is(scaleToNat(1.23, 2), 123n);
   t.is(scaleToNat(1.23, 3), 1230n);
+  t.is(scaleToNat(0.01, 2), 1n);
+  t.is(scaleToNat(0.01, 3), 10n);
+  t.is(scaleToNat(0, 2), 0n);
+  t.is(scaleToNat(0, 3), 0n);
+
+  t.is(scaleToNat(0.09, 1, 0), 1n);
+  t.is(scaleToNat(0.09, 1, 1), 1n);
+  t.is(scaleToNat(1.09, 1, 0), 11n);
+  t.is(scaleToNat(1.09, 1, 1), 11n);
 });
 
 test('scaleToNat strictness', t => {
   const lossy = { message: /precision loss/ };
+
   t.throws(
     () => scaleToNat(17.578324000000002, 6),
     lossy,
@@ -27,4 +37,17 @@ test('scaleToNat strictness', t => {
   t.throws(() => scaleToNat(17.578324000000002, 6, 9), lossy);
   t.is(scaleToNat(17.578324000000002, 6, 8), 17_578_324n);
   t.is(scaleToNat(17.578320000000002, 5, 9), 1_757_832n);
+
+  t.is(scaleToNat(1.00991, 2, 2), 101n);
+  t.is(scaleToNat(1.0099, 2, 2), 101n);
+  t.throws(() => scaleToNat(1.0098, 2, 2), lossy);
+  t.throws(() => scaleToNat(1.009, 2, 2), lossy);
+
+  // Exhaustively check 0.00_01_##.
+  for (let i = 0; i < 100; i += 1) {
+    const x = Number(`0.0001${String(i).padStart(2, '0')}`);
+    t.throws(() => scaleToNat(x, 2, 2), lossy, `x=${x}`);
+    t.is(scaleToNat(x, 2, 1), 0n, `x=${x}`);
+    t.is(scaleToNat(x, 2, 0), 0n, `x=${x}`);
+  }
 });
